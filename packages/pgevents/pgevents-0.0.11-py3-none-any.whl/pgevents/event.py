2ed7d50e790b117b12834850ed30114d6bd956d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:11 +0000

    Add package pgevents/0.0.11/pgevents-0.0.11-py3-none-any.whl

---
+++ packages/pgevents/pgevents-0.0.11-py3-none-any.whl/pgevents/event.py
@@ -0,0 +1,50 @@
+import dataclasses
+import logging
+from datetime import datetime
+from typing import Optional, Any
+
+from pgevents import data_access, constants
+
+LOGGER = logging.getLogger(__name__)
+
+
+@dataclasses.dataclass
+class Event:
+    topic: str
+    status: str = constants.PENDING
+    id: Optional[int] = None
+    payload: Optional[Any] = None
+    created_at: Optional[datetime] = None
+    process_after: Optional[datetime] = None
+    processed_at: Optional[datetime] = None
+
+    @classmethod
+    def from_dict(cls, data):
+        return cls(
+            topic=data["topic"],
+            status=data["status"],
+            id=data["id"],
+            payload=data["payload"],
+            created_at=data["created_at"],
+            process_after=data["process_after"],
+            processed_at=data["processed_at"],
+        )
+
+    def mark_processed(self, cursor):
+        assert self.id is not None, "Cannot mark processed when id is not set"
+        data_access.mark_event_processed(cursor, self.id)
+
+    def __repr__(self):
+        name = self.__class__.__qualname__
+        fields = dataclasses.fields(self)
+        fields_string = f", ".join([self.repr_field(field) for field in fields])
+        return f"{name}({fields_string})"
+
+    def repr_field(self, field):
+        value = getattr(self, field.name)
+        return f"{field.name}={_to_truncated_string(value)}"
+
+
+def _to_truncated_string(value, max_lenth=20):
+    full = str(value)
+    return full if len(full) < max_lenth else (full[:max_lenth] + "...")
