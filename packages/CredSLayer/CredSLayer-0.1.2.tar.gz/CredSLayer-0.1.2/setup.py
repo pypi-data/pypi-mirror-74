9fe73b5d2044494b23dba648273b6b4667a8e42a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:04 +0000

    Add package CredSLayer/0.1.2/CredSLayer-0.1.2.tar.gz

---
+++ packages/CredSLayer/CredSLayer-0.1.2.tar.gz/CredSLayer-0.1.2/setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+# coding: utf-8
+
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='CredSLayer',
+    version='0.1.2',
+    description='Extract credentials and other useful info from network captures',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    keywords="credentials security networking extraction mining wireshark tshark dissector password",
+    license="GPLv3",
+    license_files=["LICENSE"],
+    author='ShellCode',
+    author_email='shellcode33@protonmail.ch',
+    url='https://github.com/ShellCode33/CredSLayer',
+    packages=find_packages(),
+    python_requires='>=3.5',
+
+    classifiers=[
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: POSIX :: Linux",
+    ],
+
+    install_requires=[
+        "pyshark"
+    ],
+
+    entry_points={
+        "console_scripts": [
+            "credslayer = credslayer.credslayer:main",
+        ]
+    }
+)
