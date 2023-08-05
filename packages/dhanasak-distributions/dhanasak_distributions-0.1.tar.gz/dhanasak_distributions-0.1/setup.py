f4f3c371f5482170505614e730c79c873a3355c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:24 +0000

    Add package dhanasak-distributions/0.1/dhanasak_distributions-0.1.tar.gz

---
+++ packages/dhanasak-distributions/dhanasak_distributions-0.1.tar.gz/dhanasak_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dhanasak_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['dhanasak_distributions'],
+      author = 'Abhishek Dhanasetty',
+      author_email = 'dhanasak18@gmail.com',
+      zip_safe=False)
