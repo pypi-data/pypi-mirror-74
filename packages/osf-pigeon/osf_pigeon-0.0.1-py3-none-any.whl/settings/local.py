53d81a0dabd01b20c644cc3b5abf28db29f416ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:47 +0000

    Add package osf-pigeon/0.0.1/osf_pigeon-0.0.1-py3-none-any.whl

---
+++ packages/osf-pigeon/osf_pigeon-0.0.1-py3-none-any.whl/settings/local.py
@@ -0,0 +1,19 @@
+# New tokens can be found at https://archive.org/account/s3.php
+IA_ACCESS_KEY = 'lEhXyNqH31rfpn6M'
+IA_SECRET_KEY = 'p0rXygXhMHzX5PMa'
+'''
+#IA_ACCESS_KEY = 'wqp8Wh3HP1KX6T0b'
+#IA_SECRET_KEY = 'SObYp8M7XQ6mehfE'
+'''
+DOI_FORMAT = '{prefix}/FK2osf.io/{guid}'
+
+DATACITE_USERNAME = 'DEMO.COS'
+DATACITE_PASSWORD = 'C0SCO$Cos'
+DATACITE_URL = 'https://mds.test.datacite.org/'
+DATACITE_PREFIX = '10.70102'  # Datacite's test DOI prefix -- update in production
+
+OSF_API_URL = 'https://api.staging.osf.io/'
+OSF_FILES_URL = 'https://files.us.staging.osf.io/'
+
+OSF_THROTTLE_ENABLED = False
+OSF_BEARER_TOKEN = 'IWue2T1dczNu5pkyU0sJUaczk9EibCJPWIs0maWpOMqsCdx7YW6wtP6n6fWy'
