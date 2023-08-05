edb534be1b9be779cb960a9d428fc6d72946e634
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:43 +0000

    Add package napoleontoolbox/2.5.78/napoleontoolbox-2.5.78.tar.gz

---
+++ packages/napoleontoolbox/napoleontoolbox-2.5.78.tar.gz/napoleontoolbox-2.5.78/napoleontoolbox/parallel_run/launcher_utility.py
@@ -0,0 +1,7 @@
+def get_database_name(user, frequence, underlying, algo, starting_date, running_date, db_path_suffix):
+    return  user + '_' + frequence + '_' + underlying + '_' + algo + '_'+ starting_date.strftime(
+        '%d_%b_%Y') + '_' + running_date.strftime('%d_%b_%Y') + db_path_suffix
+
+def get_daily_database_name(user, frequence, underlying, algo, starting_date, running_date, delay, db_path_suffix):
+    return  user + '_' + frequence + '_' + underlying + '_' + algo + '_'+ starting_date.strftime(
+        '%d_%b_%Y') + '_' + running_date.strftime('%d_%b_%Y')+ '_' + str(delay) + '_' +  db_path_suffix
