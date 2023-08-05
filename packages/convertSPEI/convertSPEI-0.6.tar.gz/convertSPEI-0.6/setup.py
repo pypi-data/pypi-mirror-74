624a5e468f74c479ffcc6a0b62329f74b879ee72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:43 +0000

    Add package convertSPEI/0.6/convertSPEI-0.6.tar.gz

---
+++ packages/convertSPEI/convertSPEI-0.6.tar.gz/convertSPEI-0.6/setup.py
@@ -0,0 +1,36 @@
+from setuptools import Extension, setup
+from distutils.command.sdist import sdist as _sdist
+
+from Cython.Build import cythonize
+from Cython.Distutils import build_ext
+
+
+cmdclass = {}
+
+
+class sdist(_sdist):
+    def run(self):
+        cythonize(
+            ext_modules, annotate=True, compiler_directives={"embedsignature": True}
+        )
+        _sdist.run(self)
+
+
+cmdclass["sdist"] = sdist
+cmdclass.update({"build_ext": build_ext})
+ext_modules = cythonize([Extension("scripts.append_df", ["scripts/append_df.pyx"])])
+
+setup(
+    name="convertSPEI",
+    description="A Python script to convert Standardised Precipitation-Evapotranspiration Index (SPEI) data from the netcdf format to csv and Excel formats.",
+    version="0.6",
+    author="Uthpala Herath",
+    author_email="ukh0001@mix.wvu.edu",
+    url="https://github.com/uthpalaherath/convertSPEI",
+    download_url="https://github.com/uthpalaherath/convertSPEI/archive/0.6.tar.gz",
+    license="LICENSE.txt",
+    scripts=["scripts/convertSPEI.py", "scripts/append_df.pyx", "scripts/append_df.c"],
+    install_requires=["pandas", "cython", "netcdf4"],
+    keywords=["netcdf", "spei", "atmosphereicsciences", "earthsciences"],
+    ext_modules=ext_modules,
+)
