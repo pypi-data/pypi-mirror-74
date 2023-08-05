4f7a22ce38a2a6b69a7bba3a80426429fb341868
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:29 +0000

    Add package python-can/4.0.0.dev0/python_can-4.0.0.dev0-py3.7.egg

---
+++ packages/python-can/python_can-4.0.0.dev0-py3.7.egg/EGG-INFO/scripts/can_logger.py
@@ -0,0 +1,11 @@
+#!/home/travis/virtualenv/python3.7.1/bin/python
+
+"""
+See :mod:`can.logger`.
+"""
+
+from can.logger import main
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/python-can/python_can-4.0.0.dev0-py3.7.egg/EGG-INFO/scripts/can_player.py
@@ -0,0 +1,11 @@
+#!/home/travis/virtualenv/python3.7.1/bin/python
+
+"""
+See :mod:`can.player`.
+"""
+
+from can.player import main
+
+
+if __name__ == "__main__":
+    main()
