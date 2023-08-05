aafb9900c100cf1be42cdc143f95e2f19023f4bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:13 +0000

    Add package cvdast/1.1.1/cvdast-1.1.1-py3-none-any.whl

---
+++ packages/cvdast/cvdast-1.1.1-py3-none-any.whl/cvdast/tests/test_feedItems_navigation_homeShortcuts.py
@@ -0,0 +1,30 @@
+import requests
+import json
+from jinja2 import Template
+import pytest
+import assertions
+
+HOST_URL = ['shared-vpclink-nlb-latest-tea2-dcd6d62d99a127f5.elb.us-east-1.amazonaws.com']
+
+
+def _trigger_requests(req_method, url, header, data, proxies=None):
+    print("\n\nRegenerating traffic from CloudVector events....")
+    return requests.request(method=req_method, url=url, proxies=proxies, headers=header, data=data, verify=False)
+
+
+def test_feeditems_navigation_homeshortcuts():
+    data = {}
+    
+    req = {
+             "data": data,
+             "headers": {'Authorization': 'khkhkhk', 'BodyId': 123, 'Origin-RequestId': ''}
+          }
+    url = Template("/v1/feedItems/navigation/homeShortcuts").render(**data)
+    resp = _trigger_requests("get", url,
+                      header=req["headers"],
+                      data=json.dumps(data))
+    print(resp.status_code)
+    print(resp.text)
+    assertions.assert_for_feedItems_navigation_homeShortcuts(req,resp)
+
+
---
+++ packages/cvdast/cvdast-1.1.1-py3-none-any.whl/cvdast/tests/test_offers_contentId.py
@@ -0,0 +1,31 @@
+import requests
+import json
+from jinja2 import Template
+import pytest
+import assertions
+
+HOST_URL = ['shared-vpclink-nlb-latest-tea2-dcd6d62d99a127f5.elb.us-east-1.amazonaws.com']
+
+
+def _trigger_requests(req_method, url, header, data, proxies=None):
+    print("\n\nRegenerating traffic from CloudVector events....")
+    return requests.request(method=req_method, url=url, proxies=proxies, headers=header, data=data, verify=False)
+
+
+def test_offers_contentid(contentId):
+    data = {}
+    data["contentId"] = contentId
+    
+    req = {
+             "data": data,
+             "headers": {'Authorization': 'khkhkhk', 'BodyId': 123, 'Origin-RequestId': ''}
+          }
+    url = Template("/v1/offers/{{contentId}}").render(**data)
+    resp = _trigger_requests("get", url,
+                      header=req["headers"],
+                      data=json.dumps(data))
+    print(resp.status_code)
+    print(resp.text)
+    assertions.assert_for_offers_contentId(req,resp)
+
+
