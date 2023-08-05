8ab27222ac7e792ad08f25ace76cc7d6f81e9771
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:49 +0000

    Add package openai-finetune/0.0.1rc4/openai_finetune-0.0.1rc4.tar.gz

---
+++ packages/openai-finetune/openai_finetune-0.0.1rc4.tar.gz/openai_finetune-0.0.1rc4/setup.py
@@ -0,0 +1,30 @@
+import os
+
+from setuptools import find_packages, setup
+
+version_contents = {}
+with open(
+    os.path.join(os.path.abspath(os.path.dirname(__file__)), "openai_finetune/version.py")
+) as f:
+    exec(f.read(), version_contents)
+
+setup(
+    name="openai_finetune",
+    description="Python client library for the OpenAI Fine-tuning API",
+    version=version_contents["VERSION"],
+    install_requires=[
+        'requests >= 2.20; python_version >= "3.0"',
+        'requests[security] >= 2.20; python_version < "3.0"',
+        'numpy',
+        'transformers',
+        'blobfile>=0.16.8'
+        'openai>=0.2.3',
+    ],
+    python_requires=">=3.5",
+    scripts=["bin/openai-ft"],
+    packages=find_packages(exclude=["tests", "tests.*"]),
+    # package_data={"openai": ["data/ca-certificates.crt"]},
+    author="OpenAI",
+    author_email="support@openai.com",
+    url="https://github.com/openai/openai-finetune",
+)
