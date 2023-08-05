8537665a26d9eaad6e3e22ca71edb5b464550cdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:28 +0000

    Add package predictably/0.1.1/predictably-0.1.1.tar.gz

---
+++ packages/predictably/predictably-0.1.1.tar.gz/predictably-0.1.1/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='predictably',
+    version='0.1.1',
+    packages=setuptools.find_packages(exclude=('test', 'test_R')),
+    url='https://rnkuhns.github.io/predict-ably/',
+    license='BSD-3',
+    author='Ryan Kuhns',
+    author_email='rnkuhns@gmail.com',
+    description='Scikit-learn compatible Python forecasting module',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Development Status :: 1 - Planning"
+    ],
+    install_requires=['numpy>=1.13.3', 'scipy==0.19.1', 
+                      'statsmodels>=0.10', 'scikit-learn'],
+    tests_require=['pytest'],
+    extras_require={
+        'dev': ['pip-tools', 'rpy2'],
+    }
+)
