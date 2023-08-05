e2f11e741afe14e9db907267600d78482fcc69f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:34 +0000

    Add package KGlobal/1.5.3/KGlobal-1.5.3.tar.gz

---
+++ packages/KGlobal/KGlobal-1.5.3.tar.gz/KGlobal-1.5.3/KGlobal/data/__init__.py
@@ -0,0 +1,13 @@
+from __future__ import unicode_literals
+
+from .config import DataConfig, file_read_bytes, file_write_bytes, file_write_text, file_move, file_delete, KeyPtr
+from .cryptography import SaltHandle, CryptHandle
+from .create_key import create_key
+
+__version__ = '1.0.0'
+
+__all__ = [
+    "DataConfig", "CryptHandle", "SaltHandle",
+    "create_key", "file_read_bytes", "file_write_bytes", "file_write_text", "file_move",
+    "file_delete", "KeyPtr"
+]
