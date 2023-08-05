a466400af5f9fe929be6e582bef152ea457ad641
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:54 +0000

    Add package deepchem/2.4.0rc1.dev20200715191625/deepchem-2.4.0rc1.dev20200715191625-py3.7.egg

---
+++ packages/deepchem/deepchem-2.4.0rc1.dev20200715191625-py3.7.egg/deepchem/dock/__init__.py
@@ -0,0 +1,7 @@
+"""
+Imports all submodules 
+"""
+from deepchem.dock.pose_generation import PoseGenerator
+from deepchem.dock.pose_generation import VinaPoseGenerator
+from deepchem.dock.docking import Docker
+from deepchem.dock.binding_pocket import ConvexHullPocketFinder
