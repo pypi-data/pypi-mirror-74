425632d4646a4659f0bcf9742d8dd66e79c20c77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:44 +0000

    Add package coucharchive/2.4.0/coucharchive-2.4.0.tar.gz

---
+++ packages/coucharchive/coucharchive-2.4.0.tar.gz/coucharchive-2.4.0/setup.py
@@ -0,0 +1,20 @@
+# Copyright 2016 Adrien Vergé
+# All rights reserved
+
+from setuptools import setup
+
+
+setup(
+    name='coucharchive',
+    version='2.4.0',
+    author='Adrien Vergé',
+    url='https://github.com/adrienverge/coucharchive',
+    license='MIT',
+    description=('Create and restore backups of a whole CouchDB server, with '
+                 'simple tar.gz files.'),
+
+    scripts=['coucharchive'],
+    install_requires=[
+        'CouchDB >=1.2.0',
+    ],
+)
