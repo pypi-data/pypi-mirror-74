38f4aa5505211c50dd9ea8ddeb462342943585e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:23 +0000

    Add package curtsies/0.3.4/curtsies-0.3.4.tar.gz

---
+++ packages/curtsies/curtsies-0.3.4.tar.gz/curtsies-0.3.4/examples/demo_fullscreen_with_input.py
@@ -0,0 +1,27 @@
+import sys
+import signal
+
+from curtsies import input, Cbreak, FullscreenWindow, fmtstr
+
+def fullscreen_winch_with_input():
+    """
+    Monitors user input as well as screen size and acknowledges changes to both.
+    """
+    print('this should be just off-screen')
+    w = FullscreenWindow(sys.stdout)
+    def sigwinch_handler(signum, frame):
+        print('sigwinch! Changed from %r to %r' % ((rows, columns), (w.height, w.width)))
+    signal.signal(signal.SIGWINCH, sigwinch_handler)
+    with w:
+        with Cbreak(sys.stdin):
+            for e in input.Input():
+                rows, columns = w.height, w.width
+                a = [fmtstr((('.%sx%s.%r.' % (rows, columns, e)) * rows)[:columns]) for row in range(rows)]
+                w.render_to_terminal(a)
+
+                if e == u'<ESC>':
+                    break
+
+if __name__ == '__main__':
+    fullscreen_winch_with_input()
+
---
+++ packages/curtsies/curtsies-0.3.4.tar.gz/curtsies-0.3.4/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup
+import ast
+import os
+import io
+
+
+def version():
+    """Return version string."""
+    with open(os.path.join("curtsies", "__init__.py")) as input_file:
+        for line in input_file:
+            if line.startswith("__version__"):
+                return ast.parse(line).body[0].value.s
+
+
+setup(
+    name="curtsies",
+    version=version(),
+    description="Curses-like terminal wrapper, with colored strings!",
+    long_description=open("readme.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/bpython/curtsies",
+    author="Thomas Ballinger",
+    author_email="thomasballinger@gmail.com",
+    license="MIT",
+    packages=["curtsies"],
+    install_requires=[
+        "blessings>=1.5",
+        "wcwidth>=0.1.4",
+        'typing; python_version<"3.5"',
+    ],
+    tests_require=["mock", "pyte", "nose",],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+    ],
+    zip_safe=False,
+)
