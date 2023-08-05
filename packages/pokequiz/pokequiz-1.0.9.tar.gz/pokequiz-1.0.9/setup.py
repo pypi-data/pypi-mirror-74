1a7a064b93b9a67be56966acda1ec2b5be1b62c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:38 +0000

    Add package pokequiz/1.0.9/pokequiz-1.0.9.tar.gz

---
+++ packages/pokequiz/pokequiz-1.0.9.tar.gz/pokequiz-1.0.9/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="pokequiz",
+    version="1.0.9",
+    author="Derek Andersen",
+    author_email="derekcandersen@gmail.com",
+    description="A command line Pokemon studying tool.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Dechrissen/PokeQuiz",
+    #packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    entry_points={
+    'console_scripts': [
+        'pokequiz = pokequiz.Quiz:quiz'
+    ]
+    },
+    python_requires='>=3.6'
+)
