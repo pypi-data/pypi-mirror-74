44e09290c54aeb94ee7fff04009371347d41ad19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:46 +0000

    Add package mukeshdistributions/0.1/mukeshdistributions-0.1.tar.gz

---
+++ packages/mukeshdistributions/mukeshdistributions-0.1.tar.gz/mukeshdistributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='mukeshdistributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['mukeshdistributions'],
+      author = 'Mukesh Adhikari',
+      email = 'mukeshadhikari118@gmail.com',
+      zip_safe=False)
