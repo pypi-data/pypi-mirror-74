2b39be2d491b6c91f861587c0c6c0cb30355086a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:39 +0000

    Add package fastrec/0.0.4/fastrec-0.0.4.tar.gz

---
+++ packages/fastrec/fastrec-0.0.4.tar.gz/fastrec-0.0.4/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+def readme():
+	with open('README.md') as f:
+		return f.read()
+
+setup(name='fastrec',
+	  version='0.0.4',
+	  description='Rapidly deployed gnn based recommender',
+	  long_description=readme(),
+	  url='https://github.com/devinjdangelo/FastRec',
+	  author='Devin DAngelo',
+	  packages=['fastrec'],
+	  scripts=['fastrec/fastrec-deploy'],
+	  install_requires=['fastapi','uvicorn','tqdm','pandas'],
+	  dependency_links=['https://download.pytorch.org/whl/torch_stable.html'],
+	  keywords='recommender graph neural network gnn deployment deploy',
+	  include_package_data=True,
+	  long_description_content_type="text/markdown",
+	  test_suite='nose.collector',
+	  tests_require=['nose'],
+	  zip_safe=False)
