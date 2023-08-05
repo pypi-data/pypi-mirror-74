5e9dc8da146edd58aa1deb9fb541456627f9d857
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:45 +0000

    Add package sqlanalyzer/0.4.4/sqlanalyzer-0.4.4.tar.gz

---
+++ packages/sqlanalyzer/sqlanalyzer-0.4.4.tar.gz/sqlanalyzer-0.4.4/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(name='sqlanalyzer',
+      version='0.4.4',
+      description='A tool to parse and analyze the structure for Postgres sql queries.',
+      classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.5',
+        'Development Status :: 3 - Alpha',
+      ],
+      url='https://github.com/mathilda0902/sqlanalyzer',
+      author='Vicky Kwan',
+      author_email='vickyj.fan2016@gmail.com',
+      license='MIT',
+      packages=['sqlanalyzer'],
+      install_requires=['sqlparse==0.3.0'],
+      python_requires='>=3.5',
+      zip_safe=False)
+      
