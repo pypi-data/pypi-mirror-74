b089f6f5ca1ff4bc4e3dec51f18009a525cbff90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:17 +0000

    Add package flask-resources/0.2.0/flask_resources-0.2.0-py2.py3-none-any.whl

---
+++ packages/flask-resources/flask_resources-0.2.0-py2.py3-none-any.whl/flask_resources/parsers/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2020 CERN.
+#
+# Flask-Resources is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+"""Library for easily implementing REST APIs."""
+
+from .parsers import ArgsParser, url_args_parser
+
+__all__ = (
+    "ArgsParser",
+    "url_args_parser",
+)
