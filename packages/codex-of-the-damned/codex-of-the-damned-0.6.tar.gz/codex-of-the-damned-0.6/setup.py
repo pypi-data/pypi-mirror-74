f5291da25632b81ec32eb21ade58ca67426704f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:37:27 +0000

    Add package codex-of-the-damned/0.6/codex-of-the-damned-0.6.tar.gz

---
+++ packages/codex-of-the-damned/codex-of-the-damned-0.6.tar.gz/codex-of-the-damned-0.6/setup.py
@@ -0,0 +1,45 @@
+import setuptools
+import setuptools.command.develop
+import setuptools.command.install
+
+
+class InstallWithCompile(setuptools.command.install.install):
+    def run(self):
+        from babel.messages.frontend import compile_catalog
+
+        compiler = compile_catalog(self.distribution)
+        compiler.domain = ["messages"]
+        compiler.directory = "codex_of_the_damned/translations"
+        compiler.run()
+        super().run()
+
+
+class DevelopWithCompile(setuptools.command.develop.develop):
+    def run(self):
+        from babel.messages.frontend import compile_catalog
+
+        compiler = compile_catalog(self.distribution)
+        compiler.domain = ["messages"]
+        compiler.directory = "codex_of_the_damned/translations"
+        compiler.run()
+        super().run()
+
+
+class SDistWithCompile(setuptools.command.sdist.sdist):
+    def run(self):
+        from babel.messages.frontend import compile_catalog
+
+        compiler = compile_catalog(self.distribution)
+        compiler.domain = ["messages"]
+        compiler.directory = "codex_of_the_damned/translations"
+        compiler.run()
+        super().run()
+
+
+setuptools.setup(
+    cmdclass={
+        "install": InstallWithCompile,
+        "develop": DevelopWithCompile,
+        "sdist": SDistWithCompile,
+    }
+)
