12decebca40c1bb5b067fcedf8e5eb881f78d47f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:41 +0000

    Add package brs-utils/0.1.3/brs_utils-0.1.3.tar.gz

---
+++ packages/brs-utils/brs_utils-0.1.3.tar.gz/brs_utils-0.1.3/brs_utils/misc.py
@@ -0,0 +1,86 @@
+"""
+Created on June 16 2020
+
+@author: Joan Hérisson
+"""
+
+from sys import getsizeof, stderr
+from collections import deque
+from itertools import chain
+try:
+    from reprlib import repr
+except ImportError:
+    pass
+from os import makedirs
+from requests import get as r_get
+from tempfile import NamedTemporaryFile
+from tarfile import open as tf_open
+
+
+def total_size(o, handlers={}, verbose=False):
+    """ Returns the approximate memory footprint an object and all of its contents.
+
+    Automatically finds the contents of the following builtin containers and
+    their subclasses:  tuple, list, deque, dict, set and frozenset.
+    To search other containers, add handlers to iterate over their contents:
+
+        handlers = {SomeContainerClass: iter,
+                    OtherContainerClass: OtherContainerClass.get_elements}
+
+    """
+    dict_handler = lambda d: chain.from_iterable(d.items())
+    all_handlers = {tuple: iter,
+                    list: iter,
+                    deque: iter,
+                    dict: dict_handler,
+                    set: iter,
+                    frozenset: iter,
+                   }
+    all_handlers.update(handlers)     # user handlers take precedence
+    seen = set()                      # track which object id's have already been seen
+    default_size = getsizeof(0)       # estimate sizeof object without __sizeof__
+
+    def sizeof(o):
+        if id(o) in seen:       # do not double count the same object
+            return 0
+        seen.add(id(o))
+        s = getsizeof(o, default_size)
+
+        if verbose:
+            print(s, type(o), repr(o), file=stderr)
+
+        for typ, handler in all_handlers.items():
+            if isinstance(o, typ):
+                s += sum(map(sizeof, handler(o)))
+                break
+        return s
+
+    return sizeof(o)
+
+def check_nb_args(*args, f_name, nb_args):
+    if len(args) < nb_args:
+        raise TypeError(f_name+' missing '+str(nb_args)+' required positional argument')
+    elif len(args) > nb_args:
+        raise TypeError(f_name+' takes '+str(nb_args)+' positional arguments but '+str(len(args))+' were given')
+    return True
+
+def download(url, file):
+    r = r_get(url)
+    open(file, 'wb').write(r.content)
+
+def extract_gz(file, path):
+    print(path)
+    makedirs(path, exist_ok=True)
+    tar = tf_open(file, mode="r:gz")
+    tar.extractall(path)
+    tar.close()
+
+def download_and_extract_gz(url, path):
+    with NamedTemporaryFile() as tempf:
+        download(url, tempf.name)
+        extract_gz(tempf.name, path)
+
+def file_length(filename):
+    with open(filename, 'rb') as f:
+        n = sum(1 for line in f)
+    return n
