46ce0bed7db9f682207595db2d6088ae941ab115
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:34 +0000

    Add package uconf/0.4.1/uconf-0.4.1-py3-none-any.whl

---
+++ packages/uconf/uconf-0.4.1-py3-none-any.whl/uconf/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# This software is distributed under the two-clause BSD license.
+
+__version__ = '0.4.1'
+__author__ = "Raphaël Barrois <raphael.barrois+uconf@polytechnique.org>"
