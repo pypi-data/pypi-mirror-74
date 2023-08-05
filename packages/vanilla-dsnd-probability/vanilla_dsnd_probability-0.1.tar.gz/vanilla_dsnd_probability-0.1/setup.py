e3a2f0c1fe0a7de05a574fa225d966ff33e84327
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:21 +0000

    Add package vanilla-dsnd-probability/0.1/vanilla_dsnd_probability-0.1.tar.gz

---
+++ packages/vanilla-dsnd-probability/vanilla_dsnd_probability-0.1.tar.gz/vanilla_dsnd_probability-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='vanilla_dsnd_probability',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['vanilla_dsnd_probability'],
+      author='Shakthivanilla',
+      author_email='shakthivanilla@gmail.com',
+      zip_safe=False)
