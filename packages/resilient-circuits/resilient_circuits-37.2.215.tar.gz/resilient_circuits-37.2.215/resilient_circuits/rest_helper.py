b01753e718f7646c546156d5e25056bb4e3a8461
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:55 +0000

    Add package resilient-circuits/37.2.215/resilient_circuits-37.2.215.tar.gz

---
+++ packages/resilient-circuits/resilient_circuits-37.2.215.tar.gz/resilient_circuits-37.2.215/resilient_circuits/rest_helper.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# (c) Copyright IBM Corp. 2010, 2019. All Rights Reserved.
+
+"""Global accessor for the Resilient REST API"""
+
+import resilient
+
+resilient_client = None
+connection_opts = None
+
+
+def reset_resilient_client():
+    """Reset the cached client"""
+    global resilient_client
+    resilient_client = None
+
+
+def get_resilient_client(opts):
+    """Get a connected instance of SimpleClient for Resilient REST API"""
+    global resilient_client
+    global connection_opts
+
+    new_opts = (opts.get("cafile"),
+                opts.get("org"),
+                opts.get("host"),
+                opts.get("port"),
+                opts.get("proxy_host"),
+                opts.get("proxy_port"),
+                opts.get("proxy_user"),
+                opts.get("proxy_password"),
+                opts.get("email"),
+                opts.get("api_key_id"))
+    if new_opts != connection_opts:
+        resilient_client = None
+        connection_opts = new_opts
+    if resilient_client:
+        return resilient_client
+
+    resilient_client = resilient.get_client(opts)
+    return resilient_client
