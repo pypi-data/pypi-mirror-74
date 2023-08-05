360f3656cb8c6fd9037013a2e03688d2f7087bb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:41:43 +0000

    Add package pytransifex/0.1.10/pytransifex-0.1.10.tar.gz

---
+++ packages/pytransifex/pytransifex-0.1.10.tar.gz/pytransifex-0.1.10/pytransifex/__init__.py
@@ -0,0 +1,5 @@
+
+
+
+from pytransifex.api import Transifex
+from pytransifex.exceptions import TransifexException, PyTransifexException, InvalidSlugException
---
+++ packages/pytransifex/pytransifex-0.1.10.tar.gz/pytransifex-0.1.10/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+import sys
+
+python_min_version = (3, 6)
+
+if sys.version_info < python_min_version:
+    sys.exit('pytransifex requires at least Python version {vmaj}.{vmin}.\n'
+             'You are currently running this installation with\n\n{curver}'.format(
+        vmaj=python_min_version[0],
+        vmin=python_min_version[1],
+        curver=sys.version))
+
+setup(
+    name = 'pytransifex',
+    packages = [
+        'pytransifex'
+    ],
+    version = '0.1.10',
+    description = 'Yet another Python Transifex API.',
+    author = 'Denis Rouzaud',
+    author_email = 'denis.rouzaud@gmail.com',
+    url = 'https://github.com/opengisch/pytransifex',
+    download_url = 'https://github.com/opengisch/pytransifex/archive/0.1.10.tar.gz', # I'll explain this in a second
+    keywords = ['Transifex'],
+    classifiers = [
+        'Topic :: Software Development :: Localization',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Intended Audience :: System Administrators',
+        'Development Status :: 3 - Alpha'
+    ],
+    install_requires = [
+        'requests'
+    ],
+    python_requires=">={vmaj}.{vmin}".format(vmaj=python_min_version[0], vmin=python_min_version[1]),
+)
