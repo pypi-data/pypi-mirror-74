333ce57d62eef4c435c0f36a9688ca6e0f582fec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:28 +0000

    Add package gs/1.0.0/gs-1.0.0.tar.gz

---
+++ packages/gs/gs-1.0.0.tar.gz/gs-1.0.0/setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+import os, sys
+from setuptools import setup, find_packages
+
+install_requires = [line.rstrip() for line in open(os.path.join(os.path.dirname(__file__), "requirements.txt"))]
+tests_require = ["coverage", "flake8", "wheel"]
+
+setup(
+    name='gs',
+    version='1.0.0',
+    url='https://github.com/kislyuk/gs',
+    license='MIT License',
+    author='Andrey Kislyuk',
+    author_email='kislyuk@gmail.com',
+    description='A minimalistic Google Storage client',
+    long_description=open('README.rst').read(),
+    install_requires=install_requires,
+    tests_require=tests_require,
+    extras_require={
+        ':python_version == "2.7"': ['futures']
+    },
+    packages=find_packages(exclude=['test']),
+    entry_points={
+        'console_scripts': [
+            'gs=gs.cli:cli'
+        ],
+    },
+    platforms=['MacOS X', 'Posix'],
+    include_package_data=True,
+    test_suite='test',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
---
+++ packages/gs/gs-1.0.0.tar.gz/gs-1.0.0/test/test.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import os, sys, unittest, uuid, tempfile, time, logging
+
+from gs.util.compat import TemporaryDirectory
+
+import gs, tweak
+from gs import cli
+
+from gs.util.compat import USING_PYTHON2
+
+logging.basicConfig(level=logging.DEBUG)
+
+class TestGS(unittest.TestCase):
+    test_bucket = os.environ["GS_TEST_BUCKET"]
+    test_id = int(time.time() * 1000)
+
+    def test_ls_commands(self):
+        config = tweak.Config("gs", save_on_exit=False)
+        client = gs.GSClient(config=config)
+        cli.ls.main([], standalone_mode=False)
+        cli.ls.main(["--json"], standalone_mode=False)
+
+    def test_rw_commands(self):
+        with tempfile.NamedTemporaryFile() as tf1, tempfile.NamedTemporaryFile() as tf2:
+            payload = os.urandom(1024 * 1024 + 1)
+            tf1.write(payload)
+            tf1.flush()
+            tf2.write(payload[:16])
+            tf2.flush()
+            test_prefix = "gs://{}/{}".format(self.test_bucket, self.test_id)
+            furl1 = os.path.join(test_prefix, os.path.basename(tf1.name))
+            furl2 = os.path.join(test_prefix, os.path.basename(tf2.name))
+            cli.cp.main([tf1.name, tf2.name, test_prefix], standalone_mode=False)
+            cli.ls.main([test_prefix], standalone_mode=False)
+            # cli.presign.main([furl1], standalone_mode=False)
+            with TemporaryDirectory() as td:
+                cli.cp.main([furl1, furl2, td], standalone_mode=False)
+                with open(os.path.join(td, tf1.name), "rb") as fh:
+                    self.assertEqual(fh.read(), payload)
+                with open(os.path.join(td, tf2.name), "rb") as fh:
+                    self.assertEqual(fh.read(), payload[:16])
+                cli.cp.main([furl1, furl1 + ".2"], standalone_mode=False)
+                cli.mv.main([furl1, furl1 + ".3"], standalone_mode=False)
+                cli.ls.main([furl1 + ".*"], standalone_mode=False)
+                cli.cp.main([furl1 + ".*", furl1 + "-dir"], standalone_mode=False)
+                cli.cp.main([furl1 + ".3", furl1 + ".4"], standalone_mode=False)
+                cli.rm.main([furl1 + ".2", furl1 + ".3"], standalone_mode=False)
+                cli.sync.main([test_prefix, td], standalone_mode=False)
+                cli.sync.main([td, test_prefix], standalone_mode=False)
+                cli.rm.main([test_prefix, "--dryrun", "--recursive"], standalone_mode=False)
+                cli.rm.main([test_prefix, "--recursive"], standalone_mode=False)
+
+if __name__ == "__main__":
+    unittest.main()
