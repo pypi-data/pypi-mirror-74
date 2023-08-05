4dee66b28bfe6df25232d251f0739403f16f8974
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:22 +0000

    Add package aimmo/0.54.1b6026/aimmo-0.54.1b6026.tar.gz

---
+++ packages/aimmo/aimmo-0.54.1b6026.tar.gz/aimmo-0.54.1b6026/aimmo-game/simulation/cell.py
@@ -0,0 +1,63 @@
+from simulation.action import MoveAction
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from simulation.avatar.avatar_wrapper import AvatarWrapper
+
+
+class Cell(object):
+    """
+    Any position on the world grid.
+    """
+
+    def __init__(self, location, habitable=True, partially_fogged=False):
+        self.location = location
+        self.habitable = habitable
+        self.avatar: "AvatarWrapper" = None
+        self.interactable = None
+        self.partially_fogged = partially_fogged
+        self.actions = []
+
+    def __repr__(self):
+        return "Cell({} h={} a={} i={} f{})".format(
+            self.location,
+            self.habitable,
+            self.avatar,
+            self.interactable,
+            self.partially_fogged,
+        )
+
+    def __eq__(self, other):
+        return self.location == other.location
+
+    def __ne__(self, other):
+        return not self == other
+
+    def __hash__(self):
+        return hash(self.location)
+
+    @property
+    def moves(self):
+        return [move for move in self.actions if isinstance(move, MoveAction)]
+
+    @property
+    def is_occupied(self):
+        return self.avatar is not None
+
+    def serialize(self):
+        if self.partially_fogged:
+            return {
+                "location": self.location.serialize(),
+                "partially_fogged": self.partially_fogged,
+            }
+        else:
+            return {
+                "avatar": self.avatar.serialize() if self.avatar else None,
+                "habitable": self.habitable,
+                "location": self.location.serialize(),
+                "interactable": self.interactable.serialize()
+                if self.interactable
+                else None,
+                "partially_fogged": self.partially_fogged,
+            }
