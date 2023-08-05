c7b21f353b738a3651acf1fc7d89ce28a416642c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:44 +0000

    Add package savvidistributions/0.1/savvidistributions-0.1.tar.gz

---
+++ packages/savvidistributions/savvidistributions-0.1.tar.gz/savvidistributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='savvidistributions',
+      version='0.1',
+      description='Gaussian distributions and Binomial distributions',
+      packages=['savvidistributions'],
+      author = 'Olatunde Babawale',
+      author_email = 'obabawale@pm.me',
+      zip_safe=False)
