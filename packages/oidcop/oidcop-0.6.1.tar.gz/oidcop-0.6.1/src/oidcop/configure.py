e0720a03d8a9fedd60b78ec44fd896f7fd416b91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:53 +0000

    Add package oidcop/0.6.1/oidcop-0.6.1.tar.gz

---
+++ packages/oidcop/oidcop-0.6.1.tar.gz/oidcop-0.6.1/src/oidcop/configure.py
@@ -0,0 +1,52 @@
+"""Configuration management for IDP"""
+
+import os
+from typing import Dict
+
+from cryptojwt.key_bundle import init_key
+
+from oidcop.logging import configure_logging
+from oidcop.utils import load_yaml_config
+
+try:
+    from secrets import token_urlsafe as rnd_token
+except ImportError:
+    from oidcendpoint import rndstr as rnd_token
+
+
+class Configuration:
+    """OP Configuration"""
+
+    def __init__(self, conf: Dict) -> None:
+        self.logger = configure_logging(config=conf.get('logging')).getChild(__name__)
+        self.op = None
+
+        # OIDC provider configuration
+        for section in ['op', 'webserver', 'httpc_params', 'jinja_env']:
+            setattr(self, section, conf.get(section, {}))
+
+        # set OP session key
+        _key_args = self.op['server_info'].get('session_key')
+        if _key_args is not None:
+            self.session_key = init_key(**_key_args)
+            # self.op['server_info']['password'] = self.session_key
+            self.logger.debug("Set server password to %s", self.session_key)
+
+        # templates and Jinja environment
+        self.template_dir = os.path.abspath(conf.get('template_dir', 'templates'))
+
+        # server info
+        self.domain = conf.get("domain")
+        self.port = conf.get("port")
+        for param in ["server_name", "base_url"]:
+            _pre = conf.get(param)
+            if _pre:
+                if '{domain}' in _pre:
+                    setattr(self, param, _pre.format(domain=self.domain, port=self.port))
+                else:
+                    setattr(self, param, _pre)
+
+    @classmethod
+    def create_from_config_file(cls, filename: str):
+        """Load configuration as YAML"""
+        return cls(load_yaml_config(filename))
---
+++ packages/oidcop/oidcop-0.6.1.tar.gz/oidcop-0.6.1/src/oidcop/utils.py
@@ -0,0 +1,80 @@
+import io
+import json
+import os
+import ssl
+import sys
+
+import yaml
+
+
+def load_json(file_name):
+    with open(file_name) as fp:
+        js = json.load(fp)
+    return js
+
+
+def load_yaml_config(file_name):
+    with open(file_name) as fp:
+        c = yaml.safe_load(fp)
+    return c
+
+def yaml_to_py_stream(file_name):
+    d = load_yaml_config(file_name)
+    fstream = io.StringIO()
+    for i in d:
+        section = '{} = {}\n\n'.format(i, json.dumps(d[i], indent=2))
+        fstream.write(section)
+    fstream.seek(0)
+    return fstream
+
+
+def lower_or_upper(config, param, default=None):
+    res = config.get(param.lower(), default)
+    if not res:
+        res = config.get(param.upper(), default)
+    return res
+
+
+def create_context(dir_path, config, **kwargs):
+    _fname = lower_or_upper(config, "server_cert")
+    if _fname:
+        if _fname.startswith("/"):
+            _cert_file = _fname
+        else:
+            _cert_file = os.path.join(dir_path, _fname)
+    else:
+        return None
+
+    _fname = lower_or_upper(config, "server_key")
+    if _fname:
+        if _fname.startswith("/"):
+            _key_file = _fname
+        else:
+            _key_file = os.path.join(dir_path, _fname)
+    else:
+        return None
+
+    context = ssl.SSLContext(**kwargs)  # PROTOCOL_TLS by default
+
+    _verify_user = lower_or_upper(config, "verify_user")
+    if _verify_user:
+        if _verify_user == "optional":
+            context.verify_mode = ssl.CERT_OPTIONAL
+        elif _verify_user == "required":
+            context.verify_mode = ssl.CERT_REQUIRED
+        else:
+            sys.exit("Unknown verify_user specification: '{}'".format(_verify_user))
+        _ca_bundle = lower_or_upper(config, "ca_bundle")
+        if _ca_bundle:
+            context.load_verify_locations(_ca_bundle)
+    else:
+        context.verify_mode = ssl.CERT_NONE
+
+    try:
+        context.load_cert_chain(_cert_file, _key_file)
+    except Exception as e:
+        print("cert_file:", _cert_file)
+        print("key_file:", _key_file)
+        sys.exit("Error starting server. Missing cert or key. Details: {}".format(e))
+
+    return context
