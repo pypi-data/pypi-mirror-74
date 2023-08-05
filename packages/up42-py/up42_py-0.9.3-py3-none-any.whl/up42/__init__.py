236e64fc55cf49335ed150628ccbcd2f9c92ddcc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:36 +0000

    Add package up42-py/0.9.3/up42_py-0.9.3-py3-none-any.whl

---
+++ packages/up42-py/up42_py-0.9.3-py3-none-any.whl/up42/__init__.py
@@ -0,0 +1,149 @@
+import warnings
+from pathlib import Path
+from typing import List, Union, Dict, Tuple
+import logging
+
+from geojson import FeatureCollection
+from geopandas import GeoDataFrame
+
+# pylint: disable=wrong-import-position
+from .tools import Tools
+from .auth import Auth
+from .project import Project
+from .workflow import Workflow
+from .job import Job
+from .jobtask import JobTask
+from .catalog import Catalog
+from .utils import get_logger
+
+logger = get_logger(__name__, level=logging.INFO)
+
+warnings.simplefilter(action="ignore", category=FutureWarning)
+
+_auth = None
+
+
+def authenticate(cfg_file=None, project_id=None, project_api_key=None, **kwargs):
+    global _auth  # pylint: disable=global-statement
+    _auth = Auth(
+        cfg_file=cfg_file,
+        project_id=project_id,
+        project_api_key=project_api_key,
+        **kwargs
+    )
+
+
+def initialize_project() -> "Project":
+    """Directly returns the correct Project object (has to exist on UP42)."""
+    if _auth is None:
+        raise RuntimeError("Not authenticated, call up42.authenticate() first")
+    logger.info("Working on Project with project_id %s", _auth.project_id)
+    return Project(auth=_auth, project_id=_auth.project_id)
+
+
+def initialize_catalog() -> "Catalog":
+    """Directly returns a Catalog object."""
+    if _auth is None:
+        raise RuntimeError("Not authenticated, call up42.authenticate() first")
+    return Catalog(auth=_auth)
+
+
+def initialize_workflow(workflow_id) -> "Workflow":
+    """Directly returns a Workflow object (has to exist on UP42)."""
+    if _auth is None:
+        raise RuntimeError("Not authenticated, call up42.authenticate() first")
+    return Workflow(auth=_auth, workflow_id=workflow_id, project_id=_auth.project_id)
+
+
+def initialize_job(job_id, order_ids: List[str] = None) -> "Job":
+    """Directly returns a Job object (has to exist on UP42)."""
+    if _auth is None:
+        raise RuntimeError("Not authenticated, call up42.authenticate() first")
+    return Job(
+        auth=_auth, job_id=job_id, project_id=_auth.project_id, order_ids=order_ids
+    )
+
+
+def initialize_jobtask(jobtask_id, job_id) -> "JobTask":
+    """Directly returns a JobTask object (has to exist on UP42)."""
+    if _auth is None:
+        raise RuntimeError("Not authenticated, call up42.authenticate() first")
+    return JobTask(
+        auth=_auth, jobtask_id=jobtask_id, job_id=job_id, project_id=_auth.project_id
+    )
+
+
+def get_blocks(
+    block_type=None, basic: bool = True, as_dataframe=False,
+):
+    tools = Tools(auth=_auth)
+    return tools.get_blocks(block_type, basic, as_dataframe)
+
+
+def get_block_details(block_id: str, as_dataframe=False) -> Dict:
+    tools = Tools(auth=_auth)
+    return tools.get_block_details(block_id, as_dataframe)
+
+
+def validate_manifest(path_or_json: Union[str, Path, Dict]) -> Dict:
+    tools = Tools(auth=_auth)
+    return tools.validate_manifest(path_or_json)
+
+
+def read_vector_file(
+    filename: str = "aoi.geojson", as_dataframe: bool = False
+) -> FeatureCollection:
+    tools = Tools(auth=_auth)
+    return tools.read_vector_file(filename, as_dataframe)
+
+
+def get_example_aoi(
+    location: str = "Berlin", as_dataframe: bool = False
+) -> FeatureCollection:
+    tools = Tools(auth=_auth)
+    return tools.get_example_aoi(location, as_dataframe)
+
+
+def draw_aoi() -> None:
+    tools = Tools(auth=_auth)
+    tools.draw_aoi()
+
+
+# pylint: disable=duplicate-code
+def plot_coverage(
+    scenes: GeoDataFrame,
+    aoi: GeoDataFrame = None,
+    legend_column: str = "scene_id",
+    figsize=(12, 16),
+) -> None:
+    tools = Tools(auth=_auth)
+    tools.plot_coverage(scenes, aoi, legend_column, figsize)
+
+
+def plot_quicklooks(figsize: Tuple[int, int] = (8, 8), filepaths: List = None) -> None:
+    tools = Tools(auth=_auth)
+    tools.plot_quicklooks(figsize, filepaths)
+
+
+def plot_results(
+    figsize: Tuple[int, int] = (8, 8),
+    filepaths: List[Union[str, Path]] = None,
+    titles: List[str] = None,
+) -> None:
+    tools = Tools(auth=_auth)
+    tools.plot_results(figsize, filepaths, titles)
+
+
+def settings(log=True):
+    if log:
+        logger.info(
+            "Logging enabled (default) - use up42.settings(log=False) to disable."
+        )
+    else:
+        logger.info("Logging disabled - use up42.settings(log=True) to reactivate.")
+
+    # pylint: disable=expression-not-assigned
+    [
+        setattr(logging.getLogger(name), "disabled", not log)
+        for name in logging.root.manager.loggerDict
+    ]
---
+++ packages/up42-py/up42_py-0.9.3-py3-none-any.whl/up42/project.py
@@ -0,0 +1,179 @@
+import logging
+from typing import Dict, List, Union
+
+from tqdm import tqdm
+
+from .auth import Auth
+from .job import Job
+from .tools import Tools
+from .utils import get_logger
+from .workflow import Workflow
+
+logger = get_logger(__name__)
+
+
+class Project(Tools):
+    def __init__(self, auth: Auth, project_id: str):
+        """
+        The Project class can query all available workflows and spawn new workflows
+        within an UP42 project. Also handles project user settings.
+        """
+        self.auth = auth
+        self.project_id = project_id
+        if self.auth.get_info:
+            self.info = self._get_info()
+
+    def __repr__(self):
+        return (
+            f"Project(project_id={self.project_id}, auth={self.auth}, info={self.info})"
+        )
+
+    def _get_info(self):
+        """Gets metadata info from sever for an existing project"""
+        url = f"{self.auth._endpoint()}/projects/{self.project_id}"
+        response_json = self.auth._request(request_type="GET", url=url)
+        self.info = response_json["data"]
+        return self.info
+
+    def create_workflow(
+        self, name: str, description: str = "", use_existing: bool = False
+    ) -> "Workflow":
+        """
+        Creates a new workflow and returns a workflow object.
+
+        Args:
+            name: Name of the new workflow.
+            description: Description of the new workflow.
+            use_existing: If True, instead of creating a new workflow, uses the
+                most recent workflow with the same name & description.
+
+        Returns:
+            The workflow object.
+        """
+        if use_existing:
+            logger.info("Getting existing workflows in project ...")
+            logging.getLogger("up42.workflow").setLevel(logging.CRITICAL)
+            existing_workflows = self.get_workflows()
+            logging.getLogger("up42.workflow").setLevel(logging.INFO)
+
+            matching_workflows = [
+                workflow
+                for workflow in existing_workflows
+                if workflow.info["name"] == name
+                and workflow.info["description"] == description
+            ]
+            if matching_workflows:
+                existing_workflow = matching_workflows[0]
+                logger.info(
+                    "Using existing workflow: %s, %s.",
+                    name,
+                    existing_workflow.workflow_id,
+                )
+                return existing_workflow
+
+        url = f"{self.auth._endpoint()}/projects/{self.project_id}/workflows/"
+        payload = {"name": name, "description": description}
+        response_json = self.auth._request(request_type="POST", url=url, data=payload)
+        workflow_id = response_json["data"]["id"]
+        logger.info("Created new workflow: %s.", workflow_id)
+        workflow = Workflow(
+            self.auth, project_id=self.project_id, workflow_id=workflow_id
+        )
+        return workflow
+
+    def get_workflows(self, return_json: bool = False) -> Union[List["Workflow"], Dict]:
+        """
+        Gets all workflows in a project as workflow objects or json.
+
+        Args:
+            return_json: True returns Workflow Objects.
+
+        Returns:
+            Workflow objects in the project or alternatively json info of the workflows.
+        """
+        url = f"{self.auth._endpoint()}/projects/{self.project_id}/workflows"
+        response_json = self.auth._request(request_type="GET", url=url)
+        workflows_json = response_json["data"]
+        logger.info(
+            "Got %s workflows for project %s.", len(workflows_json), self.project_id
+        )
+
+        if return_json:
+            return workflows_json
+        else:
+            workflows = [
+                Workflow(self.auth, project_id=self.project_id, workflow_id=work["id"])
+                for work in tqdm(workflows_json)
+            ]
+            return workflows
+
+    def get_jobs(self, return_json: bool = False) -> Union[List["Job"], Dict]:
+        """
+        Get all jobs in the project as job objects or json.
+
+        Use Workflow().get_job() to get jobs associated with a specific workflow.
+
+        Args:
+            return_json: If true, returns the job info jsons instead of job objects.
+
+        Returns:
+            All job objects as a list, or alternatively the jobs info as json.
+        """
+        url = f"{self.auth._endpoint()}/projects/{self.project_id}/jobs"
+        response_json = self.auth._request(request_type="GET", url=url)
+        jobs_json = response_json["data"]
+        logger.info(
+            "Got %s jobs in project %s.", len(jobs_json), self.project_id,
+        )
+        if return_json:
+            return jobs_json
+        else:
+            jobs = [
+                Job(self.auth, job_id=job["id"], project_id=self.project_id)
+                for job in tqdm(jobs_json)
+            ]
+            return jobs
+
+    def get_project_settings(self) -> List:
+        """
+        Gets the project settings.
+
+        Returns:
+            The project settings.
+        """
+        url = f"{self.auth._endpoint()}/projects/{self.project_id}/settings"
+        response_json = self.auth._request(request_type="GET", url=url)
+        project_settings = response_json["data"]
+        return project_settings
+
+    def update_project_settings(
+        self,
+        max_aoi_size: int = None,
+        max_concurrent_jobs: int = None,
+        number_of_images=None,
+    ) -> None:
+        """
+        Updates a project's settings.
+
+        Args:
+            max_aoi_size: The maximum area of interest geometry size, from 1-1000 sqkm, default 10 sqkm.
+            max_concurrent_jobs: The maximum number of concurrent jobs, from 1-10, default 1.
+            number_of_images: The maximum number of images returned with each job, from 1-20, default 10.
+        """
+        url = f"{self.auth._endpoint()}/projects/{self.project_id}/settings"
+        payload = [
+            {
+                "name": "JOB_QUERY_MAX_AOI_SIZE",
+                "value": f"{100 if max_aoi_size is None else max_aoi_size}",
+            },
+            {
+                "name": "MAX_CONCURRENT_JOBS",
+                "value": f"{10 if max_concurrent_jobs is None else max_concurrent_jobs}",
+            },
+            {
+                "name": "JOB_QUERY_LIMIT_PARAMETER_MAX_VALUE",
+                "value": f"{10 if number_of_images is None else number_of_images}",
+            },
+        ]
+        self.auth._request(request_type="PUT", url=url, data=payload)
+        logger.info("Updated project settings: %s", payload)
