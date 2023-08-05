2d58d9d40ee06aa2f4f24eec5c399d7904d808e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:50 +0000

    Add package csv2sqllike/1.0.5/csv2sqllike-1.0.5.tar.gz

---
+++ packages/csv2sqllike/csv2sqllike-1.0.5.tar.gz/csv2sqllike-1.0.5/setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup
+
+
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
+install_requireent = []
+
+setup_requires = [
+        'pymysql',
+        'pandas',
+        'tqdm',
+        'sqlalchemy'
+        ]
+
+install_requires = [
+        'pymysql',
+        'pandas',
+        'tqdm',
+        'sqlalchemy'
+        ]
+
+setup(
+    name='csv2sqllike',
+    author='Junsang Park',
+    author_email='publichey@gmail.com',
+    url='https://github.com/hoosiki/csv2sqlLike',
+    version='1.0.5',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    description='Python functions for data analysis using python native container. Load data from csv files and deal with data like sql.',
+    packages=find_packages(),
+    licnese='BSD',
+    include_package_date=False,
+    setup_requires=setup_requires,
+    install_requires=install_requires,
+    download_url='https://github.com/hoosiki/csv2sqlLike/blob/master/dist/csv2sqllike-1.0.1.tar.gz'
+)
