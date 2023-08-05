3b654b1e5cc00bb2aeee6130c9290f5f8c18b9aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:29 +0000

    Add package rafsun-dst/0.1/rafsun_dst-0.1.tar.gz

---
+++ packages/rafsun-dst/rafsun_dst-0.1.tar.gz/rafsun_dst-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='rafsun_dst',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['rafsun_distributions'],
+      zip_safe=False)
