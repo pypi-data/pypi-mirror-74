4f39512ab0b1f4c771b176bed98074aae77fcfcd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:08 +0000

    Add package arcor2-kinali/0.7.1/arcor2_kinali-0.7.1.tar.gz

---
+++ packages/arcor2-kinali/arcor2_kinali-0.7.1.tar.gz/arcor2_kinali-0.7.1/arcor2_kinali/services/upload.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from arcor2.services.upload import upload
+
+
+def main():
+
+    upload("arcor2_kinali.services.robot/RestRobotService")
+    upload("arcor2_kinali.services.search/SearchService")
+    upload("arcor2_kinali.services.barcode/BarcodeService")
+    upload("arcor2_kinali.services.statistic/StatisticService")
+    upload("arcor2_kinali.services.interaction/InteractionService")
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/arcor2-kinali/arcor2_kinali-0.7.1.tar.gz/arcor2_kinali-0.7.1/setup.py
@@ -0,0 +1,25 @@
+# coding: utf-8
+
+
+from setuptools import setup, find_packages  # type: ignore
+
+import arcor2_kinali
+
+setup(
+    name='arcor2_kinali',
+    version=arcor2_kinali.version(),
+    packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+    package_data={"arcor2_kinali": ["py.typed"]},
+    url='',
+    license='',
+    author='Robo@FIT',
+    author_email='imaterna@fit.vut.cz',
+    description='',
+    install_requires=[
+        'dataclasses',
+        'dataclasses-jsonschema[fast-validation]',
+        'arcor2==0.7.1',
+        'Pillow'
+    ],
+    zip_safe=False,
+)
