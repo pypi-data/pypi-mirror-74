0e71e8b8d27abe1fffa8931263538f3986d8c32c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:45 +0000

    Add package maads/3.16.9/maads-3.16.9.tar.gz

---
+++ packages/maads/maads-3.16.9.tar.gz/maads-3.16.9/maads/__init__.py
@@ -0,0 +1,33 @@
+from .sendfiles import returndata
+from .sendfiles import uploadcsvfortraining
+from .sendfiles import getpredictions
+
+from .sendfiles import dolistkeys
+from .sendfiles import dolistkeyswithkey
+from .sendfiles import dodeletewithkey
+
+from .sendfiles import getpicklezip
+from .sendfiles import sendpicklezip
+from .sendfiles import deploytoprod
+from .sendfiles import nlp
+from .sendfiles import nlpclassify
+from .sendfiles import algoinfo
+from .sendfiles import nlpaudiovideo
+from .sendfiles import nlpocr
+from .sendfiles import nlpgeosentiment
+from .sendfiles import optimize
+
+from .sendfiles import hyperpredictions
+from .sendfiles import tcp_echo_client
+from .sendfiles import retraining
+from .sendfiles import genpdf
+
+from .sendfiles import featureselectionjson
+
+
+from .callmas import dotraining
+from .callmas import dopredictions
+
+
+
+name = "maads"
