ba978d2147ca84ad1f580fe46223acd7a3aeecc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:48 +0000

    Add package SacraMathEngine/0.0.7/SacraMathEngine-0.0.7-py3-none-any.whl

---
+++ packages/SacraMathEngine/SacraMathEngine-0.0.7-py3-none-any.whl/SacraMathEngine/Triangle.py
@@ -0,0 +1,39 @@
+from .Vector import vec3d
+from .Matrix import matrix3d
+
+
+class Triangle:
+    def __init__(self, vec1, vec2, vec3):
+        """
+        if not isinstance((vec1, vec2, vec3), vec3d):
+            raise TypeError("Wrong format in Triangle")
+        """
+        self.vec1 = vec1
+        self.vec2 = vec2
+        self.vec3 = vec3
+        self.set = (self.vec1, self.vec2, self.vec3)
+
+    def __str__(self):
+        """Print statement for Triangle-object"""
+        return f'[{self.vec1}, {self.vec2}, {self.vec3}]'
+
+    def __add__(self, Other):
+        if isinstance(Other, vec3d):
+            return Triangle(self.vec1 + Other.x, self.vec2 + Object.y, self.vec3 + Other.z)
+        else:
+            pass
+
+    def __len__(self):
+        """Return the length of triangle, which is 3 by definition. """
+        return 3 #Could be len(self.set)
+
+    def __getitem__(self, index):
+        """Get the vector defined at index 'index', given the attribute 'index'."""
+        return self.set[index]
+
+    def __mul__(self, Scalar):
+        """Returns a Triangle-object where each vector i multiplied by the scalar 'Scalar'."""
+        if isinstance(Scalar, (float, int)):
+            return Triangle(self.vec1 * Scalar, self.vec2 * Scalar, self.vec3 * Scalar)
+        else:
+            pass
