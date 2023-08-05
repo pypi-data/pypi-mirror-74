22e69711905dd6736f75dfd7415b79a58a9e21de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:11 +0000

    Add package pyublox/1.7.0/pyublox-1.7.0-py3-none-any.whl

---
+++ packages/pyublox/pyublox-1.7.0-py3-none-any.whl/pyublox/ublox/constants.py
@@ -0,0 +1,18 @@
+
+
+CONSTELLATIONS_STR = 'constellations'
+SIGNALS_STR = 'signals'
+RATES_STR = 'rates'
+MEAS_PROP_STR = 'measurement_properties'
+SOLUTION_STR = 'solution'
+MEASUREMENTS_STR = 'measurements'
+EPHEMERIS_STR = 'ephemeris'
+
+GPS_STR = 'GPS'
+GAL_STR = 'GAL'
+BDS_STR = 'BDS'
+QZSS_STR = 'QZSS'
+GLO_STR = 'GLO'
+
+BYTE_TRUE = 1
+BYTE_FALSE = 0
