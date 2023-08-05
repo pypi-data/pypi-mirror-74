8d029b18b5d1e765486cbe95b40451eda81568e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:54 +0000

    Add package twitter-word-cloud/1.1/twitter_word_cloud-1.1.tar.gz

---
+++ packages/twitter-word-cloud/twitter_word_cloud-1.1.tar.gz/twitter_word_cloud-1.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='twitter_word_cloud',
+      version='1.1',
+      description='Generate word cloud charts from twitter',
+      packages=['twitter_word_cloud'],
+      zip_safe=False)
