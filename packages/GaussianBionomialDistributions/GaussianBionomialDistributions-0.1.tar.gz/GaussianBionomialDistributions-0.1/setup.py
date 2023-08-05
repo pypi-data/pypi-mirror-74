742725b7fe0030379475147b56ba14527b43d9ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:41 +0000

    Add package GaussianBionomialDistributions/0.1/GaussianBionomialDistributions-0.1.tar.gz

---
+++ packages/GaussianBionomialDistributions/GaussianBionomialDistributions-0.1.tar.gz/GaussianBionomialDistributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='GaussianBionomialDistributions',
+      version='0.1',
+      description='Gaussian distributions Binomial Distibutions',
+      packages=['distributions'],
+      zip_safe=False)
