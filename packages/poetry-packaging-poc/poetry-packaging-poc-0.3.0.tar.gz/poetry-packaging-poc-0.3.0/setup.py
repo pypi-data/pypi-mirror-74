83939aaefc1ac61e2852fac933516cc1f439356a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:02 +0000

    Add package poetry-packaging-poc/0.3.0/poetry-packaging-poc-0.3.0.tar.gz

---
+++ packages/poetry-packaging-poc/poetry-packaging-poc-0.3.0.tar.gz/poetry-packaging-poc-0.3.0/setup.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['poetry_packaging_poc']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['click>=7.1.2,<8.0.0']
+
+entry_points = \
+{'console_scripts': ['poetry-packaging-poc = '
+                     'poetry_packaging_poc.console:main']}
+
+setup_kwargs = {
+    'name': 'poetry-packaging-poc',
+    'version': '0.3.0',
+    'description': '',
+    'long_description': None,
+    'author': 'Marcos Damián Mesmer y Rosset',
+    'author_email': 'marcosdmyr@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8',
+}
+
+
+setup(**setup_kwargs)
