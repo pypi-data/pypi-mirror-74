e644ed842c54f17a147887748ce30b4286df04ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:45 +0000

    Add package django-fixture-magic/0.1.5/django-fixture-magic-0.1.5.tar.gz

---
+++ packages/django-fixture-magic/django-fixture-magic-0.1.5.tar.gz/django-fixture-magic-0.1.5/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='django-fixture-magic',
+    version='0.1.5',
+    description='A few extra management tools to handle fixtures.',
+    long_description=open('README.rst').read(),
+    long_description_content_type='text/x-rst',
+    author='Dave Dash',
+    author_email='dd+pypi@davedash.com',
+    url='http://github.com/davedash/django-fixture-magic',
+    license='BSD',
+    packages=find_packages(exclude=['tests']),
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 1.8',
+        'Framework :: Django :: 1.9',
+        'Framework :: Django :: 1.10',
+        'Framework :: Django :: 1.11',
+        'Framework :: Django :: 2.0',
+        'Framework :: Django :: 2.1',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
---
+++ packages/django-fixture-magic/django-fixture-magic-0.1.5.tar.gz/django-fixture-magic-0.1.5/tests/fixture_magic/test_utils.py
@@ -0,0 +1,33 @@
+from __future__ import absolute_import
+
+import os
+import unittest
+os.environ['DJANGO_SETTINGS_MODULE'] = 'tests.test_settings'
+
+from fixture_magic.utils import reorder_json, get_fields, get_m2m
+
+__author__ = 'davedash'
+
+
+
+class UtilsTestCase(unittest.TestCase):
+    def test_reorder_json(self):
+        """Test basic ordering of JSON/python object."""
+        input_json = [{'model': 'f'}, {'model': 'x'},]
+        expected = [{'model': 'x'}, {'model': 'f'}]
+        self.assertEqual(expected, reorder_json(input_json, models=['x', 'f'])
+        )
+
+    def test_get_fields(self):
+        obj = lambda: None
+        obj._meta = lambda: None
+        obj._meta.fields = ['foo']
+
+        self.assertEqual(['foo'], get_fields(obj))
+
+    def test_get_m2m(self):
+        obj = lambda: None
+        obj._meta = lambda: None
+        obj._meta.many_to_many = ['bar']
+
+        self.assertEqual(['bar'], get_m2m(obj))
