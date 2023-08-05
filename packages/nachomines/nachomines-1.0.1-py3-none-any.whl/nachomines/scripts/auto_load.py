88a3f2d00132270da4539616b1636fc98e869d17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:29 +0000

    Add package nachomines/1.0.1/nachomines-1.0.1-py3-none-any.whl

---
+++ packages/nachomines/nachomines-1.0.1-py3-none-any.whl/nachomines/scripts/auto_load.py
@@ -0,0 +1,25 @@
+import os
+import pygame
+from nachomines.scripts.Utils import green, endC, fixPath
+from pkg_resources import resource_filename
+
+pygame.init()
+
+path = resource_filename("nachomines", fixPath("resources/images/bitmap/"))
+
+class AutoLoad:
+    def __init__(self):
+        self.names = []
+        self.images = []
+
+    def remove_extension(self, filename):
+        return filename[:filename.index(".")]
+
+    def Start_AutoLoad(self):
+       for (root, dirs, files) in os.walk(path):
+           for File in files:
+               if not File.startswith("."):
+                   self.names.append(self.remove_extension(File))
+                   self.images.append(pygame.image.load(root + os.path.sep + File))
+       final = dict(zip(self.names, self.images))
+       return final
