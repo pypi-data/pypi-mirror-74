701e7349e56765221172266c1a38d4252682592b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:41 +0000

    Add package gooise/2020.7.17/gooise-2020.7.17-py3-none-any.whl

---
+++ packages/gooise/gooise-2020.7.17-py3-none-any.whl/gooise/driver.py
@@ -0,0 +1,62 @@
+def _chrome(binary, headless):
+    from selenium.webdriver import Chrome, ChromeOptions
+
+    options = ChromeOptions()
+    if headless:
+        options.headless = headless
+    if not binary:
+        return Chrome(options=options)
+    else:
+        return Chrome(executable_path=binary, options=options)
+
+
+def _firefox(binary, headless):
+    from selenium.webdriver import Firefox, FirefoxOptions
+
+    options = FirefoxOptions()
+    if headless:
+        options.headless = headless
+    if not binary:
+        return Firefox(options=options)
+    else:
+        return Firefox(executable_path=binary, options=options)
+
+
+def _opera(binary):
+    from selenium.webdriver import Opera
+
+    if not binary:
+        return Opera(executable_path=binary)
+    else:
+        return Opera()
+
+
+def _ie(binary):
+    from selenium.webdriver import Ie
+
+    if not binary:
+        return Ie(executable_path=binary)
+    else:
+        return Ie()
+
+
+def _edge(binary):
+    from selenium.webdriver import Edge
+
+    if not binary:
+        return Edge(executable_path=binary)
+    else:
+        return Edge()
+
+
+def get_driver(name, binary, headless):
+    if name == "chrome":
+        return _chrome(binary, headless)
+    elif name == "firefox":
+        return _firefox(binary, headless)
+    elif name == "opera":
+        return _opera(binary)
+    elif name == "ie":
+        return _ie(binary)
+    elif name == "edge":
+        return _edge(binary)
