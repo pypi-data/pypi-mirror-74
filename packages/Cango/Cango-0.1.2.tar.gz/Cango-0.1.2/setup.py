94714fbedb66ba5278199f06d841f24f87591d59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:53 +0000

    Add package Cango/0.1.2/Cango-0.1.2.tar.gz

---
+++ packages/Cango/Cango-0.1.2.tar.gz/Cango-0.1.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Cango",
+    version="0.1.2",
+    url="https://github.com/YE-Kits/Cango",
+    description="A library for executing commands asynchronously.",
+    author="buzz",
+    author_email="admin@imipy.com",
+    keywords='cango',
+    license="GPL-3.0",
+    packages=find_packages(),
+    python_requires=">=3.6",
+)
