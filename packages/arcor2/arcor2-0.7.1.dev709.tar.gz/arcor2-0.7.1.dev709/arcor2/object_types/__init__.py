cf2bf8d07ab3bb1d9c06523322d6eb5e0994ca42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:50 +0000

    Add package arcor2/0.7.1.dev709/arcor2-0.7.1.dev709.tar.gz

---
+++ packages/arcor2/arcor2-0.7.1.dev709.tar.gz/arcor2-0.7.1.dev709/arcor2/object_types/__init__.py
@@ -0,0 +1,4 @@
+from arcor2.object_types.generic import Generic
+from arcor2.object_types.robot import Robot
+
+__all__ = ["Generic", "Robot"]
---
+++ packages/arcor2/arcor2-0.7.1.dev709.tar.gz/arcor2-0.7.1.dev709/arcor2/parameter_plugins/joints.py
@@ -0,0 +1,58 @@
+import json
+from typing import Any
+
+from arcor2.cached import CachedProject
+from arcor2.data.common import ProjectRobotJoints, Scene
+from arcor2.parameter_plugins.base import ParameterPlugin, ParameterPluginException, TypesDict
+from arcor2.services.robot_service import RobotService
+
+
+class JointsPlugin(ParameterPlugin):
+
+    @classmethod
+    def type(cls) -> Any:
+        return ProjectRobotJoints
+
+    @classmethod
+    def type_name(cls) -> str:
+        return "joints"
+
+    @classmethod
+    def value(cls, type_defs: TypesDict, scene: Scene, project: CachedProject, action_id: str, parameter_id: str) -> \
+            ProjectRobotJoints:
+
+        ap, action = project.action_point_and_action(action_id)
+        param = action.parameter(parameter_id)
+        joints_id = cls.param_value(param)
+
+        robot_id, action_method_name = action.parse_type()
+        robot_type = scene.object_or_service(robot_id)
+
+        if issubclass(type_defs[robot_type.type], RobotService):
+
+            for param in action.parameters:
+                if param.id == "robot_id":
+                    robot_id = json.loads(param.value)
+                    break
+            else:
+                raise ParameterPluginException(f"Parameter {param.id} of action {action.id} depends on"
+                                               f" 'robot_id' parameter, which could not be found.")
+
+        joints = project.joints(joints_id)
+
+        if joints.robot_id != robot_id:
+            raise ParameterPluginException("Joints are for different robot.")
+
+        return joints
+
+    @classmethod
+    def value_to_json(cls, value: ProjectRobotJoints) -> str:
+        return value.to_json()
+
+    @classmethod
+    def uses_robot_joints(cls, project: CachedProject, action_id: str, parameter_id: str, robot_joints_id: str) -> bool:
+
+        param = project.action(action_id).parameter(parameter_id)
+        value_id = cls.param_value(param)
+
+        return value_id == robot_joints_id
