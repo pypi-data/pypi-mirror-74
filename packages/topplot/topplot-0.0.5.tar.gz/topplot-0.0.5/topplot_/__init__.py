402cf39f13cf1e108553d6a5e54c797444cd0694
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:13:08 +0000

    Add package topplot/0.0.5/topplot-0.0.5.tar.gz

---
+++ packages/topplot/topplot-0.0.5.tar.gz/topplot-0.0.5/topplot_/__init__.py
@@ -0,0 +1,5 @@
+from topplot_.utils import *
+from topplot_.linestyler import LineStyler
+from topplot_.figman import FigManager
+from topplot_.grapher import rcParams
+from topplot_.grapher import Grapher
