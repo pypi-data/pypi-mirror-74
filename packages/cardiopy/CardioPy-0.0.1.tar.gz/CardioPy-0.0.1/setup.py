20877caef14b2f4447f32252c1af2f4ccf2abc51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:26 +0000

    Add package cardiopy/0.0.1/CardioPy-0.0.1.tar.gz

---
+++ packages/cardiopy/CardioPy-0.0.1.tar.gz/CardioPy-0.0.1/setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+	long_description = fh.read()
+
+setuptools.setup(
+	name = 'CardioPy',
+	version = "0.0.1",
+	author = "Jackie Gottshall",
+	author_email = "jackie.gottshall@gmail.com",
+	description = "Analysis package for single-lead clinical EKG data",
+	long_description = long_description,
+	long_description_content_type = "text/markdown",
+	url="https://github.com/jag2037/cardiopy",
+	packages = setuptools.find_packages(),
+	classifiers = [
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+		"Intended Audience :: Science/Research",
+		"Topic :: Scientific/Engineering :: Medical Science Apps."
+		],
+	python_requires = '>=3.6',
+	install_requires = [
+		"datetime",
+		"matplotlib",
+		"os",
+		"pandas",
+		"scipy",
+		"statistics",
+		"mne",
+		"numpy"
+	]
+	)
