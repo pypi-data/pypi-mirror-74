3c6a1b0f0e0ecf1dc5210233bf003b9a893f7edd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:12 +0000

    Add package kaczmarz-algorithms/0.6.0/kaczmarz_algorithms-0.6.0-py2.py3-none-any.whl

---
+++ packages/kaczmarz-algorithms/kaczmarz_algorithms-0.6.0-py2.py3-none-any.whl/kaczmarz/__init__.py
@@ -0,0 +1,15 @@
+"""Top-level package for Kaczmarz Algorithms."""
+
+# Do not edit this string manually, always use bumpversion
+# Details in CONTRIBUTING.md
+__version__ = "0.6.0"
+
+__author__ = "Jacob Moorman"
+__email__ = "jacob@moorman.me"
+
+__license__ = "MIT"
+__copyright__ = "Copyright (c) 2020, Jacob Moorman"
+
+
+from ._abc import Base
+from ._variants import Cyclic, MaxDistance, Random, SVRandom, UniformRandom
