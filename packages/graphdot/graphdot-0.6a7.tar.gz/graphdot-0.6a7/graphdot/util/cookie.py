2df4cefa52989eb5cae176a60d89bfac49713776
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:29 +0000

    Add package graphdot/0.6a7/graphdot-0.6a7.tar.gz

---
+++ packages/graphdot/graphdot-0.6a7.tar.gz/graphdot-0.6a7/graphdot/util/cookie.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+class VolatileCookie(dict):
+
+    def __getstate__(self):
+        return None
+
+    def __setstate__(self, state):
+        pass
+
+    def __deepcopy__(self, memo):
+        '''Deep copy of a volatile cookie is intentionally nullified.'''
+        return type(self)()
+
+
+if __name__ == '__main__':
+    import pickle
+    cookie = VolatileCookie()
+    print(pickle.dumps(cookie))
+    print(pickle.loads(pickle.dumps(cookie)))
