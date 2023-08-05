7988cb0fa0dab1ac7918bfe54ec05992b1edf888
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:31 +0000

    Add package fastapi-restful/0.2.3/FastApi-RESTful-0.2.3.tar.gz

---
+++ packages/fastapi-restful/FastApi-RESTful-0.2.3.tar.gz/FastApi-RESTful-0.2.3/README.md
@@ -0,0 +1,70 @@
+<p align="center">
+    <em>Quicker FastApi developing tools</em>
+</p>
+<p align="center">
+<img src="https://img.shields.io/github/last-commit/yuval9313/fastapi-utils.svg">
+<a href="https://github.com/yuval9313/fastapi-utils" target="_blank">
+</a>
+<a href="https://fastapi-restful.netlify.app">
+    <img src="https://img.shields.io/netlify/28b2a077-65b1-4d6c-9dba-13aaf6059877" alt="Netlify status">
+</a>
+<br />
+<a href="https://pypi.org/project/FastApi-RESTful" target="_blank">
+    <img src="https://badge.fury.io/py/fastapi-RESTful.svg" alt="Package version">
+</a>
+    <img src="https://img.shields.io/pypi/pyversions/FastApi-RESTful.svg">
+    <img src="https://img.shields.io/github/license/yuval9313/fastapi-utils.svg">
+</p>
+
+---
+
+Based on:
+**Documentation**: <a href="https://fastapi-restful.netlify.app" target="_blank">https://fastapi-utils.davidmontague.xyz</a>
+
+**Source Code**: <a href="https://github.com/yuval9313/fastapi-utils" target="_blank">https://github.com/yuval9313/fastapi-utils</a>
+
+---
+
+<a href="https://fastapi.tiangolo.com">FastAPI</a> is a modern, fast web framework for building APIs with Python 3.6+.
+
+But if you're here, you probably already knew that!
+
+---
+
+## Features
+
+This package includes a number of utilities to help reduce boilerplate and reuse common functionality across projects:
+
+* **Resource Class**: Create CRUD with ease the OOP way with `Resource` base class that lets you implement methods quick.
+* **Class Based Views**: Stop repeating the same dependencies over and over in the signature of related endpoints.
+* **Response-Model Inferring Router**: Let FastAPI infer the `response_model` to use based on your return type annotation. 
+* **Repeated Tasks**: Easily trigger periodic tasks on server startup
+* **Timing Middleware**: Log basic timing information for every request
+* **SQLAlchemy Sessions**: The `FastAPISessionMaker` class provides an easily-customized SQLAlchemy Session dependency 
+* **OpenAPI Spec Simplification**: Simplify your OpenAPI Operation IDs for cleaner output from OpenAPI Generator
+
+---
+
+It also adds a variety of more basic utilities that are useful across a wide variety of projects:
+
+* **APIModel**: A reusable `pydantic.BaseModel`-derived base class with useful defaults
+* **APISettings**: A subclass of `pydantic.BaseSettings` that makes it easy to configure FastAPI through environment variables 
+* **String-Valued Enums**: The `StrEnum` and `CamelStrEnum` classes make string-valued enums easier to maintain
+* **CamelCase Conversions**: Convenience functions for converting strings from `snake_case` to `camelCase` or `PascalCase` and back
+* **GUID Type**: The provided GUID type makes it easy to use UUIDs as the primary keys for your database tables
+
+See the [docs](https://5f10b0e61c052e01e643583d--confident-brahmagupta-e76519.netlify.app/) for more details and examples. 
+
+## Requirements
+
+This package is intended for use with any recent version of FastAPI (depending on `pydantic>=1.0`), and Python 3.6+.
+
+## Installation
+
+```bash
+pip install fastapi-utils
+```
+
+## License
+
+This project is licensed under the terms of the MIT license.
---
+++ packages/fastapi-restful/FastApi-RESTful-0.2.3.tar.gz/FastApi-RESTful-0.2.3/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['fastapi_restful']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['fastapi', 'pydantic>=1.0,<2.0', 'sqlalchemy>=1.3.12,<2.0.0']
+
+setup_kwargs = {
+    'name': 'fastapi-restful',
+    'version': '0.2.3',
+    'description': 'Quicker FastApi developing tools',
+    'long_description': '<p align="center">\n    <em>Quicker FastApi developing tools</em>\n</p>\n<p align="center">\n<img src="https://img.shields.io/github/last-commit/yuval9313/fastapi-utils.svg">\n<a href="https://github.com/yuval9313/fastapi-utils" target="_blank">\n</a>\n<a href="https://fastapi-restful.netlify.app">\n    <img src="https://img.shields.io/netlify/28b2a077-65b1-4d6c-9dba-13aaf6059877" alt="Netlify status">\n</a>\n<br />\n<a href="https://pypi.org/project/FastApi-RESTful" target="_blank">\n    <img src="https://badge.fury.io/py/fastapi-RESTful.svg" alt="Package version">\n</a>\n    <img src="https://img.shields.io/pypi/pyversions/FastApi-RESTful.svg">\n    <img src="https://img.shields.io/github/license/yuval9313/fastapi-utils.svg">\n</p>\n\n---\n\nBased on:\n**Documentation**: <a href="https://fastapi-restful.netlify.app" target="_blank">https://fastapi-utils.davidmontague.xyz</a>\n\n**Source Code**: <a href="https://github.com/yuval9313/fastapi-utils" target="_blank">https://github.com/yuval9313/fastapi-utils</a>\n\n---\n\n<a href="https://fastapi.tiangolo.com">FastAPI</a> is a modern, fast web framework for building APIs with Python 3.6+.\n\nBut if you\'re here, you probably already knew that!\n\n---\n\n## Features\n\nThis package includes a number of utilities to help reduce boilerplate and reuse common functionality across projects:\n\n* **Resource Class**: Create CRUD with ease the OOP way with `Resource` base class that lets you implement methods quick.\n* **Class Based Views**: Stop repeating the same dependencies over and over in the signature of related endpoints.\n* **Response-Model Inferring Router**: Let FastAPI infer the `response_model` to use based on your return type annotation. \n* **Repeated Tasks**: Easily trigger periodic tasks on server startup\n* **Timing Middleware**: Log basic timing information for every request\n* **SQLAlchemy Sessions**: The `FastAPISessionMaker` class provides an easily-customized SQLAlchemy Session dependency \n* **OpenAPI Spec Simplification**: Simplify your OpenAPI Operation IDs for cleaner output from OpenAPI Generator\n\n---\n\nIt also adds a variety of more basic utilities that are useful across a wide variety of projects:\n\n* **APIModel**: A reusable `pydantic.BaseModel`-derived base class with useful defaults\n* **APISettings**: A subclass of `pydantic.BaseSettings` that makes it easy to configure FastAPI through environment variables \n* **String-Valued Enums**: The `StrEnum` and `CamelStrEnum` classes make string-valued enums easier to maintain\n* **CamelCase Conversions**: Convenience functions for converting strings from `snake_case` to `camelCase` or `PascalCase` and back\n* **GUID Type**: The provided GUID type makes it easy to use UUIDs as the primary keys for your database tables\n\nSee the [docs](https://5f10b0e61c052e01e643583d--confident-brahmagupta-e76519.netlify.app/) for more details and examples. \n\n## Requirements\n\nThis package is intended for use with any recent version of FastAPI (depending on `pydantic>=1.0`), and Python 3.6+.\n\n## Installation\n\n```bash\npip install fastapi-utils\n```\n\n## License\n\nThis project is licensed under the terms of the MIT license.\n',
+    'author': 'Yuval Levi',
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://fastapi-restful.netlify.app',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
