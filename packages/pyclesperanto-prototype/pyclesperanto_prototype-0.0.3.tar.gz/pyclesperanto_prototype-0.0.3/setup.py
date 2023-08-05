2799a316be84e2de8423a4805aa17ca0d1c9d8a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:48 +0000

    Add package pyclesperanto-prototype/0.0.3/pyclesperanto_prototype-0.0.3.tar.gz

---
+++ packages/pyclesperanto-prototype/pyclesperanto_prototype-0.0.3.tar.gz/pyclesperanto_prototype-0.0.3/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyclesperanto_prototype", # Replace with your own username
+    version="0.0.3",
+    author="haesleinhuepf",
+    author_email="rhaase@mpi-cbg.de",
+    description="OpenCL based GPU-accelerated image processing (an early prototype)",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/clEsperanto/pyclesperanto_prototype",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ],
+    package_data={"pyclesperanto_prototype":
+                        ['tier0/*.cl',
+                         'tier1/*.cl',
+                         ],
+                    },
+    python_requires='>=3.7',
+)
