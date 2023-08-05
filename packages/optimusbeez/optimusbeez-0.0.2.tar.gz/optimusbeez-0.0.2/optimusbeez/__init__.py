e4131e26b2af08fc01b9bdc4d91868c9ee52856e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:06 +0000

    Add package optimusbeez/0.0.2/optimusbeez-0.0.2.tar.gz

---
+++ packages/optimusbeez/optimusbeez-0.0.2.tar.gz/optimusbeez-0.0.2/optimusbeez/__init__.py
@@ -0,0 +1,15 @@
+from .evaluate import evaluate
+
+from .PSO import optimize
+from .PSO import find_optimal_constants
+
+from .PSO import read_dictionary_from_file
+from .PSO import write_dictionary_to_file
+from .PSO import determine_n_evaluations
+from .PSO import random_informants
+from .PSO import determine_error
+from .PSO import Training_session
+from .PSO import Particle
+from .PSO import Swarm
+
+print("You have imported Optimus Beez")
---
+++ packages/optimusbeez/optimusbeez-0.0.2.tar.gz/optimusbeez-0.0.2/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+	long_description = fh.read()
+
+setup(
+	name = 'optimusbeez',
+	version = '0.0.2',
+	description = 'Simple Particle Swarm Optimization',
+	long_description = long_description,
+	long_description_content_type = 'text/markdown',
+	url='https://github.com/kaisalepajoe/Optimus-Beez',
+	author='Kaisa Lepajõe',
+	author_email='kaisa.lepajoe@gmail.com',
+	license='MIT',
+	packages=setuptools.find_packages(),
+	include_package_data=True,
+	package_data={'optimusbeez': ['optimusbeez/optimal_constants.txt',
+	'optimusbeez/function_info.txt']},
+	classifiers=['Programming Language :: Python :: 3'],
+	install_requires=[
+		'numpy>=1.18.1',
+		'matplotlib>=3.1.3'],
+	test_suite='nose.collector',
+	tests_require=['nose']
+	
+	)
