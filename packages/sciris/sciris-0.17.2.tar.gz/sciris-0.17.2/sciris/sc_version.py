7a9640d070e8214bb9f3f8066ff22ff6b5522bc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:50 +0000

    Add package sciris/0.17.2/sciris-0.17.2.tar.gz

---
+++ packages/sciris/sciris-0.17.2.tar.gz/sciris-0.17.2/sciris/sc_version.py
@@ -0,0 +1,5 @@
+__all__ = ['__version__', '__versiondate__', '__license__']
+
+__version__      = '0.17.2'
+__versiondate__  = '2020-07-13'
+__license__      = 'Sciris %s (%s) -- (c) Sciris.org' % (__version__, __versiondate__)
