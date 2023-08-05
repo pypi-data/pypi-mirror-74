7d139d6b810c7ceb418ae50fda6fe9217e0fe7c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:24 +0000

    Add package molgif/0.3.0/molgif-0.3.0.tar.gz

---
+++ packages/molgif/molgif-0.3.0.tar.gz/molgif-0.3.0/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open('molgif/_version.py', 'r') as fid:
+    exec(fid.read())
+
+with open('README.md', 'r') as readme:
+    # ignore gifs
+    description = ''.join([i for i in readme.readlines()
+                           if not i.startswith('![')])
+
+setuptools.setup(name='molgif',
+                 version=__version__,
+                 author='Michael Cowan',
+                 url='https://www.github.com/michael-cowan/molgif',
+                 description="creates smooth gifs of rotating molecules",
+                 long_description=description,
+                 long_description_content_type='text/markdown',
+                 packages=setuptools.find_packages(),
+                 entry_points={
+                    'console_scripts': ['molgif=molgif.command_line:cli'],
+                 },
+                 python_requires='>=3.5',
+                 install_requires=['matplotlib',
+                                   'pillow',
+                                   'ase>=3.17.0',
+                                   'click'])
