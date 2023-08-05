d9d2ff6c98784663c616d3c2cc63f23fd119ab2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:52 +0000

    Add package deepl-cli/0.0.7/deepl-cli-0.0.7.tar.gz

---
+++ packages/deepl-cli/deepl-cli-0.0.7.tar.gz/deepl-cli-0.0.7/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+setup(
+    name='deepl-cli',
+    version='0.0.7',
+    description='DeepL Translator CLI using Selenium',
+    description_content_type='',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/eggplants/deepl-cli',
+    author='eggplants',
+    packages=find_packages(),
+    python_requires='>=3.8',
+    include_package_data=True,
+    license='MIT',
+    install_requires=['selenium==3.141'],
+    entry_points={
+        'console_scripts': [
+            'deepl=deepl.main:main'
+        ]
+    }
+)
