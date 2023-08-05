83478dd5ee0c8836c5965c2260f078fe3c5e24d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:32 +0000

    Add package getarticle-dev/0.0.6/getarticle-dev-0.0.6.tar.gz

---
+++ packages/getarticle-dev/getarticle-dev-0.0.6.tar.gz/getarticle-dev-0.0.6/getarticle/cli.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+from argparse import ArgumentParser
+import sys
+from getarticle import GetArticle
+import appscript
+from os.path import expanduser
+
+def parse_args():
+    parser = ArgumentParser(description='getarticle CLI')
+    parser.add_argument('-i', '--input', required=False, \
+        help='article DOI or website')
+    parser.add_argument('-o', '--output', required=False, \
+        help='download direction')
+    parser.add_argument('-sd', '--setdownload', required=False, \
+        help='set default download direction')
+    args = parser.parse_args()
+    return args
+
+
+def main(args):
+    if args.setdownload:
+        open("%s/.getarticle.ini" %expanduser("~"), "wb").\
+            write(args.setdownload.encode())
+        return
+    ga = GetArticle()
+    if not args.input:
+        args.input = appscript.app("Safari").windows.first.current_tab.URL()
+    ga.input_article(args.input)
+    ga.download(direction=args.output)
+
+
+def entry_point():
+    args = parse_args()
+    main(args)
+
+
+if __name__ == '__main__':
+    entry_point()
