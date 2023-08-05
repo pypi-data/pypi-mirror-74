72beb7eb55fd9ae74cdbd8cdbee0e2d1d12fbc95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:07 +0000

    Add package sensecam-discovery/2.2.2/sensecam_discovery-2.2.2.tar.gz

---
+++ packages/sensecam-discovery/sensecam_discovery-2.2.2.tar.gz/sensecam_discovery-2.2.2/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+REQUIREMENTS = [line for line in open('requirements.txt').read().split('\n') if line != '']
+
+VERSION = '2.2.2'
+AUTHOR = 'Ricardo Barbosa Filho (Smart Sense Laboratory)'
+EMAIL = 'ricardob@dcc.ufmg.br'
+
+setuptools.setup(
+    name="sensecam_discovery",
+    version=VERSION,
+    author=AUTHOR,
+    author_email=EMAIL,
+    description="A package to discover all onvif cameras on your network",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/smartsenselab/sensecam-discovery",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Natural Language :: English',
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+        'Operating System :: OS Independent',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+    ],
+    python_requires='>=3.6',
+    install_requires=REQUIREMENTS,
+)
