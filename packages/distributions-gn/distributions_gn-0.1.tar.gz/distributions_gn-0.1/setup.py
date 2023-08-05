ca2dec4a51d8bd85640c4eb294055c9d8bc2261e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:54 +0000

    Add package distributions-gn/0.1/distributions_gn-0.1.tar.gz

---
+++ packages/distributions-gn/distributions_gn-0.1.tar.gz/distributions_gn-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='distributions_gn',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_gn'],
+      Author='Georges Naimeh',
+      zip_safe=False)
