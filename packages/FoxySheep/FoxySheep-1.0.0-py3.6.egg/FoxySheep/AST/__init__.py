ceb7f86dc422793bdc9795e2112e3b1a0cfe85c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:32 +0000

    Add package FoxySheep/1.0.0/FoxySheep-1.0.0-py3.6.egg

---
+++ packages/FoxySheep/FoxySheep-1.0.0-py3.6.egg/FoxySheep/AST/__init__.py
@@ -0,0 +1,7 @@
+from FoxySheep.AST.functions import (
+    FunctionDeclarationNode, FunctionCallNode
+    )
+
+from FoxySheep.AST.nodes import ASTNode, Symbol
+
+# Should we import all node classes, or should we only import the modules?
