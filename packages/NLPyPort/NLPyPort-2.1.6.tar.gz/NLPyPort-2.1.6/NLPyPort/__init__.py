b53e78376ec88b17246a57ac1a6c4632214a3f5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:34 +0000

    Add package NLPyPort/2.1.6/NLPyPort-2.1.6.tar.gz

---
+++ packages/NLPyPort/NLPyPort-2.1.6.tar.gz/NLPyPort-2.1.6/NLPyPort/__init__.py
@@ -0,0 +1,7 @@
+from FullPipeline import *
+from LemPyPort.LemFunctions import *
+from LemPyPort.dictionary import *
+from TokPyPort.Tokenizer import *
+from TagPyPort.Tagger import *
+from CRF.CRF_Teste import *
+from text import *
