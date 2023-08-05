80077db593bb6406e0494c763a8bf6f16ba09fb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:08 +0000

    Add package ml-evaluation-framework/0.0b59/ml_evaluation_framework-0.0b59-py3-none-any.whl

---
+++ packages/ml-evaluation-framework/ml_evaluation_framework-0.0b59-py3-none-any.whl/evaluation_framework/constants.py
@@ -0,0 +1,12 @@
+
+
+EF_UUID_NAME = "__specialEF__float32_UUID"
+
+EF_ORDERBY_NAME = "__specialEF__int32_dayDiff"
+
+EF_PREDICTION_NAME = "__specialEF__float32_predictions"
+
+HMF_MEMMAP_MAP_NAME = "__specialHMF__memmapMap"
+
+HMF_GROUPBY_NAME = "__specialHMF__groupByNumericEncoder"
+
