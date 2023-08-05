68025c9d203b10c5bf975970528460591811e374
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:06 +0000

    Add package pytest-salt-factories/0.11.0/pytest-salt-factories-0.11.0.tar.gz

---
+++ packages/pytest-salt-factories/pytest-salt-factories-0.11.0.tar.gz/pytest-salt-factories-0.11.0/saltfactories/factories/proxy.py
@@ -0,0 +1,66 @@
+"""
+saltfactories.factories.proxy
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+Proxy Minion Factory
+"""
+
+try:
+    import salt.config
+    import salt.utils.files
+    import salt.utils.dictupdate
+except ImportError:  # pragma: no cover
+    # We need salt to test salt with saltfactories, and, when pytest is rewriting modules for proper assertion
+    # reporting, we still haven't had a chance to inject the salt path into sys.modules, so we'll hit this
+    # import error, but its safe to pass
+    pass
+
+from saltfactories.utils import ports
+
+
+class ProxyMinionFactory:
+    @staticmethod
+    def default_config(
+        root_dir, proxy_minion_id, config_defaults=None, config_overrides=None, master_port=None,
+    ):
+        if config_defaults is None:
+            config_defaults = salt.config.DEFAULT_MINION_OPTS.copy()
+            config_defaults.update(salt.config.DEFAULT_PROXY_MINION_OPTS.copy())
+            config_defaults.pop("user", None)
+
+        conf_dir = root_dir.join("conf").ensure(dir=True)
+        conf_file = conf_dir.join("proxy").strpath
+
+        _config_defaults = {
+            "id": proxy_minion_id,
+            "conf_file": conf_file,
+            "root_dir": root_dir.strpath,
+            "interface": "127.0.0.1",
+            "master": "127.0.0.1",
+            "master_port": master_port or ports.get_unused_localhost_port(),
+            "tcp_pub_port": ports.get_unused_localhost_port(),
+            "tcp_pull_port": ports.get_unused_localhost_port(),
+            "pidfile": "run/proxy.pid",
+            "pki_dir": "pki",
+            "cachedir": "cache",
+            "sock_dir": "run/proxy",
+            "log_file": "logs/proxy.log",
+            "log_level_logfile": "debug",
+            "loop_interval": 0.05,
+            #'multiprocessing': False,
+            "log_fmt_console": "%(asctime)s,%(msecs)03.0f [%(name)-17s:%(lineno)-4d][%(levelname)-8s][%(processName)18s(%(process)d)] %(message)s",
+            "log_fmt_logfile": "[%(asctime)s,%(msecs)03.0f][%(name)-17s:%(lineno)-4d][%(levelname)-8s][%(processName)18s(%(process)d)] %(message)s",
+            "hash_type": "sha256",
+            "transport": "zeromq",
+            "add_proxymodule_to_opts": False,
+            "proxy": {"proxytype": "dummy"},
+            "pytest-minion": {"log": {"prefix": "{{cli_name}}({})".format(proxy_minion_id)},},
+        }
+        # Merge in the initial default options with the internal _config_defaults
+        salt.utils.dictupdate.update(config_defaults, _config_defaults, merge_lists=True)
+
+        if config_overrides:
+            # Merge in the default options with the proxy_config_overrides
+            salt.utils.dictupdate.update(config_defaults, config_overrides, merge_lists=True)
+
+        return config_defaults
---
+++ packages/pytest-salt-factories/pytest-salt-factories-0.11.0.tar.gz/pytest-salt-factories-0.11.0/saltfactories/utils/processes/sshd.py
@@ -0,0 +1,87 @@
+"""
+    saltfactories.utils.processes.sshd
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    SSHD daemon process implementation
+"""
+import logging
+
+from saltfactories.exceptions import ProcessFailed
+from saltfactories.utils import ports
+from saltfactories.utils.processes.bases import FactoryDaemonScriptBase
+from saltfactories.utils.processes.bases import Popen
+
+log = logging.getLogger(__name__)
+
+
+class SshdDaemon(FactoryDaemonScriptBase):
+    def __init__(self, *args, **kwargs):
+        config_dir = kwargs.pop("config_dir")
+        serve_port = kwargs.pop("serve_port", None)
+        sshd_config_dict = kwargs.pop("sshd_config_dict", None) or {}
+        super().__init__(*args, **kwargs)
+        self.config_dir = config_dir
+        self.serve_port = serve_port or ports.get_unused_localhost_port()
+        _default_config = {
+            "Port": self.serve_port,
+            "ListenAddress": "127.0.0.1",
+            "PermitRootLogin": "no",
+            "ChallengeResponseAuthentication": "no",
+            "PasswordAuthentication": "no",
+            "PubkeyAuthentication": "yes",
+            "PrintMotd": "no",
+            "PidFile": self.config_dir / "sshd.pid",
+        }
+        _default_config.update(sshd_config_dict)
+        self._sshd_config = _default_config
+        self._write_config()
+
+    def get_base_script_args(self):
+        """
+        Returns any additional arguments to pass to the CLI script
+        """
+        return ["-D", "-e", "-f", str(self.config_dir / "sshd_config")]
+
+    def get_check_ports(self):
+        """
+        Return a list of ports to check against to ensure the daemon is running
+        """
+        return [self.serve_port]
+
+    def _write_config(self):
+        sshd_config_file = self.config_dir / "sshd_config"
+        if not sshd_config_file.exists():
+            # Let's generat the host keys
+            host_keys = []
+            for key_type in ("dsa", "rsa"):
+                key_path = self.config_dir / "ssh_host_{}_key".format(key_type)
+                if not key_path.exists():
+                    cmdline = ["ssh-keygen", "-f", str(key_path), "-N", "", "-t", key_type]
+                    proc = Popen(cmdline)
+                    stdout, stderr = proc.communicate()
+                    if proc.returncode:
+                        raise ProcessFailed(
+                            "Failed to generate {} key.",
+                            cmdline=cmdline,
+                            stdout=stdout,
+                            stderr=stderr,
+                            exitcode=proc.returncode,
+                        )
+                key_path.chmod(0o0400)
+                host_keys.append(key_path)
+
+            # Let's write a default config file
+            with open(str(sshd_config_file), "w") as wfh:
+                config_lines = sorted(
+                    "{} {}\n".format(key, value) for key, value in self._sshd_config.items()
+                )
+                wfh.write("".join(config_lines))
+                for host_key in host_keys:
+                    wfh.write("HostKey {}\n".format(host_key))
+            sshd_config_file.chmod(0o0600)
+            with open(str(sshd_config_file)) as wfh:
+                log.debug(
+                    "Wrote to configuration file %s. Configuration:\n%s",
+                    sshd_config_file,
+                    wfh.read(),
+                )
