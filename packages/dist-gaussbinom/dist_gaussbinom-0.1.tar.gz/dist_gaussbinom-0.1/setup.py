2e429bc35e62be1be51e4b7008c27897bdea2c8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:55 +0000

    Add package dist-gaussbinom/0.1/dist_gaussbinom-0.1.tar.gz

---
+++ packages/dist-gaussbinom/dist_gaussbinom-0.1.tar.gz/dist_gaussbinom-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dist_gaussbinom',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['dist_gaussbinom'],
+      author = 'Murali Mohana Krishna Dandu',
+      author_email = 'murali.munna94@gmail.com',
+      zip_safe=False)
