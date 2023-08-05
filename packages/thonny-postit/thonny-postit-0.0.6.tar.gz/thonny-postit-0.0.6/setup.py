93903862cef0813b4116873fadbf115b03daab96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:57 +0000

    Add package thonny-postit/0.0.6/thonny-postit-0.0.6.tar.gz

---
+++ packages/thonny-postit/thonny-postit-0.0.6.tar.gz/thonny-postit-0.0.6/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup (
+        name="thonny-postit",
+        version="0.0.6",
+        description="Program post-it for Thonny IDE",
+        long_description="""Program post-it for Thonny IDE""",
+        url="https://github.com/beardad1975/thonny-postit",
+        author="Wen-Hung, Chang 張文宏",
+        author_email="beardad1975@nmes.tyc.edu.tw",
+        
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+            "Operating System :: Microsoft :: Windows",
+            #"Operating System :: MacOS",
+            #"Operating System :: POSIX :: Linux",
+            "Natural Language :: Chinese (Traditional)",
+        ],
+        
+        platforms=["Windows"],
+        python_requires=">=3.5",
+        package_data={'thonnycontrib.postit': ['images/*','tools/*']},
+        install_requires=["thonny >= 3.2.0","pillow >= 7.1.2"],
+        packages=["thonnycontrib.postit"],
+)
