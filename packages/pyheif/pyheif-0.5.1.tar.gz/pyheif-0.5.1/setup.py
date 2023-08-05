6c97df37eea2808a8582339d0378d4123bf1bf93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:27 +0000

    Add package pyheif/0.5.1/pyheif-0.5.1.tar.gz

---
+++ packages/pyheif/pyheif-0.5.1.tar.gz/pyheif-0.5.1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("pyheif/data/version.txt") as f:
+    version = f.read().strip()
+
+setup(
+    name="pyheif",
+    version=version,
+    packages=["pyheif"],
+    package_data={"pyheif": ["data/*"]},
+    install_requires=["cffi>=1.0.0"],
+    setup_requires=["cffi>=1.0.0"],
+    cffi_modules=["libheif/libheif_build.py:ffibuilder"],
+    author="David Poirier",
+    author_email="david-poirier-csn@users.noreply.github.com",
+    description="Python 3.6+ interface to libheif library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    python_requires=">= 3.6",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: POSIX :: Linux",
+    ],
+    keywords="heif heic",
+    url="https://github.com/david-poirier-csn/pyheif",
+)
