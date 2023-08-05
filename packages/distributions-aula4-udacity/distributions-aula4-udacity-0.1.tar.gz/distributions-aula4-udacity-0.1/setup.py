5727682c3cdfa5638992ec86f5e47647408823c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:45 +0000

    Add package distributions-aula4-udacity/0.1/distributions-aula4-udacity-0.1.tar.gz

---
+++ packages/distributions-aula4-udacity/distributions-aula4-udacity-0.1.tar.gz/distributions-aula4-udacity-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions-aula4-udacity',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions-aula4-udacity'],
+      zip_safe=False)
