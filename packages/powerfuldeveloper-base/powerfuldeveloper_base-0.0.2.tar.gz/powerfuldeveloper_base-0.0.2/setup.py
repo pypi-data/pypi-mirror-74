c7cba437d391812748393db4bbc59b2338502715
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:36 +0000

    Add package powerfuldeveloper-base/0.0.2/powerfuldeveloper_base-0.0.2.tar.gz

---
+++ packages/powerfuldeveloper-base/powerfuldeveloper_base-0.0.2.tar.gz/powerfuldeveloper_base-0.0.2/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="powerfuldeveloper_base",  # Replace with your own username
+    install_requires=[
+        "psutil==5.7.2",
+    ],
+    version="0.0.2",
+    author="Hussein Mirzaki",
+    author_email="husseinmirzaki@gmail.com",
+    description="Basic Foundation of All created by him",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    python_requires='>=3.7',
+)
