d87c771c516453e301cfb66d16c1a006f6edab31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:12 +0000

    Add package kav-distributions/1/kav_distributions-1.tar.gz

---
+++ packages/kav-distributions/kav_distributions-1.tar.gz/kav_distributions-1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='kav_distributions',
+      version='1',
+      description='Gaussian distributions practice of package upload',
+      packages=['kav_distributions'],
+      author = 'kavya',
+      author_email = 'kushnoor.kk@gmail.com',
+      zip_safe=False)
