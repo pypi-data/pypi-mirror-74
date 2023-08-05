2a96415964e5db847ac184086d040a8468ab4710
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:54 +0000

    Add package z2n-periodogram/2.0.5/z2n-periodogram-2.0.5.tar.gz

---
+++ packages/z2n-periodogram/z2n-periodogram-2.0.5.tar.gz/z2n-periodogram-2.0.5/setup.py
@@ -0,0 +1,64 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Generic/Built-in
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+    setuptools.setup(
+        name='z2n-periodogram',
+        version='2.0.5',
+        license='MIT',
+        python_requires='>=3.6.9',
+        install_requires=[
+            'intel-openmp',
+            'click',
+            'pillow',
+            'click-shell',
+            'matplotlib',
+            'termtables',
+            'colorama',
+            'astropy',
+            'psutil',
+            'numpy',
+            'numba',
+            'icc-rt',
+            'scipy',
+            'h5py',
+            'tqdm',
+            'tbb',
+        ],
+        tests_require=['pytest'],
+        extras_require={
+            "dev": [
+                "pytest",
+                "mkdocs",
+                "mkautodoc",
+                "markdown-katex",
+            ],
+        },
+        entry_points='''
+            [console_scripts]
+            z2n=z2n.main:cli
+        ''',
+        author='Yohan Alexander',
+        author_email='yohanfranca@gmail.com',
+        description='A package for interative periodograms analysis.',
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        url='https://z2n-periodogram.readthedocs.io',
+        project_urls={
+            "Documentation": "https://z2n-periodogram.readthedocs.io",
+            "Source": "https://github.com/yohanalexander/z2n-periodogram",
+        },
+        include_package_data=True,
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+            "Intended Audience :: Science/Research",
+            "Topic :: Scientific/Engineering :: Astronomy",
+        ],
+        packages=setuptools.find_namespace_packages(),
+    )
