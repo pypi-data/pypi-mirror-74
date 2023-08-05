d966c08610276b01a7a9b659c5bd81a205946b17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:54 +0000

    Add package fkfyz-probability/0.1/fkfyz_probability-0.1.tar.gz

---
+++ packages/fkfyz-probability/fkfyz_probability-0.1.tar.gz/fkfyz_probability-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='fkfyz_probability',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['fkfyz_probability'],
+      author = 'feng kai',
+      author_email = 'kaifeng0502@gmail.com',
+      zip_safe=False)
