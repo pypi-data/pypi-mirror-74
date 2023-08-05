f39604a093497cb517ae3f33a4a51206d206b594
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:35 +0000

    Add package standup-notes/2.1.0/standup_notes-2.1.0-py3-none-any.whl

---
+++ packages/standup-notes/standup_notes-2.1.0-py3-none-any.whl/standup_notes/resources/standup-notes.bash
@@ -0,0 +1,30 @@
+#/usr/bin/env bash
+
+_standup_completions()
+{
+    local cur opts prev prev2
+    COMPREPLY=()
+    cur="${COMP_WORDS[COMP_CWORD]}"
+    prev="${COMP_WORDS[COMP_CWORD-1]}"
+    prev2="${COMP_WORDS[COMP_CWORD-2]}"
+
+    opts="-l --list -r --read -c --copy -e --edit -d --delete -p --post -x --editcopy"
+    days="--today --tomorrow --yesterday"
+
+    case "${prev}" in
+      -l | --list | -d | --delete | -p | --post | --today | --tomorrow | --yesterday)
+        return 0
+        ;;
+      -c | --copy | -e | --edit | -r | --read | -x | --editcopy)
+        if [ "$prev2" = "--yesterday" ] || [ "$prev2" = "--today" ] ||[ "$prev2" = "--tomorrow" ] ; then
+          return 0
+        fi
+        COMPREPLY=( $(compgen -W "${days}" -- ${cur}))
+        return 0
+        ;;
+      *)
+      ;;
+    esac
+    COMPREPLY=( $(compgen -W "${opts}" -- ${cur}))
+}
+complete -F _standup_completions standup-notes
