49f40cb542521ef89021d8f90913272f780989c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:25 +0000

    Add package openhtf-plugin/0.0.2/openhtf-plugin-0.0.2.tar.gz

---
+++ packages/openhtf-plugin/openhtf-plugin-0.0.2.tar.gz/openhtf-plugin-0.0.2/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="openhtf-plugin", 
+    version="0.0.2",
+    author="Kevin Pulley",
+    author_email="kpulley@imaginecommunications.com",
+    description="Simple Kiwi-TCMS plugin for OpenHTF",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/UndyingScroll",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+       "spintop-openhtf",
+       "tcms-api"
+   ],
+    python_requires='>=3.6',
+)
