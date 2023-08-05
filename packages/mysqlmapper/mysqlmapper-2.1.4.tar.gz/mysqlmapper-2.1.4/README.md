637a6181673b84f41741e5b0fc03ac4078e62135
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:50 +0000

    Add package mysqlmapper/2.1.4/mysqlmapper-2.1.4.tar.gz

---
+++ packages/mysqlmapper/mysqlmapper-2.1.4.tar.gz/mysqlmapper-2.1.4/README.md
@@ -0,0 +1,11 @@
+# mysqlmapper
+MySQL query tool class used by python.
+
+0.x.x Original version 
+
+1.x.x 
+* No transaction version
+* Need tabledbmapper 0.0.1
+
+2.1.4
+* Need tabledbmapper 0.1.4
---
+++ packages/mysqlmapper/mysqlmapper-2.1.4.tar.gz/mysqlmapper-2.1.4/mysqlmapper/facade.py
@@ -0,0 +1,10 @@
+from tabledbmapper.facade import *
+
+from mysqlmapper.manager.mvc.holder import *
+from mysqlmapper.manager.mvc.info import *
+from mysqlmapper.manager.mvc.mapper import *
+from mysqlmapper.manager.session.sql_session_factory import *
+
+from mysqlmapper.engine import *
+
+from mysqlmapper.manager.session.decorators.sql_session import *
