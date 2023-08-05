52f515650d06234f87d2721e1822b97be7f9d003
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:07 +0000

    Add package distributionsMethod3/1.3/distributionsMethod3-1.3.tar.gz

---
+++ packages/distributionsMethod3/distributionsMethod3-1.3.tar.gz/distributionsMethod3-1.3/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributionsMethod3',
+      version='1.3',
+      description='Gaussian distributions',
+      packages=['distributionsMethod3'],
+      zip_safe=False)
