1e42b2ce4721449f35ec859406b23a1476f542c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:52 +0000

    Add package salesvision/0.1.4/salesvision-0.1.4.tar.gz

---
+++ packages/salesvision/salesvision-0.1.4.tar.gz/salesvision-0.1.4/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="salesvision",
+    version="0.1.4",
+    author="broutonlab",
+    author_email="m.yurushkin@broutonlab.com",
+    description="Salesvision provides accurate, reliable and scalable fashion image analysis",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "imagehash",
+        "io",
+        "numpy",
+        "pymongo",
+        "requests",
+        "pillow",
+        "opencv-python"
+    ],
+    python_requires='>=3.6',
+)
