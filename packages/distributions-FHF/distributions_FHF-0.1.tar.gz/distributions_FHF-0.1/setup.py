70e9f3018b0dc79d47bb59894ea7f6a8a3f1c218
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:45 +0000

    Add package distributions-FHF/0.1/distributions_FHF-0.1.tar.gz

---
+++ packages/distributions-FHF/distributions_FHF-0.1.tar.gz/distributions_FHF-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='distributions_FHF',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_FHF'],
+      author = 'Federico Hellmund',
+      zip_safe = False)
