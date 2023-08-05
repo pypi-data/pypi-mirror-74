9c54e2b394844b9d253f19269c157f8a667789bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:56 +0000

    Add package kavya-distributions/0.1/kavya_distributions-0.1.tar.gz

---
+++ packages/kavya-distributions/kavya_distributions-0.1.tar.gz/kavya_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='kavya_distributions',
+      version='0.1',
+      description='Gaussian distributions practice upload 2',
+      packages=['kavya_distributions'],
+      author = 'kavya',
+      author_email = 'k.kavya101@gmail.com',
+      zip_safe=False)
