5fc62f041c765db77b3a8a2445ab9a1ac2b34b90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:53 +0000

    Add package prettyqt/0.98.5/PrettyQt-0.98.5.tar.gz

---
+++ packages/prettyqt/PrettyQt-0.98.5.tar.gz/PrettyQt-0.98.5/prettyqt/custom_widgets/optionalwidget.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+"""
+
+from prettyqt import widgets
+
+
+class OptionalWidget(widgets.GroupBox):
+    def __init__(self, widget, title="", parent=None):
+        super().__init__(checkable=True, title=title)
+        self.set_layout("vertical")
+        self.box.add(widget)
+        self.widget = widget
+        self.toggled.connect(self.widget.setEnabled)
+
+    def __getattr__(self, value):
+        return self.widget.__getattribute__(value)
+
+    @property
+    def enabled(self) -> bool:
+        return self.isChecked()
+
+    @enabled.setter
+    def enabled(self, state: bool):
+        self.setChecked(state)
+
+    def get_value(self):
+        if self.isChecked():
+            return self.widget.get_value()
+        return None
+
+
+if __name__ == "__main__":
+    app = widgets.app()
+    img = widgets.RadioButton("test")
+    widget = OptionalWidget(img, "Test")
+    widget.show()
+    app.exec_()
+    print(widget.enabled)
