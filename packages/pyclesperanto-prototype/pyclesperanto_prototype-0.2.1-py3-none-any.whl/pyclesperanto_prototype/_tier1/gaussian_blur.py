c9b487d1e6c852ae0bc434af4071d2f6a43ffec2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:46 +0000

    Add package pyclesperanto-prototype/0.2.1/pyclesperanto_prototype-0.2.1-py3-none-any.whl

---
+++ packages/pyclesperanto-prototype/pyclesperanto_prototype-0.2.1-py3-none-any.whl/pyclesperanto_prototype/_tier1/gaussian_blur.py
@@ -0,0 +1,33 @@
+from .._tier0 import sigma_to_kernel_size
+from .._tier0 import plugin_function
+from .._tier0 import Image
+from .execute_separable_kernel import execute_separable_kernel
+
+
+@plugin_function
+def gaussian_blur (src :Image, dst :Image = None, sigma_x :float=0, sigma_y :float=0, sigma_z :float=0):
+    """
+    documentation placeholder
+    """
+
+
+    kernel_size_x = sigma_to_kernel_size(sigma_x)
+    kernel_size_y = sigma_to_kernel_size(sigma_y)
+    kernel_size_z = sigma_to_kernel_size(sigma_z)
+
+    execute_separable_kernel(
+        src,
+        dst,
+        __file__,
+        'gaussian_blur_separable_' + str(len(dst.shape)) + 'd_x.cl',
+        'gaussian_blur_separable_' + str(len(dst.shape)) + 'd',
+        kernel_size_x,
+        kernel_size_y,
+        kernel_size_z,
+        sigma_x,
+        sigma_y,
+        sigma_z,
+        len(dst.shape)
+    )
+
+    return dst
