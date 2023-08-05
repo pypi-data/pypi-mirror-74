ac8c73b1993f79efc2d5fd662cc0c1c12b848b2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:40:21 +0000

    Add package dsb-gauss-bin/0.1/dsb_gauss_bin-0.1.tar.gz

---
+++ packages/dsb-gauss-bin/dsb_gauss_bin-0.1.tar.gz/dsb_gauss_bin-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dsb_gauss_bin',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['dsb_gauss_bin'],
+      author = 'Thariq Ramadhan',
+      author_mail = 'athariqramadhan@gmail.com',
+      zip_safe=False)
