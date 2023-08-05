6bdb79ec5087835be11473736110bed165533295
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:08 +0000

    Add package rsdist/0.1/rsdist-0.1.tar.gz

---
+++ packages/rsdist/rsdist-0.1.tar.gz/rsdist-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='rsdist',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions'],
+      author= 'reham galal',
+      author_email = 'rehamgalal1@outlook.com',
+      zip_safe=False)
