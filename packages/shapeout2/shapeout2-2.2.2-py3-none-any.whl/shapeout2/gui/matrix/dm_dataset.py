e1d5a9ecdd8aed5f4944fffe0be34d72a873bc84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:57 +0000

    Add package shapeout2/2.2.2/shapeout2-2.2.2-py3-none-any.whl

---
+++ packages/shapeout2/shapeout2-2.2.2-py3-none-any.whl/shapeout2/gui/matrix/dm_dataset.py
@@ -0,0 +1,102 @@
+import pkg_resources
+
+from PyQt5 import uic, QtWidgets, QtCore
+
+from ... import meta_tool
+from ... import pipeline
+
+
+class MatrixDataset(QtWidgets.QWidget):
+    active_toggled = QtCore.pyqtSignal()
+    enabled_toggled = QtCore.pyqtSignal(bool)
+    option_action = QtCore.pyqtSignal(str)
+    modify_clicked = QtCore.pyqtSignal(str)
+
+    def __init__(self, identifier=None, state=None):
+        """Create a new dataset matrix element
+
+        Specify either an existing Dataslot identifier or a
+        Dataslot state
+        """
+        QtWidgets.QWidget.__init__(self)
+        path_ui = pkg_resources.resource_filename(
+            "shapeout2.gui.matrix", "dm_dataset.ui")
+        uic.loadUi(path_ui, self)
+
+        # options button
+        menu = QtWidgets.QMenu()
+        menu.addAction('insert anew', self.action_insert_anew)
+        menu.addAction('duplicate', self.action_duplicate)
+        menu.addAction('remove', self.action_remove)
+        self.toolButton_opt.setMenu(menu)
+
+        # toggle all active, all inactive, semi state
+        self.toolButton_toggle.clicked.connect(self.active_toggled.emit)
+
+        # toggle enabled/disabled state
+        self.checkBox.clicked.connect(self.enabled_toggled.emit)
+
+        # modify slot button
+        self.toolButton_modify.clicked.connect(self.on_modify)
+
+        # reduce font size of name
+        font = self.label.font()
+        font.setPointSize(font.pointSize()-2)
+        self.label.setFont(font)
+
+        if state is None:
+            slot = pipeline.Dataslot._instances[identifier]
+            self.identifier = identifier
+            self.path = slot.path
+            # set tooltip/label
+            self.update_content()
+        else:
+            self.__setstate__(state)
+
+    def __getstate__(self):
+        state = {"path": self.path,
+                 "identifier": self.identifier,
+                 "enabled": self.checkBox.isChecked(),
+                 }
+        return state
+
+    def __setstate__(self, state):
+        self.identifier = state["identifier"]
+        self.path = state["path"]
+        self.checkBox.setChecked(state["enabled"])
+        self.update_content()
+
+    def action_duplicate(self):
+        self.option_action.emit("duplicate")
+
+    def action_insert_anew(self):
+        self.option_action.emit("insert_anew")
+
+    def action_remove(self):
+        self.option_action.emit("remove")
+
+    def on_modify(self):
+        self.modify_clicked.emit(self.identifier)
+
+    def set_label_string(self, string):
+        if self.label.fontMetrics().boundingRect(string).width() < 65:
+            nstring = string
+        else:
+            nstring = string + "..."
+            while True:
+                width = self.label.fontMetrics().boundingRect(nstring).width()
+                if width > 65:
+                    nstring = nstring[:-4] + "..."
+                else:
+                    break
+        self.label.setText(nstring)
+
+    def update_content(self):
+        """Reset tool tips and title"""
+        if self.path is not None:
+            tip = meta_tool.get_repr(self.path, append_path=True)
+            self.setToolTip(tip)
+            self.label.setToolTip(tip)
+            slot = pipeline.Dataslot._instances[self.identifier]
+            name = slot.name
+            self.set_label_string(name)
---
+++ packages/shapeout2/shapeout2-2.2.2-py3-none-any.whl/shapeout2/util.py
@@ -0,0 +1,68 @@
+import hashlib
+import numbers
+import pathlib
+
+import h5py
+import numpy as np
+
+
+def hashobj(obj):
+    """Compute md5 hex-hash of a Python object"""
+    return hashlib.md5(obj2str(obj)).hexdigest()
+
+
+def obj2str(obj):
+    """String representation of an object for hashing"""
+    if isinstance(obj, str):
+        return obj.encode("utf-8")
+    elif isinstance(obj, pathlib.Path):
+        return obj2str(str(obj))
+    elif isinstance(obj, (bool, numbers.Number)):
+        return str(obj).encode("utf-8")
+    elif obj is None:
+        return b"none"
+    elif isinstance(obj, np.ndarray):
+        return obj.tostring()
+    elif isinstance(obj, tuple):
+        return obj2str(list(obj))
+    elif isinstance(obj, list):
+        return b"".join(obj2str(o) for o in obj)
+    elif isinstance(obj, dict):
+        return obj2str(sorted(obj.items()))
+    elif hasattr(obj, "identifier"):
+        return obj2str(obj.identifier)
+    elif isinstance(obj, h5py.Dataset):
+        return obj2str(obj[0])
+    else:
+        raise ValueError("No rule to convert object '{}' to string.".
+                         format(obj.__class__))
+
+
+def get_valid_filename(value):
+    """
+    Return the given string converted to a string that can be used
+    for a clean filename.
+    """
+    ret = ""
+
+    valid = "abcdefghijklmnopqrstuvwxyz" \
+            + "ABCDEFGHIJKLMNOPQRSTUVWXYZ" \
+            + "0123456789" \
+            + "._-()"
+    replace = {
+        " ": "_",
+        "[": "(",
+        "]": ")",
+        "µ": "u",
+    }
+
+    for ch in value:
+        if ch in valid:
+            ret += ch
+        elif ch in replace:
+            ret += replace[ch]
+        else:
+            ret += "?"
+
+    ret = ret.strip(".")
+    return ret
