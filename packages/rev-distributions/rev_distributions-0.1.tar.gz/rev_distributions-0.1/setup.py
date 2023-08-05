8209630e964112cb159ec266bfcbced647f4be50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:44 +0000

    Add package rev-distributions/0.1/rev_distributions-0.1.tar.gz

---
+++ packages/rev-distributions/rev_distributions-0.1.tar.gz/rev_distributions-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='rev_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions'],
+      classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+      zip_safe=False)
