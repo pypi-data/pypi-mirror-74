f9684769fd5704d537deee4d79b2e212983e3fdd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:23 +0000

    Add package twitter-word-cloud/1.2/twitter_word_cloud-1.2.tar.gz

---
+++ packages/twitter-word-cloud/twitter_word_cloud-1.2.tar.gz/twitter_word_cloud-1.2/README.md
@@ -0,0 +1,7 @@
+# Twitter Word Cloud, A python package to generate word cloud charts from tweets
+
+## Installation:
+`pip install twitter-word-cloud`
+
+## usage
+Checkout `example.py`
---
+++ packages/twitter-word-cloud/twitter_word_cloud-1.2.tar.gz/twitter_word_cloud-1.2/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+from setuptools import setup
+
+setup(name='twitter_word_cloud',
+      version='1.2',
+      author="Ayman Sherief",
+      author_email="aymanf.sherief@gmail.com",
+      description='Generate word cloud charts from twitter',
+      long_description=open("README.md").read(),
+      url="https://github.com/Aymanf-sherief/TwitterWordCloud",
+      packages=setuptools.find_packages(),
+      classifiers=[
+          "Programming Language :: Python :: 3",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: OS Independent",
+      ],
+      zip_safe=False)
