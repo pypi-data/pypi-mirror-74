907fbd347b27568bf1d9ed8892bf10c3fa7cb675
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:26 +0000

    Add package dsnd3756-probability/1.3756/dsnd3756_probability-1.3756.tar.gz

---
+++ packages/dsnd3756-probability/dsnd3756_probability-1.3756.tar.gz/dsnd3756_probability-1.3756/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dsnd3756_probability',
+      version='1.3756',
+      description='Gaussian and Binomial distributions',
+      packages=['dsnd3756_probability'],
+      author='Carlos A., Andrew Paster',
+      author_email='karlovski@live.com',
+      zip_safe=False)
