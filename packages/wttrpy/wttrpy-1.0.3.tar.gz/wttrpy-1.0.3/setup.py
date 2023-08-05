a4f85f204fcacf56b9c7be5a14af5eb5e827dd55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:47 +0000

    Add package wttrpy/1.0.3/wttrpy-1.0.3.tar.gz

---
+++ packages/wttrpy/wttrpy-1.0.3.tar.gz/wttrpy-1.0.3/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open('README.md', 'r') as file:
+    readme = file.read()
+
+setuptools.setup(
+    name="wttrpy",
+    version="1.0.3",
+    author="Denisov Artem",
+    author_email="hashadev@yandex.ru",
+    description="A little thing on Python that shows you forecast using wttr.in",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/hasha2982/wttr.py/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.5',
+    install_requires=['requests>=2,<3'],
+    project_urls={
+        'Documentation': "https://wttrpy.rtfd.io/",
+        'Source Code': "https://github.com/hasha2982/wttr.py/",
+        'Issues': "https://github.com/hasha2982/wttr.py/issues/"
+    }
+)
