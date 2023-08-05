03f6953468dece92cc7e0b6c723b31600fad7ab9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:53 +0000

    Add package hiMoon/0.7/hiMoon-0.7-py3-none-any.whl

---
+++ packages/hiMoon/hiMoon-0.7-py3-none-any.whl/hiMoon/__init__.py
@@ -0,0 +1,41 @@
+#    Copyright 2020 Solomon M. Adams
+
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+
+#        http://www.apache.org/licenses/LICENSE-2.0
+
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
+__author__ = "Solomon M. Adams, PharmD, PhD"
+__copyright__ = "Copyright 2020, Solomon M. Adams"
+__license__ = "Apache v2.0"
+__version__ = "0.7"
+__maintainer__ = "Solomon M. Adams, PharmD, PhD"
+__email__ = "sadams07@su.edu"
+
+import logging as LOGGING
+
+from .config import ConfigData
+
+# Initialize logger
+LOGGING.getLogger().setLevel(LOGGING.WARNING)
+
+# Initialize config
+CONFIG = ConfigData()
+
+def set_logging_info():
+    LOGGING.getLogger().setLevel(LOGGING.INFO)
+
+def set_config(config_path):
+    """
+    Set a custom config based on a path
+    Args:
+        config_path ([type]): path/to/config/file.ini
+    """
+    CONFIG = ConfigData(config_path)
