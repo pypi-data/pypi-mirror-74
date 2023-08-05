f6a31b7a0eaec2516a1c95d61f1a7be3222f6cbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:32 +0000

    Add package whatsappguru/0.2.2/whatsappguru-0.2.2.tar.gz

---
+++ packages/whatsappguru/whatsappguru-0.2.2.tar.gz/whatsappguru-0.2.2/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['whatsappguru']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['emoji>=0.5.4,<0.6.0',
+ 'pandas>=1.0.5,<2.0.0',
+ 'progressbar>=2.5,<3.0',
+ 'uuid>=1.30,<2.0']
+
+setup_kwargs = {
+    'name': 'whatsappguru',
+    'version': '0.2.2',
+    'description': '',
+    'long_description': None,
+    'author': 'Lorenzo Coacci',
+    'author_email': 'lorenzo@coacci.it',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
