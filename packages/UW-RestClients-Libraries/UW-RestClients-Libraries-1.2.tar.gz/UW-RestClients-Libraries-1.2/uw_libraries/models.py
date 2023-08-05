e3cdb7a8cdd0332c822c9127e8ab4c0442253906
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:23 +0000

    Add package UW-RestClients-Libraries/1.2/UW-RestClients-Libraries-1.2.tar.gz

---
+++ packages/UW-RestClients-Libraries/UW-RestClients-Libraries-1.2.tar.gz/UW-RestClients-Libraries-1.2/uw_libraries/models.py
@@ -0,0 +1,63 @@
+import json
+from restclients_core import models
+
+
+class Library(models.Model):
+    name = models.CharField(max_length=250, null=True)
+    description = models.CharField(max_length=1000, null=True)
+    url = models.CharField(max_length=250, null=True)
+
+
+class Librarian(models.Model):
+    name = models.CharField(max_length=250, null=True)
+    email = models.CharField(max_length=100, null=True)
+    phone = models.CharField(max_length=20, null=True)
+    url = models.CharField(max_length=250, null=True)
+
+
+class CourseGuide(models.Model):
+    guide_url = models.CharField(max_length=250, null=True)
+    guide_text = models.CharField(max_length=1000, null=True)
+
+
+class SubjectGuide(models.Model):
+    contact_url = models.CharField(max_length=250, null=True)
+    contact_text = models.CharField(max_length=1000, null=True)
+    discipline = models.CharField(max_length=250, null=True)
+    find_librarian_url = models.CharField(max_length=250, null=True)
+    find_librarian_text = models.CharField(max_length=1000, null=True)
+    guide_url = models.CharField(max_length=250, null=True)
+    guide_text = models.CharField(max_length=1000, null=True)
+    faq_url = models.CharField(max_length=250, null=True)
+    faq_text = models.CharField(max_length=1000, null=True)
+    writing_guide_url = models.CharField(max_length=250, null=True)
+    writing_guide_text = models.CharField(max_length=1000, null=True)
+    is_default_guide = models.NullBooleanField()
+    default_guide_campus = models.CharField(max_length=50, null=True)
+    course_guide = models.ForeignKey(CourseGuide, null=True)
+
+
+class MyLibAccount(models.Model):
+    holds_ready = models.IntegerField()
+    fines = models.DecimalField(max_digits=8, decimal_places=2)
+    items_loaned = models.IntegerField()
+    next_due = models.DateTimeField(null=True)
+
+    def get_next_due_date_str(self):
+        """
+        return next due date in the ISO format (yyyy-mm-dd).
+        If the next_due is None, return None.
+        """
+        if self.next_due is not None:
+            return self.next_due.isoformat()  # 2020-07-15T02:00:00+00:00
+        return None
+
+    def json_data(self):
+        return {'holds_ready': self.holds_ready,
+                'fines': self.fines,
+                'items_loaned': self.items_loaned,
+                'next_due': self.get_next_due_date_str()
+                }
+
+    def __str__(self):
+        return json.dumps(self.json_data())
---
+++ packages/UW-RestClients-Libraries/UW-RestClients-Libraries-1.2.tar.gz/UW-RestClients-Libraries-1.2/uw_libraries/mylib.py
@@ -0,0 +1,72 @@
+"""
+This is the interface for interacting with the UW Libraries Web Service.
+"""
+
+from datetime import datetime
+from dateutil.parser import parse
+import json
+from urllib.parse import urlencode
+from uw_libraries.dao import MyLib_DAO
+from uw_libraries.models import MyLibAccount
+from restclients_core.exceptions import DataFailureException
+
+MyLibDao = MyLib_DAO()
+mylib_url_prefix = '/mylibinfo/v1/'
+
+
+def _get_mylib_resource(url):
+    response = MyLibDao.getURL(url, {})
+
+    response_data = str(response.data)
+    if response.status != 200:
+        raise DataFailureException(url, response.status, response_data)
+
+    # 'Bug' with lib API causing requests with no/invalid user to return a 200
+    if "User not found" in response_data:
+        raise DataFailureException(url, 404, response_data)
+
+    return response.data
+
+
+def _get_account(netid, timestamp=None, is_html=False):
+    params = [("id", netid)]
+    if timestamp is not None:
+        params.append(("timestamp", timestamp))
+    if is_html:
+        params.append(("style", "html"))
+
+    url = "{}?{}".format(mylib_url_prefix, urlencode(params))
+    return _get_mylib_resource(url)
+
+
+def get_account(netid, timestamp=None):
+    """
+    The Libraries object has a method for getting information
+    about a user's library account
+    """
+    response = _get_account(netid, timestamp=timestamp)
+    return _account_from_json(response)
+
+
+def get_account_html(netid, timestamp=None):
+    """
+    The Libraries object has a method for getting information
+    about a user's library account
+    """
+    return _get_account(netid, timestamp=timestamp, is_html=True)
+
+
+def _account_from_json(body):
+    try:
+        account_data = json.loads(body)
+    except Exception as ex:
+        raise Exception(
+            "Unable to parse mylibinfo: {}. {}".format(body, ex))
+    account = MyLibAccount()
+    account.fines = account_data["fines"]
+    account.holds_ready = account_data["holds_ready"]
+    account.items_loaned = account_data["items_loaned"]
+    account.next_due = account_data.get("next_due_utc")
+    if account.next_due is not None:
+        account.next_due = parse(account.next_due)
+    return account
