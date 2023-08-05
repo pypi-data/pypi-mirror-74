9398ac189093d49d33d46c1f257ee2d12d47c6df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:46 +0000

    Add package synch/0.6.6/synch-0.6.6-py3-none-any.whl

---
+++ packages/synch/synch-0.6.6-py3-none-any.whl/synch/enums.py
@@ -0,0 +1,17 @@
+from enum import Enum
+
+
+class BrokerType(str, Enum):
+    redis = "redis"
+    kafka = "kafka"
+
+
+class SourceDatabase(str, Enum):
+    mysql = "mysql"
+    postgres = "postgres"
+
+
+class ClickHouseEngine(str, Enum):
+    merge_tree = "MergeTree"
+    collapsing_merge_tree = "CollapsingMergeTree"
+    versioned_collapsing_merge_tree = "VersionedCollapsingMergeTree"
