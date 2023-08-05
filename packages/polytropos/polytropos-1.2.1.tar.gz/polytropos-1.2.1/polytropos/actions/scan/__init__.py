19c3f2fa814f75961a5668fbd8d3a73f6f08fe9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:08 +0000

    Add package polytropos/1.2.1/polytropos-1.2.1.tar.gz

---
+++ packages/polytropos/polytropos-1.2.1.tar.gz/polytropos-1.2.1/polytropos/actions/scan/__init__.py
@@ -0,0 +1,5 @@
+from polytropos.actions.scan._scan import Scan
+
+def register_scan_subclasses() -> None:
+    """Import all built-in subclasses so that they can be used in tasks"""
+    import polytropos.actions.scan.quantile
