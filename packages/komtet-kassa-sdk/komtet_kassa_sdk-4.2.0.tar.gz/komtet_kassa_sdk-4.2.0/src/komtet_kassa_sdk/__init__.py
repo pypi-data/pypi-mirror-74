6d76c0cdcfe9a5ae2a943c749d830b155bd0288d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:58 +0000

    Add package komtet-kassa-sdk/4.2.0/komtet_kassa_sdk-4.2.0.tar.gz

---
+++ packages/komtet-kassa-sdk/komtet_kassa_sdk-4.2.0.tar.gz/komtet_kassa_sdk-4.2.0/src/komtet_kassa_sdk/__init__.py
@@ -0,0 +1,26 @@
+from .check import (Agent, AgentType, CalculationMethod, CalculationSubject, Check,
+                    CorrectionCheck, CorrectionType, Intent, Nomenclature, PaymentMethod,
+                    TaxSystem, VatRate)
+from .client import Client, CouriersInfo, OrderInfo, Task, TaskInfo
+from .order import Order
+
+
+__all__ = [
+    'Agent',
+    'AgentType',
+    'CalculationMethod',
+    'CalculationSubject',
+    'Check',
+    'Client',
+    'CorrectionCheck',
+    'CorrectionType',
+    'CouriersInfo',
+    'Intent',
+    'Nomenclature',
+    'PaymentMethod',
+    'Order',
+    'OrderInfo',
+    'Task',
+    'TaskInfo',
+    'TaxSystem',
+    'VatRate']
