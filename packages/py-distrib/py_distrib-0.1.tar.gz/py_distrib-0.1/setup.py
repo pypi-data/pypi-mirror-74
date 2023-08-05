7d0f38d907309509c27a3ec5ddc0241114d8513b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:42 +0000

    Add package py-distrib/0.1/py_distrib-0.1.tar.gz

---
+++ packages/py-distrib/py_distrib-0.1.tar.gz/py_distrib-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='py_distrib',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['py_distrib'],
+      author = "Kavii Suri",
+      author_email="surikavii@gmail.com",
+      zip_safe=False)
