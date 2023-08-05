5439dbb6bb7df6cb90f183564bb9e825bc19163c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:11 +0000

    Add package calcibj-test/1.0.0/calcibj-test-1.0.0.tar.gz

---
+++ packages/calcibj-test/calcibj-test-1.0.0.tar.gz/calcibj-test-1.0.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        README = f.read()
+    return README
+
+
+setup(
+    name="calcibj-test",
+    version="1.0.0",
+    description="A Python calci",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    url="",
+    author="Abhinav Jain",
+    author_email="jain.abhinav14@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    include_package_data=True,
+   
+)
