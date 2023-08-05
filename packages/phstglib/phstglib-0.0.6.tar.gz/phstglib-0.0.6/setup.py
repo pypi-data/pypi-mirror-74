3e6d32405d95e493f5075bf077e846cc4de9441e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:05 +0000

    Add package phstglib/0.0.6/phstglib-0.0.6.tar.gz

---
+++ packages/phstglib/phstglib-0.0.6.tar.gz/phstglib-0.0.6/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'phstglib',
+    version = '0.0.6',
+    keywords='puhui dj strategy',
+    description = 'a library for puhui strategy',
+    license = 'MIT License',
+    url = 'https://git.xiaojukeji.com/intell_strategy/dj_stg',
+    author = 'Kuida Wu',
+    author_email = 'wukuida@didichuxing.com',
+    packages = find_packages(),
+    include_package_data = True,
+    platforms = 'any',
+    install_requires = [],
+)
