fa0213363b44e528593aa394da292ab3056e2438
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:41 +0000

    Add package django-magiclink/0.3.1/django_magiclink-0.3.1-py3-none-any.whl

---
+++ packages/django-magiclink/django_magiclink-0.3.1-py3-none-any.whl/magiclink/backends.py
@@ -0,0 +1,65 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.backends import BaseBackend
+from django.utils import timezone
+
+from . import settings
+from .models import MagicLink
+from .utils import get_client_ip
+
+User = get_user_model()
+
+
+class MagicLinkBackend(BaseBackend):
+
+    def authenticate(self, request, token=None, email=None):
+        if settings.EMAIL_VERIFY and not email:
+            return
+
+        if settings.EMAIL_IGNORE_CASE:
+            email = email.lower()
+
+        magiclink = MagicLink.objects.filter(token=token, disabled=False)
+        if email:
+            magiclink = magiclink.filter(email=email)
+        if not magiclink:
+            return
+
+        magiclink = magiclink.first()
+
+        if timezone.now() > magiclink.expiry:
+            magiclink.disable()
+            return
+
+        if settings.REQUIRE_SAME_IP:
+            if magiclink.ip_address != get_client_ip(request):
+                magiclink.disable()
+                return
+
+        if settings.REQUIRE_SAME_BROWSER:
+            if magiclink.cookie_value != request.COOKIES.get('magiclink'):
+                magiclink.disable()
+                return
+
+        if magiclink.times_used >= settings.TOKEN_USES:
+            magiclink.disable()
+            return
+
+        user = User.objects.get(email=magiclink.email)
+
+        if not settings.ALLOW_SUPERUSER_LOGIN and user.is_superuser:
+            magiclink.disable()
+            return
+
+        if not settings.ALLOW_STAFF_LOGIN and user.is_staff:
+            magiclink.disable()
+            return
+
+        magiclink.used()
+
+        return user
+
+    def get_user(self, user_id):
+        try:
+            return User.objects.get(pk=user_id)
+        except User.DoesNotExist:
+            return
---
+++ packages/django-magiclink/django_magiclink-0.3.1-py3-none-any.whl/magiclink/views.py
@@ -0,0 +1,139 @@
+import logging
+
+from django.conf import settings as django_settings
+from django.contrib.auth import authenticate, get_user_model, login, logout
+from django.http import Http404, HttpResponseRedirect
+from django.views.generic import TemplateView
+from django.views.generic.base import RedirectView
+
+from . import settings
+from .forms import (
+    LoginForm, SignupForm, SignupFormEmailOnly, SignupFormFull,
+    SignupFormWithUsername
+)
+from .helpers import create_magiclink, get_or_create_user
+from .models import MagicLink
+from .utils import get_url_path
+
+User = get_user_model()
+log = logging.getLogger(__name__)
+
+
+class Login(TemplateView):
+    template_name = settings.LOGIN_TEMPLATE_NAME
+
+    def get(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+        context['login_form'] = LoginForm(request.POST)
+        context['require_signup'] = settings.REQUIRE_SIGNUP
+        return self.render_to_response(context)
+
+    def post(self, request, *args, **kwargs):
+        logout(request)
+        context = self.get_context_data(**kwargs)
+        form = LoginForm(request.POST)
+        if not form.is_valid():
+            context['login_form'] = form
+            return self.render_to_response(context)
+
+        email = form.cleaned_data['email']
+        if not settings.REQUIRE_SIGNUP:
+            get_or_create_user(email)
+
+        next_url = request.GET.get('next', '')
+
+        magic_link = create_magiclink(email, request, redirect_url=next_url)
+        magic_link.send(request)
+
+        sent_url = get_url_path(settings.LOGIN_SENT_REDIRECT)
+        response = HttpResponseRedirect(sent_url)
+        if settings.REQUIRE_SAME_BROWSER:
+            response.cookies['magiclink'] = magic_link.cookie_value
+        return response
+
+
+class LoginSent(TemplateView):
+    template_name = settings.LOGIN_SENT_TEMPLATE_NAME
+
+
+class LoginVerify(RedirectView):
+
+    def get(self, request, *args, **kwargs):
+        token = request.GET.get('token')
+        if not token:
+            raise Http404()
+
+        email = request.GET.get('email')
+        user = authenticate(request, token=token, email=email)
+        if not user:
+            raise Http404()
+
+        login(request, user)
+
+        magic_link = MagicLink.objects.get(token=token)
+        return HttpResponseRedirect(magic_link.redirect_url)
+
+
+class Signup(TemplateView):
+    template_name = settings.SIGNUP_TEMPLATE_NAME
+
+    def get(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+        context['SignupForm'] = SignupForm()
+        context['SignupFormEmailOnly'] = SignupFormEmailOnly()
+        context['SignupFormWithUsername'] = SignupFormWithUsername()
+        context['SignupFormFull'] = SignupFormFull()
+        return self.render_to_response(context)
+
+    def post(self, request, *args, **kwargs):
+        logout(request)
+        context = self.get_context_data(**kwargs)
+        form_name = request.POST.get('form_name')
+        from_list = [
+            'SignupForm, SignupFormEmailOnly', 'SignupFormWithUsername',
+            'SignupFormFull',
+        ]
+        forms = __import__('magiclink.forms', fromlist=from_list)
+        Form = getattr(forms, form_name)
+
+        form = Form(request.POST)
+        if not form.is_valid():
+            context[form_name] = form
+            return self.render_to_response(context)
+
+        email = form.cleaned_data['email']
+        full_name = form.cleaned_data.get('name', '')
+        try:
+            first_name, last_name = full_name.split(' ', 1)
+        except ValueError:
+            first_name = full_name
+            last_name = ''
+
+        get_or_create_user(
+            email=email,
+            username=form.cleaned_data.get('username', ''),
+            first_name=first_name,
+            last_name=last_name
+        )
+        next_url = request.GET.get('next', settings.SIGNUP_LOGIN_REDIRECT)
+        magic_link = create_magiclink(email, request, redirect_url=next_url)
+        magic_link.send(request)
+
+        sent_url = get_url_path(settings.LOGIN_SENT_REDIRECT)
+        response = HttpResponseRedirect(sent_url)
+        if settings.REQUIRE_SAME_BROWSER:
+            response.cookies['magiclink'] = magic_link.cookie_value
+        return response
+
+
+class Logout(RedirectView):
+
+    def get(self, request, *args, **kwargs):
+        logout(self.request)
+
+        next_page = request.GET.get('next')
+        if next_page:
+            return HttpResponseRedirect(next_page)
+
+        redirect_url = get_url_path(django_settings.LOGOUT_REDIRECT_URL)
+        return HttpResponseRedirect(redirect_url)
