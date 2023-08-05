3cc0295908d5d052c9b635e53eb64a838d207c95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:37 +0000

    Add package Logria/0.9.2/Logria-0.9.2.tar.gz

---
+++ packages/Logria/Logria-0.9.2.tar.gz/Logria-0.9.2/logria/utilities/keystrokes.py
@@ -0,0 +1,13 @@
+"""
+Utilities to handle input validation
+"""
+
+
+def validator(key: int):
+    """
+    Intercept invalid strokes and convert them to valid ones
+    """
+    # Handle backspace on MacOS
+    if key == 127:
+        return 263  # Ctrl-h
+    return key
