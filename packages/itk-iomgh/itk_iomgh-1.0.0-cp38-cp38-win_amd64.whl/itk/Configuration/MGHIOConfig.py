47f8aaf9dd86ef54b18e7a06fd10f3caad41cce0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:58 +0000

    Add package itk-iomgh/1.0.0/itk_iomgh-1.0.0-cp38-cp38-win_amd64.whl

---
+++ packages/itk-iomgh/itk_iomgh-1.0.0-cp38-cp38-win_amd64.whl/itk/Configuration/MGHIOConfig.py
@@ -0,0 +1,6 @@
+depends = ('ITKPyBase', 'ITKIOImageBase', )
+templates = (
+  ('MGHImageIO', 'itk::MGHImageIO', 'itkMGHImageIO', True),
+  ('MGHImageIOFactory', 'itk::MGHImageIOFactory', 'itkMGHImageIOFactory', True),
+)
+snake_case_functions = ()
