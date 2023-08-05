cf80fb7d96278dd7ed070dde18bf280e00ed8e48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:01 +0000

    Add package uswapper/0.15/uswapper-0.15.tar.gz

---
+++ packages/uswapper/uswapper-0.15.tar.gz/uswapper-0.15/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='uswapper',
+      version='0.15',
+      description='Simple wrapper for uniswap graphql api',
+      license='MIT',
+      packages=['uswapper'],
+      zip_safe=False)
