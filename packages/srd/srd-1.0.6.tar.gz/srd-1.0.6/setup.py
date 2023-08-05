94dfbad0f9c4239373836ffdeef4748f4bd12db7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:14 +0000

    Add package srd/1.0.6/srd-1.0.6.tar.gz

---
+++ packages/srd/srd-1.0.6.tar.gz/srd-1.0.6/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="srd", # Replace with your own username
+    version="1.0.6",
+    author="Équipe CREEi",
+    author_email="pierre-carl.michaud@hec.ca",
+    description="Modele de simulation du revenu disponible Quebec",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://creei-models.github.io/srd/",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+   'pandas',
+   'numpy',
+   'xlrd',
+   'srpp'
+    ],
+    python_requires='>=3.6',
+)
