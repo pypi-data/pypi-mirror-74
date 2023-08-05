bd1286b00a56463f38529d339854a726d157001d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:29 +0000

    Add package Outage-Detector/1.1.2/Outage%20Detector-1.1.2.tar.gz

---
+++ packages/Outage-Detector/Outage%20Detector-1.1.2.tar.gz/Outage Detector-1.1.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+with open('requirements.txt') as req_files:
+    requirements = req_files.read().splitlines()
+
+with open("README.md", 'r') as readme:
+    long_description = readme.read()
+
+setup(
+    name='Outage Detector',
+    version='1.1.2',
+    description='A module helping you find out when internet and power outages happen.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license='MIT',
+    packages=['outagedetector'],
+    install_requires=requirements,
+    entry_points={
+        'console_scripts': [
+            'outage_detector = outagedetector.__main__:main'
+        ]
+    },
+    url='https://github.com/fabytm/Outage-Detector',
+    author='Butean Fabian',
+    author_email='buteanfabian@gmail.com'
+)
