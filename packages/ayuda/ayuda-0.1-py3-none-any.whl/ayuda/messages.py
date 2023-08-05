6ee6a1b060ada9b9db1c7741a4f25f5ac66207a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:20 +0000

    Add package ayuda/0.1/ayuda-0.1-py3-none-any.whl

---
+++ packages/ayuda/ayuda-0.1-py3-none-any.whl/ayuda/messages.py
@@ -0,0 +1,24 @@
+teddy = """
+ ( \---/ )
+ ) . . (
+________________________,--._(___Y___)_,--._______________________
+ `--'           `--'
+"""
+doing_backup = '{}\n   Realizando Backup...'.format(teddy)
+
+
+teddy_save = """
+ ( \---/ )
+ ) . . (
+ ( ._Y_, )
+ \ \_/ /
+________________________,--.__/_____\__,--._______________________
+ `--'           `--'
+"""
+backup_sucess = '{}\n Backup realizado!'.format(teddy_save)
+
+git_explication = u"""
+ cree un nuevo proyecto en gitlab o github
+
+ y copie su url ssh a continuación\nej: git@gitlab.com:<user>/ayuda.git
+"""
