e915703eabb581050ee5c4833d4fcdf8a5b6a85f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:39 +0000

    Add package hyp3lib/1.4.0/hyp3lib-1.4.0-py3-none-any.whl

---
+++ packages/hyp3lib/hyp3lib-1.4.0-py3-none-any.whl/hyp3lib/__init__.py
@@ -0,0 +1,33 @@
+"""Common library for HyP3 plugins"""
+
+from __future__ import print_function, absolute_import, division, unicode_literals
+
+# FIXME: Python 3.8+ this should be `from importlib.metadata...`
+from importlib_metadata import PackageNotFoundError, version
+
+from hyp3lib.exceptions import (
+    DemError,
+    ExecuteError,
+    GeometryError,
+    GranuleError,
+    OrbitDownloadError,
+)
+
+try:
+    __version__ = version(__name__)
+except PackageNotFoundError:
+    # package is not installed!
+    # Install in editable/develop mode via (from the top of this repo):
+    #    pip install -e .
+    # Or, to just get the version number use:
+    #    python setup.py --version
+    pass
+
+__all__ = [
+    '__version__',
+    'DemError',
+    'ExecuteError',
+    'GeometryError',
+    'GranuleError',
+    'OrbitDownloadError',
+]
