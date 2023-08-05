0d7906f79e410f9fa17e651d861a776d7387b49a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:19 +0000

    Add package monk-obj-test2/0.0.4/monk_obj_test2-0.0.4.tar.gz

---
+++ packages/monk-obj-test2/monk_obj_test2-0.0.4.tar.gz/monk_obj_test2-0.0.4/setup.py
@@ -0,0 +1,42 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="monk_obj_test2", # Replace with your own username
+    version="0.0.4",
+    author="Tessellate Imaging",
+    author_email="abhishek@tessellateimaging.com",
+    description="Monk Object Detection's 2_pytorch_finetune",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Tessellate-Imaging/Monk_Object_Detection",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Environment :: GPU :: NVIDIA CUDA :: 9.0",
+    ],
+    install_requires=[
+        'numpy',
+        'scipy',
+        'tqdm',
+        'pillow',
+        'opencv-python',
+        'cython',
+        'jupyter',
+        'notebook',
+        'pandas',
+        'matplotlib',
+        'xmltodict',
+        'torchvision==0.5.0',
+        'torch==1.4.0'
+    ],
+    dependency_links=[
+        'https://github.com/abhi-kumar/cocoapi.git#egg=pycocotools&subdirectory=PythonAPI'
+    ],
+    python_requires='>=3.6'
+)
+
