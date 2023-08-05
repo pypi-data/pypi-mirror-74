41c8bfbb2f64b11ba49142234c322cacaffb58af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:04 +0000

    Add package rp_pypi_tut_flit/0.1.0/rp_pypi_tut_flit-0.1.0-py2.py3-none-any.whl

---
+++ packages/rp_pypi_tut_flit/rp_pypi_tut_flit-0.1.0-py2.py3-none-any.whl/realpython_pypi_tut/__init__.py
@@ -0,0 +1,2 @@
+"""Docstring here."""
+__version__ = "0.1.0"
---
+++ packages/rp_pypi_tut_flit/rp_pypi_tut_flit-0.1.0-py2.py3-none-any.whl/realpython_pypi_tut/dogge.py
@@ -0,0 +1,12 @@
+import argparse
+
+"""Docstring here."""
+
+def simple_bark():
+    print('bark bark bark you nasty!!! bark I said!')
+
+def complex_bark():
+    # print(f'bark bark bark you nasty {sys.argv[1]}!!! bark I said!')
+
+    # if your function needs arguments, you need to figure it out (not gonna bother now)
+    # https://stackoverflow.com/questions/61565153/passing-arguments-to-an-entry-point-python-script-using-argparser
