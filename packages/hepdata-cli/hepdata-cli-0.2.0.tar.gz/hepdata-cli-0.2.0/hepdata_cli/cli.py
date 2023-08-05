23462561e2ae691c4bbb1c604eb74fef8893d6a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:59 +0000

    Add package hepdata-cli/0.2.0/hepdata-cli-0.2.0.tar.gz

---
+++ packages/hepdata-cli/hepdata-cli-0.2.0.tar.gz/hepdata-cli-0.2.0/hepdata_cli/cli.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+
+import click
+
+from .version import __version__
+from .api import Client
+
+
+@click.group()
+@click.version_option(None, "-v", "--version", message=__version__)
+@click.option('--verbose', is_flag=True, help='Print more output.')
+@click.pass_context
+def cli(ctx, verbose=False):
+    """CLI interface to API Client."""
+    ctx.obj = Client(verbose)
+
+
+@cli.command()
+@click.argument('query', required=True, type=str)
+@click.option('-kw', '--keyword', default=None, type=str, help='Search result filter.')
+@click.option('-i', '--ids', type=str, help='Returns a list instead of dictionary. For concatenation with download.')
+@click.pass_obj
+def find(client, query, keyword, ids):
+    """CLI interface to API client.find function."""
+    click.echo(client.find(query, keyword=keyword, ids=ids))
+
+
+@cli.command()
+@click.argument('id_list', nargs=-1, required=True, type=str)
+@click.option('-f', '--file-format', required=True, type=str, help='Specify file format (csv, root, yaml, yoda, or json) to be downloaded.')
+@click.option('-i', '--ids', required=True, type=str, help='Specify which ids (hepdata or inspire) are given.')
+@click.option('-t', '--table-name', default='', type=str, help='Specify table to be downloaded.')
+@click.option('-d', '--download-dir', default='./hepdata-downloads', type=str, help='Specify where to download the files.')
+@click.pass_obj
+def download(client, id_list, file_format, ids, table_name, download_dir):
+    """CLI interface to API client.download function."""
+    client.download(id_list, file_format=file_format, ids=ids, table_name=table_name, download_dir=download_dir)
+
+
+@cli.command()
+@click.argument('id_list', nargs=-1, required=True, type=str)
+@click.option('-i', '--ids', required=True, type=str, help='Specify which ids (hepdata or inspire) are given.')
+@click.pass_obj
+def fetch_names(client, id_list, ids):
+    """CLI interface to API client.fetch_names function."""
+    click.echo(client.fetch_names(id_list, ids=ids))
+
+
+@cli.command()
+@click.argument('path_to_file', required=True, type=str)
+@click.option('-e', '--email', required=True, type=str, help='User email.')
+@click.option('-r', '--recid', default=None, type=str, help='Record ID (if updating already existing record).')
+@click.option('-i', '--invitation-cookie', default=None, type=str, help='Invitation cookie (if NOT uploading to sandbox).')
+@click.option('-s', '--sandbox', default=True, type=bool, help='Whether to upload to the sandbox or not.')
+@click.option('--password', '-p', prompt=True, hide_input=True, help='User password (prompt if not specified).')
+@click.pass_obj
+def upload(client, path_to_file, email, recid, invitation_cookie, sandbox, password):
+    """CLI interface to API client.upload function."""
+    client.upload(path_to_file, email, recid, invitation_cookie, sandbox, password)
---
+++ packages/hepdata-cli/hepdata-cli-0.2.0.tar.gz/hepdata-cli-0.2.0/setup.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+
+"""CLI and API to allow users to search and download from HEPData"""
+
+import os
+
+from setuptools import setup, find_packages
+
+
+g = {}
+with open(os.path.join('hepdata_cli', 'version.py'), 'rt') as fp:
+    exec(fp.read(), g)
+    version = g['__version__']
+
+
+install_requirements = [
+    'click',
+    'requests',
+    'future',
+]
+
+test_requirements = [
+    'pytest',
+    'pytest-cov',
+    'requests_mock',
+]
+
+extras_require = {
+    'tests': test_requirements,
+}
+
+
+setup(
+    name='hepdata-cli',
+    version=version,
+    author='Giuseppe De Laurentis',
+    author_email='g.dl@hotmail.it',
+    description=__doc__,
+    keywords='hepdata cli api',
+    url='https://github.com/HEPData/hepdata-cli',
+    packages=find_packages(),
+    zip_safe=False,
+    platforms='any',
+    license='GPLv3',
+    install_requires=install_requirements,
+    tests_require=test_requirements,
+    extras_require=extras_require,
+    entry_points={
+        'console_scripts': [
+            'hepdata-cli = hepdata_cli.cli:cli',
+        ],
+    },
+)
