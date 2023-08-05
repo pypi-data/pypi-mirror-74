73a5c5ce23f2626be9ea68ffbe9e912be6208888
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:10 +0000

    Add package kapitan/0.28.1rc4/kapitan-0.28.1rc4-py3-none-any.whl

---
+++ packages/kapitan/kapitan-0.28.1rc4-py3-none-any.whl/kapitan/cached.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+# Copyright 2019 The Kapitan Authors
+# SPDX-FileCopyrightText: 2020 The Kapitan Authors <kapitan-admins@googlegroups.com>
+#
+# SPDX-License-Identifier: Apache-2.0
+
+"cached module"
+
+inv = {}
+inv_cache = {}
+gpg_obj = None
+gkms_obj = None
+awskms_obj = None
+dot_kapitan = {}
+ref_controller_obj = None
+revealer_obj = None
+args = {}  # args won't need resetting
+
+
+def reset_cache():
+    global inv, inv_cache, gpg_obj, gkms_obj, awskms_obj, dot_kapitan, ref_controller_obj, revealer_obj
+    inv = {}
+    inv_cache = {}
+    gpg_obj = None
+    gkms_obj = None
+    awskms_obj = None
+    dot_kapitan = {}
+    ref_controller_obj = None
+    revealer_obj = None
