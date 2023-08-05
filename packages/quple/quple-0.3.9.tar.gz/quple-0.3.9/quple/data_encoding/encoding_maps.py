2f335114c7f9b9ea17b4e459a900f89c3594a79b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:27 +0000

    Add package quple/0.3.9/quple-0.3.9.tar.gz

---
+++ packages/quple/quple-0.3.9.tar.gz/quple-0.3.9/quple/data_encoding/encoding_maps.py
@@ -0,0 +1,15 @@
+from functools import reduce
+import numpy as np
+
+def self_product(x: np.ndarray) -> float:
+    """
+    Define a function map from R^n to R.
+
+    Args:
+        x: data
+
+    Returns:
+        float: the mapped value
+    """
+    coeff = x[0] if len(x) == 1 else reduce(lambda m, n: m * n, np.pi - x)  
+    return coeff
