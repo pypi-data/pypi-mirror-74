c3bf4e5c7d8dfe1b01797203a207a05aed7463f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:45 +0000

    Add package septentrion/0.4.0/septentrion-0.4.0-py3-none-any.whl

---
+++ packages/septentrion/septentrion-0.4.0-py3-none-any.whl/septentrion/utils.py
@@ -0,0 +1,39 @@
+"""
+All functions in here should be easily unit testable
+"""
+
+from typing import Iterable, TypeVar
+
+from septentrion import exceptions, versions
+
+
+def is_version(vstring: str) -> bool:
+    """
+    Returns True if vstring is a valid version descriptor
+    >>> is_version("1.2")
+    True
+    >>> is_version("bananas")
+    False
+    """
+    try:
+        versions.Version.from_string(vstring)
+    except exceptions.InvalidVersion:
+        return False
+    return True
+
+
+T = TypeVar("T")
+
+
+def until(iterable: Iterable[T], value: T) -> Iterable[T]:
+    """
+    Returns the values from iterable up until element is found
+    >>> list(until(range(300), 3))
+    [0, 1, 2, 3]
+    """
+    for element in iterable:
+        yield element
+        if element == value:
+            break
+    else:
+        raise ValueError("{} not found".format(value))
