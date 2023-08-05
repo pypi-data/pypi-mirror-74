c24a14d9db5fc22ee71632205bcbcd089c0926f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:46 +0000

    Add package create-tao-masternode/1.2.5/create_tao_masternode-1.2.5-py3-none-any.whl

---
+++ packages/create-tao-masternode/create_tao_masternode-1.2.5-py3-none-any.whl/create_tao_masternode/templates/compose.py
@@ -0,0 +1,35 @@
+compose = """version: "3.4"
+
+services:
+
+  tao:
+    image: taoblockchain/node:stable
+    environment:
+      IDENTITY: $IDENTITY
+      PRIVATE_KEY: $PRIVATE_KEY
+      BOOTNODES: {{ bootnodes }}
+      NETWORK_ID: {{ network_id }}
+      VERBOSITY: {{ logging_level }}
+      NETSTATS_HOST: wss://stats.tao.network
+      NETSTATS_PORT: 443
+      WS_SECRET: {{ ws_secret }}
+    volumes:
+      - $DATA:/tao/data
+    ports:
+      - 30303:30303/tcp
+      - 30303:30303/udp
+      {%- if expose_rpc %}
+      - 8545:8545
+      {%- endif %}
+      {%- if expose_ws %}
+      - 8546:8546
+      {%- endif %}
+    restart: always
+{% if storage == "docker volume" %}
+volumes:
+  {{ data }}:
+    {%- if external == "True" %}
+    external: true
+    {%- endif %}
+{% endif %}
+"""
