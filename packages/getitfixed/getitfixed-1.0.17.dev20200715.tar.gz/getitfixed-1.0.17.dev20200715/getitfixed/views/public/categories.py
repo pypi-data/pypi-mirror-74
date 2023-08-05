0c8169eaca621d022be2d58ec7c3c81d0f9585b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:06 +0000

    Add package getitfixed/1.0.17.dev20200715/getitfixed-1.0.17.dev20200715.tar.gz

---
+++ packages/getitfixed/getitfixed-1.0.17.dev20200715.tar.gz/getitfixed-1.0.17.dev20200715/getitfixed/views/public/categories.py
@@ -0,0 +1,23 @@
+from pyramid.view import view_config
+
+from getitfixed.models.getitfixed import Category
+
+
+@view_config(route_name="getitfixed_categories", renderer="json", request_method="GET")
+def categories(request):
+    return [
+        {
+            "id": c.id,
+            "label": c.label(request.locale_name),
+            "icon": c.icon_url(request),
+            "types": [
+                {
+                    "id": t.id,
+                    "label": t.label(request.locale_name),
+                    "wms_layer": t.wms_layer,
+                }
+                for t in c.types
+            ],
+        }
+        for c in request.dbsession.query(Category)
+    ]
