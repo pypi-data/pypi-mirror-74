7cdcc03dc7b6550a9ab094b32d1709af100200d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:50 +0000

    Add package durations-nlp/1.0.0/durations_nlp-1.0.0.tar.gz

---
+++ packages/durations-nlp/durations_nlp-1.0.0.tar.gz/durations_nlp-1.0.0/durations_nlp/duration.py
@@ -0,0 +1,101 @@
+from collections import namedtuple
+
+from durations_nlp.parser import extract_tokens
+from durations_nlp.scales import Scale
+from durations_nlp.exceptions import ScaleFormatError
+from durations_nlp.constants import *
+
+
+DurationRepresentation = namedtuple(
+    'DurationRepresentation',
+    ['value', 'scale']
+)
+
+
+class Duration(object):
+    """Duration representation class
+
+    Duration objects parses an input duration representation
+    string, and provides a set of methods to retrieve and
+    convert it's value to other units.
+
+    Example:
+        >>> d = Duration('1m')
+        >>> d.to_seconds()
+        60.0
+        >>> d.to_hours()
+        0.02
+        >>> d = Duration('2d 3 hours')
+        >>> d.to_minutes()
+        3060.0
+    """
+
+    def __init__(self, representation, *args, **kwargs):
+        self.representation = representation
+        self.parsed_durations = self.parse(self.representation)
+        self.seconds = self._compute_seconds_value()
+
+    def __str__(self):
+        return '<Duration {0}>'.format(self.representation)
+
+    def __repr__(self):
+        return self.__str__()
+
+    def _compute_seconds_value(self):
+        seconds = 0
+
+        for duration in self.parsed_durations:
+            seconds += duration.value * duration.scale.conversion_unit
+
+        return seconds
+
+    def parse(self, representation):
+        """Parses a duration string representation
+
+
+        :param  representation: duration as a string, example: '1d' (day),
+                                '34minutes' (minutes), '485s' (seconds)...
+        :type   representation: string
+
+        :returns: the parsed duration representation
+        :rtype: DurationRepresentation
+        """
+        elements = extract_tokens(representation)
+
+        try:
+            scales = [DurationRepresentation(float(p[0]), Scale(p[1]))
+                      for p in elements]
+        except ValueError:
+            raise ScaleFormatError("Malformed duration representation: {0}".format(representation))  # noqa: E501
+
+        return scales
+
+    def to_centuries(self):
+        return round(self.seconds / float(SCALE_CENTURY_CONVERSION_UNIT), 2)
+
+    def to_decades(self):
+        return round(self.seconds / float(SCALE_DECADE_CONVERSION_UNIT), 2)
+
+    def to_years(self):
+        return round(self.seconds / float(SCALE_YEAR_CONVERSION_UNIT), 2)
+
+    def to_months(self):
+        return round(self.seconds / float(SCALE_MONTH_CONVERSION_UNIT), 2)
+
+    def to_weeks(self):
+        return round(self.seconds / float(SCALE_WEEK_CONVERSION_UNIT), 2)
+
+    def to_days(self):
+        return round(self.seconds / float(SCALE_DAY_CONVERSION_UNIT), 2)
+
+    def to_hours(self):
+        return round(self.seconds / float(SCALE_HOUR_CONVERSION_UNIT), 2)
+
+    def to_minutes(self):
+        return round(self.seconds / float(SCALE_MINUTE_CONVERSION_UNIT), 2)
+
+    def to_seconds(self):
+        return round(self.seconds / float(SCALE_SECOND_CONVERSION_UNIT), 2)
+
+    def to_milliseconds(self):
+        return round(self.seconds / float(SCALE_MILLISECOND_CONVERSION_UNIT), 2)
---
+++ packages/durations-nlp/durations_nlp-1.0.0.tar.gz/durations_nlp-1.0.0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['durations_nlp']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'durations-nlp',
+    'version': '1.0.0',
+    'description': 'A python durations parsing library.',
+    'long_description': '# durations_nlp\n\n[![CircleCI](https://circleci.com/gh/timwedde/durations_nlp.svg?style=svg)](https://circleci.com/gh/timwedde/durations_nlp)\n[![codecov](https://codecov.io/gh/timwedde/durations_nlp/branch/master/graph/badge.svg)](https://codecov.io/gh/timwedde/durations_nlp)\n[![Downloads](https://pepy.tech/badge/durations-nlp)](https://pepy.tech/project/durations-nlp)\n\nA python durations parsing library, providing a straight-forward API to parse duration string representations such as `1d`, `1 day 2 hours` or `2 days 3h 26m 52s` and convert them to numeric values.\n\n## What and Why\nIt\'s easier and more straight forward to read a duration expressed in natural language (at least for a human), as an expression rather than an amount. When writing configuration files for example:\n\n```yaml\ninterval: 3 hours\n```\n\nis easier to understand for a human than\n\n```yaml\ninterval: 10800  # seconds\n```\n\n## Installation\n\n`durations_nlp` can be installed via pip:\n```bash\n$ pip install durations_nlp\n```\n\n## Usage\nTo parse a duration string representation, just instantiate a Duration object and let it work for you. A Duration representation is composed of as many ``<value><scale>`` pairs as you need to express it:\n* A value is an integer amount.\n* A scale is a duration unit in it\'s short or long form (both singular and plural).\n* Duration pairs can be separated with sep characters and expressions such as `,` or `and`\n\n### Example Input\n\n* `1d`\n* `2 days`\n* `2 days and 4 hours`\n* `4M, 22d and 6hours`\n\n### Scales Reference\n\n* Century: `c`, `century`, `centuries`\n* Decade: `D`, `decade`, `decades`\n* Year: `y`, `year`, `Year`\n* Month: `M`, `month`, `months`\n* Week: `w`, `week`, `weeks`\n* Day: `d`, `day`, `days`\n* Hour: `h`, `hour`, `hours`\n* Minute:`m`, `minute`, `minutes`\n* Second: `s`, `second`, `seconds`\n* Millisecond: `ms`, `millisecond`, `milliseconds`\n\n### Usage Example\n\n```python\nfrom durations_nlp import Duration\n\none_hour = "1hour"\n\none_hour_duration = Duration(one_hour)\none_hour_duration.to_seconds()\n# >>> 3600.0\none_hour_duration.to_minutes()\n# >>> 60.0\n\n# You can even compose durations in their short\n# and long variations\ntwo_days_three_hours = "2 days, 3h"\ntwo_days_three_hours_duration = Duration(two_days_three_hours)\ntwo_days_three_hours_duration.to_seconds()\n# >>> 183600.0\ntwo_days_three_hours_duration.to_hours()\n# >>> 51.0\n```\n',
+    'author': 'Tim Wedde',
+    'author_email': 'timwedde@icloud.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/timwedde/durations_nlp',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
