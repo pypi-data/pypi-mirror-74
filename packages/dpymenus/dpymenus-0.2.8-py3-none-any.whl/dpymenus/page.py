05c4766769045543e4b61ff7004785871d42ed13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:45 +0000

    Add package dpymenus/0.2.8/dpymenus-0.2.8-py3-none-any.whl

---
+++ packages/dpymenus/dpymenus-0.2.8-py3-none-any.whl/dpymenus/page.py
@@ -0,0 +1,35 @@
+from typing import Callable, List, Optional
+
+from discord import Embed
+
+
+class Page(Embed):
+    """Represents a single page inside a menu. Subclasses :py:class:`~discord.Embed`.
+
+    Attributes:
+        on_next: Reference to a Callable. Should be called manually.
+        on_fail: Reference to a Callable. Called when user input fails.
+        on_cancel: Reference to a Callable. Called when the cancel method is run.
+        on_timeout: reference to a Callable. Called when a menu instance times out.
+        buttons: A list of reaction Emoji objects or unicode strings.
+    """
+
+    def __init__(self, buttons: Optional[List] = None, on_next: Optional[Callable] = None, on_fail: Optional[Callable] = None,
+                 on_cancel: Optional[Callable] = None, on_timeout: Optional[Callable] = None, embed: Optional[Embed] = None, **kwargs):
+        self.buttons = buttons
+        self.on_next = on_next
+        self.on_fail = on_fail
+        self.on_cancel = on_cancel
+        self.on_timeout = on_timeout
+
+        if embed is None:
+            super().__init__(**kwargs)
+        else:
+            super().__init__(**embed.to_dict())
+
+    def __str__(self):
+        return f'<Page {self.title}>'
+
+    def __repr__(self):
+        return f'Page(title={self.title} on_next={self.on_next}, on_fail={self.on_fail}, on_cancel={self.on_cancel}, on_timeout={self.on_timeout}, ' \
+               f'buttons={self.buttons}, {super().__repr__()})'
