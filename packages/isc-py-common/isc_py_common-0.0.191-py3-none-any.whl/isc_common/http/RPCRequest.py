e5d5350f6bef27dc26114d9dc7e71aa0ff445c74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:34 +0000

    Add package isc-py-common/0.0.191/isc_py_common-0.0.191-py3-none-any.whl

---
+++ packages/isc-py-common/isc_py_common-0.0.191-py3-none-any.whl/isc_common/http/RPCRequest.py
@@ -0,0 +1,8 @@
+class RPCRequest:
+    def __init__(self, _dict):
+        if _dict:
+            for key, value in _dict.items():
+                if isinstance(value, (list, tuple)):
+                    setattr(self, key, [RPCRequest(x) if isinstance(x, dict) else x for x in value])
+                else:
+                    setattr(self, key, RPCRequest(value) if isinstance(value, dict) else value)
