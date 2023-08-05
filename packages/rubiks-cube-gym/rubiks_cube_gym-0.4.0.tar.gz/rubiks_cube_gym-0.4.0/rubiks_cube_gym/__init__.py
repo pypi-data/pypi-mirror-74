f87321b83c7d93ed86895d967a6c5cf5f28bb5a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:54 +0000

    Add package rubiks-cube-gym/0.4.0/rubiks_cube_gym-0.4.0.tar.gz

---
+++ packages/rubiks-cube-gym/rubiks_cube_gym-0.4.0.tar.gz/rubiks_cube_gym-0.4.0/rubiks_cube_gym/__init__.py
@@ -0,0 +1,43 @@
+from gym.envs.registration import register
+
+register(
+    id='rubiks-cube-222-v0',
+    entry_point='rubiks_cube_gym.envs:RubiksCube222Env',
+    max_episode_steps=250,
+)
+
+register(
+    id='rubiks-cube-222-lbl-v0',
+    entry_point='rubiks_cube_gym.envs:RubiksCube222EnvLBL',
+    max_episode_steps=250,
+)
+
+register(
+    id='rubiks-cube-222-ortega-v0',
+    entry_point='rubiks_cube_gym.envs:RubiksCube222EnvOrtega',
+    max_episode_steps=250,
+)
+
+register(
+    id='pyraminx-wo-tips-v0',
+    entry_point='rubiks_cube_gym.envs:PyraminxWoTipsEnv',
+    max_episode_steps=250,
+)
+
+register(
+    id='pyraminx-wo-tips-lbl-v1',
+    entry_point='rubiks_cube_gym.envs:PyraminxWoTipsEnvLBL',
+    max_episode_steps=250,
+)
+
+register(
+    id='skewb-v0',
+    entry_point='rubiks_cube_gym.envs:SkewbEnv',
+    max_episode_steps=250,
+)
+
+register(
+    id='skewb-sarah-v1',
+    entry_point='rubiks_cube_gym.envs:SkewbEnvSarah',
+    max_episode_steps=250,
+)
---
+++ packages/rubiks-cube-gym/rubiks_cube_gym-0.4.0.tar.gz/rubiks_cube_gym-0.4.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(name='rubiks_cube_gym',
+      version='0.4.0',
+      url="https://github.com/DoubleGremlin181/RubiksCubeGym/",
+      description="OpenAI Gym environments for various twisty puzzles",
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      keywords='environment, agent, rl, rubiks, cube, openai-gym, gym',
+      author="Kavish Hukmani",
+      author_email="khukmani@gmail.com",
+      license="MIT",
+      install_requires=['gym', 'numpy', 'opencv-python', 'wget'],
+      packages=find_packages()
+)
