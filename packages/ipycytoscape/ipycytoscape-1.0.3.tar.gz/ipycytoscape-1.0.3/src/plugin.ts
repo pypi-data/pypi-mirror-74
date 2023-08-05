2f31f35c874328b880644a52d3109541f4778900
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:06 +0000

    Add package ipycytoscape/1.0.3/ipycytoscape-1.0.3.tar.gz

---
+++ packages/ipycytoscape/ipycytoscape-1.0.3.tar.gz/ipycytoscape-1.0.3/src/plugin.ts
@@ -0,0 +1,40 @@
+// Copyright (c) Mariana Meireles
+// Distributed under the terms of the Modified BSD License.
+
+import { Application, IPlugin } from '@phosphor/application';
+
+import { Widget } from '@phosphor/widgets';
+
+import { IJupyterWidgetRegistry } from '@jupyter-widgets/base';
+
+import { MODULE_NAME, MODULE_VERSION } from './version';
+
+const EXTENSION_ID = 'jupyter-cytoscape:plugin';
+
+/**
+ * The example plugin.
+ */
+const examplePlugin: IPlugin<Application<Widget>, void> = {
+  id: EXTENSION_ID,
+  requires: [IJupyterWidgetRegistry as any],
+  activate: activateWidgetExtension,
+  autoStart: true,
+};
+
+export default examplePlugin;
+
+/**
+ * Activate the widget extension.
+ */
+function activateWidgetExtension(
+  app: Application<Widget>,
+  registry: IJupyterWidgetRegistry
+): void {
+  registry.registerWidget({
+    name: MODULE_NAME,
+    version: MODULE_VERSION,
+
+    exports: async () =>
+      await import(/* webpackChunkName: "jupyter-cytoscape" */ './widget'),
+  });
+}
