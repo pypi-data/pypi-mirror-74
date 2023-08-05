1fff03ffcecdc61123f85c749b0ed8efeb943206
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:48 +0000

    Add package FlexGet/3.1.67/FlexGet-3.1.67-py2.py3-none-any.whl

---
+++ packages/FlexGet/FlexGet-3.1.67-py2.py3-none-any.whl/flexget/plugins/filter/only_new.py
@@ -0,0 +1,32 @@
+from loguru import logger
+
+from flexget import plugin
+from flexget.event import event
+
+logger = logger.bind(name='only_new')
+
+
+class FilterOnlyNew:
+    """Causes input plugins to only emit entries that haven't been seen on previous runs."""
+
+    schema = {'type': 'boolean'}
+
+    def on_task_start(self, task, config):
+        """Make sure the remember_rejected plugin is available"""
+        # Raises an error if plugin isn't available
+        plugin.get('remember_rejected', self)
+
+    def on_task_learn(self, task, config):
+        """Reject all entries so remember_rejected will reject them next time"""
+        if not config or not task.entries:
+            return
+        logger.verbose(
+            'Rejecting entries after the task has run so they are not processed next time.'
+        )
+        for entry in task.entries:
+            entry.reject('Already processed entry', remember=True)
+
+
+@event('plugin.register')
+def register_plugin():
+    plugin.register(FilterOnlyNew, 'only_new', api_ver=2)
