10e03c6d7f134de6c7ec29c1b50eac81c32dc6d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:43 +0000

    Add package Wasserstein/0.0.0/Wasserstein-0.0.0.tar.gz

---
+++ packages/Wasserstein/Wasserstein-0.0.0.tar.gz/Wasserstein-0.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Wasserstein',
+    description='Python package wrapping C++ code for computing Wasserstein distances',
+    author='Patrick T. Komiske III',
+    author_email='pkomiske@mit.edu',
+    version='0.0.0',
+    url='https://github.com/pkomiske/Wasserstein',
+    packages=find_packages()
+)
