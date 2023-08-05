9adb39405c22daab4ed69de10238f61b8e098226
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:45 +0000

    Add package dvg-debug-functions/2.1.0/dvg-debug-functions-2.1.0.tar.gz

---
+++ packages/dvg-debug-functions/dvg-debug-functions-2.1.0.tar.gz/dvg-debug-functions-2.1.0/setup.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+from __future__ import absolute_import
+from __future__ import print_function
+
+import io
+import re
+from glob import glob
+from os.path import basename
+from os.path import dirname
+from os.path import join
+from os.path import splitext
+
+from setuptools import find_packages
+from setuptools import setup
+
+
+def read(*names, **kwargs):
+    with io.open(
+        join(dirname(__file__), *names), encoding=kwargs.get("encoding", "utf8")
+    ) as fh:
+        return fh.read()
+
+
+setup(
+    name="dvg-debug-functions",
+    version="2.1.0",
+    license="MIT",
+    description="Functions to neatly print debug information to the terminal output, well-suited for multithreaded programs.",
+    long_description="%s\n%s"
+    % (
+        re.compile("^.. start-badges.*^.. end-badges", re.M | re.S).sub(
+            "", read("README.rst")
+        ),
+        re.sub(":[a-z]+:`~?(.*?)`", r"``\1``", read("CHANGELOG.rst")),
+    ),
+    long_description_content_type="text/x-rst",
+    author="Dennis van Gils",
+    author_email="vangils.dennis@gmail.com",
+    url="https://github.com/Dennis-van-Gils/python-dvg-debug-functions",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    py_modules=[splitext(basename(path))[0] for path in glob("src/*.py")],
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        # complete classifier list: http://pypi.python.org/pypi?%3Aaction=list_classifiers
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Utilities",
+    ],
+    project_urls={
+        "Issue Tracker": "https://github.com/Dennis-van-Gils/python-dvg-debug-functions/issues",
+    },
+    keywords=[],
+    python_requires=">=3.6",
+    install_requires=[],
+    extras_require={},
+)
