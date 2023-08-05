ad042be5f182baca54f241afe8fbf3fdfe19a522
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:56 +0000

    Add package affability/1.0.2/affability-1.0.2.tar.gz

---
+++ packages/affability/affability-1.0.2.tar.gz/affability-1.0.2/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as readme_file:
+    long_description = readme_file.read()
+
+setuptools.setup(
+    name="affability",
+    version="1.0.2",
+    author="Aaron de los Santos",
+    author_email="hansaaron_d@yahoo.com",
+    description="A simple package for interfacing with Dialogflow",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/aarondls/affability",
+    keywords= ['Dialogflow', 'DialogFlow'],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires = [
+        'dialogflow'
+    ],
+)
