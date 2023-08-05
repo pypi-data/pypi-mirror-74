0d743dfb94d9ee5ce7678d8bfc36fe71b9f28237
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:26 +0000

    Add package tdvisu/1.1.5/tdvisu-1.1.5-py3-none-any.whl

---
+++ packages/tdvisu/tdvisu-1.1.5-py3-none-any.whl/tdvisu/version.py
@@ -0,0 +1,10 @@
+"""
+Release versions have the form "M.N.P".
+Dev versions have the form "M.N.P-devN".
+"""
+
+# Base version.
+__version__ = '1.1.5'
+
+# Year-Month-Day of last version change
+__date__ = '2020-07-17'
