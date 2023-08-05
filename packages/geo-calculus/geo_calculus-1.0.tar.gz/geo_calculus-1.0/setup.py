43c7c07df0246f5ecf7204f6a147d856999345e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:40 +0000

    Add package geo-calculus/1.0/geo_calculus-1.0.tar.gz

---
+++ packages/geo-calculus/geo_calculus-1.0.tar.gz/geo_calculus-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup 
+
+setup(name='geo_calculus',
+      version = '1.0',
+      description = 'Calculus Package with function like newton method',
+      package = ['geo_calculus'],
+      author = 'Geoffrey',
+      author_email = 'geoffrey.geofe@gmail.com',
+      zip_safe = False)
