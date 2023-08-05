aecfa3a0926a81c86a3d01ee7554750548b03010
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:09 +0000

    Add package dexcomapi/0.4.1/dexcomapi-0.4.1.tar.gz

---
+++ packages/dexcomapi/dexcomapi-0.4.1.tar.gz/dexcomapi-0.4.1/README.md
@@ -0,0 +1,8 @@
+# dexcomapi
+
+
+This simple package allows you to setup a authenticated channel to download blood glucose numbers from the Dexcom api. The intent is to use this with HomeAssistant.io.
+
+ You can use
+[Github-flavored Markdown](https://guides.github.com/features/mastering-markdown/)
+to write your content.
---
+++ packages/dexcomapi/dexcomapi-0.4.1.tar.gz/dexcomapi-0.4.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="dexcomapi",
+    version="0.4.1",
+    author="Jim Walsh",
+    author_email="jim@walshfamily.email",
+    description="Uses Dexcom APIs to download blood glucose data",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jpw1012/dexcomapi",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
