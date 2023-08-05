c37356fc4b9aa85e4578bd23d1683359f14f256e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:05 +0000

    Add package modelboost/0.0.8/modelboost-0.0.8.tar.gz

---
+++ packages/modelboost/modelboost-0.0.8.tar.gz/modelboost-0.0.8/setup.py
@@ -0,0 +1,32 @@
+from distutils.core import setup
+
+setup(
+  name = 'modelboost',   
+  packages = ['modelboost'],  
+  version = '0.0.8',      
+  license='MIT',      
+  description = 'A package that makes the process of finding the model and optimizing the model automated and simple',
+  author = 'Roni Gold',                  
+  author_email = 'ronigoldsmid@gmail.com', 
+  url = 'https://github.com/ronigold',
+  download_url = 'https://github.com/ronigold/modelboost/archive/0.0.8.tar.gz',   
+  keywords = ['machine learning', 'deep learning', 'model', 'optimizing'],  
+  install_requires=[            
+          'sklearn',
+		  'IPython',
+		  'tqdm',
+		  'catboost',
+		  'xgboost',
+		  'lightgbm'
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers', 
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License', 
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
