0e8e82fd20521b4854d5a4883a23b2f03746e188
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:54 +0000

    Add package covsirphy/2.5.2/covsirphy-2.5.2-py3-none-any.whl

---
+++ packages/covsirphy/covsirphy-2.5.2-py3-none-any.whl/covsirphy/__init__.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from collections import Counter
+import sys
+try:
+    import better_exceptions
+    better_exceptions_installed = True
+except ImportError:
+    better_exceptions_installed = False
+from covsirphy.__version__ import __version__
+from covsirphy.analysis import ODESimulator, ChangeFinder
+from covsirphy.analysis import PhaseSeries, Scenario, ExampleData
+from covsirphy.cleaning import Term, CleaningBase, DataLoader
+from covsirphy.cleaning import JHUData
+from covsirphy.cleaning import CountryData, PopulationData, OxCGRTData
+from covsirphy.ode import ModelBase, SIR, SIRD, SIRF, SIRFV, SEWIRF
+from covsirphy.phase import Estimator, Trend
+from covsirphy.util import line_plot, jpn_map, StopWatch, error
+# Deprecated
+from covsirphy.cleaning import Population, Word
+
+
+def get_version():
+    """
+    Return the version number, like CovsirPhy v0.0.0
+    """
+    return f"CovsirPhy v{__version__}"
+
+
+__all__ = [
+    "ODESimulator", "ChangeFinder",
+    "PhaseSeries", "Scenario", "ExampleData",
+    "Term", "CleaningBase", "DataLoader",
+    "JHUData", "CountryData", "PopulationData", "OxCGRTData",
+    "ModelBase", "SIR", "SIRD", "SIRF", "SIRFV", "SEWIRF",
+    "Estimator", "Trend",
+    "line_plot", "jpn_map", "StopWatch", "error",
+    # Deprecated
+    "Population", "Word",
+]
+
+# Check duplication
+dup_list = [k for (k, v) in Counter(__all__).items() if v > 1]
+if dup_list:
+    dup_str = ', '.join(dup_list)
+    raise Exception(f"Duplication was found in modules. {dup_str}")
+
+# Show exceptions in better format if used from command line
+if not hasattr(sys, "ps1") or not better_exceptions_installed:
+    better_exceptions.MAX_LENGTH = None
+    better_exceptions.hook()
---
+++ packages/covsirphy/covsirphy-2.5.2-py3-none-any.whl/covsirphy/cleaning/cbase.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from dask import dataframe as dd
+from covsirphy.cleaning.term import Term
+
+
+class CleaningBase(Term):
+    """
+    Basic class for data cleaning.
+
+    Args:
+        filename (str): CSV filename of the dataset
+    """
+
+    def __init__(self, filename):
+        self._raw = dd.read_csv(
+            filename, dtype={"Province/State": "object"}
+        ).compute()
+        self._cleaned_df = self.cleaning()
+        self._citation = str()
+
+    @property
+    def raw(self):
+        """
+        Return the raw data.
+
+        Returns:
+            (pandas.DataFrame): raw data
+        """
+        return self._raw
+
+    def cleaned(self):
+        """
+        Return the cleaned dataset.
+
+        Notes:
+            Cleaning method is defined by self.cleaning() method.
+
+        Returns:
+            (pandas.DataFrame): cleaned data
+        """
+        return self._cleaned_df
+
+    def cleaning(self):
+        """
+        Perform data cleaning of the raw data.
+
+        Notes:
+            Cleaning method is defined by self.cleaning() method.
+
+        Returns:
+            (pandas.DataFrame): cleaned data
+        """
+        df = self._raw.copy()
+        return df
+
+    def total(self):
+        """
+        Return a dataframe to show chronological change of number and rates.
+
+        Returns:
+            (pandas.DataFrame): group-by Date, sum of the values
+
+                Index:
+                    (Date) (pd.TimeStamp): Observation date
+                Columns:
+                    - Confirmed (int): the number of confirmed cases
+                    - Infected (int): the number of currently infected cases
+                    - Fatal (int): the number of fatal cases
+                    - Recovered (int): the number of recovered cases
+                    - Fatal per Confirmed (int)
+                    - Recovered per Confirmed (int)
+                    - Fatal per (Fatal or Recovered) (int)
+        """
+        df = self._cleaned_df.groupby("Date").sum()
+        cols = ["Infected", "Fatal", "Recovered"]
+        r_cols = self.RATE_COLUMNS[:]
+        total_series = df[cols].sum(axis=1)
+        df[r_cols[0]] = df["Fatal"] / total_series
+        df[r_cols[1]] = df["Recovered"] / total_series
+        df[r_cols[2]] = df["Fatal"] / (df["Fatal"] + df["Recovered"])
+        return df
+
+    @property
+    def citation(self):
+        """
+        str: citation/description of the dataset
+        """
+        return self._citation
+
+    @citation.setter
+    def citation(self, description):
+        if not isinstance(description, str):
+            raise TypeError("@description must be a string.")
+        self._citation = description
