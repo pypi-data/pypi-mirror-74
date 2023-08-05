f1ca324510612cae3897bf417ebb326e187f7c32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:03 +0000

    Add package vintools/0.0.0.2/vintools-0.0.0.2.tar.gz

---
+++ packages/vintools/vintools-0.0.0.2.tar.gz/vintools-0.0.0.2/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setuptools.setup(
+    name="vintools", # Replace with your own username
+    version="0.0.0.2",
+    author="Michael Edward Vinyard",
+    author_email="vinyard@g.harvard.edu",
+    description="general personal utils",
+    long_description="This package contains various functions used often by MEV.",
+    long_description_content_type="text/markdown",
+    url="https://github.com/mvinyard/mev-utils/vintools",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
