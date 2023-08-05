02fe2b69fb1c2e833e7ce6527e6f6a787cad3948
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:37 +0000

    Add package USCrimes/0.1/USCrimes-0.1.zip

---
+++ packages/USCrimes/USCrimes-0.1.zip/USCrimes-0.1/README.txt
@@ -0,0 +1,4 @@
+USCrimes
+This library gives quick and easy access to atrocities committed by the US Government and compiled by @dessalines (https://github.com/dessalines/essays/blob/master/us_atrocities.md).  
+To use it, simply download the USCrimes module (containing `__init__.py` and `core.py`) and add it to your project.  
+Currently the module allows you to fetch a random crime with `USCrimes.randomCrime()` or one containing a specific keyword with `USCrimes.searchCrime(keyword)`.
---
+++ packages/USCrimes/USCrimes-0.1.zip/USCrimes-0.1/setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+
+setup(
+  name = 'USCrimes',
+  packages = ['USCrimes'],
+  version = '0.1',
+  license='MIT',
+  description = 'Quick access to atrocities involving the US Government',
+  author = 'Marco Antonio Ribeiro de Toledo',
+  author_email = 'marcoantonioribeirodetoledo@gmail.com',
+  url = 'https://github.com/Ocramoi/USCrimes',
+  download_url = 'https://github.com/Ocramoi/USCrimes/archive/v0.1.tar.gz',
+  keywords = ['United States', 'crimes', 'crimes','us'],
+  install_requires=[],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.2',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.8'
+  ],
+)
