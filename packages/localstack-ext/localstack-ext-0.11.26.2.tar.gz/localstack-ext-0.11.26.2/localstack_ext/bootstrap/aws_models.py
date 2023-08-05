a6f7eee2d9abb4293f48c3e84117fec03cbfa9f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:44 +0000

    Add package localstack-ext/0.11.26.2/localstack-ext-0.11.26.2.tar.gz

---
+++ packages/localstack-ext/localstack-ext-0.11.26.2.tar.gz/localstack-ext-0.11.26.2/localstack_ext/bootstrap/aws_models.py
@@ -0,0 +1,23 @@
+from localstack.utils.aws import aws_models
+EyDWx=super
+EyDWX=None
+EyDWM=id
+class LambdaLayer(aws_models.LambdaFunction):
+ def __init__(self,arn):
+  EyDWx(LambdaLayer,self).__init__(arn)
+  self.cwd=EyDWX
+  self.runtime=''
+  self.handler=''
+  self.envvars={}
+  self.versions={}
+class RDSDatabase(aws_models.Component):
+ def __init__(self,EyDWM,env=EyDWX):
+  EyDWx(RDSDatabase,self).__init__(EyDWM,env=env)
+ def name(self):
+  return self.EyDWM.split(':')[-1]
+class RDSCluster(aws_models.Component):
+ def __init__(self,EyDWM,env=EyDWX):
+  EyDWx(RDSCluster,self).__init__(EyDWM,env=env)
+ def name(self):
+  return self.EyDWM.split(':')[-1]
+# Created by pyminifier (https://github.com/liftoff/pyminifier)
