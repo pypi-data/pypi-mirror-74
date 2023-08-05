0e346eb6e18e367912fd13c764c0344a078c1b77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:25 +0000

    Add package knb-distributions/0.1.1/knb_distributions-0.1.1.tar.gz

---
+++ packages/knb-distributions/knb_distributions-0.1.1.tar.gz/knb_distributions-0.1.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='knb_distributions',
+      version='0.1.1',
+      description='Gaussian distributions',
+      packages=['knb_distributions'],
+      zip_safe=False)
