c7804e969d241fd33551dd6a55f5b77d0bcf9b16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:47 +0000

    Add package Chamaeleo/1.18/Chamaeleo-1.18-py3-none-any.whl

---
+++ packages/Chamaeleo/Chamaeleo-1.18-py3-none-any.whl/Chamaeleo/examples/demo_case_sc_with_hm.py
@@ -0,0 +1,55 @@
+"""
+Name: Symmetrical testing for YYC
+
+Coder: HaoLing ZHANG (BGI-Research)[V1]
+
+Current Version: 1
+
+Function(s): The demo case of Simple Codec (Church et al Code) with Hamming Code.
+"""
+
+import os
+import Chamaeleo
+import Chamaeleo.codec_factory as codec_factory
+import Chamaeleo.utils.dir_checker as checker
+from Chamaeleo.methods import sc
+from Chamaeleo.methods.verifies import hm
+from Chamaeleo.utils import data_handle
+
+root_path = os.path.dirname(Chamaeleo.__file__)
+read_file_path = os.path.join(root_path, "data", "pictures", "Mona Lisa.jpg")
+current_path = os.path.dirname(os.path.realpath(__file__))
+generated_file_path = os.path.join(current_path, "generated_files")
+checker.check_dir_exists(generated_file_path)
+write_file_path = os.path.join(generated_file_path, "target.jpg")
+dna_path = os.path.join(generated_file_path, "target.dna")
+model_path = os.path.join(generated_file_path, "sc+hm.pkl")
+
+
+if __name__ == "__main__":
+    tool = sc.SC()
+    verify = hm.Hm()
+    codec_factory.encode(
+        method=tool,
+        input_path=read_file_path,
+        output_path=dna_path,
+        model_path=model_path,
+        verify=verify,
+        need_index=True,
+        need_log=True
+    )
+    # del tool, verify
+    codec_factory.decode(
+        model_path=model_path,
+        input_path=dna_path,
+        output_path=write_file_path,
+        has_index=True,
+        need_log=True
+    )
+
+    # compare two file
+    matrix_1, _ = data_handle.read_binary_from_all(read_file_path, 120, False)
+    matrix_2, _ = data_handle.read_binary_from_all(write_file_path, 120, False)
+    print("source digital file == target digital file: " + str(matrix_1 == matrix_2))
+    if matrix_1 != matrix_2:
+        raise RuntimeError("Simple Code with Hamming Code has error!")
