bc04a15e6fd43ce24006054e30946c5d00c3d6ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:44 +0000

    Add package filtragemACrm/0.0.1/filtragemACrm-0.0.1.tar.gz

---
+++ packages/filtragemACrm/filtragemACrm-0.0.1.tar.gz/filtragemACrm-0.0.1/setup.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+  name='filtragemACrm',
+  version='0.0.1',
+  url='https://github.com/alancaio/filtragemACrm',
+  license='MIT License',
+  author='Alan Caio Rodrigues Marques',
+  author_email='alancaiorm@gmail.com',
+  keywords='filtragem imagens duplicadas',
+  description=u'Métodos de filtragem de imagem repetida e com blur',
+  packages=['filtragemACrm'],
+  install_requires=[],
+)
