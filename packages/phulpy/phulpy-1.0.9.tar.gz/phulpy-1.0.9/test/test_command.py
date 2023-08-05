c5fba10d7df63d35f7b16df5a7b4fb6a40a45393
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:46 +0000

    Add package phulpy/1.0.9/phulpy-1.0.9.tar.gz

---
+++ packages/phulpy/phulpy-1.0.9.tar.gz/phulpy-1.0.9/test/test_command.py
@@ -0,0 +1,18 @@
+from phulpy.command import Command
+
+
+class TestCommand(object):
+    def test_constructor(self):
+        Command('ls', None, None, None, False, None, None, None)
+
+    def test_kill(self):
+        c = Command('ls', None, None, None, True, lambda e: e, lambda e: e, lambda e, x, y: e)
+        c.start()
+        c.kill()
+
+    def test_start(self):
+        c = Command('ls', None, None, None, True, lambda e: e, lambda e: e, lambda e, x, y: e)
+        c.start()
+
+        c = Command('ping -d', None, None, None, True, lambda e: e, lambda e: e, lambda e, x, y: e)
+        c.start()
