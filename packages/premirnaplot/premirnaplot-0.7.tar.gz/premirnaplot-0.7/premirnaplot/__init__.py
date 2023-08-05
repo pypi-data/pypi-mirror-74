fee452f18a911820a4481176ac8947e99eae47b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:44 +0000

    Add package premirnaplot/0.7/premirnaplot-0.7.tar.gz

---
+++ packages/premirnaplot/premirnaplot-0.7.tar.gz/premirnaplot-0.7/premirnaplot/__init__.py
@@ -0,0 +1,5 @@
+#__all__ = ['extra', 'imgparser', 'precursor']
+
+from .precursor import *
+from .imgparser import *
+from .extra import *
---
+++ packages/premirnaplot/premirnaplot-0.7.tar.gz/premirnaplot-0.7/setup.py
@@ -0,0 +1,32 @@
+
+from setuptools import setup, find_packages
+from premirnaplot.extra import desctxt
+
+setup(
+	name='premirnaplot',
+	version=0.7,
+	description='pre-miRNA secondary structure prediction image generator',
+	author='Igor Paim',
+	author_email='igorpaim8@gmail.com',
+	url='https://github.com/igrorp/pre-miRNA-plot',
+	license='GPL',
+	classifiers=[
+		'Intended Audience :: Science/Research',
+		'Topic :: Scientific/Engineering :: Bio-Informatics',
+		'Programming Language :: Python :: 3',
+	],
+	keywords='pre-miRNA',
+	packages=find_packages(),
+	install_requires=[
+		'matplotlib',
+		'scikit-learn',
+		'numpy',
+		'svglib',
+		'svgwrite',
+		'pandas',
+	],
+	scripts=[
+		'premirnaplot/premirnaplot',
+	],
+	long_description=desctxt
+)
