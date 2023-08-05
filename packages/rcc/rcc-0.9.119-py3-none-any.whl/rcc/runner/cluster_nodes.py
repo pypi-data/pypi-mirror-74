99638f775a8ddd77016222cd72f8cc0077a1e099
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:53 +0000

    Add package rcc/0.9.119/rcc-0.9.119-py3-none-any.whl

---
+++ packages/rcc/rcc-0.9.119-py3-none-any.whl/rcc/runner/cluster_nodes.py
@@ -0,0 +1,31 @@
+'''Display cluster nodes
+
+Copyright (c) 2020 Machine Zone, Inc. All rights reserved.
+'''
+import asyncio
+import logging
+
+import click
+
+from rcc.cluster.info import printRedisClusterInfoCoro
+
+
+@click.command()
+@click.option(
+    '--redis-url', '-u', envvar='RCC_REDIS_URL', default='redis://localhost:30001'
+)
+@click.option('--password', '-a')
+@click.option('--user')
+@click.option('--role')
+@click.option('--hide_slots', is_flag=True)
+def cluster_nodes(redis_url, password, user, role, hide_slots):
+    '''Display cluster nodes'''
+
+    displaySlots = not hide_slots
+
+    try:
+        asyncio.get_event_loop().run_until_complete(
+            printRedisClusterInfoCoro(redis_url, password, user, role, displaySlots)
+        )
+    except Exception as e:
+        logging.error(f'cluster_nodes error: {e}')
