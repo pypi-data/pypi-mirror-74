38e77336fcfe5e2525ff43ff09b5165d99b857db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:47 +0000

    Add package ATPLibrary/0.1.0.dev15/ATPLibrary-0.1.0.dev15-py3-none-any.whl

---
+++ packages/ATPLibrary/ATPLibrary-0.1.0.dev15-py3-none-any.whl/ATPLibrary/version.py
@@ -0,0 +1,14 @@
+from os import environ
+from pkg_resources import get_distribution, DistributionNotFound
+
+__VERSION__ = environ.get('GitVersion.MajorMinorPatch', '0.1.0')
+
+try:
+    __VERSION__ = get_distribution(__name__).version
+except DistributionNotFound:
+    # package is not installed
+    try:
+        with open('../LOCAL-VERSION') as f:
+            __VERSION__ = __VERSION__ + f.readline().strip()
+    except IOError:
+        pass
