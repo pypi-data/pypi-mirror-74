49abc7556acc5610228c2e0c3a4d0b62c0535474
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:14:43 +0000

    Add package rna/0.5.0/rna-0.5.0-py2.py3-none-any.whl

---
+++ packages/rna/rna-0.5.0-py2.py3-none-any.whl/rna/__init__.py
@@ -0,0 +1,11 @@
+"""Top-level package for rna."""
+
+__author__ = """Daniel Böckenhoff"""
+__email__ = "dboe@ipp.mpg.de"
+__version__ = "0.5.0"
+
+from . import plotting
+from . import log  # NOQA
+from . import path  # NOQA
+
+plotting.use("matplotlib")
