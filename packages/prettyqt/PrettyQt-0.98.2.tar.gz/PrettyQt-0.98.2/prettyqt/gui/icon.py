1224e6f3345ac52b9864bb784a78e8608f4c5188
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:34 +0000

    Add package prettyqt/0.98.2/PrettyQt-0.98.2.tar.gz

---
+++ packages/prettyqt/PrettyQt-0.98.2.tar.gz/PrettyQt-0.98.2/prettyqt/gui/icon.py
@@ -0,0 +1,87 @@
+# -*- coding: utf-8 -*-
+"""
+"""
+
+import pathlib
+from typing import Union
+
+
+import qtawesome as qta
+from qtpy import QtCore, QtGui
+
+from prettyqt import core, gui
+
+
+IconType = Union[QtGui.QIcon, str, pathlib.Path, None]
+
+
+def get_icon(icon: IconType, color: str = "black"):
+    if icon is None:
+        icon = gui.Icon()
+    elif isinstance(icon, str):
+        if icon.startswith("mdi."):
+            icon = gui.Icon(qta.icon(icon, color=color))
+        else:
+            icon = gui.Icon(icon)
+    elif isinstance(icon, pathlib.Path):
+        icon = gui.Icon(icon)
+    return icon
+
+
+def set_defaults(*args, **kwargs):
+    qta.set_defaults(*args, **kwargs)
+
+
+class Icon(QtGui.QIcon):
+    def __init__(self, icon=None):
+        if isinstance(icon, pathlib.Path):
+            icon = str(icon)
+        super().__init__(icon)
+
+    # def __reduce__(self):
+    #     return type(self), (), self.__getstate__()
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}()"
+
+    def __bool__(self):
+        return not bool(self.isNull())
+
+    def __getstate__(self):
+        ba = QtCore.QByteArray()
+        stream = QtCore.QDataStream(ba, QtCore.QIODevice.WriteOnly)
+        pixmap = self.pixmap(QtCore.QSize(256, 256))
+        stream << pixmap
+        return ba
+
+    def __setstate__(self, ba):
+        stream = QtCore.QDataStream(ba, QtCore.QIODevice.ReadOnly)
+        px = QtGui.QPixmap()
+        stream >> px
+        super().__init__(px)
+
+    @classmethod
+    def by_name(cls, name: str) -> "Icon":
+        return cls(qta.icon(name))
+
+    @classmethod
+    def for_color(cls, color_str: str) -> "Icon":
+        color = gui.Color.from_text(color_str)
+        if color.isValid():
+            bitmap = gui.Pixmap(16, 16)
+            bitmap.fill(color)
+            icon = gui.Icon(bitmap)
+        else:
+            icon = cls(qta.icon("mdi.card-outline"))
+        return icon
+
+    def get_pixmap(self, size: int) -> QtGui.QPixmap:
+        size = core.Size(size, size)
+        return self.pixmap(size)
+
+
+if __name__ == "__main__":
+    from prettyqt import widgets
+
+    app = widgets.app()
+    icon = Icon.for_color("green")
