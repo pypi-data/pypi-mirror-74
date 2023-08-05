53e487d08e17215adceda8607ae8505c8bfc5bb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:54 +0000

    Add package hexagen/1.0/hexagen-1.0.tar.gz

---
+++ packages/hexagen/hexagen-1.0.tar.gz/hexagen-1.0/README.md
@@ -0,0 +1,3 @@
+# Hexagen
+
+This generates 6 random hexadecimal digits and prints them to standard output.
---
+++ packages/hexagen/hexagen-1.0.tar.gz/hexagen-1.0/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="hexagen",
+    version="1.0",
+    author="Clemens-Alexander Brust",
+    author_email="ikosa.de@gmail.com",
+    description="Generates random hexadecimal digits and prints them to standard output.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/cabrust/hexagen",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: BSD License",
+    ],
+    python_requires=">=3.6",
+)
