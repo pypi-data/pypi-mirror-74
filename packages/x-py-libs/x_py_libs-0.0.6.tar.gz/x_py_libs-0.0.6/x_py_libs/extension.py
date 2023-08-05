da2f2061e44d970832bcdd47392a8013ecea185e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:12 +0000

    Add package x-py-libs/0.0.6/x_py_libs-0.0.6.tar.gz

---
+++ packages/x-py-libs/x_py_libs-0.0.6.tar.gz/x_py_libs-0.0.6/x_py_libs/extension.py
@@ -0,0 +1,4 @@
+# -*- coding=utf-8 -*-
+
+from extensions.src.base_file_upload_handler import *
+from extensions.src.base_handler_tornado import *
---
+++ packages/x-py-libs/x_py_libs-0.0.6.tar.gz/x_py_libs-0.0.6/x_py_libs/service.py
@@ -0,0 +1,3 @@
+# -*- coding=utf-8 -*-
+
+from services.src.model_transfer_service import *
