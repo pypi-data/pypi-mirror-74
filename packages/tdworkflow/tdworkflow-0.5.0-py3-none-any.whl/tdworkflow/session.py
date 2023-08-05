65f2d9cfb09f3bf53502dc4fc7e0a110f57d2467
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:46 +0000

    Add package tdworkflow/0.5.0/tdworkflow-0.5.0-py3-none-any.whl

---
+++ packages/tdworkflow/tdworkflow-0.5.0-py3-none-any.whl/tdworkflow/session.py
@@ -0,0 +1,36 @@
+import dataclasses
+from datetime import datetime
+
+from .attempt import Attempt
+from .project import Project
+from .util import parse_iso8601
+from .workflow import Workflow
+
+
+@dataclasses.dataclass
+class Session:
+    id: int
+    project: Project
+    workflow: Workflow
+    sessionUuid: str
+    sessionTime: datetime = None
+    lastAttempt: Attempt = None
+
+    def __post_init__(self):
+        self.id = int(self.id)
+        self.project = Project(**self.project)
+        self.workflow = Workflow(**self.workflow)
+        self.sessionTime = parse_iso8601(self.sessionTime)
+        self.lastAttempt = Attempt(**self.lastAttempt)
+
+    @property
+    def session_uuid(self):
+        return self.sessionUuid
+
+    @property
+    def session_time(self):
+        return self.session_time
+
+    @property
+    def last_attempt(self):
+        return self.lastAttempt
