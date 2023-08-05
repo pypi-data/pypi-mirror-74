4bcc7c4e755008281531490c099d459f86503124
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:42:32 +0000

    Add package filecrypt/1.1.0/filecrypt-1.1.0.tar.gz

---
+++ packages/filecrypt/filecrypt-1.1.0.tar.gz/filecrypt-1.1.0/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="filecrypt",
+    version="1.1.0",
+    author="Tami",
+    description="Wrapper for FileCrypt.cc",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/TA40/FileCryptcc",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
