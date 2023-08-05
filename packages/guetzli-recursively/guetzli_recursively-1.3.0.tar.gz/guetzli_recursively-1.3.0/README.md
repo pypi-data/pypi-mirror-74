700cf731ba963999ffdaf144a648beb49ea7d14c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:47 +0000

    Add package guetzli-recursively/1.3.0/guetzli_recursively-1.3.0.tar.gz

---
+++ packages/guetzli-recursively/guetzli_recursively-1.3.0.tar.gz/guetzli_recursively-1.3.0/README.md
@@ -0,0 +1,56 @@
+
+Guetzli is a Google program to optimize JPEG images. Unfortunately, it only works one file at a time. With this script in Python you can do it recursively a whole folder.
+
+## Image optimized with **guetzli-recursively**
+
+![after and before](demo.jpg)
+
+Image Author: [Senjuti Kundu](https://unsplash.com/@senjuti?utm_medium=referral&amp;utm_campaign=photographer-credit&amp;utm_content=creditBadge)
+
+# Install 
+
+Guetzli must be installed on your system. Follow the official instructions.
+[Guetzli](https://github.com/google/guetzli)
+
+and 2.7.10 or Python 3.
+
+After
+
+```bash
+pip3 install guetzli-recursively
+```
+
+# Use
+
+```bash
+guetzli_recursively [folder]
+```
+
+## Example
+
+```bash
+guetzli_recursively img/
+```
+
+out
+
+```bash
+img/tasks.jpg
+Save 6%
+img/portfolio/idecrea/space.jpg
+It is not necessary to optimize
+img/portfolio/home.jpg
+Save 3%
+```
+
+# Quality
+
+Must be greater than or equal to 84.
+
+```bash
+guetzli_recursively --quality 85 img/
+```
+
+# GUIs
+
+- [Mac OS](https://github.com/tanrax/guetzli-recursively-gui)
---
+++ packages/guetzli-recursively/guetzli_recursively-1.3.0.tar.gz/guetzli_recursively-1.3.0/guetzli_recursively.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Libraries
+import click
+from os import path, walk, remove, rename
+from imghdr import what
+from subprocess import call
+
+# Variables
+TEMP_FILE = 'temp.jpg'
+TYPES = ('jpeg',)
+LIMIT_QUALITY = 84
+
+
+@click.command()
+@click.option(
+    '--quality',
+    default=100,
+    help='Quality >= {quality} [default 100].'.format(
+        quality=LIMIT_QUALITY
+    )
+)
+@click.option(
+    '--memlimit',
+    default=28000,
+    help='Memory limit in MB. Guetzli will fail if unable to stay under the limit. Default is 28000 MB'
+)
+@click.argument('folder', type=click.Path(exists=True))
+def run(quality, memlimit, folder):
+    for dirpath, dirnames, files in walk(folder):
+        for name in files:
+            url = path.join(dirpath, name)
+            # Check type
+            if what(url) in TYPES or quality >= LIMIT_QUALITY:
+                # Get urls
+                click.echo(url)
+                url_out = path.join(folder, TEMP_FILE)
+                # Remove temp image
+                try:
+                    remove(url_out)
+                except:
+                    pass
+                # Execute guetzli
+                args = ['guetzli', '--quality',
+                        str(quality), '--memlimit', str(memlimit), url, url_out]
+                print(' '.join(args))
+                call(args)
+                # Print your have saved
+                size_source = path.getsize(url)
+                try:
+                    size_out = path.getsize(url_out)
+                except:
+                    size_out = size_source
+                size_acurate = 100 * size_out / size_source
+                # Check if it is cost effective to replace it
+                if size_acurate < 100:
+                    # Remove source
+                    try:
+                        remove(url)
+                    except:
+                        pass
+                    # Move temp to source
+                    rename(url_out, url)
+                    click.echo(
+                        'Save ' + str(round(100 - size_acurate, 2)) + '%')
+                else:
+                    click.echo('It is not necessary to optimize')
+
+
+if __name__ == '__main__':
+    run()
