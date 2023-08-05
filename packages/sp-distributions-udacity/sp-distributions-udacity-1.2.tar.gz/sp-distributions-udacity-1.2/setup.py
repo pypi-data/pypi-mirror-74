cca2477b933695dd31c53f3131e8d21623f62f3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:34 +0000

    Add package sp-distributions-udacity/1.2/sp-distributions-udacity-1.2.tar.gz

---
+++ packages/sp-distributions-udacity/sp-distributions-udacity-1.2.tar.gz/sp-distributions-udacity-1.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='sp-distributions-udacity',
+      version='1.2',
+      description='Gaussian and Binomial distributions',
+      packages=['sp-distributions-udacity'],
+      author = 'Shreena Parekh',
+      author_email = 'shreenaparekh1010@gmail.com',
+      zip_safe=False)
