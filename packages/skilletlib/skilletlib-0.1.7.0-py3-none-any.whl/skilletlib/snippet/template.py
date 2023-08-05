59c49dc029917d4b20fe025d7d6e55aff2da66c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:11 +0000

    Add package skilletlib/0.1.7.0/skilletlib-0.1.7.0-py3-none-any.whl

---
+++ packages/skilletlib/skilletlib-0.1.7.0-py3-none-any.whl/skilletlib/snippet/template.py
@@ -0,0 +1,25 @@
+from typing import Tuple
+
+from .base import Snippet
+
+
+class TemplateSnippet(Snippet):
+    """
+    TemplateSnippet implements a basic template object snippet
+    """
+    required_metadata = {'name', 'file'}
+
+    output_type = 'text'
+
+    template_metadata = {'element'}
+
+    def __init__(self, template_str, metadata):
+        self.template_str = template_str
+        self.rendered_template = ""
+        super().__init__(metadata)
+
+    def execute(self, context: dict) -> Tuple[str, str]:
+        return self.render(self.template_str, context), 'success'
+
+    def template(self, context) -> str:
+        return self.execute(context)[0]
