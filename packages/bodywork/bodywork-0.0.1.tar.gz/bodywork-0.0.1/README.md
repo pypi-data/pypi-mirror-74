436191ad4d1b840c6f5ebe50735e05e0e273254d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:13:45 +0000

    Add package bodywork/0.0.1/bodywork-0.0.1.tar.gz

---
+++ packages/bodywork/bodywork-0.0.1.tar.gz/bodywork-0.0.1/README.md
@@ -0,0 +1,3 @@
+# bodywork
+
+A framework for deploying statistical (machine) learning models on Kubernetes.
---
+++ packages/bodywork/bodywork-0.0.1.tar.gz/bodywork-0.0.1/src/bodywork/cli.py
@@ -0,0 +1,9 @@
+"""
+Command Line Interface (CLI)
+"""
+
+
+def cli() -> bool:
+    print('Hello Production')
+    return True
+
