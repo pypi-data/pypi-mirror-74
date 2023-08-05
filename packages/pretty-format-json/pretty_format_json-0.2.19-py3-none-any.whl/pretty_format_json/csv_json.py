71d80ec94362119a877e68273f86eb40e5ce499c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:11 +0000

    Add package pretty-format-json/0.2.19/pretty_format_json-0.2.19-py3-none-any.whl

---
+++ packages/pretty-format-json/pretty_format_json-0.2.19-py3-none-any.whl/pretty_format_json/csv_json.py
@@ -0,0 +1,164 @@
+#!/usr/local/bin/python3
+# coding: utf-8
+"""
+Author       : weaming
+Created Time : 2018-07-19 03:24:06
+
+CSV and JSON converter
+"""
+import sys
+import argparse
+import json
+import io
+from . import *
+
+try:
+    from StringIO import StringIO
+except ImportError:
+    from io import StringIO
+
+from .format import pretty_print
+from data_process.io_csv import process_row_generator, read_csv
+
+is_py2 = sys.version_info[0] == 2
+
+
+def filter_keys(data, fields, raise_missing=True, raise_extra=True):
+    def check_keys(x):
+        missing = list(set(fields) - set(x.keys()))
+        if missing and raise_missing:
+            raise ValueError(f"missing keys '{missing}' for {x}")
+        for k, v in x.items():
+            if k in fields:
+                yield k, v
+            elif raise_extra:
+                raise ValueError(f"extra key '{k}' for {x}")
+
+    for x in data:
+        xx = dict(check_keys(x))
+        if xx:
+            yield xx
+
+
+def _merge_value(v):
+    if isinstance(v, list):
+        return ', '.join(str(x) for x in v)
+    if not isinstance(v, (int, float)):
+        return str(v)
+    return v
+
+
+def to_csv(
+    data,
+    fields=None,
+    ignore_missing=False,
+    ignore_extra=False,
+    loose=False,
+    sort_keys=False,
+):
+    if is_py2:
+        fuck = lambda x: x.encode("utf8")
+    else:
+        fuck = lambda x: x
+    data = [
+        dict(
+            (sorted if sort_keys else lambda x: x)(
+                {fuck(k): _merge_value(fuck(v)) for k, v in row.items()}.items()
+            )
+        )
+        for row in data
+    ]
+    f = StringIO()
+    if not fields:
+        if loose:
+            fields = set()
+            for r in data:
+                for k in r:
+                    fields.add(k)
+            fields = sorted(fields)
+        else:
+            fields = sorted(data[0]) if is_py2 else list(data[0])
+    process_row_generator(
+        fields,
+        filter_keys(
+            data,
+            fields,
+            raise_missing=(not ignore_missing) and (not loose),
+            raise_extra=not ignore_extra,
+        ),
+        f,
+        keep_open=True,
+    )
+    return f.getvalue()
+
+
+def from_csv(text):
+    return read_csv(StringIO(text))
+
+
+def get_target_type_from_text(text):
+    text = text.strip()
+    if text and text[0] in "{[":
+        return "csv"
+    return "json"
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-t", "--type", help="type of target output, csv and json are allowed"
+    )
+    parser.add_argument(
+        "-v", "--version", default=False, action="store_true", help="print version"
+    )
+    parser.add_argument("-f", "--fields", nargs='*', help="specific the fields")
+    parser.add_argument("--loose", action='store_true', help="output all fields")
+    parser.add_argument(
+        "-m",
+        "--ignore-missing",
+        default=False,
+        action="store_true",
+        help="ignore the missing fields",
+    )
+    parser.add_argument(
+        "-e",
+        "--ignore-extra",
+        default=False,
+        action="store_true",
+        help="ignore the extra fields",
+    )
+    parser.add_argument(
+        "-s", "--sort-keys", default=False, action="store_true", help="sort keys",
+    )
+
+    args = parser.parse_args()
+    if args.version:
+        print(version)
+        sys.exit(0)
+
+    text = sys.stdin.read()
+
+    t = args.type or get_target_type_from_text(text)
+    if t == "csv":
+        try:
+            data = json.loads(text)
+        except ValueError as e:
+            print(e)
+            sys.exit(1)
+
+        y = to_csv(
+            data,
+            args.fields,
+            args.ignore_missing,
+            args.ignore_extra,
+            args.loose,
+            sort_keys=args.sort_keys,
+        )
+        print(y)
+
+    elif t == "json":
+        pretty_print(from_csv(text))
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/pretty-format-json/pretty_format_json-0.2.19-py3-none-any.whl/pretty_format_json/format.py
@@ -0,0 +1,145 @@
+#!/usr/bin/env python3
+# coding: utf-8
+"""
+Author       : weaming
+Created Time : 2018-06-25 17:07:30
+
+Pretty print json contains python style coments, string literal.
+"""
+import os
+import sys
+import json
+import ast
+import re
+import datetime
+from collections import OrderedDict
+
+try:
+    from commands import getstatusoutput as get_status_output
+except ImportError:
+    from subprocess import getstatusoutput as get_status_output
+
+DEBUG = os.getenv("DEBUG")
+JSON_SORT_KEYS = os.getenv("JSON_SORT_KEYS")
+DATE_FORMAT = os.getenv("DATE_FORMAT")
+JSON_INDENT = int(os.getenv("JSON_INDENT", 2))
+
+
+def get_text(fp):
+    if fp:
+        return open(fp).read()
+    else:
+        return sys.stdin.read()
+
+
+def parse_text(text):
+    text = "\n".join(x.strip() for x in text.split("\n"))
+    text = (
+        text.replace("true", "True").replace("false", "False").replace(
+            "null", "None"
+        ).replace("nil", "None")
+    )
+    if DEBUG:
+        print(text)
+    if not text:
+        return None
+
+    py_obj = ast.literal_eval(text)
+    return py_obj
+
+
+def eval_in_nodejs(text):
+    if "'" in text:
+        eval_text = 'node -e "console.log(JSON.stringify({}))"'.format(text)
+    else:
+        eval_text = "node -e 'console.log(JSON.stringify({}))'".format(text)
+
+    if DEBUG:
+        print("NODE: shell command")
+        print(eval_text)
+
+    try:
+        code, output = get_status_output(eval_text)
+    except:
+        if DEBUG:
+            raise
+        else:
+            return text
+
+    if DEBUG:
+        print(code)
+        print(output)
+    if code == 0:
+        return output
+
+    return text
+
+
+def pretty_print(data):
+    def json_serializer(obj):
+        if isinstance(obj, (datetime.datetime, datetime.date)):
+            if isinstance(obj, datetime.date):
+                if DATE_FORMAT:
+                    return obj.strftime(DATE_FORMAT)
+            if isinstance(obj, datetime.datetime):
+                if DATE_FORMAT:
+                    return obj.strftime(DATE_FORMAT)
+            return obj.isoformat()
+        raise TypeError("Type %s not serializable" % type(obj))
+
+    if JSON_SORT_KEYS:
+        data = to_ordered_dict(data)
+    print(
+        json.dumps(
+            data, indent=JSON_INDENT, ensure_ascii=False, default=json_serializer
+        )
+    )
+
+
+def asign(a, b):
+    for k in sorted(a.keys()):
+        v = a[k]
+        if isinstance(v, dict):
+            v = to_ordered_dict(v)
+        elif isinstance(v, list):
+            v = list(map(to_ordered_dict, v))
+
+        b[k] = v
+
+
+def to_ordered_dict(data):
+    # parse list
+    if isinstance(data, (tuple, list)):
+        return list(map(to_ordered_dict, data))
+
+    # do not process other types
+    if not isinstance(data, dict):
+        return data
+
+    # ordered already
+    if isinstance(data, OrderedDict):
+        if not JSON_SORT_KEYS:
+            return data
+
+    # sort dict
+    rv = OrderedDict()
+    asign(data, rv)
+    return rv
+
+
+def main():
+    fp = None
+    if len(sys.argv) > 1:
+        fp = sys.argv[1]
+
+    text = get_text(fp)
+    if re.search(r'\)\s*,', text):
+        text = text.replace('false', 'False').replace('true', 'True')
+        text = json.dumps(ast.literal_eval(text), ensure_ascii=False)
+    else:
+        text = eval_in_nodejs(text)
+    pretty_print(parse_text(text))
+
+
+if __name__ == "__main__":
+    main()
