f5b4133a4f733aaff8c203946bda702d3b0bd458
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:52 +0000

    Add package biggan/1.0.0a0/biggan-1.0.0a0-py3-none-any.whl

---
+++ packages/biggan/biggan-1.0.0a0-py3-none-any.whl/biggan/__init__.py
@@ -0,0 +1,5 @@
+from .data import serialize_to_tfrecords
+from .data import get_tfrecord_dataset
+from .model import build_model
+from .model import train_model
+from . import config
---
+++ packages/biggan/biggan-1.0.0a0-py3-none-any.whl/biggan/config/__init__.py
@@ -0,0 +1,46 @@
+import os
+import yaml
+import argparse
+
+
+def _get_config(config_name: str):
+    config_dir = os.path.abspath(os.path.dirname(__file__))
+    config_file = os.path.join(config_dir, f"{config_name}.yaml")
+    with open(config_file, "r") as f:
+        return yaml.load(f, Loader=yaml.Loader)
+
+
+def get_config_parser(config_name: str, parent: argparse.ArgumentParser = None):
+    config = _get_config(config_name)
+    parents = [parent] if parent else []
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+        parents=parents,
+        add_help=(parent is None),
+        conflict_handler="resolve",
+    )
+    for argument, argument_config in config.items():
+        if parent is not None:
+            matches = [a for a in parent._actions if argument == a.option_strings[0][2:]]
+            if len(matches) == 1:
+                match = matches[0]
+                for attr in ("help", "choices", "action"):
+                    if attr not in argument_config and hasattr(match, attr):
+                        argument_config[attr] = getattr(match, attr)
+        parser.add_argument(
+            "--" + argument,
+            required="default" not in argument_config,
+            **argument_config,
+        )
+    parser.defaults = argparse.Namespace(**{
+        action.dest: parser.get_default(action.dest)
+        for action in parser._actions
+        if action.option_strings
+    })
+    for action in parser._actions:
+        if action.type is None and action.dest in config and action.default is not None:
+            action.type = type(action.default)
+    return parser
+
+
+base = get_config_parser("base")
