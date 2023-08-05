9426f751d5c22963a823d5cfbfcea861304f1484
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:40:21 +0000

    Add package ml-evaluation-framework/0.0b57/ml-evaluation-framework-0.0b57.tar.gz

---
+++ packages/ml-evaluation-framework/ml-evaluation-framework-0.0b57.tar.gz/ml-evaluation-framework-0.0b57/evaluation_framework/constants.py
@@ -0,0 +1,9 @@
+
+
+EF_UUID_NAME = "__specialEF__float32_UUID"
+
+EF_ORDERBY_NAME = "__specialEF__int32_dayDiff"
+
+EF_PREDICTION_NAME = "__specialEF__float32_predictions"
+
+EF_MEMMAP_MAP_NAME = "__specialHMF__memmapMap"
