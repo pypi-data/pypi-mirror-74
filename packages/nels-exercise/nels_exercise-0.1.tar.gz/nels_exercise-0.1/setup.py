111b098176be1d5d82bcea1f3f352620b09c30b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:10 +0000

    Add package nels-exercise/0.1/nels_exercise-0.1.tar.gz

---
+++ packages/nels-exercise/nels_exercise-0.1.tar.gz/nels_exercise-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='nels_exercise',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['nels_exercise'],
+      zip_safe=False)
