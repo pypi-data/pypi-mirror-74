0e958399b511f1efd973e4f0bfd1c73daf744cea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:33 +0000

    Add package intensio-obfuscator/1.0.10.4/intensio_obfuscator-1.0.10.4-py3-none-any.whl

---
+++ packages/intensio-obfuscator/intensio_obfuscator-1.0.10.4-py3-none-any.whl/intensio_obfuscator/core/obfuscation/intensio_mixer.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+# https://github.com/Hnfull/Intensio-Obfuscator
+
+#---------------------------------------------------------- [Lib] -----------------------------------------------------------#
+
+import random
+import string
+
+#------------------------------------------------- [Function(s)/Class(es)] --------------------------------------------------#
+
+class Mixer:
+    
+    def __init__(self):
+        self.lengthLower    = 32
+        self.lengthMedium   = 64
+        self.lengthHigh     = 128
+
+
+    def StringGenerator(self, stringLength):
+        randomCharsList = []
+
+        for i in range(stringLength):
+           randomCharsList.append(random.choice(string.ascii_letters))
+        return "".join(randomCharsList)
+        
+
+    def GetStringMixer(self, mixerLengthArgDefined):
+        if mixerLengthArgDefined == "lower":
+            return Mixer.StringGenerator(self, self.lengthLower)
+        elif mixerLengthArgDefined == "medium":
+            return Mixer.StringGenerator(self, self.lengthMedium)
+        elif mixerLengthArgDefined == "high":
+            return Mixer.StringGenerator(self, self.lengthHigh)
