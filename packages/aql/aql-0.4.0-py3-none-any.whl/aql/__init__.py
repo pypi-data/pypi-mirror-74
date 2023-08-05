c85da55cb0af286511129d60ab2785e6859800fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:55 +0000

    Add package aql/0.4.0/aql-0.4.0-py3-none-any.whl

---
+++ packages/aql/aql-0.4.0-py3-none-any.whl/aql/__init__.py
@@ -0,0 +1,17 @@
+# Copyright 2020 John Reese
+# Licensed under the MIT license
+
+"""
+asyncio db query generator
+"""
+
+__author__ = "John Reese"
+__version__ = "0.4.0"
+
+from .column import Column
+from .connector import connect
+from .engines.base import Connection, Cursor, Engine, Result
+from .errors import AqlError, BuildError, QueryError
+from .query import Query
+from .table import Table, table
+from .types import And, Boolean, Comparison, Join, Location, Operator, Or, Select
