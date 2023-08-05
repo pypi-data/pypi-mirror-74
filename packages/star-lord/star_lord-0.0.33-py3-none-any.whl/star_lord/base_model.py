a861201b876c551b2326dac50c6d5b4241a6f4de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:17 +0000

    Add package star-lord/0.0.33/star_lord-0.0.33-py3-none-any.whl

---
+++ packages/star-lord/star_lord-0.0.33-py3-none-any.whl/star_lord/base_model.py
@@ -0,0 +1,18 @@
+# Django
+from django.db import models
+
+# Project
+from .delete_model import DeleteModel
+
+
+class ModifyDateModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class BaseModel(ModifyDateModel, DeleteModel):
+    class Meta:
+        abstract = True
