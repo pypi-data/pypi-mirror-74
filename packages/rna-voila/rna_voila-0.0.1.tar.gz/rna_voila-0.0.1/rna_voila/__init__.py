dd19a2d5e9759369a5ec4f8cf8a45963b7d957a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:06 +0000

    Add package rna-voila/0.0.1/rna_voila-0.0.1.tar.gz

---
+++ packages/rna-voila/rna_voila-0.0.1.tar.gz/rna_voila-0.0.1/rna_voila/__init__.py
@@ -0,0 +1,5 @@
+raise Exception("""
+This is a placeholder package to reserve the pypi name and prevent confusion when installing the "majiq" and "voila" packages created by the
+Biociphers Lab for quantification and visualization of RNA splicing from RNA-seq data. You most likely didn't mean to install this package 
+from pypi. Please see https://majiq.biociphers.org/ for the full download and installation instructions of "majiq" and "voila"
+""")
---
+++ packages/rna-voila/rna_voila-0.0.1.tar.gz/rna_voila-0.0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='rna_voila',
+    author="BioCiphers",
+    author_email="admin@biociphers.org",
+    version='0.0.1',
+    packages=find_packages(),
+    license='MIT License',
+    description="dummy package for rna_voila",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://majiq.biociphers.org/",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
