e7277233a212b433d376d15ba38bd138af971dea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:52 +0000

    Add package goamazondownloader/0.1.4/goamazondownloader-0.1.4.tar.gz

---
+++ packages/goamazondownloader/goamazondownloader-0.1.4.tar.gz/goamazondownloader-0.1.4/goamazondownloader/sbandradar/_sbandradar.py
@@ -0,0 +1,120 @@
+import sys; sys.path.insert(0, "/home/adriano/goamazondownloader")
+"""S-Band radar data download"""
+
+# Author: Adriano P. Almeida <adriano.almeida@inpe.br>
+# License: MIT
+
+from goamazondownloader import (Downloader, os, requests as req, BeautifulSoup,
+                                ElementTree as ET)
+from goamazondownloader.constants import (SIPAM_FILENAME, SIPAM_FRAME_URL,
+                                          ARM_URL_BASE, ARM_LOGIN_URL)
+from goamazondownloader._exceptions import *
+
+
+class SBandRadar(Downloader):
+    def __init__(self, **kwargs) -> None:
+        super(SBandRadar, self).__init__(instrument='sbandradar', **kwargs)
+        self.hour = kwargs.get('hour', None)
+        self.minute = kwargs.get('minute', None)
+        self.instrument = 'sbandradar'
+        self.initializer()
+
+    def initializer(self) -> None:
+        try:
+            if not self.has_date():
+                raise DateRequiredError
+            if not self.has_time():
+                raise TimeRequiredError
+            self.format_date()
+            self.format_time()
+            super(SBandRadar, self).set_directory()
+            self.set_filename()
+        except DateRequiredError as err:
+            print(err)
+        except TimeRequiredError as err:
+            print(err)
+
+    def set_remote_url(self) -> None:
+        try:
+            if not self.is_logged():
+                raise LoginRequiredError
+            if not self.has_date():
+                raise DateRequiredError
+            if not self.has_time():
+                raise TimeRequiredError
+            frame_url = SIPAM_FRAME_URL.substitute(year=self.year,
+                                                   month=self.month,
+                                                   day=self.day,
+                                                   token_access=self.token)
+            res = req.get(frame_url)
+            soup = BeautifulSoup(res.content, "html.parser")
+            tags = soup.find_all('pre')[0].find_all('a')
+            urls = list()
+            for i, tag in enumerate(tags):
+                if i % 2 != 0:
+                    file_url = os.path.join(ARM_URL_BASE, tag.attrs['href'][1:])
+                    urls.append(file_url)
+            if not urls:
+                raise ListFilesNotFoundError
+
+            substring = "_cappi_%s%s%s_%s%s" % (self.year, self.month, self.day,
+                                                self.hour, self.minute)
+            file_url = list(filter(lambda url: substring in url, urls))
+
+            if not file_url:
+                raise UrlFileNotFoundError
+            self.remote_url = file_url[0]
+
+        except LoginRequiredError as err:
+            print(err)
+        except DateRequiredError as err:
+            print(err)
+        except TimeRequiredError as err:
+            print(err)
+        except ListFilesNotFoundError as err:
+            print(err)
+        except UrlFileNotFoundError as err:
+            print(err)
+
+    def has_time(self) -> bool:
+        return self.hour is not None and self.minute is not None
+
+    def format_time(self) -> None:
+        self.hour = str(self.hour).zfill(2) \
+            if self.hour is not None else None
+        self.minute = str(self.minute).zfill(2) \
+            if self.minute is not None else None
+
+    def set_filename(self) -> None:
+        try:
+            if not self.has_date():
+                raise DateRequiredError
+            if not self.has_time():
+                raise TimeRequiredError
+            if not self.has_directory():
+                self.set_directory()
+        except DateRequiredError as err:
+            print(err)
+        except TimeRequiredError as err:
+            print(err)
+
+        self.format_date()
+        self.format_time()
+
+        self.filename = os.path.join(self.directory, SIPAM_FILENAME.\
+                                     substitute(year=self.year,
+                                                month=self.month,
+                                                day=self.day,
+                                                hour=self.hour,
+                                                minute=self.minute,
+                                                sep='_'))
+
+
+if __name__ == "__main__":
+    obj = SBandRadar(year=2014, month=2, day=18, hour=15, minute=48)
+    obj.set_directory()
+    obj.login("username")
+    obj.set_remote_url()
+    obj.download()
+
+
---
+++ packages/goamazondownloader/goamazondownloader-0.1.4.tar.gz/goamazondownloader-0.1.4/setup.py
@@ -0,0 +1,67 @@
+"""A Python library to download meteorological data of campaign GoAmazon.
+
+Author Adriano P. ALmeida <adriano.almeida@inpe.br>
+License: MIT
+
+See:
+https://github.com/AdrianoAlmeida/goamazondownloader
+"""
+
+from setuptools import setup, find_packages
+import pathlib
+import sys
+import os
+
+HERE = pathlib.Path(__file__).parent.resolve()
+LONG_DESCRIPTION = (HERE / 'README.md').read_text(encoding='utf-8')
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist bdist_wheel')
+    os.system('twine upload dist/*')
+    sys.exit()
+
+if sys.argv[-1] == 'uninstall':
+    os.system('rm -rf build')
+    os.system('rm -rf dist')
+    os.system('rm -rf goamazondownloader.egg-info')
+    sys.exit()
+
+
+setup(
+    name='goamazondownloader',
+    version='0.1.4',
+    description='A library python to download data of campaign GoAmazon',
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
+    url='https://github.com/AdrianoPereira/goamazondownloader',
+    author='Adriano P. Almeida',
+    author_email='adriano.almeida@inpe.br',
+    classifiers = [
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate you support Python 3. These classifiers are *not*
+        # checked by 'pip install'. See instead 'python_requires' below.
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3 :: Only',
+    ],
+    keywords = 'data weather, goamazon, sipam, starnet',
+    packages = find_packages(where=HERE),  # Required
+    python_requires = '>=3.5, <4',
+    install_requires = ['requests', 'beautifulsoup4'],
+    project_urls = {
+        'Bug Reports': 'https://github.com/AdrianoPereira/goamazondownloader/issues',
+        'Say Thanks!': 'http://adrianopereira.github.io',
+        'Source': 'https://github.com/AdrianoPereira/goamazondownloader',
+    },
+    license = 'MIT'
+)
