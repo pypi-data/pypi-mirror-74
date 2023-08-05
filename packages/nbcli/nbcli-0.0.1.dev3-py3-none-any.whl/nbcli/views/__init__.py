14fb9080122bc7474d9546c2bc509e8249628c3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:40 +0000

    Add package nbcli/0.0.1.dev3/nbcli-0.0.1.dev3-py3-none-any.whl

---
+++ packages/nbcli/nbcli-0.0.1.dev3-py3-none-any.whl/nbcli/views/__init__.py
@@ -0,0 +1,8 @@
+"""Load views used by nbprint."""
+
+from nbcli.views.tools import BaseView                          # noqa: F401
+from nbcli.views.device import DcimDevicesView                  # noqa: F401
+from nbcli.views.interfaces import DcimInterfacesView           # noqa: F401
+from nbcli.views.object_changes import ExtrasObjectChangesView  # noqa: F401
+from nbcli.views.racks import DcimRacksView                     # noqa: F401
+from nbcli.views.sites import DcimSitesView                     # noqa: F401
