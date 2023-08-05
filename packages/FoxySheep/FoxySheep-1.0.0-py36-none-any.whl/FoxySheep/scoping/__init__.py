8093ba6c27d24016666856e5693a67c51b561412
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:31 +0000

    Add package FoxySheep/1.0.0/FoxySheep-1.0.0-py36-none-any.whl

---
+++ packages/FoxySheep/FoxySheep-1.0.0-py36-none-any.whl/FoxySheep/scoping/__init__.py
@@ -0,0 +1,3 @@
+from FoxySheep.scoping.Scope import GlobalScope, ScopeStack, ScopeType
+from FoxySheep.scoping.Symbol import Symbol, SymbolAttributes
+from FoxySheep.scoping.VirtualSymbols import VirtualSymbolProvider
