a96363b39f78f67e0825c0e36cca42bb827db6c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:14:17 +0000

    Add package fetchhtml/0.2/fetchhtml-0.2.tar.gz

---
+++ packages/fetchhtml/fetchhtml-0.2.tar.gz/fetchhtml-0.2/fetchhtml/fetcher.py
@@ -0,0 +1,36 @@
+import requests
+import os 
+from bs4 import BeautifulSoup
+import sys 
+
+# Get HTML
+def get_html(url):
+    html = BeautifulSoup(requests.get(url).content, "lxml")
+    return html
+
+# Find HTML element by tag 
+def find_element_by_tag(html,elem):
+    return html(elem)
+
+# Find element by HTML attribute value
+def find_element_by_attribute_value(html,  **kwargs):
+    code = html(**kwargs)
+    if not code:
+        print("Your element does not exist or is not native HTML (HTML added by JavaScript)")
+        sys.exit()
+    else:
+        return code
+
+# Save HTML file 
+def save_html(html, filename):
+    with open(filename, "w") as file:
+          file.write(html.prettify())
+    
+html = get_html("https://google.com")
+
+
+
+target_id = find_element_by_attribute_value(html, id="gba")
+save_html(html, "tester.html")
+print(target_id[0].text)
+
