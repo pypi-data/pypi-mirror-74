d2ab4331448c977244771813e62ed5a82eceb3bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:46 +0000

    Add package easyFileShare/0.15/easyFileShare-0.15.tar.gz

---
+++ packages/easyFileShare/easyFileShare-0.15.tar.gz/easyFileShare-0.15/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+# include additional packages as well - requests , tabulate , json
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="easyFileShare", # Replace with your own username
+    version="0.15",
+    author="Harsh Native",
+    author_email="Harshnative@gmail.com",
+    description="Get the weather of any city instantly in your project",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/harshnative/easyFileShare_module",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
