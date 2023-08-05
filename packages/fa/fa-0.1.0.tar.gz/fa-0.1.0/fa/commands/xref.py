b3f367392a5470794f448edd792c87255f378bea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:51 +0000

    Add package fa/0.1/fa-0.1.0.tar.gz

---
+++ packages/fa/fa-0.1.0.tar.gz/fa-0.1.0/fa/commands/xref.py
@@ -0,0 +1,25 @@
+from fa import utils, context
+
+try:
+    import idautils
+except ImportError:
+    pass
+
+
+def get_parser():
+    p = utils.ArgumentParserNoExit('xref',
+                                   description='goto xrefs pointing at'
+                                               ' current search results')
+    return p
+
+
+@context.ida_context
+@utils.yield_unique
+def xref(addresses):
+    for address in addresses:
+        for ref in idautils.XrefsTo(address):
+            yield ref.frm
+
+
+def run(segments, args, addresses, interpreter=None, **kwargs):
+    return list(xref(addresses))
