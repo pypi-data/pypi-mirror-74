fa77668c32aeb4463c5380fa0404978b998461b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:42 +0000

    Add package csv2sqllike/1.0.6/csv2sqllike-1.0.6.tar.gz

---
+++ packages/csv2sqllike/csv2sqllike-1.0.6.tar.gz/csv2sqllike-1.0.6/csv2sqllike/__init__.py
@@ -0,0 +1,31 @@
+import os
+import sys
+import datetime
+import re
+
+try:
+    import csv
+    import xlrd
+    import encodings
+except ImportError:
+    print(ImportError)
+
+from .info import __VERSION__, __version__
+from .PseudoSQLFromCSV import PsuedoSQLFromCSV
+from .Transfer2SQLDB import Transfer2SQLDB
+
+
+def get_data_from_csv(file_path, sep=',', type_dict=None, encoding='utf-8'):
+    pseudo = PsuedoSQLFromCSV(file_path, sep, type_dict, encoding)
+    return pseudo
+
+
+def get_transfer(database_info_dict=None):
+    transfer = Transfer2SQLDB(database_info_dict)
+    return transfer
+
+def to_list_from_df(df, sep=',', type_dict=None, encoding='utf-8'):
+    df.to_csv("./tmp_tmp.csv", index=False)
+    tmp_pseudo_sql = get_data_from_csv("./tmp_tmp.csv", sep=sep, type_dict=type_dict, encoding=encoding)
+    os.remove("./tmp_tmp.csv")
+    return tmp_pseudo_sql
