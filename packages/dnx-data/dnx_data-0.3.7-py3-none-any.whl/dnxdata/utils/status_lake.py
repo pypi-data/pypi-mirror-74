81d93ee2b8c367cdc63be5245432dbc6e67a8b2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:25 +0000

    Add package dnx-data/0.3.7/dnx_data-0.3.7-py3-none-any.whl

---
+++ packages/dnx-data/dnx_data-0.3.7-py3-none-any.whl/dnxdata/utils/status_lake.py
@@ -0,0 +1,18 @@
+
+status_lake = {
+    "canceled": "CANCELED",
+    "decompressing": "DECOMPRESSING",
+    "deleted": "DELETED",
+    "failed": "FAILED",
+    "free": "FREE",
+    "initial_load": "INITIAL_LOAD",
+    "invalid_file": "INVALID_FILE",
+    "lost": "LOST",
+    "stage": "STAGE",
+    "processing": "PROCESSING",
+    "running": "RUNNING",
+    "succeeded": "SUCCEEDED",
+    "stopped": "STOPPED",
+    "timeout": "TIMEOUT",
+    "waiting": "WAITING",
+}
