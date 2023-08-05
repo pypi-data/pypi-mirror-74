f24f8bd252e442deda4fb03542809ef1400ea354
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:41 +0000

    Add package doommoses/0.0.44/doommoses-0.0.44.tar.gz

---
+++ packages/doommoses/doommoses-0.0.44.tar.gz/doommoses-0.0.44/setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+import setuptools
+
+console_scripts = """
+[console_scripts]
+doommoses=doommoses.cli:cli
+"""
+
+setup(
+  name = 'doommoses',
+  packages = ['doommoses'],
+  version = '0.0.44',
+  description = 'DoomMoses',
+  long_description = 'MosesTokenizer in Python',
+  author = '',
+  package_data={'doommoses': ['data/perluniprops/*.txt', 'data/nonbreaking_prefixes/nonbreaking_prefix.*']},
+  url = 'https://github.com/sortiz/doommoses',
+  keywords = [],
+  classifiers = [
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+  ],
+  install_requires = ['regex', 'six', 'click', 'joblib', 'tqdm'],
+  setup_requires = ['wheel'],
+  entry_points=console_scripts,
+)
