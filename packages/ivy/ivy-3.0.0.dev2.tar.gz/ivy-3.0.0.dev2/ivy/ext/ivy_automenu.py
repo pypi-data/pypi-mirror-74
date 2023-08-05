d21fd0a5710637dffe8d7f8c9edddcdcf79479d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:21 +0000

    Add package ivy/3.0.0.dev2/ivy-3.0.0.dev2.tar.gz

---
+++ packages/ivy/ivy-3.0.0.dev2.tar.gz/ivy-3.0.0.dev2/ivy/ext/ivy_automenu.py
@@ -0,0 +1,73 @@
+# ------------------------------------------------------------------------------
+# This extension automatically generates a menu containing links to every node
+# in the site. The menu can be accessed in templates via the 'automenu'
+# attribute.
+#
+# If a node has a 'menu_title' attribute, its value will be used in the
+# menu in place of the node's title.
+#
+# By default entries are ordered alphabetically by filename. Entry order can
+# be customized by giving nodes an integer 'menu_order' attribute with
+# lower orders coming first. The default order value is 0. (Note that the
+# homepage is an exception and will always be the first entry in the menu.)
+#
+# If a node has a 'menu_exclude' attribute set to true, it will be excluded
+# from the menu.
+# ------------------------------------------------------------------------------
+
+import ivy
+
+
+# We generate the menu once and cache it for future use.
+cache = None
+
+
+# Register a callback to add the 'automenu' attribute to each page context.
+@ivy.events.register('render_page')
+def add_automenu(page):
+    global cache
+    if cache is None:
+        cache = make_menu()
+    page['automenu'] = cache
+
+
+def make_menu():
+    menu = ['<ul>\n']
+
+    root = ivy.nodes.root()
+    title = root.get('menu_title') or root.get('title') or 'Home'
+    menu.append(f'<li><a href="@root/">{title}</a></li>\n')
+
+    for child in sorted_children(root):
+        if not child.get('menu_exclude'):
+            add_node(child, menu)
+
+    menu.append('</ul>')
+    return ''.join(menu)
+
+
+def add_node(node, menu):
+    title = node.get('menu_title') or node.get('title')
+    if title is None:
+        return
+
+    menu.append('<li>')
+    menu.append(f'<a href="{node.url}">{title}</a>')
+
+    if node.has_children:
+        children = []
+        for child in sorted_children(node):
+            if not child.get('menu_exclude'):
+                children.append(child)
+        if children:
+            menu.append('<ul>\n')
+            for child in children:
+                add_node(child, menu)
+            menu.append('</ul>\n')
+
+    menu.append('</li>\n')
+
+
+def sorted_children(node):
+    children = sorted(node.children, key=lambda n: n.stem)
+    return sorted(children, key=lambda n: n.get('menu_order', 0))
