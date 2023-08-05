eda21f7be301721fe2b3631334432aab37aef848
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:27 +0000

    Add package distributionsBG/2.1/distributionsBG-2.1.tar.gz

---
+++ packages/distributionsBG/distributionsBG-2.1.tar.gz/distributionsBG-2.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributionsBG',
+      version='2.1',
+      description='Gaussian and Binomial distributions',
+      packages=['distributionsBG'],
+      author = 'Svitlana Konopatska',
+      author_email = 'svetlana_viktorovna@meta.ua',
+      zip_safe=False)
