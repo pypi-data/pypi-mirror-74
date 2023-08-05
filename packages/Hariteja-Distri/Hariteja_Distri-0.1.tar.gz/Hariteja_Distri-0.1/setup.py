5a36a2c53cc7e849cf6db7a8199d1bd058092e40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:48 +0000

    Add package Hariteja-Distri/0.1/Hariteja_Distri-0.1.tar.gz

---
+++ packages/Hariteja-Distri/Hariteja_Distri-0.1.tar.gz/Hariteja_Distri-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='Hariteja_Distri',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['Hariteja_Distri'],
+      zip_safe=False)
