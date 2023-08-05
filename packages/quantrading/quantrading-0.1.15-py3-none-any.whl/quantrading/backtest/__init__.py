26629ff412bb539e3ff43f1d79ec27ff4f5bcd6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:41 +0000

    Add package quantrading/0.1.15/quantrading-0.1.15-py3-none-any.whl

---
+++ packages/quantrading/quantrading-0.1.15-py3-none-any.whl/quantrading/backtest/__init__.py
@@ -0,0 +1,10 @@
+from .backtest import (
+    Strategy
+)
+from .utils import (
+    divide_code_list_by_quantiles,
+    apply_equal_weights,
+    get_no_rebalancing_port_daily_value_df,
+    get_static_weight_rebalancing_port_daily_value_df,
+    get_dynamic_weight_rebalancing_port_daily_value_df
+)
