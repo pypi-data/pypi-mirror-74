543a8c2b475dbb6baad4b3d1fae8cabe3fd7f775
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:30 +0000

    Add package glimg/0.0.7/glimg-0.0.7.tar.gz

---
+++ packages/glimg/glimg-0.0.7.tar.gz/glimg-0.0.7/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="glimg",
+    version="0.0.7",
+    author="glchen",
+    author_email="chencglt@gmail.com",
+    description="image processing tool for computer vision",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/chencgln/glimg",
+    packages=setuptools.find_packages(),
+    classifiers=[]
+        # "Programming Language :: Python :: 3",
+        # "License :: OSI Approved :: Apache License",
+        # "Operating System :: OS Independent",
+)
