5dfde1b28db5afeedb5c2cb1495da36128ca0b59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:44 +0000

    Add package bearer-agent/1.1.0/bearer_agent-1.1.0-py3-none-any.whl

---
+++ packages/bearer-agent/bearer_agent-1.1.0-py3-none-any.whl/bearer_agent/py3/errors.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -
+#
+# This file is part of bearer-agent released under the Apache License 2.
+# See the NOTICE for more information.
+
+import contextlib
+import logging
+import traceback
+
+
+logger = logging.getLogger("bearer")
+
+
+class ErrorBase(Exception):
+    pass
+
+
+class ConfigError(ErrorBase):
+    """ Exception raised on config error """
+
+
+@contextlib.contextmanager
+def error_safe():
+    try:
+        yield
+    except ErrorBase:
+        logger.error(traceback.format_exc())
+    except Exception:
+        logger.error(
+            "An unexpected error has occured. Please email us at support+python_agent@bearer.sh for support."
+        )
+        logger.error(traceback.format_exc())
