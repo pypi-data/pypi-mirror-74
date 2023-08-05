87c7082e8c75b876cd90d2822a57f501ff47c6ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:42 +0000

    Add package pyB12MPS/1.0.7/pyB12MPS-1.0.7.tar.gz

---
+++ packages/pyB12MPS/pyB12MPS-1.0.7.tar.gz/pyB12MPS-1.0.7/pyB12MPS/serverConfig.py
@@ -0,0 +1,16 @@
+# Configuration file for pyB12MPS
+# Bridge12 Technologies, Inc. 2020
+
+# TCP Server
+HOST = 'localhost'
+#PORT = 50007
+PORT = 90
+
+# Serial Port Settings
+defaultSerialPort = 'COM5'
+autoDetectSerialPort = True
+initializeOnStart = True
+serialDelay = 0.00 # in seconds
+
+# System Ready String
+systemReadyString = 'System Ready'
