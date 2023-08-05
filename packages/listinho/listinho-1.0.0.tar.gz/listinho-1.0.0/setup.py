ad0f802acb3b87e8cf8cd947d6ba0fecc85243a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:06 +0000

    Add package listinho/1.0.0/listinho-1.0.0.tar.gz

---
+++ packages/listinho/listinho-1.0.0.tar.gz/listinho-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name		= "listinho",
+	version		= "1.0.0",
+	py_modules	= ["listinho"],
+	author		= "visionzpy",
+	author_email	= "uniquesouljah.11@gmail.com",
+	url		= "http://www.google.com",
+	description	= "A simple printer of nested lists",
+	)
