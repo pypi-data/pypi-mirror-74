d6912b6167db3bb69672ca0a017877611b9c8688
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:27 +0000

    Add package x-py-libs/0.0.7.1/x_py_libs-0.0.7.1.tar.gz

---
+++ packages/x-py-libs/x_py_libs-0.0.7.1.tar.gz/x_py_libs-0.0.7.1/setup.py
@@ -0,0 +1,32 @@
+# -*- coding=utf-8 -*-
+
+from distutils.core import setup
+
+from setuptools import setup, find_packages
+
+setup(
+    name="x_py_libs",
+    version="0.0.7.1",
+    # keywords = ("pip", "license","licensetool", "tool", "gm"),
+    # description = "设备指纹获取、license生成、指纹与有效期验证工具",
+    # long_description = "设备指纹获取、license生成、指纹与有效期验证工具",
+    license="MIT Licence",
+
+    # url = "https://github.com/gm19900510/licensetool",
+    author="xiaxiazhu",
+    author_email="xiaxiazhu147@gmail.com",
+
+    packages=find_packages(),
+    include_package_data=True,
+    platforms="any",
+    install_requires=[
+        'psycopg2', 'pyodbc',
+        'tornado',
+        'Flask', 'Flask_Cors', 'Flask_RESTful',
+        'redis',
+        'six', 'pymediainfo', 'itsdangerous','Werkzeug',
+        'requests',
+        'pycrypto'
+    ]
+)
+
