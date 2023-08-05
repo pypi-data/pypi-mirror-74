d26be46029e9eab2f3ce7eaa6ac9bf0c6f39d980
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:46 +0000

    Add package fort-probability/1.1/fort-probability-1.1.tar.gz

---
+++ packages/fort-probability/fort-probability-1.1.tar.gz/fort-probability-1.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='fort-probability',
+      version='1.1',
+      description='Gaussian and binomial distributions',
+      packages=['fort-probability'],
+      author = 'Fortune Adekogbe',
+      author_email = 'fortuneadekogbe@gmail.com',
+      zip_safe=False)
+
