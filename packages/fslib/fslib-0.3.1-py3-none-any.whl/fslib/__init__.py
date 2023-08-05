5559ecc8355c7708734af4503f61be3d7d7c28a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:27 +0000

    Add package fslib/0.3.1/fslib-0.3.1-py3-none-any.whl

---
+++ packages/fslib/fslib-0.3.1-py3-none-any.whl/fslib/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2011-2020 Raphaël Barrois
+# This code is distributed under the two-clause BSD License.
+
+__version__ = '0.3.1'
+__author__ = 'Raphaël Barrois <raphael.barrois+fslib@polytechnique.org>'
+
+
+from .base import FileSystem, OSFS, ROOT
+from .exceptions import FSError
