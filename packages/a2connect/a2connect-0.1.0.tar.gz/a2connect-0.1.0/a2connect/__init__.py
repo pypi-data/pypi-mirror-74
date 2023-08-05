55825bdd1fda69f2d8b2d7c41850a37c79cd5bc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:06 +0000

    Add package a2connect/0.1.0/a2connect-0.1.0.tar.gz

---
+++ packages/a2connect/a2connect-0.1.0.tar.gz/a2connect-0.1.0/a2connect/__init__.py
@@ -0,0 +1,7 @@
+print ("!!!!!!!!!!!!!!!!!! Hello, to  a2connect !!!!!!!!!!!!!!!!!!!!!!")
+import mod1
+import mod2
+def hello():
+    return "!!!!!!!!!!!!!!!!! Hello, world !!!!!!!!!!!!!!!!!!"
+x = mod1.shima
+y = mod2.evgen
