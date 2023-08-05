e15ed781a57348bc444c75eff6a994eda3f30aaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:37:27 +0000

    Add package you-live/1.0.5/you-live-1.0.5.tar.gz

---
+++ packages/you-live/you-live-1.0.5.tar.gz/you-live-1.0.5/live_recorder/you_live/_recorder.py
@@ -0,0 +1,18 @@
+# coding=utf-8
+from .douyu_recorder import DouyuRecorder
+from .bili_recorder import BiliRecorder
+from .kuaishou_recorder import KuaishouRecorder
+from .acfun_recorder import AcfunRecorder
+
+def createRecorder(liver, short_id, **args):
+    if liver == 'bili':
+        recorder = BiliRecorder(short_id, **args)
+    elif liver == 'douyu':
+        recorder = DouyuRecorder(short_id, **args)
+    elif liver == 'kuaishou':
+        recorder = KuaishouRecorder(short_id, **args)
+    elif liver == 'acfun':
+        recorder = AcfunRecorder(short_id, **args)
+    else:
+        recorder = None
+    return recorder
