4e31ba363b0a93b635aa5f865f13b8861f191f8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:58 +0000

    Add package udata/2.1.4.dev19230/udata-2.1.4.dev19230-py2.py3-none-any.whl

---
+++ packages/udata/udata-2.1.4.dev19230-py2.py3-none-any.whl/udata/api/commands.py
@@ -0,0 +1,61 @@
+import logging
+import os
+
+import click
+
+from flask import json, current_app
+from flask_restplus import schemas
+
+from udata.api import api
+from udata.commands import cli, success, exit_with_error
+
+log = logging.getLogger(__name__)
+
+
+@cli.group('api')
+def grp():
+    '''API related operations'''
+    pass
+
+
+def json_to_file(data, filename, pretty=False):
+    '''Dump JSON data to a file'''
+    kwargs = dict(indent=4) if pretty else {}
+    dirname = os.path.dirname(filename)
+    if not os.path.exists(dirname):
+        os.makedirs(dirname)
+    dump = json.dumps(api.__schema__, **kwargs)
+    with open(filename, 'wb') as f:
+        f.write(dump.encode('utf-8'))
+
+
+@grp.command()
+@click.argument('filename')
+@click.option('-p', '--pretty', is_flag=True, help='Pretty print')
+def swagger(filename, pretty):
+    '''Dump the swagger specifications'''
+    json_to_file(api.__schema__, filename, pretty)
+
+
+@grp.command()
+@click.argument('filename')
+@click.option('-p', '--pretty', is_flag=True, help='Pretty print')
+@click.option('-u', '--urlvars', is_flag=True, help='Export query strings')
+@click.option('-s', '--swagger', is_flag=True,
+              help='Export Swagger specifications')
+def postman(filename, pretty, urlvars, swagger):
+    '''Dump the API as a Postman collection'''
+    data = api.as_postman(urlvars=urlvars, swagger=swagger)
+    json_to_file(data, filename, pretty)
+
+
+@grp.command()
+def validate():
+    '''Validate the Swagger/OpenAPI specification with your config'''
+    with current_app.test_request_context():
+        schema = json.loads(json.dumps(api.__schema__))
+    try:
+        schemas.validate(schema)
+        success('API specifications are valid')
+    except schemas.SchemaValidationError as e:
+        exit_with_error('API specifications are not valid', e)
---
+++ packages/udata/udata-2.1.4.dev19230-py2.py3-none-any.whl/udata/tests/organization/test_organization_tasks.py
@@ -0,0 +1,29 @@
+from .. import TestCase, SearchTestMixin
+
+from udata.models import Dataset, Organization
+from udata.core.dataset.factories import DatasetFactory, ResourceFactory
+from udata.core.dataset.search import DatasetSearch
+from udata.core.organization import tasks
+from udata.search import es
+
+
+class OrganizationTasksTest(SearchTestMixin, TestCase):
+    def test_purge_organizations(self):
+        with self.autoindex():
+            org = Organization.objects.create(
+                name='delete me', deleted='2016-01-01', description='XXX')
+            resources = [ResourceFactory() for _ in range(2)]
+            dataset = DatasetFactory(resources=resources, organization=org)
+
+        tasks.purge_organizations()
+
+        dataset = Dataset.objects(id=dataset.id).first()
+        self.assertIsNone(dataset.organization)
+
+        organization = Organization.objects(name='delete me').first()
+        self.assertIsNone(organization)
+
+        indexed_dataset = DatasetSearch.get(id=dataset.id,
+                                            using=es.client,
+                                            index=es.index_name)
+        self.assertIsNone(indexed_dataset.organization)
