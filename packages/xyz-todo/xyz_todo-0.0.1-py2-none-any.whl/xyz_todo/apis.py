dcc713e88001ae364dfeae61beb04c07e1fd3412
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:37 +0000

    Add package xyz-todo/0.0.1/xyz_todo-0.0.1-py2-none-any.whl

---
+++ packages/xyz-todo/xyz_todo-0.0.1-py2-none-any.whl/xyz_todo/apis.py
@@ -0,0 +1,30 @@
+# -*- coding:utf-8 -*-
+from __future__ import division
+from xyz_restful.mixins import UserApiMixin
+from rest_framework.viewsets import GenericViewSet
+
+from . import models, serializers
+from rest_framework import mixins, decorators
+from xyz_restful.decorators import register
+
+
+@register()
+class TodoViewSet(UserApiMixin, mixins.ListModelMixin, GenericViewSet):
+    queryset = models.Todo.objects.all()
+    serializer_class = serializers.TodoSerializer
+    filter_fields = {
+        'user': ['exact'],
+        'is_done': ['exact'],
+        'expiration': ['gt', 'lt']
+    }
+
+    def get_queryset(self):
+        qset = super(TodoViewSet, self).get_queryset()
+        if self.action == 'current':
+            from datetime import datetime
+            qset = qset.filter(user=self.request.user, is_done=False, expiration__gt=datetime.now())
+        return qset
+
+    @decorators.list_route(methods=['GET'])
+    def current(self, request):
+        return self.list(request)
