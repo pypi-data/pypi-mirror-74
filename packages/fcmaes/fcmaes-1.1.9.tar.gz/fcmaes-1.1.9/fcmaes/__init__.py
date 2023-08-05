cd48918c10b34b077c5fb46437e8f1d0d01921cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:53 +0000

    Add package fcmaes/1.1.9/fcmaes-1.1.9.tar.gz

---
+++ packages/fcmaes/fcmaes-1.1.9.tar.gz/fcmaes-1.1.9/fcmaes/__init__.py
@@ -0,0 +1,23 @@
+# Copyright (c) Dietmar Wolz.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory.
+
+__version__ = '1.1.9'
+
+__all__ = [
+    'cmaes',
+    'cmaescpp',
+    'dacpp',
+    'decpp',
+    'gcldecpp',
+    'hhcpp',
+    'retry',
+    'advretry',
+    'multiretry',
+    'pygmoretry',
+    'optimizer',
+    'astro',
+    'evaluator',
+    'testfun',
+]
