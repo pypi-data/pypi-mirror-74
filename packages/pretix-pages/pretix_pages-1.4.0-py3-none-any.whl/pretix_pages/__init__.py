dc1ec3a359e36eec9b547d54d15525fdbad35ff4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:17 +0000

    Add package pretix-pages/1.4.0/pretix_pages-1.4.0-py3-none-any.whl

---
+++ packages/pretix-pages/pretix_pages-1.4.0-py3-none-any.whl/pretix_pages/__init__.py
@@ -0,0 +1,22 @@
+from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
+
+
+class PluginApp(AppConfig):
+    name = 'pretix_pages'
+    verbose_name = 'Pages'
+
+    class PretixPluginMeta:
+        name = _('Pages')
+        author = 'Raphael Michel'
+        category = 'FEATURE'
+        description = _('Allows you to add static pages to your event site, for example for a FAQ, '
+                        'terms of service, etc.')
+        visible = True
+        version = '1.4.0'
+
+    def ready(self):
+        from . import signals  # NOQA
+
+
+default_app_config = 'pretix_pages.PluginApp'
