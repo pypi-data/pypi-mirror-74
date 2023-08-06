baa3c8120347c5e3c56d2b8d9c96cd70ef79b33b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:49 +0000

    Add package BI-parser/0.0.5/BI_parser-0.0.5-py3-none-any.whl

---
+++ packages/BI-parser/BI_parser-0.0.5-py3-none-any.whl/BI_parser/code.py
@@ -0,0 +1,47 @@
+'''
+Contient la classe abstraite chargée de traiter, stocker et manipuler les informations liées aux blocs de code.
+'''
+from abc import abstractmethod
+from typing import List
+
+from src.BI_parser.bloc import Bloc
+from src.BI_parser.variable import Variable
+
+class Code(Bloc):
+    '''
+    description
+    '''
+
+    @abstractmethod
+    def __init__(self, texte: str, debut: int, fin: int, id_: str) -> None:
+        '''
+        description
+        '''
+
+        Bloc.__init__(self, texte, debut, fin, id_)
+        self.var_utilise: List[Variable] = []
+        self.var_declare: List[Variable] = []
+
+    def imbrication_max(self) -> int:
+        '''
+        description
+        '''
+
+        # à renseigner
+        return 6  # temporaire
+
+    def imbrication_nb(self) -> int:
+        '''
+        description
+        '''
+
+        # à renseigner
+        return 6  # temporaire
+
+    def imbrication_moyenne(self) -> int:
+        '''
+        description
+        '''
+
+        # à renseigner
+        return 6  # temporaire
