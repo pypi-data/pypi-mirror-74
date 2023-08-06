46f75cc727f69073369739600d3963fe18113540
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:45 +0000

    Add package peacepdf/1.1/peacepdf-1.1.tar.gz

---
+++ packages/peacepdf/peacepdf-1.1.tar.gz/peacepdf-1.1/peacepdf/pdf2text.py
@@ -0,0 +1,17 @@
+""" This module contains functions to convert pdf o text. """
+
+
+class Converter:
+    """ A simple converter for converting pdf to text. """
+
+    def convert(self, path):
+        """ 
+                Convert the given PDF to text.
+
+                Parameters:
+                path (str): The path to a PDF file.
+
+                Returns:
+                str: The content of the PDF file as text.
+            """
+        print("pdf2text")
