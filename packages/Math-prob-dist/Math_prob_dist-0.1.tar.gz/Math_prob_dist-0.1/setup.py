933852cc3207e59c7a08c61f50ff4a506b2f6272
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:13 +0000

    Add package Math-prob-dist/0.1/Math_prob_dist-0.1.tar.gz

---
+++ packages/Math-prob-dist/Math_prob_dist-0.1.tar.gz/Math_prob_dist-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='Math_prob_dist',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['Math_prob_dist'],
+      zip_safe=False)
