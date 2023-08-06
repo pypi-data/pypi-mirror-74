34780f470403e643d0a214901c35127bb083a8b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:29 +0000

    Add package stockquote-pkg-jjbiggins/0.0.1/stockquote-pkg-jjbiggins-0.0.1.tar.gz

---
+++ packages/stockquote-pkg-jjbiggins/stockquote-pkg-jjbiggins-0.0.1.tar.gz/stockquote-pkg-jjbiggins-0.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="stockquote-pkg-jjbiggins", # Replace with your own username
+    version="0.0.1",
+    author="Joe Biggins",
+    author_email="jjbiggins@joebiggins.io",
+    description="Stock Quote CLI",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jjbiggins/stock-cli",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
