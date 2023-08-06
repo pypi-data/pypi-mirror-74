869e7c814c6aecc14ed6fb7ef73d84440549ad34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:10 +0000

    Add package joshy-distributions/0.1/joshy_distributions-0.1.tar.gz

---
+++ packages/joshy-distributions/joshy_distributions-0.1.tar.gz/joshy_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='joshy_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['joshy_distributions'],
+      author='Joshua Adewolu',
+      author_email='ija236@nyu.edu',
+      zip_safe=False)
