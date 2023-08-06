a0cc6abdb497e07d72001130aeca46e274aa4edf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:32 +0000

    Add package python-nbaclient/0.0.1/python_nbaclient-0.0.1.tar.gz

---
+++ packages/python-nbaclient/python_nbaclient-0.0.1.tar.gz/python_nbaclient-0.0.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="python_nbaclient",
+    version="0.0.1",
+    author="Lucas H. Xu",
+    author_email="lucasxu.pub@gmail.com",
+    description="A Python Wrapper of NBA RESTful Stats APIs",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/xuhang57/python_nbaclient",
+    packages=find_packages(),
+    install_requires=['requests', 'pprint'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
