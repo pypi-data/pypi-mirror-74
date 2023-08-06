974136f6701a2643b27d7b96a2d4be70cee85867
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:25 +0000

    Add package TGenTools/1.0.0rc2/TGenTools-1.0.0rc2.tar.gz

---
+++ packages/TGenTools/TGenTools-1.0.0rc2.tar.gz/TGenTools-1.0.0rc2/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='TGenTools',
+      version="1.0.0-rc2",
+      description='A utility to analyze and visualize TGen output',
+      author='Rob Jansen',
+      url='https://github.com/shadow/tgen/',
+      packages=['tgentools'],
+      scripts=['tgentools/tgentools'],
+     )
