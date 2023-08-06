01b7ee9470bce29bf6e7f19981619afe77c256b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:24 +0000

    Add package whatlies/0.4.4/whatlies-0.4.4.tar.gz

---
+++ packages/whatlies/whatlies-0.4.4.tar.gz/whatlies-0.4.4/whatlies/language/__init__.py
@@ -0,0 +1,19 @@
+from .spacy_lang import SpacyLanguage
+from .sense2vec_lang import Sense2VecLanguage
+from .fasttext_lang import FasttextLanguage
+from .countvector_lang import CountVectorLanguage
+from .bpemblang import BytePairLanguage
+from .bpemblang import BytePairLanguage as BytePairLang
+from .convert_lang import ConveRTLanguage
+from .gensim_lang import GensimLanguage
+
+__all__ = [
+    "SpacyLanguage",
+    "Sense2VecLanguage",
+    "FasttextLanguage",
+    "CountVectorLanguage",
+    "BytePairLang",
+    "BytePairLanguage",
+    "GensimLanguage",
+    "ConveRTLanguage",
+]
