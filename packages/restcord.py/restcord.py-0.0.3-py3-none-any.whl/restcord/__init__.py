47b214bd75ed1da182f4baa1a829d228306512b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:17 +0000

    Add package restcord.py/0.0.3/restcord.py-0.0.3-py3-none-any.whl

---
+++ packages/restcord.py/restcord.py-0.0.3-py3-none-any.whl/restcord/__init__.py
@@ -0,0 +1,9 @@
+__title__ = 'restcord'
+__author__ = 'Lethys'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2020 (c) Lethys'
+__version__ = '0.0.3'
+
+from .client import RestCord
+from .errors import Forbidden, HTTPException, NotFound
+from .models import Channel, Emoji, Guild, Member, User
