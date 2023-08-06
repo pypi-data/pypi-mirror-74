6f40e2d09c496a12c586a506025daaaaebfd422d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:48 +0000

    Add package vkbottle/2.7.7/vkbottle-2.7.7-py3-none-any.whl

---
+++ packages/vkbottle/vkbottle-2.7.7-py3-none-any.whl/vkbottle/const.py
@@ -0,0 +1,15 @@
+__version__ = "2.7.7"  # Package VKBottle version
+
+__author__ = "Arsenii Timoniq"
+
+API_VERSION = "5.103"  # VK Api version
+
+API_URL = "https://api.vk.com/method/"
+
+DEFAULT_BOT_FOLDER = "vkbottle_bot"
+
+VERSION_REST = (
+    "https://raw.githubusercontent.com/timoniq/vkbottle-rest/master/version.json"
+)
+
+VBML_INSTALL = "pip install vbml -U"
