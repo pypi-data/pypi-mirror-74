f018b20107b3452b6a50719b87eeff799b011778
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:51 +0000

    Add package bino-gaus-dist/0.2/bino_gaus_dist-0.2.tar.gz

---
+++ packages/bino-gaus-dist/bino_gaus_dist-0.2.tar.gz/bino_gaus_dist-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='bino_gaus_dist',
+      version='0.2',
+      description='Gaussian and Binomial distributions',
+      packages=['bino_gaus_dist'],
+      author='Gujjar Mohan Manjunath',
+      author_email='manjunathgujjar.147@gmail.com',
+      zip_safe=False)
