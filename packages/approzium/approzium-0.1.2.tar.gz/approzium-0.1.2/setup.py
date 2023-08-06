be927435cd812f6b28534984e920c01f6182a810
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:03 +0000

    Add package approzium/0.1.2/approzium-0.1.2.tar.gz

---
+++ packages/approzium/approzium-0.1.2.tar.gz/approzium-0.1.2/setup.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['approzium',
+ 'approzium._mysql',
+ 'approzium._postgres',
+ 'approzium._protos',
+ 'approzium.asyncpg',
+ 'approzium.mysql',
+ 'approzium.mysql.connector',
+ 'approzium.psycopg2']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['boto3>=1.14.10,<2.0.0',
+ 'grpcio-tools>=1.30.0,<2.0.0',
+ 'grpcio>=1.30.0,<2.0.0']
+
+extras_require = \
+{'sqllibs': ['psycopg2>=2.8.5,<3.0.0',
+             'asyncpg>=0.20.1,<0.21.0',
+             'mysql-connector-python>=8.0.20,<9.0.0']}
+
+setup_kwargs = {
+    'name': 'approzium',
+    'version': '0.1.2',
+    'description': 'Approzium SDK provides Approzium database authentiation for Python',
+    'long_description': None,
+    'author': 'Cyral',
+    'author_email': 'security@cyral.org',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/cyralinc/approzium',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'extras_require': extras_require,
+    'python_requires': '>=3.5,<4.0',
+}
+
+
+setup(**setup_kwargs)
