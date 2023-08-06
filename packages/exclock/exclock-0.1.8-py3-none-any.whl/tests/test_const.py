abe623deab65198e216c946234f3d32c3ec06cc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:20 +0000

    Add package exclock/0.1.8/exclock-0.1.8-py3-none-any.whl

---
+++ packages/exclock/exclock-0.1.8-py3-none-any.whl/tests/test_const.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from pathlib import Path
+
+TEST_SOUND_DIR_IN_SYS = Path(__file__).parent / "data" / "sound_in_sys"
+TEST_CLOCK_DIR_IN_SYS = Path(__file__).parent / "data" / "clock_in_sys"
