9a214732fb1f4e8654a0934806d57bb3699c83c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:28 +0000

    Add package shapelyutils/0.2.0/shapelyutils-0.2.0.tar.gz

---
+++ packages/shapelyutils/shapelyutils-0.2.0.tar.gz/shapelyutils-0.2.0/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="shapelyutils",
+    version="0.2.0",
+    author="Mikko Virkkilä",
+    author_email="mikko.virkkila@steerpath.com",
+    description="Utilities to make life with shapely easier",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://bitbucket.com/steerpath/shapelyutils-python",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: Other/Proprietary License",
+        "Operating System :: OS Independent",
+    ],
+)
