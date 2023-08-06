0ae3603936c47e34b2fb63eff25324ab0b4642c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:08 +0000

    Add package gsnd-probability/0.1/gsnd_probability-0.1.tar.gz

---
+++ packages/gsnd-probability/gsnd_probability-0.1.tar.gz/gsnd_probability-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='gsnd_probability',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['gsnd_probability'],
+      author = 'Jarnen Richard',
+      auther_email = 'richard.jarnen@gmail.com',
+      zip_safe=False)
