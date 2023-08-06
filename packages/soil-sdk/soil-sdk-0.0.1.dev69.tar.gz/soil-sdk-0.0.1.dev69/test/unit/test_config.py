c8b90606e5b7af78561bb4031a8572a82d346194
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:14 +0000

    Add package soil-sdk/0.0.1.dev69/soil-sdk-0.0.1.dev69.tar.gz

---
+++ packages/soil-sdk/soil-sdk-0.0.1.dev69.tar.gz/soil-sdk-0.0.1.dev69/test/unit/test_config.py
@@ -0,0 +1,11 @@
+# pylint: disable=missing-docstring,no-self-use
+import unittest
+
+
+class TestModulify(unittest.TestCase):
+    pass
+    # def test_config(self) -> None:
+    #     test_conf = {'token': 'test_token', 'host': 'test_host'}
+    #     conf = soil.config(**test_conf)
+    #     assert conf == configuration.GLOBAL_CONFIG
+    #     assert conf._asdict() == test_conf
