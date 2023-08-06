9668cd23c41e7ea1b29ba24deaf2b3224f34b73a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:13 +0000

    Add package exercise5-distributions-prob/0.1/exercise5_distributions_prob-0.1.tar.gz

---
+++ packages/exercise5-distributions-prob/exercise5_distributions_prob-0.1.tar.gz/exercise5_distributions_prob-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='exercise5_distributions_prob',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['exercise5_distributions_prob'],
+      zip_safe=False)
