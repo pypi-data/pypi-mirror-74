ed90f76dd6013ae44002d7e40d455ae5586ec92b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:37 +0000

    Add package distributions-package-test/0.1/distributions-package-test-0.1.tar.gz

---
+++ packages/distributions-package-test/distributions-package-test-0.1.tar.gz/distributions-package-test-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions-package-test',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions-package-test'],
+      author = 'Patrick Mahomes',
+      author_email = 'patmahomes90@protonmail.com',
+      zip_safe=False)
