e95d20658724e0ddb7ce80f689a8f42e11c10e89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:58 +0000

    Add package distributions-raptvin/1.5/distributions_raptvin-1.5.tar.gz

---
+++ packages/distributions-raptvin/distributions_raptvin-1.5.tar.gz/distributions_raptvin-1.5/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_raptvin',
+      version='1.5',
+      description='Gaussian distributions, Binomial Distributions',
+      packages=['distributions_raptvin'],
+      author = 'Tanvesh Takawale',
+      author_email = 'tanveshtakawale26@gmail.com',
+      zip_safe=False)
