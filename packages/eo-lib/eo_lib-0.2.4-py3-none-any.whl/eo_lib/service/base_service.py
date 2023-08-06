81e8494b70379430eaaf4e86c20317d0dcf1f0f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:21 +0000

    Add package eo-lib/0.2.4/eo_lib-0.2.4-py3-none-any.whl

---
+++ packages/eo-lib/eo_lib-0.2.4-py3-none-any.whl/eo_lib/service/base_service.py
@@ -0,0 +1,59 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from eo_lib.config.config import engine
+from eo_lib.model.core.models import ApplicationReference
+
+class BaseService():
+    
+    def __init__(self, object):
+
+        self.object = object
+        self.type = self.object.__tablename__
+    
+    def __create_session_connection(self):
+        Session = sessionmaker(bind=engine,autocommit=True)
+        self.session = Session()
+        self.session.begin(subtransactions=True)
+
+    def __close_session_connection(self):
+        self.session.close()
+
+    def get_all(self):
+        self.__create_session_connection()
+        results = self.session.query(self.object).order_by(self.object.id).all()
+        self.__close_session_connection()
+        return results
+    
+    def find_by_uuid(self, uuid):
+        self.__create_session_connection()
+        return self.session.query(self.object).filter(self.object.uuid == uuid).first()
+        
+    def create(self, object):
+        self.__create_session_connection()
+        try:
+            self.session.add(object)
+            self.session.commit()
+        except:
+            self.session.rollback() 
+            raise
+
+    def update (self, object):
+        self.__create_session_connection()
+        try:
+            self.session.query(self.object).filter(self.object.id == object.id).update({column: getattr(object, column) for column in self.object.__table__.columns.keys()})
+            self.session.commit()
+        except:
+            self.session.rollback() 
+            raise
+        
+
+    def delete(self, object):
+        self.__create_session_connection()
+        try:
+            self.session.delete(object)
+            self.session.commit()
+        except:
+            self.session.rollback() 
+            raise
+        
+
