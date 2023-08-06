04072fc987b226226866bfdd2643b8a845522afb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:02 +0000

    Add package GaussAndNomial-distribution/1.0.2/GaussAndNomial_distribution-1.0.2.tar.gz

---
+++ packages/GaussAndNomial-distribution/GaussAndNomial_distribution-1.0.2.tar.gz/GaussAndNomial_distribution-1.0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='GaussAndNomial_distribution',
+      version='1.0.2',
+      description='Gaussian and Binomial distribution',
+      packages=['GaussAndNomial_distribution'],
+      author= 'Dharmik Shah',
+      author_email= 'dharmikshah589@gmail.com',
+      zip_safe=False)
