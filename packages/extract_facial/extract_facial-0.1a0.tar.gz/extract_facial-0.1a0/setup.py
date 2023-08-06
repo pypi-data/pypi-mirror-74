48b04f85c163c1dd23a40d135799bd2f82138030
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:49 +0000

    Add package extract_facial/0.1a0/extract_facial-0.1a0.tar.gz

---
+++ packages/extract_facial/extract_facial-0.1a0.tar.gz/extract_facial-0.1a0/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="extract_facial",
+    version="v0.1-alpha",
+    author="Vineet Kulkarni",
+    author_email="2vineetk@gmail.com",
+    description="Extract facial features in Python such as the eyes and mouth from MTCNN's predictions and find whether eyes or mouths are closed/open.",
+    packages=["extract_facial"],
+    include_package_data=True,
+    url = "https://github.com/vink246/extract_facial.git",
+    download_url = "https://github.com/vink246/extract_facial/archive/v0.1-alpha.tar.gz",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+    python_requires='>=3.5',
+    install_requires= ["opencv", "imutils", "numpy"]
+)
