0d3a477b5f979eaca3beaf2139c5a86d6d822444
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:42 +0000

    Add package new-project/0.5.4/new_project-0.5.4-py3-none-any.whl

---
+++ packages/new-project/new_project-0.5.4-py3-none-any.whl/new/template.py
@@ -0,0 +1,27 @@
+import yaml
+
+def get_input(prompt):
+    return input(prompt)
+
+def enter_loop_with(config, prompt=get_input):
+    replacement_map = []
+    for item in config['replace']['strings']:
+        match = item['match']
+        description = item['description']
+        response = prompt(f'Enter replacement text for:\n  {match}\nDescription:\n  {description}\n > ')
+        replacement_map.append((match, response))
+    return replacement_map
+
+def read(project):
+    # Prompts the user for any template replacement requirements
+    print(f'reading template for project: {project}')
+    try:
+        with open(f'{project}/.new.yml') as file:
+            f = yaml.load(file, Loader=yaml.FullLoader)
+            mp = enter_loop_with(f)
+            return mp
+    except:
+        print('no template config to process')
+        return []
+
+    
