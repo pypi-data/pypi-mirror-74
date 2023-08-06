27aeb417412c53992dfe06c1525215e33eda50e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:39 +0000

    Add package unicon.plugins/20.6.1b1/unicon.plugins-20.6.1b1-py3-none-any.whl

---
+++ packages/unicon.plugins/unicon.plugins-20.6.1b1-py3-none-any.whl/unicon/plugins/iosxe/stack/patterns.py
@@ -0,0 +1,8 @@
+from unicon.plugins.iosxe.patterns import IosXEPatterns
+
+
+class StackIosXEPatterns(IosXEPatterns):
+    def __init__(self):
+        super().__init__()
+        self.rommon_prompt = r'(.*)switch:\s?$'
+        self.tcpdump = ".*listening on lfts.*$"
