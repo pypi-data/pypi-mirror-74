c1e054076a108607dfd05d2837f2e9ad8cbdf03c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:43 +0000

    Add package grapheno/0.0.1/grapheno-0.0.1.tar.gz

---
+++ packages/grapheno/grapheno-0.0.1.tar.gz/grapheno-0.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+setuptools.setup(
+    name="grapheno", # Replace with your own username
+    version="0.0.1",
+    license="MIT",
+    author="Erik Burlingame",
+    author_email="erik.burlingame@gmail.com",
+    description="GPU-boosted PhenoGraph for fast single-cell phenotyping",
+    url="https://gitlab.com/eburling/grapheno",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'rapid==0.14.0',
+        'cupy==7.2.0'
+    ]
+)
