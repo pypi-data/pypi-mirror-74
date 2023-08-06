3b7711d40c21efc65b7c84c66ef3e9baccc764eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:54 +0000

    Add package prob-dsnd-distributions/1.0/prob_dsnd_distributions-1.0.tar.gz

---
+++ packages/prob-dsnd-distributions/prob_dsnd_distributions-1.0.tar.gz/prob_dsnd_distributions-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='prob_dsnd_distributions',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['prob_dsnd_distributions'],
+      author="Sarvaswa Tandon",
+      author_email="savvy235@gmail.com",
+      zip_safe=False)
