697d09214617bf1a914b0dd9ec2a512f52bbf27b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:47 +0000

    Add package evm-fm-python/0.1.6/evm_fm_python-0.1.6-py3-none-any.whl

---
+++ packages/evm-fm-python/evm_fm_python-0.1.6-py3-none-any.whl/evm_fm/__main__.py
@@ -0,0 +1,37 @@
+import os
+import sys
+
+import toml
+
+from evm_fm import utils
+
+
+def main():
+    """Main method
+    """
+    if not utils.check_os():
+        print('ERROR: evm_fm can only be run on Mac OS.')
+        sys.exit(1)
+
+    args = utils.parse_args(sys.argv[1:])
+
+    # Parse config file
+    config_file_loc = args.config_file
+    with open(str(config_file_loc), 'r') as config_file:
+        parsed_config_file = toml.loads(config_file.read())
+
+    utils.verify_required_configurations(parsed_config_file)
+
+    # Only create LaunchAgent if unload arg is *not* passed
+    if not args.unload:
+        utils.create_launch_agent(parsed_config_file)
+
+    if args.load:
+        utils.load_launch_agent()
+    elif args.unload:
+        utils.unload_launch_agent()
+
+    print(utils.get_launch_agent_status(parsed_config_file))
+
+if __name__ == '__main__':
+    sys.exit(main())
