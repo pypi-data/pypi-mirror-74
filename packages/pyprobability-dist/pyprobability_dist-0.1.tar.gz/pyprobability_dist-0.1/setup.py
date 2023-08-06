9a29d66edb1e78e0dbb50ecf50b4b6c02786aff7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:09 +0000

    Add package pyprobability-dist/0.1/pyprobability_dist-0.1.tar.gz

---
+++ packages/pyprobability-dist/pyprobability_dist-0.1.tar.gz/pyprobability_dist-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='pyprobability_dist',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['pyprobability_dist'],
+      zip_safe=False)
