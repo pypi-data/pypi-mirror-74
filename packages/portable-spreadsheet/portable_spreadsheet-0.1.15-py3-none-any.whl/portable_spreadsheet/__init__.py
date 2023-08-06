52de74f1bf1f40666b9a6955c4469dad8d395b83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:46 +0000

    Add package portable-spreadsheet/0.1.15/portable_spreadsheet-0.1.15-py3-none-any.whl

---
+++ packages/portable-spreadsheet/portable_spreadsheet-0.1.15-py3-none-any.whl/portable_spreadsheet/__init__.py
@@ -0,0 +1,9 @@
+from .spreadsheet import Spreadsheet  # noqa
+from .cell import Cell  # noqa
+from .cell_indices import CellIndices  # noqa
+from .cell_slice import CellSlice  # noqa
+from .grammars import GRAMMARS  # noqa
+from .grammar_utils import GrammarUtils  # noqa
+
+__version__ = "0.1.15"
+__status__ = "Production"
