0c3affbea983eeabf8ce652541c3065d54e73e57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:52 +0000

    Add package emml-prob-func/1.0/emml_prob_func-1.0.tar.gz

---
+++ packages/emml-prob-func/emml_prob_func-1.0.tar.gz/emml_prob_func-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='emml_prob_func',
+      version='1.0',
+      description='Gaussian distributions',
+      packages=['emml_prob_func'],
+      author='Emmanuel Fiagbedzi',
+      author_email='ekfiagbedzi@gmail.com',
+      zip_safe=False)
