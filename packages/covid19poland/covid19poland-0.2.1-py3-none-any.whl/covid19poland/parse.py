37e11eafd225284b1a95c880a7c8ae95acdbac37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:00 +0000

    Add package covid19poland/0.2.1/covid19poland-0.2.1-py3-none-any.whl

---
+++ packages/covid19poland/covid19poland-0.2.1-py3-none-any.whl/covid19poland/parse.py
@@ -0,0 +1,36 @@
+
+import warnings
+
+from bs4 import BeautifulSoup
+import pandas as pd
+
+from . import PLwiki
+from . import PLtwitter
+
+
+def wiki(level = 1, dt = None):
+    x = PLwiki.get_wiki(level = level, dt = dt)
+    return x
+
+def twitter(start = None, end = None, keys = ['deaths']):
+    """Parses tweets of Polish Ministry of Health and returns numbers.
+    
+    Args:
+        start (datetime,optional): Start date of the tweets to parse (inclusive).
+        end (datetime, optional): End date of the tweets to parse (exclusive).
+        keys (list, optional): List of string keys listing the output items. Available are
+            * daily = daily reports (must be manually parsed)
+            * deaths = new death announcements
+            * tests = cumulative number of performed tests (in thousands)
+            * cases = new confirmed cases
+            * cumulative = cumulative number of confirmed cases
+            * regions = regional new confirmed cases
+    Returns:
+        (dict): parsed data with linked sources (tweet URLs)
+        (dict): ignored tweets
+        (list): dates to check manually (partial errors)
+    """
+    results = PLtwitter.PolishTwitter.get(start = start, end = end, keys = keys)
+    return results
+
+__all__ = ["wiki", "twitter"]
