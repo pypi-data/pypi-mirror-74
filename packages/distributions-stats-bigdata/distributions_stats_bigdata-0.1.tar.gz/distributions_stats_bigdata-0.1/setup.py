d753966bac14d85a91b201948f39cbdc2b8a6f35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:09 +0000

    Add package distributions-stats-bigdata/0.1/distributions_stats_bigdata-0.1.tar.gz

---
+++ packages/distributions-stats-bigdata/distributions_stats_bigdata-0.1.tar.gz/distributions_stats_bigdata-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_stats_bigdata',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions_stats_bigdata'],
+      author = 'Jay Patel',
+      author_email = 'jay.00174@live.com',
+      zip_safe=False)
