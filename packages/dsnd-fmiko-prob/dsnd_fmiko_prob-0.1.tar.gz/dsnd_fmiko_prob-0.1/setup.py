6f9538b12a43665bbb7d24b8d75e2cdaf833a7cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:32 +0000

    Add package dsnd-fmiko-prob/0.1/dsnd_fmiko_prob-0.1.tar.gz

---
+++ packages/dsnd-fmiko-prob/dsnd_fmiko_prob-0.1.tar.gz/dsnd_fmiko_prob-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='dsnd_fmiko_prob',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['dsnd_fmiko_prob'],
+      zip_safe=False)
