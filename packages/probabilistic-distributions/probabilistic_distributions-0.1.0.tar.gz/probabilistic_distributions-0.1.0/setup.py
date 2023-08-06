41c467d348686a9872cf9694bc26e0806e26f135
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:34 +0000

    Add package probabilistic-distributions/0.1.0/probabilistic_distributions-0.1.0.tar.gz

---
+++ packages/probabilistic-distributions/probabilistic_distributions-0.1.0.tar.gz/probabilistic_distributions-0.1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='probabilistic_distributions',
+      version='0.1.0',
+      description='Probability discrete continous distributions',
+      packages=['probabilistic_distributions'],
+      author='Harsha S',
+      zip_safe=False)
