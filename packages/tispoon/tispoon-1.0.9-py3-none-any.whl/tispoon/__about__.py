da434a43369c01a3ec1b754b3b7c4d94b0fa3c49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:46 +0000

    Add package tispoon/1.0.9/tispoon-1.0.9-py3-none-any.whl

---
+++ packages/tispoon/tispoon-1.0.9-py3-none-any.whl/tispoon/__about__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright (c) 2020 Xvezda <xvezda@naver.com>
+#
+# Use of this source code is governed by an MIT-style
+# license that can be found in the LICENSE file or at
+# https://opensource.org/licenses/MIT.
+
+__title__ = "tispoon"
+__author__ = "Xvezda"
+__email__ = "xvezda@naver.com"
+__version__ = "1.0.9"
