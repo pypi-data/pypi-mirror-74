4a8b197cdcd19a3b8a872100edda595942fa77d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:42 +0000

    Add package sofconizer/1.0.6/sofconizer-1.0.6.tar.gz

---
+++ packages/sofconizer/sofconizer-1.0.6.tar.gz/sofconizer-1.0.6/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['sofconizer']
+
+package_data = \
+{'': ['*']}
+
+entry_points = \
+{'console_scripts': ['teslax = entry:main']}
+
+setup_kwargs = {
+    'name': 'sofconizer',
+    'version': '1.0.6',
+    'description': '',
+    'long_description': None,
+    'author': 'Deniansyah',
+    'author_email': '5lineofcode@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'entry_points': entry_points,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
