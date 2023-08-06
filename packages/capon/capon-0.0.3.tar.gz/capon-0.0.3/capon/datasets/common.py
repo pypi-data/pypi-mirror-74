c028ef23ec1ec6c77b2aa8a5c1372e9000142ee7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:06 +0000

    Add package capon/0.0.3/capon-0.0.3.tar.gz

---
+++ packages/capon/capon-0.0.3.tar.gz/capon-0.0.3/capon/datasets/common.py
@@ -0,0 +1,11 @@
+import logging
+import os
+import glob
+
+
+RESOURCES_ROOT_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), '../resources'))
+logging.info(f'RESOURCES_ROOT_PATH = "{RESOURCES_ROOT_PATH}"')
+
+
+def get_resource_path(path):
+    return os.path.normpath(f'{RESOURCES_ROOT_PATH}/{path}')
