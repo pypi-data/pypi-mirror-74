db5db0fd63e629e7cf0e566ccf2b2a93262611ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:40 +0000

    Add package lung/0.0.6/lung-0.0.6.tar.gz

---
+++ packages/lung/lung-0.0.6.tar.gz/lung-0.0.6/setup.py
@@ -0,0 +1,35 @@
+import os
+
+from setuptools import find_packages
+from setuptools import setup
+
+with open(
+    os.path.join(os.path.abspath(os.path.dirname(__file__)), "README.md"), encoding="utf-8",
+) as f:
+    long_description = f.read()
+
+setup(
+    name="lung",
+    version="0.0.6",
+    author="MinRegret",
+    author_email="minregret@minimizingregret.com",
+    url="https://github.com/MinRegret/Ventilator-Dev",
+    description="Lung simulation",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=[
+        "numpy",
+        "pandas",
+        "h5py",
+        "timecast"
+    ],
+    extras_require={
+        "dev": [
+            "bumpversion",
+            "pre-commit",
+            "ipython",
+            "jupyter"
+        ]
+    },
+    packages=find_packages()
+)
