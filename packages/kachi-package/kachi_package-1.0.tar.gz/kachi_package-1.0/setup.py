e6bc399bf401901f5a5c20da73f56166a8768fb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:06 +0000

    Add package kachi-package/1.0/kachi_package-1.0.tar.gz

---
+++ packages/kachi-package/kachi_package-1.0.tar.gz/kachi_package-1.0/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='kachi_package',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions'],
+      zip_safe=False)
