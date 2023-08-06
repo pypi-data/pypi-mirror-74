d9c2eac5f9eb646e11879b0765f7454473087451
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:37 +0000

    Add package prblt-distributions/0.1/prblt_distributions-0.1.tar.gz

---
+++ packages/prblt-distributions/prblt_distributions-0.1.tar.gz/prblt_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='prblt_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['prblt_distributions'],
+      zip_safe=False)
