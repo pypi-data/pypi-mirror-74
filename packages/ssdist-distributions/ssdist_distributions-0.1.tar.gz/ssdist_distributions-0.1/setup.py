3868504c15fdeb33616d61db10f61f5095d39698
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:54 +0000

    Add package ssdist-distributions/0.1/ssdist_distributions-0.1.tar.gz

---
+++ packages/ssdist-distributions/ssdist_distributions-0.1.tar.gz/ssdist_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='ssdist_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['ssdist_distributions'],
+      zip_safe=False)
