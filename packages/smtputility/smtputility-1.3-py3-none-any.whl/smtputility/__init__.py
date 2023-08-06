c41c5314b252c05aa907ee3c373cbf892255fb9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:48 +0000

    Add package smtputility/1.3/smtputility-1.3-py3-none-any.whl

---
+++ packages/smtputility/smtputility-1.3-py3-none-any.whl/smtputility/__init__.py
@@ -0,0 +1,6 @@
+from .email_without_attachment import email_without_attachment
+from .email_with_attachments import email_with_attachments
+from .notify_error import notify_error
+from .default_table_style import default_table_style
+# Version of the package
+__version__ = "1.3"
