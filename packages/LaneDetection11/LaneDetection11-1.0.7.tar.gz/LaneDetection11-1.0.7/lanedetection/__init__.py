8f5302d4aa1dbe77eb06ea4776365e4fce7296ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:54 +0000

    Add package LaneDetection11/1.0.7/LaneDetection11-1.0.7.tar.gz

---
+++ packages/LaneDetection11/LaneDetection11-1.0.7.tar.gz/LaneDetection11-1.0.7/lanedetection/__init__.py
@@ -0,0 +1,4 @@
+from packaging_tutorial.lanedetection import model
+from packaging_tutorial.lanedetection import utils
+from packaging_tutorial.lanedetection.Config import *
+from packaging_tutorial.lanedetection.inference import *
---
+++ packages/LaneDetection11/LaneDetection11-1.0.7.tar.gz/LaneDetection11-1.0.7/lanedetection/test.py
@@ -0,0 +1,5 @@
+from packaging_tutorial.lanedetection.model import test1
+
+def cc(x,y):
+    a = test1.bb(x,y)
+    print(a)
