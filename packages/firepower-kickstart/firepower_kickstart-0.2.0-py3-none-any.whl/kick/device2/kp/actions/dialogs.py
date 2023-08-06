17c26f5f379cd7d68d888aba0a54d4abc325ea40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:36 +0000

    Add package firepower-kickstart/0.2.0/firepower_kickstart-0.2.0-py3-none-any.whl

---
+++ packages/firepower-kickstart/firepower_kickstart-0.2.0-py3-none-any.whl/kick/device2/kp/actions/dialogs.py
@@ -0,0 +1,16 @@
+from unicon.eal.dialogs import Dialog
+from kick.device2.ftd5500x.actions.dialogs import Ftd5500xDialog
+
+
+class KpDialogs:
+    # Dialogs for state transitions
+    def __init__(self, patterns):
+        """Initializer of KpDialogs."""
+        self.patterns = patterns
+        self.d_expert_to_sudo = Dialog([[self.patterns.prompt.password_prompt,
+                                         'sendline({})'.format(self.patterns.sudo_password),
+                                         None, True, True], ])
+        self.d_ftd_to_fxos = Dialog([
+            ["Please enter 'exit' to go back", 'sendline(exit)', None, True, False], ])
+
+        self.ftd_dialogs = Ftd5500xDialog(patterns)
