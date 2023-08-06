8e40e2f5e37419c848b79f53276141d77d108988
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:58 +0000

    Add package synapse-downloader/0.0.3/synapse-downloader-0.0.3.tar.gz

---
+++ packages/synapse-downloader/synapse-downloader-0.0.3.tar.gz/synapse-downloader-0.0.3/setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="synapse-downloader",
+    version="0.0.3",
+    author="Patrick Stout",
+    author_email="pstout@prevagroup.com",
+    license="Apache2",
+    description="Utility for downloading large datasets from Synapse.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ki-tools/synapse-downloader",
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    classifiers=(
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ),
+    entry_points={
+        'console_scripts': [
+            "synapse-downloader = synapse_downloader.cli:main"
+        ]
+    },
+    install_requires=[
+        "synapseclient>=2.1.0,<3.0.0",
+        "aiohttp",
+        "aiofiles"
+    ]
+)
