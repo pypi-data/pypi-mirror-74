b01ad7bf661d45202fcb361d494e54d4e05ecbba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:33 +0000

    Add package robot-speech-to-text/0.0.1/robot_speech_to_text-0.0.1.tar.gz

---
+++ packages/robot-speech-to-text/robot_speech_to_text-0.0.1.tar.gz/robot_speech_to_text-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='robot_speech_to_text',
+    version='0.0.1',
+    packages=find_packages(),
+    install_requires=[
+        'SpeechRecognition'
+    ]
+)
