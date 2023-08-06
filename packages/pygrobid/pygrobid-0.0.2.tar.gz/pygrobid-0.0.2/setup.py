d11388d43dd626fd6e6e0edc0a94b773183b4c04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:14:07 +0000

    Add package pygrobid/0.0.2/pygrobid-0.0.2.tar.gz

---
+++ packages/pygrobid/pygrobid-0.0.2.tar.gz/pygrobid-0.0.2/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pygrobid",
+    version="0.0.2",
+    author="Samuel.Wu",
+    author_email="samuel.yh.wu@gmail.com",
+    description="A python client for Grobid service",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/wyh/pygrobid",
+    packages=setuptools.find_packages(),
+    install_requires=['requests'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    license='LICENSE',
+    python_requires='>=3.6',
+)
