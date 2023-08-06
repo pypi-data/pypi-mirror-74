c395195d8b48ed189bb6e874e3dec217f2c47635
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:36 +0000

    Add package widgetastic.patternfly4/0.17.0/widgetastic.patternfly4-0.17.0.tar.gz

---
+++ packages/widgetastic.patternfly4/widgetastic.patternfly4-0.17.0.tar.gz/widgetastic.patternfly4-0.17.0/testing/test_button.py
@@ -0,0 +1,15 @@
+from widgetastic.widget import View
+
+from widgetastic_patternfly4 import Button
+
+
+def test_button_click(browser):
+    class TestView(View):
+        any_button = Button()
+        button1 = Button("Primary")
+        button2 = Button("Link to core docs")
+
+    view = TestView(browser)
+    assert view.any_button.is_displayed
+    assert view.button1.is_displayed
+    assert view.button2.is_displayed
