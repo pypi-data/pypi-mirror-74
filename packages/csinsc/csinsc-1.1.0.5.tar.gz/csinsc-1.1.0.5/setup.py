21ce08f1afe56e29f98927b9eecf7f7f0e0ab422
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:04 +0000

    Add package csinsc/1.1.0.5/csinsc-1.1.0.5.tar.gz

---
+++ packages/csinsc/csinsc-1.1.0.5.tar.gz/csinsc-1.1.0.5/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="csinsc",
+    version="1.1.0.5",
+    author="Toan Huynh",
+    author_email="toan@csinschools.io",
+    description="Tools and libraries used in the CSinSchools course",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/toanh/csinsc",
+    packages=setuptools.find_packages(),
+    install_requires=[
+              'pytz',
+              'requests',
+              'bs4'
+          ],    
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+)
