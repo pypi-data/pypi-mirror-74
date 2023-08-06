0e0beb5cd28aa5060429b7f56a6e47fa4e111acd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:03 +0000

    Add package pytrips/0.5.22/pytrips-0.5.22.tar.gz

---
+++ packages/pytrips/pytrips-0.5.22.tar.gz/pytrips-0.5.22/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+from setuptools.command.install import install as _install
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+extras = {
+    "tools": [
+        'spacy>=2.0.0,<3.0.0',
+        'en_core_web_lg==2.1.0',
+        ]
+    }
+
+base = [
+        "nltk",
+        "thesoulshell>=0.0.2",
+        "jsontrips>=0.1.34",
+        "graphviz",
+        "anytree",
+    ]
+
+if __name__ == '__main__':
+    setup(
+        name="pytrips",
+        version="0.5.22",
+        author="Rik Bose",
+        author_email="rbose@cs.rochester.edu",
+        description="A simple python package for accessing the trips ontology and lexicon",
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        url="https://github.com/mrmechko/pytrips",
+        packages=find_packages(exclude=["test"]),
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)",
+            "Operating System :: OS Independent",
+        ],
+        install_requires=base,
+        extras_require=extras,
+    )
