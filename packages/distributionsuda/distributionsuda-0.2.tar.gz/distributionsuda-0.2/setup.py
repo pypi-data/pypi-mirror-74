61c617923070c51502d4f569da9803e982781181
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:48 +0000

    Add package distributionsuda/0.2/distributionsuda-0.2.tar.gz

---
+++ packages/distributionsuda/distributionsuda-0.2.tar.gz/distributionsuda-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributionsuda',
+      version='0.2',
+      description='Gaussian distributions',
+      packages=['distributionsuda'],
+      author = ' Morhaf Jaela',
+      author_email = 'morhaf.jely@gmail.com',
+      zip_safe=False)
