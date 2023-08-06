6592b41202c4b7ffd1e93eed4f983e97baf1fb15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:08 +0000

    Add package invenio-records-resources/0.1.0/invenio-records-resources-0.1.0.tar.gz

---
+++ packages/invenio-records-resources/invenio-records-resources-0.1.0.tar.gz/invenio-records-resources-0.1.0/invenio_records_resources/resources/record.py
@@ -0,0 +1,102 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2020 CERN.
+# Copyright (C) 2020 Northwestern University.
+#
+# Invenio-Records-Resources is free software; you can redistribute it and/or
+# modify it under the terms of the MIT License; see LICENSE file for more
+# details.
+
+"""Invenio Resources module to create REST APIs."""
+
+from flask import g
+from flask_resources import CollectionResource
+from flask_resources.context import resource_requestctx
+from flask_resources.loaders import RequestLoader
+from flask_resources.resources import ResourceConfig
+
+from ..responses import RecordResponse
+from ..schemas import RecordSchemaJSONV1
+from ..serializers import RecordJSONSerializer
+from ..services import RecordService
+
+
+class RecordResourceConfig(ResourceConfig):
+    """Record resource config."""
+
+    item_route = "/records/<pid_value>"
+    list_route = "/records"
+    response_handlers = {
+        "application/json": RecordResponse(
+            RecordJSONSerializer(schema=RecordSchemaJSONV1)
+        )
+    }
+
+
+class RecordResource(CollectionResource):
+    """Record resource."""
+
+    default_config = RecordResourceConfig
+
+    def __init__(self, service=None, *args, **kwargs):
+        """Constructor."""
+        super(RecordResource, self).__init__(*args, **kwargs)
+        self.service = service or RecordService()
+
+    #
+    # Primary Interface
+    #
+    def search(self, *args, **kwargs):
+        """Perform a search over the items."""
+        identity = g.identity
+        record_search = self.service.search(
+            querystring=resource_requestctx.request_args.get("q", ""),
+            identity=identity,
+            pagination=resource_requestctx.request_args.get("pagination"),
+        )
+
+        return record_search, 200
+
+    def create(self, *args, **kwargs):
+        """Create an item."""
+        data = resource_requestctx.request_content
+        identity = g.identity
+        return self.service.create(data, identity), 200
+
+    def read(self, *args, **kwargs):
+        """Read an item."""
+        identity = g.identity
+        return (
+            self.service.read(
+                id_=resource_requestctx.route["pid_value"], identity=identity
+            ),
+            200,
+        )
+
+    def update(self, *args, **kwargs):
+        """Update an item."""
+        data = resource_requestctx.request_content
+        identity = g.identity
+        return (
+            self.service.update(
+                id_=resource_requestctx.route["pid_value"],
+                data=data,
+                identity=identity
+            ),
+            200,
+        )
+
+    def partial_update(self, *args, **kwargs):
+        """Patch an item."""
+        # TODO
+        pass
+
+    def delete(self, *args, **kwargs):
+        """Delete an item."""
+        identity = g.identity
+        return (
+            self.service.delete(
+                id_=resource_requestctx.route["pid_value"], identity=identity
+            ),
+            204,
+        )
---
+++ packages/invenio-records-resources/invenio-records-resources-0.1.0.tar.gz/invenio-records-resources-0.1.0/tests/test_record_resource.py
@@ -0,0 +1,131 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2020 CERN.
+# Copyright (C) 2020 Northwestern University.
+#
+# Invenio-Records-Resources is free software; you can redistribute it and/or
+# modify it under the terms of the MIT License; see LICENSE file for more
+# details.
+
+"""Invenio Resources module to create REST APIs"""
+
+import json
+
+import pytest
+from flask import url_for
+from invenio_accounts.testutils import login_user_via_view
+
+from invenio_records_resources.services.errors import PermissionDeniedError
+
+HEADERS = {"content-type": "application/json", "accept": "application/json"}
+
+
+def test_create_read_record(client, input_record):
+    """Test record creation."""
+    # Create new record
+    response = client.post(
+        "/records", headers=HEADERS, data=json.dumps(input_record)
+    )
+    assert response.status_code == 200
+    response_fields = response.json.keys()
+    fields_to_check = ['pid', 'metadata', 'revision',
+                       'created', 'updated', 'links']
+    for field in fields_to_check:
+        assert field in response_fields
+
+    # Save record pid for posterior operations
+    recid = response.json["pid"]
+
+    # Read the record
+    response = client.get("/records/{}".format(recid), headers=HEADERS)
+    assert response.status_code == 200
+    assert response.json["pid"] == recid  # Check it matches with the original
+
+    response_fields = response.json.keys()
+    fields_to_check = ['pid', 'metadata', 'revision',
+                       'created', 'updated', 'links']
+    for field in fields_to_check:
+        assert field in response_fields
+
+
+# This test DOES NOT clean after itself. It works every time on CI,
+# because the CI creates a new container for each test run. It will work
+# once locally, but fail on subsequent run.
+# TODO: FIX to make it clean up itself. Adding es_clear didn't work
+@pytest.mark.skip(reason="Fix to make it clean up itself")
+def test_create_search_record(client, input_record):
+    """Test record search."""
+    # Search records, should return empty
+    response = client.get("/records", headers=HEADERS)
+    assert response.status_code == 200
+
+    # Create dummy record to test search
+    response = client.post(
+        "/records", headers=HEADERS, data=json.dumps(input_record)
+    )
+    assert response.status_code == 200
+    print("response.json", response.json)
+
+    # Search content of record, should return the record
+    response = client.get("/records?q=story", headers=HEADERS)
+    assert response.status_code == 200
+
+    # Search for something non-existent, should return empty
+    response = client.get("/records?q=notfound", headers=HEADERS)
+    assert response.status_code == 200
+
+
+def test_create_delete_record(client, input_record):
+    """Test record deletion."""
+    # Create dummy record to test delete
+    response = client.post(
+        "/records", headers=HEADERS, data=json.dumps(input_record)
+    )
+    assert response.status_code == 200
+    recid = response.json["pid"]
+
+    # Update the record
+    updated_record = input_record
+    updated_record["title"] = "updated title"
+    response = client.put("/records/{}".format(recid), headers=HEADERS,
+                          data=json.dumps(updated_record))
+    assert response.status_code == 200
+
+    # Delete the record
+    response = client.delete("/records/{}".format(recid),
+                             headers=HEADERS)
+    assert response.status_code == 204
+
+
+def test_create_update_record(client, input_record):
+    """Test record update."""
+    # Create dummy record to test update
+    response = client.post(
+        "/records", headers=HEADERS, data=json.dumps(input_record)
+    )
+    assert response.status_code == 200
+    recid = response.json["pid"]
+
+    # Update the record
+    new_title = "updated title"
+    input_record["title"] = new_title
+    response = client.put("/records/{}".format(recid), headers=HEADERS,
+                          data=json.dumps(input_record))
+    assert response.status_code == 200
+
+    # Read the record
+    response = client.get("/records/{}".format(recid), headers=HEADERS)
+    assert response.status_code == 200
+    assert response.json["metadata"]["title"] == new_title
+
+
+# # FIXME: Currently throws exception. Error handling needed
+# def test_create_invalid_record(client, input_record):
+#     """Test invalid record creation."""
+#     input_record.pop("title")  # Remove a required field of the record
+
+#     response = client.post(
+#         "/records", headers=HEADERS, data=json.dumps(input_record)
+#     )
+
+#     # TODO: Assert
