972ce4fff4bd5607fefc17a5f709d76e56db2c28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:16 +0000

    Add package Avatar-Utils/1.2.2/Avatar-Utils-1.2.2.tar.gz

---
+++ packages/Avatar-Utils/Avatar-Utils-1.2.2.tar.gz/Avatar-Utils-1.2.2/setup.py
@@ -0,0 +1,47 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Avatar-Utils",
+    version="1.2.2",
+    author="Algorithmics of Complex System",
+    author_email="artem.sementsov@gmail.com",
+    description="Common utils for services in ecosystem",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=[
+        'avatar_utils.async_requests',
+        'avatar_utils.core',
+        'avatar_utils.crawler',
+        'avatar_utils.db',
+        'avatar_utils.learn_scheduler',
+        'avatar_utils.sso_helper',
+        'avatar_utils.logs',
+        'avatar_utils.registration',
+        'avatar_utils.tests',
+        'avatar_utils.validation',
+    ],
+    install_requires=[
+        'aiohttp>=3.6.2',
+        'flask>=1.1.1',
+        'flask-sqlalchemy>=2.3.2',
+        'requests>=2.23.0',
+        'psycopg2-binary>=2.7.5',
+        'apispec>=3.3.0',
+        'marshmallow>=3.6.0',
+        'flasgger>=0.9.4',
+        'APScheduler~=3.6.3',
+        'python-keycloak~=0.21.0',
+        'cryptography~=3.0',
+        'PyJWT~=1.7.1',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
