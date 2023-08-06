ba87179242165d89f8cf7834533facc5443f1035
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:08 +0000

    Add package rpcompletion/1.0.5/rpcompletion-1.0.5-py3-none-any.whl

---
+++ packages/rpcompletion/rpcompletion-1.0.5-py3-none-any.whl/rpcompletion/__init__.py
@@ -0,0 +1,10 @@
+"""
+Created on June 16 2020
+
+@author: Joan Hérisson
+"""
+
+from .rpCompletion import rpCompletion, build_args_parser
+from .rpCache import rpCache
+
+__all__ = ["rpCompletion", "build_args_parser"]
