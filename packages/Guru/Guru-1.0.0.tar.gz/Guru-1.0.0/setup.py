96a3365c6a7acefb53a08e032b3c5b4a43da408b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:29 +0000

    Add package Guru/1.0.0/Guru-1.0.0.tar.gz

---
+++ packages/Guru/Guru-1.0.0.tar.gz/Guru-1.0.0/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+setuptools.setup(
+    name="Guru",
+    version="1.0.0",
+    author="Guru",
+    author_email="g.s.a.999@gmail.com",
+    description="packt example package",
+    long_description="This is the longer description and will appear in the web.",
+    package=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
