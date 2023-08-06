042ea8a26abf037010160a08214e581b800f6178
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:04 +0000

    Add package autopaths/1.4.4/autopaths-1.4.4.tar.gz

---
+++ packages/autopaths/autopaths-1.4.4.tar.gz/autopaths-1.4.4/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+        name             = 'autopaths',
+        version          = '1.4.4',
+        description      = 'autopaths is a python package for dealing with file paths and automation.',
+        license          = 'MIT',
+        url              = 'http://github.com/xapple/autopaths/',
+        author           = 'Lucas Sinclair',
+        author_email     = 'lucas.sinclair@me.com',
+        packages         = find_packages(),
+        install_requires = ['six'],
+        long_description = open('README.md').read(),
+        long_description_content_type = 'text/markdown',
+        include_package_data = True,
+)
