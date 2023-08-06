a2a27d92ea91e9e8f9ab8e0031a4fbd3cb1750b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:55 +0000

    Add package pmagpy/4.2.48/pmagpy-4.2.48-py3-none-any.whl

---
+++ packages/pmagpy/pmagpy-4.2.48-py3-none-any.whl/pmagpy/version.py
@@ -0,0 +1,8 @@
+"""
+Module contains current pmagpy version number.
+Version number is displayed by GUIs
+and used by setuptools to assign number to pmagpy/pmagpy-cli.
+"""
+
+"pmagpy-4.2.48"
+version = 'pmagpy-4.2.48'
