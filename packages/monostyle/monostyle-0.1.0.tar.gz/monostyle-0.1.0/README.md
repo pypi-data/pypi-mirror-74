a28b762b27219b631c8b826b805c7e28ca26b87b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:51 +0000

    Add package monostyle/0.1.0/monostyle-0.1.0.tar.gz

---
+++ packages/monostyle/monostyle-0.1.0.tar.gz/monostyle-0.1.0/README.md
@@ -0,0 +1,84 @@
+
+# Monostyle
+
+[![PyPI version shields.io](https://img.shields.io/pypi/v/monostyle.svg)](https://pypi.python.org/pypi/monostyle/)
+[![GPLv3 license](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://github.com/tobiasHeinke/monostyle/blob/master/LICENSE)
+
+Monostyle is a framework for documentation style checking and linting covering all parts of a style guide.
+
+Its tools are applied on new or changed content
+which makes it fast and it allows a wider range of tools,
+because they can have false positives. 
+For example style guide rules that have exceptions which can't be filtered out.
+
+Monostyle is build as a framework and not as a ready-made tool
+because the tools require customization like markup filtering or you might not want to use Chicago style title case. 
+It includes its own RST parser and a port of the [Reflow line wrapper](https://metacpan.org/pod/Text::Reflow).
+Its utilities can also be used for text editing with scripts.
+
+
+## Setup
+
+Requirements: The SVN command line client tools need to be installed.
+
+For spell checking a dictionary has to build.
+On first run you have to confirm to start this (or also if the dictionary file is not found).
+
+The dictionary has to be updated from time to time or after a bigger portion of content has been added
+to include new words (of cause the whole project has to be spell-checked).
+To do this run the `spelling` script:
+```sh
+python -m monostyle.spelling
+```
+
+## Running Monostyle
+
+Monostyle has four modes:
+
+<dl>
+  <dt>internal -i</dt>
+  <dd>To check your own changes (the default).</dd>
+  <dt>external -e</dt>
+  <dd>
+      To check changes made to the repository by others.
+      Run this before you update your working copy with SVN.
+   </dd>
+  <dt>patch -p</dt>
+  <dd>To check changes in a patch-file.</dd>
+  <dt>file -f</dt>
+  <dd>To check a file or directory.</dd>
+  <dt>root -r</dt>
+  <dd>
+      The root is the absolute path to the project directory or where the patch file was created.
+      If not set the directory where Monostyle is run from is used as the root.
+  </dd>
+</dl>
+
+Post processing:
+
+<dl>
+  <dt>update -u</dt>
+  <dd>Update the working copy.</dd>
+  <dt>autofix -a</dt>
+  <dd>Apply autofixes. This also does an update if the changes are external.</dd>
+  <dt>open -o</dt>
+  <dd>Open a file in a text editor if the report has a severity higher than specified.
+      Please check if your editor of choice is available (else please make a I/PR to add it).
+  </dd>
+</dl>
+
+For more info on command line arguments use the `--help` command.
+The tools can be applied individually by executing the script files. Then the tools will loop over the whole project.
+
+Running Monostyle does not replace building the project with Sphinx.
+Markup errors can lead to false negatives, so Monostyle has to be run again or
+the affected section has to be checked manually.
+
+
+### Advanced
+
+You can set a revision for internal, external and update.
+It can be colon separated or dash separated for the "change" syntax.
+When a side is kept empty e.g. ":ARG" it will default to BASE (your working copy) on the left and
+on the right to HEAD (the latest revision in the repository).
+External revision uses the "change" syntax for single arguments "ARG".
---
+++ packages/monostyle/monostyle-0.1.0.tar.gz/monostyle-0.1.0/monostyle/char.py
@@ -0,0 +1,160 @@
+
+"""
+char
+~~~~
+
+Tools on char level and encoding.
+"""
+
+import os
+import re
+
+import monostyle.util.monostylestd as monostylestd
+from monostyle.util.report import Report, print_reports
+from monostyle.util.fragment import Fragment
+from monostyle.rst_parser.core import RSTParser
+from monostyle.util.char_catalog import CharCatalog
+
+CharCatalog = CharCatalog()
+
+
+def char_search(document, reports):
+    """All chars outside of the defined Unicode region or explicit search."""
+    toolname = "char-search"
+
+    text = str(document.code)
+    chars = (
+        (r"\uFFFD", "replace char"),
+        (r"\t", "tab"),
+        (CharCatalog.data["quote"]["initial"], "initial quote"),
+        (CharCatalog.data["quote"]["final"], "final quote"),
+        (r"–", "en-dash"),
+        (r"—", "em-dash"),
+    )
+    explicits = ""
+    for pattern, msg in chars:
+        char_re = re.compile(r"[" + pattern + r"]")
+        explicits += pattern
+        for char_m in re.finditer(char_re, text):
+            out = document.code.slice_match_obj(char_m, 0, True)
+            reports.append(Report('E', toolname, out, msg))
+
+    parttern_str = r"[^\n -~À-ʨ" + ''.join(('©', '®', '°', '±', '€', '™')) + explicits +  r"]"
+    char_re = re.compile(parttern_str)
+    for char_m in re.finditer(char_re, text):
+        msg = "uncommon char: {0}, 0x{0:04x}".format(ord(char_m.group(0)))
+        out = document.code.slice_match_obj(char_m, 0, True)
+        reports.append(Report('E', toolname, out, msg))
+
+    return reports
+
+
+def file_encoding():
+    """Check text encoding."""
+    toolname = "file-encoding"
+
+    reports = []
+    # standard Unicode replace char <?>
+    repchar_re = re.compile("\uFFFD")
+    # for fn in monostylestd.po_files():
+    for fn in monostylestd.rst_files():
+        with open(fn, "r", encoding="utf-8", errors="replace") as f:
+            try:
+                text = f.read()
+
+            except UnicodeEncodeError as err:
+                out = Fragment(fn, "")
+                msg = "encode error: " + str(err)
+                reports.append(Report('E', toolname, out, msg))
+
+            except:
+                out = Fragment(fn, "")
+                msg = "unknown encode error"
+                reports.append(Report('E', toolname, out, msg))
+
+            else:
+                document_fg = Fragment(fn, text)
+                for repchar_m in re.finditer(repchar_re, text):
+                    out = document_fg.slice_match_obj(repchar_m, 0, True)
+                    msg = "unsupported character"
+                    reports.append(Report('E', toolname, out, msg))
+
+    return reports
+
+
+def init(op_names):
+    ops = []
+    for op in OPS:
+        if op[0] in op_names:
+            ops.append((op[1], {}))
+
+    return ops
+
+
+def hub(op_names):
+    rst_parser = RSTParser()
+    reports = []
+    ops = init(op_names)
+
+    for fn, text in monostylestd.rst_texts():
+        document = rst_parser.document(fn, text)
+
+        for op in ops:
+            reports = op[0](document, reports)
+
+    return reports
+
+
+OPS = (
+    ("char-search", char_search),
+    ("encoding", file_encoding)
+)
+
+def main():
+    import argparse
+    from monostyle import setup
+
+    descr = __doc__.replace('~', '')
+    parser = argparse.ArgumentParser(description=descr)
+    for op in OPS:
+        doc_str = ''
+        if op[1].__doc__ is not None:
+            # first char to lowercase
+            doc_str = op[1].__doc__[0].lower() + op[1].__doc__[1:]
+        parser.add_argument("--" + op[0], dest="op_names",
+                            action='store_const', const=op[0], metavar="",
+                            help=doc_str)
+
+    parser.add_argument("-r", "--root",
+                        dest="root", nargs='?', const="",
+                        help="defines the ROOT directory of the project")
+
+    args = parser.parse_args()
+
+    if args.root is None:
+        root_dir = os.getcwd()
+    else:
+        root_dir = os.path.normpath(args.root)
+
+        if not os.path.exists(root_dir):
+            print('Error: root {0} does not exists'.format(args.root))
+            return 2
+
+    root_dir = monostylestd.replace_windows_path_sep(root_dir)
+    monostylestd.ROOT_DIR = root_dir
+
+    setup_sucess = setup(root_dir)
+    if not setup_sucess:
+        return 2
+
+    if args.op_names == "encoding":
+        for op in OPS:
+            if op[0] == args.op_names:
+                reports = op[1]()
+    else:
+        reports = hub(args.op_names)
+
+    print_reports(reports)
+
+if __name__ == "__main__":
+    main()
