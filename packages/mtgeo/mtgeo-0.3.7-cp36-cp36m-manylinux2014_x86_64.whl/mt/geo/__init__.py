a80ec9518ea1cd63aea3b13e943abe82d60d3251
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:39 +0000

    Add package mtgeo/0.3.7/mtgeo-0.3.7-cp36-cp36m-manylinux2014_x86_64.whl

---
+++ packages/mtgeo/mtgeo-0.3.7-cp36-cp36m-manylinux2014_x86_64.whl/mt/geo/__init__.py
@@ -0,0 +1,12 @@
+from .approximation import approx, approximable
+
+from .rect import rect
+from .affine_transformation import *
+from .dilated_isometry import *
+from .isometry import *
+from .dilatation import *
+from .similarity2d import *
+from .ellipse import ellipse
+#from .linear2d import lin2
+
+# TODO: doctest for each of the classes above
