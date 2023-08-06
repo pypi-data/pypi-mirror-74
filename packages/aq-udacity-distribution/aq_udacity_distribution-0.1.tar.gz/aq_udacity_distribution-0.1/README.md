73a093e812292054c829d4b899485dd633de6d41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:00 +0000

    Add package aq-udacity-distribution/0.1/aq_udacity_distribution-0.1.tar.gz

---
+++ packages/aq-udacity-distribution/aq_udacity_distribution-0.1.tar.gz/aq_udacity_distribution-0.1/README.md
@@ -0,0 +1,8 @@
+# Andis First PyPI Project
+![Python Logo](https://www.python.org/static/community_logos/python-logo.png "Sample inline image")
+
+
+----
+
+This is the README file for the project.
+
---
+++ packages/aq-udacity-distribution/aq_udacity_distribution-0.1.tar.gz/aq_udacity_distribution-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='aq_udacity_distribution',
+      version='0.1',
+      description='Gaussian & Binomial Distributions',
+      packages=['aq_udacity_distribution'],
+      zip_safe=False)
