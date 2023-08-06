52a1c400b396c2288f597d4ab70a1c4899ec70e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:19 +0000

    Add package AWS-Matope-Sithole-distributions/0.1/AWS_Matope_Sithole_distributions-0.1.tar.gz

---
+++ packages/AWS-Matope-Sithole-distributions/AWS_Matope_Sithole_distributions-0.1.tar.gz/AWS_Matope_Sithole_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='AWS_Matope_Sithole_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['AWS_Matope_Sithole_distributions'],
+      author = 'Matope Sithole',
+      author_email = 'matopesithole@gmail.com',
+      zip_safe=False)
