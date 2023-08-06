04aedcac0ab5095f3aafe3e75977a4d52f1f5a1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:59 +0000

    Add package dsnd-abdul/0.1/dsnd-abdul-0.1.tar.gz

---
+++ packages/dsnd-abdul/dsnd-abdul-0.1.tar.gz/dsnd-abdul-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='dsnd-abdul',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['dsnd-abdul'],
+      author='abdul',
+      zip_safe=False)
