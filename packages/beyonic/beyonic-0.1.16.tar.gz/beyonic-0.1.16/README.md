22b7209cec287c11c783da8710e69df2378eece5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:16 +0000

    Add package beyonic/0.1.16/beyonic-0.1.16.tar.gz

---
+++ packages/beyonic/beyonic-0.1.16.tar.gz/beyonic-0.1.16/README.md
@@ -0,0 +1,45 @@
+# Beyonic Python Library
+
+The official Python library for interacting with http://beyonic.com API
+
+## Installation
+
+You don't need this source code unless you want to modify the package. If you just want to use the Beyonic Python bindings, you should run:
+
+```python
+pip install beyonic
+```
+
+or
+
+```python
+easy_install beyonic
+```
+
+or manually
+
+```python
+git clone https://github.com/beyonic/beyonic-python.git
+cd beyonic-python
+python setup.py install
+```
+
+## Usage
+
+Please visit https://apidocs.beyonic.com/ for usage documentation
+
+## Development
+
+Please see DEVELOPMENT.md for information on adding installing requirements, adding new API methods and running tests
+
+## Changelog
+
+0.1.8
+- Added `Accounts API Wrapper` for interacting with the `accounts api`.
+
+0.1.14
+- Added Network and Currency APIs and ability to set Duplicate-Check-Key header for each request
+
+0.1.15
+- Added `banks API wrapper` for interacting with `the banks api`
+
---
+++ packages/beyonic/beyonic-0.1.16.tar.gz/beyonic-0.1.16/setup.py
@@ -0,0 +1,43 @@
+import os
+from setuptools import setup
+import os.path
+import beyonic
+
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+
+VERSION = beyonic.__version__
+
+from os import path
+from io import open
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name="beyonic",
+    version=VERSION,
+    description="The official Python client for the Beyonic.com API",
+    author="Beyonic",
+    author_email="info@beyonic.com",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=["beyonic", "beyonic.apis"],
+    install_requires=["requests"],
+    license="MIT",
+    keywords=["api", "mobile payments", "mobile money", "beyonic", "mpesa"],
+    url="https://beyonic.com",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
