5fe7a64dca2c2239501d399f3f524c8d6c07207f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:39 +0000

    Add package gauss-bino/1.1/gauss-bino-1.1.tar.gz

---
+++ packages/gauss-bino/gauss-bino-1.1.tar.gz/gauss-bino-1.1/README.md
@@ -0,0 +1,6 @@
+Copyright: Majf Technologies (www.majftech.com)
+Author: Mohd Anwar Jamal Faiz
+Email: Toughjamy@yahoo.com
+
+
+Gaussian and Binomial distributions Utility Package from Majf Technologies.
---
+++ packages/gauss-bino/gauss-bino-1.1.tar.gz/gauss-bino-1.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='gauss-bino',
+      version='1.1',
+      description='Gaussian and Binomial distributions Utility Package from Majf Technologies.',
+      packages=['gauss-bino'],
+      zip_safe=False)
