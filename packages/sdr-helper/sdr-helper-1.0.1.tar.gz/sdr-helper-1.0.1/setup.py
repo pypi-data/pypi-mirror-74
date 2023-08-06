2385e17ce102ccd7eba4897ec29a98f0cea190ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:56 +0000

    Add package sdr-helper/1.0.1/sdr-helper-1.0.1.tar.gz

---
+++ packages/sdr-helper/sdr-helper-1.0.1.tar.gz/sdr-helper-1.0.1/setup.py
@@ -0,0 +1,45 @@
+from setuptools import find_namespace_packages, setup
+import os
+import codecs
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+
+def fpath(name):
+    return os.path.join(os.path.dirname(__file__), name)
+
+
+def read(fname):
+    return codecs.open(fpath(fname), encoding='utf-8').read()
+
+
+requirements = read(fpath('requirements.txt'))
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+about = {}
+with codecs.open(os.path.join(here, 'sk_dsp_comm', 'sdr_helper', '__version__.py'), encoding='utf-8') as f:
+    exec(f.read(), about)
+
+setup(name='sdr-helper',
+      version=about['__version__'],
+      description='SDR Helper',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      author='Mark Wickert',
+      author_email='mwickert@uccs.edu',
+      maintainer='Chiranth Siddappa',
+      maintainer_email='chiranthsiddappa@gmail.com',
+      url='https://github.com/scikit-dsp-comm/sdr_helper',
+      packages=find_namespace_packages(include=['sk_dsp_comm.*']),
+      zip_safe=False,
+      license='BSD',
+      install_requires=requirements.split(),
+      test_suite='nose.collector',
+      tests_require=['nose','numpy', 'tox'],
+      extras_require={
+            'rtlsdr': ['pyrtlsdr']
+      },
+      python_requires = '>=3.5',
+     )
