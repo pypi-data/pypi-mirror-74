427c83d0a596bb0d203d52870d88d2b035315590
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:01 +0000

    Add package scipion-em-dynamo/3.1.4/scipion_em_dynamo-3.1.4-py3-none-any.whl

---
+++ packages/scipion-em-dynamo/scipion_em_dynamo-3.1.4-py3-none-any.whl/dynamo/__init__.py
@@ -0,0 +1,76 @@
+# **************************************************************************
+# *
+# * Authors:     you (you@yourinstitution.email)
+# *
+# * your institution
+# *
+# * This program is free software; you can redistribute it and/or modify
+# * it under the terms of the GNU General Public License as published by
+# * the Free Software Foundation; either version 2 of the License, or
+# * (at your option) any later version.
+# *
+# * This program is distributed in the hope that it will be useful,
+# * but WITHOUT ANY WARRANTY; without even the implied warranty of
+# * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# * GNU General Public License for more details.
+# *
+# * You should have received a copy of the GNU General Public License
+# * along with this program; if not, write to the Free Software
+# * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+# * 02111-1307  USA
+# *
+# *  All comments concerning this program package may be sent to the
+# *  e-mail address 'scipion@cnb.csic.es'
+# *
+# **************************************************************************
+
+from os.path import join
+import pwem
+import pyworkflow.utils as pwutils
+from .constants import *
+
+__version__ = '3.1.4'
+_logo = "icon.png"
+_references = ['CASTANODIEZ2012139']
+
+class Plugin(pwem.Plugin):
+    _homeVar = DYNAMO_HOME
+    _pathVars = [DYNAMO_HOME]
+    # _supportedVersions =
+
+    @classmethod
+    def _defineVariables(cls):
+        cls._defineEmVar(DYNAMO_HOME, 'dynamo-{}'.format(DYNAMO_VERSION))
+
+    @classmethod
+    def getEnviron(cls):
+        """ Create the needed environment for Dynamo programs. """
+        environ = pwutils.Environ(os.environ)
+        dyn_home = cls.getHome()
+        environ.update({
+            'PATH': dyn_home + '/matlab/bin:' + dyn_home + '/matlab/src:' + dyn_home + '/cuda/bin:' + dyn_home + '/mpi',
+            'LD_LIBRARY_PATH': dyn_home + '/MCRLinux/runtime/glnxa64:' + dyn_home + '/MCRLinux/bin/glnxa64:' +
+                               dyn_home + '/MCRLinux/sys/os/glnxa64',
+            'DYNAMO_ROOT': dyn_home
+        }, position=pwutils.Environ.BEGIN)
+        return environ
+
+    @classmethod
+    def getDynamoProgram(cls):
+        return join(cls.getHome(), 'matlab', 'bin', DYNAMO_PROGRAM)
+
+    @classmethod
+    def runDynamo(cls, protocol, args, cwd=None):
+        """ Run Dynamo command from a given protocol. """
+        # args will be the .doc file which contains the MATLAB code
+        program = cls.getDynamoProgram()
+        protocol.runJob(program, args, env=cls.getEnviron(), cwd=cwd)
+
+    @classmethod
+    def defineBinaries(cls, env):
+        env.addPackage('dynamo',
+                       version='1.146',
+                       tar='dynamo-1.146.tar.gz',
+                       commands=[],
+                       default=True)
+
