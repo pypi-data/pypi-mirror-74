7c8e138711effdb4eda48cca2cf46aa0048fb5a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:35 +0000

    Add package orix/0.2.2/orix-0.2.2-py3.8.egg

---
+++ packages/orix/orix-0.2.2-py3.8.egg/orix/__init__.py
@@ -0,0 +1,7 @@
+__name__ = "orix"
+__version__ = "0.2.2"
+__author__ = "Ben Martineau, Phillip Crout"
+__author_email__ = "pyxem.team@gmail.com"
+__description__ = (
+    "Orientation, rotation, quaternion, and crystal symmetry handling in Python."
+)
