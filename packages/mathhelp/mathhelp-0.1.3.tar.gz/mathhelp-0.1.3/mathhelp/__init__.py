1bfc1bc140db680a403f7e548e1ae6e1510bfcce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:07 +0000

    Add package mathhelp/0.1.3/mathhelp-0.1.3.tar.gz

---
+++ packages/mathhelp/mathhelp-0.1.3.tar.gz/mathhelp-0.1.3/mathhelp/__init__.py
@@ -0,0 +1,29 @@
+import math
+
+def TR(n):
+	tr=n*(n-1)
+	return tr
+def tn(n, t1, d):
+	tn=t1+d*(n-1)
+	return tn
+
+pi=3.14159265358979323846264338327950
+e=2.7182818284590452353602874713527
+
+def sqrt(n):
+	sqrty=math.sqrt(n)
+	return sqrty
+
+def square(n):
+	n=n**2
+	return n
+
+def pow(x, y):
+	n=x**y
+	return n
+
+def pythagorean(a, b):
+	a**2+b**2=c2
+	c=sqrt(c2)
+	return c
+
