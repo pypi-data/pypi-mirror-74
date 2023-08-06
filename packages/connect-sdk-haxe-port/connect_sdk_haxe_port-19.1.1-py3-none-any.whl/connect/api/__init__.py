2d856bb3cb8a5091f0c1afe25d5b6b708498d5e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:12 +0000

    Add package connect-sdk-haxe-port/19.1.1/connect_sdk_haxe_port-19.1.1-py3-none-any.whl

---
+++ packages/connect-sdk-haxe-port/connect_sdk_haxe_port-19.1.1-py3-none-any.whl/connect/api/__init__.py
@@ -0,0 +1,22 @@
+from connect.autogen import connect_api_ConnectHelper as ConnectHelper
+from connect.autogen import connect_api_IApiClient as IApiClient
+from connect.autogen import connect_api_IFulfillmentApi as IFulfillmentApi
+from connect.autogen import connect_api_IGeneralApi as IGeneralApi
+from connect.autogen import connect_api_IMarketplaceApi as IMarketplaceApi
+from connect.autogen import connect_api_ITierApi as ITierApi
+from connect.autogen import connect_api_IUsageApi as IUsageApi
+from connect.autogen import connect_api_Query as Query
+from connect.autogen import connect_api_Response as Response
+
+
+__all__ = [
+    'ConnectHelper',
+    'IApiClient',
+    'IFulfillmentApi',
+    'IGeneralApi',
+    'IMarketplaceApi',
+    'ITierApi',
+    'IUsageApi',
+    'Query',
+    'Response',
+]
