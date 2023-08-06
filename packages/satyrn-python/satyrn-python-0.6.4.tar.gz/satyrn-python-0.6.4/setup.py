31662aace1e8921fb2744380a17b4fbc10ff4711
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:41 +0000

    Add package satyrn-python/0.6.4/satyrn-python-0.6.4.tar.gz

---
+++ packages/satyrn-python/satyrn-python-0.6.4.tar.gz/satyrn-python-0.6.4/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='satyrn-python',
+    version='0.6.4',
+    entry_points={"console_scripts": ["satyrnCLI = satyrnCLI.__main__:start",
+                                      "satyrn = satyrn_flask.run_frontend:run"]},
+    author="Charles Averill",
+    author_email="charlesaverill20@gmail.com",
+    description="A command-line based alternative to Jupyter notebooks",
+    long_description=long_description,
+    install_requires=['networkx', 'matplotlib'],
+    long_description_content_type="text/markdown",
+    url="https://github.com/CharlesAverill/satyrn/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ]
+)
