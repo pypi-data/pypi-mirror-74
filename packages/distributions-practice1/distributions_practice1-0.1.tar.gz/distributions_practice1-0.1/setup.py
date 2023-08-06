c1bea46542f11a15cd8468322b06fd66cb5646a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:32 +0000

    Add package distributions-practice1/0.1/distributions_practice1-0.1.tar.gz

---
+++ packages/distributions-practice1/distributions_practice1-0.1.tar.gz/distributions_practice1-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_practice1',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions'],
+      author = 'Tushar Sharma',
+      author_email = 'myemail@something.com',
+      zip_safe=False)
