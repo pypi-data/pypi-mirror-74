1c394c9c97852660eab674d806ef0d0b9929aae4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:08 +0000

    Add package bunnycdn-storage/0.1/bunnycdn-storage-0.1.tar.gz

---
+++ packages/bunnycdn-storage/bunnycdn-storage-0.1.tar.gz/bunnycdn-storage-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='bunnycdn-storage',
+    version='0.1',
+    description='A python module for the BunnyCDN Storage API',
+    url='https://github.com/ajacobsen/bunnycdn-storage',
+    author='Andy Jacobsen',
+    author_email='atze.danjo@gmail.com',
+    license='GPLv3+',
+    packages=['bunnycdn_storage'],
+    zip_safe=False)
