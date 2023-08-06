2f7d12c6f3773a1a53663a95f98cc0fdaabbc94a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:04 +0000

    Add package python-bioformats/4.0.0rc3/python-bioformats-4.0.0rc3.tar.gz

---
+++ packages/python-bioformats/python-bioformats-4.0.0rc3.tar.gz/python-bioformats-4.0.0rc3/setup.py
@@ -0,0 +1,44 @@
+import setuptools
+
+setuptools.setup(
+    author="Lee Kamentsky",
+    author_email="leek@broadinstitute.org",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Java",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+        "Topic :: Multimedia :: Graphics :: Graphics Conversion"
+    ],
+    description="Read and write life sciences file formats",
+    extras_require={
+        "test": [
+            "pytest"
+        ]
+    },
+    install_requires=[
+        "boto3",
+        "future",
+        "python-javabridge>=4.0.0rc2"
+    ],
+    license="GPL License",
+    long_description="""Python-bioformats is a Python wrapper for Bio-Formats, a standalone Java library for reading
+    and writing life sciences image file formats. Bio-Formats is capable of parsing both pixels and metadata for a
+    large number of formats, as well as writing to several formats. Python-bioformats uses the python-javabridge to
+    start a Java virtual machine from Python and interact with it. Python-bioformats was developed for and is used by
+    the cell image analysis software CellProfiler (cellprofiler.org).""",
+    name="python-bioformats",
+    package_data={
+        "bioformats": [
+            "jars/*.jar"
+        ]
+    },
+    packages=[
+        "bioformats"
+    ],
+    url="http://github.com/CellProfiler/python-bioformats/",
+    setup_requires=['setuptools_scm'],
+    version="4.0.0rc3"
+)
