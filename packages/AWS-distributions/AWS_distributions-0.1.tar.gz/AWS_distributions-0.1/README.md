45d6a4a3c74f49bd3eecce33e64731337c2aecd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:44 +0000

    Add package AWS-distributions/0.1/AWS_distributions-0.1.tar.gz

---
+++ packages/AWS-distributions/AWS_distributions-0.1.tar.gz/AWS_distributions-0.1/README.md
@@ -0,0 +1,3 @@
+## AWS_distributions package
+
+This package is just an assignment for the Udacity course: AWS Machine Learning Engineer Foundations
---
+++ packages/AWS-distributions/AWS_distributions-0.1.tar.gz/AWS_distributions-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='AWS_distributions',
+      version='0.1',
+      description='Gaussian distributions and Binomial distributions',
+      packages=['AWS_distributions'],
+      author='Sen Yang',
+      zip_safe=False)
