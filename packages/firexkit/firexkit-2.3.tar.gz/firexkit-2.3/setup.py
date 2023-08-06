77632478008b130bcfb867a4d8d0db4cd10d2850
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:08 +0000

    Add package firexkit/2.3/firexkit-2.3.tar.gz

---
+++ packages/firexkit/firexkit-2.3.tar.gz/firexkit-2.3/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+import versioneer
+
+
+setup(name='firexkit',
+      version=versioneer.get_version(),
+      cmdclass=versioneer.get_cmdclass(),
+      description='Core firex libraries',
+      url='https://github.com/FireXStuff/firexkit',
+      author='Core FireX Team',
+      author_email='firex-dev@gmail.com',
+      license='BSD-3-Clause',
+      packages=['firexkit', ],
+      zip_safe=True,
+      install_requires=[
+        "celery == 4.3.0",
+      ],)
