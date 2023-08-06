ae5741633e0a4b4b3b05acd998959da12e601f06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:48 +0000

    Add package binomial-guassian-eb/0.1/binomial-guassian-eb-0.1.tar.gz

---
+++ packages/binomial-guassian-eb/binomial-guassian-eb-0.1.tar.gz/binomial-guassian-eb-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='binomial-guassian-eb',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['binomial-guassian-eb'],
+      zip_safe=False)
