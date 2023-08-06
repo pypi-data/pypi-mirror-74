88ef64775e9e2e1aa439744c9461300c12e789fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:22 +0000

    Add package storage-script/0.2.1/storage_script-0.2.1-py3-none-any.whl

---
+++ packages/storage-script/storage_script-0.2.1-py3-none-any.whl/storage_script/download.py
@@ -0,0 +1,30 @@
+import os
+import sys
+import logging
+from os import environ
+import boto3
+import threading
+from botocore.exceptions import ClientError
+from typing import Text, Optional
+from storage_script.progress import ProgressPercentage
+
+
+def download_file(model: Text, folder: Text):
+    """Download a model to an S3 bucket
+
+    :param model: model to download
+    :param folder: S3 object name. 
+    :return: True if file was downloaded, else False
+    """
+    s3_client = boto3.client("s3")
+    try:
+        response = s3_client.download_file(
+            os.environ.get("AWS_S3_BUCKET_RASA_MODELS"),
+            folder + "/" + model,
+            model,
+            Callback=ProgressPercentage(model),
+        )
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
