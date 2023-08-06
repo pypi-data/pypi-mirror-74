83f14a9b1d098a4acc7103c070fc5a91049aea9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:30 +0000

    Add package yats/1.0.0/yats-1.0.0.tar.gz

---
+++ packages/yats/yats-1.0.0.tar.gz/yats-1.0.0/yats/__main__.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import argparse
+
+from yats.connector import Connector
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--username",
+                        "-u",
+                        type=str,
+                        default="realdonaldtrump")
+    parser.add_argument("--thread",
+                        "-t",
+                        type=int,
+                        default=10)
+    args = parser.parse_args()
+    con = Connector(args.username)
+    profile = con.profile(args.username)
+    profile.describe()
+    con.get_tweets_user(args.username, thread_nb=args.thread)
+
+
+if __name__ == "__main__":
+    main()
