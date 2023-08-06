1d9e849322e041be9d9c49b7436f3644dbed8db4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:57 +0000

    Add package ae-probability/1.0/ae_probability-1.0.tar.gz

---
+++ packages/ae-probability/ae_probability-1.0.tar.gz/ae_probability-1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='ae_probability',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['ae_probability'],
+      author_email = "aallpererdem@gmail.com",
+      zip_safe=False)
