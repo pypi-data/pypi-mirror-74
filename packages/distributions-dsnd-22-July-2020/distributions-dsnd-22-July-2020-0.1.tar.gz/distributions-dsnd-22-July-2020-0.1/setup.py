73a560c1fee9b94739a6301fd4d06c0f4f13ee92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:14 +0000

    Add package distributions-dsnd-22-July-2020/0.1/distributions-dsnd-22-July-2020-0.1.tar.gz

---
+++ packages/distributions-dsnd-22-July-2020/distributions-dsnd-22-July-2020-0.1.tar.gz/distributions-dsnd-22-July-2020-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions-dsnd-22-July-2020',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions-dsnd-22-July-2020'],
+      zip_safe=False)
