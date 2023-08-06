04ca11032620ee3096008d3919db73eebca9ce8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:44 +0000

    Add package africastalking/1.2.3/africastalking-1.2.3.tar.gz

---
+++ packages/africastalking/africastalking-1.2.3.tar.gz/africastalking-1.2.3/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+from setuptools import setup
+import sys
+import os
+
+version = '1.2.3'
+
+long_description = open('README.md').read()
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+setup(
+    name='africastalking',
+    version=version,
+    packages=['africastalking'],
+    description='Official Africa\'s Talking Python SDK',
+    data_files=[('', ['README.md'])],
+    license='MIT',
+    author='Africa\'s Talking',
+    install_requires=[
+        'requests>=v2.18.4',
+        'schema>=0.6.7'
+    ],
+    python_requires=">=2.7.10",
+    author_email='info@africastalking.com',
+    url='https://github.com/AfricasTalkingLtd/africastalking-python',
+    download_url='https://codeload.github.com/AfricasTalkingLtd/africastalking-python/tar.gz/' + version,
+    keywords='ussd voice sms mpesa card bank b2b b2c sender_id payments airtime africastalking',
+    classifiers=[],
+    long_description=long_description,
+    long_description_content_type='text/markdown'
+)
---
+++ packages/africastalking/africastalking-1.2.3.tar.gz/africastalking-1.2.3/test/test_airtime.py
@@ -0,0 +1,55 @@
+"""
+Airtime
+
+send(phoneNumber: String, amount: String): Send airtime to a phone number.
+
+send(recipients: Map<String,String>): Send airtime to a bunch of phone numbers. The keys in the recipients map are phone
+    numbers while the values are airtime amounts.
+"""
+import africastalking
+import unittest
+import random
+from test import USERNAME, API_KEY
+
+africastalking.initialize(USERNAME, API_KEY)
+service = africastalking.Airtime
+
+
+class TestAirtimeService(unittest.TestCase):
+
+    def test_send_single(self):
+        currency_code = "KES"
+        amount = str(random.randint(10, 1000))
+        phone = '+254718763456'
+        idempotency_key = 'req-1234'
+        res = service.send(phone_number=phone, amount=amount, currency_code=currency_code, idempotency_key=idempotency_key)
+        assert res['numSent'] == 1
+
+    def test_send_multiple(self):
+        res = service.send(recipients=[
+            {'phoneNumber': '+2348160663047', 'amount': str(random.randint(1, 10)), 'currency_code': 'USD'},
+            {'phoneNumber': '+254718769881', 'amount':str(random.randint(138, 13223)), 'currency_code':  'KES'},
+        ])
+        assert res['numSent'] == 2
+
+    def test_missing_parameter_send_failure(self):
+
+        def missing_amount():
+            service.send(recipients=[
+                {'phoneNumber': '+254718769881', 'currency_code': 'KES'}
+            ])
+        def missing_phoneNumber():
+            service.send(recipients=[
+                {'amount':str(random.randint(138, 13223)), 'currency_code': 'KES'}
+            ])
+        def missing_currencyCode():    
+            service.send(recipients=[
+                {'phoneNumber': '+254718769881', 'amount':str(random.randint(138, 13223))}
+            ])    
+
+        self.assertRaises(ValueError, missing_amount)
+        self.assertRaises(ValueError, missing_phoneNumber)
+        self.assertRaises(ValueError, missing_currencyCode)    
+
+if __name__ == '__main__':
+    unittest.main()
