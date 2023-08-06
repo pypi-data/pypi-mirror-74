6cce32ba79111ccbf3d60d91cdebd48f7a8858f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:06 +0000

    Add package dgp-lib/1.0.0b1/dgp-lib-1.0.0b1.tar.gz

---
+++ packages/dgp-lib/dgp-lib-1.0.0b1.tar.gz/dgp-lib-1.0.0b1/lib/ingest/trajectory.py
@@ -0,0 +1,134 @@
+# coding: utf-8
+
+"""
+trajectory.py
+Library for trajectory data import functions
+
+"""
+import numpy as np
+import pandas as pd
+from pandas.tseries.offsets import Milli
+
+from lib.time_utils import leap_seconds, convert_gps_time
+
+TRAJECTORY_INTERP_FIELDS = {'lat', 'long', 'ell_ht'}
+
+
+def import_trajectory(filepath, delim_whitespace=False, interval=0,
+                      interp=False, is_utc=False, columns=None, skiprows=None,
+                      timeformat='sow'):
+    """
+    Read and parse ASCII trajectory data in a comma-delimited format.
+
+    Parameters
+    ----------
+    filepath : str or File-like object.
+        Filesystem path to trajectory data file
+    delim_whitespace : bool
+    interval : float, Optional
+        Output data rate. Default behavior is to infer the rate.
+    interp : Union[List[str], List[int]], Optional
+        Gaps in data will be filled with interpolated values. List of
+        column indices (list of ints) or list of column names (list of strs)
+        to interpolate. Default behavior is not to interpolate.
+    is_utc : bool, Optional
+        Indicates that the timestamps are UTC. The index datetimes will be
+        shifted to remove the GPS-UTC leap second offset.
+    columns : List[str]
+        Strings to use as the column names.
+        If none supplied (default), columns will be determined based on
+        timeformat
+    skiprows : Union[None, Iterable, int, Callable], Optional
+        Line numbers to skip (0-indexed) or number of lines to skip (int) at
+        the start of the file. If callable, the callable function will be
+        evaluated against the row indices, returning True if the row should
+        be skipped and False otherwise. An example of a valid callable argument
+        would be lambda x: x in [0, 2].
+    timeformat : str
+        'sow' | 'hms' | 'serial'  Default: 'hms'
+        Indicates the time format to expect. The 'sow' format requires a field
+        named 'week' with the GPS week, and a field named 'sow' with the GPS
+        seconds of week. The 'hms' format requires a field named 'mdy' with the
+        date in the format 'MM/DD/YYYY', and a field named 'hms' with the time
+        in the format 'HH:MM:SS.SSS'. The 'serial' format (not yet implemented)
+        requires a field named 'datenum' with the serial date number.
+
+    Returns
+    -------
+    DataFrame
+        Pandas DataFrame of ingested Trajectory data.
+
+    """
+
+    df = pd.read_csv(filepath, delim_whitespace=delim_whitespace, header=None,
+                     engine='c', na_filter=False, skiprows=skiprows)
+
+    # assumed position of these required fields
+    if columns is None:
+        if timeformat.lower() == 'sow':
+            columns = ['week', 'sow', 'lat', 'long', 'ell_ht']
+        elif timeformat.lower() == 'hms':
+            columns = ['mdy', 'hms', 'lat', 'long', 'ell_ht']
+        elif timeformat.lower() == 'serial':
+            columns = ['datenum', 'lat', 'long', 'ell_ht']
+        else:
+            raise ValueError('timeformat value {fmt!r} not recognized'
+                             .format(fmt=timeformat))
+
+    # 'None' indicates a not-needed field
+    # if a field is after all non-essentials, and is not named, it will be removed
+    if len(df.columns) > len(columns):
+        columns.extend([None] * (len(df.columns) - len(columns)))
+
+    # drop unwanted columns
+    drop_list = list()
+    for idx, val in enumerate(columns):
+        if val is None:
+            drop_list.append(idx)
+
+    columns = [x for x in columns if x is not None]
+
+    if drop_list:
+        df.drop(df.columns[drop_list], axis=1, inplace=True)
+
+    df.columns = columns
+
+    # create index
+    if timeformat == 'sow':
+        df.index = convert_gps_time(df['week'], df['sow'], format='datetime')
+        df.index = df.index.round(Milli())
+        df.drop(['sow', 'week'], axis=1, inplace=True)
+    elif timeformat == 'hms':
+        df.index = pd.to_datetime(df['mdy'].str.strip() + df['hms'].str.strip(),
+                                  format="%m/%d/%Y%H:%M:%S.%f")
+        df.drop(['mdy', 'hms'], axis=1, inplace=True)
+    elif timeformat == 'serial':
+        raise NotImplementedError
+        # df.index = datenum_to_datetime(df['datenum'])
+
+    # remove leap second
+    if is_utc:
+        # TO DO: Check dates at beginning and end to determine whether a leap second was added in the middle of the survey.
+        shift = leap_seconds(datetime=df.index[0])
+        df.index = df.index.shift(-shift, freq='S')
+
+    # set or infer the interval
+    # TO DO: Need to infer interval for both cases to know whether resample
+    if interval > 0:
+        offset_str = '{:d}U'.format(int(interval * 1e6))
+    else:
+        offset_str = '100000U'
+
+    # fill gaps with NaNs
+    new_index = pd.date_range(df.index[0], df.index[-1], freq=offset_str)
+    df = df.reindex(new_index)
+
+    if interp:
+        numeric = df.select_dtypes(include=[np.number])
+        numeric = numeric.interpolate(method='time')
+
+        # replace columns
+        for col in numeric.columns:
+            df[col] = numeric[col]
+
+    return df
---
+++ packages/dgp-lib/dgp-lib-1.0.0b1.tar.gz/dgp-lib-1.0.0b1/lib/time_utils.py
@@ -0,0 +1,174 @@
+# coding: utf-8
+
+import collections
+from datetime import datetime, timedelta
+from functools import lru_cache
+
+import pandas as pd
+
+leap_second_table = [(datetime(1980, 1, 1), datetime(1981, 7, 1)),
+                     (datetime(1981, 7, 1), datetime(1982, 7, 1)),
+                     (datetime(1982, 7, 1), datetime(1983, 7, 1)),
+                     (datetime(1983, 7, 1), datetime(1985, 7, 1)),
+                     (datetime(1985, 7, 1), datetime(1988, 1, 1)),
+                     (datetime(1988, 1, 1), datetime(1990, 1, 1)),
+                     (datetime(1990, 1, 1), datetime(1991, 1, 1)),
+                     (datetime(1991, 1, 1), datetime(1992, 7, 1)),
+                     (datetime(1992, 7, 1), datetime(1993, 7, 1)),
+                     (datetime(1993, 7, 1), datetime(1994, 7, 1)),
+                     (datetime(1994, 7, 1), datetime(1996, 1, 1)),
+                     (datetime(1996, 1, 1), datetime(1997, 7, 1)),
+                     (datetime(1997, 7, 1), datetime(1999, 1, 1)),
+                     (datetime(1999, 1, 1), datetime(2006, 1, 1)),
+                     (datetime(2006, 1, 1), datetime(2009, 1, 1)),
+                     (datetime(2009, 1, 1), datetime(2012, 7, 1)),
+                     (datetime(2012, 7, 1), datetime(2015, 7, 1)),
+                     (datetime(2015, 7, 1), datetime(2017, 1, 1))]
+
+
+def datetime_to_sow(dt):
+    def _to_sow(dt):
+        delta = dt - datetime(1980, 1, 6)
+        week = delta.days // 7
+        sow = (delta.days % 7) * 86400. + delta.seconds + delta.microseconds * 1e-6
+        return week, sow
+
+    if isinstance(dt, collections.Iterable):
+        res = []
+        for i in dt:
+            res.append(_to_sow(i))
+        return res
+    else:
+        return _to_sow(dt)
+
+
+def convert_gps_time(gpsweek, gpsweekseconds, format='unix'):
+    """
+    Converts a GPS time format (weeks + seconds since 6 Jan 1980) to a UNIX
+    timestamp (seconds since 1 Jan 1970) without correcting for UTC leap
+    seconds.
+
+    Static values gps_delta and gpsweek_cf are defined by the below functions
+    (optimization) gps_delta is the time difference (in seconds) between UNIX
+    time and GPS time.
+
+    gps_delta = (dt.datetime(1980, 1, 6) - dt.datetime(1970, 1, 1)).total_seconds()
+
+    gpsweek_cf is the coefficient to convert weeks to seconds
+    gpsweek_cf = 7 * 24 * 60 * 60  # 604800
+
+    Parameters
+    ----------
+    gpsweek : int
+        Number of weeks since beginning of GPS time (1980-01-06 00:00:00)
+
+    gpsweekseconds : float
+        Number of seconds since the GPS week parameter
+
+    format : {'unix', 'datetime'}
+        Format of returned value
+
+    Returns
+    -------
+    float or :obj:`datetime`
+        UNIX timestamp (number of seconds since 1970-01-01 00:00:00) without
+        leapseconds subtracted if 'unix' is specified for format.
+        Otherwise, a :obj:`datetime` is returned.
+    """
+    # GPS time begins 1980 Jan 6 00:00, UNIX time begins 1970 Jan 1 00:00
+    gps_delta = 315964800.0
+    gpsweek_cf = 604800
+
+    if isinstance(gpsweek, pd.Series) and isinstance(gpsweekseconds, pd.Series):
+        gps_ticks = (gpsweek.astype('float64') * gpsweek_cf) + gpsweekseconds.astype('float64')
+    else:
+        gps_ticks = (float(gpsweek) * gpsweek_cf) + float(gpsweekseconds)
+
+    timestamp = gps_delta + gps_ticks
+
+    if format == 'unix':
+        return timestamp
+
+    elif format == 'datetime':
+        return datetime(1970, 1, 1) + pd.to_timedelta(timestamp * 1e9)
+
+
+def leap_seconds(**kwargs):
+    """
+    Look-up for the number of leap seconds for a given date
+
+    Parameters
+    ----------
+    week : int, optional
+        Number of weeks since beginning of GPS time (1980-01-06 00:00:00)
+
+    seconds : float, optional
+        If week is specified, then seconds of week since the beginning of
+        Sunday of that week, otherwise, Unix time in seconds since
+        January 1, 1970 UTC.
+
+    date : :obj:`str`, optional
+        Date string in the format specified by dateformat.
+
+    dateformat : :obj:`str`, optional
+        Format of the date string if date is used. Default: '%m-%d-%Y'
+
+        .. _Format codes:
+            https://docs.python.org/3/library/datetime.html#strftime-and-strptime-behavior
+
+    datetime : :obj:`datetime`
+
+    Returns
+    -------
+    int
+        Number of accumulated leap seconds as of the given date.
+    """
+    if 'seconds' in kwargs:
+        if 'week' in kwargs:
+            dt = (datetime(1980, 1, 6) +
+                  timedelta(weeks=kwargs['week']) +
+                  timedelta(seconds=kwargs['seconds']))
+        else:
+            # TODO Check for value out of bounds?
+            dt = (datetime(1970, 1, 1) +
+                  timedelta(seconds=kwargs['seconds']))
+
+    elif 'date' in kwargs:
+        if 'dateformat' in kwargs:
+            fmt = kwargs['dateformat']
+        else:
+            fmt = '%m-%d-%Y'
+
+        dt = datetime.strptime(kwargs['date'], fmt)
+
+    elif 'datetime' in kwargs:
+        dt = kwargs['datetime']
+
+    else:
+        raise ValueError('Only valid keyword inputs are GPS time in week '
+                         'number and seconds of week, Unix time in seconds '
+                         'since January 1, 1970 UTC, or datetime.')
+
+    return _get_leap_seconds(dt)
+
+
+@lru_cache(maxsize=1000)
+def _get_leap_seconds(dt):
+    ls = 0
+    for entry in leap_second_table:
+        if entry[0] <= dt < entry[1]:
+            break
+        else:
+            ls += 1
+    return ls
+
+
+def datenum_to_datetime(timestamp):
+    raise NotImplementedError
+    # if isinstance(timestamp, pd.Series):
+    #     return (timestamp.astype(int).map(datetime.fromordinal) +
+    #             pd.to_timedelta(timestamp % 1, unit='D') -
+    #             pd.to_timedelta('366 days'))
+    # else:
+    #     return (datetime.fromordinal(int(timestamp) - 366) +
+    #             timedelta(days=timestamp % 1))
