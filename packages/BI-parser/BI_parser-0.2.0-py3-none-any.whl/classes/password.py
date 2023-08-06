211bb8f2cbaaa353bb2c55bdd7fbd5de13fd8f22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:40 +0000

    Add package BI-parser/0.2.0/BI_parser-0.2.0-py3-none-any.whl

---
+++ packages/BI-parser/BI_parser-0.2.0-py3-none-any.whl/classes/password.py
@@ -0,0 +1,9 @@
+def password():
+    x = "BIcode"
+    while x != "BIcode":
+        x = input('password : ')
+        if x == "BIcode":
+            print('ok')
+        else:
+            print("Wrong Password")
+
---
+++ packages/BI-parser/BI_parser-0.2.0-py3-none-any.whl/classes/table.py
@@ -0,0 +1,17 @@
+'''
+Contient la classe chargée de traiter, stocker et manipuler les informations liées aux tables.
+'''
+
+from src.classes.variable import Variable
+
+
+class Table(Variable):
+    '''
+    description
+    '''
+
+    def __init__(self, nom: str = "", alias: str = "", type_table: str = "") -> None:
+        Variable.__init__(self)
+        self.nom = nom
+        self.alias = alias
+        self.type_table = type_table
