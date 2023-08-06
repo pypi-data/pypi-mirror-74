09dd6a8c0432832b6afee44d8f4918d24e3bc431
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:44 +0000

    Add package osr2mp4/0.0.3.dev2/osr2mp4-0.0.3.dev2-py3-none-any.whl

---
+++ packages/osr2mp4/osr2mp4-0.0.3.dev2-py3-none-any.whl/osr2mp4/global_var.py
@@ -0,0 +1,75 @@
+class Settings:
+
+	def __init__(self):
+		self.skin_path = None
+		self.default_path = None
+		self.skin_ini = None
+		self.default_skin_ini = None
+		self.format = ".png"
+		self.x2 = "@2x"
+
+		self.ffmpeg = None
+		self.output = None
+		self.beatmap = None
+		self.osu = None
+		self.path = None
+		self.temp = None
+
+		self.width = None
+		self.height = None
+		self.fps = None
+		self.scale = None
+		self.playfieldscale = None
+		self.playfieldwidth = None
+		self.playfieldheight = None
+		self.movedown = None
+		self.moveright = None
+		self.timeframe = None
+
+		self.settings = {
+			"Cursor size": 1,
+			"In-game interface": True,
+			"Show scoreboard": True,
+			"Background dim": 100,
+			"Rotate sliderball": False,
+			"Always show key overlay": True,
+			"Automatic cursor size": False,
+			"Score meter size": 1,
+			"Song volume": 50,
+			"Effect volume": 50,
+			"Ignore beatmap hitsounds": False,
+			"Use skin's sound samples": False,
+			"Global leaderboard": False,
+			"Mods leaderboard": "*",
+			"api key": "lol"
+		}
+
+		self.ppsettings = {
+			"x": 1240,
+			"y": 725,
+			"Size": 25,
+			"Rgb": [
+				255,
+				255,
+				255
+			],
+			"Alpha": 1,
+			"Font": "arial.ttf",
+			"Background": "",
+			"Hitresult x": 50,
+			"Hitresult y": 150,
+			"Hitresult Size": 16,
+			"Hitresult Rgb": [
+				255,
+				255,
+				255
+			],
+			"Hitresult Alpha": 1,
+			"Hitresult Font": "arial.ttf",
+			"Hitresult Background": "",
+			"Hitresult Gap": 50
+		}
+
+		self.codec = None
+		self.process = None
+		self.enablelog = False
