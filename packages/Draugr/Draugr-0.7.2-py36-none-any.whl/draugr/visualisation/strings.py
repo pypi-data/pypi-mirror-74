c1bced8e975c997a9c933f028197b5d2477f0b12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:57 +0000

    Add package Draugr/0.7.2/Draugr-0.7.2-py36-none-any.whl

---
+++ packages/Draugr/Draugr-0.7.2-py36-none-any.whl/draugr/visualisation/strings.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = "Christian Heider Nielsen"
+__doc__ = r"""
+
+           Created on 09/10/2019
+           """
+
+__all__ = ["add_indent"]
+
+
+def add_indent(input_str: str, indent_spaces_num: int = 2):
+    """
+
+    :param input_str:
+    :type input_str:
+    :param indent_spaces_num:
+    :type indent_spaces_num:
+    :return:
+    :rtype:
+    """
+    s = input_str.split("\n")
+    if len(s) == 1:  # don't do anything for single-line stuff
+        return input_str
+    first = s.pop(0)
+    indent = indent_spaces_num * " "
+    s = [indent + line for line in s]
+    s = "\n".join(s)
+    s = indent + first + "\n" + s
+    return s
+
+
+if __name__ == "__main__":
+    a = "slasc\nsaffasd\n2dasf"
+    print(a)
+    print(add_indent(a))
