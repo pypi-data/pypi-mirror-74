71fa25cf3220d31d108009fc971054a840d1ebc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:47 +0000

    Add package fiberoripy/1.0.0/fiberoripy-1.0.0.tar.gz

---
+++ packages/fiberoripy/fiberoripy-1.0.0.tar.gz/fiberoripy-1.0.0/setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""Setup file."""
+import setuptools
+
+setuptools.setup(
+    name="fiberoripy",
+    version="1.0.0",
+    author="Nils Meyer",
+    author_email="nils.meyer@kit.edu",
+    description="Fiber orientation models and closures",
+    long_description="Fiber orientation models and closures",
+    # long_description=open("README.md").read(),
+    packages=["fiberoripy"],
+    url="https://github.com/nilsmeyerkit/fiberoripy",
+    package_dir={"fiberoripy": "fiberoripy"},
+    install_requires=["numpy", "matplotlib", "scipy"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
