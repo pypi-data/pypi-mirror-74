5c32ec5ed6f41d333681885f1723f2ce34404816
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:51 +0000

    Add package distributions-Gauss-Bino/0.1/distributions_Gauss_Bino-0.1.tar.gz

---
+++ packages/distributions-Gauss-Bino/distributions_Gauss_Bino-0.1.tar.gz/distributions_Gauss_Bino-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_Gauss_Bino',
+      version='0.1',
+      description='Gaussian distributions & Binomial',
+      packages=['distributions_Gauss_Bino'],
+      author = 'Shailendra Kumar',
+      author_email = 'sckumarpy@gmail.com',
+      zip_safe=False)
