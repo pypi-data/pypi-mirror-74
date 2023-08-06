29800364ed459744c7b28529cadac6c5e793b2fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:07 +0000

    Add package m-distributions/0.1/m_distributions-0.1.tar.gz

---
+++ packages/m-distributions/m_distributions-0.1.tar.gz/m_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='m_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['m_distributions'],
+      zip_safe=False)
