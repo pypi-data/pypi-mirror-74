e80da7289d9a7396d12e9ae3c938037970c5f481
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:32 +0000

    Add package SiteLinks/0.0.7/SiteLinks-0.0.7.tar.gz

---
+++ packages/SiteLinks/SiteLinks-0.0.7.tar.gz/SiteLinks-0.0.7/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="SiteLinks",
+    version="0.0.7",
+    author="Elly Mandliel",
+    author_email="ellykido@google.com",
+    description="A link-scanning tool to scrape web urls.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/EllyMandliel/SiteLinks",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    requirements=[
+      'aiohttp',
+      'beautifulsoup4'
+    ],
+    python_requires='>=3',
+)
