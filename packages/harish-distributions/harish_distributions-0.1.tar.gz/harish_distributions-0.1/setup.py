ea828e30da94a9cebcfd921f26d73b63e914deba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:15:05 +0000

    Add package harish-distributions/0.1/harish_distributions-0.1.tar.gz

---
+++ packages/harish-distributions/harish_distributions-0.1.tar.gz/harish_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='harish_distributions',
+      version='0.1',
+      description='Gaussian And Binomial distributions',
+      packages=['harish_distributions'],
+      author='Harish Chandramohan',
+      author_email='harishkumarchandra@gmail.com',
+      zip_safe=False)
