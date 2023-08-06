66362f5898ae6966e5d3daa0929bc51c68452fe7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:29 +0000

    Add package shubhmcoeprob/0.6/shubhmcoeprob-0.6.tar.gz

---
+++ packages/shubhmcoeprob/shubhmcoeprob-0.6.tar.gz/shubhmcoeprob-0.6/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='shubhmcoeprob',
+      version='0.6',
+      description='Gaussian and Binomial distributions',
+      packages=['shubhmcoeprob'],
+      author = 'shubhmcoe',
+      author_email = 'shubhmcoe@gmail.com',
+      zip_safe=False)
