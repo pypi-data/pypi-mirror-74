e0acbc5013dc1cb7bb9e3f1f89e5cdb78559e115
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:16 +0000

    Add package sofconizer/1.0.2/sofconizer-1.0.2.tar.gz

---
+++ packages/sofconizer/sofconizer-1.0.2.tar.gz/sofconizer-1.0.2/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['sofconizer']
+
+package_data = \
+{'': ['*']}
+
+entry_points = \
+{'console_scripts': ['sofconizer = pro:run']}
+
+setup_kwargs = {
+    'name': 'sofconizer',
+    'version': '1.0.2',
+    'description': '',
+    'long_description': None,
+    'author': 'Deniansyah',
+    'author_email': '5lineofcode@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'entry_points': entry_points,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
