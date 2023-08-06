38e2c0df322d809679298aad60570d100fa3577f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:36 +0000

    Add package fbgg-distributions/0.1/fbgg_distributions-0.1.tar.gz

---
+++ packages/fbgg-distributions/fbgg_distributions-0.1.tar.gz/fbgg_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='fbgg_distributions',
+      version='0.1',
+      description='Binomial and Gaussian distributions',
+      packages=['fbgg_distributions'],
+      author = 'Faizan Ahmed',
+      author_email = 'faizanahmed2395@gmail.com',
+      zip_safe=False)
