e820730468ac9b6696fd1ff1608dfeff9ed3a813
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:42 +0000

    Add package first-package-oistamo/1.2/first_package_oistamo-1.2.tar.gz

---
+++ packages/first-package-oistamo/first_package_oistamo-1.2.tar.gz/first_package_oistamo-1.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='first_package_oistamo',
+      version='1.2',
+      description='Gaussian and Binomial distributions',
+      packages=['first_package_oistamo'],
+      author = 'Oistamo Mamadsaidova',
+      author_email = 'mamadsaidova.o@gmail.com',
+    zip_safe=False)
