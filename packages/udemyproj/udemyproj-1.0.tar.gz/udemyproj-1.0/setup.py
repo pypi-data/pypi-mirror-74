21a75b98a6458231262827223ea3695cd90e7f4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:39 +0000

    Add package udemyproj/1.0/udemyproj-1.0.tar.gz

---
+++ packages/udemyproj/udemyproj-1.0.tar.gz/udemyproj-1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='udemyproj',
+      version='1.0',
+      description='Calculate and visuallize, Gaussian, Binomial and Normal distributions',
+      packages=['udemyproj'],
+      author_email='sjahnaviprasad@gmail.com',
+      zip_safe=False)
