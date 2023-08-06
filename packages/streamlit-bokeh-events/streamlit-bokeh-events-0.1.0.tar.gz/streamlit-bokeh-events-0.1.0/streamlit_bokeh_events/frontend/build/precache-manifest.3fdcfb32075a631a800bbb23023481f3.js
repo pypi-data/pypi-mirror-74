2e7a35afd301e832a51bf2a79ee5b0dc607281a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:59 +0000

    Add package streamlit-bokeh-events/0.1.0/streamlit-bokeh-events-0.1.0.tar.gz

---
+++ packages/streamlit-bokeh-events/streamlit-bokeh-events-0.1.0.tar.gz/streamlit-bokeh-events-0.1.0/streamlit_bokeh_events/frontend/build/precache-manifest.3fdcfb32075a631a800bbb23023481f3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef59a15a358dde23803b5ec5b16f6ecc",
+    "url": "./index.html"
+  },
+  {
+    "revision": "723efb58fa8fc453721b",
+    "url": "./static/js/2.ede18996.chunk.js"
+  },
+  {
+    "revision": "07cec5cc4d188bb136df0e4204b4b983",
+    "url": "./static/js/2.ede18996.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3630d6745f07504aee09",
+    "url": "./static/js/3.2c3f9ba2.chunk.js"
+  },
+  {
+    "revision": "c2598d109a9df8c7c363",
+    "url": "./static/js/main.2292ba86.chunk.js"
+  },
+  {
+    "revision": "6515c66d2a8747a146d578e1c038a822",
+    "url": "./static/js/main.2292ba86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "081070bc6c973bab0e2f",
+    "url": "./static/js/runtime-main.60b01e8f.js"
+  }
+]);
