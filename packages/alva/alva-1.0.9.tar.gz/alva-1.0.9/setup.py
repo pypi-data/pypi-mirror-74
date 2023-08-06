141c888d137ccad64858abc912ae3373ae732930
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:46 +0000

    Add package alva/1.0.9/alva-1.0.9.tar.gz

---
+++ packages/alva/alva-1.0.9.tar.gz/alva-1.0.9/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['alva']
+
+package_data = \
+{'': ['*']}
+
+entry_points = \
+{'console_scripts': ['arguino = entry:main']}
+
+setup_kwargs = {
+    'name': 'alva',
+    'version': '1.0.9',
+    'description': '',
+    'long_description': None,
+    'author': 'Deniansyah',
+    'author_email': '5lineofcode@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'entry_points': entry_points,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
