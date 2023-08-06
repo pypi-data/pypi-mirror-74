24ebfb5fc3e617a9bd0aa0ca92f3dece65714756
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:03 +0000

    Add package embeddix/1.13.4/embeddix-1.13.4.tar.gz

---
+++ packages/embeddix/embeddix-1.13.4.tar.gz/embeddix-1.13.4/embeddix/__init__.py
@@ -0,0 +1,20 @@
+"""Exposed functions."""
+from .utils.files import get_shared_vocab
+from .utils.files import load_vocab
+from .utils.files import count_lines
+from .utils.files import load_sparse
+from .utils.files import load_dense
+from .utils.files import save_sparse
+from .utils.files import save_dense
+from .utils.files import save_vocab
+from .utils.data import load_dataset
+from .utils.metrix import similarity
+from .utils.metrix import spearman
+from .utils.metrix import pearson
+from .utils.metrix import rmse
+from .utils.metrix import hmean
+from .utils.stats import steiger_test_pval
+from .core.reducer import _reduce_dense as reduce_dense
+from .core.reducer import _reduce_sparse as reduce_sparse
+from .core.converter import convert_to_txt
+from .core.evaluator import evaluate_word_similarity
