fdbdf9842ae344d9c56d6046003d11fe12cfbc7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:19 +0000

    Add package accubits/0.2/accubits-0.2.tar.gz

---
+++ packages/accubits/accubits-0.2.tar.gz/accubits-0.2/setup.py
@@ -0,0 +1,31 @@
+import pathlib
+from setuptools import setup, find_packages
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+setup(
+    name='accubits',
+    version='0.2',
+    package=find_packages(),
+    description="CLI Tool to create front end web apps from boilerplate code",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    include_package_data=True,
+    license='MIT',
+    download_url='https://github.com/accubits-tech/accubits-cli/archive/0.2.tar.gz',
+    author = 'Nithi',
+    author_email = 'bearnithi@gmail.com',
+    keywords = ['Accubits Cli', 'Front end boilerplates', 'angular boilerplate'],
+    install_requires=[
+        'Click',
+        'gitpython'
+    ],
+    entry_points='''
+        [console_scripts]
+        accubits=accubitscli.scripts.accubits:cli
+    ''',
+)
