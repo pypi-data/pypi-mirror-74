5146a467c5961aab5c1efdeafaeaa379cabd750c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:51 +0000

    Add package KafkaHelper-athanikos/0.1.3/KafkaHelper_athanikos-0.1.3-py3-none-any.whl

---
+++ packages/KafkaHelper-athanikos/KafkaHelper_athanikos-0.1.3-py3-none-any.whl/kafkaHelper/kafkaHelper.py
@@ -0,0 +1,37 @@
+from json import dumps, loads
+from kafka import KafkaProducer
+from enum import Enum
+from kafka import KafkaConsumer
+
+
+class Action(Enum):
+    Added = 1
+    Modified = 2
+    Deleted = 3
+
+
+def produce(broker_names, topic, data_item):
+    producer = KafkaProducer(bootstrap_servers=broker_names,
+                             value_serializer=lambda x:
+                             dumps(x).encode('utf-8'))
+    future = producer.send(topic, data_item)
+    result = future.get(timeout=60)
+
+
+def consume(broker_names, topic, consumer_group):
+    consumer = KafkaConsumer(
+        topic,
+        bootstrap_servers=broker_names,
+        auto_offset_reset='earliest',
+        enable_auto_commit=True,
+        auto_commit_interval_ms=1,
+        group_id=consumer_group,
+        value_deserializer=lambda x: loads(x.decode('utf-8')),
+        consumer_timeout_ms=3000
+        )
+    items = []
+    for msg in consumer:
+        items.append(msg.value)
+    return items
+
+
