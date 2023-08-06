21037d54a184ff099d2529cd348ee6366a720f7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:54 +0000

    Add package gethash/1.1/gethash-1.1-py3-none-any.whl

---
+++ packages/gethash/gethash-1.1-py3-none-any.whl/gethash/__init__.py
@@ -0,0 +1,16 @@
+__project__ = 'gethash'
+__version__ = '1.1'
+__author__ = 'xymy'
+
+__all__ = ['ParseHashLineError', 'ParseHashLineError',
+           'calc_hash', 'fhl', 'phl', 'ghl', 'chl']
+
+from ._core import (    # noqa: F401
+    CheckHashLineError,
+    ParseHashLineError,
+    calc_hash,
+    chl,
+    fhl,
+    ghl,
+    phl
+)
