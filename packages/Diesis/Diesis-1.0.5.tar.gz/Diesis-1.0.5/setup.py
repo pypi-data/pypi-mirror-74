813074d1441b1757902a752be15ff57050edb90f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:45 +0000

    Add package Diesis/1.0.5/Diesis-1.0.5.tar.gz

---
+++ packages/Diesis/Diesis-1.0.5.tar.gz/Diesis-1.0.5/setup.py
@@ -0,0 +1,37 @@
+import setuptools
+
+
+with open('README.md', 'r') as description:
+    long_description = description.read()
+
+setuptools.setup(
+    name='Diesis',
+    version='1.0.5',
+    license='MIT',
+    description='A simple auto-tagging tool for precise music collectors.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Enrico Sola',
+    author_email='info@enricosola.com',
+    url='https://github.com/RyanJ93/diesis',
+    packages=setuptools.find_packages(),
+    keywords=['music', 'tagging', 'id3', 'mp3', 'm4a', 'flac', 'ogg'],
+    install_requires=[
+        'mutagen',
+        'beautifulsoup4',
+        'pydub'
+    ],
+    python_requires='>=3.5',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
+    ],
+    entry_points={
+        'console_scripts': ['diesis=diesis.main:main']
+    }
+)
