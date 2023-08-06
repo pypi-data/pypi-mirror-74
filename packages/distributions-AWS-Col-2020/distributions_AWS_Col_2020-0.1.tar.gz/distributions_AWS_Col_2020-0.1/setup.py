0ce166f4453787055dcae5b3038cccf2c2951386
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:59 +0000

    Add package distributions-AWS-Col-2020/0.1/distributions_AWS_Col_2020-0.1.tar.gz

---
+++ packages/distributions-AWS-Col-2020/distributions_AWS_Col_2020-0.1.tar.gz/distributions_AWS_Col_2020-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='distributions_AWS_Col_2020',
+      version='0.1',
+      description='Gaussian distributions_AWS_Col_2020',
+      packages=['distributions_AWS_Col_2020'],
+      Author = 'Michael Levkovsky',
+      Author_email = 'levkos@hotmail.com',
+      zip_safe=False)
+     
+    
