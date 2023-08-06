d30b4a64f741ab5ea6cf874aace67b402389b6c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:28 +0000

    Add package botol6/1.0/botol6-1.0.tar.gz

---
+++ packages/botol6/botol6-1.0.tar.gz/botol6-1.0/setup.py
@@ -0,0 +1,41 @@
+import setuptools
+
+
+
+with open("README.md", "r") as fh:
+
+    long_description = fh.read()
+
+
+
+setuptools.setup(
+
+    name="botol6", 
+
+    version="1.0",
+
+    author="BotolMehedi",
+
+    author_email="Botol@email.com",
+
+    description="Bangladeshi Account Cracker",
+
+    long_description=open('README.md').read() + '\n\n' + open('CHANGELOG.txt').read(),
+
+    url="https://github.com/botolmehedi/botol",
+
+    packages=setuptools.find_packages(),
+
+    classifiers=[
+
+        "Programming Language :: Python :: 2",
+
+        "License :: OSI Approved :: MIT License",
+
+        "Operating System :: OS Independent",
+
+    ],
+
+    python_requires='>=2.7',
+
+)
