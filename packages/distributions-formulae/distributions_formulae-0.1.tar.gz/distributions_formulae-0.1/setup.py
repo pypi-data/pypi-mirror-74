e8abe5ebeb09f89757639157f697cc2274bfbece
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:54 +0000

    Add package distributions-formulae/0.1/distributions_formulae-0.1.tar.gz

---
+++ packages/distributions-formulae/distributions_formulae-0.1.tar.gz/distributions_formulae-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_formulae',
+      version='0.1',
+      description='Gaussian and Binomial distributions formulae',
+      packages=['distributions_formulae'],
+      author="Digant Garude",
+      author_email="digant.dg07@gmail.com",
+      zip_safe=False)
