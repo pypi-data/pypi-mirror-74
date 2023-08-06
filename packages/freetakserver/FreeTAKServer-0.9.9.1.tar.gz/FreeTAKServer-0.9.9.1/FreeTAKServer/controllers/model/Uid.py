9be479c5c8795db36c52abb1479862121c8aa7ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:41 +0000

    Add package freetakserver/0.9.9.1/FreeTAKServer-0.9.9.1.tar.gz

---
+++ packages/freetakserver/FreeTAKServer-0.9.9.1.tar.gz/FreeTAKServer-0.9.9.1/FreeTAKServer/controllers/model/Uid.py
@@ -0,0 +1,9 @@
+class Uid:
+    def __init__(self, xml):
+        self.Droid = None
+        self.setDroid(xml.get('Droid'))
+
+    def getDroid(self):
+        return self.Droid
+    def setDroid(self, Droid):
+        self.Droid = Droid
