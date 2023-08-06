379df0ca57bc7fdf910ee64a76ab4ec1d038809f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:01 +0000

    Add package screenshot-manager/1.0.3/screenshot-manager-1.0.3.tar.gz

---
+++ packages/screenshot-manager/screenshot-manager-1.0.3.tar.gz/screenshot-manager-1.0.3/setup.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+
+from setuptools import find_packages
+from setuptools import setup
+import shlex
+import subprocess
+import sys
+
+
+version = '1.0.3'
+
+
+# release helper
+if sys.argv[-1] == 'release':
+    commands = [
+        'git pull origin master',
+        'git tag v{0}'.format(version),
+        'git push origin master --tags',
+        'python setup.py sdist',
+        'twine upload dist/screenshot-manager-{0}.tar.gz'.format(version),
+    ]
+    for cmd in commands:
+        print('+ {0}'.format(cmd))
+        subprocess.check_call(shlex.split(cmd))
+    sys.exit(0)
+
+
+def get_long_description():
+    with open('README.md') as f:
+        long_description = f.read()
+
+    try:
+        import github2pypi
+
+        return github2pypi.replace_url(
+            slug='wkentaro/screenshot-manager', content=long_description
+        )
+    except Exception:
+        return long_description
+
+
+setup(
+    name='screenshot-manager',
+    version=version,
+    packages=find_packages(),
+    install_requires=[],
+    description='Copy screenshots and organize.',
+    long_description=get_long_description(),
+    long_description_content_type='text/markdown',
+    author='Kentaro Wada',
+    author_email='www.kentaro.wada@gmail.com',
+    url='http://github.com/wkentaro/screenshot-manager',
+    license='MIT',
+    keywords='utility',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX',
+        'Topic :: Internet :: WWW/HTTP',
+    ],
+    entry_points={
+        'console_scripts': ['screenshot-manager=screenshot_manager.cli:main']
+    },
+)
