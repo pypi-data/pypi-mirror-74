3a7bd342a8d4595e18090b4f8097b482ab273a2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:53 +0000

    Add package mengyu-distributions/1.1/mengyu_distributions-1.1.tar.gz

---
+++ packages/mengyu-distributions/mengyu_distributions-1.1.tar.gz/mengyu_distributions-1.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='mengyu_distributions',
+      version='1.1',
+      description='All kinds of distributions',
+      packages=['mengyu_distributions'],
+      zip_safe=False)# cannot be run directly when ziped
