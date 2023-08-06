023bc6e26637955c97cf47df6aa9c045a15147cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:04 +0000

    Add package asyncstdlib/1.1.3/asyncstdlib-1.1.3-py2.py3-none-any.whl

---
+++ packages/asyncstdlib/asyncstdlib-1.1.3-py2.py3-none-any.whl/asyncstdlib/__init__.py
@@ -0,0 +1,78 @@
+"""The missing async toolbox"""
+from .builtins import (
+    anext,
+    zip,
+    map,
+    filter,
+    enumerate,
+    iter,
+    all,
+    any,
+    max,
+    min,
+    sum,
+    list,
+    dict,
+    set,
+    tuple,
+)
+from .functools import reduce, lru_cache, cached_property
+from .contextlib import closing, contextmanager, nullcontext, ExitStack
+from .itertools import (
+    accumulate,
+    cycle,
+    chain,
+    compress,
+    dropwhile,
+    islice,
+    takewhile,
+    starmap,
+    tee,
+    zip_longest,
+    groupby,
+)
+from .asynctools import borrow, scoped_iter
+
+__version__ = "1.1.3"
+
+__all__ = [
+    "anext",
+    "zip",
+    "map",
+    "filter",
+    "enumerate",
+    "iter",
+    "all",
+    "any",
+    "max",
+    "min",
+    "sum",
+    "list",
+    "dict",
+    "set",
+    "tuple",
+    # functools
+    "reduce",
+    "lru_cache",
+    "cached_property",
+    # contextlib
+    "closing",
+    "contextmanager",
+    "nullcontext",
+    "ExitStack",
+    # itertools
+    "accumulate",
+    "cycle",
+    "chain",
+    "compress",
+    "dropwhile",
+    "takewhile",
+    "islice",
+    "starmap",
+    "tee",
+    "zip_longest",
+    "groupby",
+    # asynctools
+    "borrow",
+    "scoped_iter",
+]
