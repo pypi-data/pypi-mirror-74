ccba1c1b748818fb3f2d39cfddd2b377db2f6667
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:50 +0000

    Add package gaubin-distribution/0.1/gaubin_distribution-0.1.tar.gz

---
+++ packages/gaubin-distribution/gaubin_distribution-0.1.tar.gz/gaubin_distribution-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='gaubin_distribution',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['gaubin_distribution'],
+      author = 'Olawumi Abayomi',
+      author_email = 'olawumiabayomi@gmail.com',
+      zip_safe=False)
