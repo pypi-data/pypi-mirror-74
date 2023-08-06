9fb176c6ee9ddbbe00d6deaad7f50f6d9ef73532
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:51 +0000

    Add package bukdjango-envsettings/0.1.2/bukdjango_envsettings-0.1.2.tar.gz

---
+++ packages/bukdjango-envsettings/bukdjango_envsettings-0.1.2.tar.gz/bukdjango_envsettings-0.1.2/bukdjango_envsettings/__init__.py
@@ -0,0 +1,50 @@
+import importlib
+import os
+from typing import Iterable, Callable, Dict, Any, Tuple
+
+from django.core.exceptions import ImproperlyConfigured
+
+from bukdjango_envsettings.utils import gather_settings, eval_settings
+from bukdjango_envsettings.conversion import MAPPING
+
+
+def update_from_env(
+        pre: str = "DJANGO_",
+        mapping: Dict[str, Callable[[str], Any]] = MAPPING,
+        extra_mapping: Dict[str, Callable[[str], Any]] = None,
+        allowed: Iterable[str] = MAPPING,
+        hook: Callable[[str, Any], Tuple[str, Any]] = None
+):
+    """
+    Update settings module that is set by `DJANGO_SETTINGS_MODULE` env variable.
+    :param pre: prefix for environment variables
+    :param mapping: mapping of `setting name`: `conversion function`
+    :param extra_mapping: same as mapping but will update defaults
+    :param allowed: iterable of settings that are allowed to be set from env
+    :param hook: function that takes and returns `setting name` and `setting value`
+    """
+    settings_module_path = os.environ.get('DJANGO_SETTINGS_MODULE')
+
+    if not settings_module_path:
+        raise ImproperlyConfigured(
+            '`DJANGO_SETTINGS_MODULE` environment variable is `None`'
+        )
+
+    # update mapping
+    if extra_mapping:
+        mapping.update(extra_mapping)
+
+    # import `DJANGO_SETTINGS_MODULE`
+    settings_module = importlib.import_module(settings_module_path)
+
+    # gather all environment settings
+    env_settings = gather_settings(pre)
+
+    # evaluate settings
+    converted_settings = eval_settings(env_settings, mapping)
+
+    for k, v in converted_settings.items():
+        if k in allowed:
+            if hook:
+                k, v = hook(k, v)
+            setattr(settings_module, k, v)
---
+++ packages/bukdjango-envsettings/bukdjango_envsettings-0.1.2.tar.gz/bukdjango_envsettings-0.1.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+APP_NAME = "bukdjango_envsettings"
+
+setup(
+    name=APP_NAME,
+    zip_safe=False,
+    version="0.1.2",
+    include_package_data=True,
+    packages=[APP_NAME],
+    python_requires=">=3.8",
+    url="https://github.com/bukdjango/envsettings",
+    install_requires=["django>=3.0.8"],
+)
