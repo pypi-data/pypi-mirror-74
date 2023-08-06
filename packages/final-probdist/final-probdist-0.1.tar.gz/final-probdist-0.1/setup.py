f7dc46060e99cd603ac3626415daae1ca03c48bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:37 +0000

    Add package final-probdist/0.1/final-probdist-0.1.tar.gz

---
+++ packages/final-probdist/final-probdist-0.1.tar.gz/final-probdist-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='final-probdist',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['final-probdist'],
+      zip_safe=False)
