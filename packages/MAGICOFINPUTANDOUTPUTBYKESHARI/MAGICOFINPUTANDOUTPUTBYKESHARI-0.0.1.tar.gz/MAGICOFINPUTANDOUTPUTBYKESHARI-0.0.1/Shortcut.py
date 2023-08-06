812ea1838e9f8e8449c0cbb3507ccdbab6e9a233
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:02 +0000

    Add package MAGICOFINPUTANDOUTPUTBYKESHARI/0.0.1/MAGICOFINPUTANDOUTPUTBYKESHARI-0.0.1.tar.gz

---
+++ packages/MAGICOFINPUTANDOUTPUTBYKESHARI/MAGICOFINPUTANDOUTPUTBYKESHARI-0.0.1.tar.gz/MAGICOFINPUTANDOUTPUTBYKESHARI-0.0.1/Shortcut.py
@@ -0,0 +1,3 @@
+cin=scanf=i=input
+documentwrite=ip=cout=printf=P=p=ଦେଖ=see=देखना=आउटपुट_देखना=ي=изглед=视图=視圖=pogled=Pohled=udsigt=visie=vidon=vaade=näkymä=vue=Aussicht=θέα=נוף=melihat=Visualizza=見る=전망=skats=vaizdas=utsikt=widok=Visão=vedere=Посмотреть=поглед=vyhliadka=pogled=ver=se=намоиш=ดู=görünüm=вид=lượtxem=دیکھیں=వీక్షణ=பார்வை=पहा=കാണുക=pandangan=tingnan=vista=দৃশ্য=print
+
