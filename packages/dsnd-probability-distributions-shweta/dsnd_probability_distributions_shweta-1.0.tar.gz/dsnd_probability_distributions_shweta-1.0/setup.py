371ff39d8ddeb6eacd34cebaf5e88f4f7428932e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:19 +0000

    Add package dsnd-probability-distributions-shweta/1.0/dsnd_probability_distributions_shweta-1.0.tar.gz

---
+++ packages/dsnd-probability-distributions-shweta/dsnd_probability_distributions_shweta-1.0.tar.gz/dsnd_probability_distributions_shweta-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dsnd_probability_distributions_shweta',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['dsnd_probability_distributions_shweta'],
+      author='Shweta',
+      author_email='shweta.universidad@gmail.com',
+      zip_safe=False)
