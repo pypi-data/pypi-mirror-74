ef416875feecbf40c6d05ed232d9861ffa6259d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:29 +0000

    Add package gersemi/0.3.1/gersemi-0.3.1.tar.gz

---
+++ packages/gersemi/gersemi-0.3.1.tar.gz/gersemi-0.3.1/gersemi/formatter.py
@@ -0,0 +1,41 @@
+from gersemi.configuration import Configuration
+from gersemi.dumper import Dumper
+from gersemi.parser import create_parser_with_postprocessing
+from gersemi.sanity_checker import check_code_equivalence
+
+
+def noop(*_):
+    pass
+
+
+class Formatter:  # pylint: disable=too-few-public-methods
+    def __init__(
+        self,
+        bare_parser,
+        sanity_checker,
+        configuration: Configuration,
+        custom_command_definitions,
+    ):
+        self.bare_parser = bare_parser
+        self.parser = create_parser_with_postprocessing(self.bare_parser)
+        self.sanity_checker = sanity_checker
+        self.configuration = configuration
+        self.custom_command_definitions = custom_command_definitions
+
+    def format(self, code):
+        dumper = Dumper(self.configuration.line_length, self.custom_command_definitions)
+        result = dumper.visit(self.parser.parse(code))
+        self.sanity_checker(self.bare_parser, code, result)
+        return result
+
+
+def create_formatter(
+    bare_parser, do_sanity_check, line_length, custom_command_definitions
+):
+    sanity_checker = check_code_equivalence if do_sanity_check else noop
+    return Formatter(
+        bare_parser,
+        sanity_checker,
+        Configuration(line_length),
+        custom_command_definitions,
+    )
