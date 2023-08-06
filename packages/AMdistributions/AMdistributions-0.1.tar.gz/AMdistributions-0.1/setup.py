7d7408ddeb86412f2b8239b091b0c929ed2654c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:24 +0000

    Add package AMdistributions/0.1/AMdistributions-0.1.tar.gz

---
+++ packages/AMdistributions/AMdistributions-0.1.tar.gz/AMdistributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='AMdistributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['AMdistributions'],
+      author = 'Ali Mbacho',
+      authot_email = 'alimbacho67@gmail.com',
+      zip_safe=False)
