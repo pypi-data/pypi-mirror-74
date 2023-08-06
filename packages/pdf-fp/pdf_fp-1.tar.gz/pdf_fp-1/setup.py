44cd6b9706b87b0352fb2291e4fb576228631cef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:47 +0000

    Add package pdf-fp/1/pdf_fp-1.tar.gz

---
+++ packages/pdf-fp/pdf_fp-1.tar.gz/pdf_fp-1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='pdf_fp', version=1,
+      description='Gaussian and Binomial Distributions',
+      packages=['pdf_fp'],
+      author = 'Abdelrahman Omran',
+      author_email = 'apdo.omran@gmail.com',
+      zip_safe=False)
