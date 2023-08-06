5dcc2b8e39f34973dd653d8003edeb730043ec72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:22 +0000

    Add package dsnd-probability-shahid/0.1/dsnd_probability_shahid-0.1.tar.gz

---
+++ packages/dsnd-probability-shahid/dsnd_probability_shahid-0.1.tar.gz/dsnd_probability_shahid-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dsnd_probability_shahid',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['dsnd_probability_shahid'],
+      author = 'Shahid ul Islam',
+      author_email = 'shahid.bhat1994@gmail.com',
+      zip_safe=False)
