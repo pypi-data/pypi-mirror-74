c2811ca30a26bbe5a65588bb652c014fe0b258cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:20 +0000

    Add package BugMaker-Project-SDK/0.0.0.2.post3/BugMaker_Project_SDK-0.0.0.2.post3-py3-none-any.whl

---
+++ packages/BugMaker-Project-SDK/BugMaker_Project_SDK-0.0.0.2.post3-py3-none-any.whl/BugMaker_Project_SDK/__init__.py
@@ -0,0 +1,4 @@
+#import warnings
+print("Hello By BugMaker❤")
+__all__=["sort"]
+#warnings.warn("Please DONOT use BugMaker_Project_SDK.Sorting.\nBecause it's a can't delete package(Who can help us!)")
