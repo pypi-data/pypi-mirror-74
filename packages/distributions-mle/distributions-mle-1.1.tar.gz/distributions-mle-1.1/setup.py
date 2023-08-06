e01ac039906ddaeb609b83fdd01e17547ed56042
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:25 +0000

    Add package distributions-mle/1.1/distributions-mle-1.1.tar.gz

---
+++ packages/distributions-mle/distributions-mle-1.1.tar.gz/distributions-mle-1.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions-mle',
+      version='1.1',
+      description='Gaussian distributions',
+      packages=['distributions'],
+      zip_safe=False)
