8b9ae5233678741bb13f2441031235fbeeee8010
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:50 +0000

    Add package meljakdistributions/0.1/meljakdistributions-0.1.tar.gz

---
+++ packages/meljakdistributions/meljakdistributions-0.1.tar.gz/meljakdistributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='meljakdistributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['meljakdistributions'],
+      zip_safe=False)
