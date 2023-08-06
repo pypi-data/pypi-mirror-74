d51eb497b6d9b5d47ff597b0fbfb755d2de3aec0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:59 +0000

    Add package uraiko/1.0.0/uraiko-1.0.0.tar.gz

---
+++ packages/uraiko/uraiko-1.0.0.tar.gz/uraiko-1.0.0/README.rst
@@ -0,0 +1,28 @@
+uraiko
+======
+
+Let's say you are working with urls in your application and you need to
+check whether the url is okay and if it redirects to another
+destination. Of course, we have some awesome libraries already to get
+this done. But ``uraiko`` makes it easy, you just feed the url and
+voila! ### Installation You can install uraiko using pip,
+
+.. code:: sh
+
+    pip install uraiko
+
+Requires Python 3 or above to run. If you have Python 2 installed too,
+make sure to use the right pip.
+
+Usage
+~~~~~
+
+::
+
+    >>> import uraiko
+    >>> link = uraiko.url_check("iamlizu.com")
+    >>> print(link)
+    https://iamlizu.com/
+
+
+Please look for logs at '~/Documents/uraiko.log'
---
+++ packages/uraiko/uraiko-1.0.0.tar.gz/uraiko-1.0.0/setup.py
@@ -0,0 +1,37 @@
+from distutils.core import setup
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+setup(
+  name = 'uraiko',
+  packages = ['uraiko'],
+  version = '1.0.0',
+  license='MIT',
+  description = 'Simplest url checker on checker!',
+  long_description=long_description,
+  long_description_content_type='text/rst',
+  author = 'S M Mahmudul Hasan',
+  author_email = 'thegeek@iamlizu.com',
+  url = 'https://iamlizu.com/tools/uraiko/',
+  download_url = 'https://github.com/IamLizu/uraiko/archive/v_1_0_0.tar.gz',
+  keywords = ['url', 'analyze', 'redirect'],
+  install_requires=[
+          'urllib.request',
+          'logging',
+          'os',
+          'sys',
+      ],
+  classifiers=[
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+  ],
+)
