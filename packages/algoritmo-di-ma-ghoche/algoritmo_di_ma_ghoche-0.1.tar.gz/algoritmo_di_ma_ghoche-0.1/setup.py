797db45edf9641a4ed3d656e54a45a68e4eff9f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:05 +0000

    Add package algoritmo-di-ma-ghoche/0.1/algoritmo_di_ma_ghoche-0.1.tar.gz

---
+++ packages/algoritmo-di-ma-ghoche/algoritmo_di_ma_ghoche-0.1.tar.gz/algoritmo_di_ma_ghoche-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='algoritmo_di_ma_ghoche',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['algoritmo_di_ma_ghoche'],
+      zip_safe=False)
