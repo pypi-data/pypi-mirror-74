25d412a3fbc93fef9940f1438fde3bc220b09ccc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:50 +0000

    Add package z3c.rest-publisher/0.9.2/z3c.rest_publisher-0.9.2.tar.gz

---
+++ packages/z3c.rest-publisher/z3c.rest_publisher-0.9.2.tar.gz/z3c.rest_publisher-0.9.2/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.9.2'
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+setup(name='z3c.rest_publisher',
+      version=version,
+      description="This product has the base classes to implement a REST-Server using the concepts of Zope-traverser (IBrowserPublisher)",
+      long_description=(
+              read('README.md')
+              + '\n\n' +
+              read('CHANGES.txt')
+      ),
+      long_description_content_type='text/x-rst',
+      # Get more strings from
+      # http://pypi.python.org/pypi?:action=list_classifiers
+      classifiers=[
+        "Programming Language :: Python :: 2.7",
+        ],
+      keywords='REST Zope IBrowserPublisher',
+      author='Rodrigo Ristow',
+      author_email='rodrigo@maxttor.com',
+      url='http://svn.plone.org/svn/collective/',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['z3c'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'zope.app.publication',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
