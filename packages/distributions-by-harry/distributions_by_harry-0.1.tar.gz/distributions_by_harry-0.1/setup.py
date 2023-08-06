81f103ba25ac067de1c880195dc366b414b9fb0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:35 +0000

    Add package distributions-by-harry/0.1/distributions_by_harry-0.1.tar.gz

---
+++ packages/distributions-by-harry/distributions_by_harry-0.1.tar.gz/distributions_by_harry-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_by_harry',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions_by_harry'],
+      author='Hari Prasanth',
+      author_email='harrymechatron@gmail.com',
+      zip_safe=False)
