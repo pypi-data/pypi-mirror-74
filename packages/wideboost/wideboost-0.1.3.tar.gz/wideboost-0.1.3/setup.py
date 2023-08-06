0e42538436a85bf617b5d6dfa7a4d9574ad3d25c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:38 +0000

    Add package wideboost/0.1.3/wideboost-0.1.3.tar.gz

---
+++ packages/wideboost/wideboost-0.1.3.tar.gz/wideboost-0.1.3/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="wideboost", 
+    version="0.1.3",
+    author="Michael Horrell",
+    author_email="mthorrell@github.com",
+    description="Implements Wide Boosting functions for popular boosting packages",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mthorrell/wideboost",
+    packages=setuptools.find_packages(),
+    install_requires=['numpy','xgboost'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
