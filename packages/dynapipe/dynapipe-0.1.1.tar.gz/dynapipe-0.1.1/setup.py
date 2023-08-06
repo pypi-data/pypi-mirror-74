bbf934438dcdc704b9d92b5ea86651b7114db9e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:36 +0000

    Add package dynapipe/0.1.1/dynapipe-0.1.1.tar.gz

---
+++ packages/dynapipe/dynapipe-0.1.1.tar.gz/dynapipe-0.1.1/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="dynapipe", 
+    version="0.1.1",
+    author="Tony Dong",
+    author_email="tonyleidong@gmail.com",
+    description="DynaPipe (Dynamic Pipeline) is an high-level API to help data scientists building models in ensemble way, and automating Machine Learning workflow with simple coding.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tonyleidong/dynapipe",
+    keywords = ['auto machine learning', 'features selection', 'model selection'],
+    packages=setuptools.find_packages(),
+    include_package_data = True,
+    install_requires=[
+        'pandas',
+        'scikit-learn',
+        'statsmodels',
+        'scipy',
+        'joblib',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+
+)
+
