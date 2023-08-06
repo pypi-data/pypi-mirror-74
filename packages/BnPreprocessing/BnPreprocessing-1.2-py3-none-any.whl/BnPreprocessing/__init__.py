6d43aee53e189d5581b41d003932b27b11b0e443
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:48 +0000

    Add package BnPreprocessing/1.2/BnPreprocessing-1.2-py3-none-any.whl

---
+++ packages/BnPreprocessing/BnPreprocessing-1.2-py3-none-any.whl/BnPreprocessing/__init__.py
@@ -0,0 +1,8 @@
+from pathlib import Path
+script_location = Path(__file__).absolute().parent
+sw_loc = script_location / "bn_stopwords.txt"
+with open(sw_loc,'r',encoding = 'utf-8') as f:
+    stop_words = f.read()
+
+import re
+from script_location.processing import *
