effbf3ae2ce28182f3f23341dd0cc205bc443e5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:25 +0000

    Add package compuglobal/0.2.5/compuglobal-0.2.5.tar.gz

---
+++ packages/compuglobal/compuglobal-0.2.5.tar.gz/compuglobal-0.2.5/setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+with open('README.rst') as f:
+    long_description = f.read()
+
+setup(
+    name='compuglobal',
+    packages=find_packages(exclude=['docs', 'tests', 'examples']),
+    version='0.2.5',
+    description='Python wrapper for the CGHMC API (Frinkiac, Morbotron,' +
+                ' Master Of All Science and more!)',
+    long_description=long_description,
+    author='MitchellAW',
+    author_email='mitchwoollatt@gmail.com',
+    license='MIT',
+    url='https://github.com/MitchellAW/CompuGlobal',
+    keywords=['frinkiac', 'morbotron', 'master-of-all-science',
+              'good-god-lemon', 'capital-beat-us', 'the-simpsons',
+              'futurama', 'rick-and-morty', '30 rock', 'west wing', 'api'],
+    scripts=[],
+    classifiers=['Intended Audience :: Developers',
+                 'License :: OSI Approved :: MIT License',
+                 'Programming Language :: Python :: 3.5',
+                 'Programming Language :: Python :: 3.6'],
+    install_requires=['requests>=2.20.0', 'aiohttp>=3.6.0,<3.7.0']
+)
