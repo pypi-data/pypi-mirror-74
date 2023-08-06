acb58a132b8950b21093c5996ab70601cbaa288c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:18 +0000

    Add package aio-periodic/0.2.8/aio_periodic-0.2.8-py3-none-any.whl

---
+++ packages/aio-periodic/aio_periodic-0.2.8-py3-none-any.whl/aio_periodic/job.py
@@ -0,0 +1,61 @@
+from .types import utils
+from .types.job import Job as J
+from .types import command as cmd
+
+class Job(object):
+
+    def __init__(self, payload, w):
+
+        self.payload = J.build(payload)
+
+        self.job_handle = utils.encode_str8(self.payload.func) + utils.encode_str8(self.payload.name)
+
+        self.w = w
+
+    async def done(self, buf = b''):
+        await self.w.agent.send(cmd.WorkDone(self.job_handle, buf))
+
+    async def sched_later(self, delay, count = 0):
+        await self.w.agent.send(cmd.SchedLater(self.job_handle, delay, count))
+
+    async def fail(self):
+        await self.w.agent.send(cmd.WorkFail(self.job_handle))
+
+    async def acquire(self, name, count):
+        agent = self.w.agent
+        await agent.send(cmd.Acquire(name, count, self.job_handle))
+        payload = await agent.receive()
+        if payload[0:1] == cmd.ACQUIRED:
+            if payload[1] == 1:
+                return True
+        return False
+
+    async def release(self, name):
+        await self.w.agent.send(cmd.Release(name, self.job_handle))
+
+    async def with_lock(self, name, count, task, release=False):
+        acquired = await self.acquire(name, count)
+        if acquired:
+            await task()
+            if release:
+                await self.release(name)
+
+    @property
+    def func_name(self):
+        return str(self.payload.func, 'utf-8')
+
+    @property
+    def name(self):
+        return str(self.payload.name, 'utf-8')
+
+    @property
+    def sched_at(self):
+        return self.payload.sched_at
+
+    @property
+    def timeout(self):
+        return self.payload.timeout
+
+    @property
+    def workload(self):
+        return self.payload.workload
---
+++ packages/aio-periodic/aio_periodic-0.2.8-py3-none-any.whl/aio_periodic/types/agent.py
@@ -0,0 +1,40 @@
+import asyncio
+from .utils import MAGIC_REQUEST, encode_int32, to_bytes
+from binascii import crc32
+
+class Agent(object):
+    def __init__(self, writer, msgid, loop=None):
+        self._writer = writer
+        self.msgid = msgid
+        self._buffer = []
+        self._loop = loop
+        self._waiters = []
+
+    def feed_data(self, data):
+        self._buffer.insert(0, data)
+        if len(self._waiters) > 0:
+            waiter = self._waiters.pop()
+            waiter.set_result(True)
+
+    async def receive(self):
+        if len(self._buffer) == 0:
+            waiter = self._make_waiter()
+            await waiter
+        return self._buffer.pop()
+
+    def buffer_len(self):
+        return len(self._buffer)
+
+    async def send(self, cmd):
+        payload = bytes(cmd)
+        if self.msgid:
+            payload = self.msgid + payload
+        size = encode_int32(len(payload))
+        crc = encode_int32(crc32(to_bytes(payload)))
+        self._writer.write(MAGIC_REQUEST + size + crc + to_bytes(payload))
+        await self._writer.drain()
+
+    def _make_waiter(self):
+        waiter = self._loop.create_future()
+        self._waiters.insert(0, waiter)
+        return waiter
