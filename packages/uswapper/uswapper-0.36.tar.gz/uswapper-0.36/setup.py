37160e01a37c4a1b8f501cacffd8027ab1532586
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:50 +0000

    Add package uswapper/0.36/uswapper-0.36.tar.gz

---
+++ packages/uswapper/uswapper-0.36.tar.gz/uswapper-0.36/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='uswapper',
+      version='0.36',
+      description='Simple wrapper for uniswap graphql api',
+      license='MIT',
+      packages=['uswapper'],
+      install_requires=[
+              'pandas',
+              'requests',
+              'python_graphql_client',
+              'six'
+              ],
+      zip_safe=False)
