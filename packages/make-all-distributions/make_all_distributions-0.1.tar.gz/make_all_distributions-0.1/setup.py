9887b41d1c92042d488fa229e40b256d7dd90e1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:31 +0000

    Add package make-all-distributions/0.1/make_all_distributions-0.1.tar.gz

---
+++ packages/make-all-distributions/make_all_distributions-0.1.tar.gz/make_all_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='make_all_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['make_all_distributions'],
+      zip_safe=False)
