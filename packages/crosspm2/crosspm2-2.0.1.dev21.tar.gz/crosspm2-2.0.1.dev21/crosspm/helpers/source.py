9d55bc73a4e370e848a6031cdba2b0956e9d77cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:03 +0000

    Add package crosspm2/2.0.1.dev21/crosspm2-2.0.1.dev21.tar.gz

---
+++ packages/crosspm2/crosspm2-2.0.1.dev21.tar.gz/crosspm2-2.0.1.dev21/crosspm/helpers/source.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+# from crosspm.helpers.exceptions import *
+import itertools
+
+from addict import Dict
+
+
+class Source:
+    def __init__(self, adapter, parser, data):
+        self._adapter = adapter
+        self._parser = parser
+        if 'repo' not in data:
+            data['repo'] = ''
+        if isinstance(data['repo'], str):
+            data['repo'] = [data['repo']]
+        self.args = {k: v for k, v in data.items() if k not in ['type', 'parser']}
+        self.path_patterns = parser._rules['path']
+
+    @property
+    def repos(self):
+        return self.args['repo']
+
+    def get_packages(self, downloader, list_or_file_path, property_validate=True):
+        return self._adapter.get_packages(self, self._parser, downloader, list_or_file_path, property_validate)
+
+    def get_usedby(self, downloader, list_or_file_path, property_validate=True):
+        return self._adapter.get_usedby(self, self._parser, downloader, list_or_file_path, property_validate)
+
+    def __getattr__(self, item):
+        return self.args.get(item, None)
+
+    def __getitem__(self, item):
+        return self.args.get(item, None)
+
+    def get_paths(self, packages_to_find):
+        paths = []
+
+        for package, repo, path_pattern in itertools.product(packages_to_find, self.repos, self.path_patterns):
+            params = {
+                'server' : self.args['server'],
+                'repo' : repo,
+                'package' : package['package'],
+                'version' : package['version']
+            }
+            paths.append(Dict({'path':path_pattern.format(**params), 'params':params}))
+
+        return paths
