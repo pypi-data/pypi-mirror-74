080af792bf9ac60489e859f5e65f90591c86da3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:13 +0000

    Add package distri-distribution/0.1/distri-distribution-0.1.tar.gz

---
+++ packages/distri-distribution/distri-distribution-0.1.tar.gz/distri-distribution-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distri-distribution',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distri-distribution'],
+      author = 'Deep Chokshi',
+      author_email = 'deep.chokshi@outlook.com',
+      zip_safe=False)
