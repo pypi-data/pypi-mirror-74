ce83e6dd3da312234ef58b2df2efb35244d98d84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:58 +0000

    Add package distributions-binomial/0.1/distributions_binomial-0.1.tar.gz

---
+++ packages/distributions-binomial/distributions_binomial-0.1.tar.gz/distributions_binomial-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_binomial',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_binomial'],
+      author = 'Patrick Ruejoma',
+      author_email = 'pruejoma@gmail.com',
+      zip_safe=False)
