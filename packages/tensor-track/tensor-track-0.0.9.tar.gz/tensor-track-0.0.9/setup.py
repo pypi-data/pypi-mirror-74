82b3cdc3a1accf410bce9c63689ce683dd796372
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:43 +0000

    Add package tensor-track/0.0.9/tensor-track-0.0.9.tar.gz

---
+++ packages/tensor-track/tensor-track-0.0.9.tar.gz/tensor-track-0.0.9/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="tensor-track",
+    version="0.0.9",
+    description="The one-stop shop for tracking TensorFlow metrics",
+    url="https://github.com/SamClaflin/Tensor-Track",
+    author="Sam Claflin",
+    author_email="samclaflin7@gmail.com",
+    license="MIT",
+    packages=["tensortrack"],
+    zip_safe=False
+)
