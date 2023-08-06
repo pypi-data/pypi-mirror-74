03eca56960c7b7193360f7a0a9da7548a989c556
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:12 +0000

    Add package cha-distributions/0.1/cha_distributions-0.1.tar.gz

---
+++ packages/cha-distributions/cha_distributions-0.1.tar.gz/cha_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='cha_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['cha_distributions'],
+      author= 'Charith Atapattu',
+      email='charithnisanka@gmail.com',
+      zip_safe=False)
