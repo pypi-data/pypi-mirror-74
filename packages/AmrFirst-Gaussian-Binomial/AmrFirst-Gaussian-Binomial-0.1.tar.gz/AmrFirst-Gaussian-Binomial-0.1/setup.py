536d83b3453a4288482a863e3a7e4ec800c7d9c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:51 +0000

    Add package AmrFirst-Gaussian-Binomial/0.1/AmrFirst-Gaussian-Binomial-0.1.tar.gz

---
+++ packages/AmrFirst-Gaussian-Binomial/AmrFirst-Gaussian-Binomial-0.1.tar.gz/AmrFirst-Gaussian-Binomial-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='AmrFirst-Gaussian-Binomial',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['AmrFirst-Gaussian-Binomial'],
+      zip_safe=False)
