72064771ad29c65cd3ca931e596c5c17a2d51f2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:44 +0000

    Add package aidungeonapi/0.0.4/aidungeonapi-0.0.4.tar.gz

---
+++ packages/aidungeonapi/aidungeonapi-0.0.4.tar.gz/aidungeonapi-0.0.4/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="aidungeonapi",
+    version="0.0.4",
+    author="1Macho",
+    author_email="ozjuanpa@gmail.com",
+    description="AI Dungeon api for python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/1Macho/AIDungeonAPI",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        "gql>=v3.0.0a1"
+    ]
+)
