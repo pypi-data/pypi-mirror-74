3f519fb6cb470f19ad15edf6f2b50da7c3b393e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:20 +0000

    Add package hpogrid/0.3.8/hpogrid-0.3.8.tar.gz

---
+++ packages/hpogrid/hpogrid-0.3.8.tar.gz/hpogrid-0.3.8/hpogrid/algorithm/nevergrad_algorithm.py
@@ -0,0 +1,24 @@
+import nevergrad as ng
+
+import ray
+from ray.tune.suggest.nevergrad import NevergradSearch
+
+from hpogrid.search_space.nevergrad_space import NeverGradSpace
+
+class NeverGradAlgoWrapper():
+    
+    def __init__(self):
+        self.algorithm = None
+        self.default_method = 'RandomSearch'
+        self.default_budget = 100
+
+    def create(self, metric, mode, search_space, **args):
+        search_space = NeverGradSpace(search_space).get_search_space()
+        if ('method' in args) and (args['method']):
+            method = args['method']
+        else:
+            method = self.default_method
+        optimizer = ng.optimizers.registry[method](
+                parametrization=search_space, budget=self.default_budget)
+        self.algorithm = NevergradSearch(optimizer, None, metric=metric, mode=mode, **args)
+        return self.algorithm
