f536336a37ecf430e3a0af7acd9dc2eb56407fa4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:39 +0000

    Add package dsnd-distributions-udacity/0.1/dsnd_distributions_udacity-0.1.tar.gz

---
+++ packages/dsnd-distributions-udacity/dsnd_distributions_udacity-0.1.tar.gz/dsnd_distributions_udacity-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='dsnd_distributions_udacity',
+      version='0.1',
+      description='dsnd_distributions udacity course',
+      author = 'Alex McNulty',
+      packages=['dsnd_distributions_udacity'],
+      zip_safe=False)
