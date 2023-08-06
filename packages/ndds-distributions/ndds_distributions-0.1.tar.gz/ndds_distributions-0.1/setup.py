1e91f960070d25c1a676f3118fb31f40c1fa817e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:13 +0000

    Add package ndds-distributions/0.1/ndds_distributions-0.1.tar.gz

---
+++ packages/ndds-distributions/ndds_distributions-0.1.tar.gz/ndds_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='ndds_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['ndds_distributions'],
+      zip_safe=False)
