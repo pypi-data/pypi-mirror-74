1a4533c37e4e75fa9769c9554542a17909c0014e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:19 +0000

    Add package PyEyeTrack/0.3.6.2/PyEyeTrack-0.3.6.2.tar.gz

---
+++ packages/PyEyeTrack/PyEyeTrack-0.3.6.2.tar.gz/PyEyeTrack-0.3.6.2/setup.py
@@ -0,0 +1,33 @@
+from distutils.core import setup
+
+setup(
+  name = 'PyEyeTrack',         
+  packages = ['PyEyeTrack'],   
+  version = '0.3.6.2',      
+  license='MIT',        
+  description = """PyEyeTrack is a python-based pupil-tracking library. The library tracks eyes with the commodity webcam 
+                and gives a real-time stream of eye coordinates. It provides the functionality of eye-tracking and 
+                blink detection and encapsulates these in a generic interface that allows clients to use these 
+                functionalities in a variety of use-cases.""", 
+  authors = 'Kanchan Sarolkar, Kimaya Badhe, Neha Chaudhari, Samruddhi Kanhed and Shrirang Karandikar',                   
+  author_email = 'pytracklibrary@gmail.com',      
+  url = 'https://github.com/algoasylum/PyEyeTrack',  
+  download_url = 'https://github.com/algoasylum/pyEyeTrack/archive/v_03_6_2.tar.gz',    
+  keywords = ['Eye Tracking','blink detection','User Interface','Webcamera'], 
+  install_requires=[
+  'dlib==19.4',
+  'keyboard==0.13.3',
+  'numpy==1.18.1',
+  'opencv-python>=4.0.*',
+  'pandas==0.24.0',
+  'pyaudio==0.2.11',
+  'pyqt==5.6.0',
+  ],  
+  classifiers=[
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'Programming Language :: Python :: 3.6',
+  ],
+)
