7661a140bd278aa5a8bce1a3ffd6e9b3a878244b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:41 +0000

    Add package panoptes-utils/0.2.22/panoptes-utils-0.2.22.tar.gz

---
+++ packages/panoptes-utils/panoptes-utils-0.2.22.tar.gz/panoptes-utils-0.2.22/scripts/testing/run-tests.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -e
+
+REPORT_FILE=${REPORT_FILE:-coverage.xml}
+
+# This assumes we are always running in a docker container.
+export COVERAGE_PROCESS_START="/var/panoptes/panoptes-utils/setup.cfg"
+
+coverage erase
+
+# Run coverage over the pytest suite.
+echo "Starting tests"
+coverage run "$(command -v pytest-3)"
+
+echo "Combining coverage"
+coverage combine
+
+echo "Making XML coverage report at ${REPORT_FILE}"
+coverage xml -o "${REPORT_FILE}"
+
+coverage report --show-missing
+
+exit 0
