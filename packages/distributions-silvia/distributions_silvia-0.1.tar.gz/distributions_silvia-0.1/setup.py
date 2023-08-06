ee528aa522348c836cc9fea362c95deccdedb28a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:14:08 +0000

    Add package distributions-silvia/0.1/distributions_silvia-0.1.tar.gz

---
+++ packages/distributions-silvia/distributions_silvia-0.1.tar.gz/distributions_silvia-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_silvia',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_silvia'],
+      zip_safe=False)
