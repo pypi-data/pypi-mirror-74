df74e0a308bcee41315caf39d42f79df1a3ad331
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:05 +0000

    Add package pydantic-webargs/1.0.0/pydantic_webargs-1.0.0.tar.gz

---
+++ packages/pydantic-webargs/pydantic_webargs-1.0.0.tar.gz/pydantic_webargs-1.0.0/pydantic_webargs/__init__.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+from functools import wraps
+from flask import request
+
+__author__ = "Ahmed Nafies Okasha Mohamed <ahmed.nafies@gmail.com>"
+__copyright__ = "Copyright 2020, Ahmed Nafies Okasha Mohamed"
+__license__ = "MIT"
+__version__ = "1.0.0"
+
+
+BODY_METHODS = ["POST", "PUT", "PATCH"]
+
+
+class Error(Exception):
+    def __init__(self, message):
+        self.message = message
+
+
+class InvalidOperation(Error):
+    pass
+
+
+def validate(query, body):
+
+    payload = None
+    query_params = None
+    if body and request.method not in BODY_METHODS:
+        raise InvalidOperation(
+            f"Http method '{request.method}' does not contain a payload,"
+            "yet a Pyndatic model for body was suppied"
+        )
+    if body:
+        payload = body(**request.json).dict()
+
+    if query:
+        params = request.args
+        query_params = query(**params).dict()
+    return dict(payload=payload, query=query_params)
+
+
+def webargs(query=None, body=None):
+    def decorator(f):
+        @wraps(f)
+        def decorated_function(*args, **kwargs):
+            result = validate(query, body)
+            kwargs.update(result)
+            response = f(*args, **kwargs)
+            return response
+
+        return decorated_function
+
+    return decorator
