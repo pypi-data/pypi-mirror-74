9210ce412d520e4cbcb1627ef88591813a290eef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:44 +0000

    Add package PythonForWindows/0.6.0/PythonForWindows-0.6.0.tar.gz

---
+++ packages/PythonForWindows/PythonForWindows-0.6.0.tar.gz/PythonForWindows-0.6.0/setup.py
@@ -0,0 +1,37 @@
+﻿# -*- coding: utf-8 -*-
+import sys
+import os.path
+from setuptools import setup
+
+PKG_NAME = "PythonForWindows"
+VERSION  = "0.6.0"
+
+# Load long description from README.md
+this_directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_directory, 'README.md')) as f:
+    long_description = f.read()
+
+setup(
+    name = PKG_NAME,
+    version = VERSION,
+    author = 'Hakril',
+    author_email = 'pfw@hakril.net',
+    description = 'A codebase aimed to make interaction with Windows and native execution easier',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license = 'BSD',
+    keywords = 'windows python',
+    url = 'https://github.com/hakril/PythonForWindows',
+    packages = ['windows',
+                'windows.crypto',
+                'windows.debug',
+                'windows.generated_def',
+                'windows.native_exec',
+                'windows.rpc',
+                'windows.utils',
+                'windows.winobject',
+                'windows.winproxy',
+                'windows.winproxy.apis'],
+    classifiers = ['Programming Language :: Python :: 3',
+                   'Programming Language :: Python :: 2.7']
+)
