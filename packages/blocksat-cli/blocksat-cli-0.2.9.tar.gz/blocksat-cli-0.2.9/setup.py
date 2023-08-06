a823ad5e2810e1cb4458c3efe1064c81b9edf3d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:08 +0000

    Add package blocksat-cli/0.2.9/blocksat-cli-0.2.9.tar.gz

---
+++ packages/blocksat-cli/blocksat-cli-0.2.9.tar.gz/blocksat-cli-0.2.9/setup.py
@@ -0,0 +1,45 @@
+import re, sys
+from setuptools import setup
+
+
+if sys.version_info[0] < 3:
+    raise SystemExit("Error: blocksat-cli requires Python 3")
+    sys.exit(1)
+
+version = re.search(
+    '^__version__\s*=\s*"(.*)"',
+    open('blocksatcli/main.py').read(),
+    re.M
+).group(1)
+
+long_description = """# Blockstream Satellite CLI
+
+A command-line interface for configuring, running and monitoring a Blockstream
+Satellite receiver setup.
+
+"""
+
+setup(
+    name = "blocksat-cli",
+    packages = ["blocksatcli"],
+    entry_points = {
+        "console_scripts": ['blocksat-cli = blocksatcli.main:main']
+    },
+    version = version,
+    description = "Blockstream Satellite CLI",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author = "Blockstream Corp",
+    author_email = "satellite@blockstream.com",
+    url = "https://github.com/Blockstream/satellite",
+    install_requires=[
+        'distro',
+        'requests'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        "Programming Language :: Python :: 3 :: Only",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)"
+    ],
+    python_requires='>=3'
+)
