82e6887d83bd04f036f7384d5be95523c28d47f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:39 +0000

    Add package osm-iterator/1.2.0/osm_iterator-1.2.0.tar.gz

---
+++ packages/osm-iterator/osm_iterator-1.2.0.tar.gz/osm_iterator-1.2.0/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="osm_iterator",
+    version="1.2.0",
+    author="Mateusz Konieczny",
+    author_email="matkoniecz@gmail.com",
+    description="Iterate over .osm file and call a provided callback function for each element",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/matkoniecz/osm_iterator",
+    packages=setuptools.find_packages(),
+    install_requires = [
+        'lxml>=3.5.0',
+        'nose>=1.3.7',
+    ],
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
