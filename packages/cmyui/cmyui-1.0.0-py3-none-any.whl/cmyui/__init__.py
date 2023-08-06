41d04d135ec26bc378cf691930e3de76e971197c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:02 +0000

    Add package cmyui/1.0.0/cmyui-1.0.0-py3-none-any.whl

---
+++ packages/cmyui/cmyui-1.0.0-py3-none-any.whl/cmyui/__init__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+"""
+A simple server built as my first library.
+Also open source! The source can be found at:
+https://github.com/cmyui/simple_serv
+"""
+
+__title__ = 'basic_server'
+__author__ = 'cmyui'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2020 cmyui'
+__version__ = '1.0.0'
+
+from .connection import *
+from .mysqlconn import *
+from .packetstream import *
+from .server import *
+from .types import *
+from .utils import *
