3108ba339ac9af03e0cbb4aa55aacb73b8dceaa4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:33 +0000

    Add package ojpacker/0.1.1/ojpacker-0.1.1.tar.gz

---
+++ packages/ojpacker/ojpacker-0.1.1.tar.gz/ojpacker-0.1.1/README.md
@@ -0,0 +1,15 @@
+# 简介
+一个简单的python脚本工具，简化生成OJ数据的过程。  
+a script can packer test data for Olympic informatics Online Judge
+
+## 指南
++ 安装: `pip3 install ojpacker`
++ 运行: 直接在终端中输入 `ojpacker`
++ 演示： 运行 `ojpacker demo` 可以生成demo， 详见demo
+
+## 需求 ~~probably~~
++ python版本 >= 3.6.9
++ [rich](https://github.com/willmcgugan/rich) >= 3.0
+
+## 计划中的功能
++ 增加交互式更改设置
---
+++ packages/ojpacker/ojpacker-0.1.1.tar.gz/ojpacker-0.1.1/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fp:
+    long_description = fp.read()
+
+setup(
+    # package
+    name="ojpacker",
+    version="0.1.1",
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': ['ojpacker = ojpacker:main'],
+    },
+    # requires
+    python_requires='>=3.6.9',
+    install_requires=[
+        'rich>=3',
+    ],
+    # description
+    description=
+    "a script can packer test data for Olympic informatics Online Judge",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: Implementation :: CPython",
+        # "Operating System :: OS Independent",
+    ],
+    # about
+    author="Simon_Chen",
+    author_email="1020359403@qq.com",
+    url="https://github.com/Simon-Chenzw/OJpacker",
+)
