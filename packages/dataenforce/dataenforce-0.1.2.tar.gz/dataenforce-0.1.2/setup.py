297baa096b036ddb7da103d914894913d139bc4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:24 +0000

    Add package dataenforce/0.1.2/dataenforce-0.1.2.tar.gz

---
+++ packages/dataenforce/dataenforce-0.1.2.tar.gz/dataenforce-0.1.2/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read().replace("```py", "```")
+
+setuptools.setup(
+    name="dataenforce",
+    version="0.1.2",
+    author="Cedric Canovas",
+    author_email="dev@canovas.me",
+    description="Enforce column names & data types of pandas DataFrames",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/CedricFR/dataenforce",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: Apache Software License"
+    ],
+)
