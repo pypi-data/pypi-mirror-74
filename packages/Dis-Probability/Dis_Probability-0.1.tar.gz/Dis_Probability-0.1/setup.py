fb659196ed1b64032f38c2c86d9bf34076916dc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:39 +0000

    Add package Dis-Probability/0.1/Dis_Probability-0.1.tar.gz

---
+++ packages/Dis-Probability/Dis_Probability-0.1.tar.gz/Dis_Probability-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='Dis_Probability',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['Dist_Probability'],
+      author =  "Oluwaseun Adeyo",
+      author_email = "johnadeyo@hotmail.com",
+      zip_safe=False)
