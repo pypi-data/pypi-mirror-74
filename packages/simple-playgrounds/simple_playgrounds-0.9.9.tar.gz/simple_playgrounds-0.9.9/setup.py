c4f7232e99ab96f3a55268b862c09366682b4e34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:17:08 +0000

    Add package simple-playgrounds/0.9.9/simple_playgrounds-0.9.9.tar.gz

---
+++ packages/simple-playgrounds/simple_playgrounds-0.9.9.tar.gz/simple_playgrounds-0.9.9/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='simple_playgrounds',
+    version='0.9.9',
+    description='Simulator for AGI and RL',
+    author='Michael Garcia Ortiz',
+    author_email='michael.garcia-ortiz@city.ac.uk',
+    packages=[package for package in find_packages()
+                if package.startswith('simple_playgrounds')]
+)
