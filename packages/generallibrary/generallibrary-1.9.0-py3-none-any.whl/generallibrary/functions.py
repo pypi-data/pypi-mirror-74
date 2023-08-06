cfa685ff60b6ce0836bb73288f4788aa378a88bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:14 +0000

    Add package generallibrary/1.9.0/generallibrary-1.9.0-py3-none-any.whl

---
+++ packages/generallibrary/generallibrary-1.9.0-py3-none-any.whl/generallibrary/functions.py
@@ -0,0 +1,193 @@
+
+import inspect
+import re
+
+def leadingArgsCount(funcOrClass):
+    """
+    Get number of leading args without a default value.
+
+    :param function funcOrClass: Generic callable
+    """
+    count = 0
+    for param in inspect.signature(funcOrClass).parameters.values():
+        if param.default is inspect.Parameter.empty and param.kind.name == "POSITIONAL_OR_KEYWORD" and param.name != "self":
+            count += 1
+    return count
+
+def getSignatureNames(funcOrClass):
+    """
+    Get a callable class' or func's signature parameter keys as a tuple.
+
+    :param any funcOrClass: Generic callable
+    :rtype: list[str]
+    """
+    if callable(funcOrClass):
+        try:
+            return list(inspect.signature(funcOrClass).parameters.keys())
+        except ValueError:
+            return None
+
+def getSignatureDefaults(funcOrClass):
+    """
+    Get a dict of each key that has a default value assigned
+
+    :param function funcOrClass: Generic callable
+    :rtype: dict[str, any]
+    """
+    if callable(funcOrClass):
+        defaults = {}
+
+        try:
+            params = inspect.signature(funcOrClass).parameters.values()
+        except ValueError:
+            return None
+
+        for param in params:
+            if param.default is not param.empty:
+                defaults[param.name] = param.default
+        return defaults
+
+def getParameter(func, args, kwargs, name):
+    """
+    Get value of a parameter if it exists, otherwise None
+
+    :param function func: Function to get signature
+    :param list or tuple args:
+    :param dict kwargs:
+    :param str name: Name of parameter
+    """
+    parameters = getSignatureNames(func)
+    if "self" in parameters:
+        parameters.remove("self")
+
+    if name in parameters:
+        index = parameters.index(name)
+    else:
+        index = None
+
+    if index is not None and len(args) > index:
+        # Args
+        return args[index]
+    else:
+        # Kwargs
+        if name in kwargs:
+            return kwargs[name]
+
+        # Default
+        defaults = getSignatureDefaults(func)
+        if name in defaults:
+            return defaults[name]
+
+        # Doesn't exist at all
+        return None
+
+def changeArgsAndKwargs(func, args, kwargs, **newParameters):
+    """
+    Return changed args and kwargs, if new parameter exists in args then it's changed there first, otherwise kwargs is changed
+
+    :param func: Function object to get signature names
+    :param args: Packed args
+    :param kwargs: Packed kwargs
+    :param newParameters: New parameters
+    :rtype: tuple[tuple, dict]
+    """
+    args = list(args)
+
+    parameters = getSignatureNames(func)
+
+    if "self" in parameters:
+        parameters.remove("self")
+
+    for name, value in newParameters.items():
+        if name in parameters:
+            index = parameters.index(name)
+        else:
+            index = None
+
+        if index is not None and len(args) > index:
+            args[index] = value
+        else:
+            kwargs[name] = value
+
+    return tuple(args), kwargs
+
+
+
+ignore = ["+", "-", "*", "/", "(", ")", "sqrt"]
+def _tokenize(expression):
+    """
+    Tokenize an expression
+    Taken from https://stackoverflow.com/questions/61948141/python-function-from-mathematical-expression-string/61949248
+    """
+    return re.findall(r"(\b\w*[\.]?\w+\b|[\(\)\+\*\-\/])", expression)
+
+def calculate(expression, *args):
+    """
+    Calculate function which can take any expression. Enter args in the order that they appear.
+    """
+    seenArgs = {}
+    newTokens = []
+    tokens = _tokenize(expression)
+    for token in tokens:
+        try:
+            float(token)
+        except ValueError:
+            tokenIsFloat = False
+        else:
+            tokenIsFloat = True
+
+        if token in ignore or tokenIsFloat:
+            newTokens.append(token)
+        else:
+            if token not in seenArgs:
+                seenArgs[token] = str(args[len(seenArgs)])
+            newTokens.append(seenArgs[token])
+    return eval("".join(newTokens))
+
+def defaults(dictionary, **kwargs):
+    """
+    Like a reversed dict.update().
+    Returns given dictionary with values updated by kwargs unless they already existed.
+
+    :param dict dictionary:
+    :param kwargs:
+    """
+    kwargs.update(dictionary)
+    return kwargs
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ packages/generallibrary/generallibrary-1.9.0-py3-none-any.whl/test/test_functions.py
@@ -0,0 +1,126 @@
+
+import unittest
+
+from generallibrary.functions import leadingArgsCount, getSignatureNames, getSignatureDefaults, changeArgsAndKwargs, getParameter, defaults
+
+
+class FunctionsTest(unittest.TestCase):
+    def test_leadingArgsCount(self):
+        self.assertEqual(0, leadingArgsCount(lambda: 5))
+        self.assertEqual(1, leadingArgsCount(lambda x: 5))
+        self.assertEqual(2, leadingArgsCount(lambda x, y: 5))
+        self.assertEqual(1, leadingArgsCount(lambda x, y=2: 5))
+        self.assertEqual(0, leadingArgsCount(lambda x=3, y=2: 5))
+
+        def hello(x, y, z=None):
+            """2 args without default"""
+            pass
+        self.assertEqual(2, leadingArgsCount(hello))
+
+        def hello(self, x, y=5, *args, **kwargs):
+            """1 leading argument without a default value because self is ignored"""
+            pass
+        self.assertEqual(1, leadingArgsCount(hello))
+
+        def hello(x, y, *args):
+            """1 leading argument without a default value because self is ignored"""
+            pass
+        self.assertEqual(2, leadingArgsCount(hello))
+
+    def test_getSignatureNames(self):
+        self.assertEqual(None, getSignatureNames(5))
+        self.assertEqual(None, getSignatureNames((5,).__init_subclass__))
+        self.assertEqual([], getSignatureNames(lambda: 5))
+        self.assertEqual(["x"], getSignatureNames(lambda x: 5))
+        self.assertEqual(["x", "y"], getSignatureNames(lambda x, y: 5))
+        self.assertEqual(["x", "y"], getSignatureNames(lambda x, y=2: 5))
+        self.assertEqual(["x", "y"], getSignatureNames(lambda x=3, y=2: 5))
+
+        def hello(x, y, z=None, *argz, **kwargz):
+            """2 arg, 1 kwarg function"""
+            pass
+        self.assertEqual(["x", "y", "z", "argz", "kwargz"], getSignatureNames(hello))
+
+    def test_getSignatureDefaults(self):
+        self.assertEqual({"y": 5}, getSignatureDefaults(lambda x, y=5: None))
+        self.assertEqual({"x": False, "y": 3.2}, getSignatureDefaults(lambda x=False, y=3.2: None))
+        self.assertEqual({"x": False}, getSignatureDefaults(lambda x=False, *y: None))
+        self.assertEqual({"y": None}, getSignatureDefaults(lambda x, y=None, *z: None))
+        self.assertEqual({"y": None}, getSignatureDefaults(lambda x, y=None, **z: None))
+        self.assertEqual({"y": "test"}, getSignatureDefaults(lambda x, y="test", *args, **z: None))
+
+    def test_changeArgsAndKwargs(self):
+        def wrapper(func):
+            def f(*args, **kwargs):
+                args, kwargs = changeArgsAndKwargs(func, args, kwargs, x=2)
+                return func(*args, **kwargs)
+            return f
+
+        @wrapper
+        def hello(x, y=5):
+            return x * y
+
+        self.assertEqual(10, hello(5))
+        self.assertEqual(10, hello(x=5))
+        self.assertEqual(4, hello(5, 2))
+        self.assertEqual(4, hello(x=5, y=2))
+        self.assertEqual(4, hello(5, y=2))
+
+    def test_getParameter(self):
+        def wrapper(func):
+            def f(*args, **kwargs):
+                self.assertEqual(2, getParameter(func, args, kwargs, "x"))
+                self.assertEqual(5, getParameter(func, args, kwargs, "y"))
+                self.assertEqual(None, getParameter(func, args, kwargs, "z"))
+                return func(*args, **kwargs)
+            return f
+        @wrapper
+        def hello(x, y=5):
+            return x * y
+        hello(2)
+        hello(x=2)
+        hello(2, 5)
+        hello(2, y=5)
+        hello(x=2, y=5)
+
+    def test_defaults(self):
+        self.assertEqual({"a": 5, "b": 3}, defaults({"a": 5}, b=3))
+        self.assertEqual({"a": 5, "b": 3}, defaults({"a": 5, "b": 3}, b=4))
+        self.assertEqual({"a": 5, "b": 3}, defaults({"a": 5, "b": 3}, a=4))
+        self.assertEqual({"a": 5, "b": 3}, defaults({"a": 5, "b": 3}, a=4, b=5))
+        self.assertEqual({"a": 5, "b": 3}, defaults({}, a=5, b=3))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
