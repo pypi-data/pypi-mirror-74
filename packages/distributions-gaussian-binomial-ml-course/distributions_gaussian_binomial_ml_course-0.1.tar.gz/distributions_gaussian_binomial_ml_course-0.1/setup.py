6890a9a64b8fe0e6d06259728ff25e5fa1ffd7ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:50 +0000

    Add package distributions-gaussian-binomial-ml-course/0.1/distributions_gaussian_binomial_ml_course-0.1.tar.gz

---
+++ packages/distributions-gaussian-binomial-ml-course/distributions_gaussian_binomial_ml_course-0.1.tar.gz/distributions_gaussian_binomial_ml_course-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='distributions_gaussian_binomial_ml_course',
+      version='0.1',
+      description='Gaussian and Binomial distributions_ml_course',
+      packages=['distributions_gaussian_binomial_ml_course'],
+      author='jramirezc',
+      zip_safe=False)
