f45aca701d23308021f138efd228086503ebea80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:41 +0000

    Add package tonicapp-helpers/1.9.0/tonicapp-helpers-1.9.0.tar.gz

---
+++ packages/tonicapp-helpers/tonicapp-helpers-1.9.0.tar.gz/tonicapp-helpers-1.9.0/helpers/permissions.py
@@ -0,0 +1,12 @@
+# TODO Waiting for user profile service
+
+from rest_framework.permissions import BasePermission
+
+
+class IsAuthenticated(BasePermission):
+    """
+    Allows access only to authenticated users.
+    """
+
+    def has_permission(self, request, view):
+        return True
