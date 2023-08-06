4e7d7c7f5672965f124f1e10d4c28ac01d86d2f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:28 +0000

    Add package DecryptLogin/0.1.33/DecryptLogin-0.1.33-py3-none-any.whl

---
+++ packages/DecryptLogin/DecryptLogin-0.1.33-py3-none-any.whl/DecryptLogin/utils/__init__.py
@@ -0,0 +1,8 @@
+'''define import all'''
+from .misc import showImage, removeImage, saveImage
+from .cookies import saveSessionCookies, loadSessionCookies
+
+
+'''all utils'''
+__all__ = ['showImage', 'removeImage', 'saveImage',
+           'saveSessionCookies', 'loadSessionCookies']
