0892f6435f14989d7653e866b49a08d096f940e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:50 +0000

    Add package shapeandshare-dicebox-core/3.4.0/shapeandshare-dicebox-core-3.4.0.tar.gz

---
+++ packages/shapeandshare-dicebox-core/shapeandshare-dicebox-core-3.4.0.tar.gz/shapeandshare-dicebox-core-3.4.0/src/shapeandshare/dicebox/models/optimizers.py
@@ -0,0 +1,26 @@
+from enum import Enum
+from random import choices
+
+
+class Optimizers(Enum):
+    ADADELTA = 'adadelta'
+    ADAGRAD = 'adagrad'
+    ADAM = 'adam'
+    ADAMAX = 'adamax'
+    FTRL = 'ftrl'
+    NADAM = 'nadam'
+    RMSPROP = 'rmsprop'
+    SGD = 'sgd'
+
+
+def select_random_optimizer() -> Optimizers:
+    return choices([
+        Optimizers.ADADELTA,
+        Optimizers.ADAGRAD,
+        Optimizers.ADAM,
+        Optimizers.ADAMAX,
+        Optimizers.FTRL,
+        Optimizers.NADAM,
+        Optimizers.RMSPROP,
+        Optimizers.SGD
+    ])[0]
