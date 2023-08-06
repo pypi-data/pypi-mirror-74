549002aea4f40304e751e3e1bb783ee6fe62d118
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:52 +0000

    Add package arus/1.1.10/arus-1.1.10-py3-none-any.whl

---
+++ packages/arus/arus-1.1.10-py3-none-any.whl/arus/__init__.py
@@ -0,0 +1,21 @@
+"""
+__arus__ python package provides a computation framework to manage and process ubiquitous sensory data for activity recognition.
+
+"""
+
+__version__ = '1.1.10'
+
+from . import accelerometer as accel
+from . import cli, dataset
+from . import developer as dev
+from . import env
+from . import extensions as ext
+from . import mhealth_format as mh
+from . import (operator, plugins, processor, segmentor, stream2, synchronizer,
+               testing)
+from .generator import *
+from .moment import Moment
+from .node import Node
+from .scheduler import Scheduler
+from .stream import Stream
+from . import models
