f116c927e9e13e0d89b96fc3dea4d74dd68ccf57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:34 +0000

    Add package tbump/6.1.1/tbump-6.1.1-py3-none-any.whl

---
+++ packages/tbump/tbump-6.1.1-py3-none-any.whl/tbump/test/test_init.py
@@ -0,0 +1,28 @@
+from path import Path
+import tomlkit
+
+import tbump.config
+import tbump.main
+
+import pytest
+from cli_ui.tests import MessageRecorder
+
+
+def test_creates_config(test_repo: Path) -> None:
+    tbump_path = test_repo / "tbump.toml"
+    tbump_path.remove()
+    current_version = "1.2.41-alpha1"
+
+    tbump.main.main(["-C", test_repo, "init", current_version])
+
+    assert tbump_path.exists()
+    config = tomlkit.loads(tbump_path.text())
+    assert config["version"]["current"] == "1.2.41-alpha1"
+
+
+def test_abort_if_tbump_toml_exists(
+    test_repo: Path, message_recorder: MessageRecorder
+) -> None:
+    with pytest.raises(SystemExit):
+        tbump.main.main(["-C", test_repo, "init", "1.2.41-alpha1"])
+    assert message_recorder.find("already exists")
