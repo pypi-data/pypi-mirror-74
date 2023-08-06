84a1476ece0bd502179af3bb96c79c36bf091329
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:53 +0000

    Add package dytss-distributions-exercise/0.1/dytss_distributions_exercise-0.1.tar.gz

---
+++ packages/dytss-distributions-exercise/dytss_distributions_exercise-0.1.tar.gz/dytss_distributions_exercise-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='dytss_distributions_exercise',
+      version='0.1',
+      description='Gaussian and Binomial Distributions',
+      packages=['dytss_distributions_exercise'],
+      zip_safe=False)
