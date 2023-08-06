eaffc6892c2af1150d151ffe2ece8fcbdd2d7f99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:00 +0000

    Add package networkqa/0.0.18/networkqa-0.0.18.tar.gz

---
+++ packages/networkqa/networkqa-0.0.18.tar.gz/networkqa-0.0.18/README.md
@@ -0,0 +1,9 @@
+﻿# Network QA Module
+
+## Purpose
+
+To provide OOP tools to automating the quality assurance checks against switches.
+
+## About
+
+Cogito Ergo Sum
---
+++ packages/networkqa/networkqa-0.0.18.tar.gz/networkqa-0.0.18/setup.py
@@ -0,0 +1,28 @@
+﻿from setuptools import setup, find_namespace_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, "README.md"), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name="networkqa",
+    version="0.0.18",
+    author="Richard Lalonde",
+    author_email="richard.lalonde@wwt.com",
+    description="A module to aid in automated qa of networking devices",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="http://naic-gitlab.wwt.com/lalonder/networkqa",
+    package_dir={'': 'src'},
+    packages=find_namespace_packages(where="src"),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=["openpyxl"]
+)
