6a512e39624d023252a6a222a1b8be0b13cf911e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:25 +0000

    Add package safrs/2.9.1/safrs-2.9.1.tar.gz

---
+++ packages/safrs/safrs-2.9.1.tar.gz/safrs-2.9.1/safrs/response.py
@@ -0,0 +1,11 @@
+# Response class
+from flask import Response
+
+
+class SAFRSResponse(Response):
+    """
+        Response class
+    """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
