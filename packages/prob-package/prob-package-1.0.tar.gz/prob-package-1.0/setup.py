c7a31d7647fce4c921fff03b92144cb80ddaca8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:35 +0000

    Add package prob-package/1.0/prob-package-1.0.tar.gz

---
+++ packages/prob-package/prob-package-1.0.tar.gz/prob-package-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='prob-package',
+      version='1.0',
+      description='Gaussian distributions',
+      packages=['prob-package'],
+      author = 'Sachin Vakkund',
+      author_email = 'sachinvakkund6@gmail.com',
+      zip_safe=False)
