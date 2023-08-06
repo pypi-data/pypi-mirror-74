079590df5a1448fe450549df5f031d450e4b547f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:44 +0000

    Add package Aeros/1.0.6/Aeros-1.0.6-py3-none-any.whl

---
+++ packages/Aeros/Aeros-1.0.6-py3-none-any.whl/Aeros/__init__.py
@@ -0,0 +1,11 @@
+from .WebServer import *
+from .misc import *
+
+from .threading import AdvancedThread
+from .compression import Compression
+from .caching import (
+    SimpleCache,
+    Cache,
+    FilesystemCache,
+    RedisCache
+)
