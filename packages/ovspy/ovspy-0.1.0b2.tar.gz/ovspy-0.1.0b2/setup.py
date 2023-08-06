e0e4fb5017161ebf0dbcc707a3c700c586e0b550
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:29 +0000

    Add package ovspy/0.1.0b2/ovspy-0.1.0b2.tar.gz

---
+++ packages/ovspy/ovspy-0.1.0b2.tar.gz/ovspy-0.1.0b2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+requires = []
+
+setup(
+    name='ovspy',
+    version='0.1.0b2',
+    description='Open vSwitch Mnipulation Library',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/kamaboko123/ovspy',
+    author='kamaboko123',
+    author_email='6112062+kamaboko123@users.noreply.github.com',
+    license='MIT',
+    packages=['ovspy'],
+    install_requires=requires,
+    classifiers=[
+        'Programming Language :: Python :: 3.5',
+    ]
+)
+
