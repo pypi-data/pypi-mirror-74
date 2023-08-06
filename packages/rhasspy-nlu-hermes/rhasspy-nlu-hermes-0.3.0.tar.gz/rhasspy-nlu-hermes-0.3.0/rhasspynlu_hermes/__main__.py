75d250b3efcd810c78a4f8c02afde36cba57c764
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:53 +0000

    Add package rhasspy-nlu-hermes/0.3.0/rhasspy-nlu-hermes-0.3.0.tar.gz

---
+++ packages/rhasspy-nlu-hermes/rhasspy-nlu-hermes-0.3.0.tar.gz/rhasspy-nlu-hermes-0.3.0/rhasspynlu_hermes/__main__.py
@@ -0,0 +1,94 @@
+"""Hermes MQTT service for rhasspynlu"""
+import argparse
+import asyncio
+import logging
+import typing
+from pathlib import Path
+
+import paho.mqtt.client as mqtt
+import rhasspyhermes.cli as hermes_cli
+
+from . import NluHermesMqtt
+
+_LOGGER = logging.getLogger("rhasspynlu_hermes")
+
+# -----------------------------------------------------------------------------
+
+
+def main():
+    """Main method."""
+    parser = argparse.ArgumentParser(prog="rhasspy-nlu-hermes")
+    parser.add_argument("--intent-graph", help="Path to intent graph (gzipped pickle)")
+    parser.add_argument(
+        "--casing",
+        choices=["upper", "lower", "ignore"],
+        default="ignore",
+        help="Case transformation for input text (default: ignore)",
+    )
+    parser.add_argument(
+        "--no-fuzzy", action="store_true", help="Disable fuzzy matching in graph search"
+    )
+    parser.add_argument(
+        "--replace-numbers",
+        action="store_true",
+        help="Replace digits with words in queries (75 -> seventy five)",
+    )
+    parser.add_argument(
+        "--language", help="Language/locale used for number replacement (default: en)"
+    )
+
+    hermes_cli.add_hermes_args(parser)
+
+    args = parser.parse_args()
+
+    hermes_cli.setup_logging(args)
+    _LOGGER.debug(args)
+
+    # Convert to Paths
+    if args.intent_graph:
+        args.intent_graph = Path(args.intent_graph)
+
+    # Listen for messages
+    client = mqtt.Client()
+    hermes = NluHermesMqtt(
+        client,
+        graph_path=args.intent_graph,
+        word_transform=get_word_transform(args.casing),
+        replace_numbers=args.replace_numbers,
+        language=args.language,
+        fuzzy=(not args.no_fuzzy),
+        site_ids=args.site_id,
+    )
+
+    _LOGGER.debug("Connecting to %s:%s", args.host, args.port)
+    hermes_cli.connect(client, args)
+    client.loop_start()
+
+    try:
+        # Run event loop
+        asyncio.run(hermes.handle_messages_async())
+    except KeyboardInterrupt:
+        pass
+    finally:
+        _LOGGER.debug("Shutting down")
+        client.loop_stop()
+
+
+# -----------------------------------------------------------------------------
+
+
+def get_word_transform(name: str) -> typing.Callable[[str], str]:
+    """Gets a word transformation function by name."""
+    if name == "upper":
+        return str.upper
+
+    if name == "lower":
+        return str.lower
+
+    return lambda s: s
+
+
+# -----------------------------------------------------------------------------
+
+if __name__ == "__main__":
+    main()
