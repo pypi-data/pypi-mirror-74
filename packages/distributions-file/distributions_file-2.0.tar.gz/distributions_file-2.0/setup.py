28b7521d48ce5d7c02d198dbd62e7d204d6cee3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:55 +0000

    Add package distributions-file/2.0/distributions_file-2.0.tar.gz

---
+++ packages/distributions-file/distributions_file-2.0.tar.gz/distributions_file-2.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_file',
+      version='2.0',
+      description='Gaussian distributions',
+      packages=['distributions_file'],
+      author_email = "bsombit@gmail.com",
+      author = "Sombit Bose",
+      zip_safe=False)
