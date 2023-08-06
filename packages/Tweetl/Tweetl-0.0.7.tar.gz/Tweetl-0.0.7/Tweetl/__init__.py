63aeb5e24b4a4b36e4dd16e5834246ff985d82a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:55 +0000

    Add package Tweetl/0.0.7/Tweetl-0.0.7.tar.gz

---
+++ packages/Tweetl/Tweetl-0.0.7.tar.gz/Tweetl-0.0.7/Tweetl/__init__.py
@@ -0,0 +1,9 @@
+from Tweetl.get_tweets import GetTweet
+from Tweetl.cleansing_tweets import CleansingTweets
+
+__copyright__    = 'Copyright (C) 2020 deepblue'
+__version__      = '0.0.7'
+__license__      = 'MIT'
+__author__       = 'deepblue'
+__author_email__ = 'main@deepbluets.page'
+__url__          = 'https://github.com/deepblue-ts/Tweetl'
