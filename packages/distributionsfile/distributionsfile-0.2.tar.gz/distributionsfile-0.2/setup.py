51e3bce2d09649e0bc576dca3563b0b9c8b3ba21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:33 +0000

    Add package distributionsfile/0.2/distributionsfile-0.2.tar.gz

---
+++ packages/distributionsfile/distributionsfile-0.2.tar.gz/distributionsfile-0.2/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributionsfile',
+      version='0.2',
+      description='Gaussian distributions',
+      packages=['distributionsfile'],
+      zip_safe=False)
