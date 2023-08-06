5c683b1f416801d07677fba9a963aa2b8b47b259
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:40 +0000

    Add package dsh-distributions/0.2/dsh_distributions-0.2.tar.gz

---
+++ packages/dsh-distributions/dsh_distributions-0.2.tar.gz/dsh_distributions-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dsh_distributions',
+      version='0.2',
+      description='Gaussian and Binomial distributions',
+      packages=['dsh_distributions'],
+      author = 'Dilan Sarith Hewawitharana',
+      author_email = 'dshewawitharana@gmail.com',
+      zip_safe=False)
