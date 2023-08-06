4c55b5e371239b6b3693d445bf2aeeaee1900493
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:50 +0000

    Add package chordparser/0.3.8/chordparser-0.3.8.tar.gz

---
+++ packages/chordparser/chordparser-0.3.8.tar.gz/chordparser-0.3.8/README.rst
@@ -0,0 +1,82 @@
+===========
+chordparser
+===========
+
+.. image:: https://travis-ci.org/titus-ong/chordparser.svg?branch=master
+   :alt: build status
+   :target: https://travis-ci.org/titus-ong/chordparser
+
+.. image:: https://readthedocs.org/projects/chordparser/badge/?version=latest
+    :target: https://chordparser.readthedocs.io/en/latest/?badge=latest
+    :alt: Documentation Status
+
+.. image:: https://coveralls.io/repos/github/titus-ong/chordparser/badge.svg?branch=master
+   :alt: coverage
+   :target: https://coveralls.io/github/titus-ong/chordparser
+
+.. image:: https://img.shields.io/pypi/v/chordparser.svg
+   :target: https://pypi.org/pypi/chordparser
+   :alt: downloads
+
+.. image:: https://img.shields.io/pypi/pyversions/chordparser.svg
+   :target: https://pypi.org/pypi/chordparser
+   :alt: downloads
+
+chordparser is a Python 3 package that provides a musical framework to analyse chords. Chord notation can be parsed into Chords, which can then be analysed against other chords or the key of the song. This allows for harmonic progression analysis in chord sheets and helps musicians understand why and how chord progressions work.
+
+--------
+Features
+--------
+
+* Create and manipulate notes, keys, scales and chords easily
+* Accept complex chord notations
+* Transpose musical classes easily and accurately
+* Automatically generate notes for scales and chords from notation
+* Parse and store chord information to be easily analysed
+* Generate roman numeral notation from chords
+* Analyse chord-key/scale relationships
+
+------------
+Installation
+------------
+
+To install chordparser, run this command in your terminal:
+
+.. code-block:: console
+
+    $ pip install chordparser
+
+-----
+Usage
+-----
+
+For a quick introduction and a working example, check out the `Colab notebook <https://colab.research.google.com/drive/1T5WcH2WMHqpqbJrzxDt_Mg03lw1aXho7?usp=sharing>`_.
+
+The full documentation can be found `here <https://chordparser.readthedocs.io/en/latest/>`_.
+
+----------
+Contribute
+----------
+
+- Issue Tracker: github.com/titus-ong/chordparser/issues
+- Source Code: github.com/titus-ong/chordparser
+
+-------
+Support
+-------
+
+If you are having issues, please let me know at: titusongyl@gmail.com
+
+-------
+Authors
+-------
+
+Development Lead
+
+* Titus Ong <titusongyl@gmail.com>
+
+-------
+License
+-------
+
+The project is licensed under the MIT license.
---
+++ packages/chordparser/chordparser-0.3.8.tar.gz/chordparser-0.3.8/src/chordparser/editors/notes_editor.py
@@ -0,0 +1,93 @@
+import re
+
+from chordparser.music.notes import Note
+
+
+class NoteEditor:
+    """
+    NoteEditor class that can create a Note and get intervals between Notes.
+
+    The NoteEditor class can create a Note using the 'create_note' method by accepting a string with notation a-g or A-G and optional accidental symbols. The symbols that can be used are b (flat), bb (doubleflat), # (sharp), ## (doublesharp) and their respective unicode characters.
+
+    The NoteEditor can also find the intervals between Notes using the 'get_intervals', 'get_min_intervals' and 'get_tone_notes' methods. The 'change_note' method allows for changing a note's value.
+    """
+    _flat = '\u266d'
+    _sharp = '\u266f'
+    _doubleflat = '\U0001D12B'
+    _doublesharp = '\U0001D12A'
+    _symbol_converter = {
+        _flat: _flat, _doubleflat: _doubleflat,
+        _sharp: _sharp, _doublesharp: _doublesharp,
+        'b': _flat, 'bb': _doubleflat,
+        '#': _sharp, '##': _doublesharp,
+        None: '',
+    }
+    _notes_tuple = (
+        'C', 'D', 'E', 'F', 'G', 'A', 'B',
+        'C', 'D', 'E', 'F', 'G', 'A', 'B')
+    _pattern = (
+        '^([a-gA-G])(\u266F|\u266D|\U0001D12B|\U0001D12A'
+        '|bb|##|b|#){0,1}$'
+    )
+
+    def create_note(self, value):
+        """Create a Note from a string."""
+        letter, symbol = self._parse_note(value)
+        return Note(letter, symbol)
+
+    def _parse_note(self, value):
+        """Parse the note string."""
+        rgx = re.match(NoteEditor._pattern, value, re.UNICODE)
+        if not rgx:
+            raise SyntaxError(f"'{value}' could not be parsed")
+        letter = rgx.group(1).upper()
+        symbol = NoteEditor._symbol_converter.get(rgx.group(2))
+        return letter, symbol
+
+    def get_tone_letter(self, *notes):
+        """Return a nested tuple of (semitone interval, letter interval) between notes."""
+        semitones = self.get_intervals(*notes)
+        letters = self.get_letter_intervals(*notes)
+        return tuple(zip(semitones, letters))
+
+    def get_letter_intervals(self, *notes):
+        """Return a tuple of letter intervals between notes."""
+        old_note = NoteEditor._notes_tuple.index(notes[0].letter)
+        note_diff = []
+        for each in notes:
+            new_note = NoteEditor._notes_tuple.index(each.letter)
+            note_diff.append((new_note - old_note) % 7)
+            print(new_note, old_note)
+            old_note = new_note
+        note_diff.pop(0)
+        return tuple(note_diff)
+
+    def get_intervals(self, *notes):
+        """Return a tuple of semitone intervals between notes."""
+        old_val = notes[0].num_value()
+        intervals = []
+        for each in notes:
+            new_val = each.num_value()
+            intervals.append((new_val - old_val) % 12)
+            old_val = new_val
+        intervals.pop(0)
+        return tuple(intervals)
+
+    def get_min_intervals(self, *notes):
+        """Return a tuple of the shortest semitone distance between notes."""
+        intervals = self.get_intervals(*notes)
+        min_int = []
+        for each in intervals:
+            if each > (12 - each):
+                shift = each - 12
+            else:
+                shift = each
+            min_int.append(shift)
+        return tuple(min_int)
+
+    def change_note(self, note, value, inplace=True):
+        """Change a note's value. Use inplace=True to return a new note."""
+        if not inplace:
+            note = self.create_note("C")
+        note.letter, note.symbol = self._parse_note(value)
+        return note
