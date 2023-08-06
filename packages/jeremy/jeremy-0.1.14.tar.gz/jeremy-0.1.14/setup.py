c5e5313d7b0ea6b8a19e7426cb2c4bb10dbc4882
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:37 +0000

    Add package jeremy/0.1.14/jeremy-0.1.14.tar.gz

---
+++ packages/jeremy/jeremy-0.1.14.tar.gz/jeremy-0.1.14/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['jer', 'jer.things']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['magicdb>=0.0.6,<0.0.7', 'requests>=2.24.0,<3.0.0']
+
+setup_kwargs = {
+    'name': 'jeremy',
+    'version': '0.1.14',
+    'description': '',
+    'long_description': '# Jeremy Poetry Test\n\nThe awesome test!',
+    'author': 'Jeremy Berman',
+    'author_email': 'jerber@sas.upenn.edu',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
