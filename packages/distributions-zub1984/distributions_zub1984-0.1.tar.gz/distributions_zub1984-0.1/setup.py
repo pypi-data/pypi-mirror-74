573f4bdc7f58c819ed1857a7f0f926bfc8636909
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:09 +0000

    Add package distributions-zub1984/0.1/distributions_zub1984-0.1.tar.gz

---
+++ packages/distributions-zub1984/distributions_zub1984-0.1.tar.gz/distributions_zub1984-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_zub1984',
+      version='0.1',
+      author="Jubair",
+      author_email="something@gmail.com",
+      description='Gaussian distributions',
+      packages=['distributions_zub1984'],
+      zip_safe=False)
