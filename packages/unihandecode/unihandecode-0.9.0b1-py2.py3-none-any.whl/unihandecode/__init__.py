2660d9a0a4daaf45749d1a08800495d9538f8f18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:05 +0000

    Add package unihandecode/0.9.0b1/unihandecode-0.9.0b1-py2.py3-none-any.whl

---
+++ packages/unihandecode/unihandecode-0.9.0b1-py2.py3-none-any.whl/unihandecode/__init__.py
@@ -0,0 +1,134 @@
+# -*- coding: utf-8 -*-
+
+__license__ = 'GPL 3'
+__copyright__ = '2010-2020 Hiroshi Miura <miurahr@linux.com>'
+__docformat__ = 'restructuredtext en'
+__all__ = ["Unihandecoder"]
+
+'''
+Decode unicode text to an ASCII representation of the text.
+Translate unicode characters to ASCII.
+
+inspired from John's unidecode library.
+Copyright(c) 2009, John Schember
+
+Transiliterate the string from unicode characters to ASCII in Chinese and others.
+Decode unicode text to an ASCII representation of the text for Japanese.
+
+API is based on the python unidecode,
+which is based on Ruby gem (http://rubyforge.org/projects/unidecode/)
+and  perl module Text::Unidecode
+(http://search.cpan.org/~sburke/Text-Unidecode-0.04/).
+
+Copyright (c) 2010,2015,2018,2020 Hiroshi Miura
+'''
+import os
+import pickle
+import pkg_resources
+from typing import Dict, Optional
+
+import unicodedata
+import pykakasi  # type: ignore
+
+
+class Unihandecoder:
+    preferred_encoding = None
+    decoder = None
+
+    def __init__(self, lang='zh', encoding='utf-8'):
+        self.preferred_encoding = encoding
+        if lang == "ja":
+            self.decoder = Jadecoder()
+        elif lang == "kr":
+            self.decoder = Krdecoder()
+        elif lang == "vn":
+            self.decoder = Vndecoder()
+        elif lang == "zh":
+            self.decoder = Unidecoder('zh')
+        else:
+            self.decoder = Unidecoder(lang)
+
+    def decode(self, text):
+        return self.decoder.decode(unicodedata.normalize('NFC', text))
+
+
+class CodePoints:
+
+    def __init__(self, lang='zh'):
+        self.config = Configurations()
+        self.codepoints = {}  # type: Dict[int, Optional[str]]
+        for c in ['unicodepoints.pickle', '%scodepoints.pickle' % lang]:
+            with open(self.config.datapath(c), 'rb') as data:
+                dic = pickle.load(data)
+                self.codepoints.update(dic)
+
+    def __getitem__(self, item):
+        try:
+            return self.codepoints[item]
+        except LookupError:
+            return None
+
+
+class Unidecoder:
+
+    def __init__(self, lang='zh'):
+        self.codepoints = CodePoints(lang)
+
+    def decode(self, text):
+        return text.translate(self.codepoints)
+
+
+_unidecoder = None
+
+
+def unidecode(text):
+    global _unidecoder
+    if _unidecoder is None:
+        _unidecoder = Unihandecoder()
+    return _unidecoder.decode(text)
+
+
+class Jadecoder(Unidecoder):
+
+    def __init__(self):
+        super(Jadecoder, self).__init__('ja')
+        self.kakasi = pykakasi.kakasi()
+        self.kakasi.setMode("J", "a")
+        self.kakasi.setMode("E", "a")
+        self.kakasi.setMode("H", "a")
+        self.kakasi.setMode("K", "a")
+        self.kakasi.setMode("s", True)
+        self.kakasi.setMode("C", True)
+        self.conv = self.kakasi.getConverter()
+
+    def decode(self, text):
+        result = self.conv.do(text)
+        return result.translate(self.codepoints)
+
+
+class Krdecoder(Unidecoder):
+
+    def __init__(self):
+        super(Krdecoder, self).__init__('kr')
+
+
+class Vndecoder(Unidecoder):
+
+    def __init__(self):
+        super(Vndecoder, self).__init__('vn')
+
+
+# This class is Borg
+class Configurations(object):
+
+    _shared_state = {}  # type: Dict[str, object]
+
+    _data_path = pkg_resources.resource_filename(__name__, 'data')
+
+    def __new__(cls, *p, **k):
+        self = object.__new__(cls, *p, **k)
+        self.__dict__ = cls._shared_state
+        return self
+
+    def datapath(self, c: str):
+        return os.path.join(self._data_path, c)
---
+++ packages/unihandecode/unihandecode-0.9.0b1-py2.py3-none-any.whl/unihandecode/gencodemap/unihan_conv.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import re
+import pickle
+from typing import Dict, Optional, Tuple
+
+
+class UnihanConv:
+
+    readings = {}  # type: Dict[str, Tuple[str, int]]
+    firsttime = True
+    lang = None
+    priority = {
+        "kr": ["kKorean", "kMandarin", "kJapaneseOn", "kVietnamese", "kCantonese", "kJapaneseKun"],
+        "zh": ["kMandarin", "kCantonese", "kKorean", "kJapaneseOn", "kVietnamese", "kJapaneseKun"],
+        "yue": ["kCantonese", "kMandarin", "kKorean", "kJapaneseOn", "kVietnamese", "kJapaneseKun"],
+        "ja": ["kJapaneseOn", "kJapaneseKun", "kMandarin", "kCantonese", "kKorean", "kVietnamese"],
+        "vn": ["kVietnamese", "kMandarin", "kCantonese", "kJapaneseOn", "kJapaneseKun", "kKorean"],
+    }
+    pronounce_char_map = {
+        "â": "a",
+        "à": "a",
+        "ắ": "a",
+        "ă": "a",
+        "ấ": "a",
+        "ü": "u",
+        "ụ": "u",
+        "ú": "u",
+        "ử": "u",
+        "ư": "u",
+        "ù": "u",
+        "é": "e",
+        "ọ": "o",
+        "ố": "o",
+        "ộ": "o",
+        "ơ": "o",
+        "ớ": "o",
+    }
+
+    def __init__(self, lang):
+        self.tbl = {}  # type: Dict[int, Optional[str]]
+        self.prio = {}  # type: Dict[int, int]
+        if lang in self.priority:
+            self.lang = lang
+        else:
+            self.lang = "zh"
+
+    def run(self, source, dest):
+        with open(source, "r", encoding="utf8") as f:
+            self.process_file(f)
+        with open(dest, "wb") as outfile:
+            pickle.dump(self.tbl, outfile)
+
+    def process_file(self, f):
+        # Retrive U+XXXX from a first of line in a definition
+        # for example;
+        # U+3432	kMandarin	DAI4
+        # r1 is a expression to get U+<char code>
+        r1 = re.compile(r"U\+([0-9A-F]{4,5})\b")
+        r2 = re.compile(r"(\w+)[1-5]")
+        r3 = re.compile(r"\w+\.\w+:(\w+)")
+        for line in f:
+            items = line[:-1].split("\t")
+            if items[0].startswith('#'):  # Skip comment lines
+                continue
+            try:
+                code = int(r1.sub(r"\1", items[0]), 16)
+            except ValueError:  # FIXME: show warning
+                continue
+            category = items[1]
+            try:
+                p = self.priority[self.lang].index(category)
+            except ValueError:
+                # When there is no definition for category, just ignore.
+                continue
+            if (code not in self.prio) or (p < self.prio[code]):
+                self.prio[code] = p
+                try:
+                    pron = re.sub("[^\00-\x7f]",
+                                  lambda x: self.pronounce_char_map[x.group()], items[2].split(" ")[0]).capitalize()
+                except KeyError:
+                    # FIXME: now skip non-ascii values
+                    continue
+                if category in ["kMandarin", "kCantonese"]:
+                    self.tbl[code] = r2.sub(r"\1 ", pron)
+                elif category == "kHanyuPinyin":  # pragma: no branch
+                    self.tbl[code] = r3.sub(r"\1 ", pron)  # pragma: no cover
+                else:
+                    self.tbl[code] = "%s " % pron
