dc5c451d3cfa5e7d8862cb4319efc9fdebe75a0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:42 +0000

    Add package typedi/0.4.0/typedi-0.4.0.tar.gz

---
+++ packages/typedi/typedi-0.4.0.tar.gz/typedi-0.4.0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='typedi',
+    version='0.4.0',
+    description='Simple yet powerful typed dependency injection container',
+    url='https://github.com/bshishov/typedi',
+    author='Boris Shishov',
+    author_email='borisshishov@gmail.com',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    py_modules=['typedi'],
+    license="MIT",
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Libraries'
+    ],
+    python_requires='>=3.6'
+)
