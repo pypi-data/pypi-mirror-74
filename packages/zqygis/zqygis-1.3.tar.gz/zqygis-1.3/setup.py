cf65cf8f738d52ed3466129fa0a5ad4eba2ef37b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:48 +0000

    Add package zqygis/1.3/zqygis-1.3.tar.gz

---
+++ packages/zqygis/zqygis-1.3.tar.gz/zqygis-1.3/setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+setup(
+    name='zqygis',
+    version='1.3',
+    description='zqygis',
+    author='zqy',#作者
+    author_email='252238741@qq.com',
+    url='https://github.com/xxx',
+    #packages=find_packages(),
+    packages=['zqygis'],  #这里是所有代码所在的文件夹名称
+    install_requires=['requests'],
+)
