92e034424d464c8435f5efff032bbb8bc67c398c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:30 +0000

    Add package lenluhub_utils/0.0.8/lenluhub_utils-0.0.8.tar.gz

---
+++ packages/lenluhub_utils/lenluhub_utils-0.0.8.tar.gz/lenluhub_utils-0.0.8/lenluhub_utils/__init__.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+#coding:utf-8
+
+"""
+@author: yangst2
+@contact: yangst2@lenovo.com
+@software: PyCharm
+@file: __init__.py.py
+@time: 2020/4/12 6:55
+"""
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
