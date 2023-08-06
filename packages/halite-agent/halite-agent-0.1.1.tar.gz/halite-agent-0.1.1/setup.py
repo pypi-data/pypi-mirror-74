496d95e9a1dcfffcef9ec909d3e1537064378e89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:14:10 +0000

    Add package halite-agent/0.1.1/halite-agent-0.1.1.tar.gz

---
+++ packages/halite-agent/halite-agent-0.1.1.tar.gz/halite-agent-0.1.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='halite-agent',
+    version='0.1.1',
+    license='MIT',
+    author='Iain Wong',
+    author_email='iainwong@outlook.com',
+    description='The challenge is to create an agent that can succeed in the game of Halite IV.  (Kaggle Proj) https://www.kaggle.com/c/halite',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/iainwo/kaggle/tree/master/halite',
+    packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'pandas',
+        'python-dotenv',
+        'Click',
+    ],
+    python_requires='>=3.6',
+)
