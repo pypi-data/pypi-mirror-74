a35d448b8da30e11d23e3e54686657a08d463275
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:13 +0000

    Add package saj-distributions/0.1/saj_distributions-0.1.tar.gz

---
+++ packages/saj-distributions/saj_distributions-0.1.tar.gz/saj_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='saj_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['saj_distributions'],
+      author='sajanraj',
+      author_email='sajanraj.t.d@gmail.com',
+      zip_safe=False)
