3286d9133165406725537c232b615e06633b7923
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:45 +0000

    Add package dimensigon/0.1b15/dimensigon-0.1b15.tar.gz

---
+++ packages/dimensigon/dimensigon-0.1b15.tar.gz/dimensigon-0.1b15/setup.py
@@ -0,0 +1,57 @@
+from setuptools import setup, find_packages
+
+import dm
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='dimensigon',
+    version=dm.__version__,
+    packages=find_packages(),
+    url='https://github.com/dimensigon/dimensigon',
+    license=dm.__license__,
+    author=dm.__author__,
+    author_email=dm.__email__,
+    description="Dimensigon (DM) is a Distributed Management and Automation RESTful framework to make easier heterogeneous/polyglot orchestrations in a Hybrid Multi-Cloud environment.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=[
+        "beautifulsoup4",
+        "PyYAML",
+        "six",
+        "flask",
+        "flask-JWT-Extended",
+        "flask-Migrate",
+        "flask-RESTFul",
+        "flask-SQLAlchemy",
+        "requests",
+        "rsa",
+        "coverage",
+        "jsonschema",
+        "aiohttp",
+        "dataclasses",
+        "cryptography",
+        "psutil",
+        "python-dotenv",
+        "netifaces",
+        "apscheduler",
+        "passlib",
+        "jinja2",
+        "SQLAlchemy",
+        "Werkzeug",
+        "jinja2schema",
+        "RestrictedPython",
+        "setuptools",
+        "click",
+        "pygments"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: POSIX",
+    ],
+    python_requires='>=3.6',
+    entry_points = {'console_scripts': ['dshell = dm.dshell.batch.dshell:main',
+                                        'dimensigon = dimensigon:main']},
+)
