32dfe121c2da57238e5850c6a6f25d335e42725c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:26 +0000

    Add package spellbot/3.4.1/spellbot-3.4.1.tar.gz

---
+++ packages/spellbot/spellbot-3.4.1.tar.gz/spellbot-3.4.1/src/spellbot/constants.py
@@ -0,0 +1,4 @@
+# Application Settings
+ADMIN_ROLE = "SpellBot Admin"
+CREATE_ENDPOINT = "https://us-central1-magic-night-30324.cloudfunctions.net/createGame"
+THUMB_URL = "https://raw.githubusercontent.com/lexicalunit/spellbot/master/spellbot.png"
