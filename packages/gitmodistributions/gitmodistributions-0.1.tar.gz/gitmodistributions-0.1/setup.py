9ade2f7480e3414eedf63dc29bd5917ad7ed3171
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:55 +0000

    Add package gitmodistributions/0.1/gitmodistributions-0.1.tar.gz

---
+++ packages/gitmodistributions/gitmodistributions-0.1.tar.gz/gitmodistributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='gitmodistributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['gitmodistributions'],
+      zip_safe=False)
