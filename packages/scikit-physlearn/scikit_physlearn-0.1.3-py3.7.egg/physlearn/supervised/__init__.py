289af8f7cf019ad9b1109d4ddd5a6b0486c61018
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:17 +0000

    Add package scikit-physlearn/0.1.3/scikit_physlearn-0.1.3-py3.7.egg

---
+++ packages/scikit-physlearn/scikit_physlearn-0.1.3-py3.7.egg/physlearn/supervised/__init__.py
@@ -0,0 +1,24 @@
+from __future__ import absolute_import
+
+
+try:
+    from .interface import RegressorDictionaryInterface
+    from .regression import BaseRegressor, Regressor
+except ImportError:
+    pass
+
+try:
+    from .interpretation.interpret_regressor import ShapInterpret
+except ImportError:
+    pass
+
+try:
+    from .model_selection.cv_comparison import plot_cv_comparison
+    from .model_selection.learning_curve import LearningCurve, plot_learning_curve
+    from .model_selection.bayesian_search import _bayesoptcv
+except ImportError:
+    pass
+
+__all__ = ['BaseRegressor', 'Regressor', 'RegressorDictionaryInterface', 
+           'ShapInterpret', 'plot_cv_comparison', 'LearningCurve',
+           'plot_learning_curve', '_bayesoptcv']
