00e29c3c82cc4cea1a03308ff4706a5aad71cfc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:46 +0000

    Add package dist-gaussian-binomial/1.1/dist_gaussian_binomial-1.1.tar.gz

---
+++ packages/dist-gaussian-binomial/dist_gaussian_binomial-1.1.tar.gz/dist_gaussian_binomial-1.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='dist_gaussian_binomial',
+      version='1.1',
+      description='Probability Distributions - Gaussian and Binomial',
+      packages=['dist_gaussian_binomial'],
+      author='Manish Agrawal',
+      zip_safe=False)
