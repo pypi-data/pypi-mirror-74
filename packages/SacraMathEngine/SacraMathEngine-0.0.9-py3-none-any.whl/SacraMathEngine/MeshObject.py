7094a77d3c72a139124e7126b170461e30182e75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:19 +0000

    Add package SacraMathEngine/0.0.9/SacraMathEngine-0.0.9-py3-none-any.whl

---
+++ packages/SacraMathEngine/SacraMathEngine-0.0.9-py3-none-any.whl/SacraMathEngine/MeshObject.py
@@ -0,0 +1,245 @@
+import os
+import json
+from .Vector import vec3d, vec4d
+from .Triangle import Triangle
+from .Matrix import matrix3d
+
+class PathError(Exception):
+    """Error-handeling."""
+    pass
+
+class MeshObjectError(Exception):
+    """Error-handeling."""
+    pass
+
+
+class MeshObject:
+
+    AllActiveMeshes = []
+    MeshCenterOfMass = []
+
+    def __init__(self, Iterated = None):
+        """A small __init__ function, pass no argument, it's only when scaling or adding something the Iterated argument
+        is used."""
+        if Iterated != None:
+            self.Mesh = Iterated #Everything should be in correct format by this point.
+            #print(self.Mesh)
+            self.AllActiveMeshes.append(self.Mesh)
+            self.CenterOfMass()
+            #print(self.Mesh)
+        else:
+            self.Mesh = None #Just initialize the artibute self.Mesh
+
+    def setter(self, NameOfObject):
+        """The argument NameOfObject is path to the MeshObject, of which should be in json format. The path will be found
+        as in the Saves folder in SacraGameEngine."""
+        try:
+            path = os.path.join('/Users/andreasevensen/Documents/GitHub/Sacra/saves', NameOfObject + '.json') #change when done
+            with open(path) as file:
+                Data = json.load(file)
+            for x in Data.keys():
+                #If there are multiple keys in the .json file, the last key will be used.
+                ListMesh = Data[x]
+            #print(ListMesh[0])
+        except:
+            raise PathError("Current file do not exits.")
+        Vectors = []
+        self.Mesh = []
+        for vec in ListMesh:
+            vec = vec3d(vec[0], vec[1], vec[2])
+            Vectors.append(vec)
+        for i in range(0, len(Vectors)-2, 3):
+            self.Mesh.append(Triangle(Vectors[i], Vectors[i + 1], Vectors[i + 2]))
+        """
+        for key in Data.keys():
+            print(key, Data[key])
+        """ # Small thing to get acess to the name
+        self.AllActiveMeshes.append(self.Mesh)
+        self.CenterOfMass()
+        return '[System:] Intalization have been completed.'
+
+    def getter(self):
+        return f'{self.Mesh}'
+
+    def __repr__(self):
+        return f'{self.Mesh}'
+
+    def __str__(self):
+        return f'{self.Mesh}'
+
+    def __add__(self, Object):
+        if isinstance(Object, Triangle):
+            NewMesh = self.Mesh.copy()
+            NewMesh.append(Object)
+            return MeshObject(NewMesh)
+        elif isinstance(Object, MeshObject):
+            NewMesh = self.Mesh + Object.Mesh
+            return MeshObject(NewMesh)
+        elif isinstance(Object, vec3d):
+            NewMesh = self.Mesh.copy()
+            ShiftedMesh = []
+            for tri in NewMesh:
+                ShiftedMesh.append(tri + Object)
+            return MeshObject(ShiftedMesh)
+        else:
+            pass
+
+    def __len__(self):
+        return len(self.Mesh)
+
+    def __mul__(self, Object):
+        if isinstance(Object, (int, float)):
+            NewMesh = []
+            for tri in self.Mesh: # Each atribute "tri" is a triangle object.
+                NewMesh.append(tri * Object)
+            return MeshObject(NewMesh)
+
+    def __getitem__(self, index):
+        if isinstance(index, int):
+            return self.Mesh[index]
+
+    def __dir__(self):
+        return ['__mul__', '__add__', 'CenterOfMass', 'Collission', '__len__', '__getitem__',
+        '__str__', '__repr__', 'getter', 'setter', 'SaveToJson']
+
+    def CenterOfMass(self):
+        """Returns a vuege center of mass, with some deviation; The deviation is the longest distance from center of mass.
+        This method is not optimized for non-symetrical Meshes."""
+        center = vec3d(0,0,0)
+        for i in range(len(self.Mesh)):
+            sum = (self.Mesh[i][0] + self.Mesh[i][1] + self.Mesh[i][2]) * (1/3)
+            center += sum
+        center = center / len(self.Mesh)
+        deviation = []
+        for i in range(len(self.Mesh)):
+            for j in range(3):
+                delta = center - self.Mesh[i][j]
+                if delta != None:
+                    deviation.append(abs(delta.norm()))
+        epsilon = max(deviation)
+        self.CenterOfGravity = (center, epsilon)
+        self.MeshCenterOfMass.append(self.CenterOfGravity)
+
+
+    def Collission(self):
+        """Returns a boolean expression for each object colliding; Works upon the center of mass definition."""
+        MeshesToSearchThrough = self.AllActiveMeshes.copy()
+        MeshesCenterOfMass = self.MeshCenterOfMass.copy()
+        try:
+            IndexForMesh = MeshesToSearchThrough.index(self.Mesh)
+            IndexForMass = MeshesCenterOfMass.index(self.CenterOfGravity)
+            del MeshesToSearchThrough[IndexForMesh: IndexForMass +1]
+            del MeshesCenterOfMass[IndexForMass: IndexForMass + 1]
+        except:
+            raise MeshObjectError("Cannot performe Collission calculations, indicies do not exist \n Needs atleast two meshes to search through.")
+        for mesh in MeshesCenterOfMass: #This is not yet completed!
+            if self.CenterOfGravity[1] == 0:
+                print('Something happens')
+
+
+    def SaveToJson(self, Name):
+        """Save the current MeshObject as a .json file."""
+        # Add so that we cannot override!
+        try:
+            path = os.path.join('/Users/andreasevensen/Documents/GitHub/Sacra/saves', Name + '.json')
+        except:
+            raise PathError("Cannot load the path")
+        MeshSave = []
+        for i in range(len(self.Mesh)):
+            for j in range(len(self.Mesh[i])):
+                MeshSave.append([self.Mesh[i][j][0], self.Mesh[i][j][1], self.Mesh[i][j][2]])
+        data = {Name : MeshSave}
+        try:
+            with open(path, 'w') as file:
+                json.dump(data, file, indent = 4)
+        except:
+            raise PathError("Cannot save file.")
+
+
+
+Cube = """{"Cube" : [[0,0,0], [0, 1, 0], [1, 1, 0],
+    [0, 0, 0], [1, 1, 0], [1, 0, 0],
+    [1, 0, 0], [1, 1, 0], [1, 1, 1],
+    [1, 0, 0], [1, 1, 1], [1, 0, 1],
+    [1, 0, 1], [1, 1, 1], [0, 1, 1],
+    [1, 0, 1], [0, 1, 1], [0, 0, 1],
+    [0, 0, 1], [0, 1, 1], [0, 1, 0],
+    [0, 0, 1], [0, 1, 0], [0, 0, 0],
+    [0, 1, 0], [0, 1, 1], [1, 1, 1],
+    [0, 1, 0], [1, 1, 1], [1, 1, 0],
+    [1, 0, 1], [0, 0, 1], [0, 0, 0],
+    [1, 0, 1], [0, 0, 0], [1, 0, 0]]}"""
+
+#data = json.loads(Cube)
+#rawdata = json.dumps(data, indent = 4)
+#print(json.dumps(data, indent = 4))
+
+
+"""
+Cube = {
+    "Cube" : [[[vec3d(0, 0, 0), vec3d(0, 1, 0), vec3d(1, 1, 0)],
+    [vec3d(0, 0, 0), vec3d(1, 1, 0), vec3d(1, 0, 0)]],
+    [[vec3d(1, 0, 0), vec3d(1, 1, 0), vec3d(1, 1, 1)],
+    [vec3d(1, 0, 0), vec3d(1, 1, 1), vec3d(1, 0, 1)]],
+    [[vec3d(1, 0, 1), vec3d(1, 1, 1), vec3d(0, 1, 1)],
+    [vec3d(1, 0, 1), vec3d(0, 1, 1), vec3d(0, 0, 1)]],
+    [[vec3d(0, 0, 1), vec3d(0, 1, 1), vec3d(0, 1, 0)],
+    [vec3d(0, 0, 1), vec3d(0, 1, 0), vec3d(0, 0, 0)]],
+    [[vec3d(0, 1, 0), vec3d(0, 1, 1), vec3d(1, 1, 1)],
+    [vec3d(0, 1, 0), vec3d(1, 1, 1), vec3d(1, 1, 0)]],
+    [[vec3d(1, 0, 1), vec3d(0, 0, 1), vec3d(0, 0, 0)],
+    [vec3d(1, 0, 1), vec3d(0, 0, 0), vec3d(1, 0, 0)]]]
+}
+"""
+"""
+Cube = [[[vec3d(0, 0, 0), vec3d(0, 1, 0), vec3d(1, 1, 0)],
+    [vec3d(0, 0, 0), vec3d(1, 1, 0), vec3d(1, 0, 0)]],
+    [[vec3d(1, 0, 0), vec3d(1, 1, 0), vec3d(1, 1, 1)],
+    [vec3d(1, 0, 0), vec3d(1, 1, 1), vec3d(1, 0, 1)]],
+    [[vec3d(1, 0, 1), vec3d(1, 1, 1), vec3d(0, 1, 1)],
+    [vec3d(1, 0, 1), vec3d(0, 1, 1), vec3d(0, 0, 1)]],
+    [[vec3d(0, 0, 1), vec3d(0, 1, 1), vec3d(0, 1, 0)],
+    [vec3d(0, 0, 1), vec3d(0, 1, 0), vec3d(0, 0, 0)]],
+    [[vec3d(0, 1, 0), vec3d(0, 1, 1), vec3d(1, 1, 1)],
+    [vec3d(0, 1, 0), vec3d(1, 1, 1), vec3d(1, 1, 0)]],
+    [[vec3d(1, 0, 1), vec3d(0, 0, 1), vec3d(0, 0, 0)],
+    [vec3d(1, 0, 1), vec3d(0, 0, 0), vec3d(1, 0, 0)]]]
+"""
+
+"""
+Cube = {
+    "Cube" : [[Triangle(vec3d(0, 0, 0), vec3d(0, 1, 0), vec3d(1, 1, 0))],
+    [Triangle(vec3d(0, 0, 0), vec3d(1, 1, 0), vec3d(1, 0, 0))],
+    [Triangle(vec3d(1, 0, 0), vec3d(1, 1, 0), vec3d(1, 1, 1))],
+    [Triangle(vec3d(1, 0, 0), vec3d(1, 1, 1), vec3d(1, 0, 1))],
+    [Triangle(vec3d(1, 0, 1), vec3d(1, 1, 1), vec3d(0, 1, 1))],
+    [Triangle(vec3d(1, 0, 1), vec3d(0, 1, 1), vec3d(0, 0, 1))],
+    [Triangle(vec3d(0, 0, 1), vec3d(0, 1, 1), vec3d(0, 1, 0))],
+    [Triangle(vec3d(0, 0, 1), vec3d(0, 1, 0), vec3d(0, 0, 0))],
+    [Triangle(vec3d(0, 1, 0), vec3d(0, 1, 1), vec3d(1, 1, 1))],
+    [Triangle(vec3d(0, 1, 0), vec3d(1, 1, 1), vec3d(1, 1, 0))],
+    [Triangle(vec3d(1, 0, 1), vec3d(0, 0, 1), vec3d(0, 0, 0))],
+    [Triangle(vec3d(1, 0, 1), vec3d(0, 0, 0), vec3d(1, 0, 0))]]
+}
+
+Cube2 = MeshObject3d(Cube, 'Cube')
+print(Cube2)
+"""
+
+
+"""
+MeshCube = {
+    "South" : [(vec3d(0, 0, 0), vec3d(0, 1, 0), vec3d(1, 1, 0)),
+    (vec3d(0, 0, 0), vec3d(1, 1, 0), vec3d(1, 0, 0))],
+    "East" : [(vec3d(1, 0, 0), vec3d(1, 1, 0), vec3d(1, 1, 1)),
+    (vec3d(1, 0, 0), vec3d(1, 1, 1), vec3d(1, 0, 1))],
+    "North": [(vec3d(1, 0, 1), vec3d(1, 1, 1), vec3d(0, 1, 1)),
+    (vec3d(1, 0, 1), vec3d(0, 1, 1), vec3d(0, 0, 1))],
+    "West" : [(vec3d(0, 0, 1), vec3d(0, 1, 1), vec3d(0, 1, 0)),
+    (vec3d(0, 0, 1), vec3d(0, 1, 0), vec3d(0, 0, 0))],
+    "Top" : [(vec3d(0, 1, 0), vec3d(0, 1, 1), vec3d(1, 1, 1)),
+    (vec3d(0, 1, 0), vec3d(1, 1, 1), vec3d(1, 1, 0))],
+    "Bottom" : [(vec3d(1, 0, 1), vec3d(0, 0, 1), vec3d(0, 0, 0)),
+    (vec3d(1, 0, 1), vec3d(0, 0, 0), vec3d(1, 0, 0))]
+}
+"""
---
+++ packages/SacraMathEngine/SacraMathEngine-0.0.9-py3-none-any.whl/SacraMathEngine/Vector.py
@@ -0,0 +1,221 @@
+
+class vec3d:
+    def __init__(self, x, y, z):
+        if x == None or y == None or z == None:
+            if x == None:
+                self.x = 0
+            elif y == None:
+                self.y = 0
+            elif z == None:
+                self.z = 0
+        else:
+            self.x = x
+            self.y = y
+            self.z = z
+        self.set = self.x, self.y, self.z
+
+    def __repr__(self):
+        """Default print statement"""
+        return f'<({self.x}, {self.y}, {self.z})>'
+
+    def __str__(self):
+        """Default print statement"""
+        return f'<({self.x}, {self.y}, {self.z})>'
+
+    def __add__(self, other):
+        """Defintion of addition of two vectors, returns a vec3d-object."""
+        if isinstance(other, vec3d):
+            return vec3d(self.x + other.x, self.y + other.y, self.z + other.z)
+        else:
+            pass
+
+    def __radd__(self, other):
+        """Definition of reverse addition of two vectors, returns a vec3d-object."""
+        if isinstance(other, vec3d):
+            return vec3d(self.x + other.x, self.y + other.y, self.z + other.z)
+        else:
+            pass
+
+    def __sub__(self, other):
+        """Definition of subtraction of two vectors, returns a vec3d-object.s"""
+        if isinstance(other, vec3d):
+            return vec3d(self.x - other.x, self.y - other.y, self.z - other.z)
+        else:
+            pass
+
+    def __rsub__(self, other):
+        """Definition of reverse-subtraction of two vectors, returns a vec3d-object.s"""
+        if isinstance(other, vec3d):
+            return vec3d(self.x - other.x, self.y - other.y, self.z - other.z)
+        else:
+            pass
+
+    def __mul__(self, value):
+        """Definition of scalar multiplication, only works with float or integers; Returns a vec3d-object.s"""
+        if isinstance(value, vec3d):
+            pass
+        elif isinstance(value, (float, int)):
+            return vec3d(self.x * value, self.y * value, self.z * value)
+        else:
+            pass
+
+    def __rmul__(self, value):
+        """Definition of reverse-scalar multiplication, only works with float or integers; Returns a vec3d-object.s"""
+        if isinstance(value, vec3d):
+            pass
+        else:
+            return vec3d(self.x * value, self.y * value, self.z * value)
+
+    def __getitem__(self, Index):
+        """Get the value of the vec3d-object at index 'Index'."""
+        return self.set[Index]
+
+    def __abs__(self):
+        return vec3d(abs(self.x), abs(self.y), abs(self.y))
+
+    def __len__(self):
+        return len(self.set)
+
+    def __truediv__(self, Scalar):
+        if isinstance(Scalar, (float, int)):
+            if Scalar != 0:
+                return vec3d(self.x / Scalar, self.y / Scalar, self.z / Scalar)
+            else:
+                raise ValueError("Cannot performe division with zero.")
+        elif isinstance(Scalar, vec3d):
+            raise TypeError("Cannot performe division with a vector object")
+        else:
+            pass
+
+    def dot(self, other):
+        """Compute the dot-product of two vectors, returns a float or integer."""
+        if isinstance(other, vec3d):
+            value = self.x * other.x + self.y * other.y + self.z * other.z
+            if value != 0:
+                return value
+            else:
+                return None
+        else:
+            pass
+
+    def cross(self, other):
+        """Compute the cross-product of two vec3d-objects; Returns a vec3d-object."""
+        if isinstance(other, vec3d):
+            col1 = self.y * other.z - self.z * other.y
+            col2 = self.z * other.x - self.x * other.z
+            col3 = self.x * other.y - self.y * other.z
+            return vec3d(col1, col2, col3)
+        else:
+            pass
+
+    def norm(self):
+        """Calculates the norm of a given vector, returns a scalar of either type integer or float."""
+        value = (self.x ** 2 + self.y ** 2 + self.z ** 2) ** (1/2)
+        return value
+
+    def normalize(self):
+        """Normalizes a vector with self.norm() function and returns a vec3d-object"""
+        return 1 / (self.norm()) * vec3d(self.vec1, self.vec2, self.vec3) # a valid solution is * self
+
+    def line(self, Other, Number):
+        """Returns multiple vectors that increase equvilantly"""
+        #Not done yet
+        if isinstance(Number, int) and isinstance(Other, vec3d):
+
+            Number = Number + 1
+            epsilonx = abs(self.x - Other.x) / Number
+            epsilony = abs(self.y - Other.y) / Number
+            epsilonz = abs(self.z - Other.z) / Number
+            Line = []
+            for i in range(1, Number):
+                 Line.append(vec3d(epsilonx * i, epsilony * i, epsilonz * i))
+            return Line
+
+            #while self < = Other
+
+        else:
+            raise TypeError("Line method can only take integers as inputs")
+
+
+class vec4d:
+
+    def __init__(self, x, y, z, w = 1):
+        self.x = x
+        self.y = y
+        self.z = z
+        self.w = w
+        self.vector = (self.x, self.y, self.z, self.w)
+
+    def __truediv__(self, value):
+        if value != 0:
+            x, y, z, w = self.x / value, self.y / value, self.z / value, self.w / value
+            return vec4d(x, y, z, w)
+        else:
+            pass
+    def __add__(self, other):
+        if isinstance(other, vec4d):
+            return vec4d(self.x + other.x, self.y + other.y, self.z + other.z, self.w + other.w)
+
+    def __radd__(self, other):
+        if isinstance(other, vec4d):
+            return vec4d(self.x + other.x, self.y + other.y, self.z + other.z, self.w + other.w)
+
+    def __sub__(self, other):
+        if isinstance(other, vec4d):
+            return vec4d(self.x - other.x, self.y - other.y, self.z - other.z, self.w - other.w)
+
+    def __rsub__(self, other):
+        if isinstance(other, vec4d):
+            return vec4d(self.x - other.x, self.y - other.y, self.z - other.z, self.w - other.w)
+
+    def __str__(self):
+        return f'<({self.x}, {self.y}, {self.z}, {self.w})>'
+
+    def __repr__(self):
+        return f'<({self.x}, {self.y}, {self.z}, {self.w})>'
+
+    def __mul__(self, value):
+        """Definition of scalar multiplication, only works with float or integers; Returns a vec3d-object.s"""
+        if isinstance(value, vec4d):
+            pass
+        elif isinstance(value, (float, int)):
+            return vec4d(self.x * value, self.y * value, self.z * value, self.w * value)
+        else:
+            pass
+
+    def __rmul__(self, value):
+        """Definition of scalar multiplication, only works with float or integers; Returns a vec3d-object.s"""
+        if isinstance(value, vec4d):
+            pass
+        elif isinstance(value, (float, int)):
+            return vec4d(self.x * value, self.y * value, self.z * value, self.w * value)
+        else:
+            pass
+
+    def __getitem__(self, index):
+        return self.vector[index]
+
+    def cross(self, other):
+        """Compute the cross-product of two vec3d-objects; Returns a vec3d-object."""
+        if isinstance(other, vec3d):
+            col1 = self.y * other.z - self.z * other.y
+            col2 = self.z * other.x - self.x * other.z
+            col3 = self.x * other.y - self.y * other.z
+            return vec4d(col1, col2, col3, 1)
+        else:
+            pass
+
+    def dot(self, other):
+        if isinstance(other, vec4d):
+            value = self.x * other.x + self.y * other.y + self.z * other.z + self.w * other.w
+            return value
+        else:
+            pass
+
+    def norm(self):
+        """Calculates the norm of a given vector, returns a scalar of either type integer or float."""
+        value = (self.x ** 2 + self.y ** 2 + self.z ** 2 + self.w ** 2) ** (1/2)
+        return value
+
+    def normalize(self):
+        return 1 / self.norm() * vec4d(self.x, self.y, self.z, self.w)
