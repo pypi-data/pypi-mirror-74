3ca39c0c5e7bbc14c8d38497bedfff5785566370
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:54 +0000

    Add package GnBProbabilities/1.0/GnBProbabilities-1.0.tar.gz

---
+++ packages/GnBProbabilities/GnBProbabilities-1.0.tar.gz/GnBProbabilities-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='GnBProbabilities',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['GnBProbabilities'],
+      author='Seema Savadi',
+      author_email='seemasavadi@gmail.com',
+      zip_safe=False)
