4388e5e58446deebfd3685089624d7318294fd7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:50 +0000

    Add package dutch-pluralizer/0.0.35/dutch-pluralizer-0.0.35.tar.gz

---
+++ packages/dutch-pluralizer/dutch-pluralizer-0.0.35.tar.gz/dutch-pluralizer-0.0.35/dutch_pluralizer/__init__.py
@@ -0,0 +1,16 @@
+__version__ = "0.0.35"
+
+__all__ = ['pluralize', 'pluralize_advanced', 'singularize',
+           'could_be_plural', 'singularize_advanced', 'NounEndingMap', 'Pluralizer']
+
+try:
+    from hunspell import Hunspell
+except:
+    #do nothing
+    a = 1
+
+
+from .mapping import NounEndingMap
+from .pluralizer import pluralize, pluralize_advanced
+from .pluralizer_cls import Pluralizer
+from .singularizer import could_be_plural, singularize, singularize_advanced
