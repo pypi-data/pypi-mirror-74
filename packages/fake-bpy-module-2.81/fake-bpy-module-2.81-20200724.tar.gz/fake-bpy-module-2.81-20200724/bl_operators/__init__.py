4abfed97d729275e397ab7944c584df618ce2587
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:15 +0000

    Add package fake-bpy-module-2.81/20200724/fake-bpy-module-2.81-20200724.tar.gz

---
+++ packages/fake-bpy-module-2.81/fake-bpy-module-2.81-20200724.tar.gz/fake-bpy-module-2.81-20200724/bl_operators/__init__.py
@@ -0,0 +1,45 @@
+import sys
+import typing
+from . import wm
+from . import vertexpaint_dirt
+from . import mesh
+from . import console
+from . import uvcalc_smart_project
+from . import uvcalc_lightmap
+from . import object
+from . import constraint
+from . import bmesh
+from . import presets
+from . import add_mesh_torus
+from . import mask
+from . import clip
+from . import object_randomize_transform
+from . import view3d
+from . import node
+from . import screen_play_rendered_anim
+from . import rigidbody
+from . import sequencer
+from . import freestyle
+from . import file
+from . import object_quick_effects
+from . import image
+from . import userpref
+from . import anim
+from . import object_align
+from . import uvcalc_follow_active
+
+
+def register():
+    ''' 
+
+    '''
+
+    pass
+
+
+def unregister():
+    ''' 
+
+    '''
+
+    pass
