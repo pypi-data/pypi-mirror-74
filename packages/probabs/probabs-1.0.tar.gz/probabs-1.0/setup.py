db0187264237450d91346c64e7394469b3f4e71e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:05 +0000

    Add package probabs/1.0/probabs-1.0.tar.gz

---
+++ packages/probabs/probabs-1.0.tar.gz/probabs-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name = 'probabs',
+      version = '1.0',
+      description = 'Gaussian and Binomial distributions',
+      packages = ['probabs'],
+      author = 'Ankur Chourasia',
+      author_email = 'chourasia.ankur1@gmail.com',
+      zip_safe = False)
