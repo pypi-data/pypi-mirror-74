47237df153cc35ecfbbff71040050a3d143afb00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:13:53 +0000

    Add package mlintro-distrib/0.1/mlintro_distrib-0.1.tar.gz

---
+++ packages/mlintro-distrib/mlintro_distrib-0.1.tar.gz/mlintro_distrib-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='mlintro_distrib',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['mlintro_distrib'],
+      author_email='isaac.gutierrez@msn.com',
+      zip_safe=False)
