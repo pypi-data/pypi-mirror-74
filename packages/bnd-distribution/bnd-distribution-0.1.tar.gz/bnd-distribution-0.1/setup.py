cca05ea3a021e54600bf30bef8555fc4207350cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:21 +0000

    Add package bnd-distribution/0.1/bnd-distribution-0.1.tar.gz

---
+++ packages/bnd-distribution/bnd-distribution-0.1.tar.gz/bnd-distribution-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='bnd-distribution',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['bnd-distribution'],
+      author = 'Ehsan Alahi',
+      author_email = 'ehsanalahi617@gmail.com',
+      zip_safe=False)
