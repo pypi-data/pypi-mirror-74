f3dcc84d829abca11ad248420d317d75fb12e8e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:11 +0000

    Add package cerberus-api-client/1.0.0/cerberus-api-client-1.0.0.tar.gz

---
+++ packages/cerberus-api-client/cerberus-api-client-1.0.0.tar.gz/cerberus-api-client-1.0.0/setup.py
@@ -0,0 +1,14 @@
+import sys
+from setuptools import setup
+from pkg_resources import VersionConflict, require
+
+
+try:
+    require('setuptools>=38.3')
+except VersionConflict:
+    print("Error: version of setuptools is too old (<38.3)!")
+    sys.exit(1)
+
+
+if __name__ == "__main__":
+    setup()
