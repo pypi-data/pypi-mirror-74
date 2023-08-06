bf2f87993c53e7e34bf81c604224b775e26b180a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:53 +0000

    Add package oneroster/0.0.17/oneroster-0.0.17.tar.gz

---
+++ packages/oneroster/oneroster-0.0.17.tar.gz/oneroster-0.0.17/setup.py
@@ -0,0 +1,44 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+test_deps = ['mock', 'pytest', 'pytest-cov']
+
+setup(name='oneroster',
+      version='0.0.17',
+      description='Simple Oneroster client for user management',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      classifiers=[
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+          'License :: OSI Approved :: MIT License',
+          'Intended Audience :: Developers',
+          'Intended Audience :: System Administrators',
+      ],
+      url='https://github.com/vossenv/oneroster-python',
+      maintainer='Danimae Vossen',
+      maintainer_email='vossen.dm@gmail.com',
+      license='MIT',
+      packages=find_packages(),
+      install_requires=[
+          'six',
+          'requests'
+      ],
+      tests_require=[
+          'mock',
+          'pytest',
+          'pyyaml',
+          'vcrpy',
+          'pytest-vcr'
+      ],
+)
+
+# twine upload --repository testpypi dist/*.tar.gz dist/*.whl
+# twine upload dist/*.tar.gz dist/*.whl
+
+# Github: https://github.com/vossenv/oneroster-python
+# PyPI: https://pypi.org/project/oneroster
---
+++ packages/oneroster/oneroster-0.0.17.tar.gz/oneroster-0.0.17/tests/test_integration.py
@@ -0,0 +1,42 @@
+import pytest
+import yaml
+from oneroster.clever import *
+
+
+@pytest.fixture()
+def clever_api():
+    options = {
+        'host': 'https://api.clever.com/v2.1/',
+        'key_identifier': 'id',
+        'page_size': 1000,
+        'max_users': 0,
+        'match_on': 'name',
+        'access_token': 'TEST_TOKEN'
+    }
+
+    return CleverConnector(**options)
+
+
+@pytest.fixture()
+def source_data():
+    with open('fixtures/comparison_data.yaml', 'r') as stream:
+        yield yaml.safe_load(stream)
+
+
+@pytest.fixture(scope='module')
+def vcr(vcr):
+    vcr.serializer = 'yaml'
+    vcr.record_mode = 'new_episodes'
+    vcr.cassette_library_dir = 'fixtures/cassettes'
+    vcr.match_on = ['method', 'host', 'path', 'query']
+    return vcr
+
+
+@pytest.mark.vcr()
+def test_simple(clever_api, source_data):
+    clever_api.max_users = 100
+    response = clever_api.get_users(user_filter='students')
+    t = [r['id'] for r in response]
+    x = collections.Counter(t) == collections.Counter(source_data['test_simple'])
+
+    print()
