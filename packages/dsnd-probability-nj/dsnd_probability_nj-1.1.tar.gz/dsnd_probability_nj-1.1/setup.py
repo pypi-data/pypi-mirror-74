6098ceeb09689b1b79f1c02762a700217144baf5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:11 +0000

    Add package dsnd-probability-nj/1.1/dsnd_probability_nj-1.1.tar.gz

---
+++ packages/dsnd-probability-nj/dsnd_probability_nj-1.1.tar.gz/dsnd_probability_nj-1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dsnd_probability_nj',
+      version='1.1',
+      description='Gaussian and Binomial distributions',
+      packages=['dsnd_probability_nj'],
+      author = 'Nirav Jain',
+      author_email = 'jainnirav125@gmail.com',
+      zip_safe=False)
