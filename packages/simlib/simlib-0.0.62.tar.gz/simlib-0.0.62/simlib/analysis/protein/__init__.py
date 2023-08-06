59eaf40ff1d605ecdbb9ba64da570d59f5acc684
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:28 +0000

    Add package simlib/0.0.62/simlib-0.0.62.tar.gz

---
+++ packages/simlib/simlib-0.0.62.tar.gz/simlib-0.0.62/simlib/analysis/protein/__init__.py
@@ -0,0 +1,11 @@
+
+from .secondary_structure import *
+from .hydrophobicity import *
+
+__all__ = [
+    'compute_hydrophobic_moment',
+    'HydrophobicityScale',
+    'r1N',
+    'SecondaryStructure',
+    'secondary_structure',
+]
