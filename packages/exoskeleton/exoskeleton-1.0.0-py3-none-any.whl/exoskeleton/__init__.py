d901edd8921beced9162ef62935a9fad7b51ccd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:04 +0000

    Add package exoskeleton/1.0.0/exoskeleton-1.0.0-py3-none-any.whl

---
+++ packages/exoskeleton/exoskeleton-1.0.0-py3-none-any.whl/exoskeleton/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from exoskeleton.__main__ import Exoskeleton
+from exoskeleton.DatabaseConnection import DatabaseConnection
+from exoskeleton.TimeManager import TimeManager
+from exoskeleton.QueueManager import QueueManager
+
+name = "exoskeleton"
+__version__ = "1.0.0"
+__author__ = "Rüdiger Voigt"
