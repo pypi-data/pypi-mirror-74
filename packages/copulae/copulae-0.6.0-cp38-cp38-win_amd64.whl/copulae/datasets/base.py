22b7cc7234342b848d607fe294d611d9fb10778d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:12 +0000

    Add package copulae/0.6.0/copulae-0.6.0-cp38-cp38-win_amd64.whl

---
+++ packages/copulae/copulae-0.6.0-cp38-cp38-win_amd64.whl/copulae/datasets/base.py
@@ -0,0 +1,81 @@
+import os
+
+import pandas as pd
+
+__all__ = ["load_danube", "load_residuals", "load_smi"]
+
+
+def _load_file(fn: str):
+    return os.path.join(os.path.dirname(__file__), 'data', fn)
+
+
+def load_danube() -> pd.DataFrame:
+    """
+    The danube dataset contains ranks of base flow observations from the Global River Discharge
+    project of the Oak Ridge National Laboratory Distributed Active Archive Center (ORNL DAAC),
+    a NASA data center. The measurements are monthly average flow rate for two stations situated
+    at Scharding (Austria) on the Inn river and at Nagymaros (Hungary) on the Danube.
+
+    The data have been pre-processed to remove any time trend. Specifically, Bacigal et al. (2011)
+    extracted the raw data, and obtain the fast Fourier transformed centered observations. The
+    negative spectrum is retained and a linear time series model with 12 seasonal components is
+    fitted. Residuals are then extracted and AR model fitted to the series, the selection being
+    done based on the AIC criterion with imposed maximum order of 3 and the number of autoregressive
+    components may differ for each series.
+
+    This data frame contains the following columns:
+
+    inn:
+        A numeric vector containing the rank of pre-whitened level observations of the Inn river
+        at Nagyramos.
+
+    donau:
+        A numeric vector containing the rank of prewhitened level observations of the Donau river
+        at Scharding.
+
+    Returns
+    -------
+    DataFrame
+        A dataframe containing the Danube data
+    """
+    df = pd.read_csv(_load_file('danube.csv'))
+    df.columns = df.columns.str.strip()
+    return df
+
+
+def load_residuals() -> pd.DataFrame:
+    """
+    Loads a 394 x 7 array of regression residuals from unknown processes
+
+    Returns
+    -------
+    DataFrame
+        A data frame of simulated regression residuals
+    """
+    df = pd.read_csv(_load_file('residuals.csv'))
+    df.columns = df.columns.str.strip()
+    return df
+
+
+def load_smi(as_returns=False) -> pd.DataFrame:
+    """
+    Dataset contains the close prices of all 20 constituents of the Swiss Market Index (SMI) from
+    2011-09-09 to 2012-03-28.
+
+    Parameters
+    ----------
+    as_returns: bool
+        If true, transforms the price data to returns data
+
+    Returns
+    -------
+    DataFrame
+        A data frame of the closing prices of all 20 constituents of the Swiss Market Index
+    """
+    df = pd.read_csv(_load_file('smi.csv'), index_col=0, parse_dates=[0])
+    df.index.name = df.index.name.strip()
+
+    if as_returns:
+        df = df.pct_change().dropna()
+
+    return df
---
+++ packages/copulae/copulae-0.6.0-cp38-cp38-win_amd64.whl/copulae/elliptical/abstract.py
@@ -0,0 +1,139 @@
+from abc import ABC
+from typing import Tuple
+
+import numpy as np
+
+from copulae.copula.base import BaseCopula
+from copulae.core import create_cov_matrix, is_psd, near_psd, tri_indices
+from copulae.utility import array_io
+
+
+class AbstractEllipticalCopula(BaseCopula, ABC):
+    """
+    The abstract base class for Elliptical Copulas
+    """
+
+    def __init__(self, dim: int, name: str):
+        super().__init__(dim, name)
+        self._rhos = np.zeros(sum(range(dim)))
+
+    @array_io(optional=True)
+    def drho(self, x=None):
+        if x is None:
+            x = self._rhos
+        return 6 / (np.pi * np.sqrt(4 - x ** 2))
+
+    @array_io(optional=True)
+    def dtau(self, x=None):
+        if x is None:
+            x = self._rhos
+        return 2 / (np.pi * np.sqrt(1 - x ** 2))
+
+    @array_io
+    def itau(self, tau):
+        return np.sin(np.asarray(tau) * np.pi / 2)
+
+    def log_lik(self, data: np.ndarray, *, to_pobs=True):
+        if not is_psd(self.sigma):
+            return -np.inf
+
+        if hasattr(self, '_df') and self._df <= 0:  # t copula
+            return -np.inf
+
+        return super().log_lik(data, to_pobs=to_pobs)
+
+    @property
+    def rho(self):
+        return np.arcsin(self._rhos / 2) * 6 / np.pi
+
+    @property
+    def sigma(self):
+        """
+        The covariance matrix for the elliptical copula
+
+        :return: numpy array
+            Covariance matrix for elliptical copula
+        """
+        return create_cov_matrix(self._rhos)
+
+    @property
+    def tau(self):
+        return 2 * np.arcsin(self._rhos) / np.pi
+
+    def __getitem__(self, i):
+        if isinstance(i, int):
+            return self._rhos[i]
+        elif isinstance(i, (slice, tuple, list, np.ndarray)):
+            if len(i) == 2:
+                return self.sigma[i]
+            else:
+                raise IndexError('only 2-dimensional indices supported')
+        raise IndexError("only integers, slices (`:`), ellipsis (`...`), numpy.newaxis (`None`) and integer or boolean "
+                         "arrays are valid indices")
+
+    def __setitem__(self, i, value):
+        d = self.dim
+
+        if isinstance(i, slice):
+            value = near_psd(value)
+            if value.shape != (d, d):
+                return IndexError(f"The value being set should be a matrix of dimension ({d}, {d})")
+            self._rhos = value[tri_indices(d, 1, 'lower')]
+            return
+
+        assert -1.0 <= value <= 1.0, "correlation value must be between -1 and 1"
+        if isinstance(i, int):
+            self._rhos[i] = value
+
+        else:
+            i = _get_rho_index(d, i)
+            self._rhos[i] = value
+
+        self._force_psd()
+
+    def __delitem__(self, i):
+        d = self.dim
+
+        if isinstance(i, slice):
+            self._rhos = np.zeros(len(self._rhos))
+
+        elif isinstance(i, int):
+            self._rhos[i] = 0
+
+        else:
+            i = _get_rho_index(d, i)
+            self._rhos[i] = 0
+
+        self._force_psd()
+
+    def _force_psd(self):
+        """
+        Forces covariance matrix to be positive semi-definite. This is useful when user is overwriting covariance
+        parameters
+        """
+        cov = near_psd(self.sigma)
+        self._rhos = cov[tri_indices(self.dim, 1, 'lower')]
+
+
+def _get_rho_index(d: int, i: Tuple[int, int]) -> int:
+    i = tuple(i)
+    if not isinstance(i, tuple):
+        raise IndexError("only integers, slices (`:`), ellipsis (`...`), numpy.newaxis (`None`) and integer or boolean "
+                         "arrays are valid indices")
+
+    if len(i) != 2:
+        raise IndexError('only 2-dimensional indices supported')
+
+    x, y = i
+    if x < 0 or y < 0:
+        raise IndexError('Only positive indices are supported')
+    elif x >= d or y >= d:
+        raise IndexError('Index cannot be greater than dimension of copula')
+    elif x == y:
+        raise IndexError('Cannot set values along the diagonal')
+
+    for j, v in enumerate(zip(*tri_indices(d, 1, 'upper' if x < y else 'lower'))):
+        if i == v:
+            return j
+
+    raise IndexError(f"Unable to find index {i}")
