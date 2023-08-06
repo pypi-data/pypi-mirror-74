35c9ffe87aaaf7c1f7ed66c3148e8f5ed5df292c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:35 +0000

    Add package jira-select/1.1.0.dev26/jira-select-1.1.0.dev26.tar.gz

---
+++ packages/jira-select/jira-select-1.1.0.dev26.tar.gz/jira-select-1.1.0.dev26/jira_select/query.py
@@ -0,0 +1,305 @@
+from __future__ import annotations
+
+from typing import Any, Callable, Dict, Generator, List, Optional
+
+from jira import JIRA, Issue
+from rich.progress import Progress, TaskID
+
+from .exceptions import UserError
+from .plugin import get_installed_functions
+from .types import ExpressionList, QueryDefinition, SelectFieldDefinition
+from .utils import (
+    calculate_result_hash,
+    clean_query_definition,
+    evaluate_expression,
+    get_field_data,
+    get_row_dict,
+    parse_order_by_definition,
+    parse_select_definition,
+)
+
+
+class Result:
+    def __init__(
+        self, query: Query, group_fields: ExpressionList = None, rows: List[Any] = None,
+    ):
+        super().__init__()
+        self._rows: List[Any] = rows if rows is not None else []
+        self._group_fields: ExpressionList = group_fields if group_fields else []
+        self._query: Query = query
+
+    @classmethod
+    def for_row(
+        cls, query: Query, group_fields: List[str] = None, row: Any = None
+    ) -> Result:
+        return cls(query, group_fields, [row])
+
+    @property
+    def query(self):
+        return self._query
+
+    @property
+    def rows(self):
+        return self._rows
+
+    @property
+    def value_fields(self):
+        return self._group_fields
+
+    @property
+    def scalar_fields(self):
+        fields = set()
+
+        for row in self._rows:
+            fields |= set(get_row_dict(row))
+
+        for row in self.value_fields:
+            if row in fields:
+                fields.remove(row)
+
+        return fields
+
+    def __hash__(self):
+        return calculate_result_hash(self._rows[0], self.value_fields)
+
+    def __eq__(self, other):
+        return hash(self) == hash(other)
+
+    def add(self, record: Any):
+        self._rows.append(record)
+
+    def _grouped_as_dict(self) -> Dict[str, Any]:
+        result: Dict[str, Any] = {}
+
+        first = get_row_dict(self._rows[0])
+
+        for field in self.value_fields:
+            result[field] = evaluate_expression(
+                field, first, self.query.get_functions(),
+            )
+
+        scalar_fields = self.scalar_fields
+
+        for field in scalar_fields:
+            # Exclude empty rows -- in SQL when you run an aggregation
+            # function on a set of rows, NULL rows are skipped, so we
+            # should probably do the same?
+            result[field] = [
+                get_row_dict(row)[field]
+                for row in self._rows
+                if get_row_dict(row).get(field) is not None
+            ]
+
+        return result
+
+    def _single_as_dict(self) -> Dict[str, Any]:
+        return get_row_dict(self._rows[0])
+
+    def as_dict(self) -> Dict[str, Any]:
+        if self._group_fields:
+            return self._grouped_as_dict()
+        return self._single_as_dict()
+
+    def __getattr__(self, name):
+        result = self.as_dict()
+
+        if name not in result:
+            raise AttributeError(name)
+
+        return result[name]
+
+
+class Query:
+    def __init__(
+        self, jira: JIRA, definition: QueryDefinition, progress_bar: bool = False
+    ):
+        self._definition: QueryDefinition = clean_query_definition(definition)
+        self._jira: JIRA = jira
+        self._functions: Dict[str, Callable] = get_installed_functions(jira)
+        self._progress_bar = progress_bar
+
+    @property
+    def progress_bar_enabled(self):
+        return self._progress_bar
+
+    @property
+    def jira(self) -> JIRA:
+        return self._jira
+
+    @property
+    def definition(self) -> QueryDefinition:
+        return self._definition
+
+    def get_functions(self) -> Dict[str, Callable]:
+        return self._functions
+
+    def get_fields(self) -> List[SelectFieldDefinition]:
+        fields: List[SelectFieldDefinition] = []
+
+        for field in self.definition["select"]:
+            fields.append(parse_select_definition(field))
+
+        return fields
+
+    def _generate_row_dict(self, row: Any) -> Dict[str, Any]:
+        result: Dict[str, Any] = {}
+
+        for field_defn in self.get_fields():
+            result[field_defn["column"]] = get_field_data(
+                row, field_defn["expression"], functions=self._functions
+            )
+
+        return result
+
+    def _get_jql(self) -> str:
+        where = self.definition.get("where")
+
+        if where is None:
+            return ""
+        elif isinstance(where, List):
+            return " AND ".join(self.definition["where"])
+
+        raise UserError(f"Could not generate JQL from {where}.")
+
+    def _get_issues(self, progress: Progress) -> Generator[Issue, None, None]:
+        jql = self._get_jql()
+
+        expand = self.definition.get("expand", [])
+
+        start_at = 0
+        max_results = 2 ** 32
+        result_limit = self.definition.get("limit", 2 ** 32)
+
+        task: Optional[TaskID] = None
+        if self.progress_bar_enabled:
+            task = progress.add_task("select", total=min(max_results, result_limit))
+
+        while start_at < max_results:
+            results = self.jira.search_issues(
+                jql, startAt=start_at, expand=",".join(expand), fields="*all",
+            )
+            max_results = results.total
+            for result in results:
+                if task is not None:
+                    progress.update(
+                        task, advance=1, total=min(max_results, result_limit)
+                    )
+
+                yield result
+                start_at += 1
+
+                # Return early if our result limit has been reached
+                if start_at >= result_limit:
+                    return
+
+    def _process_group_by(
+        self, iterator: Generator[Result, None, None], progress: Progress
+    ) -> Generator[Result, None, None]:
+        group_fields = self.definition.get("group_by", [])
+        groups: Dict[int, Result] = {}
+
+        if not group_fields:
+            for row in iterator:
+                yield Result.for_row(self, row=row)
+            return
+
+        task: Optional[TaskID] = None
+        if self.progress_bar_enabled:
+            task = progress.add_task("group_by", total=1)
+
+        for row in iterator:
+            row_hash = calculate_result_hash(
+                # We only need this particular Row instance for long
+                # enough to calculate expression results
+                Result.for_row(self, group_fields, row),
+                self._functions,
+            )
+            if row_hash not in groups:
+                groups[row_hash] = Result(self, group_fields)
+
+            groups[row_hash].add(row)
+
+        if task is not None:
+            progress.update(task, completed=0.5)
+
+        for _, value in groups.items():
+            yield value
+
+        if task is not None:
+            progress.update(task, completed=1)
+
+    def _process_having(
+        self, iterator: Generator[Result, None, None], progress: Progress
+    ) -> Generator[Result, None, None]:
+        if not self.definition.get("having", []):
+            yield from iterator
+            return
+
+        task: Optional[TaskID] = None
+        if self.progress_bar_enabled:
+            task = progress.add_task("having", total=1)
+
+        for row in iterator:
+            include_row = True
+            for having in self.definition["having"]:
+                if not get_field_data(row, having, self._functions):
+                    include_row = False
+                    break
+
+            if include_row:
+                yield row
+
+        if task is not None:
+            progress.update(task, completed=1)
+
+    def _process_order_by(
+        self, iterator: Generator[Result, None, None], progress: Progress
+    ) -> Generator[Result, None, None]:
+        if not self.definition.get("order_by", []):
+            yield from iterator
+            return
+
+        task: Optional[TaskID] = None
+        if self.progress_bar_enabled:
+            task = progress.add_task(
+                "order_by", total=len(self.definition["order_by"]) + 1
+            )
+
+        # First, materialize our list
+        rows = list(iterator)
+
+        # Now, order by each of the ordering expressions in reverse order
+        for order_by in reversed(self.definition["order_by"]):
+            rows = sorted(
+                rows,
+                key=lambda row: get_field_data(
+                    row, parse_order_by_definition(order_by), self._functions
+                ),
+            )
+            if task is not None:
+                progress.update(task, advance=1)
+
+        yield from rows
+
+        if task is not None:
+            progress.update(task, advance=1)
+
+    def _get_iterator(self, progress: Progress) -> Generator[Any, None, None]:
+        source = self.definition["from"]
+
+        if source == "issues":
+            iterator = self._get_issues
+        else:
+            raise NotImplementedError(f"No search for source {source} implemented.")
+
+        yield from self._process_order_by(
+            self._process_having(
+                self._process_group_by(iterator(progress), progress), progress
+            ),
+            progress,
+        )
+
+    def __iter__(self) -> Generator[Dict[str, Any], None, None]:
+        with Progress() as progress:
+            for row in self._get_iterator(progress):
+                yield self._generate_row_dict(row)
---
+++ packages/jira-select/jira-select-1.1.0.dev26.tar.gz/jira-select-1.1.0.dev26/jira_select/utils.py
@@ -0,0 +1,155 @@
+from __future__ import annotations
+
+import hashlib
+import logging
+import os
+import re
+from typing import TYPE_CHECKING, Any, Callable, Dict, Optional, Union
+
+from appdirs import user_config_dir
+from simpleeval import AttributeDoesNotExist, simple_eval
+from yaml import safe_dump, safe_load
+
+from .constants import APP_NAME
+from .types import ConfigDict, QueryDefinition, SelectFieldDefinition
+
+if TYPE_CHECKING:
+    from .query import Result
+
+FIELD_DISPLAY_DEFN_RE = re.compile(r'^(?P<expression>.*) as "(?P<column>.*)"$')
+ORDER_BY_DESC_FN = re.compile(r"^(?P<expression>.*) DESC", re.IGNORECASE)
+
+
+logger = logging.getLogger(__name__)
+
+
+def get_default_config_path() -> str:
+    root_path = user_config_dir(APP_NAME, "coddingtonbear")
+    os.makedirs(root_path, exist_ok=True)
+    return os.path.join(root_path, "config.yaml",)
+
+
+def get_config(path: str = None) -> ConfigDict:
+    if path is None:
+        path = get_default_config_path()
+
+    if not os.path.isfile(path):
+        return {}
+
+    with open(path, "r") as inf:
+        return safe_load(inf)
+
+
+def save_config(data: ConfigDict, path: str = None) -> None:
+    if path is None:
+        path = get_default_config_path()
+
+    with open(path, "w") as outf:
+        safe_dump(data, outf)
+
+
+def clean_query_definition(query: QueryDefinition) -> QueryDefinition:
+    cleaned_query: QueryDefinition = {
+        "select": [],
+        "from": query["from"],
+    }
+
+    for select in query["select"]:
+        if isinstance(select, dict):
+            cleaned_query["select"].append(select)
+        else:
+            cleaned_query["select"].append(str(select))
+
+    for section in (
+        "where",
+        "having",
+        "group_by",
+        "order_by",
+        "expand",
+    ):
+        if section in query:
+            # I'm not sure I understand why the typing checks here are failing
+            cleaned_query[section] = [str(line) for line in query[section]]  # type: ignore
+
+    if "limit" in query:
+        cleaned_query["limit"] = query["limit"]
+
+    return cleaned_query
+
+
+def parse_select_definition(
+    expression: Union[str, SelectFieldDefinition]
+) -> SelectFieldDefinition:
+    if isinstance(expression, str):
+        as_match = FIELD_DISPLAY_DEFN_RE.match(expression)
+        column = expression
+        expression = expression
+        if as_match:
+            match_dict = as_match.groupdict()
+            expression = match_dict["expression"]
+            column = match_dict["column"]
+
+        return {"expression": expression, "column": column}
+    return expression
+
+
+def parse_order_by_definition(expression: str):
+    is_reversed = ORDER_BY_DESC_FN.match(expression)
+
+    if is_reversed:
+        expression = is_reversed.groupdict()["expression"]
+
+        return f"-1 * ({expression})"
+
+    return expression
+
+
+def calculate_result_hash(row: Result, functions: Dict[str, Callable]) -> int:
+    params = [
+        str(get_field_data(row, group_field, functions))
+        for group_field in row.value_fields
+    ]
+
+    return int(hashlib.sha1(":".join(params).encode("UTF-8")).hexdigest(), 16)
+
+
+def get_row_dict(row: Any) -> Dict[str, Any]:
+    names: Dict[str, Any] = {}
+
+    if hasattr(row, "fields"):
+        for field_name in dir(row.fields):
+            names[field_name] = getattr(row.fields, field_name)
+
+    # Gather any top-level keys, too, to make sure we fetch any expansions
+    for key in dir(row):
+        value = getattr(row, key)
+        if (
+            key.lower() == key
+            and not key.startswith("_")
+            and isinstance(value, (str, float, int, list, dict))
+        ):
+            names[key] = getattr(row, key)
+
+    return names
+
+
+def evaluate_expression(
+    expression: str,
+    names: Dict[str, Any],
+    functions: Optional[Dict[str, Callable]] = None,
+) -> Any:
+    return simple_eval(expression, names=names, functions=functions)
+
+
+def get_field_data(
+    row: Result, expression: str, functions: Optional[Dict[str, Callable]] = None
+) -> Any:
+    if functions is None:
+        functions = {}
+
+    try:
+        return simple_eval(
+            expression, names={"_": row, **row.as_dict()}, functions=functions
+        )
+    except (AttributeDoesNotExist, KeyError, IndexError, TypeError):
+        return None
