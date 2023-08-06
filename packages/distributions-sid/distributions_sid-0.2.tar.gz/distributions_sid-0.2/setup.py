2fd784fcac8f505369f4e12c88e05b5d187fb8fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:06 +0000

    Add package distributions-sid/0.2/distributions_sid-0.2.tar.gz

---
+++ packages/distributions-sid/distributions_sid-0.2.tar.gz/distributions_sid-0.2/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_sid',
+      version='0.2',
+      description='Gaussian distributions',
+      packages=['distributions_sid'],
+      zip_safe=False)
