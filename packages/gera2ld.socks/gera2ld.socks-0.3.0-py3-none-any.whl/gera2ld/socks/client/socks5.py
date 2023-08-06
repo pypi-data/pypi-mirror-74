6de6f5a20a504ac729f7be15f7a302f3b356c42f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:43 +0000

    Add package gera2ld.socks/0.3.0/gera2ld.socks-0.3.0-py3-none-any.whl

---
+++ packages/gera2ld.socks/gera2ld.socks-0.3.0-py3-none-any.whl/gera2ld/socks/client/socks5.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# coding=utf-8
+import struct, socket
+from ..utils import SOCKS5MixIn, get_host, EMPTY_ADDR
+from .base import BaseClient
+from .udp import UDPClient
+
+class SOCKS5Client(BaseClient, SOCKS5MixIn):
+    '''
+    SOCKS5 client
+    '''
+    def __init__(self, addr, auth=None, remote_dns=False):
+        super().__init__(addr, remote_dns)
+        self.auth = auth
+        self.methods = [0]
+        if auth:
+            # validate auth info
+            user, pwd = auth
+            self.methods.append(2)
+
+    async def hand_shake(self, command, addr):
+        l = len(self.methods)
+        self.writer.write(struct.pack('B%dB' % l, l, *self.methods))
+        version, method = struct.unpack('BB', await self.reader.readexactly(2))
+        assert version == 5, 'Version unmatched'
+        assert method < 255, 'Method unsupported'
+        if method == 2:
+            user, pwd = self.auth
+            luser = len(user)
+            lpwd = len(pwd)
+            data = struct.pack('BB%dsB%ds' % (luser, lpwd), 1, luser, user.encode(), lpwd, pwd.encode())
+            self.writer.write(data)
+            _, ret = struct.unpack('BB', await self.reader.readexactly(2))
+            assert _ == 1
+            assert ret == 0, 'Authentication failed'
+        if not self.remote_dns:
+            addr = (await get_host(addr[0])), addr[1]
+        data = struct.pack('BBB', self.version, command, 0) + self.pack_address(addr)
+        self.writer.write(data)
+
+    async def get_address(self):
+        _, addr_type = struct.unpack('BB', await self.reader.readexactly(2))
+        if addr_type == 1:
+            # IPv4
+            host = socket.inet_ntoa(await self.reader.readexactly(4))
+        elif addr_type == 3:
+            # Hostname
+            l, = struct.unpack('B', await self.reader.readexactly(1))
+            host = await self.reader.readexactly(l)
+        elif addr_type == 4:
+            # IPv6
+            host = socket.inet_ntop(socket.AF_INET6, await self.reader.readexactly(16))
+        port, = struct.unpack('!H', await self.reader.readexactly(2))
+        return host, port
+
+    async def handle_udp(self):
+        await self.handle_connect_proxy()
+        await self.hand_shake(3, EMPTY_ADDR)
+        await self.get_reply()
+        client = UDPClient(self.proxy_addr)
+        await client.initialize()
+        return client
---
+++ packages/gera2ld.socks/gera2ld.socks-0.3.0-py3-none-any.whl/gera2ld/socks/server/socks4.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# coding=utf-8
+import struct, socket, io
+from .base import BaseHandler
+from ..utils import SOCKS4MixIn
+from .logger import logger
+
+class SOCKS4Handler(BaseHandler, SOCKS4MixIn):
+    '''
+    SOCKS4 Handler
+    '''
+    commands = {
+        1: 'connect',
+        2: 'bind',
+    }
+
+    async def read_until_null(self):
+        buf = io.BytesIO()
+        while True:
+            byte = await self.reader.readexactly(1)
+            if byte == b'\0':
+                break
+            buf.write(byte)
+        return buf.getvalue()
+
+    def reply(self, code, addr=None):
+        self.writer.write(struct.pack('BB', self.reply_flag, code))
+        # address must be IPv4
+        self.writer.write(self.pack_address(addr))
+
+    async def hand_shake(self):
+        command, port = struct.unpack('!BH', (await self.reader.readexactly(3)))
+        ip = await self.reader.readexactly(4)
+        ipn, = struct.unpack('!I', ip)
+        # SOCKS4a supports domain names by given IP 0.0.0.x (x > 0)
+        socks4a = ipn < 256
+        userid = await self.read_until_null()
+        if socks4a:
+            host = await self.read_until_null()
+        else:
+            host = socket.inet_ntoa(ip)
+        self.addr = host, port
+        logger.debug('hand_shake v4: %s %s:%d', command, host, port)
+        return command
