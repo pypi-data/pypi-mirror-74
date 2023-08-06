7973240bce127c0e221dedd0beadece2415b2aa1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:16 +0000

    Add package pydatastructures/0.2.2/pydatastructures-0.2.2.tar.gz

---
+++ packages/pydatastructures/pydatastructures-0.2.2.tar.gz/pydatastructures-0.2.2/setup.py
@@ -0,0 +1,30 @@
+from distutils.core import setup
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setup(
+  name = 'pydatastructures',         
+  packages = ['pydatastructures'],   # Chose the same as "name"
+  version = '0.2.2',      
+  license='MIT',       
+  description = 'This is a python library to implement data structures and algorithms',
+  long_description=long_description,
+  author = 'Juan Pablo Montoya',                   
+  author_email = 'A01251887@itesm.mx',      
+  url = 'https://github.com/JuanPabloMontoya271/pydatastructures.git',   
+  download_url = "https://github.com/JuanPabloMontoya271/pydatastructures/archive/v_02.2.tar.gz",
+
+  keywords = ['python', 'data structures'],
+  install_requires=[            # I get to this in a second
+          'numpy'
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
