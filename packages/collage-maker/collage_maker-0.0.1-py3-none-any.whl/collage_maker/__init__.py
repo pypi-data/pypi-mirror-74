3286eca79e2df67bfbcbf8b5975a1bbcb7af8d6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:01 +0000

    Add package collage-maker/0.0.1/collage_maker-0.0.1-py3-none-any.whl

---
+++ packages/collage-maker/collage_maker-0.0.1-py3-none-any.whl/collage_maker/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*- 
+"""
+Project:        image_collage_maker
+Creator:        TROY.MAO
+Create time:    2020-07-19 00:10 Shanghai, China
+Filename:       __init__.py
+Introduction:   - 
+"""
+from . import collage_maker
