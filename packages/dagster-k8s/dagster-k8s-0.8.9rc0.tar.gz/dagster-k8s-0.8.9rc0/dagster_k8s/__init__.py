b1575d2dfbbdb6b09a04f0ec16f6c3af2f9410ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:57 +0000

    Add package dagster-k8s/0.8.9rc0/dagster-k8s-0.8.9rc0.tar.gz

---
+++ packages/dagster-k8s/dagster-k8s-0.8.9rc0.tar.gz/dagster-k8s-0.8.9rc0/dagster_k8s/__init__.py
@@ -0,0 +1,9 @@
+from dagster.core.utils import check_dagster_package_version
+
+from .job import DagsterK8sJobConfig, construct_dagster_graphql_k8s_job
+from .launcher import K8sRunLauncher
+from .version import __version__
+
+check_dagster_package_version('dagster-k8s', __version__)
+
+__all__ = ['K8sRunLauncher']
