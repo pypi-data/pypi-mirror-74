a42d6d134cb271309643085f78df36b46a640114
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:09 +0000

    Add package common-ui-components/2.3.0/common-ui-components-2.3.0.tar.gz

---
+++ packages/common-ui-components/common-ui-components-2.3.0.tar.gz/common-ui-components-2.3.0/commonuicomponents/__init__.py
@@ -0,0 +1,7 @@
+from .appbuilder import AppBuilder
+from .applauncher import AppLauncher
+from .basetab import BaseTab
+from .basetabloader import BaseTabLoader
+from .json import Config, Storage
+from .staticutils import StaticUtils
+from .version import __version__
