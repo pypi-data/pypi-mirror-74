d8268bef92969d885c7ac1e32e57a0c5f6eb63ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:44 +0000

    Add package py-dsnt-probability/0.1/py_dsnt_probability-0.1.tar.gz

---
+++ packages/py-dsnt-probability/py_dsnt_probability-0.1.tar.gz/py_dsnt_probability-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='py_dsnt_probability',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['py_dsnt_probability'],
+      zip_safe=False)
