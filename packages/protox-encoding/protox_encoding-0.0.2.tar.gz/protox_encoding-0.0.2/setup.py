6784d846469d485ba96752dc72f4f1cec7c70315
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:49 +0000

    Add package protox-encoding/0.0.2/protox_encoding-0.0.2.tar.gz

---
+++ packages/protox-encoding/protox_encoding-0.0.2.tar.gz/protox_encoding-0.0.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+try:
+    from Cython.Build import cythonize
+except ModuleNotFoundError:
+    raise RuntimeError(
+        'Please install Cython to compile the library from source'
+    )
+
+setup(
+    name='protox_encoding',
+    version='0.0.2',
+    url='http://github.com/sergey-tikhonov/protox_encoding',
+    description='Protox encoding library written in Cython',
+    long_description=open('README.md', 'r').read(),
+    long_description_content_type='text/markdown',
+    ext_modules=cythonize('protox_encoding.pyx'),
+    package_data={'': ['protox_encoding.pyx']},
+    author='Sergey Tikhonov',
+    author_email='srg.tikhonov@gmail.com',
+    license='MIT',
+    zip_safe=False,
+    python_requires=">=3.6",
+)
