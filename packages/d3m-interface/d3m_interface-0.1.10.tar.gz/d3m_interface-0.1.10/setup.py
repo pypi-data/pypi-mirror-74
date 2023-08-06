ab99dd12cf722c1259ece6e73faa525f686b2bbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:06 +0000

    Add package d3m-interface/0.1.10/d3m_interface-0.1.10.tar.gz

---
+++ packages/d3m-interface/d3m_interface-0.1.10.tar.gz/d3m_interface-0.1.10/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="d3m_interface",
+    version="0.1.10",
+    author="Roque Lopez",
+    author_email="rlopez@nyu.edu",
+    description="Library to use D3M AutoML Systems",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.com/ViDA-NYU/d3m/d3m_interface",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'ta3ta2-api==2020.6.2',
+        'd3m==2020.5.18',
+        'pipelineprofiler==0.1.15',
+        'pandas==1.0.3',
+        'altair==4.1.0',
+        'datamart_profiler==0.6.2'
+    ],
+    python_requires='>=3.6',
+)
