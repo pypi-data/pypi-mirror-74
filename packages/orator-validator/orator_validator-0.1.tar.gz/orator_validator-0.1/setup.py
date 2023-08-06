47b2e9b3a88b4810fe4eea7a20c55fd22c3edf32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:37 +0000

    Add package orator-validator/0.1/orator_validator-0.1.tar.gz

---
+++ packages/orator-validator/orator_validator-0.1.tar.gz/orator_validator-0.1/setup.py
@@ -0,0 +1,19 @@
+import setuptools, sys
+
+with open("README.rst", 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="orator_validator",
+    version="0.1",
+    author="Alfonso Villalobos",
+    author_email="alfonso@codepeat.com",
+    long_description=long_description,
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    python_requires='>=3.7'
+)
