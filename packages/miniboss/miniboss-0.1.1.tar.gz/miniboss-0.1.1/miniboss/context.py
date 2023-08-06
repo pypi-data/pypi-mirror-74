90deb14f2fdaa6e9db3300620122e3d855a7fd9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:03 +0000

    Add package miniboss/0.1.1/miniboss-0.1.1.tar.gz

---
+++ packages/miniboss/miniboss-0.1.1.tar.gz/miniboss-0.1.1/miniboss/context.py
@@ -0,0 +1,23 @@
+class ContextError(Exception):
+    pass
+
+class _Context(dict):
+
+    def extrapolate(self, env_value):
+        if not hasattr(env_value, "format"):
+            return env_value
+        try:
+            return env_value.format(**self)
+        except KeyError:
+            raise ContextError("Could not extrapolate string '{}', existing keys: {}".format(
+                env_value, ",".join(self.keys())))
+        except ValueError:
+            # This happens when there is a type mismatch
+            raise ContextError("Could not extrapolate string '{}' due to type mismatch".format(env_value))
+        except IndexError:
+            raise ContextError("Only keyword argument extrapolation allowed, violating string: '{}'".format(env_value))
+
+    def extrapolate_values(self, a_dict):
+        return {key: self.extrapolate(value) for key,value in a_dict.items()}
+
+Context = _Context()
