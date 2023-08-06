4f3479ee182446c9441ed7090cfaa77812a57ea0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:34 +0000

    Add package nikita-probability/0.1/nikita_probability-0.1.tar.gz

---
+++ packages/nikita-probability/nikita_probability-0.1.tar.gz/nikita_probability-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='nikita_probability',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['nikita_probability'],
+      zip_safe=False)
