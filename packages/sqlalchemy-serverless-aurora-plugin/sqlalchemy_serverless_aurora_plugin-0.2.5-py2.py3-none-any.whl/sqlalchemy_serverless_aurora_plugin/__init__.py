2db2eaebc2d04f467d4fe4304df16e0c76ecfe3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:19 +0000

    Add package sqlalchemy-serverless-aurora-plugin/0.2.5/sqlalchemy_serverless_aurora_plugin-0.2.5-py2.py3-none-any.whl

---
+++ packages/sqlalchemy-serverless-aurora-plugin/sqlalchemy_serverless_aurora_plugin-0.2.5-py2.py3-none-any.whl/sqlalchemy_serverless_aurora_plugin/__init__.py
@@ -0,0 +1,128 @@
+"""
+sqlalchemy-aurora-data-api
+"""
+
+import json, datetime, re
+
+from sqlalchemy import cast, func, util
+import sqlalchemy.sql.sqltypes as sqltypes
+from sqlalchemy.dialects.postgresql.base import PGDialect
+from sqlalchemy.dialects.postgresql import JSON, JSONB, UUID, DATE, TIME, TIMESTAMP, ARRAY, ENUM
+from sqlalchemy.dialects.mysql.base import MySQLDialect
+
+import serverless_aurora
+
+
+class AuroraMySQLDataAPIDialect(MySQLDialect):
+    # See https://docs.sqlalchemy.org/en/13/core/internals.html#sqlalchemy.engine.interfaces.Dialect
+    driver = "serverless_aurora"
+    default_schema_name = None
+
+    @classmethod
+    def dbapi(cls):
+        return serverless_aurora
+
+    def _detect_charset(self, connection):
+        return connection.execute("SHOW VARIABLES LIKE 'character_set_client'").fetchone()[1]
+
+    def _extract_error_code(self, exception):
+        return exception.args[0]
+
+
+class _ADA_SA_JSON(sqltypes.JSON):
+    def bind_expression(self, value):
+        return cast(value, sqltypes.JSON)
+
+
+class _ADA_JSON(JSON):
+    def bind_expression(self, value):
+        return cast(value, JSON)
+
+
+class _ADA_JSONB(JSONB):
+    def bind_expression(self, value):
+        return cast(value, JSONB)
+
+
+class _ADA_UUID(UUID):
+    def bind_expression(self, value):
+        return cast(value, UUID)
+
+
+class _ADA_ENUM(ENUM):
+    def bind_expression(self, value):
+        return cast(value, self)
+
+
+# TODO: is TZ awareness needed here?
+class _ADA_DATETIME_MIXIN:
+    iso_ts_re = re.compile(r"\d{4}-\d\d-\d\d \d\d:\d\d:\d\d\.\d+")
+
+    def bind_processor(self, dialect):
+        def process(value):
+            return value.isoformat() if isinstance(value, self.py_type) else value
+        return process
+
+    def bind_expression(self, value):
+        return cast(value, self.sa_type)
+
+    def result_processor(self, dialect, coltype):
+        def process(value):
+            # When the microsecond component ends in zeros, they are omitted from the return value,
+            # and datetime.datetime.fromisoformat can't parse the result (example: '2019-10-31 09:37:17.31869'). Pad it.
+            if isinstance(value, str) and self.iso_ts_re.match(value):
+                value = self.iso_ts_re.sub(lambda match: match.group(0).ljust(26, "0"), value)
+            return self.py_type.fromisoformat(value) if isinstance(value, str) else value
+        return process
+
+
+class _ADA_DATE(_ADA_DATETIME_MIXIN, DATE):
+    py_type = datetime.date
+    sa_type = sqltypes.Date
+
+
+class _ADA_TIME(_ADA_DATETIME_MIXIN, TIME):
+    py_type = datetime.time
+    sa_type = sqltypes.Time
+
+
+class _ADA_TIMESTAMP(_ADA_DATETIME_MIXIN, TIMESTAMP):
+    py_type = datetime.datetime
+    sa_type = sqltypes.DateTime
+
+
+class _ADA_ARRAY(ARRAY):
+    def bind_processor(self, dialect):
+        def process(value):
+            # FIXME: escape strings properly here
+            return "\v".join(value) if isinstance(value, list) else value
+        return process
+
+    def bind_expression(self, value):
+        return func.string_to_array(value, "\v")
+
+
+class AuroraPostgresDataAPIDialect(PGDialect):
+    # See https://docs.sqlalchemy.org/en/13/core/internals.html#sqlalchemy.engine.interfaces.Dialect
+    driver = "serverless_aurora"
+    default_schema_name = None
+    colspecs = util.update_copy(PGDialect.colspecs, {
+        sqltypes.JSON: _ADA_SA_JSON,
+        JSON: _ADA_JSON,
+        JSONB: _ADA_JSONB,
+        UUID: _ADA_UUID,
+        sqltypes.Date: _ADA_DATE,
+        sqltypes.Time: _ADA_TIME,
+        sqltypes.DateTime: _ADA_TIMESTAMP,
+        sqltypes.Enum: _ADA_ENUM,
+        ARRAY: _ADA_ARRAY
+    })
+    @classmethod
+    def dbapi(cls):
+        return serverless_aurora
+
+
+def register_dialects():
+    from sqlalchemy.dialects import registry
+    registry.register("mysql.auroradataapi", __name__, AuroraMySQLDataAPIDialect.__name__)
+    registry.register("postgresql.auroradataapi", __name__, AuroraPostgresDataAPIDialect.__name__)
