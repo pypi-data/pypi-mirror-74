81afa80894ee1bdc05149e3e05cae18d3affc011
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:48 +0000

    Add package dsnd-probability-jlfsjr/1.0/dsnd_probability_jlfsjr-1.0.tar.gz

---
+++ packages/dsnd-probability-jlfsjr/dsnd_probability_jlfsjr-1.0.tar.gz/dsnd_probability_jlfsjr-1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='dsnd_probability_jlfsjr',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      long_description='This python package is part of an AWS Machine Learning Foundation Course outcome',
+      long_description_content_type='text/plain',
+      packages=['dsnd_probability_jlfsjr'],
+      author = 'Jorge Luiz Figueira',
+      author_email = 'jorgeluizfigueira@gmail.com',
+      zip_safe=False)
