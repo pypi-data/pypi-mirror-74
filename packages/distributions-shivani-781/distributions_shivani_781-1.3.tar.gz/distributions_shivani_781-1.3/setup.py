985f8f81612976195bb36946f46bf6197ab2eac7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:11 +0000

    Add package distributions-shivani-781/1.3/distributions_shivani_781-1.3.tar.gz

---
+++ packages/distributions-shivani-781/distributions_shivani_781-1.3.tar.gz/distributions_shivani_781-1.3/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='distributions_shivani_781',
+      version='1.3',
+      description='Gaussian & Normal distributions',
+      packages=['distributions_shivani_781'],
+      author = 'Shivani Chauhan',
+      zip_safe=False)
