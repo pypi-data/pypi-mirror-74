5fefb23b1b660fcda5d20942c301cf763aa050cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:17 +0000

    Add package finitelycomputable-flask-dispatcher/20.7/finitelycomputable-flask-dispatcher-20.7.tar.gz

---
+++ packages/finitelycomputable-flask-dispatcher/finitelycomputable-flask-dispatcher-20.7.tar.gz/finitelycomputable-flask-dispatcher-20.7/finitelycomputable_microsites_setup.py
@@ -0,0 +1,44 @@
+version = '20.7'
+#version += 'a0'
+
+base_setup = {
+    'package_dir': {'': '.'},
+    'package_data': {'': ['templates/*/*']},
+    'data_files': [('', ['README.rst', 'LICENSE.txt'])],
+    'include_package_data': False,
+    'license': 'AGPL-3.0-only',
+    'python_requires': '>=3.6',
+    'author': 'Samuel Newbold',
+    'author_email': 'sam@rwsh.org',
+    'classifiers': [
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Affero General Public License v3',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Internet :: WWW/HTTP :: WSGI',
+    ],
+    'project_urls': {
+        'Documentation': 'https://github.com/thrasymache/microsites',
+        'Source': 'https://github.com/thrasymache/microsites',
+    },
+}
+
+django_setup = {
+    'classifiers': [
+        'Framework :: Django',
+        'Framework :: Django :: 2.0',
+    ],
+}
+
+wsgi_extras_require={
+        'bjoern': ['bjoern<4'],
+        'cheroot': ['cheroot<9'],
+        'cherrypy': ['cherrypy<19'],
+        'gunicorn': ['gunicorn<20'],
+        'waitress': ['waitress<1.4'],
+        }
