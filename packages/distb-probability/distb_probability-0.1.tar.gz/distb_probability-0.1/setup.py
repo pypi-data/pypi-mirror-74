e1c11bab9a9eac85e1d6cc07d7d18bb960841541
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:46 +0000

    Add package distb-probability/0.1/distb_probability-0.1.tar.gz

---
+++ packages/distb-probability/distb_probability-0.1.tar.gz/distb_probability-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distb_probability',
+      version='0.1',
+      description='Gaussian_Binomial distributions',
+      packages=['distb_probability'],
+      zip_safe=False)
