ab24c1f6385cb400092a8e85c911948a0ccb98db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:27 +0000

    Add package vvs25-distributions/0.1/vvs25_distributions-0.1.tar.gz

---
+++ packages/vvs25-distributions/vvs25_distributions-0.1.tar.gz/vvs25_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='vvs25_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['vvs25_distributions'],
+      author = 'Varun Vinod Salian',
+      author_email = 'salianvarun30@gmail.com',
+      zip_safe=False)
