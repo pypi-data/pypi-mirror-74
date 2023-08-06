6ee3b4e1648e852040acb3d2f3130b1964a36a85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:59 +0000

    Add package eo-lib/0.2.2/eo_lib-0.2.2-py3-none-any.whl

---
+++ packages/eo-lib/eo_lib-0.2.2-py3-none-any.whl/eo_lib/config/config.py
@@ -0,0 +1,14 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+SQLALCHEMY_DATABASE_URI = "postgres://seon:seon@localhost/eo_ontology"
+engine = create_engine(SQLALCHEMY_DATABASE_URI)
+session = sessionmaker(bind=engine)
+session = session()
+Base = declarative_base()
+
+class Config():
+
+    def create_database(self):
+        Base.metadata.create_all(engine)
