a84548c2234ad6b7654b3963b108b3b96207aecd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:31 +0000

    Add package pyapt/0.0.6/pyapt-0.0.6-py3-none-any.whl

---
+++ packages/pyapt/pyapt-0.0.6-py3-none-any.whl/pyapt/core/core.py
@@ -0,0 +1,11 @@
+import json
+import os
+
+
+def read_config():
+    file_name = os.path.expanduser("~/.pyapt/config.json")
+    data = json.loads(file_name)
+    return data
+
+def apply_ppa():
+    pass
