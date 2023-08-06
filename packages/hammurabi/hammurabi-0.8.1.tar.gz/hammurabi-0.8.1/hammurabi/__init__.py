ceefa0d76c36a59a56dc96d2082ce1f5fb2c06c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:39 +0000

    Add package hammurabi/0.8.1/hammurabi-0.8.1.tar.gz

---
+++ packages/hammurabi/hammurabi-0.8.1.tar.gz/hammurabi-0.8.1/hammurabi/__init__.py
@@ -0,0 +1,60 @@
+# flake8: noqa
+
+from hammurabi.config import config
+from hammurabi.law import Law
+from hammurabi.notifications.base import Notification
+from hammurabi.notifications.slack import SlackNotification
+from hammurabi.pillar import Pillar
+from hammurabi.preconditions.attributes import (
+    HasMode,
+    HasNoMode,
+    IsNotOwnedBy,
+    IsOwnedBy,
+)
+from hammurabi.preconditions.base import Precondition
+from hammurabi.preconditions.directories import IsDirectoryExists, IsDirectoryNotExists
+from hammurabi.preconditions.files import IsFileExists, IsFileNotExists
+from hammurabi.preconditions.text import IsLineExists, IsLineNotExists
+from hammurabi.reporters.base import Reporter
+from hammurabi.reporters.json import JSONReporter
+from hammurabi.rules.attributes import ModeChanged, OwnerChanged
+from hammurabi.rules.base import Rule
+from hammurabi.rules.directories import (
+    DirectoryEmptied,
+    DirectoryExists,
+    DirectoryNotExists,
+)
+from hammurabi.rules.files import (
+    FileEmptied,
+    FileExists,
+    FileNotExists,
+    FilesExist,
+    FilesNotExist,
+)
+from hammurabi.rules.ini import (
+    OptionRenamed,
+    OptionsExist,
+    OptionsNotExist,
+    SectionExists,
+    SectionNotExists,
+    SectionRenamed,
+)
+from hammurabi.rules.json import (
+    JSONKeyExists,
+    JSONKeyNotExists,
+    JSONKeyRenamed,
+    JSONValueExists,
+    JSONValueNotExists,
+)
+from hammurabi.rules.operations import Copied, Moved, Renamed
+from hammurabi.rules.templates import TemplateRendered
+from hammurabi.rules.text import LineExists, LineNotExists, LineReplaced
+from hammurabi.rules.yaml import (
+    YAMLKeyExists,
+    YAMLKeyNotExists,
+    YAMLKeyRenamed,
+    YAMLValueExists,
+    YAMLValueNotExists,
+)
+
+__version__ = "0.8.1"
