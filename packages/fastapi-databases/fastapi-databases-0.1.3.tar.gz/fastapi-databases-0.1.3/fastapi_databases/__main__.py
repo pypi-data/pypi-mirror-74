c0fe414ff496b1c75d7c158ba8a262f08871eba6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:10 +0000

    Add package fastapi-databases/0.1.3/fastapi-databases-0.1.3.tar.gz

---
+++ packages/fastapi-databases/fastapi-databases-0.1.3.tar.gz/fastapi-databases-0.1.3/fastapi_databases/__main__.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+import typer
+from pprint import pprint
+from enum import Enum
+
+from alembic.config import Config
+from alembic import command
+
+alembic_cfg = Config("/path/to/yourapp/alembic.ini")
+
+cli = typer.Typer()
+
+
+@cli.command()
+def init():
+    """Initialize db"""
+
+    typer.echo(typer.style("fetching data from", fg="green"))
+
+    command.init(alembic_cfg, "alembic", template="generic", package=True)
+    return
+
+
+@cli.command()
+def makemigrations():
+    """make db migrations"""
+
+    command.revision(alembic_cfg, autogenerate=True)
+    return
+
+
+@cli.command()
+def migrate():
+    """migrate db"""
+
+    command.upgrade(alembic_cfg, "head")
+    return
+
+
+if __name__ == "__main__":
+    cli()
