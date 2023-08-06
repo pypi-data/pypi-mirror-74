f5f96ecd088924beef6ac64126c1d3130d9ab594
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:34 +0000

    Add package redshift-query/0.1.3/redshift-query-0.1.3.tar.gz

---
+++ packages/redshift-query/redshift-query-0.1.3.tar.gz/redshift-query-0.1.3/redshift_query/__init__.py
@@ -0,0 +1,7 @@
+"""Top-level package for redshift-query."""
+
+__author__ = """Farid Nouri Neshat"""
+__email__ = 'faridn@helecloud.com'
+__version__ = '0.1.3'
+
+from .redshift_query import set_config, query
