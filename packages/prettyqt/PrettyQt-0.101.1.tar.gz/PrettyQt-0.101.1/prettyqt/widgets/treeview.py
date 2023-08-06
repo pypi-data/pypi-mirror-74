c4a785bea09fe2c12a717a6c65cad993d3ad699b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:54 +0000

    Add package prettyqt/0.101.1/PrettyQt-0.101.1.tar.gz

---
+++ packages/prettyqt/PrettyQt-0.101.1.tar.gz/PrettyQt-0.101.1/prettyqt/widgets/treeview.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+"""
+"""
+
+from qtpy import QtWidgets
+
+from prettyqt import widgets
+
+
+QtWidgets.QTreeView.__bases__ = (widgets.AbstractItemView,)
+
+
+class TreeView(QtWidgets.QTreeView):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        class_name = type(self).__name__
+        self.set_id(class_name)
+        # visual settings
+        self.setAnimated(True)
+        self.setRootIsDecorated(False)
+        self.setAllColumnsShowFocus(True)
+        self.setUniformRowHeights(True)
+        self.setAlternatingRowColors(True)
+        self.setWordWrap(False)
+
+        # misc
+        self.h_header = widgets.HeaderView("horizontal", parent=self)
+        self.set_selection_mode("extended")
+
+    @property
+    def h_header(self):
+        return self.header()
+
+    @h_header.setter
+    def h_header(self, header):
+        self.setHeader(header)
+
+    def expand_all(self):
+        self.expandAll()
+
+    def set_indentation(self, indentation: int):
+        self.setIndentation(indentation)
+
+    def setup_list_style(self):
+        self.setSelectionBehavior(self.SelectRows)
+        self.h_header.setStretchLastSection(True)
+
+    def adapt_sizes(self):
+        model = self.model()
+        if model is not None and (model.rowCount() * model.columnCount()) < 1000:
+            self.h_header.resizeSections(self.h_header.ResizeToContents)
+        else:
+            self.h_header.resize_sections("interactive")
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication([])
+    dlg = QtWidgets.QMainWindow()
+    status_bar = TreeView()
+    dlg.show()
+    app.exec_()
