933cb0df0e14db5d93353662fc93a75a85448289
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:53 +0000

    Add package Bayesian2D/0.0.9/Bayesian2D-0.0.9.tar.gz

---
+++ packages/Bayesian2D/Bayesian2D-0.0.9.tar.gz/Bayesian2D-0.0.9/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Bayesian2D", # Replace with your own username
+    version="0.0.9",
+    author="Juhan Raidal",
+    description="Package used to find the maximum or minimum of any 2D function using Bayesian optimization",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages= setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
