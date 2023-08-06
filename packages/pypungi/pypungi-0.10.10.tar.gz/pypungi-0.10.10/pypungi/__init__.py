8586e82fa251cc236cb5c7683e255ae3b4fd39ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:57 +0000

    Add package pypungi/0.10.10/pypungi-0.10.10.tar.gz

---
+++ packages/pypungi/pypungi-0.10.10.tar.gz/pypungi-0.10.10/pypungi/__init__.py
@@ -0,0 +1,14 @@
+'''Helper tool'''
+
+__version__ = "0.10.10"
+
+from pypungi.main import hub
+
+if __name__ == '__main__':
+    pass
+    #import pypungi
+    #
+    #pp = pypungi.link()
+    #pp.stash('hi')
+    #pp.getStash()
+    
