def05e47aa9227b13402279ebeec58aa7e5c0b85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:34 +0000

    Add package BI-parser/0.0.4/BI_parser-0.0.4.tar.gz

---
+++ packages/BI-parser/BI_parser-0.0.4.tar.gz/BI_parser-0.0.4/src/BI_parser/parser.py
@@ -0,0 +1,28 @@
+'''
+Contient la classe principal de notre module et la fonction d'ajout de repertoire client.
+'''
+from typing import List
+
+from src.classes.client import Client
+
+
+class Parser:
+    '''
+    description
+    '''
+
+    list_clients: List[Client] = []
+
+    def __init__(self) -> None:  # Notre méthode constructeur
+        '''
+        description
+        '''
+
+def parse(chemin_du_repertoire: str, nom_du_client: str = '', langage: str = 'sql') -> None:
+    '''
+    description
+    '''
+
+    Parser.list_clients.append( \
+            Client(chemin_du_repertoire, str(len(Parser.list_clients) + 1), nom_du_client, langage) \
+                            )
