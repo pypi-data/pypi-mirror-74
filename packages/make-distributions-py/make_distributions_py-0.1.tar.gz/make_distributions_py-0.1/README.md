dad75d277d4d89b90bd955b27fa27ceb37ff7bc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:50 +0000

    Add package make-distributions-py/0.1/make_distributions_py-0.1.tar.gz

---
+++ packages/make-distributions-py/make_distributions_py-0.1.tar.gz/make_distributions_py-0.1/README.md
@@ -0,0 +1,1 @@
+# This is a Package for calculating and plotting Binomial and Gaussian distributions
---
+++ packages/make-distributions-py/make_distributions_py-0.1.tar.gz/make_distributions_py-0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='make_distributions_py',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions'],
+      author="Ridwan Salahuddeen",
+    author_email="ridzy619@yahoo.com",
+      classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+      zip_safe=False)
