a6a7b43258a46216e56e58b139728725069e28be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:27:01 +0000

    Add package bfengine/0.1.6/bfengine-0.1.6-py3-none-any.whl

---
+++ packages/bfengine/bfengine-0.1.6-py3-none-any.whl/bf_engine/core/bot.py
@@ -0,0 +1,16 @@
+from .dialogue_manager import DialogueManager
+from .task_engine import TaskEngine
+from .knowledge_graph import KnowledgeGraph
+from .question_answering import QuestionAnswering
+
+
+class Bot:
+    """
+    机器人
+    """
+    def __init__(self, app_id):
+        self.app_id = app_id
+        self.qa = QuestionAnswering(app_id)
+        self.kg = KnowledgeGraph(app_id)
+        self.te = TaskEngine(app_id)
+        self.dm = DialogueManager(app_id)
