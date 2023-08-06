ed28d7adc9b74a86c7a07b743e7d408948871161
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:47 +0000

    Add package pysirix/0.1.3/pysirix-0.1.3.tar.gz

---
+++ packages/pysirix/pysirix-0.1.3.tar.gz/pysirix-0.1.3/tests/test_json_store.py
@@ -0,0 +1,84 @@
+from datetime import datetime
+
+import httpx
+
+import pysirix
+from pysirix import DBType
+
+base_url = "http://localhost:9443"
+
+
+def setup_function():
+    global client
+    global sirix
+    global store
+    client = httpx.Client(base_url=base_url)
+    sirix = pysirix.sirix_sync("admin", "admin", client)
+    db = sirix.database("First", DBType.JSON)
+    store = db.json_store("test_resource")
+
+
+def teardown_function():
+    sirix.delete_all()
+    sirix.shutdown()
+    client.close()
+
+
+def test_create_store():
+    assert store.create() == "[]"
+
+
+def test_exists():
+    assert store.exists() is False
+    store.create()
+    assert store.exists() is True
+
+
+def test_insert_into_store():
+    store.create()
+    assert (
+        store.insert_one({"city": "Brooklyn", "state": "NY"})
+        == '[{"city":"Brooklyn","state":"NY"}]'
+    )
+
+
+def test_find_all_store():
+    store.create()
+    store.insert_one({"city": "New York", "state": "NY"})
+    response = store.find_all({"city": "New York"}, node_key=False)
+    assert type(response["rest"]) is list and len(response["rest"]) == 1
+    assert response["rest"][0] == {"city": "New York", "state": "NY"}
+    response = store.find_all({"city": "New York"})
+    assert response["rest"][0] == {"city": "New York", "state": "NY", "nodeKey": 2}
+
+
+def test_find_all_projection():
+    store.create()
+    store.insert_one({"key": 1, "location": {"state": "NY", "city": "New York"}})
+    store.insert_one({"key": 2, "location": {"state": "CA", "city": "Los Angeles"}})
+    response = store.find_all({"key": 2}, ["location"], node_key=False)
+    assert response["rest"][0] == {
+        "location": {"state": "CA", "city": "Los Angeles"},
+    }
+    response = store.find_all({"key": 2}, ["location"])
+    assert response["rest"][0] == {
+        "location": {"state": "CA", "city": "Los Angeles"},
+        "nodeKey": 11,
+    }
+
+
+def test_find_all_old_revision_number():
+    store.create()
+    store.insert_one({"city": "New York", "state": "NY"})
+    response = store.find_all({"city": "New York"}, revision=1)
+    assert response == {"rest": []}
+    response = store.find_all({"city": "New York"}, revision=2)
+    assert response == {"rest": [{"city": "New York", "state": "NY", "nodeKey": 2}]}
+
+
+def test_find_all_old_revision_date():
+    store.create()
+    timestamp = datetime.now()
+    store.insert_one({"city": "New York", "state": "NY"})
+    response = store.find_all({"city": "New York"}, revision=timestamp)
+    assert response == {"rest": []}
