3465e361e05b79a6ccd1eef7fc081bf1aa241810
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:42 +0000

    Add package phonopy/2.7.0/phonopy-2.7.0.tar.gz

---
+++ packages/phonopy/phonopy-2.7.0.tar.gz/phonopy-2.7.0/build-wheels.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e -x
+
+yum install -y hdf5 freetype freetype-devel pkgconfig libpng-devel
+
+for PYBIN in /opt/python/*/bin; do
+    if  [[ ! $PYBIN == *"34"* ]] && [[ ! $PYBIN == *"39"* ]]; then
+        "${PYBIN}/pip" install -r /io/dev-requirements.txt
+        "${PYBIN}/pip" wheel /io/ -w wheelhouse/
+    fi
+done
+
+# Bundle external shared libraries into the wheels
+for whl in wheelhouse/phonopy*.whl; do
+    auditwheel repair "$whl" --plat $PLAT -w /io/wheelhouse/
+done
+
+# Install packages and test
+for PYBIN in /opt/python/*/bin/; do
+    if  [[ ! $PYBIN == *"34"* ]] && [[ ! $PYBIN == *"39"* ]]; then
+        "${PYBIN}/pip" install phonopy --no-index -f /io/wheelhouse
+        (cd "$HOME"; "${PYBIN}/nosetests" phonopy)
+    fi
+done
