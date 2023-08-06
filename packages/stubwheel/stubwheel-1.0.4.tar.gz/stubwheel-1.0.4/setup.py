24c6f98a95eb6922755e1dc78e7ecc80d749a2ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:02 +0000

    Add package stubwheel/1.0.4/stubwheel-1.0.4.tar.gz

---
+++ packages/stubwheel/stubwheel-1.0.4.tar.gz/stubwheel-1.0.4/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='stubwheel',
+    version='1.0.4',
+    url='https://github.com/AlyShmahell/stubwheel',
+    author='AlyShmahell',
+    author_email='',
+    description='a script that reserves a project name on PyPi',
+    packages=['stubwheel'],    
+    install_requires=['twine >= 3.2.0'],
+)
