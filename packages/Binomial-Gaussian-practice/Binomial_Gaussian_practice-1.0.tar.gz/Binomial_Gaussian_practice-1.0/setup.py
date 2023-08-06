0ac7333aa8fd9bb3b041eaed03ef10d86ccadb62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:54 +0000

    Add package Binomial-Gaussian-practice/1.0/Binomial_Gaussian_practice-1.0.tar.gz

---
+++ packages/Binomial-Gaussian-practice/Binomial_Gaussian_practice-1.0.tar.gz/Binomial_Gaussian_practice-1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='Binomial_Gaussian_practice',
+      version='1.0',
+      description='Gaussian & Binomial distributions',
+      packages=['Binomial_Gaussian_practice'],
+      author='Sandeep',
+      zip_safe=False)
