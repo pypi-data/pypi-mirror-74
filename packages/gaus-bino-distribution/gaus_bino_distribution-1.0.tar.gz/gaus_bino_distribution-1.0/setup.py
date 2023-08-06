3b5c311f96a5da3622e69a5098b7cd7af48dbd02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:57 +0000

    Add package gaus-bino-distribution/1.0/gaus_bino_distribution-1.0.tar.gz

---
+++ packages/gaus-bino-distribution/gaus_bino_distribution-1.0.tar.gz/gaus_bino_distribution-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='gaus_bino_distribution',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['gaus_bino_distribution'],
+      author = 'Navjot Singh Bajaj',
+      author_email = 'singhnavjotb246@gmail.com',
+      zip_safe=False)
