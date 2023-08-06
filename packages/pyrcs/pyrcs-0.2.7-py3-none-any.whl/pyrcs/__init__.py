bd07f6dd5b6aaf98a18ac51d7c8cdde9f1bb075c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:18 +0000

    Add package pyrcs/0.2.7/pyrcs-0.2.7-py3-none-any.whl

---
+++ packages/pyrcs/pyrcs-0.2.7-py3-none-any.whl/pyrcs/__init__.py
@@ -0,0 +1,8 @@
+from ._line_data import *
+from ._other_assets import *
+
+__package_name__ = 'pyrcs'
+__version__ = '0.2.7'
+__author__ = 'Qian Fu'
+__email__ = 'qian.fu@outlook.com'
+__description__ = "A web-scraping tool for collecting codes being used in Britain's railway."
