272b2b30e661750313aa4f915a0d4e5b9963eec9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:17 +0000

    Add package Binomial-distributions/0.10.5/Binomial_distributions-0.10.5.tar.gz

---
+++ packages/Binomial-distributions/Binomial_distributions-0.10.5.tar.gz/Binomial_distributions-0.10.5/README.md
@@ -0,0 +1,1 @@
+This is a distribution file, contains gaussian, general and binomial distribution class.
---
+++ packages/Binomial-distributions/Binomial_distributions-0.10.5.tar.gz/Binomial_distributions-0.10.5/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='Binomial_distributions',
+      version='0.10.5',
+      description='Gaussian distributions',
+      packages=['distributions'],
+      zip_safe=False)
