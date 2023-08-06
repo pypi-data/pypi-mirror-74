241dd188385cb98562ee5baa38bd87a8590be1a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:50 +0000

    Add package john-prob-distribution/0.2/john_prob_distribution-0.2.tar.gz

---
+++ packages/john-prob-distribution/john_prob_distribution-0.2.tar.gz/john_prob_distribution-0.2/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='john_prob_distribution',
+      version='0.2',
+      description='Gaussian distributions',
+      packages=['john_prob_distribution'],
+      zip_safe=False)
