c12b68f779c806e823e339bfaa92d7a11e0f5329
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:22 +0000

    Add package tikup/2020.7.19.1/tikup-2020.7.19.1.tar.gz

---
+++ packages/tikup/tikup-2020.7.19.1.tar.gz/tikup-2020.7.19.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='tikup',
+    version='2020.07.19.1',
+    description='An auto downloader and uploader for TikTok videos.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/Coloradohusky/TikUp',
+    author='Coloradohusky',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'tikup = tikup.tikup:main',
+        ],
+    },
+    python_requires='>=3.5, <4',
+    install_requires=['internetarchive>=1.9.3', 'TikTokApi>=3.3.1', 'youtube-dl>=2020.06.16.1']
+)
