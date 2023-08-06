70eaf5080c9291c39a3822aef527e3c93b96c5bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:33 +0000

    Add package SQLAlchemy-Api-Handler/0.4.3/SQLAlchemy_Api_Handler-0.4.3-py2.py3-none-any.whl

---
+++ packages/SQLAlchemy-Api-Handler/SQLAlchemy_Api_Handler-0.4.3-py2.py3-none-any.whl/sqlalchemy_api_handler/bases/accessor.py
@@ -0,0 +1,23 @@
+import inflect
+
+
+class Accessor():
+    @classmethod
+    def get_db(cls):
+        return Accessor.db
+
+    @classmethod
+    def set_db(cls, db):
+        Accessor.db = db
+
+    @classmethod
+    def model_from_table_name(cls, table_name):
+        for model in Accessor.get_db().Model._decl_class_registry.values():
+            if not hasattr(model, '__table__'):
+                continue
+            if model.__tablename__ == table_name:
+                return model
+
+    @classmethod
+    def model_from_plural_name(cls, plural_name):
+        return Accessor.model_from_table_name(inflect.engine().singular_noun(plural_name))
