a51d79a12348cf2480b9c5e45620f03f062604ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:47 +0000

    Add package wizprint/1.0.3/wizprint-1.0.3-py3-none-any.whl

---
+++ packages/wizprint/wizprint-1.0.3-py3-none-any.whl/wizprint/__main__.py
@@ -0,0 +1,4 @@
+from .__init__ import fnt, wprint
+
+if __name__ == "__main__":
+    wprint("\t\t\t Hello, World!\n\t\thttps://jasutin.xyz/\n  https://github.com/NamasteJasutin/", foreground=fnt.G, signed="🔧 NamasteJasutin", bgchar="'")
