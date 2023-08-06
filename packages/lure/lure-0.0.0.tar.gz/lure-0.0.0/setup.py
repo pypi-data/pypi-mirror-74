8e3e63dc8e944eb91d66fd1dfb30ef3af7a501cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:43 +0000

    Add package lure/0.0.0/lure-0.0.0.tar.gz

---
+++ packages/lure/lure-0.0.0.tar.gz/lure-0.0.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="lure",
+    version="0.0.0",
+    description="",
+    long_description="",
+    url="https://github.com/jcthomassie/lure",
+    author="Julian Thomassie",
+    author_email="julianthomassie@gmail.com",
+    packages=[],
+    classifiers=["Development Status :: 1 - Planning"],
+)
