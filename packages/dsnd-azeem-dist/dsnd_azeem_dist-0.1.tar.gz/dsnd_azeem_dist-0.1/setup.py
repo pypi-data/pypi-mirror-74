3d106a79b1187aeb61cf3d3a3bec15c7cd37d481
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:28 +0000

    Add package dsnd-azeem-dist/0.1/dsnd_azeem_dist-0.1.tar.gz

---
+++ packages/dsnd-azeem-dist/dsnd_azeem_dist-0.1.tar.gz/dsnd_azeem_dist-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='dsnd_azeem_dist',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['dsnd_azeem_dist'],
+      zip_safe=False)
