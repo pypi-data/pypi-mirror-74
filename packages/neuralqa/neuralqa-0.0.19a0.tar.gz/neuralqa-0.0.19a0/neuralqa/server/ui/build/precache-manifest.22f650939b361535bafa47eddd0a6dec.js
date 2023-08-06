804d9dd3e7c02e100076a848a1c414ecaad5bb82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:07 +0000

    Add package neuralqa/0.0.19a0/neuralqa-0.0.19a0.tar.gz

---
+++ packages/neuralqa/neuralqa-0.0.19a0.tar.gz/neuralqa-0.0.19a0/neuralqa/server/ui/build/precache-manifest.22f650939b361535bafa47eddd0a6dec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f88b7207b8128c5af05df445b05a9744",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ab099d05d836433214c2",
+    "url": "/static/css/main.0010f720.chunk.css"
+  },
+  {
+    "revision": "558886bf6f1c4cbe0389",
+    "url": "/static/js/2.5fbea032.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.5fbea032.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab099d05d836433214c2",
+    "url": "/static/js/main.bae77bc9.chunk.js"
+  },
+  {
+    "revision": "b7d5e4d288fb48c3a4d5",
+    "url": "/static/js/runtime-main.985d1449.js"
+  }
+]);
