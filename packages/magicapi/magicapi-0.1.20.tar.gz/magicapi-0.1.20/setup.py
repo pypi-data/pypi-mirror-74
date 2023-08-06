1ba96b445f8bb680dc2a4250a509e11dbeeb1730
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:26 +0000

    Add package magicapi/0.1.20/magicapi-0.1.20.tar.gz

---
+++ packages/magicapi/magicapi-0.1.20.tar.gz/magicapi-0.1.20/setup.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['magicapi',
+ 'magicapi.Decorators',
+ 'magicapi.Errors',
+ 'magicapi.FieldTypes',
+ 'magicapi.Globals',
+ 'magicapi.Models',
+ 'magicapi.Services',
+ 'magicapi.Services.Doorman',
+ 'magicapi.Services.Email',
+ 'magicapi.Services.MagicLink',
+ 'magicapi.Services.Mailgun',
+ 'magicapi.Services.Segment',
+ 'magicapi.Services.Stripe',
+ 'magicapi.Services.Twilio',
+ 'magicapi.Utils']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['boto3>=1.14.23,<2.0.0',
+ 'fastapi[all]>=0.59.0,<0.60.0',
+ 'magicdb>=0.0.6,<0.0.7',
+ 'mangum>=0.9.2,<0.10.0',
+ 'pydantic[dotenv]>=1.6.1,<2.0.0',
+ 'requests>=2.24.0,<3.0.0']
+
+setup_kwargs = {
+    'name': 'magicapi',
+    'version': '0.1.20',
+    'description': '',
+    'long_description': '# Magic API\n\nThe Magic API!',
+    'author': 'Jeremy Berman',
+    'author_email': 'jerber@sas.upenn.edu',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/jerber/MagicAPI',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
