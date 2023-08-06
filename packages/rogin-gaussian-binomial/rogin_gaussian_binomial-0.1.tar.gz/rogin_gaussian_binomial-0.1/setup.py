dd6fa512b91c05f3c694b5f43a273e50ea3096e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:51 +0000

    Add package rogin-gaussian-binomial/0.1/rogin_gaussian_binomial-0.1.tar.gz

---
+++ packages/rogin-gaussian-binomial/rogin_gaussian_binomial-0.1.tar.gz/rogin_gaussian_binomial-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='rogin_gaussian_binomial',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['rogin_gaussian_binomial'],
+      author='Rogin Koshy',
+      author_email='rgkss13@gmail.com',
+      zip_safe=False)
