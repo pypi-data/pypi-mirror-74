be1db2e79bddc3a5eed27cb68b0811c000c50b9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:34 +0000

    Add package robot-motor-fake/0.0.3/robot_motor_fake-0.0.3.tar.gz

---
+++ packages/robot-motor-fake/robot_motor_fake-0.0.3.tar.gz/robot_motor_fake-0.0.3/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='robot_motor_fake',
+    version='0.0.3',
+    packages=find_packages(),
+    install_requires=[
+        'imutils',
+        'numpy',
+        'opencv-python'
+    ]
+)
