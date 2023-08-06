ba4a485f9acbb1209c5c2b26bf98d6ac6b7b8e5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:03 +0000

    Add package gau-my-distributions/0.1/gau_my_distributions-0.1.tar.gz

---
+++ packages/gau-my-distributions/gau_my_distributions-0.1.tar.gz/gau_my_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='gau_my_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['gau_my_distributions'],
+      author='Ananth Mamidipally',
+      author_email='test@testemail.com',
+      zip_safe=False)
