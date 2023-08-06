5c4d91c074a57080bc01e56958ab13213e3e7cf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:38 +0000

    Add package udacity-distributions-samuel-k/0.1/udacity_distributions_samuel_k-0.1.tar.gz

---
+++ packages/udacity-distributions-samuel-k/udacity_distributions_samuel_k-0.1.tar.gz/udacity_distributions_samuel_k-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='udacity_distributions_samuel_k',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['udacity_distributions_samuel_k'],
+      author = 'samuel.k',
+      author_email = 'samkura47@gmail.com',
+      zip_safe=False)
