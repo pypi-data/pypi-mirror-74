c9794e0d25ad5e8428c961d7311fc8cc7938ef69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:36 +0000

    Add package python-nbaclient/0.0.2/python_nbaclient-0.0.2-py3-none-any.whl

---
+++ packages/python-nbaclient/python_nbaclient-0.0.2-py3-none-any.whl/python_nbaclient/constants.py
@@ -0,0 +1,3 @@
+TEAM_DETAILS_URI = "https://stats.nba.com/stats/teamdetails?"
+COMMON_TEAM_ROSTER_URI = "https://stats.nba.com/stats/commonteamroster?"
+FRANCHISE_LEADER_URI = "https://stats.nba.com/stats/franchiseleaders?"
