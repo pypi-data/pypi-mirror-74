f6689d94873de3b1bd2431f9cd3ff6caa8151089
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:44 +0000

    Add package dsbn-distributions/0.1/dsbn_distributions-0.1.tar.gz

---
+++ packages/dsbn-distributions/dsbn_distributions-0.1.tar.gz/dsbn_distributions-0.1/README.md
@@ -0,0 +1,14 @@
+#dsbn-distribution package
+
+
+
+summary of the package
+
+
+#files
+
+
+Explanation of files in the package
+
+
+#Installatiom
---
+++ packages/dsbn-distributions/dsbn_distributions-0.1.tar.gz/dsbn_distributions-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='dsbn_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['dsbn_distributions'],
+      author='P. Dinagaran',
+      zip_safe=False)
