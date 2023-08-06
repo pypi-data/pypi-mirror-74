302427b4a5780d3af7ffb127d17f7689ca2fed9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:11 +0000

    Add package nobi-distributions/0.1/nobi_distributions-0.1.tar.gz

---
+++ packages/nobi-distributions/nobi_distributions-0.1.tar.gz/nobi_distributions-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='nobi_distributions',
+      version='0.1',
+      license='MIT',
+      description='Gaussian and Binomial distributions',
+      author='Emanuel Dickson Wanyonyi',
+      author_email='wanyonyi.d.emanuel@gmail.com',
+      packages=['distributions'],
+      zip_safe=False)
