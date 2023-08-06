0cc8ac8e3f84ae2afa280db97c8bf52b95be90ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:59 +0000

    Add package distributions-calc-ml-test/0.1/distributions_calc_ml_test-0.1.tar.gz

---
+++ packages/distributions-calc-ml-test/distributions_calc_ml_test-0.1.tar.gz/distributions_calc_ml_test-0.1/README.md
@@ -0,0 +1,3 @@
+# distributions_calc_ml_test package 
+
+Gaussian and Binomial distribution counting package.
---
+++ packages/distributions-calc-ml-test/distributions_calc_ml_test-0.1.tar.gz/distributions_calc_ml_test-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_calc_ml_test',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions_calc_ml_test'],
+      zip_safe=False)
