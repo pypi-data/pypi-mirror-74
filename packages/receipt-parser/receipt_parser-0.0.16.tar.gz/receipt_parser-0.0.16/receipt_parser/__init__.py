6ef613a446293eb091f35a5cc808b917641d2da1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:01 +0000

    Add package receipt-parser/0.0.16/receipt_parser-0.0.16.tar.gz

---
+++ packages/receipt-parser/receipt_parser-0.0.16.tar.gz/receipt_parser-0.0.16/receipt_parser/__init__.py
@@ -0,0 +1,17 @@
+__version__ = '0.0.16'
+__license__ = 'MIT'
+
+
+from .receipt_parser import RuleBased
+from .finder import Finder
+from .normalizer import Normalizer
+from . import data, parsers, benchmarks
+# try:
+#     from .receipt_parser import RuleBased
+#     from .finder import Finder
+#     from .normalizer import Normalizer
+# except ModuleNotFoundError:
+#     from receipt_parser import RuleBased
+#     from finder import Finder
+#     from normalizer import Normalizer
+
