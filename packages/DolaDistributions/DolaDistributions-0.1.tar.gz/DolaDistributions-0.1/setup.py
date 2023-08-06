85c38d10ff480353b1b7d7ca4131a615a9f08072
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:37 +0000

    Add package DolaDistributions/0.1/DolaDistributions-0.1.tar.gz

---
+++ packages/DolaDistributions/DolaDistributions-0.1.tar.gz/DolaDistributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='DolaDistributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['DolaDistributions'],
+      zip_safe=False)
