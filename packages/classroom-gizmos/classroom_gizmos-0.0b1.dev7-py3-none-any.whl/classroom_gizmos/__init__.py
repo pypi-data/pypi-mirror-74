d8ec608302357733f5e0512805ede0d9496eefc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:51 +0000

    Add package classroom-gizmos/0.0b1.dev7/classroom_gizmos-0.0b1.dev7-py3-none-any.whl

---
+++ packages/classroom-gizmos/classroom_gizmos-0.0b1.dev7-py3-none-any.whl/classroom_gizmos/__init__.py
@@ -0,0 +1,4 @@
+## See https://packaging.python.org/guides/single-sourcing-package-version/
+## ----- Time-stamp: <2020-07-14 15:44:38 cws2> -----
+
+__version__ = "0.0b1.dev7", # X.YaN.devM format for now
