7ff229509d52ced9cf27e9168d2d2379aa013d50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:01 +0000

    Add package pst-distributions/1.0/pst_distributions-1.0.tar.gz

---
+++ packages/pst-distributions/pst_distributions-1.0.tar.gz/pst_distributions-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='pst_distributions',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['pst_distributions'],
+      author = 'Olaoluwayemi Rasheed',
+      author_email = 'rasholayemi@gmail.com',
+      zip_safe=False)
