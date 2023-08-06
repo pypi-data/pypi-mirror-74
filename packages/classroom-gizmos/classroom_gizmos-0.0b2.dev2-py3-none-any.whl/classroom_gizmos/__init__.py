c8442e7c36b1f4ae9369dbb0d54c41962ca10d91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:53 +0000

    Add package classroom-gizmos/0.0b2.dev2/classroom_gizmos-0.0b2.dev2-py3-none-any.whl

---
+++ packages/classroom-gizmos/classroom_gizmos-0.0b2.dev2-py3-none-any.whl/classroom_gizmos/__init__.py
@@ -0,0 +1,7 @@
+## See https://packaging.python.org/guides/single-sourcing-package-version/
+## ----- Time-stamp: <2020-07-21 17:51:23 hedfp> ------
+
+__version__ = "0.0b2.dev2", # X.YaN.devM format for now
+
+import astropy
+import func_timeout
