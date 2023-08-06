cf30021103f4160c2980bc0296ba40cce234db17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:49 +0000

    Add package pyQiwiP2P/1.0a1/pyQiwiP2P-1.0a1.tar.gz

---
+++ packages/pyQiwiP2P/pyQiwiP2P-1.0a1.tar.gz/pyQiwiP2P-1.0a1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+	name='pyQiwiP2P',
+	version='1.0a1',
+	packages=['pyqiwip2p', 'pyqiwip2p.types', 'pyqiwip2p.notify'],
+	url='https://github.com/WhiteApfel/pyQiwiP2P',
+	license='Mozilla Public License 2.0',
+	author='WhiteApfel pfel.cc',
+	author_email='white@pfel.ru',
+	description='pyQiwiP2P'
+)
