a163a83730f76ad6497d6f6bfeb74630b5a24b0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:14:01 +0000

    Add package distribution-sp-01/11.0/distribution_sp_01-11.0.tar.gz

---
+++ packages/distribution-sp-01/distribution_sp_01-11.0.tar.gz/distribution_sp_01-11.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='distribution_sp_01',
+      version='11.0',
+      description='Gaussian & Binomial distributions',
+      packages=['distribution_sp_01'],
+      author='Sandeep',
+      zip_safe=False)
