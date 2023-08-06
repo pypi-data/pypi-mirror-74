2f9fc4d16256c8613eddef611df501bfd3cbb830
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:01 +0000

    Add package jania-probability/0.1/jania_probability-0.1.tar.gz

---
+++ packages/jania-probability/jania_probability-0.1.tar.gz/jania_probability-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='jania_probability',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['jania_probability'],
+      author = 'Jania Mulanovich', 
+      zip_safe=False)
