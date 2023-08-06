8db12b9bb620b752a836f10ba512a56b4ba82204
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:44 +0000

    Add package distributionsfunc/0.1/distributionsfunc-0.1.tar.gz

---
+++ packages/distributionsfunc/distributionsfunc-0.1.tar.gz/distributionsfunc-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributionsfunc',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributionsfunc'],
+      zip_safe=False)
