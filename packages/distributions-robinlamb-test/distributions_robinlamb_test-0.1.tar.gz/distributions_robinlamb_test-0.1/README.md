f214fbe8ec61c75fd6e3d097ba3eebbd415258c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:18 +0000

    Add package distributions-robinlamb-test/0.1/distributions_robinlamb_test-0.1.tar.gz

---
+++ packages/distributions-robinlamb-test/distributions_robinlamb_test-0.1.tar.gz/distributions_robinlamb_test-0.1/README.md
@@ -0,0 +1,5 @@
+This package is for:
+
+Blah blah blah
+
+(test)
---
+++ packages/distributions-robinlamb-test/distributions_robinlamb_test-0.1.tar.gz/distributions_robinlamb_test-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_robinlamb_test',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_robinlamb_test'],
+      zip_safe=False)
