6c820a0da0483f268e578f027fdede7c1e657091
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:43 +0000

    Add package distributions-HBK/0.1/distributions_HBK-0.1.tar.gz

---
+++ packages/distributions-HBK/distributions_HBK-0.1.tar.gz/distributions_HBK-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_HBK',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_HBK'],
+      zip_safe=False)
