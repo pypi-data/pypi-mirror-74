2696bbf3f727689062ce4210b7104bc0102fcff9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:28 +0000

    Add package eddy-dsnd-probability/0.1/eddy_dsnd_probability-0.1.tar.gz

---
+++ packages/eddy-dsnd-probability/eddy_dsnd_probability-0.1.tar.gz/eddy_dsnd_probability-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='eddy_dsnd_probability',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['eddy_dsnd_probability'],
+      author='Edward Amankwah',
+      author_email='eaakwasi@yahoo.ca',
+      zip_safe=False)
