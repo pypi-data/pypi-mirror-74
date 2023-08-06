1b2374fe5d554532d34d32e6da291bb8b7b5d33f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:33 +0000

    Add package napari/0.3.6/napari-0.3.6-py3-none-any.whl

---
+++ packages/napari/napari-0.3.6-py3-none-any.whl/napari/utils/colormaps/__init__.py
@@ -0,0 +1,11 @@
+from .colormaps import (
+    matplotlib_colormaps,
+    simple_colormaps,
+    ensure_colormap_tuple,
+    AVAILABLE_COLORMAPS,
+    ALL_COLORMAPS,
+    MAGENTA_GREEN,
+    RGB,
+    CYMRGB,
+)
+from .colorbars import make_colorbar
