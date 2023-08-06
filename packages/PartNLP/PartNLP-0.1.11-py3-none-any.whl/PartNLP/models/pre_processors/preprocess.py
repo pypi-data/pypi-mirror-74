e7707a2be115f018320a19580d64dc92583e2d6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:13 +0000

    Add package PartNLP/0.1.11/PartNLP-0.1.11-py3-none-any.whl

---
+++ packages/PartNLP/PartNLP-0.1.11-py3-none-any.whl/PartNLP/models/pre_processors/preprocess.py
@@ -0,0 +1,64 @@
+"""
+This model designed to pre-process data and
+it has many features to help users to minimize their codes for
+pre-processing purposes!
+"""
+import os
+import json
+import logging
+from PartNLP.models.helper.color import Color
+
+
+class PreProcess:
+    def __init__(self, config):
+        self.normalize_text = []
+        self.sentences = []
+        self.words = []
+        self.result = []
+        self.stem_words = []
+        self.lemmatized_words = []
+        self.non_stopwords = []
+        if config['text'] != '':
+            self.data = config['text']
+        else:
+            self.file_path = config['InputFilePath']
+            self.read_from_file(self.file_path)
+        self.language = config['Language']
+        self.dataset_type = config['DatasetType']
+        self.input_path = config['InputFilePath']
+        self.output_path = config['OutputFilePath']
+
+    def sent_tokenize(self):
+        """
+        :return:
+        """
+        self.sentences = self.model.sent_tokenize(self.data)
+        return self.sentences
+
+    def word_tokenize(self):
+        """
+        :return:
+        """
+        self.words = [self.model.word_tokenize(sent) for sent in self.sentences]
+        return self.words
+
+    def lemmatize(self):
+        pass
+
+    def normalize(self):
+        pass
+        
+    def stem(self):
+        pass
+
+    def pos(self):
+        pass
+
+    def read_from_file(self, path=''):
+        """
+        :param path:
+        :return:
+        """
+        if path:
+            self.data = open(path, encoding='utf-8', errors='ignore').read()
+            print()
