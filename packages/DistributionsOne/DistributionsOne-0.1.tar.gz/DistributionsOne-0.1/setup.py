9357de433c9718be8047688636f1744a3937b1c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:46 +0000

    Add package DistributionsOne/0.1/DistributionsOne-0.1.tar.gz

---
+++ packages/DistributionsOne/DistributionsOne-0.1.tar.gz/DistributionsOne-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='DistributionsOne',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['DistributionsOne'],
+      zip_safe=False)
