0ee2899eb2381e5e3c7b7a9e9c7912b7ef7f59e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:27 +0000

    Add package pyhafas/0.1.0/pyhafas-0.1.0.tar.gz

---
+++ packages/pyhafas/pyhafas-0.1.0.tar.gz/pyhafas-0.1.0/pyhafas/profile/vsn/__init__.py
@@ -0,0 +1,54 @@
+from pyhafas.profile import BaseProfile
+
+
+class VSNProfile(BaseProfile):
+    baseUrl = "https://fahrplaner.vsninfo.de/hafas/mgate.exe"
+    defaultUserAgent = "vsn/5.3.1 (iPad; iOS 13.3; Scale/2.00)"
+
+    salt = 'SP31mBufSyCLmNxp'
+    addMicMac = True
+
+    locale = 'de-DE'
+    timezone = 'Europe/Berlin'
+
+    requestBody = {
+        'client': {
+            'id': 'VSN',
+            'v': '5030100',
+            'type': 'IPA',
+            'name': 'vsn',
+            'os': 'iOS 13.3'
+        },
+        'ver': '1.24',
+        'lang': 'de',
+        'auth': {
+            'type': 'AID',
+            'aid': 'Mpf5UPC0DmzV8jkg'
+        }
+    }
+
+    available_products = {
+        'long_distance_express': [1],  # ICE
+        'long_distance': [2],  # IC/EC/CNL
+        'regional_express': [4],  # RE/IR
+        'regional': [8],  # NV
+        'suburban': [16],  # S
+        'bus': [32],  # BUS
+        'ferry': [64],  # F
+        'subway': [128],  # U
+        'tram': [256],  # T
+        'anruf_sammel_taxi': [512]  # Group Taxi
+    }
+
+    default_products = [
+        'long_distance_express',
+        'long_distance',
+        'regional_express',
+        'regional',
+        'suburban',
+        'bus',
+        'ferry',
+        'subway',
+        'tram',
+        'anruf_sammel_taxi'
+    ]
