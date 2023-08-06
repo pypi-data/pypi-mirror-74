1441f9a596c8695f4cd385f0e2d53e2b11da31c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:03 +0000

    Add package demisto-sdk/1.1.7/demisto-sdk-1.1.7.tar.gz

---
+++ packages/demisto-sdk/demisto-sdk-1.1.7.tar.gz/demisto-sdk-1.1.7/demisto_sdk/commands/common/tests/pack_metadata_validator_test.py
@@ -0,0 +1,47 @@
+import io
+import os
+
+import pytest
+from demisto_sdk.commands.common.git_tools import git_path
+from demisto_sdk.commands.common.hook_validations.base_validator import \
+    BaseValidator
+from demisto_sdk.commands.common.hook_validations.pack_unique_files import \
+    PackUniqueFilesValidator
+
+
+class TestPackMetadataValidator:
+    FILES_PATH = os.path.normpath(os.path.join(__file__, f'{git_path()}/demisto_sdk/tests', 'test_files'))
+
+    def test_metadata_validator_valid(self, mocker):
+        mocker.patch.object(PackUniqueFilesValidator, '_read_file_content',
+                            return_value=TestPackMetadataValidator.
+                            read_file(os.path.join(TestPackMetadataValidator.FILES_PATH,
+                                                   'valid_pack_metadata.json')))
+        mocker.patch.object(PackUniqueFilesValidator, '_is_pack_file_exists', return_value=True)
+
+        validator = PackUniqueFilesValidator('fake')
+        assert validator.validate_pack_meta_file()
+
+    # TODO: add the validation for price after #23546 is ready.
+    @pytest.mark.parametrize('metadata', [os.path.join(FILES_PATH, 'pack_metadata_missing_fields.json'),
+                                          # os.path.join(FILES_PATH, 'pack_metadata_invalid_price.json'),
+                                          os.path.join(FILES_PATH, 'pack_metadata_invalid_dependencies.json'),
+                                          os.path.join(FILES_PATH, 'pack_metadata_list_dependencies.json'),
+                                          os.path.join(FILES_PATH, 'pack_metadata_empty_category.json'),
+                                          os.path.join(FILES_PATH, 'pack_metadata_invalid_keywords.json'),
+                                          os.path.join(FILES_PATH, 'pack_metadata_invalid_tags.json'),
+                                          os.path.join(FILES_PATH, 'pack_metadata_list.json')
+                                          ])
+    def test_metadata_validator_invalid(self, mocker, metadata):
+        mocker.patch.object(PackUniqueFilesValidator, '_read_file_content',
+                            return_value=TestPackMetadataValidator.read_file(metadata))
+        mocker.patch.object(PackUniqueFilesValidator, '_is_pack_file_exists', return_value=True)
+        mocker.patch.object(BaseValidator, 'check_file_flags', return_value='')
+
+        validator = PackUniqueFilesValidator('fake')
+        assert not validator.validate_pack_meta_file()
+
+    @staticmethod
+    def read_file(file_):
+        with io.open(file_, mode="r", encoding="utf-8") as data:
+            return data.read()
---
+++ packages/demisto-sdk/demisto-sdk-1.1.7.tar.gz/demisto-sdk-1.1.7/demisto_sdk/commands/format/tests/test_formatting_json_test.py
@@ -0,0 +1,158 @@
+import os
+import shutil
+
+import pytest
+from demisto_sdk.commands.format import (update_dashboard, update_incidenttype,
+                                         update_indicatortype)
+from demisto_sdk.commands.format.format_module import format_manager
+from demisto_sdk.commands.format.update_dashboard import DashboardJSONFormat
+from demisto_sdk.commands.format.update_incidenttype import \
+    IncidentTypesJSONFormat
+from demisto_sdk.commands.format.update_indicatortype import \
+    IndicatorTypeJSONFormat
+from demisto_sdk.tests.constants_test import (
+    DASHBOARD_PATH, DESTINATION_FORMAT_DASHBOARD_COPY,
+    DESTINATION_FORMAT_INCIDENTFIELD_COPY,
+    DESTINATION_FORMAT_INCIDENTTYPE_COPY,
+    DESTINATION_FORMAT_INDICATORFIELD_COPY,
+    DESTINATION_FORMAT_INDICATORTYPE_COPY, DESTINATION_FORMAT_LAYOUT_COPY,
+    INCIDENTFIELD_PATH, INCIDENTTYPE_PATH, INDICATORFIELD_PATH,
+    INDICATORTYPE_PATH, INVALID_OUTPUT_PATH, LAYOUT_PATH,
+    SOURCE_FORMAT_DASHBOARD_COPY, SOURCE_FORMAT_INCIDENTFIELD_COPY,
+    SOURCE_FORMAT_INCIDENTTYPE_COPY, SOURCE_FORMAT_INDICATORFIELD_COPY,
+    SOURCE_FORMAT_INDICATORTYPE_COPY, SOURCE_FORMAT_LAYOUT_COPY)
+
+
+class TestFormattingJson:
+    FORMAT_FILES = [
+        (SOURCE_FORMAT_INCIDENTFIELD_COPY, DESTINATION_FORMAT_INCIDENTFIELD_COPY, INCIDENTFIELD_PATH, 0),
+        (SOURCE_FORMAT_INCIDENTTYPE_COPY, DESTINATION_FORMAT_INCIDENTTYPE_COPY, INCIDENTTYPE_PATH, 0),
+        (SOURCE_FORMAT_INDICATORFIELD_COPY, DESTINATION_FORMAT_INDICATORFIELD_COPY, INDICATORFIELD_PATH, 0),
+        (SOURCE_FORMAT_INDICATORTYPE_COPY, DESTINATION_FORMAT_INDICATORTYPE_COPY, INDICATORTYPE_PATH, 0),
+        (SOURCE_FORMAT_LAYOUT_COPY, DESTINATION_FORMAT_LAYOUT_COPY, LAYOUT_PATH, 0),
+        (SOURCE_FORMAT_DASHBOARD_COPY, DESTINATION_FORMAT_DASHBOARD_COPY, DASHBOARD_PATH, 0),
+    ]
+
+    @pytest.mark.parametrize('source, target, path, answer', FORMAT_FILES)
+    def test_format_file(self, source, target, path, answer):
+        os.makedirs(path, exist_ok=True)
+        shutil.copyfile(source, target)
+        res = format_manager(input=target, output=target)
+        os.remove(target)
+        os.rmdir(path)
+
+        assert res is answer
+
+    @pytest.mark.parametrize('invalid_output', [INVALID_OUTPUT_PATH])
+    def test_output_file(self, invalid_output):
+        try:
+            res_invalid = format_manager(input=invalid_output, output=invalid_output)
+            assert res_invalid
+        except Exception as e:
+            assert str(e) == "The given output path is not a specific file path.\nOnly file path can be a output path." \
+                             "  Please specify a correct output."
+
+
+def test_update_id_indicatortype_positive(mocker, tmpdir):
+    """
+    Given
+        - A dictionary of indicatortype file that the id is not equal to the details
+    When
+        - Run format on indicatortype file
+    Then
+        - Ensure id updated successfully
+    """
+    mocker.patch.object(update_indicatortype, 'IndicatorTypeJSONFormat')
+
+    indicator_formater = IndicatorTypeJSONFormat(input='test', output=tmpdir)
+    indicator_formater.data = {'id': '1234', 'details': '12345'}
+    indicator_formater.update_id()
+    assert indicator_formater.data['id'] == indicator_formater.data['details']
+
+
+def test_update_id_indicatortype_negative(mocker, tmpdir):
+    """
+    Given
+        - A dictionary of indicator-type file that the details field is missing
+    When
+        - Run format on indicator-type file
+    Then
+        - Ensure the return Exception is 'Missing "details" field in file test - add this field manually'
+    """
+    mocker.patch.object(update_indicatortype, 'IndicatorTypeJSONFormat')
+    indicator_formater = IndicatorTypeJSONFormat(input='test', output=tmpdir)
+    indicator_formater.data = {'id': '1234'}
+    try:
+        indicator_formater.update_id()
+    except Exception as error:
+        assert error.args[0] == 'Missing "details" field in file test - add this field manually'
+
+
+def test_update_id_incidenttype_positive(mocker, tmpdir):
+    """
+    Given
+        - A dictionary of incident-type file that the id is not equal to the name
+    When
+        - Run format on incident-type file
+    Then
+        - Ensure id updated successfully
+    """
+    mocker.patch.object(update_incidenttype, 'IncidentTypesJSONFormat')
+
+    incident_formater = IncidentTypesJSONFormat(input='test', output=tmpdir)
+    incident_formater.data = {'id': '1234', 'name': '12345'}
+    incident_formater.update_id()
+    assert incident_formater.data['id'] == incident_formater.data['name']
+
+
+def test_update_id_incidenttype_negative(mocker, tmpdir):
+    """
+    Given
+        - A dictionary of incident-type file that the name field is missing
+    When
+        - Run format on incident-type file
+    Then
+        - Ensure the return Exception is 'Missing "name" field in file test - add this field manually'
+    """
+    mocker.patch.object(update_incidenttype, 'IncidentTypesJSONFormat')
+    incident_formater = IncidentTypesJSONFormat(input='test', output=tmpdir)
+    incident_formater.data = {'id': '1234'}
+    try:
+        incident_formater.update_id()
+    except Exception as error:
+        assert error.args[0] == 'Missing "name" field in file test - add this field manually'
+
+
+def test_update_id_dashboard_positive(mocker, tmpdir):
+    """
+    Given
+        - A dictionary of dashboard file that the id is not equal to the name
+    When
+        - Run format on dashboard file
+    Then
+        - Ensure id updated successfully
+    """
+    mocker.patch.object(update_dashboard, 'DashboardJSONFormat')
+
+    dashboard_formater = DashboardJSONFormat(input='test', output=tmpdir)
+    dashboard_formater.data = {'id': '1234', 'name': '12345'}
+    dashboard_formater.update_id()
+    assert dashboard_formater.data['id'] == dashboard_formater.data['name']
+
+
+def test_update_id_dashboard_negative(mocker, tmpdir):
+    """
+    Given
+        - A dictionary of dashboard file that the name field is missing
+    When
+        - Run format on dashboard file
+    Then
+        - Ensure the return Exception is 'Missing "name" field in file test - add this field manually'
+    """
+    mocker.patch.object(update_dashboard, 'DashboardJSONFormat')
+    dashboard_formater = DashboardJSONFormat(input='test', output=tmpdir)
+    dashboard_formater.data = {'id': '1234'}
+    try:
+        dashboard_formater.update_id()
+    except Exception as error:
+        assert error.args[0] == 'Missing "name" field in file test - add this field manually'
