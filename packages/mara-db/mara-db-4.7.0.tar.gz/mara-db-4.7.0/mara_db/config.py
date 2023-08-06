c773f32b4728caa61f1dc0c67d3c34c5e5a7aa76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:45 +0000

    Add package mara-db/4.7.0/mara-db-4.7.0.tar.gz

---
+++ packages/mara-db/mara-db-4.7.0.tar.gz/mara-db-4.7.0/mara_db/config.py
@@ -0,0 +1,22 @@
+"""Configuration of database connections"""
+import typing
+
+from mara_db import dbs
+
+
+def databases() -> {str: dbs.DB}:
+    """The list of database connections to use, by alias"""
+    return {'mara': dbs.PostgreSQLDB(host='localhost', database='mara', user='root')}
+
+
+def default_timezone() -> str:
+    """
+    The default timezone to be used for database connections
+    See: https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+    """
+    return 'Europe/Berlin'
+
+
+def schema_ui_foreign_key_column_regex() -> typing.Pattern:
+    """A regex that classifies a table column as being used in a foreign constraint (for coloring missing constraints)"""
+    return r'.*_fk$'
