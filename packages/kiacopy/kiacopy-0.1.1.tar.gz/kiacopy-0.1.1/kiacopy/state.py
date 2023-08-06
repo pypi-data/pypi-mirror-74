06a5a35a57b7cf4a68bfe76960f31112f84c0ddd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:48 +0000

    Add package kiacopy/0.1.1/kiacopy-0.1.1.tar.gz

---
+++ packages/kiacopy/kiacopy-0.1.1.tar.gz/kiacopy-0.1.1/kiacopy/state.py
@@ -0,0 +1,33 @@
+class State:
+
+    def __init__(self, graph, ants, limit, gen_size, colony, rho, q, top, problem, gamma, theta, inf):
+        self.graph = graph
+        self.ants = ants
+        self.limit = limit
+        self.gen_size = gen_size
+        self.colony = colony
+        self.rho = rho
+        self.q = q
+        self.top = top
+        self.problem = problem
+        self.gamma = gamma
+        self.theta = theta
+        self.inf = inf
+        self.gamma = gamma
+        self.solutions = None
+        self.record = None
+        self.previous_record = None
+        self.is_new_record = False
+        self._best = None
+
+    @property
+    def best(self):
+        return self._best
+
+    @best.setter
+    def best(self, best):
+        self.is_new_record = self.record is None or best < self.record
+        if self.is_new_record:
+            self.previous_record = self.record
+            self.record = best
+        self._best = best
