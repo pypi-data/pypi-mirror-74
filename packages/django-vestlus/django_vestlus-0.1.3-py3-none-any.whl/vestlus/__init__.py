cf8b1a5d65a0ad5c5b0366ef597fb53c47afcce4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:20 +0000

    Add package django-vestlus/0.1.3/django_vestlus-0.1.3-py3-none-any.whl

---
+++ packages/django-vestlus/django_vestlus-0.1.3-py3-none-any.whl/vestlus/__init__.py
@@ -0,0 +1,14 @@
+# coding: utf-8
+"""
+    vestlus is a django app with support for private and public channels.
+"""
+from __future__ import unicode_literals
+from .requires import REQUIRED_APPS
+
+__version__ = "0.1.3"
+__license__ = 'BSD 3-Clause'
+__copyright__ = 'Copyright 2020 Lehvitus ÖU'
+
+VERSION = __version__
+
+default_app_config = 'vestlus.apps.VestlusConfig'
