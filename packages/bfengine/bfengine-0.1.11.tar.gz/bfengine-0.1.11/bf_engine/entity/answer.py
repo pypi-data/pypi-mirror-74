eeffa750d6585ea025367804623c985647791cee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:43 +0000

    Add package bfengine/0.1.11/bfengine-0.1.11.tar.gz

---
+++ packages/bfengine/bfengine-0.1.11.tar.gz/bfengine-0.1.11/bf_engine/entity/answer.py
@@ -0,0 +1,10 @@
+class Answer:
+    """
+    机器人回答
+    """
+    def __init__(self, text: str, score: float):
+        self.text = text
+        self.score = score
+
+    def __str__(self):
+        return 'text: {}, score: {}'.format(self.text, self.score)
