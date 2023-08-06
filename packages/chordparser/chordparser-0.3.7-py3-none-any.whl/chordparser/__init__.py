979f49d782b3b60ff4bf4b3d2a7e4edcbeceaefe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:03 +0000

    Add package chordparser/0.3.7/chordparser-0.3.7-py3-none-any.whl

---
+++ packages/chordparser/chordparser-0.3.7-py3-none-any.whl/chordparser/__init__.py
@@ -0,0 +1,15 @@
+"""
+A package to parse ChordPro files.
+"""
+from chordparser.parser import Parser
+from chordparser.notes import Note
+from chordparser.keys import Key
+from chordparser.scales import Scale
+from chordparser.chords import Chord
+from chordparser.quality import Quality
+from chordparser.roman import Roman
+
+
+__author__ = 'Titus Ong'
+__email__ = 'titusongyl@gmail.com'
+__version__ = '0.3.7'
