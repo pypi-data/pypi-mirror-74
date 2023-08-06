33587802079eb95b8e7560db463583a614696be3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:51 +0000

    Add package pybrary/0.1.0/pybrary-0.1.0.tar.gz

---
+++ packages/pybrary/pybrary-0.1.0.tar.gz/pybrary-0.1.0/setup.py
@@ -0,0 +1,33 @@
+from os.path import join, dirname, abspath
+
+from setuptools import setup, find_packages
+
+from pybrary import __version__
+
+curdir = abspath(dirname(__file__))
+readme = open(join(curdir, 'README.rst')).read()
+
+setup(
+    name             = 'pybrary',
+    version          = __version__,
+    description      = 'System deployment',
+    long_description = readme,
+    keywords         = ['utility', ],
+    url              = 'https://framagit.org/louis-riviere-xyz/pybrary/tree/stable',
+    author           = 'Louis RIVIERE',
+    author_email     = 'louis@riviere.xyz',
+    license          = 'MIT',
+    classifiers      = [
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        "Operating System :: POSIX :: Linux",
+        "Environment :: Console",
+        'Intended Audience :: Developers',
+    ],
+    install_requires = [
+    ],
+    package_dir = {
+    },
+    packages = find_packages(),
+)
