0e1e8d7dd239e39b131107389bf658f17923d92d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:48 +0000

    Add package galau/0.1.3/galau-0.1.3.tar.gz

---
+++ packages/galau/galau-0.1.3.tar.gz/galau-0.1.3/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['galau']
+
+package_data = \
+{'': ['*']}
+
+entry_points = \
+{'console_scripts': ['galau = galau:main']}
+
+setup_kwargs = {
+    'name': 'galau',
+    'version': '0.1.3',
+    'description': '',
+    'long_description': None,
+    'author': 'Deniansyah',
+    'author_email': '5lineofcode@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'entry_points': entry_points,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
