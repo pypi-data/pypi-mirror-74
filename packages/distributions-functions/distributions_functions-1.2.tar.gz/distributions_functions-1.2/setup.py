b20640d54644f258715190e4d0105f11018479fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:07 +0000

    Add package distributions-functions/1.2/distributions_functions-1.2.tar.gz

---
+++ packages/distributions-functions/distributions_functions-1.2.tar.gz/distributions_functions-1.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_functions',
+      version='1.2',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions_functions'],
+      author = "Sombit Bose",
+      author_email = "sombit.bose15@gmail.com",
+      zip_safe=False)
