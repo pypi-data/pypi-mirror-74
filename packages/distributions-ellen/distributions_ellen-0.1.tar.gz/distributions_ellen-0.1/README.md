145ad977bb5f0f8e0fb0e311f5ad0ef122555f16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:42 +0000

    Add package distributions-ellen/0.1/distributions_ellen-0.1.tar.gz

---
+++ packages/distributions-ellen/distributions_ellen-0.1.tar.gz/distributions_ellen-0.1/README.md
@@ -0,0 +1,3 @@
+# distribution_ellen package
+
+including both Gaussian and Binomial distribution operations for mean and stdev.
---
+++ packages/distributions-ellen/distributions_ellen-0.1.tar.gz/distributions_ellen-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_ellen',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions_ellen'],
+      zip_safe=False)
