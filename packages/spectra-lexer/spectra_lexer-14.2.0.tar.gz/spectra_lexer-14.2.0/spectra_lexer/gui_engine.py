e39ef7a216c2847758553ea62d0cefb368543367
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:22 +0000

    Add package spectra-lexer/14.2.0/spectra_lexer-14.2.0.tar.gz

---
+++ packages/spectra-lexer/spectra_lexer-14.2.0.tar.gz/spectra_lexer-14.2.0/spectra_lexer/gui_engine.py
@@ -0,0 +1,154 @@
+import random
+from typing import List, Sequence
+
+from spectra_lexer.resource import FrozenStruct
+from spectra_lexer.resource.rules import StenoRule
+from spectra_lexer.spc_board import BoardDiagram, BoardEngine
+from spectra_lexer.spc_graph import GraphEngine, HTMLGraph
+from spectra_lexer.spc_lexer import StenoAnalyzer, Translation
+from spectra_lexer.spc_resource import StenoResourceIO
+from spectra_lexer.spc_search import ExamplesDict, MatchDict, SearchEngine, TranslationsDict
+
+
+class GUIOptions(FrozenStruct):
+    """ Namespace for all GUI-related steno engine options. """
+
+    search_mode_strokes: bool = False       # If True, search for strokes instead of translations.
+    search_mode_regex: bool = False         # If True, perform search using regex characters.
+    search_match_limit: int = 100           # Maximum number of matches returned on one page of a search.
+    lexer_strict_mode: bool = False         # Only return lexer results that match every key in a translation.
+    board_aspect_ratio: float = None        # Aspect ratio for board viewing area (None means pure horizontal layout).
+    board_show_compound: bool = True        # Show compound keys on board with alt labels (i.e. F instead of TP).
+    board_show_letters: bool = True         # Show letters on board when possible. Letters override alt labels.
+    graph_compressed_layout: bool = True    # Compress the graph layout vertically to save space.
+    graph_compatibility_mode: bool = False  # Force correct spacing in the graph using HTML tables.
+
+
+class GUIEngine:
+    """ Layer for executing common user GUI actions. """
+
+    _opts = GUIOptions()  # Current (frozen) user options.
+
+    def __init__(self, io:StenoResourceIO, search_engine:SearchEngine, analyzer:StenoAnalyzer,
+                 graph_engine:GraphEngine, board_engine:BoardEngine, translations_paths=(), examples_path="") -> None:
+        self._io = io
+        self._search_engine = search_engine
+        self._analyzer = analyzer
+        self._graph_engine = graph_engine
+        self._board_engine = board_engine
+        self._translations_paths = translations_paths  # Starting translation file paths.
+        self._examples_path = examples_path            # User examples index file path.
+        self._last_translations = {}                   # Most recently loaded translations (for indexing).
+        empty_graph = graph_engine.graph(analyzer.query("", ""))
+        self._graph = empty_graph                      # Graph of current analysis.
+
+    def set_options(self, options:dict) -> None:
+        """ Replace all <options> at once. """
+        self._opts = GUIOptions(**options)
+
+    def set_translations(self, translations:TranslationsDict) -> None:
+        """ Send a new translations dict to the search engine. Keep a copy in case we need to make an index. """
+        self._search_engine.set_translations(translations)
+        self._last_translations = translations
+
+    def load_translations(self, *filenames:str) -> None:
+        """ Load and merge RTFCRE steno translations from JSON files. """
+        translations = self._io.load_json_translations(*filenames)
+        self.set_translations(translations)
+
+    def set_examples(self, examples:ExamplesDict) -> None:
+        """ Send a new examples index dict to the search engine. """
+        self._search_engine.set_examples(examples)
+
+    def load_examples(self, filename:str) -> None:
+        """ Load an examples index from a JSON file. """
+        examples = self._io.load_json_examples(filename)
+        self.set_examples(examples)
+
+    def compile_examples(self, size:int) -> None:
+        """ Make an examples index, set it as active, and save it as JSON. """
+        examples = self._analyzer.compile_index(self._last_translations.items(), size=size)
+        self.set_examples(examples)
+        self._io.save_json_examples(self._examples_path, examples)
+
+    def load_initial(self) -> None:
+        """ Load optional startup resources. Ignore I/O errors since any of them may be missing. """
+        if self._translations_paths:
+            try:
+                self.load_translations(*self._translations_paths)
+            except OSError:
+                pass
+        if self._examples_path:
+            try:
+                self.load_examples(self._examples_path)
+            except OSError:
+                pass
+
+    def search(self, pattern:str, pages=1) -> MatchDict:
+        """ Perform a search based on the current options. """
+        count = pages * self._opts.search_match_limit
+        mode_strokes = self._opts.search_mode_strokes
+        mode_regex = self._opts.search_mode_regex
+        return self._search_engine.search(pattern, count, mode_strokes=mode_strokes, mode_regex=mode_regex)
+
+    def random_pattern(self, example_id:str) -> str:
+        """ Return a valid example search pattern for <example_id> centered on a random translation if one exists. """
+        mode_strokes = self._opts.search_mode_strokes
+        return self._search_engine.random_pattern(example_id, mode_strokes=mode_strokes)
+
+    def best_translation(self, match:str, mappings:Sequence[str]) -> Translation:
+        """ Return the best translation in a match-mappings pair from search. """
+        if self._opts.search_mode_strokes:
+            # There can only be one mapping in strokes mode.
+            keys = match
+            letters = mappings[0]
+        else:
+            keys = self._analyzer.best_translation(mappings, match)
+            letters = match
+        return keys, letters
+
+    def random_translation(self, matches:MatchDict) -> Translation:
+        """ Return a translation randomly chosen from <matches>. """
+        assert matches
+        match = random.choice(list(matches))
+        mappings = matches[match]
+        return self.best_translation(match, mappings)
+
+    def search_selection(self, keys:str, letters:str):
+        return [keys, letters] if self._opts.search_mode_strokes else [letters, keys]
+
+    def _analyze(self, keys:str, letters:str) -> StenoRule:
+        return self._analyzer.query(keys, letters, strict_mode=self._opts.lexer_strict_mode)
+
+    def _build_graph(self, analysis:StenoRule) -> None:
+        """ Build a node graph of every rule in <analysis> and store it. """
+        self._graph = self._graph_engine.graph(analysis, compressed=self._opts.graph_compressed_layout,
+                                               compat=self._opts.graph_compatibility_mode)
+
+    def query(self, keys:str, letters:str) -> List[str]:
+        analysis = self._analyze(keys, letters)
+        self._build_graph(analysis)
+        return list(self._graph)
+
+    def get_caption(self, ref="") -> str:
+        return self._graph.caption(ref)
+
+    def draw_graph(self, ref="", intense=False) -> HTMLGraph:
+        return self._graph.draw(ref, intense=intense)
+
+    def draw_board(self, ref="") -> BoardDiagram:
+        rule = self._graph[ref]
+        aspect_ratio = self._opts.board_aspect_ratio
+        if self._opts.board_show_compound:
+            board = self._board_engine.draw_rule(rule, aspect_ratio, show_letters=self._opts.board_show_letters)
+        else:
+            board = self._board_engine.draw_keys(rule.keys, aspect_ratio)
+        return board
+
+    def get_example_id(self, ref="") -> str:
+        """ Return a rule ID usable in an example link, but only for rules that actually have examples. """
+        rule = self._graph[ref]
+        r_id = rule.id
+        if not self._search_engine.has_examples(r_id):
+            r_id = ""
+        return r_id
---
+++ packages/spectra-lexer/spectra_lexer-14.2.0.tar.gz/spectra_lexer-14.2.0/spectra_lexer/gui_rest.py
@@ -0,0 +1,111 @@
+from threading import Lock
+from typing import Dict, Iterable, Sequence
+
+from spectra_lexer.gui_engine import GUIEngine
+from spectra_lexer.spc_board import BoardDiagram
+from spectra_lexer.spc_graph import HTMLGraph
+from spectra_lexer.spc_search import MatchDict
+
+
+class RESTSelections:
+    """ Data class that contains a specific selection in the search lists. """
+
+    def __init__(self, match:str, mapping:str) -> None:
+        self.match = match      # Top list selection.
+        self.mapping = mapping  # Bottom list selection.
+
+
+class RESTDisplayPage:
+    """ Data class that contains two HTML formatted graphs, a caption, an SVG board, and a rule ID reference. """
+
+    def __init__(self, graph:HTMLGraph, intense_graph:HTMLGraph, caption:str, board:BoardDiagram, rule_id="") -> None:
+        self.graph = graph                  # HTML graph text for this selection.
+        self.intense_graph = intense_graph  # Brighter HTML text graph for this selection.
+        self.caption = caption              # Text characters drawn as a caption (possibly on a tooltip).
+        self.board = board                  # XML string containing this rule's SVG board diagram.
+        self.rule_id = rule_id              # If the selection uses a valid rule, its rule ID, else an empty string.
+
+
+class RESTDisplay:
+    """ Data class that contains graphical data for an entire analysis. """
+
+    def __init__(self, keys:str, letters:str, pages:Dict[str, RESTDisplayPage], default_page:RESTDisplayPage) -> None:
+        self.keys = keys                  # Translation keys in RTFCRE.
+        self.letters = letters            # Translation letters.
+        self.pages_by_ref = pages         # Analysis pages keyed by HTML anchor reference.
+        self.default_page = default_page  # Default starting analysis page. May also be included in pages_by_ref.
+
+
+class RESTUpdates:
+    """ Data class for a set of REST GUI updates. All fields are optional. """
+
+    def __init__(self, matches:MatchDict=None, selections:RESTSelections=None, display:RESTDisplay=None) -> None:
+        self.matches = matches        # New items in the search lists.
+        self.selections = selections  # New selections in the search lists.
+        self.display = display        # New graphical objects.
+
+
+class RESTGUIApplication:
+    """ Thread-safe GUI application for use in a web server.
+        Steno rules may be parsed into a tree of nodes, each of which may have several forms of representation.
+        All information for a single node is combined into a display "page" which can be used for GUI updates.
+        All display pages for to a single rule or lexer query are further stored in a single data object.
+        This allows for fewer HTTP requests and more opportunities for caching. """
+
+    def __init__(self, engine:GUIEngine) -> None:
+        self._engine = engine
+        self._lock = Lock()
+
+    def run(self, action:str, args:Iterable=(), options:dict=None) -> RESTUpdates:
+        """ Perform a REST app action. Input data includes an action method, its arguments (if any), and GUI options.
+            Option and graph state is not thread-safe, so we need a lock. """
+        with self._lock:
+            self._engine.set_options(options or {})
+            method = getattr(self, "REST_" + action)
+            updates = method(*args)
+            return RESTUpdates(**updates)
+
+    def _draw_page(self, ref="") -> RESTDisplayPage:
+        """ Create a display page for a rule <ref>erence, or a default page if ref is empty or invalid. """
+        ngraph = self._engine.draw_graph(ref)
+        igraph = self._engine.draw_graph(ref, intense=True)
+        caption = self._engine.get_caption(ref)
+        board = self._engine.draw_board(ref)
+        r_id = self._engine.get_example_id(ref)
+        return RESTDisplayPage(ngraph, igraph, caption, board, r_id)
+
+    def _display(self, keys:str, letters:str) -> RESTDisplay:
+        """ Run a query and return a full set of display data, including all possible selections. """
+        refs = self._engine.query(keys, letters)
+        pages = {ref: self._draw_page(ref) for ref in refs}
+        default_page = self._draw_page()
+        return RESTDisplay(keys, letters, pages, default_page)
+
+    def _select(self, keys:str, letters:str) -> RESTSelections:
+        match, mapping = self._engine.search_selection(keys, letters)
+        return RESTSelections(match, mapping)
+
+    def REST_search(self, pattern:str, pages=1) -> dict:
+        """ Do a new search and return results (unless the pattern is just whitespace). """
+        return {"matches": self._engine.search(pattern, pages)}
+
+    def REST_query(self, keys:str, letters:str) -> dict:
+        """ Execute and return a full display of a lexer query. """
+        return {"display": self._display(keys, letters)}
+
+    def REST_query_match(self, match:str, mappings:Sequence[str]) -> dict:
+        """ Query and display the best translation in a match-mappings pair from search. """
+        keys, letters = self._engine.best_translation(match, mappings)
+        return {"selections": self._select(keys, letters),
+                "display": self._display(keys, letters)}
+
+    def REST_search_examples(self, link_ref:str) -> dict:
+        """ Search for examples of the named rule and display one at random. """
+        pattern = self._engine.random_pattern(link_ref)
+        if not pattern:
+            return {}
+        matches = self._engine.search(pattern)
+        keys, letters = self._engine.random_translation(matches)
+        return {"matches": matches,
+                "selections": self._select(keys, letters),
+                "display": self._display(keys, letters)}
