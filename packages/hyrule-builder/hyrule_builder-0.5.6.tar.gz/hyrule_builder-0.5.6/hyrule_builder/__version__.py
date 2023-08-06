3429ba86762692cc418cf92043a4266fb0c3a58f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:08 +0000

    Add package hyrule-builder/0.5.6/hyrule_builder-0.5.6.tar.gz

---
+++ packages/hyrule-builder/hyrule_builder-0.5.6.tar.gz/hyrule_builder-0.5.6/hyrule_builder/__version__.py
@@ -0,0 +1,10 @@
+_MAJOR = 0
+_MINOR = 5
+_PATCH = "6"
+
+VERSION = f"{_MAJOR}.{_MINOR}.{_PATCH}"
+USER_VERSION = f"""{_MAJOR}.{_MINOR}.{_PATCH} {
+    'alpha' if _MAJOR < 1 else ''
+}{
+    'beta' if 'b' in _PATCH else ''
+}"""
