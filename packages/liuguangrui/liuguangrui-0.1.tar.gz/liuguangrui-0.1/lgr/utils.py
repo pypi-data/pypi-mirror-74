1af1116ffd9feacb201bbcbdb2e4366244143b8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:06 +0000

    Add package liuguangrui/0.1/liuguangrui-0.1.tar.gz

---
+++ packages/liuguangrui/liuguangrui-0.1.tar.gz/liuguangrui-0.1/lgr/utils.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+' a math module '
+
+__author__ = 'lgr'
+
+
+def sqrt(x):
+    """
+    就是打印一下，和x一点关系都没有
+    :param x:
+    :return:
+    """
+    print('这是 math_a 的 sqrt')
---
+++ packages/liuguangrui/liuguangrui-0.1.tar.gz/liuguangrui-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='liuguangrui',
+      version='0.1',
+      description='test1',
+      author='liuguangrui',
+      author_email='18302465887@163.com',
+      packages=['lgr'],
+      zip_safe=False)
