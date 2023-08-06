8f4c531778862e23b3bc7add908239726bade897
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:01 +0000

    Add package drweizhang-testpy/0.1/drweizhang_testpy-0.1.tar.gz

---
+++ packages/drweizhang-testpy/drweizhang_testpy-0.1.tar.gz/drweizhang_testpy-0.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='drweizhang_testpy',
+    version='0.1',
+    packages=['src',],
+    license='MIT',
+    url='https://github.com/wei-zhang-thz/exp',
+    install_requires=[
+        'numpy',
+    ],
+)
