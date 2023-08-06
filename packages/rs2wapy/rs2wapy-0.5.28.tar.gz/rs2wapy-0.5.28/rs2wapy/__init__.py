993267357864564378f83ac019f4e8c7d0a60f8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:31 +0000

    Add package rs2wapy/0.5.28/rs2wapy-0.5.28.tar.gz

---
+++ packages/rs2wapy/rs2wapy-0.5.28.tar.gz/rs2wapy-0.5.28/rs2wapy/__init__.py
@@ -0,0 +1,15 @@
+from ._version import get_versions
+from .adapters import adapters
+from .models import models
+from .parsing import parsing
+from .rs2wapy import RS2WebAdmin
+
+__version__ = get_versions()["version"]
+del get_versions
+
+__all__ = [
+    "adapters",
+    "models",
+    "parsing",
+    "RS2WebAdmin",
+]
