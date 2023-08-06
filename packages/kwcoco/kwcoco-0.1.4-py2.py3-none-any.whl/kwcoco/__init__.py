6c9ea819680631a02aae93d4d183fa214bef140e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:05 +0000

    Add package kwcoco/0.1.4/kwcoco-0.1.4-py2.py3-none-any.whl

---
+++ packages/kwcoco/kwcoco-0.1.4-py2.py3-none-any.whl/kwcoco/__init__.py
@@ -0,0 +1,12 @@
+"""
+mkinit ~/code/kwcoco/kwcoco/__init__.py -w
+"""
+__version__ = '0.1.4'
+
+__submodules__ = ['coco_dataset']
+
+from kwcoco import coco_dataset
+
+from kwcoco.coco_dataset import (CocoDataset,)
+
+__all__ = ['CocoDataset', 'coco_dataset']
