a257f23bb266aec71fdb299d699dbfc1dc0a8c6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:23 +0000

    Add package SummarizeIt/0.0.2/SummarizeIt-0.0.2.tar.gz

---
+++ packages/SummarizeIt/SummarizeIt-0.0.2.tar.gz/SummarizeIt-0.0.2/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+
+with open("Documentation.rst") as f:
+    description = f.read()
+
+setuptools.setup(
+    author="Jash Shah",
+    author_email="shahjash271@gmail.com",
+    name="SummarizeIt",
+    license="MIT",
+    description=f"Package for analyzing Youtube Videos from searching by relevant entities to analyzing sentiments and clustering different parts of the video according to your liking ",
+    version="v0.0.2",
+    long_description=description,
+    url="https://github.com/Jash271/YouGlance",
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6.8",
+    install_requires=[
+     "nltk"
+    ],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Intended Audience :: Developers",
+    ],
+    include_package_data=True,
+)
