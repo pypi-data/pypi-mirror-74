37a1497e03aefcb1e352e7ed92ed88b8e8fd0b5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:46 +0000

    Add package dda-queue/1.1.17/dda-queue-1.1.17.tar.gz

---
+++ packages/dda-queue/dda-queue-1.1.17.tar.gz/dda-queue-1.1.17/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(name='dda-queue',
+      version='1.1.17',
+      description='a queue manager (yet another)',
+      author='Volodymyr Savchenko',
+      author_email='vladimir.savchenko@gmail.com',
+      license='MIT',
+      packages=['dqueue'],
+      entry_points={
+          'console_scripts':  [
+              'dqueue=dqueue.cli:main',
+                ]
+          },
+      install_requires=[
+          'marshmallow',
+          'apispec',
+          'flasgger',
+          'bravado',
+          'termcolor',
+          ],
+      zip_safe=False,
+     )
