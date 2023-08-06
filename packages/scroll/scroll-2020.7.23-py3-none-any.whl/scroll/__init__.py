ee41fe68e493ee5520cd605c8114d14241b366e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:55 +0000

    Add package scroll/2020.7.23/scroll-2020.7.23-py3-none-any.whl

---
+++ packages/scroll/scroll-2020.7.23-py3-none-any.whl/scroll/__init__.py
@@ -0,0 +1,33 @@
+"""SCROLL: Automatically generates documentation and unit-tests for python programs"""
+__version__ = "2020.7.23"
+
+import os
+import sys
+import click
+import runpy
+from scroll.runner import RuntimeContextManager
+from scroll.tracer import Tracer
+from scroll.generators import generate_docs
+import pprint
+
+
+@click.command()
+@click.argument('filename')
+def scroll(filename):
+    """This is the main controller of the app.
+        It aslo responds to commandline args using click
+    """
+    click.echo(f'SCROLL: Running file {filename}')
+    # use a context manager to enter the file runtime and introspect it
+    with RuntimeContextManager(Tracer) as manager:
+        runpy.run_path(filename, run_name='__main__')
+    sys.settrace(None)
+    click.echo(f'SCROLL: Traces collected')
+    collected_traces = manager.traces
+    # pickle.dump(collected_traces, open( "collected_traces.pkl", "wb"))
+    # collected_traces = pickle.load(open( "collected_traces.pkl", "rb" ))
+    pprint.pprint(collected_traces)
+    # generate docs and write to files
+    directory = os.path.dirname(filename)
+    generate_docs(collected_traces, directory)
+    click.echo(f'SCROLL: Finished generating docs at docs/index.html')
