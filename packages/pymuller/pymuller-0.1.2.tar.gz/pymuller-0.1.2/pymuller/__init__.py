00cf8b27a6a88fa8e6126d5b13fa69340d180dab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:20 +0000

    Add package pymuller/0.1.2/pymuller-0.1.2.tar.gz

---
+++ packages/pymuller/pymuller-0.1.2.tar.gz/pymuller-0.1.2/pymuller/__init__.py
@@ -0,0 +1,5 @@
+from pymuller.logic import _muller_plot
+
+
+def muller(*args, **kwargs):
+    return _muller_plot(*args, **kwargs)
---
+++ packages/pymuller/pymuller-0.1.2.tar.gz/pymuller-0.1.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='pymuller',
+      version='0.1.2',
+      description='Muller plot generation in Python',
+      packages=['pymuller'],
+      author_email='boaz85@gmail.com',
+      install_requires=['pandas', 'matplotlib', 'scipy'],
+      zip_safe=False)
