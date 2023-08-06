605df9a1732c5c22018763c8af259f1e066073c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:43 +0000

    Add package starcli/2.11.0/starcli-2.11.0-py3-none-any.whl

---
+++ packages/starcli/starcli-2.11.0-py3-none-any.whl/starcli/layouts.py
@@ -0,0 +1,153 @@
+""" starcli.layouts """
+
+# Standard library imports
+import textwrap
+import math
+from shutil import get_terminal_size
+
+# Third party imports
+from rich.align import Align
+from rich.console import Console, render_group
+from rich.rule import Rule
+from rich.table import Table
+from rich.text import Text
+from rich.panel import Panel
+from rich.columns import Columns
+
+
+def shorten_count(number):
+    """Shortens number"""
+    if number < 1000:
+        return str(number)
+
+    number = int(number)
+    new_number = math.ceil(round(number / 100.0, 1)) * 100
+
+    if new_number % 1000 == 0:
+        return str(new_number)[0] + "k"
+    if new_number < 1000:
+        # returns the same old integer if no changes were made
+        return number
+    else:
+        # returns a new string if the number was shortened
+        return str(new_number / 1000.0) + "k"
+
+
+def get_stats(repo):
+    """ return formatted string of repo stats """
+    stats = f"{repo['stargazers_count']} ⭐ " if repo["stargazers_count"] != "-1" else ""
+    stats += f"{repo['forks_count']} 🍴" if repo["forks_count"] != "-1" else ""
+    stats += f"{repo['watchers_count']} 👀 " if repo["watchers_count"] else ""
+    return stats
+
+
+def list_layout(repos):
+    """ Displays repositories in list layout using rich """
+
+    LAYOUT_WIDTH = 80
+
+    @render_group()
+    def render_repo(repo):
+        """Yields renderables for a single repo."""
+        yield Rule(style="bright_yellow")
+        yield ""
+        # Table with description and stats
+        title_table = Table.grid(padding=(0, 1))
+        title_table.expand = True
+        stats = get_stats(repo)
+        title = Text(repo["full_name"], overflow="fold")
+        title.stylize_all(f"yellow link {repo['html_url']}")
+        title_table.add_row(title, Text(stats, style="bold blue"))
+        title_table.columns[1].no_wrap = True
+        title_table.columns[1].justify = "right"
+        yield title_table
+        yield ""
+        # Language
+        language = repo["language"]
+        if language:
+            yield Text(language, style="bold cyan")
+        else:
+            yield "[i cyan]unknown language"
+        yield ""
+        # Descripion
+        description = repo["description"]
+        if description:
+            yield Text(description.strip(), style="green")
+        else:
+            yield "[i green]no description"
+        yield ""
+
+    def column(renderable):
+        """Constrain width and align to center to create a column."""
+        return Align.center(renderable, width=LAYOUT_WIDTH, pad=False)
+
+    console = Console()  # initialise rich
+    for repo in repos:
+        console.print(column(render_repo(repo)))
+    console.print(column(Rule(style="bright_yellow")))
+
+
+def table_layout(repos):
+    """ Displays repositories in a table format using rich """
+
+    table = Table(leading=1)
+
+    # make the columns
+    table.add_column("Name", style="bold cyan")
+    table.add_column("Language", style="green")
+    table.add_column("Description", style="blue")
+    table.add_column("Stats", style="magenta")
+
+    for repo in repos:
+
+        stats = get_stats(repo)
+
+        if not repo["language"]:  # if language is not provided
+            repo["language"] = "None"  # make it a string
+        if not repo["description"]:  # same here
+            repo["description"] = "None"
+
+        table.add_row(
+            repo["name"],
+            repo["language"],  # so that it can work here
+            repo["description"],
+            stats,
+        )
+
+    console = Console()
+    console.print(table)
+
+
+def grid_layout(repos):
+    """ Displays repositories in a grid format using rich """
+
+    max_desc_len = 90
+
+    panels = []
+    for repo in repos:
+
+        stats = get_stats(repo)
+
+        if not repo["language"]:  # if language is not provided
+            repo["language"] = "None"  # make it a string
+        if not repo["description"]:
+            repo["description"] = "None"
+
+        name = Text(repo["name"], style="bold yellow")
+        language = Text(repo["language"], style="magenta")
+        description = Text(repo["description"], style="green")
+        stats = Text(stats, style="blue")
+
+        # truncate rest of the description if
+        # it's more than 90 (max_desc_len) chars
+        # using truncate() is better because it also
+        # takes care of asian characters
+        description.truncate(max_desc_len, overflow="ellipsis")
+
+        repo_summary = Text.assemble(
+            name, "\n", stats, "\n", language, "\n", description,
+        )
+        panels.append(Panel(repo_summary, expand=True))
+
+    console = Console()
+    console.print((Columns(panels, width=30, expand=True)))
---
+++ packages/starcli/starcli-2.11.0-py3-none-any.whl/starcli/search.py
@@ -0,0 +1,181 @@
+""" starcli.search """
+
+# Standard library imports
+from datetime import datetime, timedelta
+import logging
+from random import randint
+import re
+
+# Third party imports
+import requests
+from click import secho
+import colorama
+from bs4 import BeautifulSoup
+
+API_URL = "https://api.github.com/search/repositories"
+
+
+def debug_requests_on():
+    """ Turn on the logging for requests """
+    logger = logging.getLogger(__name__)
+    try:
+        from http.client import HTTPConnection
+
+        HTTPConnection.set_debuglevel(HTTPConnection, 1)
+    except ImportError:
+        import httplib
+
+        httplib.HTTPConnection.debuglevel = 2
+
+    logging.basicConfig()
+    logging.getLogger().setLevel(logging.DEBUG)
+    requests_log = logging.getLogger("requests.packages.urllib3")
+    requests_log.setLevel(logging.DEBUG)
+    requests_log.propagate = True
+
+
+def convert_datetime(date, date_format):
+    """ Safely convert a date string to datetime """
+    try:
+        # try to turn the string into a date-time object
+        tmp_date = datetime.strptime(date, date_format)
+    except ValueError:  # ValueError will be thrown if format is invalid
+        secho(
+            "Invalid date: " + date + " must be yyyy-mm-dd", fg="bright_red",
+        )
+        return None
+    return tmp_date
+
+
+def search(
+    language=None,
+    date_created=None,
+    last_updated=None,
+    stars=">=100",
+    topics=[],
+    debug=False,
+    order="desc",
+):
+    """ Returns repositories based on the language, date, and stars
+
+    """
+    date_format = "%Y-%m-%d"  # date format in iso format
+    if debug:
+        debug_requests_on()
+        print("DEBUG: search: date_created param:", date_created)
+        print("DEBUG: search: order param: ", order)
+
+    day_range = 0 - randint(100, 400)  # random negative from 100 to 400
+
+    if not date_created:  # if date_created not provided
+        # creation date: start, is the time now minus a random number of days
+        # 100 to 400 days - which was stored in day_range
+        start_date_created = (datetime.utcnow() + timedelta(days=day_range)).strftime(
+            date_format
+        )
+        end_date_created = (datetime.utcnow() + timedelta(days=1)).strftime(date_format)
+    else:  # if date_created is provided
+        tmp_date = convert_datetime(date_created, date_format)
+        if tmp_date is None:
+            return None
+        end_date_created = (tmp_date + timedelta(days=1)).strftime(date_format)
+        start_date_created = tmp_date.strftime(date_format)
+
+    if not last_updated:  # if last_updated not provided
+        # update date: start, is the time now minus a random number of days
+        # 100 to 400 days - which was stored in day_range
+        start_last_updated = (datetime.utcnow() + timedelta(days=day_range)).strftime(
+            date_format
+        )
+        end_last_updated = (datetime.utcnow() + timedelta(days=1)).strftime(date_format)
+    else:  # if last_updated is provided
+        tmp_date = convert_datetime(last_updated, date_format)
+        if tmp_date is None:
+            return None
+        end_last_updated = (tmp_date + timedelta(days=1)).strftime(date_format)
+        start_last_updated = tmp_date.strftime(date_format)
+
+    if debug:  # print start_date and end_date if debugging
+        print("DEBUG: search: start_date_created:", start_date_created)
+        print("DEBUG: search: end_date_created:", end_date_created)
+        print("DEBUG: search: start_last_updated:", start_last_updated)
+        print("DEBUG: search: end_last_updated:", end_last_updated)
+
+    query = f"stars:{stars}+created:{start_date_created}..{end_date_created}"  # construct query
+    query += (
+        f"+pushed:{start_last_updated}..{end_last_updated}"  # add last updated to query
+    )
+    query += f"+language:{language}" if language else ""  # add language to query
+    query += f"".join(["+topic:" + i for i in topics])  # add topics to query
+
+    url = f"{API_URL}?q={query}&sort=stars&order={order}"  # use query to construct url
+    if debug:
+        print("DEBUG: search: url:", url)  # print the url when debugging
+
+    try:
+        repositories = requests.get(url).json()  # get the response using the url
+    except requests.exceptions.ConnectionError:
+        secho("Internet connection error...", fg="bright_red")
+        return None
+
+    return repositories["items"]
+
+
+def search_by_spoken_language(
+    language=None, spoken_language=None, order="desc", stars=">=10"
+):
+    """ Returns trending repositories from github trending page """
+    API_URL = "https://github.com/trending"  # filter for spoken language is available only here
+    if language:
+        API_URL += "{language}"  # filter by programming language
+    query = f"?spoken_language_code={spoken_language}"  # filter by spoken language
+    url = f"{API_URL}{query}"
+    try:
+        page = requests.get(url).text
+    except requests.exceptions.ConnectionError:
+        secho("Internet connection error...", fg="bright_red")
+        return None
+
+    soup = BeautifulSoup(page, "lxml")
+    repo_list = soup.find_all("article", class_="Box-row")
+    repositories = []
+    for repo in repo_list:
+        repo_dict = {}
+        repo_dict["full_name"] = repo.h1.a.text.replace(" ", "").replace("\n", "")
+        repo_dict["name"] = repo_dict["full_name"].split("/")[1]
+        repo_dict["html_url"] = f"https://github.com/{repo_dict['full_name']}"
+        div = repo.find("div", class_="f6 text-gray mt-2")
+        anchor_list = div.find_all("a")
+        # if stars present
+        repo_dict["stargazers_count"] = (
+            int(anchor_list[0].text.strip().replace(",", ""))
+            if anchor_list[0].text
+            else -1
+        )
+        # if forks count present
+        repo_dict["forks_count"] = (
+            int(anchor_list[1].text.strip().replace(",", ""))
+            if anchor_list[1].text
+            else -1
+        )
+        language_span = div.span.find_all("span")
+        # if language is present
+        repo_dict["language"] = (
+            language_span[1].text.strip() if len(language_span) > 0 else None
+        )
+        # if description is present
+        repo_dict["description"] = repo.p.text.strip() if repo.p else None
+        repo_dict["watchers_count"] = None  # watchers count not available
+        # filter by number of stars
+        num = [int(s) for s in re.findall(r"\d+", stars)][0]
+        if (
+            ("<" in stars and repo_dict["stargazers_count"] < num)
+            or ("<=" in stars and repo_dict["stargazers_count"] <= num)
+            or (">" in stars and repo_dict["stargazers_count"] > num)
+            or (">=" in stars and repo_dict["stargazers_count"] >= num)
+        ):
+            repositories.append(repo_dict)
+
+    if order == "asc":
+        return sorted(repositories, key=lambda repo: repo["stargazers_count"])
+    return sorted(repositories, key=lambda repo: repo["stargazers_count"], reverse=True)
