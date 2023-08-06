587e3110555c45bc5e5f81913ba264cdd1219cb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:13 +0000

    Add package npmdownloader/1.2.1/npmdownloader-1.2.1-py2.py3-none-any.whl

---
+++ packages/npmdownloader/npmdownloader-1.2.1-py2.py3-none-any.whl/npmdownloader/__main__.py
@@ -0,0 +1,30 @@
+import argparse
+
+from .multipackagedownloader import MultiPackageDownloader
+from .logger import log
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='Download npm packages including dependencies')
+    parser.add_argument(
+        '-f', '--packages-file', required=True,
+        help='File with packages to download, seperated by newlines')
+    parser.add_argument(
+        '-o', '--download-dir', required=True,
+        help='Output directory to download packages to')
+    parser.add_argument(
+        '-c', '--workers-count', required=True, type=int,
+        help='Number of worker processes to spawn')
+    args = parser.parse_args()
+
+    downloader = MultiPackageDownloader(
+        args.packages_file, args.download_dir, args.workers_count)
+    log.info('Multi Download Started')
+    downloader.start()
+    downloader.wait()
+    log.info('Multi Download Finished')
+
+
+if __name__ == '__main__':
+    main()
