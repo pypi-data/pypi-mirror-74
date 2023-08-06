ddafe0811e0161ba538c2eb3fc20aa5fcafe8f73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:47 +0000

    Add package ttncli/0.0.5/ttncli-0.0.5.tar.gz

---
+++ packages/ttncli/ttncli-0.0.5.tar.gz/ttncli-0.0.5/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ttncli",
+    version="0.0.5",
+    author="TTN",
+    author_email="vibhor.kukreja@tothenew.com",
+    description="Basic Command line utility",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/vibhor-kukreja/ttn-cli",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'Click==7.1.0',
+        'GitPython==3.1.3',
+    ],
+    entry_points={
+        'console_scripts': [
+            'ttn_cli=ttn_cli:main',
+        ],
+    },
+    python_requires='>=3.6',
+)
