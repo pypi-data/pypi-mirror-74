67ed1451f177570f7a3cff65df397655c8465042
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:42 +0000

    Add package trainerdex/3.0.6/trainerdex-3.0.6-py3-none-any.whl

---
+++ packages/trainerdex/trainerdex-3.0.6-py3-none-any.whl/trainerdex/__init__.py
@@ -0,0 +1,22 @@
+"""
+Official TrainerDex API Wrapper
+-------------------------------
+
+A basic wrapper for the TrianerDex API.
+
+:copyright: (c) 2020 JayTurnr
+:licence: GNU-GPL3, see LICENSE for more details
+
+"""
+
+__title__ = 'trainerdex'
+__author__ = 'JayTurnr'
+__licence__ = 'GNU-GPL'
+__copyright__ = 'Copyright 2020 JayTurnr'
+__version__ = '3.0.6'
+
+from trainerdex.client import Client
+from trainerdex.exceptions import *
+from trainerdex.http import Route, HTTPClient
+from trainerdex.leaderboard import DiscordLeaderboard, WorldwideLeaderboard
+from trainerdex.models import DiscordUser, Trainer, Update, User, Level, Levels, Team, Teams
