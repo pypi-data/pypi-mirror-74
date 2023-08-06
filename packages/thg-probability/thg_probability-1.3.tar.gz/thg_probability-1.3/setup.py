9d4161345c4bdad3f424a422c42b775b64cb54ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:22 +0000

    Add package thg-probability/1.3/thg_probability-1.3.tar.gz

---
+++ packages/thg-probability/thg_probability-1.3.tar.gz/thg_probability-1.3/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='thg_probability',
+      version='1.3',
+      description='Gaussian and Binomial distributions',
+      packages=['thg_probability'],
+      author="Thomas Hatting",
+      author_email= 'mail@thomashatting.co',
+      zip_safe=False)
