0c38a858f677bf93c53b140821f5b5fa2af61a00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:19 +0000

    Add package essdistributions/1.2/essdistributions-1.2.tar.gz

---
+++ packages/essdistributions/essdistributions-1.2.tar.gz/essdistributions-1.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name='essdistributions',
+      version='1.2',
+      author='Esraa Abduallah',
+      author_email='esraahisham753@gmail.com',
+      description='Gaussian&Binomial distributions',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url='https://github.com/esraahisham753/Gaussian_Binomial_package',
+      classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+      ],
+      setup_requires=['wheel'],
+      python_requires='>=3.0',
+      packages=['distributions'])
