16165111a55b2c043879fbf080ca93e33d1c10a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:53 +0000

    Add package miniboss/0.1.0/miniboss-0.1.0.tar.gz

---
+++ packages/miniboss/miniboss-0.1.0.tar.gz/miniboss-0.1.0/setup.py
@@ -0,0 +1,22 @@
+import os
+from setuptools import setup
+
+setup(
+    name = "miniboss",
+    version = "0.1.0",
+    author = "Ulas Turkmen",
+    description = "Containerized app testing framework",
+    install_requires = [],
+    tests_require = [],
+    packages=['miniboss'],
+    url = "https://github.com/afroisalredyinu/miniboss",
+    license = "MIT",
+    classifiers = [
+        "License :: OSI Approved :: MIT License",
+        "Development Status :: 3 - Alpha",
+        "Topic :: Software Development :: Build Tools",
+        "Topic :: Software Development :: Testing",
+        "Programming Language :: Python :: 3",
+        "Intended Audience :: Developers"
+    ]
+)
