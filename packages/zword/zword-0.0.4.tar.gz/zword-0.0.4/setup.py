176ce5dba5262c09c3963b9f4fdef60fc2d61c20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:45 +0000

    Add package zword/0.0.4/zword-0.0.4.tar.gz

---
+++ packages/zword/zword-0.0.4.tar.gz/zword-0.0.4/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+with open("README.md", 'r') as f:
+  long_description = f.read()
+
+with open('requirements.txt') as f:
+  install_requires = f.read().splitlines()
+
+setup(
+  author="zuroc",
+  author_email="zsp042@gmail.com",
+  description="从维基百科抽取中文语料",
+  include_package_data=True,
+  install_requires=install_requires,
+  name='zword',
+  packages=['zword'],
+  url="https://gitee.com/znlp/zword",
+  version="0.0.4",
+  zip_safe=True,
+  long_description_content_type='text/markdown',
+  long_description=long_description,
+  scripts=[
+    "wiki_txt",
+  ]
+)
