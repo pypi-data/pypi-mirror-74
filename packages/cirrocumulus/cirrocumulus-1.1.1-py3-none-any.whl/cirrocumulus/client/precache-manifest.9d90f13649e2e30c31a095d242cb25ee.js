2697fb34473a991be881671c1bbbbff2a965f14e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:47 +0000

    Add package cirrocumulus/1.1.1/cirrocumulus-1.1.1-py3-none-any.whl

---
+++ packages/cirrocumulus/cirrocumulus-1.1.1-py3-none-any.whl/cirrocumulus/client/precache-manifest.9d90f13649e2e30c31a095d242cb25ee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0dc4d4d5d475e92980470d2ff910231a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fb80cc06642023ecb871",
+    "url": "/static/js/2.a8f0c708.chunk.js"
+  },
+  {
+    "revision": "0da29b6b2f3869397813e5bdf8b84698",
+    "url": "/static/js/2.a8f0c708.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fd9e1578f4ea060cd89",
+    "url": "/static/js/main.b1c906b3.chunk.js"
+  },
+  {
+    "revision": "9e766b7ee20fa086f639",
+    "url": "/static/js/runtime-main.e853a34d.js"
+  }
+]);
