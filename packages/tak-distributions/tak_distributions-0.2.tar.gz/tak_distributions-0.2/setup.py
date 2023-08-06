0942bf2a4bd4af365b90396ff7729955d9dcadd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:16 +0000

    Add package tak-distributions/0.2/tak_distributions-0.2.tar.gz

---
+++ packages/tak-distributions/tak_distributions-0.2.tar.gz/tak_distributions-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='tak_distributions',
+      version='0.2',
+      description='Gaussian & Binomial distributions',
+      long_description='Sample packaging for pip learning, contaning sample code for calculating Gaussian & Binomial Distribution',
+      packages=['tak_distributions'],
+      author = 'TAK',
+      zip_safe=False)
