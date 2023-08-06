5b7e9d43d510cb6d11d785f456f403ce5d5f02db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:42 +0000

    Add package image-embeddings/1.2.1/image_embeddings-1.2.1.tar.gz

---
+++ packages/image-embeddings/image_embeddings-1.2.1.tar.gz/image_embeddings-1.2.1/image_embeddings/cli/main.py
@@ -0,0 +1,20 @@
+from image_embeddings.downloader import save_examples_to_folder
+from image_embeddings.inference import write_tfrecord
+from image_embeddings.inference import run_inference
+from image_embeddings.knn import random_search
+import fire
+import logging
+
+
+def main():
+    """Main entry point"""
+    logging.basicConfig(level=logging.INFO)
+    logging.getLogger("tensorflow").handlers = []
+    fire.Fire(
+        {
+            "save_examples_to_folder": save_examples_to_folder,
+            "write_tfrecord": write_tfrecord,
+            "run_inference": run_inference,
+            "random_search": random_search,
+        }
+    )
