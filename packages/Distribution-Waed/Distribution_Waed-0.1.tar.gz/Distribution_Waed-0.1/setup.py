0469e15114b421b2c53b793cdbdec0d93d2a5a6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:36 +0000

    Add package Distribution-Waed/0.1/Distribution_Waed-0.1.tar.gz

---
+++ packages/Distribution-Waed/Distribution_Waed-0.1.tar.gz/Distribution_Waed-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='Distribution_Waed',
+      version='0.1',
+      description=' distributions',
+      packages=['Distribution_Waed'],
+      zip_safe=False)
