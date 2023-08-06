a49ddcbc41efae5ca9b43a47c4e7c7b65b875651
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:14 +0000

    Add package gaubin-distributions/0.1/gaubin_distributions-0.1.tar.gz

---
+++ packages/gaubin-distributions/gaubin_distributions-0.1.tar.gz/gaubin_distributions-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='gaubin_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['gaubin_distributions'],
+      author='Shivansh Aggarwal',
+      zip_safe=False)
