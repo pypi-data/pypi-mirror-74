54a41df64c3f3d75535db157b8eeadf6775a3754
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:46 +0000

    Add package gaussian-binomialDis/1.0/gaussian_binomialDis-1.0.tar.gz

---
+++ packages/gaussian-binomialDis/gaussian_binomialDis-1.0.tar.gz/gaussian_binomialDis-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='gaussian_binomialDis',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['gaussian_binomialDis'],
+      author = 'Souilah Amine',
+      author_email = 'souilah.amine@gmail.com',
+    zip_safe=False)
