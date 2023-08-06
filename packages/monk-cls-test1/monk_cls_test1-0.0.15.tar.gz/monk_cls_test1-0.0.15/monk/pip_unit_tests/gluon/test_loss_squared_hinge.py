c73f0322aca63e1a9e864ac826088a3fc5fc5308
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:47 +0000

    Add package monk-cls-test1/0.0.15/monk_cls_test1-0.0.15.tar.gz

---
+++ packages/monk-cls-test1/monk_cls_test1-0.0.15.tar.gz/monk_cls_test1-0.0.15/monk/pip_unit_tests/gluon/test_loss_squared_hinge.py
@@ -0,0 +1,53 @@
+import os
+import sys
+
+import psutil
+
+from monk.gluon_prototype import prototype
+from monk.compare_prototype import compare
+from monk.pip_unit_tests.gluon.common import print_start
+from monk.pip_unit_tests.gluon.common import print_status
+
+import mxnet as mx
+import numpy as np
+from monk.gluon.losses.return_loss import load_loss
+
+
+def test_loss_squared_hinge(system_dict):
+    forward = True;
+    if(not os.path.isdir("datasets")):
+        os.system("! wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1rG-U1mS8hDU7_wM56a1kc-li_zHLtbq2' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1rG-U1mS8hDU7_wM56a1kc-li_zHLtbq2" -O datasets.zip && rm -rf /tmp/cookies.txt")
+        os.system("! unzip -qq dataset.zip")
+
+    test = "test_loss_squared_hinge";
+    system_dict["total_tests"] += 1;
+    print_start(test, system_dict["total_tests"])
+    if(forward):
+        try:
+            gtf = prototype(verbose=0);
+            gtf.Prototype("sample-project-1", "sample-experiment-1");
+
+            label = np.random.rand(1, 5);
+            label = mx.nd.array(label);
+
+            y = np.random.rand(1, 5);
+            y = mx.nd.array(y);
+
+            gtf.loss_squared_hinge();
+            load_loss(gtf.system_dict);
+            loss_obj = gtf.system_dict["local"]["criterion"];
+            loss_val = loss_obj(y, label);           
+
+            system_dict["successful_tests"] += 1;
+            print_status("Pass");
+
+        except Exception as e:
+            system_dict["failed_tests_exceptions"].append(e);
+            system_dict["failed_tests_lists"].append(test);
+            forward = False;
+            print_status("Fail");
+    else:
+        system_dict["skipped_tests_lists"].append(test);
+        print_status("Skipped");
+
+    return system_dict
