08c9cba53fc13393d5cc1a96a59ea5d551988a6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:14 +0000

    Add package bfengine/0.1.7/bfengine-0.1.7-py3-none-any.whl

---
+++ packages/bfengine/bfengine-0.1.7-py3-none-any.whl/bf_engine/entity/answer.py
@@ -0,0 +1,7 @@
+class Answer:
+    """
+    机器人回答
+    """
+    def __init__(self, text: str, score: float):
+        self.text = text
+        self.score = score
---
+++ packages/bfengine/bfengine-0.1.7-py3-none-any.whl/bf_engine/entity/exception.py
@@ -0,0 +1,15 @@
+class ErrorCode:
+    faq_import_error = 10010
+    faq_train_error = 10011
+    kg_import_error = 10020
+    kg_train_error = 10021
+    dm_load_error = 10001
+
+
+class BfEngineException(Exception):
+    def __init__(self, code, msg):
+        self.code = code
+        self.message = msg
+
+    def __str__(self):
+        return '{}: {}'.format(self.code, self.message)
