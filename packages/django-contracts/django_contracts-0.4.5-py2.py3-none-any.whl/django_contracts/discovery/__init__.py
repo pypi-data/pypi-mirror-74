59616411bdcadc4c4c954af883208c22f3d219cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:35 +0000

    Add package django-contracts/0.4.5/django_contracts-0.4.5-py2.py3-none-any.whl

---
+++ packages/django-contracts/django_contracts-0.4.5-py2.py3-none-any.whl/django_contracts/discovery/__init__.py
@@ -0,0 +1,21 @@
+from django.urls import get_resolver
+
+from .primitives import view_to_json
+
+
+def get_application_discovery_document():
+    serializing_iterator = (
+        (view_to_json(vf), url_info)
+        for vf, url_info in get_resolver().reverse_dict.items() if callable(vf)
+    )
+
+    make_into_list_of_dictionaries = [
+        {
+            'url': x[1][1],
+            **x[0],
+        }
+        for x in serializing_iterator
+        if x[0]
+    ]
+
+    return make_into_list_of_dictionaries
