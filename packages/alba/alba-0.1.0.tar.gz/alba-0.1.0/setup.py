1c8150fbcc3d5026d389b67c91db56675c86328b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:43 +0000

    Add package alba/0.1.0/alba-0.1.0.tar.gz

---
+++ packages/alba/alba-0.1.0.tar.gz/alba-0.1.0/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+
+with open("README.md") as f:
+    description = f.read()
+
+setup(
+    name="alba",
+    author="ALBA Synchrotron computing team",
+    author_email="computing@cells.es",
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    ],
+    packages=find_packages(),
+    description="ALBA controls meta package",
+    long_description=description,
+    long_description_content_type="text/markdown",
+    keywords="ALBA, synchrotron, controls",
+    url="https://github.com/alba-synchrotron/alba",
+    version="0.1.0",
+    python_requires=">=3.5",
+)
