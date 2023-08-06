e9cac411031afebff45a2a6c598c4203af6ce83f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:11 +0000

    Add package ahmed-package/1.0/ahmed_package-1.0.tar.gz

---
+++ packages/ahmed-package/ahmed_package-1.0.tar.gz/ahmed_package-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='ahmed_package',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['ahmed_package'],
+      author = 'Ahmed Gaal',
+      author_email = 'ahmedito14@gmail.com',
+      zip_safe=False)
