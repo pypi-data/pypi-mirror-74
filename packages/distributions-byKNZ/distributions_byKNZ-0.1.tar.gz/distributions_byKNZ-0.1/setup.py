f953c24882496c16fed3d00c0eb4c63df17698a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:25 +0000

    Add package distributions-byKNZ/0.1/distributions_byKNZ-0.1.tar.gz

---
+++ packages/distributions-byKNZ/distributions_byKNZ-0.1.tar.gz/distributions_byKNZ-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='distributions_byKNZ',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions_byKNZ'],
+      author="Kanza Syed",
+      zip_safe=False)
