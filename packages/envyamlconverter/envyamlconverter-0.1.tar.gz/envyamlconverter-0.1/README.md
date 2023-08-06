5b2a8e762dd0a627282c236ef3fc8ca9a3c21a81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:36 +0000

    Add package envyamlconverter/0.1/envyamlconverter-0.1.tar.gz

---
+++ packages/envyamlconverter/envyamlconverter-0.1.tar.gz/envyamlconverter-0.1/README.md
@@ -0,0 +1,2 @@
+# envToDeploymentYaml
+Python Script to convert .env files to a deployment yaml
---
+++ packages/envyamlconverter/envyamlconverter-0.1.tar.gz/envyamlconverter-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='envyamlconverter',
+      version='0.1',
+      description='Env Yaml Converter',
+      url='https://github.com/asrvstv5/envToDeploymentYaml',
+      author='Amitesh Srivastava',
+      author_email='asrvstv5@gmail.com',
+      license='MIT',
+      packages=['envyamlconverter'],
+      zip_safe=False)
