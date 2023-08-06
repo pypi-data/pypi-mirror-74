dc7f831e6a6e7a91323de7b347961bf473b5803d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:19 +0000

    Add package pyfxgit/0.0.1/pyfxgit-0.0.1.tar.gz

---
+++ packages/pyfxgit/pyfxgit-0.0.1.tar.gz/pyfxgit-0.0.1/setup.py
@@ -0,0 +1,27 @@
+from os import path
+from setuptools import setup
+
+# read the contents of your README file
+strCwd = path.abspath(path.dirname(__file__))
+with open(path.join(strCwd, 'README.md'), encoding='utf-8') as f:
+  strDesc = f.read()
+
+# setup details
+# 0.1.0 version
+# ┬ ┬ ┬
+# │ │ └─────────────── test version
+# │ └──────────────────── minor version
+# └───────────────────────── major version
+setup(name='pyfxgit',
+  version='0.0.1',
+  description='This is a package of helper classes and libraries.',
+  packages=['pyfxgit'],
+  install_requires=['matplotlib==3.2.2','mpl_finance','numpy==1.18.5','pandas==1.0.5'],
+  author='Dennis Lee',
+  author_email='dennislwm@gmail.com',
+  URL="https://github.com/dennislwm/pyfxgit", 
+  long_description=strDesc,
+  long_description_content_type='text/markdown',
+  license="MIT", 
+  zip_safe=False
+)
