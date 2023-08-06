aad71aa0111f9b0ee8371c188303be5cea78caf1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:58 +0000

    Add package eo-lib/0.2.1/eo_lib-0.2.1-py3-none-any.whl

---
+++ packages/eo-lib/eo_lib-0.2.1-py3-none-any.whl/eo_lib/application/teams/application.py
@@ -0,0 +1,35 @@
+from eo_lib.service.teams.service import *
+from eo_lib.application.abstract_application import AbstractApplication
+
+class ApplicationOrganization(AbstractApplication):
+	def __init__(self):
+		super().__init__(OrganizationService())
+
+class ApplicationPerson(AbstractApplication):
+	def __init__(self):
+		super().__init__(PersonService())
+	
+class ApplicationTeamMember(AbstractApplication):
+	def __init__(self):
+		super().__init__(TeamMemberService())
+	
+class ApplicationOrganization_Role(AbstractApplication):
+	def __init__(self):
+		super().__init__(Organization_RoleService())
+	
+class ApplicationTeamMembership(AbstractApplication):
+	def __init__(self):
+		super().__init__(TeamMembershipService())
+	
+class ApplicationTeam(AbstractApplication):
+	def __init__(self):
+		super().__init__(TeamService())
+	
+class ApplicationProjectTeam(AbstractApplication):
+	def __init__(self):
+		super().__init__(ProjectTeamService())
+	
+class ApplicationProject(AbstractApplication):
+	def __init__(self):
+		super().__init__(ProjectService())
+	
