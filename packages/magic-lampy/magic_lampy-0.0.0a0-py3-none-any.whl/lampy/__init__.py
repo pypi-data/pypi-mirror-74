09131c065ef079b96b513a0d91d768a68f6de1fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:28 +0000

    Add package magic-lampy/0.0.0a0/magic_lampy-0.0.0a0-py3-none-any.whl

---
+++ packages/magic-lampy/magic_lampy-0.0.0a0-py3-none-any.whl/lampy/__init__.py
@@ -0,0 +1,7 @@
+__copyright__    = 'Copyright (C) 2020 Shotaro Kataoka, Masashi Oba'
+__version__      = '0.0.0.alpha0'
+__license__      = 'MIT'
+__author__       = 'Shotaro Kataoka, Masashi Oba'
+__author_email__ = 'shotaro.kata@gmail.com'
+__url__          = 'https://github.com/MagicLamPy/MagicLamPy'
+
