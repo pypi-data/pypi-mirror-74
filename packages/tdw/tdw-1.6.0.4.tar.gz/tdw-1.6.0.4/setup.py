15db9494a4bf1d02c52ba905af4d43caa9a92ddc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:24 +0000

    Add package tdw/1.6.0.4/tdw-1.6.0.4.tar.gz

---
+++ packages/tdw/tdw-1.6.0.4.tar.gz/tdw-1.6.0.4/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+from pathlib import Path
+
+version = "v1.6.0.4"
+
+setup(
+    name='tdw',
+    version=version,
+    description='3D simulation environment',
+    long_description=Path('README.md').read_text(encoding='utf-8'),
+    long_description_content_type='text/markdown',
+    url='https://github.com/threedworld-mit/tdw',
+    download_url=f'https://github.com/threedworld-mit/tdw/archive/v{version}.tar.gz',
+    author_email='alters@mit.edu',
+    author='Massachusetts Institute of Technology',
+    license='BSD',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8'
+    ],
+    keywords='unity simulation ml machine-learning',
+    install_requires=['pyzmq', 'pymongo', 'numpy', 'scipy', 'pillow', 'tqdm', 'psutil', 'boto3', 'botocore'],
+)
