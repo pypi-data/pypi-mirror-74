4ec877c5ae3c25238c93bcc81766d65087327307
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:24 +0000

    Add package nesterAKAJ/1.0.0/nesterAKAJ-1.0.0.tar.gz

---
+++ packages/nesterAKAJ/nesterAKAJ-1.0.0.tar.gz/nesterAKAJ-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+	name = "nesterAKAJ",
+	version = '1.0.0',
+	py_modules = ['nesterAKAJ'],
+	author = 'Akshay Karhade',
+	author_email = 'akshaykarhade@gmail.com',
+	url = 'https://www.akshaykarhade.com',
+	description = 'A simple printer of nested lists',
+
+	)
