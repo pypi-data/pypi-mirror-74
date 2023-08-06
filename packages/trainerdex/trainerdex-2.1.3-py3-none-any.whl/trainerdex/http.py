b7e687aca77c428d18506815e947569e1144ceb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:58 +0000

    Add package trainerdex/2.1.3/trainerdex-2.1.3-py3-none-any.whl

---
+++ packages/trainerdex/trainerdex-2.1.3-py3-none-any.whl/trainerdex/http.py
@@ -0,0 +1,23 @@
+﻿import requests
+
+api_url = 'https://www.trainerdex.co.uk/api/v1/'
+
+def request_status(r, detailed=False):
+	"""Returns a formatted string about the status, useful for logging.
+	
+	args:
+	r - takes requests.models.Response
+	"""
+	
+	base_string = "HTTP {r.request.method} {r.request.url}: {r.status_code}"
+	
+	if r.status_code in range(200,99):
+		string = base_string
+		if detailed is True:
+			string += " - {r.json()}"
+		else:
+			string += " - 👍"
+		return string.format(r=r)
+	else:
+		string = base_string
+		return string.format(r=r)
