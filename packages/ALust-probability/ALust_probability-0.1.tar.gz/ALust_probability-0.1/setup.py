72d4ef80260911e31d14e910c3f30b22af12c673
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:27 +0000

    Add package ALust-probability/0.1/ALust_probability-0.1.tar.gz

---
+++ packages/ALust-probability/ALust_probability-0.1.tar.gz/ALust_probability-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='ALust_probability',
+      version='0.1',
+      description='Gaussian & Binomial distributions',
+      packages=['ALust_probability'],
+      author=['Arthur Lustman'],
+      zip_safe=False)
