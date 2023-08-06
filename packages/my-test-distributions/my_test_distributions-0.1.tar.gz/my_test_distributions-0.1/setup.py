a0267c1f125d23c2f92d3ccce348b9364a4a46ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:49 +0000

    Add package my-test-distributions/0.1/my_test_distributions-0.1.tar.gz

---
+++ packages/my-test-distributions/my_test_distributions-0.1.tar.gz/my_test_distributions-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='my_test_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['my_test_distributions'],
+      install_requires=['matplotlib'],
+      zip_safe=False)
