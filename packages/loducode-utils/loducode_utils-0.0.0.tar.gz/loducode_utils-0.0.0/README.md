9d927f45f75374e230a5693c8b09b9abcfda23c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:39 +0000

    Add package loducode-utils/0.0.0/loducode_utils-0.0.0.tar.gz

---
+++ packages/loducode-utils/loducode_utils-0.0.0.tar.gz/loducode_utils-0.0.0/README.md
@@ -0,0 +1,31 @@
+# Loducode utils
+
+Basic components for the development of loducode s.a.s.
+
+## functionalities
+
+- **Admin**
+    - ReadOnlyAdmin
+    - ReadOnlyStackedInline
+    - ReadOnlyTabularInline
+- **Models**
+    - Audit
+    - City  
+- **Tasks**
+    - send_mail_task(email, subject, message)
+- **Urls Api**
+    - /api/token/
+    - /api/logout/
+    - /api/forget/
+- **Utils**
+    - PaginatedListView
+- **Views api**
+    - ObtainCustomAuthToken
+    - LogoutView
+    - ForgetPasswordView
+
+### Required ENV
+
+- ID_PHONE
+- PRODUCT_ID
+- TOKEN
---
+++ packages/loducode-utils/loducode_utils-0.0.0.tar.gz/loducode_utils-0.0.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='loducode_utils',
+    packages=['loducode_utils'],  # this must be the same as the name above
+    version='0.0.0',
+    description='Basic components for the development of loducode s.a.s.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Cristian Dulcey',
+    author_email='cristian@loducode.com',
+    url='https://github.com/UnTalDulcey/loducode_maytapi',  # use the URL to the github repo
+    download_url='https://github.com/UnTalDulcey/loducode_maytapi/tarball/0.1',
+    keywords=['utils', 'loducode_utils', 'loducode'],
+    classifiers=[],
+    install_requires=[i.strip() for i in open("requirements.txt").readlines()],
+    setup_requires=['wheel']
+)
