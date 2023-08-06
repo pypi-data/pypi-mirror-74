91a8a8bd2cb84d6bfb5bbef4a3ce9ed1783660cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:07 +0000

    Add package facex/0.0.7/facex-0.0.7.tar.gz

---
+++ packages/facex/facex-0.0.7.tar.gz/facex-0.0.7/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="facex", # Replace with your own username
+    version="0.0.7",
+    author="Hasby Fahrudin",
+    author_email="fahrudinhasby12@gmail.com",
+    description="Facial Expression Classifier API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/hfahrudin/facex",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    include_package_data=True,
+    install_requires=[
+        'scikit-image>=0.16.2'
+    ],
+    python_requires='>=3.6',
+)
