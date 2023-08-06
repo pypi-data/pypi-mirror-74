e5616b33dffcc984051b34f0ab1c8ca875e870e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:09 +0000

    Add package probab-dists/0.1/probab-dists-0.1.tar.gz

---
+++ packages/probab-dists/probab-dists-0.1.tar.gz/probab-dists-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='probab-dists',
+      version='0.1',
+      description='Gaussian, Binomial distributions',
+      packages=['probab-dists'],
+      zip_safe=False)
