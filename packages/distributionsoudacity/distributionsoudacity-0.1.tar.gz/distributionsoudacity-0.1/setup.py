b005964e6180eda5d6738d6bcb525dc900b6b665
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:42 +0000

    Add package distributionsoudacity/0.1/distributionsoudacity-0.1.tar.gz

---
+++ packages/distributionsoudacity/distributionsoudacity-0.1.tar.gz/distributionsoudacity-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributionsoudacity',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributionsoudacity'],
+      zip_safe=False)
