6cb3e71ca67fcbc42aba405705d4114c03191d4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:58 +0000

    Add package gauss-bin-prob-dist2020/0.1/gauss_bin_prob_dist2020-0.1.tar.gz

---
+++ packages/gauss-bin-prob-dist2020/gauss_bin_prob_dist2020-0.1.tar.gz/gauss_bin_prob_dist2020-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='gauss_bin_prob_dist2020',
+      version='0.1',
+      author='Sonia Agrawal',
+      description='Gaussian and Binomial distributions',
+      packages=['gauss_bin_prob_dist2020'],
+      zip_safe=False)
