944c57dcc006ec363c5a3992683982503081969e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:15:22 +0000

    Add package txtrader-client/1.5.3/txtrader-client-1.5.3.tar.gz

---
+++ packages/txtrader-client/txtrader-client-1.5.3.tar.gz/txtrader-client-1.5.3/txtrader_client/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+  txTrader Client Module
+
+  Copyright (c) 2017 Reliance Systems Inc. <mkrueger@rstms.net>
+  Licensed under the MIT license.  See LICENSE for details.
+
+"""
+
+from .cli import cli
+from .client import API
+from .version import VERSION as __version__
+from .version import DATE as __date__
+from . import defaults
