bcd124b7aef521126541bba69c834ffde4301f31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:57 +0000

    Add package basic-xl-distributions/1.0/basic_xl_distributions-1.0.tar.gz

---
+++ packages/basic-xl-distributions/basic_xl_distributions-1.0.tar.gz/basic_xl_distributions-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name = 'basic_xl_distributions',
+      version = '1.0',
+      description = 'Gaussian and Binomial distributions',
+      packages = ['basic_xl_distributions'],
+      author = 'Rocio Cruz Linares',
+      author_email = 'rocio.x.linares95@gmail.com',
+      zip_safe=False)
