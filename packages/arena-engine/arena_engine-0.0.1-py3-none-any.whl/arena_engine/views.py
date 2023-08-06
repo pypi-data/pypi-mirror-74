da1a9dff828e1afad4399e6fa16eb432abc7a2d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:59 +0000

    Add package arena-engine/0.0.1/arena_engine-0.0.1-py3-none-any.whl

---
+++ packages/arena-engine/arena_engine-0.0.1-py3-none-any.whl/arena_engine/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, unicode_literals
+
+from django.http import HttpResponse
+from django.views.generic import View
+
+
+class DeepThoughtView(View):
+
+    def get(self, request):
+        return HttpResponse(42)
