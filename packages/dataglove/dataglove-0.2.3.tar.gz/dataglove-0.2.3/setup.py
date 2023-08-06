ef427aa8feacb1407efa9cbd937217bcd905d268
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:52 +0000

    Add package dataglove/0.2.3/dataglove-0.2.3.tar.gz

---
+++ packages/dataglove/dataglove-0.2.3.tar.gz/dataglove-0.2.3/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="dataglove",
+    version="0.2.3",
+    author="BeBop Sensors",
+    author_email="code@bebopsensors.com",
+    description="64-bit Python Module of the DataGlove API.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    #url="",
+    packages=["dataglove"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Microsoft :: Windows",
+    ],
+	python_requires='>=3',
+	include_package_data=True,
+	package_data={"dataglove": ["DataGloveWindows.dll"]},
+)
