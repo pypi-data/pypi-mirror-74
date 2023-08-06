6b0e505b298b5ac08abf2f22da3e838eeb6411ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:58 +0000

    Add package rim-prob-dist/0.1/rim_prob_dist-0.1.tar.gz

---
+++ packages/rim-prob-dist/rim_prob_dist-0.1.tar.gz/rim_prob_dist-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='rim_prob_dist',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['rim_prob_dist'],
+      author='Andres Iglesias',
+      author_email='amir_andres@hotmail.com',
+      zip_safe=False)
