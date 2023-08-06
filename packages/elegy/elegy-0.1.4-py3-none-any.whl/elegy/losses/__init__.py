79474a01a984069d52e900b60ecf73e508887e0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:24 +0000

    Add package elegy/0.1.4/elegy-0.1.4-py3-none-any.whl

---
+++ packages/elegy/elegy-0.1.4-py3-none-any.whl/elegy/losses/__init__.py
@@ -0,0 +1,10 @@
+from .loss import Loss, Reduction
+from .categorical_crossentropy import CategoricalCrossentropy
+from .mean_squared_error import MeanSquaredError, mean_squared_error
+from .sparse_categorical_crossentropy import (
+    SparseCategoricalCrossentropy,
+    sparse_categorical_crossentropy,
+)
+
+from .mean_absolute_error import MeanAbsoluteError, mean_absolute_error
+from .binary_crossentropy import BinaryCrossentropy, binary_crossentropy
