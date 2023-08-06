f1fa898cc82270ec3db778dc502dbba5dcad04de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:15 +0000

    Add package karateclub/1.0.10/karateclub-1.0.10.tar.gz

---
+++ packages/karateclub/karateclub-1.0.10.tar.gz/karateclub-1.0.10/karateclub/utils/walker.py
@@ -0,0 +1,46 @@
+import random
+import networkx as nx
+
+class RandomWalker:
+    """
+    Class to do fast first-order random walks.
+
+    Args:
+        walk_length (int): Number of random walks.
+        walk_number (int): Number of nodes in truncated walk.
+    """
+    def __init__(self, walk_length: int, walk_number: int):
+        self.walk_length = walk_length
+        self.walk_number = walk_number
+
+    def do_walk(self, node):
+        """
+        Doing a single truncated random walk from a source node.
+
+        Arg types:
+            * **node** *(int)* - The source node of the diffusion.
+
+        Return types:
+            * **walk** *(list of strings)* - A single truncated random walk.
+        """
+        walk = [node]
+        for _ in range(self.walk_length-1):
+            nebs = [node for node in self.graph.neighbors(walk[-1])]
+            if len(nebs) > 0:
+                walk = walk + random.sample(nebs, 1)
+        walk = [str(w) for w in walk]
+        return walk
+
+    def do_walks(self, graph):
+        """
+        Doing a fixed number of truncated random walk from every node in the graph.
+
+        Arg types:
+            * **graph** *(NetworkX graph)* - The graph to run the random walks on.
+        """
+        self.walks = []
+        self.graph = graph
+        for node in self.graph.nodes():
+            for _ in range(self.walk_number):
+                walk_from_node = self.do_walk(node)
+                self.walks.append(walk_from_node)
