dfb804e335ba7b92aff8d3cb96d84905972706c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:38 +0000

    Add package maltego-trx/1.3.7/maltego-trx-1.3.7.tar.gz

---
+++ packages/maltego-trx/maltego-trx-1.3.7.tar.gz/maltego-trx-1.3.7/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+from maltego_trx import VERSION
+
+setup(name='maltego-trx',
+      version=VERSION,
+      description='Python library used to develop Maltego transforms',
+      url='https://github.com/paterva/maltego-trx/',
+      author='Maltego Staff',
+      author_email='support@maltego.com',
+      license='MIT',
+      install_requires=[
+       'flask>=1',
+       'six>=1',
+       'pycryptodome>=3.9.7'
+      ],
+      packages=[
+          'maltego_trx',
+          'maltego_trx/template_dir',
+          'maltego_trx/template_dir/transforms'
+      ],
+      entry_points={'console_scripts': [
+          'maltego-trx = maltego_trx.commands:execute_from_command_line',
+      ]},
+      zip_safe=False
+      )
