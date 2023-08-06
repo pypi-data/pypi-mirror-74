a5a24435b74d11f219ec79a5ddc947bea5518c1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:59 +0000

    Add package nodistributions/0.1/nodistributions-0.1.tar.gz

---
+++ packages/nodistributions/nodistributions-0.1.tar.gz/nodistributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='nodistributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['nodistributions'],
+      zip_safe=False)
