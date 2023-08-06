9469894ae941fd02902030c10b8bca6c54c2dc3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:21 +0000

    Add package arnobsprob-dist/0.1/arnobsprob_dist-0.1.tar.gz

---
+++ packages/arnobsprob-dist/arnobsprob_dist-0.1.tar.gz/arnobsprob_dist-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='arnobsprob_dist',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['arnobsprob_dist'],
+      author = 'Syed Sanzam',
+      zip_safe=False)
