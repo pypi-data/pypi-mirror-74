4b761645c51cc5ceb4b5bc58b780d040c532ba1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:13 +0000

    Add package stow/0.0.2/stow-0.0.2-py3-none-any.whl

---
+++ packages/stow/stow-0.0.2-py3-none-any.whl/stow/__init__.py
@@ -0,0 +1,20 @@
+from .artefacts import File, Directory
+from .utils import find, connect
+
+from .stateless import (
+    artefact,
+    exists,
+    basename,
+    dirname,
+    join,
+    touch,
+    mkdir,
+    localise,
+    open,
+    get,
+    put,
+    ls,
+    cp,
+    mv,
+    rm
+)
