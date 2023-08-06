f7db20a1945fe049fe08da8fd1d4d7c6cf25b59b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:01 +0000

    Add package datup/0.1.8-2/datup-0.1.8-2.tar.gz

---
+++ packages/datup/datup-0.1.8-2.tar.gz/datup-0.1.8-2/datup/__init__.py
@@ -0,0 +1,72 @@
+'''
+    The hub of datup libraries
+'''
+
+from datup.io.api import (
+    download_csv,
+    download_csvm,
+    upload_csv,
+    #profiling_report,
+    upload_model,
+    upload_log,
+    download_excel,
+    list_items,
+    delete_items,
+    get_model_name,
+    download_model
+)
+from datup.preparation.api import (
+    col_cast,
+    featureselection_correlation,
+    Error,
+    CorrelationError,
+    remove_chars_metadata,
+    to_lowercase_metadata,
+    profile_dataset,
+    replace_categorical_na,
+    replace_numeric_na,
+    replace_datetime_na
+)
+
+from datup.timeseries.api import (
+    compute_mae,
+    compute_maep,
+    compute_mape,
+    compute_mase,
+    compute_rmse,
+    compute_rmsep,
+    compute_smape,
+    ExponentialSmoothing
+)
+
+from datup.utils.api import (
+    filter_by_list,
+    antifilter_by_list,
+    reorder_columns,
+    check_data_format,
+    count_na,
+    cast_datetime,
+    parse_months,
+    transform_log_plus_one,
+    transform_positive_timeseries,
+    test_gaussianity,
+    test_acorr
+)
+
+from datup.core.api import (Datup)
+
+from datup.anonymization.api import (
+    data_in,
+    data_out,
+    variables_pii,
+    concatenar,
+    concatenar_id,
+    organizar,
+    profiling,
+    normalize,
+    observaciones,
+    organizar_observaciones,
+    eliminar_duplicados,
+    anonimizacion,
+    eliminacion
+)
