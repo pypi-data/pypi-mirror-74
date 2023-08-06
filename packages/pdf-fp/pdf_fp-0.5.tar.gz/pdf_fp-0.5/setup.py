7ecf3d555eb83a3d8e0cc738cc2df3d25037c50c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:06 +0000

    Add package pdf-fp/0.5/pdf_fp-0.5.tar.gz

---
+++ packages/pdf-fp/pdf_fp-0.5.tar.gz/pdf_fp-0.5/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='pdf_fp',
+      version='0.5',
+      description='Gaussian distributions',
+      packages=['pdf_fp'],
+      author = 'Abdelrahman Omran',
+      author_email = 'apdo.omran@gmail.com',
+      zip_safe=False)
