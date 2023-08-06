fd586cb805f9fd37edb1626f8f107ef19299b8d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:53 +0000

    Add package receipt-parser/0.0.10/receipt_parser-0.0.10-py3-none-any.whl

---
+++ packages/receipt-parser/receipt_parser-0.0.10-py3-none-any.whl/receipt_parser/__init__.py
@@ -0,0 +1,18 @@
+__version__ = '0.0.10'
+__license__ = 'MIT'
+
+
+from .receipt_parser import RuleBased
+from .finder import Finder
+from .normalizer import Normalizer
+# from . import receipt_parser, finder, normalizer
+
+# try:
+#     from .receipt_parser import RuleBased
+#     from .finder import Finder
+#     from .normalizer import Normalizer
+# except ModuleNotFoundError:
+#     from receipt_parser import RuleBased
+#     from finder import Finder
+#     from normalizer import Normalizer
+
