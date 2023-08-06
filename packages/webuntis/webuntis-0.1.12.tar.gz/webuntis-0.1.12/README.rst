34de86d087d9a41f231e710247d3403921cd11d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:28 +0000

    Add package webuntis/0.1.12/webuntis-0.1.12.tar.gz

---
+++ packages/webuntis/webuntis-0.1.12.tar.gz/webuntis-0.1.12/README.rst
@@ -0,0 +1,48 @@
+===============================================================================================================
+This project is now maintained again. It was originally written by `untitaker <https://github.com/untitaker>`_
+===============================================================================================================
+
+===============
+python-webuntis
+===============
+
+.. image:: https://travis-ci.org/maphy-psd/python-webuntis.png?branch=master
+    :target: https://travis-ci.org/github/AugustH/python-webuntis
+
+
+.. image:: https://coveralls.io/repos/github/AugustH/python-webuntis/badge.svg?branch=master
+     :target: https://coveralls.io/github/AugustH/python-webuntis?branch=master
+
+Bindings for WebUntis API
+=========================
+
+::
+
+    import webuntis
+
+    s = webuntis.Session(
+        username='api',
+        password='api',
+        server='webuntis.grupet.at:8080',
+        school='demo_inf',
+        useragent='WebUntis Test'
+    ).login()
+
+    for klasse in s.klassen():
+        print(klasse.name)
+
+
+`read more... <http://python-webuntis.readthedocs.org/en/latest/>`_
+
+Installation
+============
+
+::
+
+    pip install webuntis
+
+License
+=======
+
+``python-webuntis`` is released under the 3-clause BSD license, see ``LICENSE``
+for details.
---
+++ packages/webuntis/webuntis-0.1.12.tar.gz/webuntis-0.1.12/setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+"""
+    This file is part of python-webuntis
+
+    :copyright: (c) 2012 by Markus Unterwaditzer.
+    :license: BSD, see LICENSE for more details.
+"""
+
+from setuptools import setup, find_packages
+from sys import version_info
+
+dependencies = ['requests']
+
+setup(
+    name='webuntis',
+    version='0.1.12',
+    author='Markus Unterwaditzer',
+    author_email='markus@unterwaditzer.net',
+    packages=find_packages(),
+    include_package_data=True,
+    url='https://github.com/python-webuntis/python-webuntis',
+    license='new-style BSD',
+    description='Bindings for WebUntis API',
+    long_description_content_type='text/x-rst',
+    long_description=open('README.rst').read(),
+    install_requires=dependencies,
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: Implementation :: PyPy'
+    ]
+)
