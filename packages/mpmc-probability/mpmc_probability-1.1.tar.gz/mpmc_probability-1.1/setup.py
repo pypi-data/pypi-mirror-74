f82a101a093a7b547ba7f5d6093b84ae8cae0817
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:22 +0000

    Add package mpmc-probability/1.1/mpmc_probability-1.1.tar.gz

---
+++ packages/mpmc-probability/mpmc_probability-1.1.tar.gz/mpmc_probability-1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='mpmc_probability',
+      version='1.1',
+      description='Gaussian and Binomial Distributions',
+      packages=['mpmc_probability'],
+      author=['Muskaan Patel'],
+      author_email='muskaan.patel14@gmail.com',
+      zip_safe=False)
