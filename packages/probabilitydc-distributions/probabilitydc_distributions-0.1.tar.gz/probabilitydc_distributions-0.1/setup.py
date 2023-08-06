154cecad403035121d96c5e43fceeb33da0015d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:24 +0000

    Add package probabilitydc-distributions/0.1/probabilitydc_distributions-0.1.tar.gz

---
+++ packages/probabilitydc-distributions/probabilitydc_distributions-0.1.tar.gz/probabilitydc_distributions-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='probabilitydc_distributions',
+      version='0.1',
+      description='Probability discrete continous distributions',
+      packages=['probabilitydc_distributions'],
+      author='Harsha S',
+      zip_safe=False)
