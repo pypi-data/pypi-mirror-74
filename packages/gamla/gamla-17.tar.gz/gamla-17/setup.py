bd99c10bc3c99bf6b49ce8ced2db46a1eca486ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:44 +0000

    Add package gamla/17/gamla-17.tar.gz

---
+++ packages/gamla/gamla-17.tar.gz/gamla-17/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    _LONG_DESCRIPTION = fh.read()
+
+
+setuptools.setup(
+    name="gamla",
+    version="17",
+    long_description=_LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_namespace_packages(),
+    install_requires=[
+        "async-timeout",
+        "dataclasses_json",
+        "frozendict",
+        "heapq_max",
+        "httpx",
+        "pytest-asyncio",
+        "pytest>=5.4.0",
+        "requests",
+        "toolz",
+    ],
+)
