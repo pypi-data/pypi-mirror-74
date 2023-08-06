065827cc38f103674269e16dcd0fb04134f5f4ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:34 +0000

    Add package pytconf/0.0.44/pytconf-0.0.44.tar.gz

---
+++ packages/pytconf/pytconf-0.0.44.tar.gz/pytconf-0.0.44/pytconf/__init__.py
@@ -0,0 +1,9 @@
+from pytconf.param import ParamCreator
+from pytconf.config import (
+    Config,
+    register_main,
+    register_function,
+    register_function_group,
+    config_arg_parse_and_launch,
+    register_endpoint,
+)
