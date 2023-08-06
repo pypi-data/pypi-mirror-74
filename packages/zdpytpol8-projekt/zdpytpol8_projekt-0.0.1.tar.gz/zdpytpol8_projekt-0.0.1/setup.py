378701d1f921da2ebd301f83cfeb8f293a770606
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:47 +0000

    Add package zdpytpol8-projekt/0.0.1/zdpytpol8_projekt-0.0.1.tar.gz

---
+++ packages/zdpytpol8-projekt/zdpytpol8_projekt-0.0.1.tar.gz/zdpytpol8_projekt-0.0.1/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+
+from setuptools import find_packages, setup
+
+setup(
+	name='zdpytpol8_projekt',
+	version='0.0.1',
+	author='Krzysztof Lopatynski',
+	author_email='krzysztof.lopatynski@gmail.com',
+	packages=find_packages(),
+	include_package_data=True,
+	description='super useful library'
+)
