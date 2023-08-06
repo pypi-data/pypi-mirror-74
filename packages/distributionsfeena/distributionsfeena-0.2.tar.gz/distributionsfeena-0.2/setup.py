4255a9de16eef19bce694565ddd35d5a8d334495
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:54 +0000

    Add package distributionsfeena/0.2/distributionsfeena-0.2.tar.gz

---
+++ packages/distributionsfeena/distributionsfeena-0.2.tar.gz/distributionsfeena-0.2/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='distributionsfeena',
+      version='0.2',
+      description='Gaussian distributions',
+      packages=['distributions'],
+      author = ' Rafeena ',
+      zip_safe=False)
