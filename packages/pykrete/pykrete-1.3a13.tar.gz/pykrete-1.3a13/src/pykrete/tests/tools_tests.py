1005c3ce3a138051de401611dc93c10f629b286f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:38 +0000

    Add package pykrete/1.3a13/pykrete-1.3a13.tar.gz

---
+++ packages/pykrete/pykrete-1.3a13.tar.gz/pykrete-1.3a13/src/pykrete/tests/tools_tests.py
@@ -0,0 +1,25 @@
+"""
+Pykrete tools tests
+Author: Shai Bennathan - shai.bennathan@gmail.com
+(C) 2020
+"""
+import logging
+import unittest
+from pykrete.tools import VisualStudio
+
+
+class PykreteToolsTestCase(unittest.TestCase):
+    """Unit tests pykrete's tools
+    """
+
+    _logger = logging.getLogger(__name__)
+
+    def test_visual_studio(self):
+        """Verify creation and basic usage of the VisualStudio tool set
+        """
+        visual_studio = VisualStudio()
+        self._logger.info(visual_studio)
+        self._logger.info(visual_studio.build.path)
+        self._logger.info(visual_studio.build.run('-version', 'failed to get msbuild version'))
+        self._logger.info(visual_studio.test.path)
+        self._logger.info(visual_studio.test.run('/help', 'failed to get mstest help'))
---
+++ packages/pykrete/pykrete-1.3a13.tar.gz/pykrete-1.3a13/src/pykrete/tools/__init__.py
@@ -0,0 +1,8 @@
+"""
+Tool classes
+Author: Shai Bennathan - shai.bennathan@gmail.com
+(C) 2020
+"""
+from .visual_studio import VisualStudio
+
+__all__ = ['VisualStudio']
