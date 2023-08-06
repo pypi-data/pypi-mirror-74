610686a609ce5c6983cbcd9460d8b40d183524e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:11 +0000

    Add package prot/3.7.7/prot-3.7.7-py3-none-any.whl

---
+++ packages/prot/prot-3.7.7-py3-none-any.whl/prot/bs/__init__.py
@@ -0,0 +1,8 @@
+from .. import printMsg, printWarn, printErr, Database, LoopBack, ProtString
+
+from . import bs
+from . import bsMap
+from . import bsPro
+from . import bsSpaz
+
+__version__ = '1.1'
