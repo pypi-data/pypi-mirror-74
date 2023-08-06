220d4dd593480a0944ccdada87583aa43f3b0f0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:46 +0000

    Add package LaneDetection11/1.0.3/LaneDetection11-1.0.3-py3-none-any.whl

---
+++ packages/LaneDetection11/LaneDetection11-1.0.3-py3-none-any.whl/lanedetection/Config.py
@@ -0,0 +1,8 @@
+class ConfigTrain(object):
+    DEVICE = 'cuda'
+    EPOCH = 1
+    BATCH_SIZE = 4
+    OUT_SIZE = (1024,384)
+    CROP = 690
+    LR = 0.001
+    NUM_CLASSES = 8
---
+++ packages/LaneDetection11/LaneDetection11-1.0.3-py3-none-any.whl/lanedetection/model/__init__.py
@@ -0,0 +1,1 @@
+from .aspp import *
