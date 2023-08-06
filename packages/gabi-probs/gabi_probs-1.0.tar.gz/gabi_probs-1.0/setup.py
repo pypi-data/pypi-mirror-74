e15c4f94689c37b84412a8779c2435de929f3e64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:59 +0000

    Add package gabi-probs/1.0/gabi_probs-1.0.tar.gz

---
+++ packages/gabi-probs/gabi_probs-1.0.tar.gz/gabi_probs-1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='gabi_probs',
+      version='1.0',
+      description='Gaussian and Binomail distributions',
+      packages=['gabi_probs'],
+      author = 'Ceren Yaşar',
+      zip_safe=False)
