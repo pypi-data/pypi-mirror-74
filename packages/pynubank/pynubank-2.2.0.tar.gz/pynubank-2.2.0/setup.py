238a9741d8790226fef782391e960648ec5132b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:14:04 +0000

    Add package pynubank/2.2.0/pynubank-2.2.0.tar.gz

---
+++ packages/pynubank/pynubank-2.2.0.tar.gz/pynubank-2.2.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pynubank',
+    version='2.2.0',
+    url='https://github.com/andreroggeri/pynubank',
+    author='André Roggeri Campos',
+    author_email='a.roggeri.c@gmail.com',
+    license='MIT',
+    packages=find_packages(),
+    package_data={'pynubank': ['queries/*.gql']},
+    install_requires=['requests', 'qrcode', 'pyOpenSSL', 'colorama', 'requests-pkcs12'],
+    entry_points={
+        'console_scripts': [
+            'pynubank = pynubank.cli:main'
+        ]
+    }
+)
