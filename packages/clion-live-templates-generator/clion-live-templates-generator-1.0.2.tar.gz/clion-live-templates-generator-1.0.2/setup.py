3c56c5e209a2ed96a043b3ee59e1d5f4d4a8e019
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:50 +0000

    Add package clion-live-templates-generator/1.0.2/clion-live-templates-generator-1.0.2.tar.gz

---
+++ packages/clion-live-templates-generator/clion-live-templates-generator-1.0.2.tar.gz/clion-live-templates-generator-1.0.2/setup.py
@@ -0,0 +1,36 @@
+from setuptools import find_packages, setup
+from codecs import open
+from os import path
+
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='clion-live-templates-generator',
+
+    version='1.0.2',
+
+    license='MIT',
+
+    author='Keigo Habara',
+    author_email='k.habara.aa@gmail.com',
+
+    url='https://github.com/habara-k/clion-live-templates-generator',
+
+    description="Generate CLion's Live templates from a library of competitive programming",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+    ],
+
+    packages=find_packages(),
+
+    entry_points={
+        'console_scripts': [
+            'lt-generate = clion_live_templates_generator.main:main',
+        ],
+    },
+)
