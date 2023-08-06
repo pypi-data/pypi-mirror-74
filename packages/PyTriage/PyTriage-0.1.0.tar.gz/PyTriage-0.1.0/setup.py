7dcb23a703d638d4b006461abd76595ee887beac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:40 +0000

    Add package PyTriage/0.1.0/PyTriage-0.1.0.tar.gz

---
+++ packages/PyTriage/PyTriage-0.1.0.tar.gz/PyTriage-0.1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='PyTriage',
+    version='0.1.0',
+    packages=['pytriage'],
+    url='https://github.com/tyler-tee/pytriage',
+    license='GPLv3',
+    author='Tyler Talaga',
+    author_email='ttalaga@wgu.edu',
+    description='PyTriage is a Python library for interacting with Cofense Triage\'s v2 API.'
+)
