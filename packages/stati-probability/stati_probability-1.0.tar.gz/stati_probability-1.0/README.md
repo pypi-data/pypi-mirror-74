751dbb5fb77c590bc12ca90293e625543530413b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:43 +0000

    Add package stati-probability/1.0/stati_probability-1.0.tar.gz

---
+++ packages/stati-probability/stati_probability-1.0.tar.gz/stati_probability-1.0/README.md
@@ -0,0 +1,3 @@
+Probability Distribution
+A package to run General distributions like Gaussian and Binomial distributions
+
---
+++ packages/stati-probability/stati_probability-1.0.tar.gz/stati_probability-1.0/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='stati_probability',
+      version='1.0',
+      description='Gaussian distributions',
+      packages=['stati_probability'],
+      zip_safe=False)
