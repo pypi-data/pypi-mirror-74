82f053a921f2729673053cd8b7800382459a991f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:15 +0000

    Add package fake-bpy-module-2.82/20200724/fake-bpy-module-2.82-20200724.tar.gz

---
+++ packages/fake-bpy-module-2.82/fake-bpy-module-2.82-20200724.tar.gz/fake-bpy-module-2.82-20200724/bl_operators/__init__.py
@@ -0,0 +1,44 @@
+import sys
+import typing
+from . import uvcalc_follow_active
+from . import add_mesh_torus
+from . import object
+from . import image
+from . import presets
+from . import view3d
+from . import constraint
+from . import console
+from . import uvcalc_smart_project
+from . import userpref
+from . import anim
+from . import bmesh
+from . import uvcalc_lightmap
+from . import object_randomize_transform
+from . import wm
+from . import vertexpaint_dirt
+from . import mesh
+from . import object_align
+from . import freestyle
+from . import screen_play_rendered_anim
+from . import sequencer
+from . import object_quick_effects
+from . import clip
+from . import node
+from . import rigidbody
+from . import file
+
+
+def register():
+    ''' 
+
+    '''
+
+    pass
+
+
+def unregister():
+    ''' 
+
+    '''
+
+    pass
---
+++ packages/fake-bpy-module-2.82/fake-bpy-module-2.82-20200724.tar.gz/fake-bpy-module-2.82-20200724/bpy/ops/__init__.py
@@ -0,0 +1,106 @@
+import sys
+import typing
+from . import sequencer
+from . import import_curve
+from . import scene
+from . import buttons
+from . import camera
+from . import view2d
+from . import pose
+from . import export_mesh
+from . import graph
+from . import screen
+from . import file
+from . import paintcurve
+from . import collection
+from . import sound
+from . import material
+from . import constraint
+from . import fluid
+from . import uv
+from . import node
+from . import rigidbody
+from . import info
+from . import action
+from . import import_scene
+from . import armature
+from . import texture
+from . import workspace
+from . import world
+from . import outliner
+from . import dpaint
+from . import curve
+from . import import_mesh
+from . import export_scene
+from . import palette
+from . import script
+from . import poselib
+from . import cloth
+from . import export_anim
+from . import paint
+from . import anim
+from . import gizmogroup
+from . import mball
+from . import sculpt
+from . import preferences
+from . import boid
+from . import ui
+from . import marker
+from . import gpencil
+from . import cycles
+from . import console
+from . import surface
+from . import mesh
+from . import import_anim
+from . import font
+from . import wm
+from . import object
+from . import render
+from . import transform
+from . import cachefile
+from . import nla
+from . import safe_areas
+from . import clip
+from . import mask
+from . import brush
+from . import text
+from . import view3d
+from . import particle
+from . import ptcache
+from . import lattice
+from . import ed
+from . import image
+
+
+class BPyOps:
+    pass
+
+
+class BPyOpsSubMod:
+    pass
+
+
+class BPyOpsSubModOp:
+    def get_rna_type(self):
+        ''' 
+
+        '''
+        pass
+
+    def idname(self):
+        ''' 
+
+        '''
+        pass
+
+    def idname_py(self):
+        ''' 
+
+        '''
+        pass
+
+    def poll(self, args):
+        ''' 
+
+        '''
+        pass
