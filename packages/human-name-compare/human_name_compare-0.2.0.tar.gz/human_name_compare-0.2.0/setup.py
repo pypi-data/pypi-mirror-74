0a3020243f60e98e7da1b7a3eb5e42e7ee0afa7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:42 +0000

    Add package human-name-compare/0.2.0/human_name_compare-0.2.0.tar.gz

---
+++ packages/human-name-compare/human_name_compare-0.2.0.tar.gz/human_name_compare-0.2.0/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='human_name_compare',
+    version='0.2.0',
+    scripts=['hn-compare'],
+    author="Robert Schoenthal",
+    author_email="robert.schoenthal@gmail.com",
+    description="a tool for comparing human names",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/digitalkaoz/py_human_name_compare",
+    download_url="https://github.com/digitalkaoz/py_human_name_compare/archive/0.2.tar.gz",
+    packages=setuptools.find_packages(),
+    keywords=['NLP', 'HUMANNAME'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
