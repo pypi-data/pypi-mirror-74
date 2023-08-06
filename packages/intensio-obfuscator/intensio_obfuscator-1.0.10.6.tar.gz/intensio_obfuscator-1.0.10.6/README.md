df8c9197bf16799241c9c9c1b8c7fecc202e22f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:49 +0000

    Add package intensio-obfuscator/1.0.10.6/intensio_obfuscator-1.0.10.6.tar.gz

---
+++ packages/intensio-obfuscator/intensio_obfuscator-1.0.10.6.tar.gz/intensio_obfuscator-1.0.10.6/README.md
@@ -0,0 +1,105 @@
+# Intensio-Obfuscator
+
+# Project not maintained anymore
+# Thanks to all contributors
+
+![](https://img.shields.io/badge/Python->=3.5-green.svg)
+[![PyPI Version](https://img.shields.io/pypi/v/intensio-obfuscator.svg)](https://pypi.python.org/pypi/intensio-obfuscator/)
+![](https://img.shields.io/badge/Licence-MIT-red.svg)
+
+## What is this ?
+- Intensio-Obfsucator tool takes a python source code and transform it into an obfuscated python code
+  - **Replace** all names of `variables/classes/functions/files-name` to random strings with length defined then all `chars` to their hexadecimal value
+  - **Delete** all `comments`, all `spaces lines`
+  - **Padding** random `snippets code/functions/classes` with an always differents values
+  
+## What purpose ?
+- Provides a high level obfuscation layer to prevent or delay the reading and understanding of your python program
+
+## Level of obfuscation
+- Weak obfuscation if used alone, can be used with other types of obfuscation
+
+## Requirements
+- Python >= 3.5
+- requirements.txt
+
+## Files supported
+- Files written in python 2.x and 3.x 
+
+## Installation
+- From source
+```sh
+$ git clone https://github.com/Hnfull/Intensio-Obfuscator.git
+$ python3 -m pip install -r Intensio-Obfuscator/requirements.txt
+$ cd Intensio-Obfuscator/src/intensio_obfuscator
+$ python3 intensio_obfuscator.py --help
+```
+- From python package
+```sh
+$ python3 -m pip install intensio-obfuscator
+$ intensio_obfuscator --help
+```
+
+## Features
+| Features | Descriptions | Purpose of obfuscation | Compatibility with all types of python codes/syntaxes |
+| ------ | ------ | ------ | ------ |
+| Delete comments | Delete all comments (this feature is executed by default) | Delete potential behavioral informations | high `python files 2 & 3`|
+| Delete line spaces | Delete all spaces line (this feature is executed by default) | Reduce the code visibility in clear | high `python files 2 & 3`|
+| Correction padding empty classes/functions | Add padding to empty classes and functions, if the class or function contains comments only, the default feature `Delete comments` can potentially let a class or function empty, this will avoid to generate an error (this feature is executed by default) | None, only to avoid to generate errors | high `python files 2 & 3`|
+| Replace string to string mixed | Replace all names of variables/classes/functions to random strings with length defined| Reduce the code visibility in clear - Delay the deduction of the behavior of variables/classes/functions | low - high (depends of number of names that must exclude or not) `python files 2 & 3`
+| Exclude words | file to exclude word (check documentation for the format) only for 'replace file name' obfuscation feature | Information not required | Information not required |
+| Padding script | Add padding of random scripts after each line| Reduce the code visibility in clear - add dead snippets code/classes/functions to blur and delay behavior analysis of program | high `python file 2 & 3`|
+| Replace files name | Replace all files name to random strings with length defined | Reduce the code visibility in clear - Reduce the deduction of functionnalities of files | low `python files 2 & 3`|
+| Exclude file names | file to exclude file names (check documentation for the format) only for 'replace file name' obfuscation feature | Information not required | Information not required |
+| Replace string to hex | Replace all chars to their hexadecimal value | Reduce the code visibility in clear / avoid to be detected by the \'grep\' commands per example| medium `python files 2 only` |
+| Correction delete pyc file | Delete all pyc file in output directory (this feature is executed by default) | Delete files already compiled without having been obfuscated before | high `python files 2 & 3`|
+| Mixer length lower | Define random strings length of `32` chars when `-rts, --replacetostr` or `-ps, --paddingscripts` or `-rfn, --replacefilesname` or `-rth, --replacetohex` parameters are specified | The longer the length is used, the more difficult the visibility of the code | Information not required |
+| Mixer length medium | Define random strings length of `64` chars when `-rts, --replacetostr` or `-ps, --paddingscripts` or `-rfn, --replacefilesname` or `-rth, --replacetohex` parameters are specified | The longer the length is used, the more difficult the visibility of the code | Information not required |
+| Mixer length high | Define random strings length of `128` chars when `-rts, --replacetostr` or `-ps, --paddingscripts` or `-rfn, --replacefilesname` or `-rth, --replacetohex` parameters are specified | The longer the length is used, the more difficult the visibility of the code | Information not required |
+- Features can be executed separatly:
+    - `replace string to string mixed` -> `-rts, --replacetostr`
+    - `padding script` -> `-ps, --paddingscript`
+    - `replace file name` -> `-rfn, --replacefilename`
+    - `replace string to hex` -> `-rth, --replacetohex`
+
+## Usages
+- **Read these Documentations before to use Intensio-Obfuscator tool !**
+- **Certain types of pattern are not supported**
+    - [Steps of usage](docs/steps_usage/python_steps_usage.md)
+    - [Required code format](docs/recommendations/python_code_recommendations.md)
+    - [Malfunctions](docs/malfunctions/python_code_malfunctions.md)
+    
+| Parameters | Descriptions |
+| ------ | ------ |
+| -h, --help | show this help message and exit |
+| -i, --input  | source directory - indicate a directory that contain your file |
+| -o, --output | output directory that will be obfuscated - indicate a empty directory that will contain your file |
+| -mlen, --mixerlength | define length of random strings generated [`lower:32`\|`medium:64`\|`high:128`] chars when `--replacetostr` or `--paddingscripts` or `-rfn, --replacefilesname` or `--replacetohex` features are specified, possible values: [`lower`\|`medium`\|`high`]|
+|-ind, --indent | indicate the indentation of your python source code, possible values: [`2`\|`4`\|`8`] 
+| -rts, --replacetostr | enable `replace string to string mixed` obfuscation feature |
+| --excludewords | file to exclude specific words (check documentation for the format) only for `-rts, --replacetostr` obfuscation feature |
+| -ps, --paddingscript | enable `padding script` obfuscation feature|
+| -rfn, --replacefilename | enable `replace file name` obfuscation feature |
+| --excludefiles | file to exclude file name (check documentation for the format) only for `-rfn, --replacefilename` obfuscation feature |
+| -rth, --replacetohex | enable `replace string to hex` obfuscation `(python files 2 only)`|
+| --version | check current version|
+| -v, --verbose | improve verbosity |
+
+## Obfuscation examples 
+- [Python files obfuscated](docs/examples/python_code_examples.md)
+
+## Todo
+- Version 1.x.x-x:
+    - Code optimization
+    - Fix issues
+    - Improved features already present
+    - Add other functionalities
+
+## License
+- MIT
+
+## Contact
+- Hnfull **gitland@protonmail.com**
+
+## Disclamer
+- Intensio-Obfuscator is for education/research purposes only. The author takes NO responsibility ay for how you choose to use any of the tools provided
---
+++ packages/intensio-obfuscator/intensio_obfuscator-1.0.10.6.tar.gz/intensio_obfuscator-1.0.10.6/src/intensio_obfuscator/core/obfuscation/intensio_analyze.py
@@ -0,0 +1,269 @@
+# -*- coding: utf-8 -*-
+
+# https://github.com/Hnfull/Intensio-Obfuscator
+
+#---------------------------------------------------------- [Lib] -----------------------------------------------------------#
+
+import shutil
+import os
+import sys
+import glob
+import re
+from progress.bar import Bar
+
+try:
+    from intensio_obfuscator.core.utils.intensio_utils import Utils, Colors
+except ModuleNotFoundError:
+    from core.utils.intensio_utils import Utils, Colors
+
+#------------------------------------------------- [Function(s)/Class(es)] --------------------------------------------------#
+
+class Analyze:
+    
+    def __init__(self):
+        self.utils = Utils()
+
+
+    def InputAvailable(self, inputArg, verboseArg):
+        inputFileFound      = []
+        inputFileEmpty      = []
+        inputFileFoundCount = 0
+        inputFileEmptyCount = 0
+        countRecursFiles    = 0 
+        numberLine          = 0
+
+        if os.path.exists(inputArg) == True:
+            if os.path.isdir(inputArg) == True:     
+                detectFiles = "py"
+                blockDir    = "__pycache__"
+
+                recursFiles = [
+                                file for file in glob.glob("{0}{1}**{1}*.{2}".format(
+                                                                                    inputArg, 
+                                                                                    self.utils.Platform(
+                                                                                                        getOS=False, 
+                                                                                                        getPathType=True
+                                                                                    ), 
+                                                                                    detectFiles), 
+                                                                                    recursive=True
+                                                                            )
+                ]
+ 
+                if recursFiles == []:
+                    print(Colors.ERROR + "[-] {} directory empty or python file not found".format(inputArg) + Colors.DISABLE)
+                    return 1
+
+                for number in recursFiles:
+                    countRecursFiles += 1
+                
+                print("\n[+] Running analyze input of {} file(s)...\n".format(countRecursFiles))
+                
+                with Bar("Analysis    ", fill="=", max=countRecursFiles, suffix="%(percent)d%%") as bar:
+                    for file in recursFiles:
+                        if blockDir in file:
+                            continue
+                        else:
+                            if os.path.getsize(file) > 0:
+                                inputFileFound.append(file)
+                                inputFileFoundCount += inputFileFoundCount + 1
+                            else:
+                                inputFileEmpty.append(file)
+                                inputFileEmptyCount += inputFileEmptyCount + 1
+
+                        bar.next(1)
+                    bar.finish()
+
+                    if inputFileFoundCount >= 1 and inputFileEmptyCount < inputFileFoundCount:
+                        if verboseArg:
+                            print("\n\n[+] File input found :\n")
+                            
+                            if inputFileFound == []:
+                                print("-> no result")
+                            else:
+                                for file in inputFileFound:
+                                    print("-> {}".format(file))
+                        
+                                for file in inputFileEmpty:
+                                    print("-> {} : empty".format(file))
+                    
+                        return 1
+                
+                    elif inputFileFoundCount == inputFileEmptyCount and inputFileFoundCount > 0:
+                        print(Colors.ERROR + "[-] All files in directory specified are emtpy !" + Colors.DISABLE)
+                        return 0
+                    else:
+                        print(Colors.ERROR + "[-] No file available in '{}'.".format(inputArg) + Colors.DISABLE)
+                        return 0
+            else:
+                print(Colors.ERROR + "[-] '{}' is not a directory".format(inputArg) + Colors.DISABLE)
+                return 0
+        else:
+            print(Colors.ERROR + "[-] '{}' not exists".format(inputArg) + Colors.DISABLE)
+            return 0
+
+
+    def OutputAvailable(self, inputArg, outputArg, verboseArg):
+        outputFileFound         = []
+        outputFileEmpty         = []
+        outputFileFoundCount    = 0
+        outputFileEmptyCount    = 0
+        countRecursFiles        = 0
+
+        if os.path.exists(outputArg) == True:
+            deleteRequest = input("[!] Output '{}' already exists, do you want delete it ? (Y/N) : ".format(outputArg))
+            deleteRequest = deleteRequest.upper()
+            if deleteRequest == "Y":
+                try:
+                    shutil.rmtree(outputArg)
+                    if os.path.exists(outputArg) == False:
+                        shutil.copytree(inputArg, outputArg)
+                        if os.path.exists(outputArg) == True:
+                            if os.path.isdir(outputArg) == True:
+                                detectFiles = "py"
+                                blockDir    = "__pycache__"
+
+                                recursFiles = [
+                                                file for file in glob.glob("{0}{1}**{1}*.{2}".format(
+                                                                                                    outputArg, 
+                                                                                                    self.utils.Platform(
+                                                                                                                getOS=False, 
+                                                                                                                getPathType=True
+                                                                                                    ), 
+                                                                                                    detectFiles), 
+                                                                                                    recursive=True
+                                                                                            )
+                                ]
+
+                                for number in recursFiles:
+                                    countRecursFiles += 1
+
+                                print("\n[+] Running analyze output of {} file(s)...\n".format(countRecursFiles))
+
+                                if recursFiles == []:
+                                    print(Colors.ERROR + "[-] {} directory empty, no copied file".format(inputArg) + \
+                                        Colors.DISABLE)
+                                    return 0
+                                                                
+                                with Bar("Analysis    ", fill="=", max=countRecursFiles, suffix="%(percent)d%%") as bar:
+                                    for file in recursFiles:
+                                        if blockDir in file:
+                                            continue
+                                        else:
+                                            if os.path.getsize(file) > 0:
+                                                outputFileFound.append(file)
+                                                outputFileFoundCount += outputFileFoundCount + 1
+                                            else:
+                                                outputFileEmpty.append(file)
+                                                outputFileEmptyCount += outputFileEmptyCount + 1
+
+                                        bar.next(1)
+                                    bar.finish()
+                                
+                                    if outputFileFoundCount >= 1 and outputFileFoundCount > outputFileEmptyCount:
+                                        if verboseArg:
+                                            print("\n\n[+] Output files copy :\n")
+
+                                            if outputFileFound == []:
+                                                print("-> no result")
+                                            else:
+                                                for file in outputFileFound:
+                                                    print("-> {}".format(file))
+                                            
+                                                for file in outputFileEmpty:
+                                                    print("-> {} : empty".format(file))
+                                                
+                                        return 1
+                                    else:
+                                        print(Colors.ERROR + "[-] No files available in '{}'".format(outputArg) + Colors.DISABLE)
+                                        return 0
+                            else:
+                                print(Colors.ERROR + "[-] Copy '{}' to '{}' failed, this is not a output directory copied"\
+                                    .format(inputArg, outputArg) + Colors.DISABLE)
+                                return 0
+                        else:
+                            print(Colors.ERROR + "[-] Copy '{}' to '{}' failed".format(inputArg, outputArg) + Colors.DISABLE)
+                            return 0
+                    else:
+                        print(Colors.ERROR + "[-] Delete '{}' failed".format(outputArg) + Colors.DISABLE)
+                        return 0
+
+                except Exception as e:
+                    print(Colors.ERROR + "[-] {}".format(e) + Colors.DISABLE)
+                    return 0
+            else:
+                print(Colors.ERROR + "[-] Delete '{}' failed, the user has refused".format(outputArg) + Colors.DISABLE)
+                return 0
+        else:    
+            try:
+                shutil.copytree(inputArg, outputArg)
+                if os.path.exists(outputArg) == True:
+                    if os.path.isdir(outputArg) == True:
+                        detectFiles = "py"
+                        blockDir    = "__pycache__"
+
+                        recursFiles = [
+                                        file for file in glob.glob("{0}{1}**{1}*.{2}".format(
+                                                                                            outputArg, 
+                                                                                            self.utils.Platform(
+                                                                                                                getOS=False, 
+                                                                                                                getPathType=True
+                                                                                            ), 
+                                                                                            detectFiles), 
+                                                                                            recursive=True
+                                                                                    )
+                        ]
+
+                        for number in recursFiles:
+                            countRecursFiles += 1
+
+                        print("\n[+] Running analyze output of {} file(s)...\n".format(countRecursFiles))
+
+                        if recursFiles == []:
+                            print(Colors.ERROR + "[-] {} directory empty, no copied file".format(inputArg) + Colors.DISABLE)
+                            return 0
+                                                
+                        with Bar("Analysis    ", fill="=", max=countRecursFiles, suffix="%(percent)d%%") as bar:
+                            for file in recursFiles:
+                                if blockDir in file:
+                                    continue
+                                else:
+                                    if os.path.getsize(file) > 0:
+                                        outputFileFound.append(file)
+                                        outputFileFoundCount += outputFileFoundCount + 1
+                                    else:
+                                        outputFileEmpty.append(file)
+                                        outputFileEmptyCount += outputFileEmptyCount + 1
+
+                                bar.next(1)
+                            bar.finish()    
+
+                            if outputFileFoundCount >= 1 and outputFileFoundCount > outputFileEmptyCount:
+                                if verboseArg:
+                                    print("\n\n[+] File output found :\n")
+
+                                    if outputFileFound == []:
+                                        print("-> no result")
+                                    else:
+                                        for file in outputFileFound:
+                                            print("-> {} : copy".format(file))
+                                    
+                                        for file in outputFileEmpty:
+                                            print("-> {} : copy empty".format(file))
+                                
+                                return 1
+                            else:
+                                print(Colors.ERROR + "[-] No files available in '{}'".format(outputArg) + Colors.DISABLE)
+                                return 0                
+                    else:
+                        print(Colors.ERROR + "[-] Copy '{}' to '{}' failed, this is not a output directory copied !"\
+                            .format(inputArg, outputArg) + Colors.DISABLE)
+                        return 0
+                else:
+                    print(Colors.ERROR + "[-] Copy '{}' to '{}' failed".format(inputArg, outputArg) + Colors.DISABLE)
+                    return 0
+
+            except Exception as e:
+                print(Colors.ERROR + "[-] {}".format(e) + Colors.DISABLE)
+                return 0
+
+            
