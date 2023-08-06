d1096d049dac1176b6befddc1c792efd8e5d7452
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:15 +0000

    Add package BI-parser/0.0.15/BI_parser-0.0.15.tar.gz

---
+++ packages/BI-parser/BI_parser-0.0.15.tar.gz/BI_parser-0.0.15/src/classes/password.py
@@ -0,0 +1,9 @@
+def password():
+    x = 0
+    while x != "BIcode":
+        x = input('password : ')
+        if x == "BIcode":
+            print('ok')
+        else:
+            print("Wrong Password")
+
