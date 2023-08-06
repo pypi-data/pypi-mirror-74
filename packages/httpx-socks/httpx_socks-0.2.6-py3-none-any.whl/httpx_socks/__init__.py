5144ed6ddcc1de6faba5ca2a09e6380a1b383a9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:35 +0000

    Add package httpx-socks/0.2.6/httpx_socks-0.2.6-py3-none-any.whl

---
+++ packages/httpx-socks/httpx_socks-0.2.6-py3-none-any.whl/httpx_socks/__init__.py
@@ -0,0 +1,23 @@
+__title__ = 'httpx-socks'
+__version__ = '0.2.6'
+
+from .core_socks import (
+    ProxyError,
+    ProxyTimeoutError,
+    ProxyConnectionError,
+    ProxyType
+)
+
+from ._sync_transport import SyncProxyTransport
+from ._async_transport import AsyncProxyTransport
+
+__all__ = (
+    '__title__',
+    '__version__',
+    'SyncProxyTransport',
+    'AsyncProxyTransport',
+    'ProxyError',
+    'ProxyTimeoutError',
+    'ProxyConnectionError',
+    'ProxyType',
+)
