c433aa61ceec77493af19769f7a4acb4f8ab93c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:12 +0000

    Add package dist-prob-g-n/1.1/dist_prob_g_n-1.1.tar.gz

---
+++ packages/dist-prob-g-n/dist_prob_g_n-1.1.tar.gz/dist_prob_g_n-1.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='dist_prob_g_n',
+      version='1.1',
+      description='Gaussian and binomial distributions',
+      packages=['dist_prob_g_n'],
+      author ='Sayana V',
+      zip_safe=False)
