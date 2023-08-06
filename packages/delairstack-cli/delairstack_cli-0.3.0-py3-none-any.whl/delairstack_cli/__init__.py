2c7befcacbf8b8e422911f331afddc52860708db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:50 +0000

    Add package delairstack-cli/0.3.0/delairstack_cli-0.3.0-py3-none-any.whl

---
+++ packages/delairstack-cli/delairstack_cli-0.3.0-py3-none-any.whl/delairstack_cli/__init__.py
@@ -0,0 +1,1 @@
+__version__ = '0.3.0'  # must match the version in pyproject.toml
---
+++ packages/delairstack-cli/delairstack_cli-0.3.0-py3-none-any.whl/delairstack_cli/main.py
@@ -0,0 +1,23 @@
+import typer
+
+from delairstack_cli import analytics, config, credentials, products
+
+
+app = typer.Typer()
+app.add_typer(analytics.app, name='analytics', help='Interact with Analytics')
+app.add_typer(
+    credentials.app,
+    name='credentials',
+    help='Interact your Docker registry credentials'
+)
+app.add_typer(products.app, name='products', help='Interact with Products')
+
+
+@app.command()
+def configure():
+    """ Configure your credentials to connect to the platform """
+    config.setup()
+
+
+if __name__ == "__main__":
+    app()
