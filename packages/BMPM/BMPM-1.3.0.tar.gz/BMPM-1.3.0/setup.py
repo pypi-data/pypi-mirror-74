df922c6c85de754983c24b03222e4fa2c6f87145
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:15 +0000

    Add package BMPM/1.3.0/BMPM-1.3.0.tar.gz

---
+++ packages/BMPM/BMPM-1.3.0.tar.gz/BMPM-1.3.0/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as desc:
+    long_description = desc.read()
+
+setuptools.setup(
+    name="BMPM",
+    version="1.3.0",
+    author="SDarkMagic",
+    author_email="TheSDarkMagic@gmail.com",
+    description="A program for bulk replacement of BYML map file parameters.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/SDarkMagic/BMPM",
+    packages=setuptools.find_packages(),
+    entry_points={
+        'console_scripts': ['bmpm=bmpm.__main__:main']
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+    install_requires=[
+        "oead>=1.1.1",
+    ],
+)
