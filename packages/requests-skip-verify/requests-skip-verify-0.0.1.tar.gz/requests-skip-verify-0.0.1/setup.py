2d8b900ad09d29e6cfba6eb3519caae9431dfe6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:45 +0000

    Add package requests-skip-verify/0.0.1/requests-skip-verify-0.0.1.tar.gz

---
+++ packages/requests-skip-verify/requests-skip-verify-0.0.1.tar.gz/requests-skip-verify-0.0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+if __name__ == "__main__":
+    setup(
+        name="requests-skip-verify",
+        packages=find_packages("src"),
+        package_dir={"": "src"},
+        version="0.0.1",
+        author="Tsuuko",
+        description="Skip SSL verification for requests.",
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        license="MIT",
+        classifiers=["Programming Language :: Python :: 3", "License :: OSI Approved :: MIT License", "Operating System :: OS Independent"],
+    )
