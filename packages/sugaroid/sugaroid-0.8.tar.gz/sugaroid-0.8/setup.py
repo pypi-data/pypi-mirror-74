13b24ad1b35179ae1d510d618ee4ac004d9ec127
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:32 +0000

    Add package sugaroid/0.8/sugaroid-0.8.tar.gz

---
+++ packages/sugaroid/sugaroid-0.8.tar.gz/sugaroid-0.8/setup.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+"""
+MIT License
+
+Copyright (c) 2020 Srevin Saju
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+
+
+"""
+
+from setuptools import setup
+import os
+import platform
+
+try:
+    this_directory = os.path.abspath(os.path.dirname(__file__))
+    with open(
+        os.path.join(this_directory, 'README.md'),
+        encoding='utf-8'
+    ) as f:
+        long_description = f.read()
+except FileNotFoundError:
+    long_description = \
+        "Sugaroid Bot by @srevinsaju"
+
+requirements = ['PyQt5>=5.14,<5.16', 'pynput']
+if platform.system() == 'Windows':
+    requirements.extend(['pywin32', 'psutil'])
+elif platform.system() == 'Linux':
+    requirements.extend(['fonttools', 'psutil', 'cairosvg'])
+
+
+setup(
+    name='sugaroid',
+    version='0.8',
+    description='Open Source Natural Language Processing Bot.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license='MIT',
+    author='srevinsaju',
+    author_email="srevin03@gmail.com",
+    packages=['sugaroid', 'sugaroid.brain', 'sugaroid.gui', 'sugaroid.cli', 'sugaroid.config', 'sugaroid.reader', 'sugaroid.config', 'sugaroid.game', 'sugaroid.web', 'sugaroid.trivia', 'sugaroid.platform', 'sugaroid.google', 'sugaroid.translator', 'sugaroid.tts', 'sugaroid.trainer'],
+    url="https://srevinsaju.github.io/sugaroid",
+    download_url="https://github.com/srevinsaju/sugaroid/archive/master.zip",
+    package_data={'sugaroid': ['data/*', 'gui/ui/*']},  # noqa: E501
+    include_package_data=True,
+    install_requires=requirements,
+    entry_points={'console_scripts': ['sugaroid = sugaroid.sugaroid:main']},  # noqa: E501
+    classifiers=[
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.8',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)'
+    ],
+)
---
+++ packages/sugaroid/sugaroid-0.8.tar.gz/sugaroid-0.8/sugaroid/brain/joke.py
@@ -0,0 +1,61 @@
+"""
+MIT License
+
+Sugaroid Artificial Intelligence
+Chatbot Core
+Copyright (c) 2020 Srevin Saju
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+"""
+
+import pyjokes
+from chatterbot.logic import LogicAdapter
+from sugaroid.sugaroid import SugaroidStatement
+from sugaroid.brain.ooo import Emotion
+from sugaroid.brain.preprocessors import normalize
+
+
+class JokeAdapter(LogicAdapter):
+    """
+    Gets a random joke from the Chuck Norris Database
+    """
+
+    def __init__(self, chatbot, **kwargs):
+        super().__init__(chatbot, **kwargs)
+
+    def can_process(self, statement):
+        normalized = normalize(str(statement).lower())
+        if (('tell' in normalized) or ('say' in normalized) or ('crack' in normalized)) and ('joke' in normalized):
+            return True
+        elif (len(normalized) == 1) and (self.chatbot.lp.similarity("joke", str(statement).lower()) >= 0.9):
+            return True
+        elif 'joke' in normalized:
+            return True
+        else:
+            return False
+
+    def process(self, statement, additional_response_selection_parameters=None):
+        joke = pyjokes.get_joke('en', 'all')
+        selected_statement = SugaroidStatement(joke, chatbot=True)
+        selected_statement.confidence = 0.95
+
+        emotion = Emotion.lol
+        selected_statement.emotion = emotion
+        return selected_statement
