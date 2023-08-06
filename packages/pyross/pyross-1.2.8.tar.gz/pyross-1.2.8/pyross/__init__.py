f6fbb03676a00b074af55ad03c74de26302f7e28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:53 +0000

    Add package pyross/1.2.8/pyross-1.2.8.tar.gz

---
+++ packages/pyross/pyross-1.2.8.tar.gz/pyross-1.2.8/pyross/__init__.py
@@ -0,0 +1,13 @@
+import pyross.contactMatrix
+import pyross.control
+import pyross.deterministic
+import pyross.forecast
+import pyross.hybrid
+import pyross.inference
+import pyross.stochastic
+import pyross.utils
+import pyross.utils_python 
+
+## current version of PyRoss
+__version__="1.2.8"
+
