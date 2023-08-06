10e5640864a5b578d85c1f602516c898ebfa151c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:50 +0000

    Add package sphinx-click/2.4.0/sphinx-click-2.4.0.tar.gz

---
+++ packages/sphinx-click/sphinx-click-2.4.0.tar.gz/sphinx-click-2.4.0/docs/usage.rst
@@ -0,0 +1,158 @@
+Usage
+=====
+
+To enable the plugin, add the extension to the list of extensions in your
+Sphinx `conf.py` file:
+
+.. code-block:: python
+
+   extensions = ['sphinx_click.ext']
+
+Once enabled, *sphinx-click* enables automatic documentation for
+`click-based`_ applications by way of a `Sphinx directive`_.
+
+.. rst:directive:: .. click:: module:parser
+
+   Automatically extract documentation from a `click-based`_ application and
+   include it in your docs.
+
+   .. code-block:: rst
+
+       .. click:: module:parser
+          :prog: hello-world
+          :show-nested:
+
+   The directive takes the import name of a *click* object as its sole
+   argument. This should be a subclass of |click.core.BaseCommand|_, such as
+   ``click.Command``, ``click.Group``, ``click.MultiCommand``, etc.
+
+   In addition, the following options are required:
+
+   ``:prog:``
+     The name of your tool (or how it should appear in your documentation). For
+     example, if you run your script as ``./boo --opts args`` then ``:prog:``
+     will be ``boo``. If this is not given, the module name is used.
+
+   The following options are optional:
+
+   ``:show-nested:``
+     Enable full documentation for sub-commands.
+
+   ``:commands:``
+     Document only listed commands.
+
+   The generated documentation includes anchors for the generated commands,
+   their options and their environment variables using the `Sphinx standard
+   domain`_.
+
+.. _Sphinx directive: http://www.sphinx-doc.org/en/stable/extdev/markupapi.html
+.. _click-based: http://click.pocoo.org/6/
+.. _Sphinx standard domain: http://www.sphinx-doc.org/en/stable/domains.html#the-standard-domain
+.. |click.core.BaseCommand| replace:: ``click.core.BaseCommand``
+.. _click.core.BaseCommand: http://click.pocoo.org/6/api/#click.BaseCommand
+
+Example
+-------
+
+Take the below ``click`` application, which is defined in the ``hello_world``
+module:
+
+.. code-block:: python
+
+    import click
+
+    @click.group()
+    def greet():
+        """A sample command group."""
+        pass
+
+    @greet.command()
+    @click.argument('user', envvar='USER')
+    def hello(user):
+        """Greet a user."""
+        click.echo('Hello %s' % user)
+
+    @greet.command()
+    def world():
+        """Greet the world."""
+        click.echo('Hello world!')
+
+To document this, use the following:
+
+.. code-block:: rst
+
+    .. click:: hello_world:greet
+      :prog: hello-world
+
+If you wish to include full documentation for the subcommand, ``hello``, in the
+output, add the ``show-nested`` flag.
+
+.. code-block:: rst
+
+    .. click:: hello_world:greet
+      :prog: hello-world
+      :show-nested:
+
+You can also document only selected commands by using ``:commands:`` option.
+
+.. code-block:: rst
+
+    .. click:: hello_world:greet
+      :prog: hello-world
+      :commands: hello
+
+You can cross-reference the commands, option and environment variables using
+the roles provided by the `Sphinx standard domain`_.
+
+.. code-block:: rst
+
+    .. click:: hello_world:greet
+       :prog: hello-world
+
+    The :program:`hello` command accepts a :option:`user` argument. If this is
+    not provided, the :envvar:`USER` environment variable will be used.
+
+.. note::
+
+    Cross-referencing using the ``:program:`` directive is not currently
+    supported by Sphinx. Refer to the `Sphinx issue`__ for more information.
+
+    __ https://github.com/sphinx-doc/sphinx/issues/880
+
+Modifying ``sys.path``
+----------------------
+
+If the application or script you wish to document is not installed (i.e. you
+have not installed it with `pip` or run ``python setup.py``), then you may need
+to modify ``sys.path``. For example, given the following application::
+
+    git
+      |- git
+      |    |- __init__.py
+      |    \- git.py
+      \- docs
+          |- git.rst
+          |- index.rst
+           \- conf.py
+
+then it would be necessary to add the following to ``git/docs/conf.py``:
+
+.. code-block:: python
+
+   import os
+   import sys
+   sys.path.insert(0, os.path.abspath('..'))
+
+Once done, you could include the following in ``git/docs/git.rst`` to document
+the application:
+
+.. code-block:: rst
+
+    .. click:: git.git:cli
+       :prog: git
+       :show-nested:
+
+assuming the group or command in ``git.git`` is named ``cli``.
+
+Refer to `issue #2 <https://github.com/click-contrib/sphinx-click/issues/2>`__
+for more information.
---
+++ packages/sphinx-click/sphinx-click-2.4.0.tar.gz/sphinx-click-2.4.0/tests/test_formatter.py
@@ -0,0 +1,612 @@
+import textwrap
+import unittest
+
+import click
+from sphinx_click import ext
+
+
+class CommandTestCase(unittest.TestCase):
+    """Validate basic ``click.Command`` instances."""
+
+    def test_no_parameters(self):
+        """Validate a `click.Command` with no parameters.
+
+        This exercises the code paths for a command with *no* arguments, *no*
+        options and *no* environment variables.
+        """
+
+        @click.command()
+        def foobar():
+            """A sample command."""
+            pass
+
+        ctx = click.Context(foobar, info_name='foobar')
+        output = list(ext._format_command(ctx, show_nested=False))
+
+        self.assertEqual(
+            textwrap.dedent(
+                """
+        A sample command.
+
+        .. program:: foobar
+        .. code-block:: shell
+
+            foobar [OPTIONS]
+        """
+            ).lstrip(),
+            '\n'.join(output),
+        )
+
+    def test_basic_parameters(self):
+        """Validate a combination of parameters.
+
+        This exercises the code paths for a command with arguments, options and
+        environment variables.
+        """
+
+        @click.command()
+        @click.option('--param', envvar='PARAM', help='A sample option')
+        @click.option(
+            '--choice',
+            help='A sample option with choices',
+            type=click.Choice(['Option1', 'Option2']),
+        )
+        @click.argument('ARG', envvar='ARG')
+        def foobar(bar):
+            """A sample command."""
+            pass
+
+        ctx = click.Context(foobar, info_name='foobar')
+        output = list(ext._format_command(ctx, show_nested=False))
+
+        self.assertEqual(
+            textwrap.dedent(
+                """
+        A sample command.
+
+        .. program:: foobar
+        .. code-block:: shell
+
+            foobar [OPTIONS] ARG
+
+        .. rubric:: Options
+
+        .. option:: --param <param>
+
+            A sample option
+
+        .. option:: --choice <choice>
+
+            A sample option with choices
+
+            :options: Option1|Option2
+
+        .. rubric:: Arguments
+
+        .. option:: ARG
+
+            Required argument
+
+        .. rubric:: Environment variables
+
+        .. _foobar-param-PARAM:
+
+        .. envvar:: PARAM
+           :noindex:
+
+            Provide a default for :option:`--param`
+
+        .. _foobar-arg-ARG:
+
+        .. envvar:: ARG
+           :noindex:
+
+            Provide a default for :option:`ARG`
+        """
+            ).lstrip(),
+            '\n'.join(output),
+        )
+
+    @unittest.skipIf(
+        ext.CLICK_VERSION < (7, 0),
+        'Allowing show_default to be a string was added in Click 7.0',
+    )
+    def test_defaults(self):
+        """Validate formatting of user documented defaults."""
+
+        @click.command()
+        @click.option('--num-param', type=int, default=42, show_default=True)
+        @click.option(
+            '--param',
+            default=lambda: None,
+            show_default='Something computed at runtime',
+        )
+        def foobar(bar):
+            """A sample command."""
+            pass
+
+        ctx = click.Context(foobar, info_name='foobar')
+        output = list(ext._format_command(ctx, show_nested=False))
+
+        self.assertEqual(
+            textwrap.dedent(
+                """
+        A sample command.
+
+        .. program:: foobar
+        .. code-block:: shell
+
+            foobar [OPTIONS]
+
+        .. rubric:: Options
+
+        .. option:: --num-param <num_param>
+
+            [default: 42]
+
+        .. option:: --param <param>
+
+            [default: Something computed at runtime]
+        """
+            ).lstrip(),
+            '\n'.join(output),
+        )
+
+    @unittest.skipIf(
+        ext.CLICK_VERSION < (7, 0), 'The hidden flag was added in Click 7.0'
+    )
+    def test_hidden(self):
+        """Validate a `click.Command` with the `hidden` flag."""
+
+        @click.command(hidden=True)
+        def foobar():
+            """A sample command."""
+            pass
+
+        ctx = click.Context(foobar, info_name='foobar')
+        output = list(ext._format_command(ctx, show_nested=False))
+
+        self.assertEqual('', '\n'.join(output))
+
+    def test_titles(self):
+        """Validate a `click.Command` with nested titles."""
+
+        @click.command()
+        @click.option('--name', help='Name to say hello to.', required=True, type=str)
+        def hello(name):
+            """Prints hello to name given.
+
+            Examples
+            --------
+
+            .. code:: bash
+
+                my_cli hello --name "Jack"
+            """
+
+        ctx = click.Context(hello, info_name='hello')
+        output = list(ext._format_command(ctx, show_nested=False))
+
+        self.assertEqual(
+            textwrap.dedent(
+                """
+        Prints hello to name given.
+
+        Examples
+        --------
+
+        .. code:: bash
+
+            my_cli hello --name "Jack"
+
+        .. program:: hello
+        .. code-block:: shell
+
+            hello [OPTIONS]
+
+        .. rubric:: Options
+
+        .. option:: --name <name>
+
+            Name to say hello to.  [required]
+        """
+            ).lstrip(),
+            '\n'.join(output),
+        )
+
+
+class GroupTestCase(unittest.TestCase):
+    """Validate basic ``click.Group`` instances."""
+
+    def test_no_parameters(self):
+        """Validate a `click.Group` with no parameters.
+
+        This exercises the code paths for a group with *no* arguments, *no*
+        options and *no* environment variables.
+        """
+
+        @click.group()
+        def cli():
+            """A sample command group."""
+            pass
+
+        ctx = click.Context(cli, info_name='cli')
+        output = list(ext._format_command(ctx, show_nested=False))
+
+        self.assertEqual(
+            textwrap.dedent(
+                """
+        A sample command group.
+
+        .. program:: cli
+        .. code-block:: shell
+
+            cli [OPTIONS] COMMAND [ARGS]...
+        """
+            ).lstrip(),
+            '\n'.join(output),
+        )
+
+    def test_basic_parameters(self):
+        """Validate a combination of parameters.
+
+        This exercises the code paths for a group with arguments, options and
+        environment variables.
+        """
+
+        @click.group()
+        @click.option('--param', envvar='PARAM', help='A sample option')
+        @click.argument('ARG', envvar='ARG')
+        def cli():
+            """A sample command group."""
+            pass
+
+        ctx = click.Context(cli, info_name='cli')
+        output = list(ext._format_command(ctx, show_nested=False))
+
+        self.assertEqual(
+            textwrap.dedent(
+                """
+        A sample command group.
+
+        .. program:: cli
+        .. code-block:: shell
+
+            cli [OPTIONS] ARG COMMAND [ARGS]...
+
+        .. rubric:: Options
+
+        .. option:: --param <param>
+
+            A sample option
+
+        .. rubric:: Arguments
+
+        .. option:: ARG
+
+            Required argument
+
+        .. rubric:: Environment variables
+
+        .. _cli-param-PARAM:
+
+        .. envvar:: PARAM
+           :noindex:
+
+            Provide a default for :option:`--param`
+
+        .. _cli-arg-ARG:
+
+        .. envvar:: ARG
+           :noindex:
+
+            Provide a default for :option:`ARG`
+        """
+            ).lstrip(),
+            '\n'.join(output),
+        )
+
+    def test_no_line_wrapping(self):
+        r"""Validate behavior when a \b character is present.
+
+        https://click.palletsprojects.com/en/7.x/documentation/#preventing-rewrapping
+        """
+
+        @click.group()
+        def cli():
+            """A sample command group.
+
+            \b
+            This is
+            a paragraph
+            without rewrapping.
+
+            And this is a paragraph
+            that will be rewrapped again.
+            """
+            pass
+
+        ctx = click.Context(cli, info_name='cli')
+        output = list(ext._format_command(ctx, show_nested=False))
+
+        self.assertEqual(
+            textwrap.dedent(
+                """
+        A sample command group.
+
+        | This is
+        | a paragraph
+        | without rewrapping.
+
+        And this is a paragraph
+        that will be rewrapped again.
+
+        .. program:: cli
+        .. code-block:: shell
+
+            cli [OPTIONS] COMMAND [ARGS]...
+        """
+            ).lstrip(),
+            '\n'.join(output),
+        )
+
+
+class NestedCommandsTestCase(unittest.TestCase):
+    """Validate ``click.Command`` instances inside ``click.Group`` instances."""
+
+    @staticmethod
+    def _get_ctx():
+        @click.group()
+        def cli():
+            """A sample command group."""
+            pass
+
+        @cli.command()
+        def hello():
+            """A sample command."""
+            pass
+
+        return click.Context(cli, info_name='cli')
+
+    def test_hide_nested(self):
+        """Validate a nested command without show_nested.
+
+        If we're not showing sub-commands separately, we should list them.
+        """
+
+        ctx = self._get_ctx()
+        output = list(ext._format_command(ctx, show_nested=False))
+
+        self.assertEqual(
+            textwrap.dedent(
+                """
+        A sample command group.
+
+        .. program:: cli
+        .. code-block:: shell
+
+            cli [OPTIONS] COMMAND [ARGS]...
+
+        .. rubric:: Commands
+
+        .. object:: hello
+
+            A sample command.
+        """
+            ).lstrip(),
+            '\n'.join(output),
+        )
+
+    def test_show_nested(self):
+        """Validate a nested command with show_nested.
+
+        If we're not showing sub-commands separately, we should not list them.
+        """
+
+        ctx = self._get_ctx()
+        output = list(ext._format_command(ctx, show_nested=True))
+
+        self.assertEqual(
+            textwrap.dedent(
+                """
+        A sample command group.
+
+        .. program:: cli
+        .. code-block:: shell
+
+            cli [OPTIONS] COMMAND [ARGS]...
+        """
+            ).lstrip(),
+            '\n'.join(output),
+        )
+
+
+class CommandFilterTestCase(unittest.TestCase):
+    """Validate filtering of commands."""
+
+    @staticmethod
+    def _get_ctx():
+        @click.group()
+        def cli():
+            """A sample command group."""
+
+        @cli.command()
+        def hello():
+            """A sample command."""
+
+        @cli.command()
+        def world():
+            """A world command."""
+
+        return click.Context(cli, info_name='cli')
+
+    def test_no_commands(self):
+        """Validate an empty command group."""
+
+        ctx = self._get_ctx()
+        output = list(ext._format_command(ctx, show_nested=False, commands=''))
+
+        self.assertEqual(
+            textwrap.dedent(
+                """
+        A sample command group.
+
+        .. program:: cli
+        .. code-block:: shell
+
+            cli [OPTIONS] COMMAND [ARGS]...
+        """
+            ).lstrip(),
+            '\n'.join(output),
+        )
+
+    def test_order_of_commands(self):
+        """Validate the order of commands."""
+
+        ctx = self._get_ctx()
+        output = list(
+            ext._format_command(ctx, show_nested=False, commands='world, hello')
+        )
+
+        self.assertEqual(
+            textwrap.dedent(
+                """
+        A sample command group.
+
+        .. program:: cli
+        .. code-block:: shell
+
+            cli [OPTIONS] COMMAND [ARGS]...
+
+        .. rubric:: Commands
+
+        .. object:: world
+
+            A world command.
+
+        .. object:: hello
+
+            A sample command.
+        """
+            ).lstrip(),
+            '\n'.join(output),
+        )
+
+
+class CustomMultiCommandTestCase(unittest.TestCase):
+    """Validate ``click.MultiCommand`` instances."""
+
+    def test_basics(self):
+        """Validate a custom ``click.MultiCommand`` with no parameters.
+
+        This exercises the code paths to extract commands correctly from these
+        commands.
+        """
+
+        @click.command()
+        def hello():
+            """A sample command."""
+
+        @click.command()
+        def world():
+            """A world command."""
+
+        class MyCLI(click.MultiCommand):
+            _command_mapping = {
+                'hello': hello,
+                'world': world,
+            }
+
+            def list_commands(self, ctx):
+                return ['hello', 'world']
+
+            def get_command(self, ctx, name):
+                return self._command_mapping[name]
+
+        cli = MyCLI(help='A sample custom multicommand.')
+        ctx = click.Context(cli, info_name='cli')
+        output = list(ext._format_command(ctx, show_nested=False))
+
+        self.assertEqual(
+            textwrap.dedent(
+                """
+        A sample custom multicommand.
+
+        .. program:: cli
+        .. code-block:: shell
+
+            cli [OPTIONS] COMMAND [ARGS]...
+
+        .. rubric:: Commands
+
+        .. object:: hello
+
+            A sample command.
+
+        .. object:: world
+
+            A world command.
+        """
+            ).lstrip(),
+            '\n'.join(output),
+        )
+
+    @unittest.skipIf(
+        ext.CLICK_VERSION < (7, 0), 'The hidden flag was added in Click 7.0'
+    )
+    def test_hidden(self):
+        """Ensure 'hidden' subcommands are not shown."""
+
+        @click.command()
+        def hello():
+            """A sample command."""
+
+        @click.command()
+        def world():
+            """A world command."""
+
+        @click.command(hidden=True)
+        def hidden():
+            """A hidden command."""
+
+        class MyCLI(click.MultiCommand):
+            _command_mapping = {
+                'hello': hello,
+                'world': world,
+                'hidden': hidden,
+            }
+
+            def list_commands(self, ctx):
+                return ['hello', 'world', 'hidden']
+
+            def get_command(self, ctx, name):
+                return self._command_mapping[name]
+
+        cli = MyCLI(help='A sample custom multicommand.')
+        ctx = click.Context(cli, info_name='cli')
+        output = list(ext._format_command(ctx, show_nested=False))
+
+        # Note that we do NOT expect this to show the 'hidden' command
+        self.assertEqual(
+            textwrap.dedent(
+                """
+        A sample custom multicommand.
+
+        .. program:: cli
+        .. code-block:: shell
+
+            cli [OPTIONS] COMMAND [ARGS]...
+
+        .. rubric:: Commands
+
+        .. object:: hello
+
+            A sample command.
+
+        .. object:: world
+
+            A world command.
+        """
+            ).lstrip(),
+            '\n'.join(output),
+        )
