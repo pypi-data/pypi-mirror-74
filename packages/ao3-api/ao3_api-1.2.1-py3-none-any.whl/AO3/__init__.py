ead6d44bee3ab23de0309bd1ce8fe498697792db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:49 +0000

    Add package ao3-api/1.2.1/ao3_api-1.2.1-py3-none-any.whl

---
+++ packages/ao3-api/ao3_api-1.2.1-py3-none-any.whl/AO3/__init__.py
@@ -0,0 +1,8 @@
+from .users import User
+from .session import Session, GuestSession
+from .works import Work
+from .search import Search
+from .comments import Comment
+from . import utils
+
+VERSION = "1.2.1"
