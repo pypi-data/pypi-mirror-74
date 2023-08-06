faa263927db817eec0818da7b02eda628aff287f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:44 +0000

    Add package skyhigh1312-probability/1.0/skyhigh1312_probability-1.0.tar.gz

---
+++ packages/skyhigh1312-probability/skyhigh1312_probability-1.0.tar.gz/skyhigh1312_probability-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='skyhigh1312_probability',
+      version='1.0',
+      description='Gaussian & Binomial distributions',
+      packages=['skyhigh1312_probability'],
+      author = 'Sukesh Seth',
+      author_email = 'myemail@something.com',
+      zip_safe=False)
