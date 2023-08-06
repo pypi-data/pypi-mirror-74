3b4131c6e3fddcc11f4a23751f41f9e06db38d49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:48 +0000

    Add package evogression/0.5.3/evogression-0.5.3.tar.gz

---
+++ packages/evogression/evogression-0.5.3.tar.gz/evogression-0.5.3/evogression/calc_target_cython.pyx
@@ -0,0 +1,48 @@
+'''
+Cython implementation of EvogressionCreature._calc_single_layer_target()
+'''
+
+
+cpdef double calc_target_cython(dict parameters, dict modifiers):
+
+    cdef str layer_name
+    cpdef double T = -99999  # bogus value for first layer
+    for layer_name in modifiers:
+        T = calc_single_layer_target_cython(parameters, modifiers, layer_name, T)
+    return T
+
+
+
+cdef double calc_single_layer_target_cython(dict parameters, dict modifiers, str layer_name, double previous_T):
+
+    cdef double T = 0
+    cdef str param
+    cdef double value
+    cdef dict layer_modifiers = modifiers[layer_name]
+
+    cdef double C, B, Z, X
+
+    for param, value in parameters.items():
+        try:
+            C, B, Z, X = layer_modifiers[param]  # unpacked namedtuple
+            T += C * (B * value + Z) ** X
+        except KeyError:  # if param is not in self.modifiers[layer_name]
+            pass
+        except OverflowError:
+            T += 10 ** 150  # really big number should make this creature die if crazy bad calculations (overflow)
+
+    if previous_T != -99999:
+        try:
+            C, B, Z, X = layer_modifiers['T']  # unpacked namedtuple
+            T += C * (B * previous_T + Z) ** X
+        except KeyError:  # if param is not in self.modifiers[layer_name]
+            pass
+        except OverflowError:
+            T += 10 ** 150  # really big number should make this creature die if crazy bad calculations (overflow)
+
+    try:
+        T += layer_modifiers['N']
+    except KeyError:
+        pass
+
+    return T
---
+++ packages/evogression/evogression-0.5.3.tar.gz/evogression-0.5.3/tests/surface_3d_population_regression_test.py
@@ -0,0 +1,44 @@
+import unittest
+import sys
+sys.path.insert(1, '..')
+import evogression
+from test_data import surface_3d_data
+from pprint import pprint as pp
+import matplotlib
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import axes3d
+
+import random
+random.seed(10)  # for reproducing the same plot
+
+
+class Test3DSurfaceRegression(unittest.TestCase):
+
+    def test_best_creature_3d(self):
+        evolutions = evogression.random_population(surface_3d_data, 'z', num_creatures=500, num_cycles=5, group_size=10, optimize=3, use_multip=False)
+        z_test = [sum(e.predict(d, 'pred')['pred'] for e in evolutions) / len(evolutions) for d in surface_3d_data]
+
+        # standardized_3d_data = [evolution.standardizer.convert_parameter_dict_to_standardized(d) for d in surface_3d_data]
+        # z_test = [evolution.standardizer.unstandardize_value('z', evolution.best_creature.calc_target(d)) for d in standardized_3d_data]
+
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+
+        x = [point_dict['x'] for point_dict in surface_3d_data]
+        y = [point_dict['y'] for point_dict in surface_3d_data]
+        z = [point_dict['z'] for point_dict in surface_3d_data]
+
+        ax.scatter3D(x, y, z)
+        ax.scatter3D(x, y, z_test)
+
+        ax.set_xlabel('x')
+        ax.set_ylabel('y')
+        ax.set_zlabel('z')
+        plt.title('Surface Regression - Random Population Test')
+
+        plt.show()
+
+
+
+if __name__ == '__main__':
+    unittest.main()
