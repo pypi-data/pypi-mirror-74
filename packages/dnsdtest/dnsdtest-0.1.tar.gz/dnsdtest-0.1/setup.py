a39a91f2d1b5bf67fab41fa8c6300297ba710bb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:09 +0000

    Add package dnsdtest/0.1/dnsdtest-0.1.tar.gz

---
+++ packages/dnsdtest/dnsdtest-0.1.tar.gz/dnsdtest-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dnsdtest',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['dnsdtest'],
+      author='Mamadou Bousso',
+      author_email = 'mbousso@univ-thies.sn',
+      zip_safe=False)
