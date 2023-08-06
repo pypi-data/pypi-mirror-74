649da3257a4dac1b2890bf19e678c04d2e0fe2b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:39 +0000

    Add package three-3-distributions/0.1/three_3_distributions-0.1.tar.gz

---
+++ packages/three-3-distributions/three_3_distributions-0.1.tar.gz/three_3_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='three_3_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['three_3_distributions'],
+      author = 'Dayanand Ghelaro',
+      author_email = 'dayanandghelaro@gmail.com',
+      zip_safe=False)
