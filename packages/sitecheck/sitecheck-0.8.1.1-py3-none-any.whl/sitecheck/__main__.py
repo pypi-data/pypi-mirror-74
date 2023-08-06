980dd53c6dd9b4c757b4ce6d73b8cc5a4101047f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:15 +0000

    Add package sitecheck/0.8.1.1/sitecheck-0.8.1.1-py3-none-any.whl

---
+++ packages/sitecheck/sitecheck-0.8.1.1-py3-none-any.whl/sitecheck/__main__.py
@@ -0,0 +1,31 @@
+"""
+    Geo-Instruments
+    Sitecheck scanner
+"""
+# __author__ = "Dan Edens"
+# __version__= "0.8.19"
+# __url__= "https://github.com/DanEdens/Sitecheck_Scrapper"
+
+import asyncio
+import os
+import sys
+
+from os import path
+
+ROOT_DIR = os.path.dirname(path.abspath(sys.modules['__main__'].__file__))
+os.environ['ROOT_DIR'] = ROOT_DIR
+sys.path.insert(0, ROOT_DIR)
+
+from sitecheck.scanner import Sitecheck
+
+
+async def main():
+    """
+    Retrieves Sensor data from Project websites and produces
+    an Adaptive card for Microsoft Teams to intake.
+    """
+    await Sitecheck()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
