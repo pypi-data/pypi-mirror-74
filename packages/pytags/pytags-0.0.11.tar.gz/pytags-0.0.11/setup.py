a60d0dca6a7c239173c96fb36dce96eb898c33c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:31 +0000

    Add package pytags/0.0.11/pytags-0.0.11.tar.gz

---
+++ packages/pytags/pytags-0.0.11.tar.gz/pytags-0.0.11/setup.py
@@ -0,0 +1,63 @@
+import setuptools
+
+
+def get_readme():
+    with open('README.rst') as f:
+        return f.read()
+
+
+setuptools.setup(
+    # the first three fields are a must according to the documentation
+    name="pytags",
+    version="0.0.11",
+    packages=[
+        'pytags',
+        'pytags.utils',
+    ],
+    # from here all is optional
+    description="module to help you tag interesting places in your code",
+    long_description=get_readme(),
+    long_description_content_type="text/x-rst",
+    author="Mark Veltzer",
+    author_email="mark.veltzer@gmail.com",
+    maintainer="Mark Veltzer",
+    maintainer_email="mark.veltzer@gmail.com",
+    keywords=[
+        'pytags',
+        'tag',
+        'code',
+        'command',
+        'line',
+    ],
+    url="https://veltzer.github.io/pytags",
+    download_url="https://github.com/veltzer/pytags",
+    license="MIT",
+    platforms=[
+        'python3',
+    ],
+    install_requires=[
+        'pytconf',
+        'pylogconf',
+    ],
+    extras_require={
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Utilities',
+        'License :: OSI Approved :: MIT License',
+    ],
+    data_files=[
+    ],
+    entry_points={"console_scripts": [
+        'tagger=pytags.tagger:main',
+    ]},
+    python_requires=">=3.6",
+)
