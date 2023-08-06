d8db890189809cfe5aeb17d3b2261614b029e4bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:12 +0000

    Add package helios1101-gaussian-distributions/0.1/helios1101_gaussian_distributions-0.1.tar.gz

---
+++ packages/helios1101-gaussian-distributions/helios1101_gaussian_distributions-0.1.tar.gz/helios1101_gaussian_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='helios1101_gaussian_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['helios1101_gaussian_distributions'],
+      author = 'Harshit Singh',
+      author_email = 'harshit@codeasylums.com',
+    zip_safe=False)
