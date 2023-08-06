da3686ff7e171ae73ff7fc4b6b62bb4d6cc73043
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:27 +0000

    Add package prob-package-1/0.0/prob_package_1-0.0.tar.gz

---
+++ packages/prob-package-1/prob_package_1-0.0.tar.gz/prob_package_1-0.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='prob_package_1',
+      version='0.0',
+      description='Gaussian and Binomial distributions',
+      packages=['prob_package_1'],
+      author="Vinamr Bajaj",
+      author_email="bajajvinamr@gmail.com",
+      zip_safe=False)
