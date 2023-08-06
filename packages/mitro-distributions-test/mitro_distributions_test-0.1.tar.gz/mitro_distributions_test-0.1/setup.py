2ff5581bd62c3ef708c02e45b5f6536e4e524538
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:01 +0000

    Add package mitro-distributions-test/0.1/mitro_distributions_test-0.1.tar.gz

---
+++ packages/mitro-distributions-test/mitro_distributions_test-0.1.tar.gz/mitro_distributions_test-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='mitro_distributions_test',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['mitro_distributions_test'],
+      zip_safe=False)
