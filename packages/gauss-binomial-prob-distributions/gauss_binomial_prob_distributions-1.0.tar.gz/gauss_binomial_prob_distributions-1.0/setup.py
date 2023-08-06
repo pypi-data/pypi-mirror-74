479e8e2f1d145d10cb614725ba90476d1303309b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:07 +0000

    Add package gauss-binomial-prob-distributions/1.0/gauss_binomial_prob_distributions-1.0.tar.gz

---
+++ packages/gauss-binomial-prob-distributions/gauss_binomial_prob_distributions-1.0.tar.gz/gauss_binomial_prob_distributions-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='gauss_binomial_prob_distributions',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['gauss_binomial_prob_distributions'],
+      author='Sandipan Das',
+      author_email='sandipan.das898@gmail.com',
+      zip_safe=False)
