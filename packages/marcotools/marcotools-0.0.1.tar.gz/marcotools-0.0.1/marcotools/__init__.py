f98cff92c064c136277b9d877010deaf535d38f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:52 +0000

    Add package marcotools/0.0.1/marcotools-0.0.1.tar.gz

---
+++ packages/marcotools/marcotools-0.0.1.tar.gz/marcotools-0.0.1/marcotools/__init__.py
@@ -0,0 +1,7 @@
+import marcotools.schedule
+import marcotools.filestools
+import marcotools.generaltools
+import marcotools.logtools
+import marcotools.nettools
+import marcotools.retools
+import marcotools.telegrambot
