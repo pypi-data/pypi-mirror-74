8d196a2f83dbc51b3dee107f3b6644e2a77a0fbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:45 +0000

    Add package py-pde/0.8.7/py_pde-0.8.7-py3-none-any.whl

---
+++ packages/py-pde/py_pde-0.8.7-py3-none-any.whl/pde/__init__.py
@@ -0,0 +1,17 @@
+"""
+The py-pde package provides classes and methods for solving partial differential
+equations.
+"""
+
+__version__ = "0.8.7"
+
+from .fields import *  # @UnusedWildImport
+from .grids import *  # @UnusedWildImport
+from .pdes import *  # @UnusedWildImport
+from .solvers import *  # @UnusedWildImport
+from .storage import *  # @UnusedWildImport
+from .trackers import *  # @UnusedWildImport
+from .visualization import *  # @UnusedWildImport
+
+from .tools.misc import environment
+from .tools.parameters import Parameter
