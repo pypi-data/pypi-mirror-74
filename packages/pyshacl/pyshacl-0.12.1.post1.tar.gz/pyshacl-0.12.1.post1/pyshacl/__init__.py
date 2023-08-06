ae2d96afbf8ed5f4e5736520fa3d1cdcdaac5331
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:32 +0000

    Add package pyshacl/0.12.1.post1/pyshacl-0.12.1.post1.tar.gz

---
+++ packages/pyshacl/pyshacl-0.12.1.post1.tar.gz/pyshacl-0.12.1.post1/pyshacl/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: latin-1 -*-
+#
+from pyshacl.validate import Validator, validate
+
+
+# version compliant with https://www.python.org/dev/peps/pep-0440/
+__version__ = '0.12.1.post1'
+
+__all__ = ['validate', 'Validator', '__version__']
