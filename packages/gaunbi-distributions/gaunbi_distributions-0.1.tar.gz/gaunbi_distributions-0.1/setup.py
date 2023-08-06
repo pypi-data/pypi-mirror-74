e400b5ad181c6438427c6168983a472ac26dd0a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:58 +0000

    Add package gaunbi-distributions/0.1/gaunbi_distributions-0.1.tar.gz

---
+++ packages/gaunbi-distributions/gaunbi_distributions-0.1.tar.gz/gaunbi_distributions-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='gaunbi_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      author = 'Esther Ye',
+      packages=['gaunbi_distributions'],
+      zip_safe=False)
