3d90b19a7df58426a6bff46611ee83500ff9daa0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:06 +0000

    Add package ammu-distributions/0.1/ammu_distributions-0.1.tar.gz

---
+++ packages/ammu-distributions/ammu_distributions-0.1.tar.gz/ammu_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='ammu_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['ammu_distributions'],
+      author = 'Aman Bahuguna',
+      author_email = 'amanbahuguna002@gmail.com',
+      zip_safe=False)
