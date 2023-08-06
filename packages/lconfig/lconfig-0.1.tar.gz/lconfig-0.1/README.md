53bf5ce083618981e51309ea0b92929c9793ad25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:25 +0000

    Add package lconfig/0.1/lconfig-0.1.tar.gz

---
+++ packages/lconfig/lconfig-0.1.tar.gz/lconfig-0.1/README.md
@@ -0,0 +1,6 @@
+
+
+![Python application](https://github.com/lbacik/python-config/workflows/Python%20application/badge.svg)
+
+
+work in progress...
---
+++ packages/lconfig/lconfig-0.1.tar.gz/lconfig-0.1/src/config/tests/test_config.py
@@ -0,0 +1,7 @@
+from config.config import Config
+
+
+def test_config_get():
+    config = Config({'bar': 'baz'})
+    assert config.get('foo') is None
+    assert config.get('bar') == 'baz'
