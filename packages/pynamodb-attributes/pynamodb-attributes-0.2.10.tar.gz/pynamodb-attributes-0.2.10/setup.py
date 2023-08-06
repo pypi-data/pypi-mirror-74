ebd701aa3a459dd2d7b2c1bf0af9b9464c2eb144
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:43 +0000

    Add package pynamodb-attributes/0.2.10/pynamodb-attributes-0.2.10.tar.gz

---
+++ packages/pynamodb-attributes/pynamodb-attributes-0.2.10.tar.gz/pynamodb-attributes-0.2.10/setup.py
@@ -0,0 +1,18 @@
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    name='pynamodb-attributes',
+    version='0.2.10',
+    description='Common attributes for PynamoDB',
+    url='https://www.github.com/lyft/pynamodb-attributes',
+    maintainer='Lyft',
+    maintainer_email='ikonstantinov@lyft.com',
+    packages=find_packages(exclude=['tests*']),
+    dependency_links=[],
+    install_requires=[
+        "pynamodb>=3.3.3",
+    ],
+    python_requires='>=3',
+    package_data={'pynamodb_attributes': ['py.typed']},
+)
