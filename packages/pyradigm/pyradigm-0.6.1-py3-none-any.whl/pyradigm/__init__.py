f858e3b7bb79740e1d5162452096e8de425c70ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:34 +0000

    Add package pyradigm/0.6.1/pyradigm-0.6.1-py3-none-any.whl

---
+++ packages/pyradigm/pyradigm-0.6.1-py3-none-any.whl/pyradigm/__init__.py
@@ -0,0 +1,21 @@
+__all__ = ['ClassificationDataset', 'RegressionDataset',
+           'MultiDatasetClassify', 'MultiDatasetRegress',
+           'BaseDataset', 'check_compatibility', 'MLDataset', 'cli_run']
+
+from sys import version_info
+
+if version_info.major >= 3:
+    from pyradigm.base import BaseDataset
+    from pyradigm.classify import ClassificationDataset
+    from pyradigm.regress import RegressionDataset
+    from pyradigm.pyradigm import MLDataset, cli_run
+    from pyradigm.multiple import MultiDatasetClassify, MultiDatasetRegress
+    from pyradigm.utils import check_compatibility
+else:
+    raise NotImplementedError('pyradigm supports only Python 3 or higher! '
+                              'Upgrade to Python 3+ is recommended.')
+
+from ._version import get_versions
+
+__version__ = get_versions()['version']
+del get_versions
