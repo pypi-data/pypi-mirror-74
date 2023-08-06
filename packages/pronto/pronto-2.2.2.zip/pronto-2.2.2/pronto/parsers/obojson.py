a7a5329119545010378eff30b54a6191b6c5dd32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:18 +0000

    Add package pronto/2.2.2/pronto-2.2.2.zip

---
+++ packages/pronto/pronto-2.2.2.zip/pronto-2.2.2/pronto/parsers/obojson.py
@@ -0,0 +1,50 @@
+import os
+import multiprocessing.pool
+
+import fastobo
+
+from ..logic.lineage import Lineage
+from .base import BaseParser
+from ._fastobo import FastoboParser
+
+
+class OboJSONParser(FastoboParser, BaseParser):
+    @classmethod
+    def can_parse(cls, path, buffer):
+        return buffer.lstrip().startswith(b"{")
+
+    def parse_from(self, handle, threads=None):
+        # Load the OBO graph into a syntax tree using fastobo
+        doc = fastobo.load_graph(handle).compact_ids()
+
+        # Extract metadata from the graph metadata and resolve imports
+        self.ont.metadata = self.extract_metadata(doc.header)
+        self.ont.imports.update(
+            self.process_imports(
+                self.ont.metadata.imports,
+                self.ont.import_depth,
+                os.path.dirname(self.ont.path or str()),
+                self.ont.timeout,
+            )
+        )
+
+        # Merge inheritance cache from imports
+        self.import_inheritance()
+
+        # Extract frames from the current document.
+        try:
+            with multiprocessing.pool.ThreadPool(threads) as pool:
+                pool.map(self.extract_entity, doc)
+        except SyntaxError as err:
+            location = self.ont.path, err.lineno, err.offset, err.text
+            raise SyntaxError(err.args[0], location) from None
+
+        # OBOJSON can define classes implicitly using only `is_a` properties
+        # mapping to unresolved identifiers: in this case, we create the
+        # term ourselves
+        for lineage in list(self.ont._inheritance.values()):
+            for superclass in lineage.sup.difference(self.ont._inheritance):
+                self.ont.create_term(superclass)
+
+        # Update inheritance cache
+        self.symmetrize_inheritance()
