7cdab6157034b9b348dc266e402cffeba54ac3a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:38 +0000

    Add package distributions-gordon-test/0.1/distributions_gordon_test-0.1.tar.gz

---
+++ packages/distributions-gordon-test/distributions_gordon_test-0.1.tar.gz/distributions_gordon_test-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_gordon_test',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions_gordon_test'],
+      zip_safe=False)
