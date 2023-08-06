4a8d844d12ae908aaa400eed6d09233c1a82e703
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:18 +0000

    Add package usertestdistributions/0.2/usertestdistributions-0.2.tar.gz

---
+++ packages/usertestdistributions/usertestdistributions-0.2.tar.gz/usertestdistributions-0.2/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='usertestdistributions',
+      version='0.2',
+      description='Gaussian distributions',
+      packages=['usertestdistributions'],
+      zip_safe=False)
