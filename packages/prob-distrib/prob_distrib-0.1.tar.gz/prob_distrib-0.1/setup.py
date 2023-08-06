49c3348c06718a99540a2a6c5a4577cbf3309d73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:05 +0000

    Add package prob-distrib/0.1/prob_distrib-0.1.tar.gz

---
+++ packages/prob-distrib/prob_distrib-0.1.tar.gz/prob_distrib-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='prob_distrib',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['prob_distrib'],
+      author = 'Grevians Onula',
+      author_email = 'grevians@yahoo.com',
+      zip_safe=False)
