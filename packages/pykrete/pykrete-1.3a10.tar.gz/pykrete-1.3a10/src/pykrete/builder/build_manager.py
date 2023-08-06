c6900d65c7739364e146c1dbb4f5f0822618324b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:57 +0000

    Add package pykrete/1.3a10/pykrete-1.3a10.tar.gz

---
+++ packages/pykrete/pykrete-1.3a10.tar.gz/pykrete-1.3a10/src/pykrete/builder/build_manager.py
@@ -0,0 +1,52 @@
+"""
+Build manager base class
+Author: Shai Bennathan - shai.bennathan@gmail.com
+(C) 2020
+"""
+import logging
+from abc import abstractmethod
+from pykrete.args import environ
+from pykrete.versioning import Version, RevisionType
+
+
+class BuildManager:
+    """Manages building a python package"""
+
+    _logger = logging.getLogger(__name__)
+
+    def __init__(self, version: Version):
+        """Initializes this instance
+        :param version: Build version
+        """
+        self._version = version
+        self._project = environ('CI_TARGET', 'the name of the project under src')
+
+    def run(self):
+        """Runs the build process"""
+        self._clean_distribution()
+        self._set_package_version()
+        self._build()
+        self._upload(self._version.revision_type == RevisionType.Release)
+
+    @abstractmethod
+    def _clean_distribution(self):
+        """Removes previous distribution files"""
+
+    @abstractmethod
+    def _set_package_version(self):
+        """Sets the package version in the project's version file"""
+
+    @abstractmethod
+    def _build(self):
+        """Performs the actual build using build.py"""
+
+    @abstractmethod
+    def _upload(self, is_release):
+        """Uploads the package
+
+        :param is_release: flag this package as release
+        """
+
+    def __str__(self):
+        """Gets a string representation of this object"""
+        return f'Builder for version {self._version} of project "{self._project}"'
---
+++ packages/pykrete/pykrete-1.3a10.tar.gz/pykrete-1.3a10/src/pykrete/patterns/__init__.py
@@ -0,0 +1,8 @@
+"""
+Pattern classes
+Author: Shai Bennathan - shai.bennathan@gmail.com
+(C) 2020
+"""
+from .common import assert_true, pykrete_root, pykrete_externals
+
+__all__ = ['visitor', 'assert_true', 'pykrete_root', 'pykrete_externals']
