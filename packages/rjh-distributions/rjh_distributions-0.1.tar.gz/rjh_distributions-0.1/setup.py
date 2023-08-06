2f8ea0ff83269a5554c702a2188249f81248e124
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:50 +0000

    Add package rjh-distributions/0.1/rjh_distributions-0.1.tar.gz

---
+++ packages/rjh-distributions/rjh_distributions-0.1.tar.gz/rjh_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='rjh_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['rjh_distributions'],
+      zip_safe=False)
