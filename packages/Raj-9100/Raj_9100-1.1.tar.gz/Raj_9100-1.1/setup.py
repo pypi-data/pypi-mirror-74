c8fcaf30973a69b8ff457df2f694ca92ce4c268a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:40 +0000

    Add package Raj-9100/1.1/Raj_9100-1.1.tar.gz

---
+++ packages/Raj-9100/Raj_9100-1.1.tar.gz/Raj_9100-1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='Raj_9100',
+      version='1.1',
+      description='Gaussian distributions',
+      packages=['Raj_9100'],
+      author='GaneshRaj',
+      author_email='edugan28@gmail.com',
+      zip_safe=False)
