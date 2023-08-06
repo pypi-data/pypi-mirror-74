c50761d11624bd8c8f55fe3c085771a21f24d0f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:02 +0000

    Add package maxattacks-distributions/0.1/maxattacks_distributions-0.1.tar.gz

---
+++ packages/maxattacks-distributions/maxattacks_distributions-0.1.tar.gz/maxattacks_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='maxattacks_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['maxattacks_distributions'],
+      author = 'Maximilian Kemper',
+      author_email = 'bienemaya1337@gmx.de',
+      zip_safe=False)
