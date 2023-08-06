530d5322998d2479d7d4bcd4bd879e55cc5e5bd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:52 +0000

    Add package adidnsdump/1.3.0/adidnsdump-1.3.0.tar.gz

---
+++ packages/adidnsdump/adidnsdump-1.3.0.tar.gz/adidnsdump-1.3.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(name='adidnsdump',
+      version='1.3.0',
+      description='Active Directory Integrated DNS dumping by any authenticated user',
+      license='MIT',
+      classifiers=[
+          'Intended Audience :: Information Technology',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+      ],
+      author='Dirk-jan Mollema (@_dirkjan)',
+      author_email='dirkjan.mollema@fox-it.com',
+      url='https://github.com/dirkjanm/adidnsdump',
+      packages=['adidnsdump'],
+      install_requires=['impacket', 'ldap3>=2.5,!=2.5.2,!=2.5.0,!=2.6'],
+      entry_points={
+          'console_scripts': ['adidnsdump=adidnsdump:main']
+      }
+     )
