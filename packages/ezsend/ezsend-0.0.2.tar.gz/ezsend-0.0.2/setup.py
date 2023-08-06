c1ffc008aaffa3cfbd62cfadd4274c25fbe96950
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:52 +0000

    Add package ezsend/0.0.2/ezsend-0.0.2.tar.gz

---
+++ packages/ezsend/ezsend-0.0.2.tar.gz/ezsend-0.0.2/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+
+setuptools.setup(
+    name="ezsend", # Replace with your own username
+    version="0.0.2",
+    author="Eshan Nalajala",
+    author_email="eshan.nalajala@gmail.com",
+    description="Ezsend, the easiest way to send mail and texts in python",
+    long_description_content_type="text/markdown",
+    url="https://github.com/BouncyBird/ezsend",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
