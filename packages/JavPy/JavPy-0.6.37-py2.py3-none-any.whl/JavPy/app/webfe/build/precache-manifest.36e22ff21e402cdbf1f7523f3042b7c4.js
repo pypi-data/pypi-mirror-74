ecb9df28efdc9323e7786cffe93de25d0f4e7f97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:13 +0000

    Add package JavPy/0.6.37/JavPy-0.6.37-py2.py3-none-any.whl

---
+++ packages/JavPy/JavPy-0.6.37-py2.py3-none-any.whl/JavPy/app/webfe/build/precache-manifest.36e22ff21e402cdbf1f7523f3042b7c4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d847b199ffa694bad11d3341b43d25e4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5710db8433b672cb4ff4",
+    "url": "/static/css/2.a33d7f97.chunk.css"
+  },
+  {
+    "revision": "d0ff4f71fd0c7ad15929",
+    "url": "/static/css/main.12301873.chunk.css"
+  },
+  {
+    "revision": "5710db8433b672cb4ff4",
+    "url": "/static/js/2.61a51e60.chunk.js"
+  },
+  {
+    "revision": "69aceadffa80df17147cfa73022ec51e",
+    "url": "/static/js/2.61a51e60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0ff4f71fd0c7ad15929",
+    "url": "/static/js/main.d70974a3.chunk.js"
+  },
+  {
+    "revision": "00e68b54667811347eae",
+    "url": "/static/js/runtime-main.39a99049.js"
+  }
+]);
