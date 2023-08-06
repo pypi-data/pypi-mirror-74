8386a0b178e0b51671a13da94565e503d7a5b7ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:19 +0000

    Add package mpcontribs-client/3.0.1/mpcontribs_client-3.0.1-py3-none-any.whl

---
+++ packages/mpcontribs-client/mpcontribs_client-3.0.1-py3-none-any.whl/mpcontribs/client/test_client.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+import pytest
+
+from unittest.mock import patch, MagicMock
+from mpcontribs.client import validate_email, Client, DEFAULT_HOST, email_format
+from swagger_spec_validator.common import SwaggerValidationError
+
+
+def test_validate_email():
+    validate_email("phuck@lbl.gov")
+    with pytest.raises(SwaggerValidationError):
+        validate_email("not-an-email!")
+
+
+@patch(
+    "bravado.swagger_model.Loader.load_spec",
+    new=MagicMock(
+        return_value={
+            "swagger": "2.0",
+            "paths": {},
+            "info": {"title": "Swagger", "version": "0.0"},
+        }
+    ),
+)
+def test_Client():
+    kwargs = {}
+    spec = Client(**kwargs).swagger_spec
+    assert spec.http_client.headers == {}
+    assert spec.origin_url == f"http://{DEFAULT_HOST}/apispec.json"
+    assert spec.spec_dict["host"] == DEFAULT_HOST
+    assert spec.spec_dict["schemes"] == ["http"]
+    assert spec.user_defined_formats["email"] == email_format
+    kwargs = {"apikey": "1234", "headers": {"a": "b"}, "host": "api.example.com"}
+    spec = Client(**kwargs).swagger_spec
+    assert spec.http_client.headers == {"x-api-key": "1234"}
+    assert spec.origin_url == "https://api.example.com/apispec.json"
+    assert spec.spec_dict["host"] == "api.example.com"
+    assert spec.spec_dict["schemes"] == ["https"]
+    assert spec.user_defined_formats["email"] == email_format
+    kwargs = {"headers": {"a": "b"}}
+    spec = Client(**kwargs).swagger_spec
+    assert spec.http_client.headers == {"a": "b"}
+    assert spec.origin_url == f"http://{DEFAULT_HOST}/apispec.json"
+    assert spec.spec_dict["host"] == DEFAULT_HOST
+    assert spec.spec_dict["schemes"] == ["http"]
+    assert spec.user_defined_formats["email"] == email_format
