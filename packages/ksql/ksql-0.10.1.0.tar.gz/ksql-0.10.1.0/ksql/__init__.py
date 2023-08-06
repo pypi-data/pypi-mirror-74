b6bf307eb28c2a7ddec0d64ec80f461d6facd6f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:51 +0000

    Add package ksql/0.10.1.0/ksql-0.10.1.0.tar.gz

---
+++ packages/ksql/ksql-0.10.1.0.tar.gz/ksql-0.10.1.0/ksql/__init__.py
@@ -0,0 +1,9 @@
+
+__package_name__ = "ksql"
+__ksql_server_version__ = "0.10.1"
+__ksql_api_version__ = "0.1.2"
+__version__ = __ksql_server_version__ + "." + __ksql_api_version__
+
+from ksql.client import KSQLAPI
+from ksql.builder import SQLBuilder
+from ksql.api import SimplifiedAPI
