7305d2a7de03ddd140a50cc700231b681320204d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:47 +0000

    Add package geopatra/0.2.0/geopatra-0.2.0-py3-none-any.whl

---
+++ packages/geopatra/geopatra-0.2.0-py3-none-any.whl/geopatra/folium/geojson.py
@@ -0,0 +1,63 @@
+"""Folium geojson Plot."""
+
+import folium
+from .utils import _random_color_hex, _folium_map, _get_tooltip, _random_string
+
+
+def geojson(
+    gdf,
+    name="layer",
+    width="100%",
+    height="100%",
+    location=None,
+    color="blue",
+    tooltip=None,
+    zoom=7,
+    tiles="OpenStreetMap",
+    attr=None,
+    style={},
+):
+    """
+    Convert Geodataframe to geojson and plot it.
+
+    Parameters
+    ----------
+    gdf : GeoDataframe
+    name : name of the geojson layer, optional, default "layer"
+    width : width of the map, default 950
+    height : height of the map, default 550
+    location : center of the map rendered, default centroid of first geometry
+    color : color of your geometries, default blue
+            use random to randomize the colors
+    tooltip : hover box on the map with geometry info, default all columns
+            can be a list of column names
+    zoom : zoom level of the map, default 7
+    tiles : basemap, default openstreetmap,
+            options ['google','googlesatellite','googlehybrid'] or custom wms
+    attr : Attribution to external basemaps being used, default None
+    style : dict, additional style to geometries
+    Returns
+    -------
+    m : folium.map
+    """
+    gpd_copy = gdf.copy()
+    color_column = "color{}".format(_random_string(4))
+    if isinstance(color, list):
+        gpd_copy[color_column] = color
+    elif color.lower() == "random":
+        gpd_copy[color_column] = _random_color_hex(len(gdf))
+    else:
+        gpd_copy[color_column] = [color] * len(gpd_copy)
+
+    geo_json = gpd_copy.__geo_interface__
+
+    m = _folium_map(
+        gpd_copy, width, height, location, tiles=tiles, attr=attr, zoom_start=zoom
+    )
+    folium.GeoJson(
+        geo_json,
+        tooltip=_get_tooltip(tooltip, gdf),
+        style_function=lambda x: {"color": x["properties"][color_column], **style},
+        name=name,
+    ).add_to(m)
+    return m
---
+++ packages/geopatra/geopatra-0.2.0-py3-none-any.whl/geopatra/map.py
@@ -0,0 +1,60 @@
+"""Combine plots and expose them with geopandas."""
+
+
+import geopandas
+from geopatra import folium
+from geopatra import kepler
+
+
+@geopandas.tools.util.pd.api.extensions.register_dataframe_accessor("folium")
+class InteractiveMapFolium:
+    """Extends geopandas to enable easy interactive plotting with folium.
+
+    Refer folium/ to call the below functions directly or to customize them
+    """
+
+    def __init__(self, geopandas_obj):
+        """Init InteractiveMap with geodataframe."""
+        self._gdf = geopandas_obj
+
+    def plot(self, **kwargs):
+        """Plot geodataframe as a geojson."""
+        m = folium.geojson(self._gdf, **kwargs)
+        return m
+
+    def circle(self, **kwargs):
+        """Plot geodataframe as a geojson."""
+        m = folium.circle(self._gdf, **kwargs)
+        return m
+
+    def chloropeth(self, **kwargs):
+        """Plot geodataframe as a chloropeth."""
+        m = folium.chloropeth(self._gdf, **kwargs)
+        return m
+
+    def markercluster(self, **kwargs):
+        """Plot geodataframe as a markercluster."""
+        m = folium.markercluster(self._gdf, **kwargs)
+        return m
+
+    def heatmap(self, **kwargs):
+        """Plot geodataframe as a heatmap."""
+        m = folium.heatmap(self._gdf, **kwargs)
+        return m
+
+
+@geopandas.tools.util.pd.api.extensions.register_dataframe_accessor("kepler")
+class InteractiveMapKepler:
+    """Extends geopandas to enable easy interactive plotting with kepler.
+
+    Refer kepler/ to call the below functions directly or to customize them
+    """
+
+    def __init__(self, geopandas_obj):
+        """Init InteractiveMap with geodataframe."""
+        self._gdf = geopandas_obj
+
+    def plot(self, **kwargs):
+        """Plot geodataframe as a geojson."""
+        m = kepler.geojson(self._gdf, **kwargs)
+        return m
