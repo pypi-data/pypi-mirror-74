efac24aea94f05b108a03b4201b2de9b87c0a2b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:00 +0000

    Add package distributions-sarish/0.1/distributions_sarish-0.1.tar.gz

---
+++ packages/distributions-sarish/distributions_sarish-0.1.tar.gz/distributions_sarish-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_sarish',
+      version='0.1',
+      author="Sarish Biswas",
+      author_email="sarishbiswas@gmail.com",
+      description='Gaussian and Binomial distributions',
+      packages=['distributions_sarish'],
+      zip_safe=False)
