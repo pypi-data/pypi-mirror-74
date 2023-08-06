eb4d876c32170a447923a3ded5807f1a71e5bbfc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:41 +0000

    Add package distributions-swf/0.1/distributions_swf-0.1.tar.gz

---
+++ packages/distributions-swf/distributions_swf-0.1.tar.gz/distributions_swf-0.1/README.md
@@ -0,0 +1,6 @@
+#DSND_Probability
+
+### Statistical Package containing a Gaussian and Binomial Distribution
+
+### Install:
+pip install dsnd_probability_071720
---
+++ packages/distributions-swf/distributions_swf-0.1.tar.gz/distributions_swf-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_swf',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_swf'],
+      zip_safe=False)
