abd56c8b77fbfaebfac9f6720fbe62e208b918d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:35 +0000

    Add package R0State/0.1/R0State-0.1.tar.gz

---
+++ packages/R0State/R0State-0.1.tar.gz/R0State-0.1/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+     name='R0State',  
+     version='0.1',
+     author="Lukas Merkle",
+     author_email="lukas.merkle@tum.de",
+     description="Returning interpolated R0_BOL",
+     long_description=long_description,
+   		long_description_content_type="text/markdown",
+     url="https://github.com/___empty/___empty",
+     packages=setuptools.find_packages(),
+     # package_data={'dt_access': ['.aws_api_key.txt']},
+     # include_package_data=True,
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+     install_requires=[
+     "pandas",
+     "numpy"
+		]
+ )
