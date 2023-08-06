5bdd39d441fb1e1173a9ecf6cc9723c9b8f7b817
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:46 +0000

    Add package pyspectacle/1.4.3/pyspectacle-1.4.3.tar.gz

---
+++ packages/pyspectacle/pyspectacle-1.4.3.tar.gz/pyspectacle-1.4.3/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+      name="pyspectacle",
+      version="1.4.3",
+      description="SPECTACLE camera calibration module",
+      long_description = long_description,
+      url="https://github.com/monocle-h2020/camera_calibration",
+      author="Olivier Burggraaff",
+      author_email="burggraaff@strw.leidenuniv.nl",
+      packages=["spectacle"],
+      install_requires=["numpy", "scipy", "matplotlib", "rawpy", "exifread", "astropy"],
+      classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+      ],
+      python_requires='>=3.6'
+)
