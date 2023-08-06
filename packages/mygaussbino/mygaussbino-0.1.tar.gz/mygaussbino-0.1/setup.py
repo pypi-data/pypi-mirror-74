7b00f27959e4871860b283e769b1288cfb55d840
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:10 +0000

    Add package mygaussbino/0.1/mygaussbino-0.1.tar.gz

---
+++ packages/mygaussbino/mygaussbino-0.1.tar.gz/mygaussbino-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='mygaussbino',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['mygaussbino'],
+      author='Poorvika A',
+      author_email='poorvikanaidu18@gmail.com',
+      zip_safe=False)
