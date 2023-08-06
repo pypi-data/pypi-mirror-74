a9d5c9700deef09cac81db3a88ae9903381b026c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:37 +0000

    Add package markkk/0.0.1/markkk-0.0.1.tar.gz

---
+++ packages/markkk/markkk-0.0.1.tar.gz/markkk-0.0.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="markkk",
+    version="0.0.1",
+    author="Mark Huang",
+    author_email="mark.h.huang@gmail.com",
+    description="Personal Package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/MarkHershey/python-utils",
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Natural Language :: English",
+    ],
+    python_requires=">=3.6",
+)
+
+
+# Classifiers ref: https://pypi.org/classifiers/
