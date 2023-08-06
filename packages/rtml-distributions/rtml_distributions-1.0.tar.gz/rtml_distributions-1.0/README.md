10dab85bfad7e88ed5f854bbf9b6421094f822ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:13:52 +0000

    Add package rtml-distributions/1.0/rtml_distributions-1.0.tar.gz

---
+++ packages/rtml-distributions/rtml_distributions-1.0.tar.gz/rtml_distributions-1.0/README.md
@@ -0,0 +1,18 @@
+# rtml-distributions package
+
+Sumary of the package
+
+# Files 
+
+- Binomialdistribution.py
+- Gaussiandistribution.py
+- Generaldistribution.py
+- Setup.cfg
+- README.md
+- license.txt
+
+# Instalation
+
+'''
+pip install distributions
+'''
---
+++ packages/rtml-distributions/rtml_distributions-1.0.tar.gz/rtml_distributions-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='rtml_distributions',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['rtml_distributions'],
+      author='Rodolfo Teles',
+      author_email='rodolfojeronimoteles@gmail.com',
+      zip_safe=False)
