765d952cbc07170d48c354e86f6018d07ab030e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:02 +0000

    Add package hystore/0.2.4.dev4/hystore-0.2.4.dev4.tar.gz

---
+++ packages/hystore/hystore-0.2.4.dev4.tar.gz/hystore-0.2.4.dev4/hystore/__init__.py
@@ -0,0 +1,6 @@
+from __future__ import absolute_import
+
+import hystore.contrib
+import hystore.core
+import hystore.covidspecific
+import hystore.processing
---
+++ packages/hystore/hystore-0.2.4.dev4.tar.gz/hystore-0.2.4.dev4/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='hystore',
+    version='0.2.4.dev4',
+    description='High-volume key-value store and analytics, based on hdf5',
+    url='https://github.com/kcl-bmeis/zoe-data-store',
+    author='Ben Murray',
+    author_email='benjamin.murray@kcl.ac.uk',
+    license='http://www.apache.org/licenses/LICENSE-2.0',
+    packages=['hystore'],
+    install_requires=[
+        'numpy',
+        'numba',
+        'pandas',
+        'h5py'
+    ]
+)
