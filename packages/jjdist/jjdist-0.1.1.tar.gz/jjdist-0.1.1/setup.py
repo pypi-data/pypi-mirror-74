a7d0d3175afeb4e652a2819435b2cee097ebc8e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:49 +0000

    Add package jjdist/0.1.1/jjdist-0.1.1.tar.gz

---
+++ packages/jjdist/jjdist-0.1.1.tar.gz/jjdist-0.1.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='jjdist',
+      version='0.1.1',
+      description='Gaussian distributions & Binomial distributions',
+      packages=['jjdist'],
+      zip_safe=False)
