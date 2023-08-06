f388618fc680d7038a25774d3b8959d98acfb8e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:18 +0000

    Add package gcgc/0.12.2/gcgc-0.12.2.tar.gz

---
+++ packages/gcgc/gcgc-0.12.2.tar.gz/gcgc-0.12.2/gcgc/__init__.py
@@ -0,0 +1,15 @@
+# (c) Copyright 2018 Trent Hauck
+# All Rights Reserved
+"""Top-level GCGC module."""
+
+import warnings as _warnings
+
+from gcgc import tokenizer
+from gcgc.tokenizer.kmer_tokenzier import KmerTokenizer
+from gcgc.tokenizer.kmer_tokenzier import SequenceTokenizer
+
+__all__ = ["tokenizer", "KmerTokenizer", "SequenceTokenizer"]
+
+__version__ = "0.12.2"
+
+_warnings.simplefilter(action="ignore", category=PendingDeprecationWarning)
