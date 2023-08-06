35c28fbe7bebae66c0fb42cfe0f9f13951d2973c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:58 +0000

    Add package elegy/0.1.1b1/elegy-0.1.1b1.tar.gz

---
+++ packages/elegy/elegy-0.1.1b1.tar.gz/elegy-0.1.1b1/elegy/__init__.py
@@ -0,0 +1,7 @@
+__version__ = "0.1.0"
+
+from . import losses, metrics, model, regularizers, callbacks
+from .losses import Loss
+from .metrics import Metric
+from .model import Model
+from .module import Module
