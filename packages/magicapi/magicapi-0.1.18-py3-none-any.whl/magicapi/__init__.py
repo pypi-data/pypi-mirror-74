0fddabb16cae3764b1eee06c48269cd9136dd084
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:10 +0000

    Add package magicapi/0.1.18/magicapi-0.1.18-py3-none-any.whl

---
+++ packages/magicapi/magicapi-0.1.18-py3-none-any.whl/magicapi/__init__.py
@@ -0,0 +1,29 @@
+__version__ = "0.1.0"
+
+
+"""Imports the Routes and the app and glues them all together"""
+from fastapi import APIRouter
+
+from .config import settings
+from magicapi.app_factory import create_app, create_handler
+
+app = create_app(config_settings=settings)
+handler = create_handler(app)
+
+background_router = APIRouter()
+# this requires a background router
+from magicapi.Utils.middleware import CallRoute
+
+router = APIRouter(route_class=CallRoute)
+
+
+def add_routes(this_app):
+
+    # import all of the routes
+    from app import Routes
+
+    this_app.include_router(router)
+    this_app.include_router(background_router)
+
+
+add_routes(app)
