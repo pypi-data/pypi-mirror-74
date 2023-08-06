47b31b227f963b749a9ce8ee2418dc96757c82f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:15:26 +0000

    Add package dcube/1.0.1/dcube-1.0.1-py3-none-any.whl

---
+++ packages/dcube/dcube-1.0.1-py3-none-any.whl/fzdata/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding:utf-8 -*- 
+import codecs
+import os
+
+__version__ = '1.0.0'
+__author__ = 'tushare'
+
+
+
+"""
+for  fz api
+"""
+from fzdata.data_pro import (pro_api, pro_bar)
+
+from fzdata.upass import (get_token, set_token)
