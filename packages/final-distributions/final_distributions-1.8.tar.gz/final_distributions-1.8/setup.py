bc131520539ba226dc458755343b5c6c777f3005
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:49 +0000

    Add package final-distributions/1.8/final_distributions-1.8.tar.gz

---
+++ packages/final-distributions/final_distributions-1.8.tar.gz/final_distributions-1.8/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='final_distributions',
+      version='1.8',
+      description='Binomial & Gaussian distributions',
+      packages=['final_distributions'],
+      author = 'Franklin Rivers',
+      author_email = 'letsT17@gmail.com',
+      zip_safe=False)
