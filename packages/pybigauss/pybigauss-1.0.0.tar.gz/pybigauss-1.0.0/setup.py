b68b3de7912a02e89ad11fa5795033734f31c509
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:17 +0000

    Add package pybigauss/1.0.0/pybigauss-1.0.0.tar.gz

---
+++ packages/pybigauss/pybigauss-1.0.0.tar.gz/pybigauss-1.0.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='pybigauss',
+      version='1.0.0',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions'],
+      author='Umar Faruk Abdullahi',
+      author_email='ufakz99@gmail.com',
+      zip_safe=False)
