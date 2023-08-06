074d9986aabbcd356110976b3479d86418560ee2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:58 +0000

    Add package pyANA/0.2.5/pyANA-0.2.5.tar.gz

---
+++ packages/pyANA/pyANA-0.2.5.tar.gz/pyANA-0.2.5/setup.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 12 12:34:04 2019
+
+@author: nelson
+"""
+
+from distutils.core import setup
+
+setup(name='pyANA',
+      version='0.2.5',
+      description='Work with Nanosurf ANA results',
+      author='EN',
+      packages=['pyANA']
+      )
