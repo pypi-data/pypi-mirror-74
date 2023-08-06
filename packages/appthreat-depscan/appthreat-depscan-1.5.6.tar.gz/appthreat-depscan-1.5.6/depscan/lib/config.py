eaac56ebca75e0c74b0e2f89909092cdc2ee564f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:43 +0000

    Add package appthreat-depscan/1.5.6/appthreat-depscan-1.5.6.tar.gz

---
+++ packages/appthreat-depscan/appthreat-depscan-1.5.6.tar.gz/appthreat-depscan-1.5.6/depscan/lib/config.py
@@ -0,0 +1,56 @@
+import os
+
+license_data_dir = os.path.join(
+    os.path.dirname(os.path.realpath(__file__)),
+    "..",
+    "..",
+    "vendor",
+    "choosealicense.com",
+    "_licenses",
+)
+
+# CPE Vendor aliases
+vendor_alias = {
+    "org.apache.tomcat": "apache_tomcat",
+    "org.apache.commons.io": "commons-io",
+    "org.apache.logging.log4j": "log4j",
+    "org.apache.commons.beanutils": "commons-beanutils",
+    "org.apache.commons.collections": "commons-collections",
+    "org.apache.solr": "apache_solr",
+    "org.springframework": "pivotal_software",
+    "io.undertow": "redhat",
+}
+
+# Package aliases
+package_alias = {
+    "struts2-core": "struts",
+    "struts2-rest-plugin": "struts",
+    "struts2-showcase": "struts",
+    "jackson-databind": "jackson",
+    "apache_tomcat": "tomcat",
+    "tomcat_native": "tomcat",
+    "tomcat_connectors": "tomcat",
+    "tomcat_jk_connector": "tomcat",
+    "spring-security-core": "spring_security",
+}
+
+# Default ignore list
+ignore_directories = [
+    ".git",
+    ".svn",
+    ".mvn",
+    ".idea",
+    "dist",
+    "bin",
+    "obj",
+    "backup",
+    "docs",
+    "tests",
+    "test",
+    "tmp",
+    "report",
+    "reports",
+    "node_modules",
+    ".terraform",
+    ".serverless",
+]
