e85efc14b9c8cb7d782b49583380ff88c11d9571
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:51 +0000

    Add package sklearn-pmml-model/0.0.15/sklearn_pmml_model-0.0.15-cp35-cp35m-manylinux2010_i686.whl

---
+++ packages/sklearn-pmml-model/sklearn_pmml_model-0.0.15-cp35-cp35m-manylinux2010_i686.whl/sklearn_pmml_model/tree/__init__.py
@@ -0,0 +1,8 @@
+"""
+The :mod:`sklearn_pmml_model.tree` module includes decision tree-based models for
+classification and regression.
+"""
+
+from .tree import PMMLTreeClassifier, construct_tree, unflatten
+
+__all__ = ["PMMLTreeClassifier", "construct_tree", "unflatten"]
