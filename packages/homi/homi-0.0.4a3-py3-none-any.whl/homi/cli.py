de83408a355a27ca8cf826670b2da8667c584436
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:49 +0000

    Add package homi/0.0.4a3/homi-0.0.4a3-py3-none-any.whl

---
+++ packages/homi/homi-0.0.4a3-py3-none-any.whl/homi/cli.py
@@ -0,0 +1,44 @@
+import sys
+from os.path import dirname
+
+import click
+
+
+@click.group()
+def cli():
+    pass
+
+
+@click.command("run", short_help="Run a development server.")
+@click.argument('file', type=click.Path(exists=True, resolve_path=True), default="app.py")
+@click.option("--host", "-h", default="127.0.0.1", help="The interface to bind to.")
+@click.option("--port", "-p", default='50051', help="The port to bind to.")
+@click.option('--worker', '-w', default=10, type=int)
+@click.option('--health', default=True, type=bool)
+# @click.option(
+#     "--reload/--no-reload",
+#     default=None,
+#     help="Enable or disable the reloader. By default the reloader "
+#          "is active if debug is enabled.",
+# )
+@click.option(
+    "--debug",
+    default=False,
+    help="Server Debug Mode",
+)
+def run_command(file, host, port, worker, health, debug):
+    sys.path.append(dirname(file))
+    import importlib.util
+
+    spec = importlib.util.spec_from_file_location("app", file)
+    foo = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(foo)
+
+    from .server import Server
+    Server(host, port, worker, health).run()
+
+
+cli.add_command(run_command)
+
+if __name__ == '__main__':
+    cli()
