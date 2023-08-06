5b9a2cdb2ce8915c11f7a6f6292f24ae4e4928ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:02 +0000

    Add package Jakaria08-distributions/0.1/Jakaria08_distributions-0.1.tar.gz

---
+++ packages/Jakaria08-distributions/Jakaria08_distributions-0.1.tar.gz/Jakaria08_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='Jakaria08_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['jakaria08_distributions'],
+      zip_safe=False)
