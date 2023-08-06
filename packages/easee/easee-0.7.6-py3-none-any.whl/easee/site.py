65b9434a66dc2d54f41223613ad09d81ac903544
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:51 +0000

    Add package easee/0.7.6/easee-0.7.6-py3-none-any.whl

---
+++ packages/easee/easee-0.7.6-py3-none-any.whl/easee/site.py
@@ -0,0 +1,25 @@
+import logging
+from typing import Any, Dict, List
+
+from .utils import BaseDict, validate_iso8601
+from .charger import Charger
+
+_LOGGER = logging.getLogger(__name__)
+
+
+class Circuit(BaseDict):
+    def __init__(self, entries: Dict[str, Any], easee: Any):
+        super().__init__(entries)
+        self.easee = easee
+
+    def get_chargers(self) -> List[Charger]:
+        return [Charger(c["id"], c["name"], self.easee) for c in self["chargers"]]
+
+
+class Site(BaseDict):
+    def __init__(self, data: Dict[str, Any], easee: Any):
+        super().__init__(data)
+        self.easee = easee
+
+    def get_circuits(self) -> List[Circuit]:
+        return [Circuit(c, self.easee) for c in self["circuits"]]
