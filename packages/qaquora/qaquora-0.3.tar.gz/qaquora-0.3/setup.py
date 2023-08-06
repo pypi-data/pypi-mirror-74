761d9bb1e71002385bf43efaaac334c81462e27a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:19 +0000

    Add package qaquora/0.3/qaquora-0.3.tar.gz

---
+++ packages/qaquora/qaquora-0.3.tar.gz/qaquora-0.3/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+from setuptools import setup
+import os
+
+with open(os.path.join('C:/Users/HP/qaquora','README.md'),encoding='utf-8') as j:
+    long_description = j.read()
+setup(name="qaquora",
+version="0.3",
+description="This is a python package to extra all questions and answers related to any topic on quora",
+author="Yugal Jain",
+packages=['qaquora'],
+long_description = long_description,
+long_description_content_type = 'text/markdown',
+install_requires=['selenium','pandas','beautifulsoup4'])
+
