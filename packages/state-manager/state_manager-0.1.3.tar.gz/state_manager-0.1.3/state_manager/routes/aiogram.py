5e663c99fc230fc6cba7c72cbf595b6fdd6d2935
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:10:59 +0000

    Add package state-manager/0.1.3/state_manager-0.1.3.tar.gz

---
+++ packages/state-manager/state_manager-0.1.3.tar.gz/state_manager-0.1.3/state_manager/routes/aiogram.py
@@ -0,0 +1,57 @@
+from logging import getLogger
+from typing import Callable, Optional, Tuple, Union, List, Set
+
+from aiogram import Dispatcher
+
+from state_manager.middlewares.aiogram import AiogramStateMiddleware
+from state_manager.routes.base import BaseRouter, BaseMainRouter
+from state_manager.storages.base import BaseStorage
+
+logger = getLogger(__name__)
+
+
+class AiogramRouter(BaseRouter):
+    def default_handler_logic(
+        self, handler_name: str, filters: Optional[Tuple[Callable[..., bool], ...]], state_name: Optional[str] = None
+    ) -> Callable:
+        def wrap(callback: Callable):
+            self.registration_state_handler(handler_name, callback, state_name=state_name, filters=filters)
+            return callback
+
+        return wrap
+
+    def message_handler(self, *filters: Callable[..., bool], state_name: Optional[str] = None) -> Callable:
+        return self.default_handler_logic("message", filters, state_name)
+
+    def callback_query_handler(self, *filters: Callable[..., bool], state_name: Optional[str] = None) -> Callable:
+        return self.default_handler_logic("callback_query", filters, state_name)
+
+    def edited_message_handler(self, *filters: Callable[..., bool], state_name: Optional[str] = None) -> Callable:
+        return self.default_handler_logic("edited_message", filters, state_name)
+
+    def channel_post_handler(self, *filters: Callable[..., bool], state_name: Optional[str] = None) -> Callable:
+        return self.default_handler_logic("channel_post", filters, state_name)
+
+    def edited_channel_post_handler(self, *filters: Callable[..., bool], state_name: Optional[str] = None) -> Callable:
+        return self.default_handler_logic("edited_channel_post", filters, state_name)
+
+
+class AiogramMainRouter(AiogramRouter, BaseMainRouter):
+    def __init__(
+        self,
+        dispatcher: Optional[Dispatcher] = None,
+        routers: Optional[Union[List[BaseRouter], Set[BaseRouter]]] = None,
+    ) -> None:
+        super().__init__(routers=routers)
+        self.dispatcher = dispatcher or Dispatcher.get_current()
+
+    def install(
+        self,
+        *,
+        storage: Optional[BaseStorage] = None,
+        default_state_name: Optional[str] = None,
+        is_cached: bool = True,
+    ) -> None:
+        logger.info(f"Install AiogramMainRouter")
+        logger.debug(f"install, {storage=}, {default_state_name=}, {is_cached=}")
+        self.dispatcher.middleware.setup(AiogramStateMiddleware(self, storage, default_state_name, is_cached))
---
+++ packages/state-manager/state_manager-0.1.3.tar.gz/state_manager-0.1.3/state_manager/routes/base.py
@@ -0,0 +1,53 @@
+from abc import ABC, abstractmethod
+from logging import getLogger
+from typing import Callable, Optional, Set, Tuple, Union, List
+
+from state_manager.models.state import StateModel
+from state_manager.storages.base import BaseStorage
+from state_manager.storages.state_storage import StateStorage
+
+logger = getLogger(__name__)
+
+
+class BaseRouter(ABC):
+    def __init__(self, routers: Optional[Union[List["BaseRouter"], Set["BaseRouter"]]] = None) -> None:
+        self.state_storage = StateStorage()
+        if isinstance(routers, list) or isinstance(routers, tuple):
+            routers = set(routers)
+        self.routers: Set["BaseRouter"] = routers or set()
+
+    def registration_state_handler(
+        self,
+        event_type: str,
+        handler: Callable,
+        *,
+        state_name: Optional[Union[str, list, tuple]] = None,
+        filters: Optional[Tuple[Callable[..., bool], ...]] = None,
+    ) -> None:
+        if isinstance(state_name, str) or state_name is None:
+            state_names = [state_name]
+        else:
+            state_names = state_name
+        logger.debug(f"registration_state_handler, {event_type=}, {handler=}, {state_names=}, {filters=}")
+        if isinstance(filters, tuple) and (len(filters) == 0):
+            filters = None
+        for state_name in state_names:
+            state_name = state_name or handler.__name__
+            state = StateModel(name=state_name, event_type=event_type, handler=handler, filters=filters)
+            self.state_storage.add_state(state)
+
+    def include_router(self, router: "BaseRouter") -> None:
+        logger.debug(f"include_router, {router=}")
+        self.routers.add(router)
+
+
+class BaseMainRouter(BaseRouter):
+    @abstractmethod
+    def install(
+        self,
+        *,
+        storage: Optional[BaseStorage] = None,
+        default_state_name: Optional[str] = None,
+        is_cached: bool = True,
+    ) -> None:
+        ...
