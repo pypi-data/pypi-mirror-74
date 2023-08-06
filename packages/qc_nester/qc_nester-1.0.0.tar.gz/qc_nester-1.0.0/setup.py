d0274a772e8b8a2d0405a4331d98fb242724efbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:49 +0000

    Add package qc_nester/1.0.0/qc_nester-1.0.0.tar.gz

---
+++ packages/qc_nester/qc_nester-1.0.0.tar.gz/qc_nester-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name = 'qc_nester',
+        version = '1.0.0',
+        py_modules = ['qc_nester'],
+        author = '31863',
+        author_email = 'qwertyuiopqc@163.com',
+        url = 'http://www.qc.com',
+        description = 'a simple printer of nested lists',
+        )
