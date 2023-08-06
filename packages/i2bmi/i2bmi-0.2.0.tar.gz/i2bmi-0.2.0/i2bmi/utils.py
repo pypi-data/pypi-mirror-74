f2304d9375189c011ed5fccb88e9f27c3fe08b12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:06 +0000

    Add package i2bmi/0.2.0/i2bmi-0.2.0.tar.gz

---
+++ packages/i2bmi/i2bmi-0.2.0.tar.gz/i2bmi-0.2.0/i2bmi/utils.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+def haha(x):
+    """
+    Hello my name is haha
+    
+    Parameters
+    ----------
+    param1
+        The first parameter.
+    param2
+        The second parameter.
+        
+    Returns
+    -------
+    x : whatever
+        whatever you put in x
+    y : whatever
+        whatever you put in y
+        
+    """
+    return x
