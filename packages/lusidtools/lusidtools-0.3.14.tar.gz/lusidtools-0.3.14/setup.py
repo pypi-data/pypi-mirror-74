d15c24ad1aacdc57edcce572d5a6db3fb3597701
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:06 +0000

    Add package lusidtools/0.3.14/lusidtools-0.3.14.tar.gz

---
+++ packages/lusidtools/lusidtools-0.3.14.tar.gz/lusidtools-0.3.14/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages  # noqa: H301
+
+version = {}
+with open("./__version__.py") as fp:
+    exec(fp.read(), version)
+
+setup(
+    name="lusidtools",
+    version=version["__version__"],
+    description="Python Tools for LUSID",
+    url="https://github.com/finbourne/lusid-python-tools",
+    author="FINBOURNE Technology",
+    author_email="engineering@finbourne.com",
+    license="MIT",
+    keywords=["FINBOURNE", "LUSID", "LUSID API", "python"],
+    packages=find_packages(exclude=["tests*"]),
+    install_requires=[
+        "lusid-sdk-preview>=0.11.*",
+        "msrest==0.6.*",
+        "xlrd==1.*",
+        "pandas==1.0.*",
+        "numpy==1.*",
+        "pytz==2019.*",
+        "tqdm==4.*",
+        "requests==2.*",
+        "coloredlogs==10.*",
+        "detect_delimiter==0.1.*",
+        "pyyaml==5.*",
+        "flatten-json==0.1.*",
+    ],
+    include_package_data=True,
+    python_requires=">=3.7",
+    entry_points={
+        "console_scripts": [
+            "lusidtools=lusidtools.commands.commands:main",
+            "upsert_instruments=lusidtools.apps.upsert_instruments:main",
+            "upsert_holdings=lusidtools.apps.upsert_holdings:main",
+            "upsert_quotes=lusidtools.apps.upsert_quotes:main",
+            "upsert_transactions=lusidtools.apps.upsert_transactions:main",
+        ],
+    },
+)
