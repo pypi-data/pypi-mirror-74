d5e421a2e457af72ee1b899a92a92671510c3d92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:47 +0000

    Add package tellus-api-client/0.0.5/tellus_api_client-0.0.5-py3-none-any.whl

---
+++ packages/tellus-api-client/tellus_api_client-0.0.5-py3-none-any.whl/tellus_api_client/main.py
@@ -0,0 +1,22 @@
+import requests
+
+from skimage import io
+from io import BytesIO
+from typing import Dict
+
+DOMAIN = "tellusxdp.com"
+
+
+class TellusApiClient():
+    def __init__(self, token: str):
+        self.token = token
+
+    def get_blend(self, zoom: int, xtile: int, ytile: int, params: Dict = {}):
+        url: str = 'https://gisapi.{}/blend/{}/{}/{}.png'.format(
+            DOMAIN, zoom, xtile, ytile)
+        headers: dict = {
+            "Authorization": "Bearer " + self.token
+        }
+
+        r = requests.get(url, headers=headers, params=params)
+        return io.imread(BytesIO(r.content))
