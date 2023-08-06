8355babea5666475dfba78bb0789753318cd4625
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:40 +0000

    Add package gb-krkk-distributions/1.2/gb_krkk_distributions-1.2.tar.gz

---
+++ packages/gb-krkk-distributions/gb_krkk_distributions-1.2.tar.gz/gb_krkk_distributions-1.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='gb_krkk_distributions',
+      version='1.2',
+      description='Gaussian and binomial distributions',
+      packages=['gb_krkk_distributions'],
+      author = 'Rushi',
+      author_email = 'rushikirankuchipudi@gmail.com',
+      zip_safe=False)
