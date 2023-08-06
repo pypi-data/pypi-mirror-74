c49118649263a5db88fc6d26d6c89bd4b1c923ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:17 +0000

    Add package sqs-workers/0.5.3/sqs_workers-0.5.3-py2.py3-none-any.whl

---
+++ packages/sqs-workers/sqs_workers-0.5.3-py2.py3-none-any.whl/sqs_workers/__init__.py
@@ -0,0 +1,15 @@
+from sqs_workers.backoff_policies import (  # noqa: F401
+    DEFAULT_BACKOFF,
+    IMMEDIATE_RETURN,
+    ConstantBackoff,
+    ExponentialBackoff,
+)
+from sqs_workers.exceptions import SQSError  # noaq: F401
+from sqs_workers.memory_sqs import MemorySession  # noqa: F401
+from sqs_workers.queue import JobQueue, RawQueue  # noqa: F401
+from sqs_workers.sqs_env import SQSEnv  # noqa: F401
+from sqs_workers.sqs_manage import (  # noqa: F401
+    create_fifo_queue,
+    create_standard_queue,
+    delete_queue,
+)
