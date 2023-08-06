96c0d5609f85b9b296a7161c424cbfae7f6015b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:20 +0000

    Add package SacraMathEngine/0.0.9/SacraMathEngine-0.0.9.tar.gz

---
+++ packages/SacraMathEngine/SacraMathEngine-0.0.9.tar.gz/SacraMathEngine-0.0.9/SacraMathEngine/Triangle.py
@@ -0,0 +1,60 @@
+"""
+from .Vector import vec3d, vec4d
+from .Matrix import matrix3d
+"""
+from SacraMathEngine import *
+
+
+class Triangle:
+    def __init__(self, vec1, vec2, vec3, reminder = 1):
+        """
+        if not isinstance((vec1, vec2, vec3), vec3d):
+            raise TypeError("Wrong format in Triangle")
+        """
+        self.vec1 = vec1
+        self.vec2 = vec2
+        self.vec3 = vec3
+        self.reminder = reminder
+        self.set = [self.vec1, self.vec2, self.vec3]
+
+
+    def __str__(self):
+        """Print statement for Triangle-object"""
+        return f'[{self.vec1}, {self.vec2}, {self.vec3}]'
+
+    def __add__(self, vector):
+        if isinstance(vector, (vec3d, vec4d)):
+            return Triangle(self.vec1 + vector, self.vec2 + vector, self.vec3 + vector)
+        else:
+            pass
+
+    def __sub__(self, vector):
+        if isinstance(vector, (vec3d, vec4d)):
+            return Triangle(self.vec1 - vector, self.vec2 - vector, self.vec3 - vector)
+
+    def __len__(self):
+        """Return the length of triangle, which is 3 by definition. """
+        return len(self.set)
+
+    def __getitem__(self, index):
+        """Get the vector defined at index 'index', given the attribute 'index'."""
+        return self.set[index]
+
+    def __mul__(self, Scalar):
+        """Returns a Triangle-object where each vector i multiplied by the scalar 'Scalar'."""
+        if isinstance(Scalar, (float, int)):
+            return Triangle(self.vec1 * Scalar, self.vec2 * Scalar, self.vec3 * Scalar)
+        else:
+            pass
+
+    def __len__(self):
+        return len(self.set)
+
+
+
+    def normvector(self):
+        """Calculates the facing of the Triangle."""
+        vec1 = self.vec1 - self.vec3
+        vec2 = self.vec2 - self.vec3
+        normvector = vec1.cross(vec2)
+        return normvector
---
+++ packages/SacraMathEngine/SacraMathEngine-0.0.9.tar.gz/SacraMathEngine-0.0.9/SacraMathEngine/__init__.py
@@ -0,0 +1,7 @@
+
+#from .Matrix import matrix3d, Matrix4d
+from .Vector import vec3d, vec4d
+from .Triangle import Triangle
+from .MeshObject import MeshObject
+from .MeshObject import PathError
+from .MeshObject import MeshObjectError
