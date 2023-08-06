5e2a34ee37c18db97dd5ca9d5d1634298ff176de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:00 +0000

    Add package euroleague-scrapping/0.0.1.dev13/euroleague_scrapping-0.0.1.dev13.tar.gz

---
+++ packages/euroleague-scrapping/euroleague_scrapping-0.0.1.dev13.tar.gz/euroleague_scrapping-0.0.1.dev13/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    setup_requires=["pbr"],
+    pbr=True,
+    # name='euroleague_scrapping',
+    # packages=['euroleague_scrapping'],
+    # url='https://github.com/newstatseu/euroleague_scrapping',
+    # license='Apache 2.0',
+    # author='NewstatsEu',
+    # author_email='euroleague.dataguy@gmail.com',
+    # description='Eurocup/Euroleague Scrapping',
+    # long_description='Eurocup/Euroleague Scraping'
+)
