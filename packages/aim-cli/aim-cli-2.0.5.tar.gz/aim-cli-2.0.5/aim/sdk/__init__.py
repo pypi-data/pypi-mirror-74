7facbe43f23e450df0c4dfa24fde404a84578d7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:10 +0000

    Add package aim-cli/2.0.5/aim-cli-2.0.5.tar.gz

---
+++ packages/aim-cli/aim-cli-2.0.5.tar.gz/aim-cli-2.0.5/aim/sdk/__init__.py
@@ -0,0 +1,15 @@
+from aim.sdk.track import *
+from aim.sdk.init import *
+
+
+# Serializable classes references
+metric = 'Metric'
+hyperparams = 'HyperParameters'
+dataset = 'Dataset'
+map = 'Map'
+nested_map = 'NestedMap'
+checkpoint = 'Checkpoint'
+
+# Serializable aliases
+params = hyperparams
+dictionary = nested_map
