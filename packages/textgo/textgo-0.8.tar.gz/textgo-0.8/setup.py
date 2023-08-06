02c4556220263406c2c4060723560c7169e3c8fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:28 +0000

    Add package textgo/0.8/textgo-0.8.tar.gz

---
+++ packages/textgo/textgo-0.8.tar.gz/textgo-0.8/setup.py
@@ -0,0 +1,39 @@
+# coding: utf-8
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="textgo",
+    version="0.8",
+    author="Pairui Li",
+    author_email="953383269@qq.com",
+    description="Let's go and play with text!",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Lipairui/textgo",
+    packages=setuptools.find_packages(),
+    include_package_data = True,
+    #packages=['textgo','textgo.classifier'],
+    install_requires=[ # 依赖列表
+        'jieba>=0.42.1',
+        'tqdm>=4.46.0',
+        'transformers>=2.3.0',
+        'numpy>=1.16.4',
+        'torch>=1.5.0+cu101',
+        'pandas>=1.0.3',
+        'gensim>=3.8.3',
+        'faiss_cpu>=1.6.3',
+        'scikit_learn>=0.23.1',
+        'xgboost>=1.1.0'
+    ],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+    classifiers=(
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
