f3c100eb79d97cca0426f75271f08a2f99872c8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:50 +0000

    Add package charm-tools/2.7.8/charm-tools-2.7.8.tar.gz

---
+++ packages/charm-tools/charm-tools-2.7.8.tar.gz/charm-tools-2.7.8/charmtools/build/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+
+import charmtools.build.tactics
+import charmtools.build.errors
+from charmtools.build.builder import (
+    Builder,
+    Layer,
+    Interface,
+    Configable,
+    Fetched,
+    log,
+)
+
