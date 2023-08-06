83c3b11f05e50558a9b0ecb8b375daa294ad734e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:57 +0000

    Add package bsdiff4/1.2.0/bsdiff4-1.2.0.tar.gz

---
+++ packages/bsdiff4/bsdiff4-1.2.0.tar.gz/bsdiff4-1.2.0/bsdiff4/__init__.py
@@ -0,0 +1,9 @@
+from .format import diff, patch, file_diff, file_patch, file_patch_inplace
+
+
+__version__ = "1.2.0"
+
+
+def test(verbosity=1):
+    from .test_all import run
+    return run(verbosity=verbosity)
