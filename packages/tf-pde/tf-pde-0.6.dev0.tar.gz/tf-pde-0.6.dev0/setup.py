38302915c3b8b696969a07adfb247e312f4fe0d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:11 +0000

    Add package tf-pde/0.6.dev0/tf-pde-0.6.dev0.tar.gz

---
+++ packages/tf-pde/tf-pde-0.6.dev0.tar.gz/tf-pde-0.6.dev0/setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 10 19:23:09 2020
+
+@author: Vicky
+
+Python Package Setup
+"""
+import io
+from os import path
+from setuptools import setup
+from setuptools import find_packages
+
+with io.open("README.md", "r", encoding="utf-8") as f:
+    long_desc = f.read()
+
+# with open("requirements.txt", "r") as f:
+#     install_requires = [x.strip() for x in f.readlines()]
+    
+setup(
+    name='tf-pde',
+    version='0.6dev',
+    description="Deep learning library for solving partial differential equations",
+    author="Vignesh Gopakumar",
+    author_email="vignesh7g@gmail.com",
+    url = "https://github.com/gitvicky/tf-pde",
+    packages = find_packages(),
+    license='MIT',
+    long_description = long_desc,
+    long_description_content_type = "text/markdown",
+    install_requires = ['numpy==1.18.5',
+                        'matplotlib==3.2.1',
+                        'scipy==1.4.1',
+                        'sympy==1.6',
+                        'tensorflow==2.2.0',
+                        'tensorflow-probability==0.10.0',
+                        'pydoe==0.3.8',
+    ],
+
+)
