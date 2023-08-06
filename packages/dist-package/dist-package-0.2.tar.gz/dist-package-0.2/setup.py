292c02a9451d71300e745133b57caaaecb562509
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:52 +0000

    Add package dist-package/0.2/dist-package-0.2.tar.gz

---
+++ packages/dist-package/dist-package-0.2.tar.gz/dist-package-0.2/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='dist-package',
+      version='0.2',
+      description='Gaussian distributions',
+      packages=['dist-package'],
+      zip_safe=False)
