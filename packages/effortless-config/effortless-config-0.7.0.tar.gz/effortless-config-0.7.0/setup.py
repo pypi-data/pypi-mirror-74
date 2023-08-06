043c9b10425f5a6ea92877b9e711df8b73283f38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:21 +0000

    Add package effortless-config/0.7.0/effortless-config-0.7.0.tar.gz

---
+++ packages/effortless-config/effortless-config-0.7.0.tar.gz/effortless-config-0.7.0/setup.py
@@ -0,0 +1,24 @@
+import os
+from setuptools import setup, find_packages
+
+cwd = os.path.dirname(os.path.abspath(__file__))
+
+with open(os.path.join(cwd, 'effortless_config', 'VERSION')) as f:
+    version = f.read().strip()
+
+with open(os.path.join(cwd, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='effortless-config',
+    description='Globally scoped configuration with argparse integration',
+    url='https://github.com/andreasjansson/effortless-config',
+    version=version,
+    packages=find_packages(exclude=['test', '*.test', '*.test.*']),
+    include_package_data=True,
+    classifiers=['Programming Language :: Python'],
+    install_requires=[],
+    python_requires='>=3.6',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+)
