b63990a1c0d98d7516c010c878b1cb4d33e44959
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:01 +0000

    Add package rsherer-udacity-gaussian-distributions/0.9/rsherer_udacity_gaussian_distributions-0.9.tar.gz

---
+++ packages/rsherer-udacity-gaussian-distributions/rsherer_udacity_gaussian_distributions-0.9.tar.gz/rsherer_udacity_gaussian_distributions-0.9/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='rsherer_udacity_gaussian_distributions',
+      version='0.9',
+      description='Gaussian and Binomial distributions',
+      packages=['rsherer_udacity_gaussian_distributions'],
+      author = 'Robert Sherer',
+      author_email = 'rsherer@gmail.com',
+      zip_safe=False)
