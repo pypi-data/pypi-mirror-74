23d4afcf1b1b71582f4401d628e5e1f7072b6c4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:47 +0000

    Add package firefly-iaaa/0.1.3/firefly_iaaa-0.1.3-py3-none-any.whl

---
+++ packages/firefly-iaaa/firefly_iaaa-0.1.3-py3-none-any.whl/firefly_iaaa/application/service/get_token_access_rights.py
@@ -0,0 +1,20 @@
+from __future__ import annotations
+
+import firefly as ff
+
+
+@ff.query_handler()
+class GetTokenAccessRights(ff.ApplicationService):
+    _repository: ff.Repository = None
+
+    def __call__(self, event: dict, **kwargs):
+        print(event)
+        event['response'] = {
+            'claimsOverrideDetails': {
+                'groupOverrideDetails': {
+                    'groupsToOverride': ['groupA', 'groupB'],
+                    'iamRolesToOverride': ['distributed_events.Activity.write', 'distributed_events.Activity.admin']
+                }
+            }
+        }
+        return event
