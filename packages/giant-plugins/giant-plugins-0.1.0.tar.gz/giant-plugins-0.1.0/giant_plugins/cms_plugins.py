a0483208f5d9117545483508c52ce2b410d61c9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:55 +0000

    Add package giant-plugins/0.1.0/giant-plugins-0.1.0.tar.gz

---
+++ packages/giant-plugins/giant-plugins-0.1.0.tar.gz/giant-plugins-0.1.0/giant_plugins/cms_plugins.py
@@ -0,0 +1,6 @@
+# flake8: noqa
+from .content_width_image.cms_plugins import *
+from .content_width_video.cms_plugins import *
+from .page_card.cms_plugins import *
+from .pullquote.cms_plugins import *
+from .rich_text.cms_plugins import *
