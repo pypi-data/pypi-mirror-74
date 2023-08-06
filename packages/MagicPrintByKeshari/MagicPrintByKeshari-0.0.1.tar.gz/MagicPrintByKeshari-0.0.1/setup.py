ce28e90fde3da694e562f9ba0c638c75bd674433
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:55 +0000

    Add package MagicPrintByKeshari/0.0.1/MagicPrintByKeshari-0.0.1.tar.gz

---
+++ packages/MagicPrintByKeshari/MagicPrintByKeshari-0.0.1.tar.gz/MagicPrintByKeshari-0.0.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+ 
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+ 
+setup(
+  name='MagicPrintByKeshari',
+  version='0.0.1',
+  url='https://knpjg.blogspot.com/',  
+  author='KESHARI NANDANA PRATAP',
+  author_email='kesharipratap52@gmail.com',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='Shortcut,System', 
+  packages=find_packages(),
+  install_requires=[''] 
+)
