b97385ecb2cf8183e4ca208d746556e08a1f1e64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:36 +0000

    Add package infrastructure/2.0.7/infrastructure-2.0.7-py3.7.egg

---
+++ packages/infrastructure/infrastructure-2.0.7-py3.7.egg/infrastructure/variables/hb_conf.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# @Author: yongfanmao
+# @Date:   2020-05-29 19:01:35
+# @E-mail: maoyongfan@163.com
+# @Last Modified by:   yongfanmao
+# @Last Modified time: 2020-06-02 20:46:42
+
+JAVA_COV = {
+	"repoDir": '/home/maoyongfan10020/jc/repo',
+	"serviceRepoDir": '/home/maoyongfan10020/jc/repo/{service_name}',
+	"jarDir": "/home/maoyongfan10020/jc/{service_name}",
+	"reportDir": "/home/maoyongfan10020/jc/{service_name}/report/{recordID}",
+	"copySourceDir": "/home/maoyongfan10020/jc/{service_name}/source",
+	"jacocoDir": "/home/maoyongfan10020/jc/lib",
+	"destfile" : "/home/maoyongfan10020/jc/{service_name}/jacoco-it-{index}.exec"
+}
