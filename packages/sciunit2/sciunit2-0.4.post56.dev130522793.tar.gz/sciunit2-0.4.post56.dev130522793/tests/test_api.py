33ca1a62d6c64302159c82cad24d37920dbd53f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:13:45 +0000

    Add package sciunit2/0.4.post56.dev130522793/sciunit2-0.4.post56.dev130522793.tar.gz

---
+++ packages/sciunit2/sciunit2-0.4.post56.dev130522793.tar.gz/sciunit2-0.4.post56.dev130522793/tests/test_api.py
@@ -0,0 +1,13 @@
+# from __future__ import absolute_import
+#
+# from sciunit2 import sciunit
+# from sciunit2.sciunit import Sciunit
+#
+#
+# class TestAPI(sciunit.LocalCase):
+#     def test_all(self):
+#         _sciunit = Sciunit()
+#         _sciunit.create('project1')
+#         _sciunit.exec('pwd')
+#         # _sciunit.commit()
+#         _sciunit.repeat('e1')
