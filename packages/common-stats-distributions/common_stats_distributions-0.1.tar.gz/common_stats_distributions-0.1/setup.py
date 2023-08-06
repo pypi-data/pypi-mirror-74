214476e555dee57c6d7b2be8985adb17d234d38c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:08 +0000

    Add package common-stats-distributions/0.1/common_stats_distributions-0.1.tar.gz

---
+++ packages/common-stats-distributions/common_stats_distributions-0.1.tar.gz/common_stats_distributions-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='common_stats_distributions',
+      version='0.1',
+      description='Gaussian and binomial distributions',
+      packages=['common_stats_distributions'], 
+      author = 'Chaojie Wang',
+      zip_safe=False)
