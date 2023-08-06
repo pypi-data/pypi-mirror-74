facddb02756d70a1894bce850831dd4e2545f246
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:12 +0000

    Add package wqrfnium/0.3.0/wqrfnium-0.3.0.tar.gz

---
+++ packages/wqrfnium/wqrfnium-0.3.0.tar.gz/wqrfnium-0.3.0/setup.py
@@ -0,0 +1,21 @@
+#############################################
+# File Name: setup.py
+# Author: wqrf
+# Mail: 1074321997@qq.com
+# Created Time:  2019-10-08 14:39:00
+#############################################
+
+from setuptools import setup, find_packages
+
+setup(
+    name = "wqrfnium",
+    version = "0.3.0",
+    keywords = ("wqrf"),
+    description = "to fix the selenium error which caused by ui change,wqrfnium can find element!",
+    long_description = "If you can't find element,please use this.And you can get help in https://github.com/Woqurefan/wqrf_selenium",
+    license = "MIT",
+    url = "https://github.com/Woqurefan/wqrf_selenium",
+    author = "wqrf",
+    author_email = "1074321997@qq.com",
+    packages = ['wqrfnium'],
+)
