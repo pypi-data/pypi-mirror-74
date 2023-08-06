dd9a8b6231d95ba9b6dc5e924db31ed02ef766e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:13 +0000

    Add package glasskit/3.10.2/glasskit-3.10.2-py3-none-any.whl

---
+++ packages/glasskit/glasskit-3.10.2-py3-none-any.whl/glasskit/json.py
@@ -0,0 +1,26 @@
+from flask.json import JSONEncoder
+from bson import ObjectId, Timestamp
+from .uorm.models.base_model import BaseModel
+from .uorm.db import ObjectsCursor
+import simplejson
+
+
+def default(o):  # pylint: disable=method-hidden
+
+    if isinstance(o, ObjectId):
+        return str(o)
+    if isinstance(o, Timestamp):
+        return o.time
+    if isinstance(o, (ObjectsCursor, set)):
+        return list(o)
+    if isinstance(o, BaseModel):
+        return o.to_dict()
+    return o
+
+
+def dumps(o, default=default, **kwargs):
+    return simplejson.dumps(o, default=default, **kwargs)
+
+
+def loads(text: str, **kwargs):
+    return simplejson.loads(text, **kwargs)
