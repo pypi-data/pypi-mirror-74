1872e13bcedb3578b9b6ac380bd16d71eba81c5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:43 +0000

    Add package willing-zg/1.1.0/willing_zg-1.1.0-py3-none-any.whl

---
+++ packages/willing-zg/willing_zg-1.1.0-py3-none-any.whl/willing_zg/all_components.py
@@ -0,0 +1,17 @@
+from zygoat.components import Component
+from zygoat.components.backend.reformat import reformat
+
+from .deployment import deployment
+from .email import email
+from .simple_jwt import simple_jwt
+from .tokens import token_util
+from .chat import chat
+
+
+class AllComponents(Component):
+    pass
+
+
+all_components = AllComponents(
+    sub_components=[deployment, email, simple_jwt, token_util, reformat, chat]
+)
