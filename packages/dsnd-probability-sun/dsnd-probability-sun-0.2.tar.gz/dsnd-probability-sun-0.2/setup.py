1de4e4d3f818dcac444dfb4e620d33df139d31fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:50 +0000

    Add package dsnd-probability-sun/0.2/dsnd-probability-sun-0.2.tar.gz

---
+++ packages/dsnd-probability-sun/dsnd-probability-sun-0.2.tar.gz/dsnd-probability-sun-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dsnd-probability-sun',
+      version='0.2',
+      description='Gaussian and Binomial Distribution distributions',
+      packages=['dsnd-probability-sun'],
+      author='Sunit Bachal',
+      author_email='sun@ba.com',
+      zip_safe=False)
